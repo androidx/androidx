c9cf2eb0a9b6694d0fda3dbc313844955db60820
Adam Powell <adamp@google.com>
2012-01-12 19:24:13 -0800

    Add app navigation helper code to the support library.
    Add TaskStackBuilder for synthesizing back stacks for cross-task
    navigation on Android 3.0 and newer.
    
    Add NavUtils for general navigation helper code.
    
    Add NotificationCompat.Builder as a partial backport of Android 3.0's
    Notification.Builder. (It does not currently support progress
    notifications.)
    
    Change-Id: I2d7d4dab3dd26e34f6b2210d0f0f3909f2162d6f

--- v4/honeycomb/android/support/v4/app/ActivityCompatHoneycomb.java
+++ v4/honeycomb/android/support/v4/app/ActivityCompatHoneycomb.java
@@ -17,6 +17,7 @@
 package android.support.v4.app;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -27,6 +28,10 @@ import java.io.PrintWriter;
 class ActivityCompatHoneycomb {
     static void invalidateOptionsMenu(Activity activity) {
         activity.invalidateOptionsMenu();
+    }
+
+    static void startActivities(Activity activity, Intent[] intents) {
+        activity.startActivities(intents);
     }
 
     static void dump(Activity activity, String prefix, FileDescriptor fd,
