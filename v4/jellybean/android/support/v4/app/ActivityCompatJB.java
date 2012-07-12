3a96487b54eca412f51ad00b8f8096055e94dcbb
Jake Wharton <jakewharton@gmail.com>
2012-07-12 16:27:00 -0700

    Add compatibility implementation of ActivityOptions and start activity.
    This will allow for constructing ActivityOptions and starting an activity
    using them in a backwards-compatible way. The implementation is only used
    on API 16 and newer. On platforms prior to API 16 the traditional
    startActivity and startActivityForResult methods are used.
    
    Change-Id: I86c0df28dadc391b340303d448c70a25d33a76de

---
+++ v4/jellybean/android/support/v4/app/ActivityCompatJB.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.v4.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+class ActivityCompatJB {
+    public static void startActivity(Context context, Intent intent, Bundle options) {
+        context.startActivity(intent, options);
+    }
+
+    public static void startActivityForResult(Activity activity, Intent intent, int requestCode, Bundle options) {
+        activity.startActivityForResult(intent, requestCode, options);
+    }
+}
