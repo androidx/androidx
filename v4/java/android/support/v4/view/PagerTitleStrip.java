4295e22421c26dd4f24cc0780a594d08a74a5cca
Adam Powell <adamp@google.com>
2012-08-23 14:50:41 -0700

    Fix PagerTitleStrip positioning during layout
    Alternative patch for the proposed fix at
    https://android-review.googlesource.com/#/c/41811/
    
    Preserve the correct titles when offset by more than 50% of a page
    during a layout pass.
    
    Change-Id: If9bbd5ce957ce6229bfea5eb7b8bcfef1e1a14b5

--- v4/java/android/support/v4/view/PagerTitleStrip.java
+++ v4/java/android/support/v4/view/PagerTitleStrip.java
@@ -448,7 +448,12 @@ public class PagerTitleStrip extends ViewGroup implements ViewPager.Decor {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (mPager != null) {
             final float offset = mLastKnownPositionOffset >= 0 ? mLastKnownPositionOffset : 0;
-            updateTextPositions(mPager.getCurrentItem(), offset, true);
+            int position = mPager.getCurrentItem();
+            if (offset > 0.5f) {
+                // Consider ourselves to be on the next page when we're 50% of the way there.
+                position++;
+            }
+            updateTextPositions(position, offset, true);
         }
     }
 
