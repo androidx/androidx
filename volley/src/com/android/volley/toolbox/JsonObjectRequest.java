eceb8e275fd07e44b855ad27f73e363ea4e14d62
Christoph Studer <chstuder@google.com>
2012-11-02 11:16:56 -0700

    Use GET or POST automatically in JsonObjectRequest
    Bug: 7415339
    Change-Id: I3ea5d3bb21c7813516c939b2c26d5e51880dde7c

--- volley/src/com/android/volley/toolbox/JsonObjectRequest.java
+++ volley/src/com/android/volley/toolbox/JsonObjectRequest.java
@@ -35,16 +35,29 @@ public class JsonObjectRequest extends JsonRequest<JSONObject> {
 
     /**
      * Creates a new request.
+     * @param method the HTTP method to use
      * @param url URL to fetch the JSON from
      * @param jsonRequest A {@link JSONObject} to post with the request. Null is allowed and
      *   indicates no parameters will be posted along with request.
      * @param listener Listener to receive the JSON response
      * @param errorListener Error listener, or null to ignore errors.
      */
+    public JsonObjectRequest(int method, String url, JSONObject jsonRequest,
+            Listener<JSONObject> listener, ErrorListener errorListener) {
+        super(method, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
+                    errorListener);
+    }
+
+    /**
+     * Constructor which defaults to <code>GET</code> if <code>jsonRequest</code> is
+     * <code>null</code>, <code>POST</code> otherwise.
+     *
+     * @see #JsonObjectRequest(int, String, JSONObject, Listener, ErrorListener)
+     */
     public JsonObjectRequest(String url, JSONObject jsonRequest, Listener<JSONObject> listener,
             ErrorListener errorListener) {
-        super(Method.GET, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
-                    errorListener);
+        this(jsonRequest == null ? Method.GET : Method.POST, url, jsonRequest,
+                listener, errorListener);
     }
 
     @Override
