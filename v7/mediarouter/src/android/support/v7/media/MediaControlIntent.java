55eb54486dde6c1eb7857bdfff29f420fb2ecdfb
Jeff Brown <jeffbrown@google.com>
2013-04-01 19:42:08 -0700

    Declare metadata and status information.
    Bug: 8175766
    Change-Id: Ib1d15e03dffdd4c5b9a20c7be278e6fc898d30fa

--- v7/mediarouter/src/android/support/v7/media/MediaControlIntent.java
+++ v7/mediarouter/src/android/support/v7/media/MediaControlIntent.java
@@ -16,6 +16,7 @@
 
 package android.support.v7.media;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -97,16 +98,26 @@ public final class MediaControlIntent {
      * Once initiated, playback of the specified content will be queued and managed
      * independently by the destination.  The application will receive status
      * and progress updates as the content is played.
+     * </p><p>
+     * If the data uri specifies an HTTP or HTTPS scheme, then the destination is
+     * responsible for following HTTP redirects to a reasonable depth of at least 3
+     * levels as might typically be handled by a web browser.  If an HTTP error
+     * occurs, then the destination should send a status update back to the client
+     * indicating the {@link MediaStreamStatus#PLAYBACK_STATE_ERROR error}
+     * {@link MediaStreamStatus#KEY_PLAYBACK_STATE state}
+     * and include the {@link MediaStreamStatus#KEY_HTTP_STATUS_CODE HTTP status code}.
      * </p>
      *
      * <h3>Request parameters</h3>
      * <ul>
-     * <li>{@link #EXTRA_QUEUE_BEHAVIOR}: specifies when the content should be played.
+     * <li>{@link #EXTRA_STREAM_QUEUE_BEHAVIOR}: specifies when the content should be played.
      * <li>{@link #EXTRA_STREAM_POSITION}: specifies the initial start position of the
      * content stream.
      * <li>{@link #EXTRA_STREAM_METADATA}: specifies metadata associated with the
      * content stream such as the title of a song.
-     * <li>{@link #EXTRA_HTTP_HEADERS}: specifies HTTP headers to supply to the
+     * <li>{@link #EXTRA_STREAM_STATUS_UPDATE_RECEIVER}: specifies a {@link PendingIntent}
+     * for a broadcast receiver that will receive status updates.
+     * <li>{@link #EXTRA_STREAM_HTTP_HEADERS}: specifies HTTP headers to supply to the
      * server when requesting the content stream.
      * </ul>
      *
@@ -115,6 +126,13 @@ public final class MediaControlIntent {
      * <li>{@link #EXTRA_STREAM_ID}: specifies a string identifier to use to refer
      * to the media stream in subsequent requests such as {@link #ACTION_SEEK}.
      * </ul>
+     *
+     * <h3>Status updates</h3>
+     * <p>
+     * If the client supplies a {@link #EXTRA_STREAM_STATUS_UPDATE_RECEIVER status update receiver}
+     * then the media route provider is responsible for sending status updates to the receiver
+     * when significant state changes occur.
+     * </p>
      *
      * <h3>Example</h3>
      * <pre>
@@ -244,14 +262,6 @@ public final class MediaControlIntent {
      * </p><p>
      * This action asks a remote playback route to provide updated status information
      * about playback of the specified stream.
-     * </p><p>
-     * If the route caches this information, it must take care to ensure that
-     * it is in sync with all previously completed control actions.  For example,
-     * if an applications sends {@link #ACTION_PAUSE} to pause a content stream,
-     * waits for the callback to indicate that the pause completed successfully,
-     * then sends {@link #ACTION_GET_STATUS} to query the current status, it
-     * must consistently observe that the stream is now paused (assuming no other
-     * application has acted upon the content stream in the meantime).
      * </p>
      *
      * <h3>Request parameters</h3>
@@ -272,52 +282,88 @@ public final class MediaControlIntent {
     public static final String ACTION_GET_STATUS = "android.media.intent.action.GET_STATUS";
 
     /**
+     * Media control action: Get progress.
+     * <p>
+     * Used with routes that support {@link #CATEGORY_REMOTE_PLAYBACK remote playback}
+     * media control.
+     * </p><p>
+     * This action asks a remote playback route to provide the stream playback
+     * progress information.  The client may use this information to provide feedback
+     * to the user about the current playback position and overall duration.
+     * </p>
+     *
+     * <h3>Request parameters</h3>
+     * <ul>
+     * <li>{@link #EXTRA_STREAM_ID}: specifies the stream id of the stream to be
+     * queried.  This value was returned as a result from the
+     * {@link #ACTION_PLAY play} action.
+     * </ul>
+     *
+     * <h3>Result data</h3>
+     * <ul>
+     * <li>{@link #EXTRA_STREAM_POSITION}: specifies the stream playback position.
+     * <li>{@link #EXTRA_STREAM_DURATION}: specifies the stream overall duration.
+     * </ul>
+     *
+     * @see MediaRouter.RouteInfo#sendControlRequest
+     * @see #CATEGORY_REMOTE_PLAYBACK
+     */
+    public static final String ACTION_GET_PROGRESS = "android.media.intent.action.GET_PROGRESS";
+
+    /**
      * Integer extra: Queue behavior.
      * <p>
      * Used with {@link #ACTION_PLAY} to specify when the requested content should be
      * played.  The default is to play the content immediately.
      * </p><p>
-     * The value must be one of {@link #QUEUE_BEHAVIOR_PLAY_NOW},
-     * {@link #QUEUE_BEHAVIOR_PLAY_NEXT}, or {@link #QUEUE_BEHAVIOR_PLAY_LATER}.
+     * The value must be one of {@link #STREAM_QUEUE_BEHAVIOR_PLAY_NOW},
+     * {@link #STREAM_QUEUE_BEHAVIOR_PLAY_NEXT}, or {@link #STREAM_QUEUE_BEHAVIOR_PLAY_LATER}.
      * </p>
      *
      * @see #ACTION_PLAY
      */
-    public static final String EXTRA_QUEUE_BEHAVIOR =
+    public static final String EXTRA_STREAM_QUEUE_BEHAVIOR =
             "android.media.intent.extra.QUEUE_BEHAVIOR";
 
     /**
-     * Value for {@link #EXTRA_QUEUE_BEHAVIOR}: Play now.
+     * Value for {@link #EXTRA_STREAM_QUEUE_BEHAVIOR}: Play now.
      * <p>
-     * Requests that the new content play immediately, cancelling the currently playing
-     * media item.  (This is the default queue behavior.)
+     * This is the default queue behavior.
+     * </p><p>
+     * Requests that the new content stream play immediately, canceling the currently playing
+     * stream and all subsequent items in the queue.  When this control request returns, the
+     * queue will contain exactly one item consisting of the newly requested content.
      * </p>
      *
-     * @see #EXTRA_QUEUE_BEHAVIOR
+     * @see #EXTRA_STREAM_QUEUE_BEHAVIOR
      */
-    public final static int QUEUE_BEHAVIOR_PLAY_NOW = 0;
+    public static final int STREAM_QUEUE_BEHAVIOR_PLAY_NOW = 0;
 
     /**
-     * Value for {@link #EXTRA_QUEUE_BEHAVIOR}: Play next.
+     * Value for {@link #EXTRA_STREAM_QUEUE_BEHAVIOR}: Play next.
      * <p>
-     * Requests that the new content be enqueued to play next after the currently playing
-     * media item.
+     * Requests that the new content stream be enqueued to play next after the currently playing
+     * stream, canceling all subsequent items in the queue.  When this control request returns,
+     * the queue will contain either one or two items consisting of the currently playing
+     * content, if any, followed by the newly requested content.
      * </p>
      *
-     * @see #EXTRA_QUEUE_BEHAVIOR
+     * @see #EXTRA_STREAM_QUEUE_BEHAVIOR
      */
-    public final static int QUEUE_BEHAVIOR_PLAY_NEXT = 1;
+    public static final int STREAM_QUEUE_BEHAVIOR_PLAY_NEXT = 1;
 
     /**
-     * Value for {@link #EXTRA_QUEUE_BEHAVIOR}: Play later.
+     * Value for {@link #EXTRA_STREAM_QUEUE_BEHAVIOR}: Play later.
      * <p>
-     * Requests that the new content be enqueued to play later after all other items
-     * currently in the queue.
+     * Requests that the new content stream be enqueued to play later after all other items
+     * currently in the queue.  When this control request returns, the queue will contain at
+     * least one item consisting of the currently playing content and all previously
+     * enqueued items followed by the newly requested content.
      * </p>
      *
-     * @see #EXTRA_QUEUE_BEHAVIOR
+     * @see #EXTRA_STREAM_QUEUE_BEHAVIOR
      */
-    public final static int QUEUE_BEHAVIOR_PLAY_LATER = 2;
+    public static final int STREAM_QUEUE_BEHAVIOR_PLAY_LATER = 2;
 
     /**
      * Integer extra: Stream position.
@@ -326,15 +372,32 @@ public final class MediaControlIntent {
      * </p><p>
      * Used with {@link #ACTION_SEEK} to set a new playback position.
      * </p><p>
-     * The value is an integer number of sections from the beginning of the
+     * Used with {@link #ACTION_GET_PROGRESS} to report the current playback position.
+     * </p><p>
+     * The value is an integer number of seconds from the beginning of the
      * content stream.
      * <p>
      *
      * @see #ACTION_PLAY
      * @see #ACTION_SEEK
+     * @see #ACTION_GET_PROGRESS
      */
     public static final String EXTRA_STREAM_POSITION =
             "android.media.intent.extra.STREAM_POSITION";
+
+    /**
+     * Integer extra: Stream duration.
+     * <p>
+     * Used with {@link #ACTION_GET_PROGRESS} to report the overall duration of the stream.
+     * </p><p>
+     * The value is an integer number of seconds from the beginning of the
+     * content stream.
+     * <p>
+     *
+     * @see #ACTION_GET_PROGRESS
+     */
+    public static final String EXTRA_STREAM_DURATION =
+            "android.media.intent.extra.STREAM_DURATION";
 
     /**
      * Bundle extra: Stream metadata.
@@ -374,10 +437,11 @@ public final class MediaControlIntent {
             "android.media.intent.extra.STREAM_ID";
 
     /**
-     * Bundle extra: Stream id.
+     * Bundle extra: Stream status.
      * <p>
-     * Returned as a result from {@link #ACTION_GET_STATUS} to describe the
-     * status of the route.
+     * Returned as a result from {@link #ACTION_GET_STATUS} and in broadcasts
+     * sent to a {@link #EXTRA_STREAM_STATUS_UPDATE_RECEIVER status update receiver}
+     * to describe the status of the stream.
      * </p><p>
      * The value is a {@link android.os.Bundle} of status keys and values as defined
      * in {@link MediaStreamStatus}.
@@ -391,6 +455,24 @@ public final class MediaControlIntent {
      */
     public static final String EXTRA_STREAM_STATUS =
             "android.media.intent.extra.STREAM_STATUS";
+
+    /**
+     * Bundle extra: Stream status update receiver.
+     * <p>
+     * Used with {@link #ACTION_PLAY} to specify a {@link PendingIntent} for a
+     * broadcast receiver that will receive status updates.
+     * </p><p>
+     * Whenever the status of the stream changes, the media route provider will
+     * send a broadcast to the pending intent which includes the {@link #EXTRA_STREAM_STATUS}
+     * that describes the new status.
+     * </p><p>
+     * The value is a {@link PendingIntent}.
+     * </p>
+     *
+     * @see #ACTION_PLAY
+     */
+    public static final String EXTRA_STREAM_STATUS_UPDATE_RECEIVER =
+            "android.media.intent.extra.STREAM_STATUS_UPDATE_RECEIVER";
 
     /**
      * Bundle extra: HTTP headers.
@@ -407,7 +489,7 @@ public final class MediaControlIntent {
      *
      * @see #ACTION_PLAY
      */
-    public static final String EXTRA_HTTP_HEADERS =
+    public static final String EXTRA_STREAM_HTTP_HEADERS =
             "android.media.intent.extra.HTTP_HEADERS";
 
     private MediaControlIntent() {
--- v7/mediarouter/src/android/support/v7/media/MediaStreamStatus.java
+++ v7/mediarouter/src/android/support/v7/media/MediaStreamStatus.java
@@ -24,4 +24,106 @@ import android.os.Bundle;
 public final class MediaStreamStatus {
     private MediaStreamStatus() {
     }
+
+    /**
+     * Status key: Playback state.
+     * <p>
+     * Specifies the playback state of a content stream in the queue.
+     * </p><p>
+     * Value is one of {@link #PLAYBACK_STATE_QUEUED}, {@link #PLAYBACK_STATE_PLAYING},
+     * {@link #PLAYBACK_STATE_PAUSED}, {@link #PLAYBACK_STATE_BUFFERING},
+     * {@link #PLAYBACK_STATE_CANCELED}, or {@link #PLAYBACK_STATE_ERROR}.
+     * </p>
+     */
+    public static final String KEY_PLAYBACK_STATE = "PLAYBACK_STATE";
+
+    /**
+     * Playback state: Queued.
+     * <p>
+     * Indicates that the stream is in the queue to be played eventually.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_QUEUED = 0;
+
+    /**
+     * Playback state: Playing.
+     * <p>
+     * Indicates that the stream is currently playing.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_PLAYING = 1;
+
+    /**
+     * Playback state: Paused.
+     * <p>
+     * Indicates that the stream has been paused.  Playback can be
+     * resumed playback by sending {@link MediaControlIntent#ACTION_RESUME}.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_PAUSED = 2;
+
+    /**
+     * Playback state: Buffering or seeking to a new position.
+     * <p>
+     * Indicates that the stream has been temporarily interrupted
+     * to fetch more content.  Playback will resume automatically
+     * when enough content has been buffered.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_BUFFERING = 3;
+
+    /**
+     * Playback state: Stopped.
+     * <p>
+     * Indicates that the stream has been stopped permanently either because
+     * it reached the end of the content or because the user ended playback.
+     * </p><p>
+     * A stopped stream cannot be resumed.  To play the content again, the application
+     * must send a new {@link MediaControlIntent#ACTION_PLAY} action to enqueue
+     * a new playback request and obtain a new stream id from that request.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_STOPPED = 4;
+
+    /**
+     * Playback state: Canceled.
+     * <p>
+     * Indicates that the stream was canceled permanently.  This may
+     * happen because a new item was queued which caused this stream
+     * to be stopped and removed from the queue.
+     * </p><p>
+     * A canceled stream cannot be resumed.  To play the content again, the application
+     * must send a new {@link MediaControlIntent#ACTION_PLAY} action to enqueue
+     * a new playback request and obtain a new stream id from that request.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_CANCELED = 5;
+
+    /**
+     * Playback state: Playback halted or aborted due to an error.
+     * <p>
+     * Examples of errors are no network connectivity when attempting to stream data from a
+     * server, or expired user credentials when trying to play subscription-based content.
+     * </p><p>
+     * An errored stream cannot be resumed.  To play the content again, the application
+     * must send a new {@link MediaControlIntent#ACTION_PLAY} action to enqueue
+     * a new playback request and obtain a new stream id from that request.
+     * </p>
+     */
+    public static final int PLAYBACK_STATE_ERROR = 6;
+
+    /**
+     * Status key: HTTP status code.
+     * <p>
+     * Specifies the HTTP status code that was encountered when the content stream
+     * was requested after all redirects were followed.  This key only needs to
+     * specified when the content uri uses the HTTP or HTTPS scheme and an error
+     * occurred.  This key may be omitted if the content stream was able to be played
+     * successfully; there is no need to report a 200 (OK) status code.
+     * </p><p>
+     * Value is an integer HTTP status code such as 401 (Unauthorized), 404 (Not Found),
+     * or 500 (Server Error).
+     * </p>
+     */
+    public static final String KEY_HTTP_STATUS_CODE = "HTTP_STATUS_CODE";
 }
