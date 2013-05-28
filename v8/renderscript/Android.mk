099deb8fb1715e62bcb24513f8e9305ab4f7743a
Scott Main <smain@google.com>
2013-05-28 15:32:19 -0700

    re-organize renderscript support lib to match organization patterns
    Bug: 9188088
    
    Change-Id: I8ef73a288aa0dc6dc92adbe64389dc6dee3ed807

--- v8/renderscript/Android.mk
+++ v8/renderscript/Android.mk
@@ -21,7 +21,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.support.v8.renderscript
+LOCAL_MODULE := android-support-v8-renderscript
 #LOCAL_SDK_VERSION := 17
 LOCAL_SRC_FILES := $(call all-java-files-under, java/src)
 
@@ -36,4 +36,5 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 include $(call all-makefiles-under, $(LOCAL_PATH))
 
 endif
+
 
