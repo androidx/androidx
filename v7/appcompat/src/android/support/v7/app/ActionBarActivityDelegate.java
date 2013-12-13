d48594f521469d15a9541972b5c5be7ba4afa4f3
Chris Banes <chrisbanes@google.com>
2013-12-13 16:24:06 +0000

    Fix MenuInflater creation with no Action Bar
    https://code.google.com/p/android/issues/detail?id=61394
    
    Change-Id: Iebc630523bc536cfda271261280f616f51bff057

--- v7/appcompat/src/android/support/v7/app/ActionBarActivityDelegate.java
+++ v7/appcompat/src/android/support/v7/app/ActionBarActivityDelegate.java
@@ -93,12 +93,7 @@ abstract class ActionBarActivityDelegate {
 
     MenuInflater getMenuInflater() {
         if (mMenuInflater == null) {
-            ActionBar ab = getSupportActionBar();
-            if (ab != null) {
-                mMenuInflater = new SupportMenuInflater(ab.getThemedContext());
-            } else {
-                mMenuInflater = new SupportMenuInflater(mActivity);
-            }
+            mMenuInflater = new SupportMenuInflater(getActionBarThemedContext());
         }
         return mMenuInflater;
     }
