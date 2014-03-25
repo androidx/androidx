a83005b70853ea52c5d98910762344de16b850a8
Tim Kilbourn <tkilbourn@google.com>
2014-03-25 10:33:47 -0700

    Actions and details overview row for leanback.
    A DetailsOverviewRow contains the row data for presenting the top
    details overview in a DetailsFragment.
    
    TODO: presenter and click handlers for actions
    
    Change-Id: I3d1cd24e5dac59c65449d6c9729538845c7b59eb

---
+++ v17/leanback/src/android/support/v17/leanback/widget/AbstractDetailsDescriptionPresenter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package android.support.v17.leanback.widget;
+
+import android.support.v17.leanback.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * An abstract {@link Presenter} for rendering a detailed description of an
+ * item. Typically this Presenter will be used in a DetailsOveriewRowPresenter.
+ *
+ * <p>Subclasses will override {@link #onBindDescription} to implement the data
+ * binding for this Presenter.
+ */
+public abstract class AbstractDetailsDescriptionPresenter extends Presenter {
+
+    public static class ViewHolder extends Presenter.ViewHolder {
+        private final TextView mTitle;
+        private final TextView mSubtitle;
+        private final TextView mBody;
+
+        public ViewHolder(View view) {
+            super(view);
+            mTitle = (TextView) view.findViewById(R.id.lb_details_description_title);
+            mSubtitle = (TextView) view.findViewById(R.id.lb_details_description_subtitle);
+            mBody = (TextView) view.findViewById(R.id.lb_details_description_body);
+        }
+
+        public TextView getTitle() {
+            return mTitle;
+        }
+
+        public TextView getSubtitle() {
+            return mSubtitle;
+        }
+
+        public TextView getBody() {
+            return mBody;
+        }
+    }
+
+    public final ViewHolder onCreateViewHolder(ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.lb_details_description, parent, false);
+        return new ViewHolder(v);
+    }
+
+    public final void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
+        ViewHolder vh = (ViewHolder) viewHolder;
+        DetailsOverviewRow row = (DetailsOverviewRow) item;
+        onBindDescription(vh, row);
+    }
+
+    /**
+     * Binds the data from the DetailsOverviewRow to the ViewHolder. The item
+     * being described in this row is accessible via the row's {@link
+     * DetailsOverviewRow#getItem getItem} method.
+     *
+     * @param vh The ViewHolder for this details description view.
+     * @param row The DetailsOverviewRow being presented.
+     */
+    protected abstract void onBindDescription(ViewHolder vh, DetailsOverviewRow row);
+
+    public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {}
+}
