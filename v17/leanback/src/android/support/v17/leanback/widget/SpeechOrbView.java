25aacd3f5896ec09053739cc731bdbab3a6f2b81
Jerome Poichet <jpoichet@google.com>
2014-05-15 12:07:55 -0700

    Adding voice support in search bar
    - Use private IME option to catch when voice key is pressed in IME
    - Use private IME option to allow "escape north"
    - Add a speech orb/button in search to start recognition
    - Animate speech orb when speaking
    - Handle switching from voice to IME
    
    Change-Id: I14774ca173a8da94b26f4942e267377a496b838c

---
+++ v17/leanback/src/android/support/v17/leanback/widget/SpeechOrbView.java
@@ -0,0 +1,113 @@
+package android.support.v17.leanback.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v17.leanback.R;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class SpeechOrbView extends FrameLayout implements View.OnClickListener {
+    private OnClickListener mListener;
+    private View mSpeechOrbView;
+    private final float mFocusedZoom;
+    private final float mSoundLevelMaxZoom;
+    private final int mNotRecordingColor;
+    private final int mRecordingColor;
+
+    private int mCurrentLevel = 0;
+    private boolean mListening = false;
+
+    public SpeechOrbView(Context context) {
+        this(context, null);
+    }
+
+    public SpeechOrbView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SpeechOrbView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View root = inflater.inflate(R.layout.lb_speech_orb, this, true);
+        mSpeechOrbView = root.findViewById(R.id.lb_speech_orb);
+
+        setFocusable(true);
+        setClipChildren(false);
+
+        Resources resources = context.getResources();
+        mFocusedZoom =
+                resources.getFraction(R.fraction.lb_search_bar_speech_orb_focused_zoom, 1, 1);
+        mSoundLevelMaxZoom =
+                resources.getFraction(R.fraction.lb_search_bar_speech_orb_max_level_zoom, 1, 1);
+        mNotRecordingColor = resources.getColor(R.color.lb_speech_orb_not_recording);
+        mRecordingColor = resources.getColor(R.color.lb_speech_orb_recording);
+
+        setOnClickListener(this);
+        showNotListening();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (null != mListener) {
+            mListener.onClick(view);
+        }
+    }
+
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+        final float zoom = gainFocus ? mFocusedZoom : 1f;
+        mSpeechOrbView.animate().scaleX(zoom).scaleY(zoom).setDuration(200).start();
+    }
+
+    /**
+     * Set the on click listener for the orb
+     * @param listener The listener.
+     */
+    public void setOnOrbClickedListener(OnClickListener listener) {
+        mListener = listener;
+    }
+
+    public void showListening() {
+        setOrbColor(mRecordingColor);
+        mSpeechOrbView.setScaleX(1f);
+        mSpeechOrbView.setScaleY(1f);
+        mListening = true;
+    }
+
+    public void showNotListening() {
+        setOrbColor(mNotRecordingColor);
+        mSpeechOrbView.setScaleX(1f);
+        mSpeechOrbView.setScaleY(1f);
+        mListening = false;
+    }
+
+    public void setSoundLevel(int level) {
+        if (!mListening) return;
+
+        // Either ease towards the target level, or decay away from it depending on whether
+        // its higher or lower than the current.
+        if (level > mCurrentLevel) {
+            mCurrentLevel = mCurrentLevel + ((level - mCurrentLevel) / 4);
+        } else {
+            mCurrentLevel = (int) (mCurrentLevel * 0.95f);
+        }
+
+        float zoom = mFocusedZoom + ((mSoundLevelMaxZoom - mFocusedZoom) * mCurrentLevel) / 100;
+        mSpeechOrbView.setScaleX(zoom);
+        mSpeechOrbView.setScaleY(zoom);
+    }
+
+    public void setOrbColor(int color) {
+        if (mSpeechOrbView.getBackground() instanceof GradientDrawable) {
+            ((GradientDrawable) mSpeechOrbView.getBackground()).setColor(color);
+        }
+    }
+
+}
