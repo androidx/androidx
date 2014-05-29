aadb8928f5920c758c1a385bbc8b8b128a60657c
Craig Stout <cstout@google.com>
2014-05-28 17:43:35 -0700

    Improvements to browse search orb.
    Now supports color animation.
    
    b/15094070
    
    Change-Id: Id66afa01711bcac4c950f19fda14d9593ff6b539

--- v17/leanback/src/android/support/v17/leanback/widget/SearchOrbView.java
+++ v17/leanback/src/android/support/v17/leanback/widget/SearchOrbView.java
@@ -16,8 +16,10 @@
 
 package android.support.v17.leanback.widget;
 
-
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v17.leanback.R;
@@ -29,7 +31,23 @@ import android.widget.FrameLayout;
 public class SearchOrbView extends FrameLayout implements View.OnClickListener {
     private OnClickListener mListener;
     private View mSearchOrbView;
+    private int mSearchOrbColor, mSearchOrbColorBright;
     private final float mFocusedZoom;
+    private final float mBrightnessAlpha;
+    private final int mPulseDurationMs;
+    private final int mScaleDownDurationMs;
+    private ValueAnimator mColorAnimator;
+
+    private final ArgbEvaluator mColorEvaluator = new ArgbEvaluator();
+
+    private final ValueAnimator.AnimatorUpdateListener mUpdateListener =
+            new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animator) {
+            Integer color = (Integer) animator.getAnimatedValue();
+            setOrbViewColor(color.intValue());
+        }
+    };
 
     public SearchOrbView(Context context) {
         this(context, null);
@@ -54,6 +72,12 @@ public class SearchOrbView extends FrameLayout implements View.OnClickListener {
 
         mFocusedZoom = context.getResources().getFraction(
                 R.fraction.lb_search_orb_focused_zoom, 1, 1);
+        mBrightnessAlpha = context.getResources().getFraction(
+                R.fraction.lb_search_orb_brightness_alpha, 1, 1);
+        mPulseDurationMs = context.getResources().getInteger(
+                R.integer.lb_search_orb_pulse_duration_ms);
+        mScaleDownDurationMs = context.getResources().getInteger(
+                R.integer.lb_search_orb_scale_down_duration_ms);
 
         setOnClickListener(this);
     }
@@ -69,7 +93,31 @@ public class SearchOrbView extends FrameLayout implements View.OnClickListener {
     protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
         super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
         final float zoom = gainFocus ? mFocusedZoom : 1f;
-        mSearchOrbView.animate().scaleX(zoom).scaleY(zoom).setDuration(200).start();
+        final int duration = gainFocus ? mPulseDurationMs : mScaleDownDurationMs;
+        mSearchOrbView.animate().scaleX(zoom).scaleY(zoom).setDuration(duration).start();
+        enableOrbColorAnimation(gainFocus);
+    }
+
+    private void enableOrbColorAnimation(boolean enable) {
+        if (mColorAnimator != null) {
+            mColorAnimator.end();
+            mColorAnimator = null;
+        }
+        if (enable) {
+            // TODO: set interpolator (quantum if available)
+            mColorAnimator = ValueAnimator.ofObject(mColorEvaluator,
+                    mSearchOrbColor, mSearchOrbColorBright, mSearchOrbColor);
+            mColorAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mColorAnimator.setDuration(mPulseDurationMs * 2);
+            mColorAnimator.addUpdateListener(mUpdateListener);
+            mColorAnimator.start();
+        }
+    }
+
+    private void setOrbViewColor(int color) {
+        if (mSearchOrbView.getBackground() instanceof GradientDrawable) {
+            ((GradientDrawable) mSearchOrbView.getBackground()).setColor(color);
+        }
     }
 
     /**
@@ -85,9 +133,23 @@ public class SearchOrbView extends FrameLayout implements View.OnClickListener {
         }
     }
 
+    private int getBrightColor(int color) {
+        final float brightnessValue = 0xff * mBrightnessAlpha;
+        int red = (int)(Color.red(color) * (1 - mBrightnessAlpha) + brightnessValue);
+        int green = (int)(Color.green(color) * (1 - mBrightnessAlpha) + brightnessValue);
+        int blue = (int)(Color.blue(color) * (1 - mBrightnessAlpha) + brightnessValue);
+        int alpha = (int)(Color.alpha(color) * (1 - mBrightnessAlpha) + brightnessValue);
+        return Color.argb(alpha, red, green, blue);
+    }
+
     public void setOrbColor(int color) {
-        if (mSearchOrbView.getBackground() instanceof GradientDrawable) {
-            ((GradientDrawable) mSearchOrbView.getBackground()).setColor(color);
+        mSearchOrbColor = color;
+        mSearchOrbColorBright = getBrightColor(color);
+
+        if (mColorAnimator == null) {
+            setOrbViewColor(color);
+        } else {
+            enableOrbColorAnimation(true);
         }
     }
 }
