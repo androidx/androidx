794383a3ff98ae27e8e64dc5d7919a152cb794cb
Craig Stout <cstout@google.com>
2014-09-15 14:14:46 -0700

    Fix build.
    Change-Id: I90dda90014131e48eace67bac26453ea278b4e87

--- samples/SupportLeanbackDemos/src/com/example/android/leanback/SearchActivity.java
+++ samples/SupportLeanbackDemos/src/com/example/android/leanback/SearchActivity.java
@@ -14,6 +14,7 @@
 package com.example.android.leanback;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v17.leanback.app.SearchFragment;
 import android.support.v17.leanback.widget.SpeechRecognitionCallback;
