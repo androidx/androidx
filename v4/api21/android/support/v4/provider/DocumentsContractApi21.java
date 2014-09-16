5fdfbc2e02f46509474057e4366585f6a6062cb9
Jeff Sharkey <jsharkey@android.com>
2014-09-16 15:46:41 -0700

    Utility to represent document trees.
    API 21 adds the powerful new ACTION_OPEN_DOCUMENT_TREE, but it can
    be tedious to use correctly, and developers can be overwhelmed when
    suddenly forced to use it because of SD card security changes.
    
    To ease the transition process, this change introduces a new
    DocumentFile object that looks and feels like a traditional
    java.util.File object.  However, it has nontrivial overhead because
    it creates new objects for every file traversed, and because it
    doesn't cache any metadata.
    
    Since developers probably have code heavily using java.util.File,
    offer fromFile() to simplify their code paths; they can adopt
    DocumentFile and have their core logic work on both API 21 and older
    versions.
    
    Bug: 17490054
    Change-Id: I034a0d6b800bcba489e5a6231b4fe62b5076456d

---
+++ v4/api21/android/support/v4/provider/DocumentsContractApi21.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.v4.provider;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+class DocumentsContractApi21 {
+    private static final String TAG = "DocumentFile";
+
+    public static Uri createFile(Context context, Uri self, String mimeType,
+            String displayName) {
+        return DocumentsContract.createDocument(context.getContentResolver(), self, mimeType,
+                displayName);
+    }
+
+    public static Uri createDirectory(Context context, Uri self, String displayName) {
+        return createFile(context, self, DocumentsContract.Document.MIME_TYPE_DIR, displayName);
+    }
+
+    public static Uri prepareTreeUri(Uri treeUri) {
+        return DocumentsContract.buildDocumentUriUsingTree(treeUri,
+                DocumentsContract.getTreeDocumentId(treeUri));
+    }
+
+    public static Uri[] listFiles(Context context, Uri self) {
+        final ContentResolver resolver = context.getContentResolver();
+        final Uri childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(self,
+                DocumentsContract.getDocumentId(self));
+        final ArrayList<Uri> results = new ArrayList<Uri>();
+
+        Cursor c = null;
+        try {
+            c = resolver.query(childrenUri, new String[] {
+                    DocumentsContract.Document.COLUMN_DOCUMENT_ID }, null, null, null);
+            while (c.moveToNext()) {
+                final String documentId = c.getString(0);
+                final Uri documentUri = DocumentsContract.buildDocumentUriUsingTree(self,
+                        documentId);
+                results.add(documentUri);
+            }
+        } catch (Exception e) {
+            Log.w(TAG, "Failed query: " + e);
+        } finally {
+            closeQuietly(c);
+        }
+
+        return results.toArray(new Uri[results.size()]);
+    }
+
+    public static Uri renameTo(Context context, Uri self, String displayName) {
+        return DocumentsContract.renameDocument(context.getContentResolver(), self, displayName);
+    }
+
+    private static void closeQuietly(AutoCloseable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (RuntimeException rethrown) {
+                throw rethrown;
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
