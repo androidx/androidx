da0b9d63d6f940cbfd5b526c3f81858aeadcb7e8
Chris Banes <chrisbanes@google.com>
2014-09-24 09:13:59 +0100

    Fix List Menu for Toolbar Action Bars
    The problem is that PhoneWindow keeps hold of
    the first menu returned from onCreatePanelView(). The update
    of that is controlled by PanelStateFeature.refreshDecorView flag
    is rarely set, so the new view returned by onCreatePanelView()
    is never actually used.
    
    This CL works around it by keeping one ListMenuPresenter (and thus
    one MenuView) and passing it to each ToolbarActionBar. It can
    then update the view as needed.
    
    BUG: 17049242
    
    Change-Id: Iedaa93c1dfc0068e4ce588aee28548949f9c4d15

--- v7/appcompat/src/android/support/v7/app/ActionBarActivityDelegateBase.java
+++ v7/appcompat/src/android/support/v7/app/ActionBarActivityDelegateBase.java
@@ -123,6 +123,8 @@ class ActionBarActivityDelegateBase extends ActionBarActivityDelegate
 
     private boolean mEnableDefaultActionBarUp;
 
+    private ListMenuPresenter mToolbarListMenuPresenter;
+
     ActionBarActivityDelegateBase(ActionBarActivity activity) {
         super(activity);
     }
@@ -153,15 +155,22 @@ class ActionBarActivityDelegateBase extends ActionBarActivityDelegate
 
     @Override
     void setSupportActionBar(Toolbar toolbar) {
-        if (getSupportActionBar() instanceof WindowDecorActionBar) {
+        final ActionBar ab = getSupportActionBar();
+        if (ab instanceof WindowDecorActionBar) {
             throw new IllegalStateException("This Activity already has an action bar supplied " +
                     "by the window decor. Do not request Window.FEATURE_ACTION_BAR and set " +
                     "windowActionBar to false in your theme to use a Toolbar instead.");
+        } else if (ab instanceof ToolbarActionBar) {
+            // Make sure we reset the old toolbar AB's list menu presenter
+            ((ToolbarActionBar) ab).setListMenuPresenter(null);
         }
+
         // Need to make sure we give the action bar the default window callback. Otherwise multiple
         // setSupportActionBar() calls lead to memory leaks
         ToolbarActionBar tbab = new ToolbarActionBar(toolbar, mActivity.getTitle(),
-                mDefaultWindowCallback);
+                mActivity.getWindow(), mDefaultWindowCallback);
+        ensureToolbarListMenuPresenter();
+        tbab.setListMenuPresenter(mToolbarListMenuPresenter);
         setSupportActionBar(tbab);
         setWindowCallback(tbab.getWrappedWindowCallback());
         tbab.invalidateOptionsMenu();
@@ -1285,6 +1294,22 @@ class ActionBarActivityDelegateBase extends ActionBarActivityDelegate
             mStatusGuard.setVisibility(showStatusGuard ? View.VISIBLE : View.GONE);
         }
         return true;
+    }
+
+    private void ensureToolbarListMenuPresenter() {
+        if (mToolbarListMenuPresenter == null) {
+            // First resolve panelMenuListTheme
+            TypedValue outValue = new TypedValue();
+            mActivity.getTheme().resolveAttribute(R.attr.panelMenuListTheme, outValue, true);
+
+            Context context = new ContextThemeWrapper(mActivity,
+                    outValue.resourceId != 0
+                            ? outValue.resourceId
+                            : R.style.Theme_AppCompat_CompactMenu);
+
+            mToolbarListMenuPresenter = new ListMenuPresenter(context,
+                    R.layout.abc_list_menu_item_layout);
+        }
     }
 
     /**
