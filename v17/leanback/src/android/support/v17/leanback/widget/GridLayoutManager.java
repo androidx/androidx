d633c87b6565c6068a5c5beb5a1ef91a493acea8
Craig Stout <cstout@google.com>
2014-11-21 09:45:39 -0800

    DO NOT MERGE Fix crash when focus requested on a child that was removed.
    This can happen when all views are removed.
    This a regression triggered by a cherry-picked of recyclerView change ag/582987
    
    b/18474947
    
    Change-Id: I1b9c4b82488a893ba7e668cdc79e41a03d7c61a2
    (cherry picked from commit 9b68e453d30984d524dcf25fdc2e645ae853ac33)

--- v17/leanback/src/android/support/v17/leanback/widget/GridLayoutManager.java
+++ v17/leanback/src/android/support/v17/leanback/widget/GridLayoutManager.java
@@ -1489,6 +1489,7 @@ final class GridLayoutManager extends RecyclerView.LayoutManager {
             mFocusPositionOffset = 0;
         }
         saveContext(recycler, state);
+
         // Track the old focus view so we can adjust our system scroll position
         // so that any scroll animations happening now will remain valid.
         // We must use same delta in Pre Layout (if prelayout exists) and second layout.
@@ -1520,6 +1521,9 @@ final class GridLayoutManager extends RecyclerView.LayoutManager {
             if (mFocusPosition != savedFocusPos) {
                 if (DEBUG) Log.v(getTag(), "savedFocusPos " + savedFocusPos +
                         " mFocusPosition " + mFocusPosition);
+            }
+            if (mFocusPosition == NO_POSITION) {
+                mBaseGridView.clearFocus();
             }
 
             mWindowAlignment.mainAxis().invalidateScrollMin();
@@ -2002,6 +2006,11 @@ final class GridLayoutManager extends RecyclerView.LayoutManager {
     @Override
     public boolean onRequestChildFocus(RecyclerView parent, View child, View focused) {
         if (mFocusSearchDisabled) {
+            return true;
+        }
+        if (getPositionByView(child) == NO_POSITION) {
+            // This shouldn't happen, but in case it does be sure not to attempt a
+            // scroll to a view whose item has been removed.
             return true;
         }
         if (!mInLayout && !mInSelection) {
