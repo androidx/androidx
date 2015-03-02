94756719872ef2f4a60d59eb1e2169011312676b
Scott Kennedy <skennedy@google.com>
2015-03-01 16:11:13 -0800

    Mark attrs @Nullable in Toolbar constructors
    Change-Id: I972392218debe491594d43c52d415940d645fb25

--- v7/appcompat/src/android/support/v7/widget/Toolbar.java
+++ v7/appcompat/src/android/support/v7/widget/Toolbar.java
@@ -188,11 +188,11 @@ public class Toolbar extends ViewGroup {
         this(context, null);
     }
 
-    public Toolbar(Context context, AttributeSet attrs) {
+    public Toolbar(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, R.attr.toolbarStyle);
     }
 
-    public Toolbar(Context context, AttributeSet attrs, int defStyleAttr) {
+    public Toolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         // We manually themify the context here so that we don't break apps which only
         // use app:theme when running on >= Lollipop
         super(ViewUtils.themifyContext(context, attrs, false, true), attrs, defStyleAttr);
