9b2e27b330a5047774442f112efd8ba40b046c39
Tor Norbye <tnorbye@google.com>
2015-03-18 09:01:58 -0700

    Update annotation retention for resource type annotations
    The resource type annotations in the support library should use
    class file retention. That's more compact than having to use external
    annotations for these. (IntDef and StringDef on the other hand should
    just use source retention, since the class file format for annotations
    only allows storing the values of the constants they refer to, not
    references to the constants.)
    
    Also allows the resource type annotations to be applied to local
    variables as requested in
    150475: Update Res support annotations to permit LOCAL_VARIABLE
            annotations
    
    Change-Id: I29bdfe3c258b349bd6c2a89c41b2bb76454d0ed4

--- annotations/src/android/support/annotation/AnyRes.java
+++ annotations/src/android/support/annotation/AnyRes.java
@@ -20,9 +20,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.SOURCE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Denotes that an integer parameter, field or method return value is expected
@@ -31,7 +32,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  * {@link DrawableRes}.
  */
 @Documented
-@Retention(SOURCE)
-@Target({METHOD, PARAMETER, FIELD})
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE})
 public @interface AnyRes {
 }
--- annotations/src/android/support/annotation/RawRes.java
+++ annotations/src/android/support/annotation/RawRes.java
@@ -20,16 +20,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.SOURCE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Denotes that an integer parameter, field or method return value is expected
  * to be a raw resource reference.
  */
 @Documented
-@Retention(SOURCE)
-@Target({METHOD, PARAMETER, FIELD})
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE})
 public @interface RawRes {
 }
