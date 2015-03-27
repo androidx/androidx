020243a81ca7f43c84c36889047e3bd1bf185f52
ztenghui <ztenghui@google.com>
2015-03-26 17:16:42 -0700

    DO NOT MERGE: Discontinue the vectordrawable support lib for this branch.
    Change-Id: Ie52b54a8b021be4adc8cb244655435a61c882b7b

--- v7/vectordrawable/runtest.sh
+++
@@ -1,5 +0,0 @@
-. ../../../../build/envsetup.sh
-mmm -j20 . && mmm -j20 ./tests/ && \
-adb install -r $OUT/data/app/AndroidVectorDrawableTests/AndroidVectorDrawableTests.apk && \
-adb shell am start -n android.support.v7.vectordrawable/android.support.v7.vectordrawable.TestActivity
-
