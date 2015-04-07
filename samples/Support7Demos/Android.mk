a79e320372f428831f865c7ae26b50822df8e259
Chris Banes <chrisbanes@google.com>
2015-04-07 19:16:35 +0100

    Disable the design support library and demos
    Change-Id: Ia0247179d3230d0ec2a198d458bc417fe3102d9d

--- samples/Support7Demos/Android.mk
+++ samples/Support7Demos/Android.mk
@@ -29,20 +29,17 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
         android-support-v7-mediarouter \
         android-support-v7-palette \
         android-support-v7-recyclerview \
-        android-support-v7-cardview \
-        android-support-design
+        android-support-v7-cardview
 LOCAL_RESOURCE_DIR = \
         $(LOCAL_PATH)/res \
         frameworks/support/v7/appcompat/res \
         frameworks/support/v7/gridlayout/res \
         frameworks/support/v7/mediarouter/res \
-        frameworks/support/v7/cardview/res \
-        frameworks/support/design/res
+        frameworks/support/v7/cardview/res
 LOCAL_AAPT_FLAGS := \
         --auto-add-overlay \
         --extra-packages android.support.v7.appcompat \
         --extra-packages android.support.v7.cardview \
         --extra-packages android.support.v7.gridlayout \
-        --extra-packages android.support.v7.mediarouter \
-        --extra-packages android.support.design
+        --extra-packages android.support.v7.mediarouter
 include $(BUILD_PACKAGE)
