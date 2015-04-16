1623bd442b92ae80d4a3aa2b077e980144bf8e8f
Chris Banes <chrisbanes@google.com>
2015-04-16 16:39:52 +0100

    Hide GridLayout tests from doc build
    Change-Id: I4eb06ff56639df713eead801eb5ec4ab8fe3f0c5
    (cherry picked from commit a8bda6f79fa68d0072377059893f50f2b1acacd0)

--- v7/gridlayout/tests/src/android/support/v7/widget/test/GridLayoutTest.java
+++ v7/gridlayout/tests/src/android/support/v7/widget/test/GridLayoutTest.java
@@ -27,6 +27,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+/**
+ * @hide
+ */
 public class GridLayoutTest extends ActivityInstrumentationTestCase2 {
 
     public GridLayoutTest() {
