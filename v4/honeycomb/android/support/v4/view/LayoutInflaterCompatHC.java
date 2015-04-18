b6c17efbee5757b878c2c02facf626cbfc6ade75
Adam Powell <adamp@google.com>
2015-04-18 15:28:19 -0700

    Work around a framework bug setting LayoutInflater.Factory2
    Prior to commit 630c077463fecaa8eb1015865770f2c05c514ade in
    frameworks/base there was a platform bug starting at API 11 (fixed in
    API 21) that would cause a Factory2 not to be set properly on a
    LayoutInflater obtained via the LayoutInflater#cloneInContext method.
    
    The intended behavior was to merge the old Factory/Factory2 with the
    new one set on the cloned LayoutInflater. Precedence is given to the
    new Factory/Factory2, but if the new factory returns null, the old one
    is used. The bug would cause the merge to be correct for Factory, but
    not Factory2.
    
    Now that AppCompat uses Factory2, this matters. Without this
    workaround, nested fragment inflation for android-support-v4 fragments
    is broken and nested fragments are inflated into the
    FragmentActivity's FragmentManager instead of the parent Fragment's
    child FragmentManager.
    
    Bug 20343535
    
    Change-Id: Ib9cd690725b365ad40d4a6cf12c1e6dc6c0089fd
    (cherry picked from commit 10850534c9e2949857133437cf1cd9af82721ab1)

--- v4/honeycomb/android/support/v4/view/LayoutInflaterCompatHC.java
+++ v4/honeycomb/android/support/v4/view/LayoutInflaterCompatHC.java
@@ -18,10 +18,18 @@ package android.support.v4.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
 class LayoutInflaterCompatHC {
+    private static final String TAG = "LayoutInflaterCompatHC";
+
+    private static Field sLayoutInflaterFactory2Field;
+    private static boolean sCheckedField;
 
     static class FactoryWrapperHC extends LayoutInflaterCompatBase.FactoryWrapper
             implements LayoutInflater.Factory2 {
@@ -38,7 +46,37 @@ class LayoutInflaterCompatHC {
     }
 
     static void setFactory(LayoutInflater inflater, LayoutInflaterFactory factory) {
-        inflater.setFactory2(factory != null ? new FactoryWrapperHC(factory) : null);
+        final LayoutInflater.Factory2 factory2 = factory != null
+                ? new FactoryWrapperHC(factory) : null;
+        inflater.setFactory2(factory2);
+        forceSetFactory2(inflater, factory2);
     }
 
+    /**
+     * For APIs >= 11 && < 21, there was a framework bug that prevented a LayoutInflater's
+     * Factory2 from being merged properly if set after a cloneInContext from a LayoutInflater
+     * that already had a Factory2 registered. We work around that bug here. If we can't we
+     * log an error.
+     */
+    static void forceSetFactory2(LayoutInflater inflater, LayoutInflater.Factory2 factory) {
+        if (!sCheckedField) {
+            try {
+                sLayoutInflaterFactory2Field = LayoutInflater.class.getDeclaredField("mFactory2");
+                sLayoutInflaterFactory2Field.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                Log.e(TAG, "forceSetFactory2 Could not find field 'mFactory2' on class "
+                        + LayoutInflater.class.getName()
+                        + "; inflation may have unexpected results.", e);
+            }
+            sCheckedField = true;
+        }
+        if (sLayoutInflaterFactory2Field != null) {
+            try {
+                sLayoutInflaterFactory2Field.set(inflater, factory);
+            } catch (IllegalAccessException e) {
+                Log.e(TAG, "forceSetFactory2 could not set the Factory2 on LayoutInflater "
+                        + inflater + "; inflation may have unexpected results.", e);
+            }
+        }
+    }
 }
