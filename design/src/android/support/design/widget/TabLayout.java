7c71281dc4879e7889c65283aabaeed68521fb14
Alan Jeon <skyisle@gmail.com>
2015-04-21 10:17:12 +0900

    Re-measure tab width after max width is decided
    Tabview's onMeasure can be called before mTabMaxWidth is decided.
    
    Change-Id: I063f31232041bc6d8447756df9689f824a199019
    Signed-off-by: Alan Jeon <skyisle@gmail.com>

--- design/src/android/support/design/widget/TabLayout.java
+++ design/src/android/support/design/widget/TabLayout.java
@@ -672,7 +672,7 @@ public class TabLayout extends HorizontalScrollView {
         if (newPosition == Tab.INVALID_POSITION) {
             return;
         }
-        
+
         if (getWindowToken() == null || !ViewCompat.isLaidOut(this)) {
             // If we don't have a window token, or we haven't been laid out yet just draw the new
             // position now
@@ -1048,7 +1048,7 @@ public class TabLayout extends HorizontalScrollView {
         public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            if (getMeasuredWidth() > mTabMaxWidth) {
+            if (mTabMaxWidth != 0 && getMeasuredWidth() > mTabMaxWidth) {
                 // Re-measure if we went beyond our maximum size.
                 super.onMeasure(MeasureSpec.makeMeasureSpec(
                         mTabMaxWidth, MeasureSpec.EXACTLY), heightMeasureSpec);
