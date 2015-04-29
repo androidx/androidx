f9cabe2ad76a19d555b5b656d8167bdb167c9d03
Filip Gruszczynski <gruszczy@google.com>
2015-04-29 13:16:32 -0700

    Percentage based layouts.
    Change-Id: I366ad93a61804231b934d08ff0d1a31d8b66817c

---
+++ percent/Android.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+# Build the resources using the current SDK version.
+# We do this here because the final static library must be compiled with an older
+# SDK version than the resources.  The resources library and the R class that it
+# contains will not be linked into the final static library.
+include $(CLEAR_VARS)
+LOCAL_MODULE := android-support-percent-res
+LOCAL_SDK_VERSION := current
+LOCAL_SRC_FILES := $(call all-java-files-under, dummy)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_JAR_EXCLUDE_FILES := none
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+# Here is the final static library that apps can link against.
+# The R class is automatically excluded from the generated library.
+# Applications that use this library must specify LOCAL_RESOURCE_DIR
+# in their makefiles to include the resources in their package.
+include $(CLEAR_VARS)
+LOCAL_MODULE := android-support-percent
+LOCAL_SDK_VERSION := 7
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_JAVA_LIBRARIES := android-support-percent-res \
+    android-support-v4
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+# API Check
+# ---------------------------------------------
+support_module := $(LOCAL_MODULE)
+support_module_api_dir := $(LOCAL_PATH)/api
+support_module_src_files := $(LOCAL_SRC_FILES)
+support_module_java_libraries := $(LOCAL_JAVA_LIBRARIES)
+support_module_java_packages := android.support.percent
+include $(SUPPORT_API_CHECK)
---
+++ percent/src/android/support/percent/PercentRelativeLayout.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.percent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Subclass of {@link android.widget.RelativeLayout} that supports percentage based dimensions and
+ * margins.
+ *
+ * You can specify dimension or a margin of child by using attributes with "Percent" suffix. Follow
+ * this example:
+ *
+ * <pre class="prettyprint">
+ * &lt;android.support.percent.PercentRelativeLayout
+ *         xmlns:android="http://schemas.android.com/apk/res/android"
+ *         xmlns:app="http://schemas.android.com/apk/res-auto"
+ *         android:layout_width="match_parent"
+ *         android:layout_height="match_parent"/&gt
+ *     &lt;ImageView
+ *         app:layout_widthPercent="50%"
+ *         app:layout_heightPercent="50%"
+ *         app:layout_marginTopPercent="25%"
+ *         app:layout_marginLeftPercent="25%"/&gt
+ * &lt;/android.support.percent.PercentFrameLayout/&gt
+ * </pre>
+ *
+ * The attributes that you can use are:
+ * <ul>
+ *     <li>{@code layout_widthPercent}
+ *     <li>{@code layout_heightPercent}
+ *     <li>{@code layout_marginPercent}
+ *     <li>{@code layout_marginLeftPercent}
+ *     <li>{@code layout_marginTopPercent}
+ *     <li>{@code layout_marginRightPercent}
+ *     <li>{@code layout_marginBottomPercent}
+ *     <li>{@code layout_marginStartPercent}
+ *     <li>{@code layout_marginEndPercent}
+ * </ul>
+ *
+ * It is not necessary to specify {@code layout_width/height} if you specify {@code
+ * layout_widthPercent.} However, if you want the view to be able to take up more space than what
+ * percentage value permits, you can add {@code layout_width/height="wrap_content"}. In that case
+ * if the percentage size is too small for the View's content, it will be resized using
+ * {@code wrap_content} rule.
+ */
+public class PercentRelativeLayout extends RelativeLayout {
+    private final PercentLayoutHelper mHelper = new PercentLayoutHelper(this);
+
+    public PercentRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public PercentRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PercentRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mHelper.adjustChildren(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (mHelper.handleMeasuredStateTooSmall()) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mHelper.restoreOriginalParams();
+    }
+
+    public static class LayoutParams extends RelativeLayout.LayoutParams
+            implements PercentLayoutHelper.PercentLayoutParams {
+        private PercentLayoutHelper.PercentLayoutInfo mPercentLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+            mPercentLayoutInfo = PercentLayoutHelper.getPercentLayoutInfo(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        @Override
+        public PercentLayoutHelper.PercentLayoutInfo getPercentLayoutInfo() {
+            return mPercentLayoutInfo;
+        }
+
+        @Override
+        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
+            PercentLayoutHelper.fetchWidthAndHeight(this, a, widthAttr, heightAttr);
+        }
+    }
+}
