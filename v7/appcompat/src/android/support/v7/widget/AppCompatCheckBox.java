091b0f935e68ce9bfecc2422e60eada33fa3b09c
Chris Banes <chrisbanes@google.com>
2015-05-06 19:13:34 +0100

    AppCompatWidgets, now with added pixie dust.
    This CL moves a lot of the internal code for apply background
    tints to a central helper class. This allows us to have one place
    to add/fix things. This has enabled an easier implementation of
    reading the backgroundTint and backgroundTintMode attributes.
    
    With that, our CompoundButton implementations have followed in
    the same vain with a new helper class, reading of the new
    buttonTint and buttonTintMode attributes, with a v4 shim to allow
    dynamic setting.
    
    Also tidied up the javadoc on each class too so that developers
    know what they're getting from each class, and fixed an NPE in
    the AutoCompleteTextView classes.
    
    BUG: 19037149
    BUG: 21102590
    
    Change-Id: Ice097ed44e1253e5799bbe9ab30285912617b4dc

--- v7/appcompat/src/android/support/v7/widget/AppCompatCheckBox.java
+++ v7/appcompat/src/android/support/v7/widget/AppCompatCheckBox.java
@@ -17,29 +17,35 @@
 package android.support.v7.widget;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.TintableCompoundButton;
 import android.support.v7.appcompat.R;
 import android.support.v7.internal.widget.TintManager;
-import android.support.v7.internal.widget.TintTypedArray;
 import android.util.AttributeSet;
 import android.widget.CheckBox;
 
 /**
- * A tint aware {@link android.widget.CheckBox}.
- * <p>
- * This will automatically be used when you use {@link android.widget.CheckBox} in your layouts.
- * You should only need to manually use this class when writing custom views.
+ * A {@link CheckBox} which supports compatible features on older version of the platform,
+ * including:
+ * <ul>
+ *     <li>Allows dynamic tint of it background via the background tint methods in
+ *     {@link android.support.v4.widget.CompoundButtonCompat}.</li>
+ *     <li>Allows setting of the background tint using {@link R.attr#buttonTint} and
+ *     {@link R.attr#buttonTintMode}.</li>
+ * </ul>
+ *
+ * <p>This will automatically be used when you use {@link CheckBox} in your layouts.
+ * You should only need to manually use this class when writing custom views.</p>
  */
-public class AppCompatCheckBox extends CheckBox {
-
-    private static final int[] TINT_ATTRS = {
-            android.R.attr.button
-    };
+public class AppCompatCheckBox extends CheckBox implements TintableCompoundButton {
 
     private TintManager mTintManager;
-    private Drawable mButtonDrawable;
+    private AppCompatCompoundButtonHelper mCompoundButtonHelper;
 
     public AppCompatCheckBox(Context context) {
         this(context, null);
@@ -51,43 +57,77 @@ public class AppCompatCheckBox extends CheckBox {
 
     public AppCompatCheckBox(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
-        if (TintManager.SHOULD_BE_USED) {
-            TintTypedArray a = TintTypedArray.obtainStyledAttributes(getContext(), attrs,
-                    TINT_ATTRS, defStyleAttr, 0);
-            setButtonDrawable(a.getDrawable(0));
-            a.recycle();
-
-            mTintManager = a.getTintManager();
-        }
+        mTintManager = TintManager.get(context);
+        mCompoundButtonHelper = new AppCompatCompoundButtonHelper(this, mTintManager);
+        mCompoundButtonHelper.loadFromAttributes(attrs, defStyleAttr);
     }
 
     @Override
     public void setButtonDrawable(Drawable buttonDrawable) {
         super.setButtonDrawable(buttonDrawable);
-        mButtonDrawable = buttonDrawable;
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.onSetButtonDrawable();
+        }
     }
 
     @Override
     public void setButtonDrawable(@DrawableRes int resId) {
-        if (mTintManager != null) {
-            setButtonDrawable(mTintManager.getDrawable(resId));
-        } else {
-            super.setButtonDrawable(resId);
-        }
+        setButtonDrawable(mTintManager != null
+                ? mTintManager.getDrawable(resId)
+                : ContextCompat.getDrawable(getContext(), resId));
     }
 
     @Override
     public int getCompoundPaddingLeft() {
-        int padding = super.getCompoundPaddingLeft();
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            // Before JB-MR1 the button drawable wasn't taken into account for padding. We'll
-            // workaround that here
-            if (mButtonDrawable != null) {
-                padding += mButtonDrawable.getIntrinsicWidth();
-            }
-        }
-        return padding;
+        final int value = super.getCompoundPaddingLeft();
+        return mCompoundButtonHelper != null
+                ? mCompoundButtonHelper.getCompoundPaddingLeft(value)
+                : value;
     }
 
+    /**
+     * This should be accessed from {@link android.support.v4.widget.CompoundButtonCompat}
+     * @hide
+     */
+    @Override
+    public void setSupportButtonTintList(@Nullable ColorStateList tint) {
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setSupportButtonTintList(tint);
+        }
+    }
+
+    /**
+     * This should be accessed from {@link android.support.v4.widget.CompoundButtonCompat}
+     * @hide
+     */
+    @Nullable
+    @Override
+    public ColorStateList getSupportButtonTintList() {
+        return mCompoundButtonHelper != null
+                ? mCompoundButtonHelper.getSupportButtonTintList()
+                : null;
+    }
+
+    /**
+     * This should be accessed from {@link android.support.v4.widget.CompoundButtonCompat}
+     * @hide
+     */
+    @Override
+    public void setSupportButtonTintMode(@Nullable PorterDuff.Mode tintMode) {
+        if (mCompoundButtonHelper != null) {
+            mCompoundButtonHelper.setSupportButtonTintMode(tintMode);
+        }
+    }
+
+    /**
+     * This should be accessed from {@link android.support.v4.widget.CompoundButtonCompat}
+     * @hide
+     */
+    @Nullable
+    @Override
+    public PorterDuff.Mode getSupportButtonTintMode() {
+        return mCompoundButtonHelper != null
+                ? mCompoundButtonHelper.getSupportButtonTintMode()
+                : null;
+    }
 }
