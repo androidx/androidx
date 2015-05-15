976b6a8c18550ace670e0410453741d11dafa474
Ian Lake <ilake@google.com>
2015-05-15 13:02:57 -0700

    Introduce NotificationCompat.MediaStyle DO NOT MERGE
    Make Notification.MediaStyle available from v7 with the support
    library. For versions prior ICS, the normal template is used, as
    buttons within notifications weren't supported. From ICS to
    Jellybean, a custom template is used which maches the platform one.
    On Lollipop and later, the platform implementation is used.
    
    Bug: 17425365
    Change-Id: I6a44c8b325c0fb204e3043e1114c4a2c3276d78f
    (cherry picked from commit 10d7d250bc41b21059a90095e8b693c9e7bc3bc3)

--- v4/ics/android/support/v4/app/NotificationCompatIceCreamSandwich.java
+++ v4/ics/android/support/v4/app/NotificationCompatIceCreamSandwich.java
@@ -23,34 +23,47 @@ import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 
 class NotificationCompatIceCreamSandwich {
-    static Notification add(Context context, Notification n,
-            CharSequence contentTitle, CharSequence contentText, CharSequence contentInfo,
-            RemoteViews tickerView, int number,
-            PendingIntent contentIntent, PendingIntent fullScreenIntent, Bitmap largeIcon,
-            int progressMax, int progress, boolean progressIndeterminate) {
-        Notification.Builder b = new Notification.Builder(context)
-                .setWhen(n.when)
-                .setSmallIcon(n.icon, n.iconLevel)
-                .setContent(n.contentView)
-                .setTicker(n.tickerText, tickerView)
-                .setSound(n.sound, n.audioStreamType)
-                .setVibrate(n.vibrate)
-                .setLights(n.ledARGB, n.ledOnMS, n.ledOffMS)
-                .setOngoing((n.flags & Notification.FLAG_ONGOING_EVENT) != 0)
-                .setOnlyAlertOnce((n.flags & Notification.FLAG_ONLY_ALERT_ONCE) != 0)
-                .setAutoCancel((n.flags & Notification.FLAG_AUTO_CANCEL) != 0)
-                .setDefaults(n.defaults)
-                .setContentTitle(contentTitle)
-                .setContentText(contentText)
-                .setContentInfo(contentInfo)
-                .setContentIntent(contentIntent)
-                .setDeleteIntent(n.deleteIntent)
-                .setFullScreenIntent(fullScreenIntent,
-                        (n.flags & Notification.FLAG_HIGH_PRIORITY) != 0)
-                .setLargeIcon(largeIcon)
-                .setNumber(number)
-                .setProgress(progressMax, progress, progressIndeterminate);
 
-        return b.getNotification();
+    public static class Builder implements NotificationBuilderWithBuilderAccessor {
+
+        private Notification.Builder b;
+
+        public Builder(Context context, Notification n, CharSequence contentTitle,
+                CharSequence contentText, CharSequence contentInfo, RemoteViews tickerView,
+                int number, PendingIntent contentIntent, PendingIntent fullScreenIntent,
+                Bitmap largeIcon, int progressMax, int progress, boolean progressIndeterminate) {
+            b = new Notification.Builder(context)
+                    .setWhen(n.when)
+                    .setSmallIcon(n.icon, n.iconLevel)
+                    .setContent(n.contentView)
+                    .setTicker(n.tickerText, tickerView)
+                    .setSound(n.sound, n.audioStreamType)
+                    .setVibrate(n.vibrate)
+                    .setLights(n.ledARGB, n.ledOnMS, n.ledOffMS)
+                    .setOngoing((n.flags & Notification.FLAG_ONGOING_EVENT) != 0)
+                    .setOnlyAlertOnce((n.flags & Notification.FLAG_ONLY_ALERT_ONCE) != 0)
+                    .setAutoCancel((n.flags & Notification.FLAG_AUTO_CANCEL) != 0)
+                    .setDefaults(n.defaults)
+                    .setContentTitle(contentTitle)
+                    .setContentText(contentText)
+                    .setContentInfo(contentInfo)
+                    .setContentIntent(contentIntent)
+                    .setDeleteIntent(n.deleteIntent)
+                    .setFullScreenIntent(fullScreenIntent,
+                            (n.flags & Notification.FLAG_HIGH_PRIORITY) != 0)
+                    .setLargeIcon(largeIcon)
+                    .setNumber(number)
+                    .setProgress(progressMax, progress, progressIndeterminate);
+        }
+
+        @Override
+        public Notification.Builder getBuilder() {
+            return b;
+        }
+
+        @Override
+        public Notification build() {
+            return b.getNotification();
+        }
     }
 }
