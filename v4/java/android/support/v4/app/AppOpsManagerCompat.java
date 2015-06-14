e2458f312876e3f9ac64f0155997ab21b2b906f2
Svet Ganov <svetoslavganov@google.com>
2015-06-13 17:49:22 -0700

    Add permission checking APIs that look at both permissions and app ops.
    bug:21277214
    
    Change-Id: I8fe98863333270666964adb69f92b2d3dcd78ef7

---
+++ v4/java/android/support/v4/app/AppOpsManagerCompat.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.v4.app;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+/**
+ * Helper for accessing features in android.app.AppOpsManager
+ * introduced after API level 4 in a backwards compatible fashion.
+ */
+public class AppOpsManagerCompat {
+
+    /**
+     * Result from {@link #noteOp}: the given caller is allowed to
+     * perform the given operation.
+     */
+    public static final int MODE_ALLOWED = 0;
+
+    /**
+     * Result from {@link #noteOp}: the given caller is not allowed to perform
+     * the given operation, and this attempt should <em>silently fail</em> (it
+     * should not cause the app to crash).
+     */
+    public static final int MODE_IGNORED = 1;
+
+    /**
+     * Result from {@link #noteOp}: the given caller should use its default
+     * security check.  This mode is not normally used; it should only be used
+     * with appop permissions, and callers must explicitly check for it and
+     * deal with it.
+     */
+    public static final int MODE_DEFAULT = 3;
+
+    private static class AppOpsManagerImpl {
+        public String permissionToOp(String permission) {
+            return null;
+        }
+
+        public int noteOp(Context context, String op, int uid, String packageName) {
+            return MODE_IGNORED;
+        }
+    }
+
+    private static class AppOpsManager23 extends AppOpsManagerImpl {
+        @Override
+        public String permissionToOp(String permission) {
+            return AppOpsManagerCompat23.permissionToOp(permission);
+        }
+
+        @Override
+        public int noteOp(Context context, String op, int uid, String packageName) {
+            return AppOpsManagerCompat23.noteOp(context, op, uid, packageName);
+        }
+    }
+
+    private static final AppOpsManagerImpl IMPL;
+    static {
+        // TODO: Change to comparison against API 23 once we have it defined.
+        if (Build.VERSION.CODENAME.equals("MNC") || Build.VERSION.SDK_INT > 22) {
+            IMPL = new AppOpsManager23();
+        } else {
+            IMPL = new AppOpsManagerImpl();
+        }
+    }
+
+    /**
+     * Gets the app op name associated with a given permission.
+     *
+     * @param permission The permission.
+     * @return The app op associated with the permission or null.
+     */
+    public static String permissionToOp(@NonNull String permission) {
+        return IMPL.permissionToOp(permission);
+    }
+
+    /**
+     * Make note of an application performing an operation.  Note that you must pass
+     * in both the uid and name of the application to be checked; this function will verify
+     * that these two match, and if not, return {@link #MODE_IGNORED}.  If this call
+     * succeeds, the last execution time of the operation for this app will be updated to
+     * the current time.
+     * @param context Your context.
+     * @param op The operation to note.  One of the OPSTR_* constants.
+     * @param uid The user id of the application attempting to perform the operation.
+     * @param packageName The name of the application attempting to perform the operation.
+     * @return Returns {@link #MODE_ALLOWED} if the operation is allowed, or
+     * {@link #MODE_IGNORED} if it is not allowed and should be silently ignored (without
+     * causing the app to crash).
+     * @throws SecurityException If the app has been configured to crash on this op.
+     */
+    public static int noteOp(@NonNull Context context, String op, int uid, String packageName) {
+        return IMPL.noteOp(context, op, uid, packageName);
+    }
+}
