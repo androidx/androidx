9c1039484be273dcc63d9ee475655105361b65a6
Keisuke Kuroyanagi <ksk@google.com>
2015-07-09 14:36:28 +0900

    Fix: TextUtilsCompat doesn't respect DEVELOPMENT_FORCE_RTL.
    With this change, TextUtils#getLayoutDirectionFromLocale
    and TextUtils#htmlEncode are directly used for JellyBeanMR1
    (API Level=17) and later.
    Note that this CL changes nothing for API Level < 17.
    
    Bug: 18759277
    Change-Id: I8d085ed5094d80e75b5b219c05cc4bd752595915

---
+++ v4/jellybean-mr1/android/support/v4/text/TextUtilsCompatJellybeanMr1.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.v4.text;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import java.util.Locale;
+
+/**
+ * Jellybean MR1 - specific TextUtils API access.
+ */
+public class TextUtilsCompatJellybeanMr1 {
+    @NonNull
+    public static String htmlEncode(@NonNull String s) {
+        return TextUtils.htmlEncode(s);
+    }
+
+    public static int getLayoutDirectionFromLocale(@Nullable Locale locale) {
+        return TextUtils.getLayoutDirectionFromLocale(locale);
+    }
+}
