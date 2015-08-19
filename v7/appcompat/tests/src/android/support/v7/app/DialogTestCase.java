a4b5c35036e68c7551dd77641ceec17b92c6368d
Chris Banes <chrisbanes@google.com>
2015-08-19 10:41:42 +0100

    Fix AppCompatDialogFragment crashing
    We now model PhoneWindow in when it reads the theme
    window flags. Added a unit test too
    
    BUG: 23336734
    
    Change-Id: I990bd91d29d1009e0e18f976c7b3c542782660e6

---
+++ v7/appcompat/tests/src/android/support/v7/app/DialogTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.v7.app;
+
+import org.junit.Test;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.internal.app.WindowDecorActionBar;
+
+public class DialogTestCase extends BaseInstrumentationTestCase<WindowDecorActionBarActivity> {
+
+    public DialogTestCase() {
+        super(WindowDecorActionBarActivity.class);
+    }
+
+    @Test
+    public void testDialogFragmentShows() {
+        getInstrumentation().waitForIdleSync();
+
+        TestDialogFragment fragment = new TestDialogFragment();
+        fragment.show(getActivity().getSupportFragmentManager(), null);
+
+        getInstrumentation().waitForIdleSync();
+
+        assertNotNull("Dialog was null", fragment.getDialog());
+        assertTrue("Dialog was not being shown", fragment.getDialog().isShowing());
+    }
+
+    public static class TestDialogFragment extends AppCompatDialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle("Test")
+                    .setMessage("Message")
+                    .setPositiveButton("Button", null)
+                    .create();
+        }
+    }
+}
+
