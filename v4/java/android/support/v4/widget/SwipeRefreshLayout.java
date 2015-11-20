f629b8e6d20e9b5ae373bfad904a3c8932a82ca6
Aurimas Liutikas <aurimas@google.com>
2015-11-19 17:16:53 -0800

    Fix swipe to refresh arrow trimming and scaling issues for quick scrolls.
    For fast scrolls refresh circle arrow would get stuck in a bad state because
    we only called setStartEndTrim and setArrowScale only when
    overscrollTop < mTotalDragDistance. So it was possible that the last update
    for adjusted percentage was less than one.
    
    This CL calls setStartEndTrim and setArrowScale for all values of overscrollTop.
    
    Bug: 25311564
    Change-Id: Iae7182b42c8f2c61a64e78c36e82f20ff5ea7403

--- v4/java/android/support/v4/widget/SwipeRefreshLayout.java
+++ v4/java/android/support/v4/widget/SwipeRefreshLayout.java
@@ -906,24 +906,26 @@ public class SwipeRefreshLayout extends ViewGroup implements NestedScrollingPare
             ViewCompat.setScaleX(mCircleView, 1f);
             ViewCompat.setScaleY(mCircleView, 1f);
         }
+
+        if (mScale) {
+            setAnimationProgress(Math.min(1f, overscrollTop / mTotalDragDistance));
+        }
         if (overscrollTop < mTotalDragDistance) {
-            if (mScale) {
-                setAnimationProgress(overscrollTop / mTotalDragDistance);
-            }
             if (mProgress.getAlpha() > STARTING_PROGRESS_ALPHA
                     && !isAnimationRunning(mAlphaStartAnimation)) {
                 // Animate the alpha
                 startProgressAlphaStartAnimation();
             }
-            float strokeStart = adjustedPercent * .8f;
-            mProgress.setStartEndTrim(0f, Math.min(MAX_PROGRESS_ANGLE, strokeStart));
-            mProgress.setArrowScale(Math.min(1f, adjustedPercent));
         } else {
             if (mProgress.getAlpha() < MAX_ALPHA && !isAnimationRunning(mAlphaMaxAnimation)) {
                 // Animate the alpha
                 startProgressAlphaMaxAnimation();
             }
         }
+        float strokeStart = adjustedPercent * .8f;
+        mProgress.setStartEndTrim(0f, Math.min(MAX_PROGRESS_ANGLE, strokeStart));
+        mProgress.setArrowScale(Math.min(1f, adjustedPercent));
+
         float rotation = (-0.25f + .4f * adjustedPercent + tensionPercent * 2) * .5f;
         mProgress.setProgressRotation(rotation);
         setTargetOffsetTopAndBottom(targetY - mCurrentTargetOffsetTop, true /* requires update */);
