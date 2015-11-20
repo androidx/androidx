2f2b4ac338abb059272e2b9ad8a59402f31e20c8
Filip Gruszczynski <gruszczy@google.com>
2015-11-20 10:15:30 -0800

    Remove abc_allow_stacked_button_bar from support library.
    Bug: 25394588
    Change-Id: Ifb14e8b3f7246db1003ca4fdf6f0e57c8e0cfc9f

--- v7/appcompat/src/android/support/v7/widget/ButtonBarLayout.java
+++ v7/appcompat/src/android/support/v7/widget/ButtonBarLayout.java
@@ -30,6 +30,9 @@ import android.widget.LinearLayout;
  * @hide
  */
 public class ButtonBarLayout extends LinearLayout {
+    // Whether to allow vertically stacked button bars. This is disabled for
+    // configurations with a small (e.g. less than 320dp) screen height. -->
+    private static final int ALLOW_STACKING_MIN_HEIGHT_DP = 320;
 
     /** Whether the current configuration allows stacking. */
     private boolean mAllowStacking;
@@ -37,8 +40,12 @@ public class ButtonBarLayout extends LinearLayout {
 
     public ButtonBarLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        final boolean allowStackingDefault =
+                context.getResources().getConfiguration().screenHeightDp
+                        >= ALLOW_STACKING_MIN_HEIGHT_DP;
         final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ButtonBarLayout);
-        mAllowStacking = ta.getBoolean(R.styleable.ButtonBarLayout_allowStacking, false);
+        mAllowStacking = ta.getBoolean(R.styleable.ButtonBarLayout_allowStacking,
+                allowStackingDefault);
         ta.recycle();
     }
 
