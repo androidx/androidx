18ceb7a18836e906d2c575c90e6e979cff3c4414
Teng-Hui Zhu <ztenghui@google.com>
2015-12-07 15:27:29 -0800

    Move the manual test to <root>/development/ repository
    Under /development/samples/SupportVectorDrawable
    b/22564270
    
    Change-Id: I2a1833ced1beadb12c56aa51bf14fb7ae1f1b11b

--- graphics/drawable/runtest.sh
+++
@@ -1,5 +0,0 @@
-. ../../../../build/envsetup.sh
-mmm -j20 . && mmm -j20 ./teststatic/ && \
-adb install -r $OUT/data/app/AndroidVectorDrawableTests/AndroidVectorDrawableTests.apk && \
-adb shell am start -n android.support.test.vectordrawable/android.support.test.vectordrawable.TestActivity
-
