f28f4e01a22df1afb72e32977639ef8ae08bf814
Chris Banes <chrisbanes@google.com>
2016-01-04 12:07:31 +0000

    Add demo for ShareActionProvider
    Change-Id: I083354ef09738da812ea508f3091496f08f10123

---
+++ samples/Support7Demos/src/com/example/android/supportv7/app/ActionBarShareActionProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.supportv7.app;
+
+import com.example.android.supportv7.R;
+
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.View;
+
+/**
+ * This activity demonstrates how to use {@link ShareActionProvider} with the Action Bar.
+ */
+public class ActionBarShareActionProvider extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_share_action_provider, menu);
+        return true;
+    }
+
+    @Override
+    protected boolean onPrepareOptionsPanel(View view, Menu menu) {
+        final ShareActionProvider sap = (ShareActionProvider) MenuItemCompat.getActionProvider(
+                menu.findItem(R.id.menu_item_share_provider_action_bar));
+
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello!");
+
+        sap.setShareIntent(shareIntent);
+
+        return true;
+    }
+}
