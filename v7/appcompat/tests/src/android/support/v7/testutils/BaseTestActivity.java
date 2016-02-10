320463c1cf5f8e50cce340df195b60835fe23002
Aurimas Liutikas <aurimas@google.com>
2016-02-10 15:45:49 -0800

    Set activity enter and exit transitions to be no animation.
    This should speed up our tests as we spend a significant amount of
    time opening and closing activities.
    
    Bug: 27116907
    Change-Id: I1bdb0530100abe8c4e8ae217e873b35e4332e79d

--- v7/appcompat/tests/src/android/support/v7/testutils/BaseTestActivity.java
+++ v7/appcompat/tests/src/android/support/v7/testutils/BaseTestActivity.java
@@ -46,6 +46,7 @@ public abstract class BaseTestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         final int contentView = getContentViewLayoutResId();
         if (contentView > 0) {
@@ -53,6 +54,12 @@ public abstract class BaseTestActivity extends AppCompatActivity {
         }
         onContentViewSet();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(0, 0);
     }
 
     protected abstract int getContentViewLayoutResId();
