235b1ebe1ebdcdeaf8814be397dbeb8ed53e8074
Keyvan Amiri <keyvana@google.com>
2016-03-07 16:08:45 -0800

    Leanback Support Test: Added a wide range of unit tests for checking correctness of DatePicker
    The tests include checking the scrolling behavior, the transition across
    months to make sure the number of days within each month remains valid;
    checking the date formats and ensuring that all selected and scrollable
    dates remain within the specificed ranges.
    
    Change-Id: I6ffacd3210dd1b16ef28f34351aa0dfe4d00a3d4

--- v17/tests/src/android/support/v17/leanback/app/wizard/GuidedStepAttributesTestFragment.java
+++ v17/tests/src/android/support/v17/leanback/app/wizard/GuidedStepAttributesTestFragment.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package android.support.v17.leanback.app.wizard;
 
 import android.os.Bundle;
@@ -60,6 +74,18 @@ public class GuidedStepAttributesTestFragment extends GuidedStepFragment {
         super.onSubGuidedActionClicked(action);
         LAST_CLICKED_ACTION_ID = action.getId();
         return true;
+    }
+
+    @Override
+    public long onGuidedActionEditedAndProceed(GuidedAction action) {
+
+        Callback callback = sCallbacks.get(action.getId());
+        if (callback != null) {
+            callback.onActionClicked(this, action.getId());
+        } else {
+            super.onGuidedActionEditedAndProceed(action);
+        }
+        return GuidedAction.ACTION_ID_CURRENT;
     }
 
     public static void setActionClickCallback(long id, Callback callback) {
