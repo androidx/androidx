bc90b9d5f39b24f75a52b4ca1128dca477482e19
Adam Lesinski <adamlesinski@google.com>
2016-04-01 22:13:06 +0000

    Revert "Build Support7Demos with AAPT2"
    This reverts commit 9eb025a98dec74d64b147239998baf860ef67632.
    
    Change-Id: Ia0c13e51cdf01b704bd945332cbef6239f5403b6

--- samples/Support7Demos/Android.mk
+++ samples/Support7Demos/Android.mk
@@ -15,20 +15,36 @@
 LOCAL_PATH:= $(call my-dir)
 
 # Build the samples.
+# We need to add some special AAPT flags to generate R classes
+# for resources that are included from the libraries.
 include $(CLEAR_VARS)
-LOCAL_USE_AAPT2 := true
 LOCAL_PACKAGE_NAME := Support7Demos
 LOCAL_MODULE_TAGS := samples tests
 LOCAL_SDK_VERSION := current
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v7-appcompat \
-    android-support-v7-gridlayout \
-    android-support-v7-mediarouter \
-    android-support-v7-cardview \
-    android-support-design \
-    android-support-v7-recyclerview \
-    android-support-v7-palette \
-    android-support-v4
-LOCAL_AAPT_FLAGS := --no-version-vectors
+LOCAL_STATIC_JAVA_LIBRARIES := \
+        android-support-v4 \
+        android-support-v7-appcompat \
+        android-support-v7-gridlayout \
+        android-support-v7-mediarouter \
+        android-support-v7-palette \
+        android-support-v7-recyclerview \
+        android-support-v7-cardview
+LOCAL_RESOURCE_DIR = \
+        $(LOCAL_PATH)/res \
+        frameworks/support/v7/appcompat/res \
+        frameworks/support/v7/gridlayout/res \
+        frameworks/support/v7/mediarouter/res \
+        frameworks/support/v7/cardview/res \
+        frameworks/support/design/res \
+        frameworks/support/v7/recyclerview/res
+LOCAL_AAPT_FLAGS := \
+        --auto-add-overlay \
+        --extra-packages android.support.v7.appcompat \
+        --extra-packages android.support.v7.cardview \
+        --extra-packages android.support.v7.gridlayout \
+        --extra-packages android.support.v7.mediarouter \
+        --extra-packages android.support.design \
+        --extra-packages android.support.v7.recyclerview \
+        --no-version-vectors
 include $(BUILD_PACKAGE)
