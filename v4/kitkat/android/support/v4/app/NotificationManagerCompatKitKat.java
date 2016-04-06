85e038ee7cac169b3a9878cba881dc7eb401de2a
Chris Wren <cwren@android.com>
2016-04-06 14:40:37 -0400

    add support for areNotificationsEnabled
    now with more current.txt
    
    Bug: 28021291
    Change-Id: Ia569a4f8aad832470b5c0759613b06af1d725007
    Previously: Ibba1d8bdac59a1835f9a50d8a4f879b056f96be9

---
+++ v4/kitkat/android/support/v4/app/NotificationManagerCompatKitKat.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.support.v4.app;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class NotificationManagerCompatKitKat {
+    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+
+    public static boolean areNotificationsEnabled(Context context) {
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        ApplicationInfo appInfo = context.getApplicationInfo();
+        String pkg = context.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        try {
+            Class<?> appOpsClass = Class.forName(AppOpsManager.class.getName());
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE,
+                    Integer.TYPE, String.class);
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int) opPostNotificationValue.get(Integer.class);
+            return ((int) checkOpNoThrowMethod.invoke(appOps, value, uid, pkg)
+                    == AppOpsManager.MODE_ALLOWED);
+        } catch (ClassNotFoundException | NoSuchMethodException | NoSuchFieldException |
+                InvocationTargetException | IllegalAccessException | RuntimeException e) {
+            return true;
+        }
+    }
+}
