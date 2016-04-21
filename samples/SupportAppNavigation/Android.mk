0ee6452dbc30fdfa036509214d7c4b52fa616c96
Jae Seo <jaeseo@google.com>
2016-04-21 10:25:16 -0700

    Add missing LOCAL_MIN_SDK_VERSION for Support Library demos
    Bug: 28302992
    Change-Id: I456d407ba5442cd66571f307e02d5d26a4ee23aa

--- samples/SupportAppNavigation/Android.mk
+++ samples/SupportAppNavigation/Android.mk
@@ -11,6 +11,8 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 
 LOCAL_SDK_VERSION := current
 
+LOCAL_MIN_SDK_VERSION := 8
+
 include $(BUILD_PACKAGE)
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
