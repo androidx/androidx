8b98e883bc3fb72c13e0f41be3e6362ffb28a37c
Kirill Grouchnikov <kirillg@google.com>
2016-05-04 14:59:07 -0400

    Second clean-up pass after support-v4 split.
    Remove test resources / classes that are not relevant for the
    compat / core modules.
    
    Change-Id: I6c05774892727c3103003672e8a8e1481d7111c9

--- compat/tests/java/android/support/v4/testutils/TestUtils.java
+++ compat/tests/java/android/support/v4/testutils/TestUtils.java
@@ -34,34 +34,6 @@ import junit.framework.Assert;
 
 public class TestUtils {
     /**
-     * Converts the specified value from dips to pixels for use as view size.
-     */
-    public static int convertSizeDipsToPixels(DisplayMetrics displayMetrics, int dipValue) {
-        // Round to the nearest int value. This follows the logic in
-        // TypedValue.complexToDimensionPixelSize
-        final int res = (int) (dipValue * displayMetrics.density + 0.5f);
-        if (res != 0) {
-            return res;
-        }
-        if (dipValue == 0) {
-            return 0;
-        }
-        if (dipValue > 0) {
-            return 1;
-        }
-        return -1;
-    }
-
-    /**
-     * Converts the specified value from dips to pixels for use as view offset.
-     */
-    public static int convertOffsetDipsToPixels(DisplayMetrics displayMetrics, int dipValue) {
-        // Round to the nearest int value.
-        return (int) (dipValue * displayMetrics.density);
-    }
-
-
-    /**
      * Checks whether all the pixels in the specified drawable are of the same specified color.
      * If the passed <code>Drawable</code> does not have positive intrinsic dimensions set, this
      * method will throw an <code>IllegalArgumentException</code>. If there is a color mismatch,
