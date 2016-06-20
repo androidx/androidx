9b3f46259ee963c8a250cf4da78afb0c4b7787e9
Chris Banes <chrisbanes@google.com>
2016-06-20 08:39:48 +0100

    Fix ViewPager.DecorView annotation inheritance
    We forgot to add the @Inherited flag to the annotation
    which means that the annotation does not pass down the
    class hierarchy.
    
    BUG: 29452434
    Change-Id: Ie1534fbc82cf56984321864b25154dad339fd004

--- core-ui/java/android/support/v4/view/ViewPager.java
+++ core-ui/java/android/support/v4/view/ViewPager.java
@@ -55,6 +55,7 @@ import android.view.animation.Interpolator;
 import android.widget.Scroller;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -384,6 +385,7 @@ public class ViewPager extends ViewGroup {
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
+    @Inherited
     public @interface DecorView {
     }
 
