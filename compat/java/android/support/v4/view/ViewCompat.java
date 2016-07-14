17d15d92db2288bd27b8710c68e5bc1b9b5945f0
Kirill Grouchnikov <kirillg@google.com>
2016-07-14 13:15:02 -0400

    Additional deprecation notices on Compat APIs.
    Bug: 30089429
    Change-Id: Ie835c165a5a265146fda8f6cb9cf594933d0ce7d

--- compat/java/android/support/v4/view/ViewCompat.java
+++ compat/java/android/support/v4/view/ViewCompat.java
@@ -1841,7 +1841,8 @@ public class ViewCompat {
      *
      * @param v The View against which to invoke the method.
      * @return This view's over-scroll mode.
-     * @deprecated Call {@link View#getOverScrollMode()} directly.
+     * @deprecated Call {@link View#getOverScrollMode()} directly. This method will be
+     * removed in a future release.
      */
     @Deprecated
     @OverScroll
@@ -1861,7 +1862,8 @@ public class ViewCompat {
      *
      * @param v The View against which to invoke the method.
      * @param overScrollMode The new over-scroll mode for this view.
-     * @deprecated Call {@link View#setOverScrollMode(int)} directly.
+     * @deprecated Call {@link View#setOverScrollMode(int)} directly. This method will be
+     * removed in a future release.
      */
     @Deprecated
     public static void setOverScrollMode(View v, @OverScroll int overScrollMode) {
@@ -2350,7 +2352,8 @@ public class ViewCompat {
      * draw all the pixels overlapping its bounds using a fully opaque color.
      *
      * @return True if this View is guaranteed to be fully opaque, false otherwise.
-     * @deprecated Use {@link View#isOpaque()} directly.
+     * @deprecated Use {@link View#isOpaque()} directly. This method will be
+     * removed in a future release.
      */
     @Deprecated
     public static boolean isOpaque(View view) {
