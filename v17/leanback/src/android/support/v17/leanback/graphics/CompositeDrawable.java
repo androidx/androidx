df64e524a2bad079e6b0aa2d22c287f2c62aed82
susnata <susnata@google.com>
2016-07-15 10:59:57 -0700

    Revert "Revert "Creating a generic drawable class that will be used for parallax effect.""
    This reverts commit 31986ef8ab6ed7fb902aee7fd836542f78f8bae2.
    
    Change-Id: Ie9d1980f538ecf6e3067986906dc4197b4b41f4d

---
+++ v17/leanback/src/android/support/v17/leanback/graphics/CompositeDrawable.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.support.v17.leanback.graphics;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Generic drawable class that can be composed of multiple regions. Whenever the bounds changes
+ * for this class, it updates those of it's children by calling {@link RegionDrawable#updateBounds}.
+ */
+public class CompositeDrawable extends Drawable {
+    protected ArrayList<RegionDrawable> regions = new ArrayList();
+
+    /**
+     * Adds the supplied region.
+     */
+    public void addRegionDrawable(RegionDrawable region) {
+        this.regions.add(region);
+    }
+
+    /**
+     * Returns the {@link RegionDrawable} for the given index.
+     */
+    public RegionDrawable getRegion(int index) {
+        return regions.get(index);
+    }
+
+    /**
+     * Removes the region corresponding to the given index.
+     */
+    public void removeRegion(int index) {
+        regions.remove(index);
+    }
+
+    /**
+     * Removes the given region.
+     */
+    public void removeRegion(RegionDrawable drawable) {
+        this.regions.remove(drawable);
+    }
+
+    /**
+     * Returns the total number of regions.
+     */
+    public int getRegionCount() {
+        return regions.size();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        for (int i = 0; i < regions.size(); i++) {
+            regions.get(i).draw(canvas);
+        }
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        super.onBoundsChange(bounds);
+        for (int i = 0; i < regions.size(); i++) {
+            regions.get(i).updateBounds(bounds);
+        }
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        for (int i = 0; i < regions.size(); i++) {
+            regions.get(i).setColorFilter(colorFilter);
+        }
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.UNKNOWN;
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        for (int i = 0; i < regions.size(); i++) {
+            regions.get(i).setAlpha(alpha);
+        }
+    }
+}
