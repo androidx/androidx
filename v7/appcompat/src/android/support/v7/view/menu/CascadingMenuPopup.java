e1e6484a2a2b58ec5ff8d22c437b122b2823f3c6
Chris Banes <chrisbanes@google.com>
2016-07-27 08:31:17 +0100

    Fix CascadingMenuPopup width on API 19 and below
    Happens because CascadingMenuPopup calls setWidth() rather
    than setContentWidth() like StandardMenuPopup does.
    setContentWidth() takes into account any padding in the popup
    background, which we use since the drawable is a 9-patch on
    API <= 19.
    
    BUG: 30365568
    Change-Id: I2cfeb83e9dd33ac46ab9a445b4dcdf999c507ce7

--- v7/appcompat/src/android/support/v7/view/menu/CascadingMenuPopup.java
+++ v7/appcompat/src/android/support/v7/view/menu/CascadingMenuPopup.java
@@ -361,7 +361,7 @@ final class CascadingMenuPopup extends MenuPopup implements MenuPresenter, OnKey
         final int menuWidth = measureIndividualMenuWidth(adapter, null, mContext, mMenuMaxWidth);
         final MenuPopupWindow popupWindow = createPopupWindow();
         popupWindow.setAdapter(adapter);
-        popupWindow.setWidth(menuWidth);
+        popupWindow.setContentWidth(menuWidth);
         popupWindow.setDropDownGravity(mDropDownGravity);
 
         final CascadingMenuInfo parentInfo;
