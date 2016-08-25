657ea1100fee4750f148f9d0dcb7e7e2028f105e
Aurimas Liutikas <aurimas@google.com>
2016-08-25 10:08:48 -0700

    Remove all the extra accessor methods added by javac in support-design.
    Change private methods to be package protected to avoid extra methods
    that get added to be able to access them from inner classes.
    
    This CL saves 138 methods in our jar.
    
    Bug: 31075707
    Change-Id: I9ee192652fe005957e28ccfe35c9707be242aac1

--- design/src/android/support/design/widget/CoordinatorLayout.java
+++ design/src/android/support/design/widget/CoordinatorLayout.java
@@ -132,9 +132,9 @@ public class CoordinatorLayout extends ViewGroup implements NestedScrollingParen
             new ThreadLocal<>();
 
 
-    private static final int EVENT_PRE_DRAW = 0;
-    private static final int EVENT_NESTED_SCROLL = 1;
-    private static final int EVENT_VIEW_REMOVED = 2;
+    static final int EVENT_PRE_DRAW = 0;
+    static final int EVENT_NESTED_SCROLL = 1;
+    static final int EVENT_VIEW_REMOVED = 2;
 
     /** @hide */
     @Retention(RetentionPolicy.SOURCE)
@@ -172,7 +172,7 @@ public class CoordinatorLayout extends ViewGroup implements NestedScrollingParen
     private boolean mDrawStatusBarBackground;
     private Drawable mStatusBarBackground;
 
-    private OnHierarchyChangeListener mOnHierarchyChangeListener;
+    OnHierarchyChangeListener mOnHierarchyChangeListener;
     private android.support.v4.view.OnApplyWindowInsetsListener mApplyWindowInsetsListener;
 
     private final NestedScrollingParentHelper mNestedScrollingParentHelper =
@@ -2502,8 +2502,8 @@ public class CoordinatorLayout extends ViewGroup implements NestedScrollingParen
          */
         public int dodgeInsetEdges = Gravity.NO_GRAVITY;
 
-        private int mInsetOffsetX;
-        private int mInsetOffsetY;
+        int mInsetOffsetX;
+        int mInsetOffsetY;
 
         View mAnchorView;
         View mAnchorDirectChild;
@@ -2851,6 +2851,9 @@ public class CoordinatorLayout extends ViewGroup implements NestedScrollingParen
     }
 
     private class HierarchyChangeListener implements OnHierarchyChangeListener {
+        HierarchyChangeListener() {
+        }
+
         @Override
         public void onChildViewAdded(View parent, View child) {
             if (mOnHierarchyChangeListener != null) {
