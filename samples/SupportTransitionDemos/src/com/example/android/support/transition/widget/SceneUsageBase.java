8934dca5ba4e6a365f8c0ab9c434b71c9f495294
Yuichi Araki <yaraki@google.com>
2016-09-12 11:47:40 -0700

    Add usage of beginDelayedTransition
    Change-Id: Ida9533ec3607b6d36cb29a05b80d559aa870c283

--- samples/SupportTransitionDemos/src/com/example/android/support/transition/widget/SceneUsageBase.java
+++ samples/SupportTransitionDemos/src/com/example/android/support/transition/widget/SceneUsageBase.java
@@ -16,35 +16,54 @@
 
 package com.example.android.support.transition.widget;
 
-import android.support.annotation.LayoutRes;
-import com.example.android.support.transition.R;
-
 import android.os.Bundle;
 import android.support.transition.Scene;
 import android.support.transition.TransitionManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import com.example.android.support.transition.R;
 
-/**
- * Base class for usages of the Transition API.
- */
-abstract class TransitionUsageBase extends AppCompatActivity {
+abstract class SceneUsageBase extends TransitionUsageBase {
 
-    @LayoutRes
-    abstract int getLayoutResId();
+    private Scene[] mScenes;
+
+    private int mCurrentScene;
+
+    abstract Scene[] setUpScenes(ViewGroup root);
+
+    abstract void go(Scene scene);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutResId());
 
-        // Retrieve the Toolbar from our content view, and set it as the action bar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        FrameLayout root = (FrameLayout) findViewById(R.id.root);
+        mScenes = setUpScenes(root);
+        TransitionManager.go(mScenes[0]);
+    }
+
+    @Override
+    int getLayoutResId() {
+        return R.layout.scene_usage;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.basic_usage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_toggle:
+                mCurrentScene = (mCurrentScene + 1) % mScenes.length;
+                go(mScenes[mCurrentScene]);
+                return true;
+        }
+        return false;
     }
 
 }
