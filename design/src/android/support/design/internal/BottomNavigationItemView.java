155d8e15c8f1b959196c16555c54d19a5ccafe99
Vladislav Kaznacheev <kaznacheev@google.com>
2016-09-21 12:02:49 -0700

    Implement custom pointer icon for widgets in support lib
    BottomNavigationItemView and TabLayout now display
    PointerIcon.TYPE_HAND when hovered.
    
    Bug: 31652293
    Change-Id: I6cf4690f7cdf85eed04609c169f0b60bce044674

--- design/src/android/support/design/internal/BottomNavigationItemView.java
+++ design/src/android/support/design/internal/BottomNavigationItemView.java
@@ -25,6 +25,7 @@ import android.support.annotation.NonNull;
 import android.support.design.R;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.view.PointerIconCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.view.menu.MenuItemImpl;
@@ -84,6 +85,8 @@ public class BottomNavigationItemView extends FrameLayout implements MenuView.It
         mSmallLabel = (TextView) findViewById(R.id.smallLabel);
         mLargeLabel = (TextView) findViewById(R.id.largeLabel);
 
+        ViewCompat.setPointerIcon(this,
+                PointerIconCompat.getSystemIcon(getContext(), PointerIconCompat.TYPE_HAND));
     }
 
     @Override
