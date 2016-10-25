6f1e49c8279c54bc46508eba7b46a5cda4d0d725
Alan Viverette <alanv@google.com>
2016-10-25 17:40:39 +0100

    Clean up v17-leanback makefile
    Test: docs target builds successfully
    Change-Id: Icf2ea1c372559476181836c0362d799a5c556c0d

--- v17/leanback/Android.mk
+++ v17/leanback/Android.mk
@@ -47,51 +47,30 @@ LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 LOCAL_AAPT_FLAGS := --add-javadoc-annotation doconly
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
-# ===========================================================
-# Common Droiddoc vars
-leanback.docs.src_files := \
-    $(call all-java-files-under, src) \
-    $(call all-html-files-under, src)
-leanback.docs.java_libraries := \
-    android-support-annotations \
-    android-support-v4 \
-    android-support-v7-recyclerview \
-    android-support-v17-leanback-res \
-    android-support-v17-leanback
-
 # Documentation
 # ===========================================================
 include $(CLEAR_VARS)
-
+LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := android-support-v17-leanback
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-
-gen_res_src_dirs := $(call intermediates-dir-for,JAVA_LIBRARIES,android-support-v17-leanback-res,,COMMON)/src
-
-LOCAL_SRC_FILES := $(leanback.docs.src_files)
-LOCAL_ADDITIONAL_JAVA_DIR := $(gen_res_src_dirs)
-
 LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-html-files-under, src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_JAVA_LIBRARIES := \
+    android-support-annotations \
+    android-support-v4 \
+    android-support-v7-recyclerview \
+    android-support-v17-leanback
+LOCAL_ADDITIONAL_JAVA_DIR := $(call intermediates-dir-for,JAVA_LIBRARIES,android-support-v17-leanback-res,,COMMON)/src
 LOCAL_IS_HOST_MODULE := false
 LOCAL_DROIDDOC_CUSTOM_TEMPLATE_DIR := build/tools/droiddoc/templates-sdk
-
-LOCAL_JAVA_LIBRARIES := $(leanback.docs.java_libraries)
-
 LOCAL_DROIDDOC_OPTIONS := \
     -offlinemode \
     -hdf android.whichdoc offline \
     -federate Android http://developer.android.com \
     -federationapi Android prebuilts/sdk/api/17.txt \
     -hide 113
-
 include $(BUILD_DROIDDOC)
-
-# Cleanup temp vars
-# ===========================================================
-leanback.docs.src_files :=
-leanback.docs.java_libraries :=
-gen_res_src_dirs :=
-leanback_internal_api_file :=
-leanback_stubs_stamp :=
-leanback.docs.stubpackages :=
