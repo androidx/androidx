5c68eb49a2a99f5a61be3b743430bdd5621b31e5
Yigit Boyar <yboyar@google.com>
2016-12-01 12:18:11 -0800

    Lock layout while consuming updates for measure
    If a layout is requestd during measure due to adapter updates, we
    were not blocking it which is a problem. This CL makes sure that
    we don't call nested layouts while processing adapter updates.
    
    Bug: 32316425
    Bug: 33270009
    Test: RecyclerViewLayoutTest#triggerFocusSearchInOnRecycledCallback
    Change-Id: I83f3678cf29cadd17deae7c3ec5f7805c08ffdeb

--- v7/recyclerview/src/android/support/v7/widget/RecyclerView.java
+++ v7/recyclerview/src/android/support/v7/widget/RecyclerView.java
@@ -2989,7 +2989,9 @@ public class RecyclerView extends ViewGroup implements ScrollingView, NestedScro
             // custom onMeasure
             if (mAdapterUpdateDuringMeasure) {
                 eatRequestLayout();
+                onEnterLayoutOrScroll();
                 processAdapterUpdatesAndSetAnimationFlags();
+                onExitLayoutOrScroll();
 
                 if (mState.mRunPredictiveAnimations) {
                     mState.mInPreLayout = true;
