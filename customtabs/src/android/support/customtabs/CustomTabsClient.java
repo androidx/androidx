914073c7cf933229a3f51ddeddb63bb9725a70be
Yusuf Ozuysal <yusufo@google.com>
2017-01-30 12:00:23 -0800

    Add postMessage APIs to customtabs support lib
    Adds CustomTabsSession#requestPostMessageChannel,
    CustomTabsSession#postMessage,
    CustomTabsCallback#onMessageChannelCreated,
    CustomTabsCallback#onPostMessage for sending an receiving postMessage
    requests to the mainframe of the initial url.
    
    Adds PostMessageService and PostMessageServiceConnection for handling
    the browser->client callback lifecycle and message routing
    
    BUG=32809427
    Test: android.support.customtabs.PostMessageTest, android.support.customtabs.PostMessageConnectionTest
    
    Change-Id: I3324a382b81fad97000d48389f4a0411e0b6f89e

--- customtabs/src/android/support/customtabs/CustomTabsClient.java
+++ customtabs/src/android/support/customtabs/CustomTabsClient.java
@@ -26,6 +26,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
@@ -177,21 +179,60 @@ public class CustomTabsClient {
      * then later with a Custom Tab. The client can then send later service calls or intents to
      * through same session-intent-Custom Tab association.
      * @param callback The callback through which the client will receive updates about the created
-     *                 session. Can be null.
+     *                 session. Can be null. All the callbacks will be received on the UI thread.
      * @return The session object that was created as a result of the transaction. The client can
-     *         use this to relay {@link CustomTabsSession#mayLaunchUrl(Uri, Bundle, List)} calls.
+     *         use this to relay session specific calls.
      *         Null on error.
      */
     public CustomTabsSession newSession(final CustomTabsCallback callback) {
         ICustomTabsCallback.Stub wrapper = new ICustomTabsCallback.Stub() {
+            private Handler mHandler = new Handler(Looper.getMainLooper());
+
             @Override
-            public void onNavigationEvent(int navigationEvent, Bundle extras) {
-                if (callback != null) callback.onNavigationEvent(navigationEvent, extras);
+            public void onNavigationEvent(final int navigationEvent, final Bundle extras) {
+                if (callback == null) return;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onNavigationEvent(navigationEvent, extras);
+                    }
+                });
             }
 
             @Override
-            public void extraCallback(String callbackName, Bundle args) throws RemoteException {
-                if (callback != null) callback.extraCallback(callbackName, args);
+            public void extraCallback(final String callbackName, final Bundle args)
+                    throws RemoteException {
+                if (callback == null) return;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.extraCallback(callbackName, args);
+                    }
+                });
+            }
+
+            @Override
+            public void onMessageChannelReady(final Bundle extras)
+                    throws RemoteException {
+                if (callback == null) return;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onMessageChannelReady(extras);
+                    }
+                });
+            }
+
+            @Override
+            public void onPostMessage(final String message, final Bundle extras)
+                    throws RemoteException {
+                if (callback == null) return;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onPostMessage(message, extras);
+                    }
+                });
             }
         };
 
