b6e9d617ea32dc83840a030982a97a20f7889010
Tobias Thierer <tobiast@google.com>
2017-02-09 11:06:49 +0000

    Apply MapCollections behavior changes to appcompat copy
    Array{Map,Set} iterators no longer allow iterating past the end.
    For apps that care about consistent behavior, this CL makes
    the new behavior available on old versions of Android through
    the appcompat library.
    
    Added tests for the behavior part that differs from earlier
    Android versions.
    
    Changes are ported from http://ag/1886799 and http://ag/1885941
    
    Bug: 19853326
    
    Test: ./gradlew support-compat:connectedCheck --info
    
    Change-Id: I9f5ff9ad0a1f5974c1696da06b8962f7fcac8134

--- compat/java/android/support/v4/util/MapCollections.java
+++ compat/java/android/support/v4/util/MapCollections.java
@@ -20,6 +20,7 @@ import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
@@ -49,6 +50,7 @@ abstract class MapCollections<K, V> {
 
         @Override
         public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
             Object res = colGetEntry(mIndex, mOffset);
             mIndex++;
             mCanRemove = true;
@@ -84,6 +86,7 @@ abstract class MapCollections<K, V> {
 
         @Override
         public Map.Entry<K, V> next() {
+            if (!hasNext()) throw new NoSuchElementException();
             mIndex++;
             mEntryValid = true;
             return this;
