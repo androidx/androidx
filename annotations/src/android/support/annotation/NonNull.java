a0a049fa0df234d1159ab45bcf1c83a2e990a07d
Tor Norbye <tnorbye@google.com>
2017-02-09 16:31:26 -0800

    227810: Allow @Nullable annotations on local variables.
    Test: N/A
    Change-Id: Ie4a55e2d630e18896afd985a21a65c07ff8f9c50

--- annotations/src/android/support/annotation/NonNull.java
+++ annotations/src/android/support/annotation/NonNull.java
@@ -17,6 +17,7 @@ package android.support.annotation;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.PARAMETER;
@@ -33,6 +34,6 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(CLASS)
-@Target({METHOD, PARAMETER, FIELD, ANNOTATION_TYPE, PACKAGE})
+@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
 public @interface NonNull {
 }
