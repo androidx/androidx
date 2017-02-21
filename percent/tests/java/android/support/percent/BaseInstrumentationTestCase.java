d2900e6bd6bd80bfb28044499279e276e76e6851
Aurimas Liutikas <aurimas@google.com>
2017-02-21 10:32:05 -0800

    Cleanup test AndroidManifest.xml files.
    - We moved away from the old test runner and we use JUnit4 everywhere.
    - Move palette tests to v7/palette/tests to match the rest of support library.
    
    Test: None
    Change-Id: Ifc4603dafa676937baff7465a8cad26439b433ef

--- percent/tests/java/android/support/percent/BaseInstrumentationTestCase.java
+++ percent/tests/java/android/support/percent/BaseInstrumentationTestCase.java
@@ -16,18 +16,14 @@
 
 package android.support.percent;
 
+import static junit.framework.Assert.assertEquals;
+
 import android.app.Activity;
-import android.app.Instrumentation;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
-import junit.framework.Assert;
-import org.junit.Before;
+
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import static junit.framework.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public abstract class BaseInstrumentationTestCase<A extends Activity> {
