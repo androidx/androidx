ab6d8e656800efdb3de2595880c1fd24fd0d5d7d
Chris Craik <ccraik@google.com>
2017-02-23 13:47:45 -0800

    Temporarily suppress Browse fragment tests
    Test: none
    Bug: 35708283
    
    Change-Id: I7c87cff9951bec650af7597f9805844a27b87bc8

--- v17/leanback/tests/java/android/support/v17/leanback/app/BrowseFragmentTest.java
+++ v17/leanback/tests/java/android/support/v17/leanback/app/BrowseFragmentTest.java
@@ -25,6 +25,7 @@ import static org.mockito.Mockito.verify;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
+import android.support.test.filters.Suppress;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v17.leanback.widget.ItemBridgeAdapter;
@@ -40,6 +41,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
+@Suppress
 @MediumTest
 @RunWith(AndroidJUnit4.class)
 public class BrowseFragmentTest {
