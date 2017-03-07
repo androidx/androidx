ffcdd45f193e0243ab3c0b1a04c71ef6e9488b3a
Aurimas Liutikas <aurimas@google.com>
2017-03-06 17:22:32 -0800

    Add missing import in DirectedAcyclicGraphTest
    This fixes host side test building.
    
    Test: ./gradlew test now works
    Change-Id: I4935595303f6b056cfbf66a71214d39e6c03ba15

--- design/jvm-tests/src/android/support/design/widget/DirectedAcyclicGraphTest.java
+++ design/jvm-tests/src/android/support/design/widget/DirectedAcyclicGraphTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import android.support.annotation.NonNull;
+import android.support.test.filters.SmallTest;
 
 import org.junit.Before;
 import org.junit.Test;
