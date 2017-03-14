643eafb3919ebd3d4412d31926586fe35a97d510
Ian Lake <ilake@google.com>
2017-03-14 13:45:57 -0700

    Initial commit of Navigation into ApiReviewDemo
    Add first draft of Navigation code into the ApiReviewDemo,
    allowing users to see a list of items and click on one to
    go to a detail screen of that item.
    
    Also creates a series of navigation XML files that can be
    used to step through the functionality available to developers.
    
    Test: Run ApiReviewDemo's News activity
    Change-Id: I2758718814ce673d29e15d0ee2f72a4fe484b18c

---
+++ samples-flatfoot/ApiReviewDemo/app/src/main/java/com/android/flatfoot/apireviewdemo/navigation/ArticleFragment.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.flatfoot.apireviewdemo.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.flatfoot.apireviewdemo.R;
+
+/**
+ * Fragment which shows the details of a single article
+ */
+public class ArticleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.article_fragment, container, false);
+        TextView articleNumber = (TextView) view.findViewById(R.id.article_number);
+        articleNumber.setText("Article " + getArguments().getInt("articleId", -1));
+        return view;
+    }
+}
