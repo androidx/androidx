94cfd171726b86d5d52ff835dde5d62402af5a7e
Sungsoo <sungsoo@google.com>
2017-04-19 09:56:33 +0900

    MediaControllerCompat: Apply API Review
    - Update documentation of getMediaController and setMediaController
    - Fix reference to getMediaController
    - Add reference to the Media App developer guide
    
    Bug: 35947966
    Test: ./gradlew support-media-compat:connectedCheck
    Change-Id: I1f9288b64309eb3d3d1b828324ea01c33d6e51ba

--- media-compat/java/android/support/v4/media/MediaBrowserServiceCompat.java
+++ media-compat/java/android/support/v4/media/MediaBrowserServiceCompat.java
@@ -105,6 +105,12 @@ import java.util.List;
  *     &lt;/intent-filter>
  * &lt;/service>
  * </pre>
+ *
+ * <div class="special reference">
+ * <h3>Developer Guides</h3>
+ * <p>For information about building your media application, read the
+ * <a href="{@docRoot}guide/topics/media-apps/index.html">Media Apps</a> developer guide.</p>
+ * </div>
  */
 public abstract class MediaBrowserServiceCompat extends Service {
     static final String TAG = "MBServiceCompat";
