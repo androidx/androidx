0584aa1690129f0c41e42306a3e0bc7fc59c205c
Ian Lake <ilake@google.com>
2017-04-20 16:55:43 -0700

    Add ActivityNavigator attributes
    Fills out ActivityNavigator with new attributes:
    - android:name for setting an explicit class name
    - action and data for setting the appropriate Intent attributes
    - dataPattern, providing a dynamic pattern
    
    dataPattern takes any {argName} sections in the pattern and
    automatically fills them in with the arguments passed to the
    navigate call, making them much more useful than static data URIs.
    
    Also fixed crashes in ActivityNavigator due to null extras and
    null NavOptions.
    
    Test: Run testapp, click on Settings (implicit) and Help (explicit)
    
    BUG: 37539035
    Change-Id: I45f37821a1230701086079797951385f3d22f8d9

--- navigation/runtime/src/main/java/android/support/navigation/app/nav/ActivityNavigator.java
+++ navigation/runtime/src/main/java/android/support/navigation/app/nav/ActivityNavigator.java
@@ -20,10 +20,18 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+
+import com.android.support.navigation.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * ActivityNavigator implements cross-activity navigation.
@@ -71,19 +79,38 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
     public boolean navigate(Destination destination, Bundle args, NavOptions navOptions) {
         Intent intent = new Intent(destination.getIntent());
         if (args != null) {
-            intent.getExtras().putAll(args);
+            intent.putExtras(args);
+            String dataPattern = destination.getDataPattern();
+            if (!TextUtils.isEmpty(dataPattern)) {
+                // Fill in the data pattern with the args to build a valid URI
+                StringBuffer data = new StringBuffer();
+                Pattern fillInPattern = Pattern.compile("\\{(.+?)\\}");
+                Matcher matcher = fillInPattern.matcher(dataPattern);
+                while (matcher.find()) {
+                    String argName = matcher.group(1);
+                    if (args.containsKey(argName)) {
+                        matcher.appendReplacement(data, "");
+                        data.append(Uri.encode(args.getString(argName)));
+                    } else {
+                        throw new IllegalArgumentException("Could not find " + argName + " in "
+                                + args + " to fill data pattern " + dataPattern);
+                    }
+                }
+                matcher.appendTail(data);
+                intent.setData(Uri.parse(data.toString()));
+            }
         }
-        if (navOptions.shouldClearTask()) {
+        if (navOptions != null && navOptions.shouldClearTask()) {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         }
-        if (navOptions.shouldLaunchDocument()
+        if (navOptions != null && navOptions.shouldLaunchDocument()
                 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         } else if (!(mContext instanceof Activity)) {
             // If we're not launching from an Activity context we have to launch in a new task.
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
-        if (navOptions.shouldLaunchSingleTop()) {
+        if (navOptions != null && navOptions.shouldLaunchSingleTop()) {
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         }
         if (mHostActivity != null) {
@@ -110,6 +137,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
      */
     public static class Destination extends NavDestination {
         private Intent mIntent;
+        private String mDataPattern;
 
         /**
          * Construct a new activity destination to navigate to the given Intent.
@@ -130,7 +158,19 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
         @Override
         public void onInflate(Context context, AttributeSet attrs) {
             super.onInflate(context, attrs);
-            // TODO Implement me!
+            TypedArray a = context.getResources().obtainAttributes(attrs,
+                    R.styleable.ActivityNavigator);
+            String cls = a.getString(R.styleable.ActivityNavigator_android_name);
+            if (!TextUtils.isEmpty(cls)) {
+                setComponentName(new ComponentName(context, cls));
+            }
+            setAction(a.getString(R.styleable.ActivityNavigator_action));
+            String data = a.getString(R.styleable.ActivityNavigator_data);
+            if (data != null) {
+                setData(Uri.parse(data));
+            }
+            setDataPattern(a.getString(R.styleable.ActivityNavigator_dataPattern));
+            a.recycle();
         }
 
         /**
@@ -170,6 +210,75 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
                 return null;
             }
             return mIntent.getComponent();
+        }
+
+        /**
+         * Sets the action sent when navigating to this destination.
+         * @param action The action string to use.
+         */
+        public void setAction(String action) {
+            if (mIntent == null) {
+                mIntent = new Intent();
+            }
+            mIntent.setAction(action);
+        }
+
+        /**
+         * Get the action used to start the Activity, if any
+         */
+        public String getAction() {
+            if (mIntent == null) {
+                return null;
+            }
+            return mIntent.getAction();
+        }
+
+        /**
+         * Sets a static data URI that is sent when navigating to this destination.
+         *
+         * <p>To use a dynamic URI that changes based on the arguments passed in when navigating,
+         * use {@link #setDataPattern(String)}, which will take precedence when arguments are
+         * present.</p>
+         *
+         * @param data A static URI that should always be used.
+         * @see #setDataPattern(String)
+         */
+        public void setData(Uri data) {
+            if (mIntent == null) {
+                mIntent = new Intent();
+            }
+            mIntent.setData(data);
+        }
+
+        /**
+         * Get the data URI used to start the Activity, if any
+         */
+        public Uri getData() {
+            if (mIntent == null) {
+                return null;
+            }
+            return mIntent.getData();
+        }
+
+        /**
+         * Sets a dynamic data URI pattern that is sent when navigating to this destination.
+         *
+         * <p>If a non-null arguments Bundle is present when navigating, any segments in the form
+         * <code>{argName}</code> will be replaced with a URI encoded string from the arguments.</p>
+         * @param dataPattern A URI pattern with segments in the form of <code>{argName}</code> that
+         *                    will be replaced with URI encoded versions of the Strings in the
+         *                    arguments Bundle.
+         * @see #setData
+         */
+        public void setDataPattern(String dataPattern) {
+            mDataPattern = dataPattern;
+        }
+
+        /**
+         * Gets the dynamic data URI pattern, if any
+         */
+        public String getDataPattern() {
+            return mDataPattern;
         }
     }
 }
