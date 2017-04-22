75b9131e050fa89532745deca4313c08f261d4e4
Siyamed Sinir <siyamed@google.com>
2017-04-21 19:10:41 -0700

    Update EmojiCompat build
    support-emoji and support-emoji-bundled build.gradle and Android.mk
    files are updated to use artifacts from noto-fonts/emoji-compat.
    
    Test: ran ./gradlew uploadArchives
    ran ./gradlew generateDocs
    ran ./gradlew support-emoji:connectedAndroidTest
    
    Bug: 37477831
    Change-Id: I692924fc7e0850b8a30826859e5ebd0110f6449f

--- emoji/core/Android.mk
+++ emoji/core/Android.mk
@@ -24,33 +24,16 @@ LOCAL_PATH := $(call my-dir)
 # in their makefiles to include the resources and their dependencies in their package.
 #
 include $(CLEAR_VARS)
-EXTERNAL_FONT_DIR := ../../../../external/noto-fonts/emoji-compat
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := android-support-emoji
 LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-    $(call all-java-files-under, $(EXTERNAL_FONT_DIR)/src/java)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-emoji-flatbuffers-jarjar
+    noto-emoji-compat-java
 LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-annotations \
     android-support-compat
 LOCAL_JAR_EXCLUDE_FILES := none
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 LOCAL_AAPT_FLAGS := --add-javadoc-annotation doconly
-include $(BUILD_STATIC_JAVA_LIBRARY)
-
-#############################################################
-# Pre-built dependency jars
-#############################################################
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    android-support-emoji-flatbuffers:$(EXTERNAL_FONT_DIR)/libs/flatbuffers-java-1.6.0.jar
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-emoji-flatbuffers
-LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
-LOCAL_MODULE := android-support-emoji-flatbuffers-jarjar
 include $(BUILD_STATIC_JAVA_LIBRARY)
