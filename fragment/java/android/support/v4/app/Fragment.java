72e36e00c109db79f81877802e9db6ccfc9cef4f
George Mount <mount@google.com>
2017-07-17 12:44:05 -0700

    DO NOT MERGE: Fix Fragment.onCreate() not being called in some cases.
    Bug 62798751
    
    When an application process has been killed with a retained instance
    fragment on its back stack, and a configuration change causes
    state save, the retained instance fragment was not calling onCreate()
    when it was popped back into existence.
    
    The cause of the problem is that the mRetaining flag was being set,
    and when it was supposed to go through onCreate(), it was skipped.
    This CL uses a new flag to judge whether or not a Fragment has
    called onCreate() or not.
    
    Test: ran fragment tests
    Change-Id: Ia66a1f474d815f657b6962097139c796f7fa198b

--- fragment/java/android/support/v4/app/Fragment.java
+++ fragment/java/android/support/v4/app/Fragment.java
@@ -340,6 +340,11 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
     // getLayoutInflater()
     LayoutInflater mLayoutInflater;
 
+    // Keep track of whether or not this Fragment has run performCreate(). Retained instance
+    // fragments can have mRetaining set to true without going through creation, so we must
+    // track it separately.
+    boolean mIsCreated;
+
     /**
      * State information that has been retrieved from a fragment instance
      * through {@link FragmentManager#saveFragmentInstanceState(Fragment)
@@ -2322,6 +2327,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
         mState = CREATED;
         mCalled = false;
         onCreate(savedInstanceState);
+        mIsCreated = true;
         if (!mCalled) {
             throw new SuperNotCalledException("Fragment " + this
                     + " did not call through to super.onCreate()");
@@ -2583,6 +2589,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
         }
         mState = INITIALIZING;
         mCalled = false;
+        mIsCreated = false;
         onDestroy();
         if (!mCalled) {
             throw new SuperNotCalledException("Fragment " + this
