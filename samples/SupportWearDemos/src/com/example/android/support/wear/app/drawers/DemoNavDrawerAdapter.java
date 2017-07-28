65ab191c2a396a77d24227719c7c1e7656b48084
Sean Kelley <smskelley@google.com>
2017-07-28 15:17:05 -0700

    Add WAD/WND sample to :support-wear-demos
    This adds a WAD/WND sample to :support-wear-demos. In its current form,
    it allows the user to select between two fragments: one with a
    FrameLayout and the other with a ScrollView. In the future, it would be
    useful to also add NestedScrollView, ListView, and RecyclerView.
    
    Test: Manually launching the sample app
    Bug: 63423140
    Change-Id: I0f4e86f0e0e1667ef9ed64d4ca9ca02783070b35

---
+++ samples/SupportWearDemos/src/com/example/android/support/wear/app/drawers/DemoNavDrawerAdapter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.support.wear.app.drawers;
+
+import android.graphics.drawable.Drawable;
+import android.support.wear.widget.drawer.WearableNavigationDrawerView.WearableNavigationDrawerAdapter;
+
+/** Simple and declarative {@link WearableNavigationDrawerAdapter}. */
+public class DemoNavDrawerAdapter extends WearableNavigationDrawerAdapter {
+
+    private final NavItem[] mNavItems;
+
+    public DemoNavDrawerAdapter(NavItem[] navItems) {
+        mNavItems = navItems;
+    }
+
+    @Override
+    public CharSequence getItemText(int pos) {
+        return mNavItems[pos].getTitle();
+    }
+
+    @Override
+    public Drawable getItemDrawable(int pos) {
+        return mNavItems[pos].getDrawable();
+    }
+
+    @Override
+    public int getCount() {
+        return mNavItems.length;
+    }
+}
