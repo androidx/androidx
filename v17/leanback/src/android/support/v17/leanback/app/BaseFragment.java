8b66ca38dbb9f4af7814e7048a14924deb6251a9
Dake Gu <dake@google.com>
2017-09-14 16:10:42 -0700

    leanback: skip startEntranceTransition after destroyed
    Though the app should check before calling startEntranceTransition,
    added extra check in leanback library.
    
    Bug: 65687171
    Test: startEntranceTransitionAfterDestroyed
    Change-Id: I5b3fd108d907b404798f1fbf6facc8cc4b732074

--- v17/leanback/src/android/support/v17/leanback/app/BaseFragment.java
+++ v17/leanback/src/android/support/v17/leanback/app/BaseFragment.java
@@ -267,6 +267,10 @@ public class BaseFragment extends BrandedFragment {
     void onExecuteEntranceTransition() {
         // wait till views get their initial position before start transition
         final View view = getView();
+        if (view == null) {
+            // fragment view destroyed, transition not needed
+            return;
+        }
         view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
             public boolean onPreDraw() {
