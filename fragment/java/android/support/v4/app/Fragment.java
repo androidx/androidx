abdae2e27083efa81020e1619250fb2864f3bbc4
Jake Wharton <jakew@google.com>
2017-10-02 15:25:15 -0400

    Annotate Fragment.onSaveInstanceState Bundle as non-null.
    Test: none
    Change-Id: I88d43430dd59783c534b181cb450afdcbaedff3a

--- fragment/java/android/support/v4/app/Fragment.java
+++ fragment/java/android/support/v4/app/Fragment.java
@@ -1469,7 +1469,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
      *
      * @param outState Bundle in which to place your saved state.
      */
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
     }
 
     /**
