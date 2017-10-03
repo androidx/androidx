e3f5cedb11c0acd754496274e4185839b2d558aa
Aurimas Liutikas <aurimas@google.com>
2017-10-02 17:45:56 -0700

    Megamove of source directories.
    Move source code to more standard src/main/java style directories.
    
    Bug: 67054326
    Test: ./gradlew assembleDebug
    Change-Id: I7db70a3815246ea5a5f0864bbf36a01a956138a3

--- recommendation/Android.mk
+++ recommendation/Android.mk
@@ -25,7 +25,7 @@ include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := android-support-recommendation
 LOCAL_SDK_VERSION := current
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-v4 \
