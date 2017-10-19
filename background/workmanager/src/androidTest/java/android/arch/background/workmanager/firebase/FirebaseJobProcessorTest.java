729ad0918afe749314d1cd8de8c5adad44b1e541
Xyan Bhatnagar <xbhatnag@google.com>
2017-10-19 15:43:41 -0700

    Added FirebaseJobProcessor and implemented FirebaseJobService. These are very similar to the current implementation of SystemJobProcessor and SystemJobService.
    Test: Added FirebaseJobProcessorTest (similar to SystemJobProcessorTest)
    Change-Id: I559be48873645e7d7994564e1c0c2f9d78328f7d

---
+++ background/workmanager/src/androidTest/java/android/arch/background/workmanager/firebase/FirebaseJobProcessorTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.arch.background.workmanager.firebase;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import android.arch.background.workmanager.ExecutionListener;
+import android.arch.background.workmanager.Scheduler;
+import android.arch.background.workmanager.Work;
+import android.arch.background.workmanager.WorkDatabase;
+import android.arch.background.workmanager.WorkSpecs;
+import android.arch.background.workmanager.WorkerWrapper;
+import android.arch.background.workmanager.model.WorkSpec;
+import android.arch.background.workmanager.worker.TestWorker;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class FirebaseJobProcessorTest {
+
+    private WorkDatabase mWorkDatabase;
+    private ExecutionListener mMockListener;
+    private Scheduler mMockScheduler;
+    private FirebaseJobProcessor mFirebaseJobProcessor;
+
+    @Before
+    public void setUp() {
+        Context appContext = InstrumentationRegistry.getTargetContext().getApplicationContext();
+        mWorkDatabase = WorkDatabase.create(appContext, true);
+        mMockListener = mock(ExecutionListener.class);
+        mMockScheduler = mock(Scheduler.class);
+        mFirebaseJobProcessor =
+                new FirebaseJobProcessor(appContext, mWorkDatabase, mMockScheduler, mMockListener);
+    }
+
+    @After
+    public void tearDown() {
+        mWorkDatabase.close();
+    }
+
+    @Test
+    @SmallTest
+    public void testSimpleWorker() throws InterruptedException {
+        WorkSpec workSpec = WorkSpecs.getWorkSpec(TestWorker.class);
+        mWorkDatabase.workSpecDao().insertWorkSpec(workSpec);
+        mFirebaseJobProcessor.process(workSpec.getId(), 0L);
+        Thread.sleep(1000L);
+        verify(mMockListener).onExecuted(workSpec.getId(), WorkerWrapper.RESULT_SUCCEEDED);
+        assertThat(mWorkDatabase.workSpecDao().getWorkSpecStatus(workSpec.getId()),
+                is(Work.STATUS_SUCCEEDED));
+        verify(mMockScheduler).schedule();
+    }
+}
+
--- background/workmanager/src/main/java/android/arch/background/workmanager/firebase/FirebaseJobService.java
+++ background/workmanager/src/main/java/android/arch/background/workmanager/firebase/FirebaseJobService.java
@@ -16,22 +16,71 @@
 
 package android.arch.background.workmanager.firebase;
 
+import android.arch.background.workmanager.ExecutionListener;
+import android.arch.background.workmanager.WorkDatabase;
+import android.arch.background.workmanager.WorkManager;
+import android.arch.background.workmanager.WorkerWrapper;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Service invoked by {@link com.firebase.jobdispatcher.FirebaseJobDispatcher} to run work tasks.
  */
-// TODO(xbhatnag): Complete implementation
-public class FirebaseJobService extends JobService {
+public class FirebaseJobService extends JobService implements ExecutionListener {
+    private static final String TAG = "FirebaseJobService";
+    private FirebaseJobProcessor mFirebaseJobProcessor;
+    private Map<String, JobParameters> mJobParameters = new HashMap<>();
 
     @Override
-    public boolean onStartJob(JobParameters job) {
-        return false;
+    public void onCreate() {
+        super.onCreate();
+        Context context = getApplicationContext();
+        WorkManager workManager = WorkManager.getInstance(context);
+        WorkDatabase database = workManager.getWorkDatabase();
+        mFirebaseJobProcessor =
+                new FirebaseJobProcessor(context, database, workManager.getScheduler(), this);
     }
 
     @Override
-    public boolean onStopJob(JobParameters job) {
-        return false;
+    public boolean onStartJob(JobParameters params) {
+        String workSpecId = params.getTag();
+        if (TextUtils.isEmpty(workSpecId)) {
+            Log.e(TAG, "WorkSpec id not found!");
+            return false;
+        }
+        Log.d(TAG, workSpecId + " started on FirebaseJobDispatcher");
+        mJobParameters.put(workSpecId, params);
+
+        // Delay has already occurred via FirebaseJobDispatcher.
+        mFirebaseJobProcessor.process(workSpecId, 0L);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        String workSpecId = params.getTag();
+        if (TextUtils.isEmpty(workSpecId)) {
+            Log.e(TAG, "WorkSpec id not found!");
+            return false;
+        }
+        boolean cancelled = mFirebaseJobProcessor.cancel(workSpecId, true);
+        Log.d(TAG, workSpecId + "; cancel = " + cancelled);
+        return cancelled;
+    }
+
+    @Override
+    public void onExecuted(String workSpecId, @WorkerWrapper.ExecutionResult int result) {
+        Log.d(TAG, workSpecId + " executed on FirebaseJobDispatcher");
+        JobParameters parameters = mJobParameters.get(workSpecId);
+        boolean needsReschedule = (result == WorkerWrapper.RESULT_INTERRUPTED
+                || result == WorkerWrapper.RESULT_RESCHEDULED);
+        jobFinished(parameters, needsReschedule);
     }
 }
