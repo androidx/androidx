9a1448479860981e486518e3b6e6abc1cbdd39f1
Yigit Boyar <yboyar@google.com>
2017-11-10 10:55:56 -0800

    Use latest available source in processors.
    This will avoid an unnecessary warning due to version mismatch between our min
    version and the developer's javac version.
    
    Bug: 69164173
    Test: n/a
    Change-Id: I1c6dc41833eaac02b68009679025bca34ffc7d67

--- lifecycle/compiler/src/main/kotlin/android/arch/lifecycle/LifecycleProcessor.kt
+++ lifecycle/compiler/src/main/kotlin/android/arch/lifecycle/LifecycleProcessor.kt
@@ -24,12 +24,15 @@ import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 
 @SupportedAnnotationTypes("android.arch.lifecycle.OnLifecycleEvent")
-@SupportedSourceVersion(SourceVersion.RELEASE_7)
 class LifecycleProcessor : AbstractProcessor() {
     override fun process(annotations: MutableSet<out TypeElement>,
                          roundEnv: RoundEnvironment): Boolean {
         val input = collectAndVerifyInput(processingEnv, roundEnv)
         writeModels(transformToOutput(processingEnv, input), processingEnv)
         return true
+    }
+
+    override fun getSupportedSourceVersion(): SourceVersion {
+        return SourceVersion.latest()
     }
 }
