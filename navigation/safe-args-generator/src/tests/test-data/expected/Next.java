16e05dcb2c2b47193b55239be6ae498f7e6e082a
Sergey Vasilinets <sergeyv@google.com>
2017-11-30 15:03:51 -0800

    Naive writer for NavDirections
    Test: NavWriter
    Change-Id: I5476f8abb976e6b3de378e87b1a4ea4b92166505

---
+++ navigation/safe-args-generator/src/tests/test-data/expected/Next.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package a.b;
+
+import android.arch.navigation.NavDirections;
+import android.arch.navigation.NavOptions;
+import android.os.Bundle;
+import java.lang.String;
+
+public static class Next implements NavDirections {
+    private final String main;
+
+    private String optional;
+
+    public Next(String main) {
+        this.main = main;
+    }
+
+    public Next setOptional(String optional) {
+        this.optional = optional;
+        return this;
+    }
+
+    public Bundle getArguments() {
+        Bundle __outBundle = new Bundle();
+        __outBundle.putString("main", main);
+        __outBundle.putString("optional", optional);
+        return __outBundle;
+    }
+
+    public int getDestinationId() {
+        return a.b.R.id.destA;
+    }
+
+    public NavOptions getOptions() {
+        return null;
+    }
+}
