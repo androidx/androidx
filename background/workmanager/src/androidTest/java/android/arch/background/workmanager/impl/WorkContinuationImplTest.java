87d37e5587b277ba523d69df71e17a4039274cda
Rahul Ravikumar <rahulrav@google.com>
2018-01-04 18:18:29 -0800

    Make the enqueue() of a continuation transactional. Test: Updated unit tests
    Change-Id: Iecd60d247b008856a59fb8f61c24d0e9f86e54ae

--- background/workmanager/src/androidTest/java/android/arch/background/workmanager/impl/WorkContinuationImplTest.java
+++ background/workmanager/src/androidTest/java/android/arch/background/workmanager/impl/WorkContinuationImplTest.java
@@ -16,58 +16,104 @@
 
 package android.arch.background.workmanager.impl;
 
-
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
+import android.arch.background.workmanager.TestLifecycleOwner;
 import android.arch.background.workmanager.Work;
-import android.arch.background.workmanager.impl.utils.EnqueueRunnable;
-import android.arch.background.workmanager.impl.utils.taskexecutor.TaskExecutor;
+import android.arch.background.workmanager.executors.SynchronousExecutorService;
+import android.arch.background.workmanager.impl.utils.taskexecutor.InstantTaskExecutorRule;
 import android.arch.background.workmanager.worker.TestWorker;
+import android.arch.core.executor.ArchTaskExecutor;
+import android.arch.lifecycle.Lifecycle;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class WorkContinuationImplTest {
 
+    private WorkDatabase mDatabase;
     private WorkManagerImpl mWorkManagerImpl;
-    private TaskExecutor mTaskExecutor;
+
+    @Rule
+    public InstantTaskExecutorRule mRule = new InstantTaskExecutorRule();
 
     @Before
-    public void setup() {
-        mWorkManagerImpl = mock(WorkManagerImpl.class);
-        mTaskExecutor = mock(TaskExecutor.class);
-        when(mWorkManagerImpl.getTaskExecutor()).thenReturn(mTaskExecutor);
+    public void setUp() {
+        ArchTaskExecutor.getInstance().setDelegate(new android.arch.core.executor.TaskExecutor() {
+            @Override
+            public void executeOnDiskIO(@NonNull Runnable runnable) {
+                runnable.run();
+            }
+
+            @Override
+            public void postToMainThread(@NonNull Runnable runnable) {
+                runnable.run();
+            }
+
+            @Override
+            public boolean isMainThread() {
+                return true;
+            }
+        });
+
+        TestLifecycleOwner lifecycleOwner = new TestLifecycleOwner();
+        lifecycleOwner.mLifecycleRegistry.markState(Lifecycle.State.CREATED);
+
+        Context context = InstrumentationRegistry.getTargetContext();
+        WorkManagerConfiguration configuration = new WorkManagerConfiguration(
+                context,
+                true,
+                new SynchronousExecutorService(),
+                new SynchronousExecutorService(),
+                lifecycleOwner);
+        mWorkManagerImpl = new WorkManagerImpl(context, configuration);
+        mDatabase = mWorkManagerImpl.getWorkDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        List<String> ids = mDatabase.workSpecDao().getAllWorkSpecIds();
+        for (String id : ids) {
+            mWorkManagerImpl.cancelWorkForId(id);
+        }
+        mDatabase.close();
+        ArchTaskExecutor.getInstance().setDelegate(null);
     }
 
     @Test
-    public void testLazyContinuation_noParent() {
+    public void testContinuation_noParent() {
         Work testWork = createTestWorker();
-        WorkContinuationImpl continuation = new WorkContinuationImpl(mWorkManagerImpl,
-                testWork);
-        assertNull(continuation.mParent);
+        WorkContinuationImpl continuation =
+                new WorkContinuationImpl(mWorkManagerImpl, testWork);
 
-        assertEquals(continuation.mIds[0], testWork.getId());
-        assertEquals(continuation.mIds.length, 1);
-        assertEquals(continuation.mAllIds.size(), 1);
+        assertThat(continuation.getParent(), is(nullValue()));
+        assertThat(continuation.getIds().length, is(1));
+        assertThat(continuation.getIds()[0], is(testWork.getId()));
+        assertThat(continuation.getAllIds().size(), is(1));
     }
 
     @Test
-    public void testLazyContinuation_singleChain() {
+    public void testContinuation_singleChain() {
         Work testWork = createTestWorker();
         Work dependentWork = createTestWorker();
         WorkContinuationImpl continuation =
@@ -75,59 +121,59 @@ public class WorkContinuationImplTest {
         WorkContinuationImpl dependent = (WorkContinuationImpl) (continuation.then(
                 dependentWork));
 
-        assertNotNull(dependent.mParent);
-        assertEquals(dependent.mParent, continuation);
 
-        assertEquals(dependent.mIds.length, 1);
-        assertEquals(dependent.mAllIds.size(), 2);
+        assertThat(dependent.getParent(), is(notNullValue()));
+        assertThat(dependent.getParent(), is(continuation));
+        assertThat(dependent.getIds().length, is(1));
+        assertThat(dependent.getIds()[0], is(dependentWork.getId()));
+        assertThat(dependent.getAllIds().size(), is(2));
+        assertThat(
+                dependent.getAllIds(),
+                containsInAnyOrder(dependentWork.getId(), testWork.getId()));
     }
 
     @Test
-    public void testLazyContinuation_enqueue() {
+    public void testContinuation_enqueue() {
         Work testWork = createTestWorker();
         WorkContinuationImpl continuation = new WorkContinuationImpl(mWorkManagerImpl,
                 testWork);
-        assertEquals(continuation.mEnqueued, false);
+        assertThat(continuation.isEnqueued(), is(false));
         continuation.enqueue();
-
         verifyEnqueued(continuation);
-        verify(mTaskExecutor, times(1)).executeOnBackgroundThread(any(EnqueueRunnable.class));
     }
 
     @Test
-    public void testLazyContinuation_chainEnqueue() {
+    public void testContinuation_chainEnqueue() {
         Work testWork = createTestWorker();
         WorkContinuationImpl continuation =
                 new WorkContinuationImpl(mWorkManagerImpl, testWork);
         WorkContinuationImpl chain = (WorkContinuationImpl) (
                 continuation.then(createTestWorker()).then(createTestWorker(), createTestWorker()));
-
         chain.enqueue();
         verifyEnqueued(continuation);
-        verify(mTaskExecutor, times(3)).executeOnBackgroundThread(any(EnqueueRunnable.class));
     }
 
     @Test
-    public void testLazyContinuation_chainEnqueueNoOpOnRetry() {
+    public void testContinuation_chainEnqueueNoOpOnRetry() {
         Work testWork = createTestWorker();
         WorkContinuationImpl continuation =
                 new WorkContinuationImpl(mWorkManagerImpl, testWork);
         WorkContinuationImpl chain = (WorkContinuationImpl) (
                 continuation.then(createTestWorker()).then(createTestWorker(), createTestWorker()));
-
         chain.enqueue();
         verifyEnqueued(continuation);
-        verify(mTaskExecutor, times(3)).executeOnBackgroundThread(any(EnqueueRunnable.class));
-        chain.enqueue();
-        verifyNoMoreInteractions(mTaskExecutor);
+        WorkContinuationImpl spy = spy(chain);
+        spy.enqueue();
+        // Verify no more calls to markEnqueued().
+        verify(spy, times(0)).markEnqueued();
     }
 
     private void verifyEnqueued(WorkContinuationImpl continuation) {
-        assertEquals(continuation.mEnqueued, true);
-        WorkContinuationImpl parent = continuation.mParent;
+        assertThat(continuation.isEnqueued(), is(true));
+        WorkContinuationImpl parent = continuation.getParent();
         while (parent != null) {
-            assertEquals(parent.mEnqueued, true);
-            parent = parent.mParent;
+            assertThat(parent.isEnqueued(), is(true));
+            parent = parent.getParent();
         }
     }
 
