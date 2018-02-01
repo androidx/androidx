93ac8cdb99bdbea5b1b2c8348173303f1949793d
Mady Mellor <madym@google.com>
2018-02-01 11:06:42 -0800

    Fix issue with replacing small row with small grid
    In mode small we use either RowView or GridRowView depending
    on the type of slice, we we’re checking for a difference
    when mode small was being reused so we would use the wrong
    view potentially.
    
    Bug: 72669326
    Test: ./gradlew :slices-view:connectedCheck, look at
          screenshots of slices
    
    Change-Id: I95d7ac16de9fa7dc373f420744bd8cfd42e82ada

--- slices/view/src/main/java/androidx/app/slice/widget/SliceView.java
+++ slices/view/src/main/java/androidx/app/slice/widget/SliceView.java
@@ -227,6 +227,11 @@ public class SliceView extends ViewGroup implements Observer<Slice> {
      * content see {@link SliceLiveData}.
      */
     public void setSlice(@Nullable Slice slice) {
+        if (mCurrentSlice != null && slice != null
+                && !mCurrentSlice.getUri().equals(slice.getUri())) {
+            // New slice, reset view
+            mCurrentView.resetView();
+        }
         mCurrentSlice = slice;
         reinflate();
     }
@@ -322,7 +327,9 @@ public class SliceView extends ViewGroup implements Observer<Slice> {
         }
         // TODO: Smarter mapping here from one state to the next.
         int mode = getMode();
-        if (mMode == mCurrentView.getMode()) {
+        boolean isSmallGridShowing = mCurrentView instanceof GridRowView;
+        boolean isGridSlice = SliceQuery.hasHints(mCurrentSlice, HINT_HORIZONTAL);
+        if (mMode == mCurrentView.getMode() && isGridSlice == isSmallGridShowing) {
             mCurrentView.setSlice(mCurrentSlice);
         } else {
             removeAllViews();
