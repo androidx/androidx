011d9c66f0e350203ba1a4b06a6fd64b1c58971b
David Ogutu <ogutu@google.com>
2018-02-28 15:42:35 -0500

    Make main standalone function discoverable by jvm
    This is for the google3 jetify tool. Changed gradle classname to unbreak
    `gradle run`
    
    Merged-In: Ia6c3fe9ffd5c949376429e4970a8867a65ab85d6
    
    Test: $(./gradle run) and $(blaze run)
    Bug: 73231426
    Change-Id: Ia6c3fe9ffd5c949376429e4970a8867a65ab85d6
    (cherry picked from commit 6540e862ec13b6e1581eca2d77b20021f08b49fb)

--- jetifier/jetifier/standalone/src/main/kotlin/android/support/tools/jetifier/standalone/Main.kt
+++ jetifier/jetifier/standalone/src/main/kotlin/android/support/tools/jetifier/standalone/Main.kt
@@ -70,6 +70,10 @@ class Main {
             OPTIONS.addOption(op)
             return op
         }
+
+        @JvmStatic fun main(args: Array<String>) {
+            Main().run(args)
+        }
     }
 
     fun run(args: Array<String>) {
@@ -156,6 +160,3 @@ class Main {
     }
 }
 
-fun main(args: Array<String>) {
-    Main().run(args)
-}
