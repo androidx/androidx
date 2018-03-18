5c9d0cba0e999bd61a8a940e9523b3270053416e
Jake Wharton <jakew@google.com>
2018-03-17 22:02:38 -0400

    Remove RecyclerView ItemTouchUIUtil abstractions.
    Bug: 75289452
    Test: ./gradlew :recyclerview:build :recyclerview:cC
    Change-Id: Id8cfb400e643f5f8a7a71fa49780b0b236778b7e

--- v7/recyclerview/src/main/java/androidx/recyclerview/widget/ItemTouchUIUtilImpl.java
+++ v7/recyclerview/src/main/java/androidx/recyclerview/widget/ItemTouchUIUtilImpl.java
@@ -17,6 +17,7 @@
 package androidx.recyclerview.widget;
 
 import android.graphics.Canvas;
+import android.os.Build;
 import android.view.View;
 
 import androidx.core.view.ViewCompat;
@@ -26,11 +27,13 @@ import androidx.recyclerview.R;
  * Package private class to keep implementations. Putting them inside ItemTouchUIUtil makes them
  * public API, which is not desired in this case.
  */
-class ItemTouchUIUtilImpl {
-    static class Api21Impl extends BaseImpl {
-        @Override
-        public void onDraw(Canvas c, RecyclerView recyclerView, View view,
-                float dX, float dY, int actionState, boolean isCurrentlyActive) {
+class ItemTouchUIUtilImpl implements ItemTouchUIUtil {
+    static final ItemTouchUIUtil INSTANCE =  new ItemTouchUIUtilImpl();
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView recyclerView, View view, float dX, float dY,
+            int actionState, boolean isCurrentlyActive) {
+        if (Build.VERSION.SDK_INT >= 21) {
             if (isCurrentlyActive) {
                 Object originalElevation = view.getTag(R.id.item_touch_helper_previous_elevation);
                 if (originalElevation == null) {
@@ -40,60 +43,48 @@ class ItemTouchUIUtilImpl {
                     view.setTag(R.id.item_touch_helper_previous_elevation, originalElevation);
                 }
             }
-            super.onDraw(c, recyclerView, view, dX, dY, actionState, isCurrentlyActive);
         }
 
-        private float findMaxElevation(RecyclerView recyclerView, View itemView) {
-            final int childCount = recyclerView.getChildCount();
-            float max = 0;
-            for (int i = 0; i < childCount; i++) {
-                final View child = recyclerView.getChildAt(i);
-                if (child == itemView) {
-                    continue;
-                }
-                final float elevation = ViewCompat.getElevation(child);
-                if (elevation > max) {
-                    max = elevation;
-                }
+        view.setTranslationX(dX);
+        view.setTranslationY(dY);
+    }
+
+    private static float findMaxElevation(RecyclerView recyclerView, View itemView) {
+        final int childCount = recyclerView.getChildCount();
+        float max = 0;
+        for (int i = 0; i < childCount; i++) {
+            final View child = recyclerView.getChildAt(i);
+            if (child == itemView) {
+                continue;
             }
-            return max;
+            final float elevation = ViewCompat.getElevation(child);
+            if (elevation > max) {
+                max = elevation;
+            }
         }
+        return max;
+    }
 
-        @Override
-        public void clearView(View view) {
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView recyclerView, View view, float dX, float dY,
+            int actionState, boolean isCurrentlyActive) {
+    }
+
+    @Override
+    public void clearView(View view) {
+        if (Build.VERSION.SDK_INT >= 21) {
             final Object tag = view.getTag(R.id.item_touch_helper_previous_elevation);
             if (tag != null && tag instanceof Float) {
                 ViewCompat.setElevation(view, (Float) tag);
             }
             view.setTag(R.id.item_touch_helper_previous_elevation, null);
-            super.clearView(view);
         }
+
+        view.setTranslationX(0f);
+        view.setTranslationY(0f);
     }
 
-    static class BaseImpl implements ItemTouchUIUtil {
-
-        @Override
-        public void clearView(View view) {
-            view.setTranslationX(0f);
-            view.setTranslationY(0f);
-        }
-
-        @Override
-        public void onSelected(View view) {
-
-        }
-
-        @Override
-        public void onDraw(Canvas c, RecyclerView recyclerView, View view,
-                float dX, float dY, int actionState, boolean isCurrentlyActive) {
-            view.setTranslationX(dX);
-            view.setTranslationY(dY);
-        }
-
-        @Override
-        public void onDrawOver(Canvas c, RecyclerView recyclerView,
-                View view, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-
-        }
+    @Override
+    public void onSelected(View view) {
     }
 }
