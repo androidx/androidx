c416cc7288a700f295423a710a9fe8c7fc7cdfa1
Jan Althaus <jalt@google.com>
2018-04-05 18:14:08 +0200

    Adding RemoteActionCompat
    Differences to RemoteAction:
    - Added createFromRemoteAction and toRemoteAction.
    - Didn't port "dump" because a matching implementation would require
      access to the Intent in PendingIntent.
    - Not Parcelable, but serialization via Bundle
    - No support for clone, but copy c-tor
    
    Bug: 72307474
    Test: Added
    Change-Id: Iadd01406f3e3533458d3639e9b3988698d901ded

---
+++ compat/src/androidTest/java/androidx/core/app/RemoteActionCompatTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.BaseInstrumentationTestCase;
+
+import androidx.core.graphics.drawable.IconCompat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class RemoteActionCompatTest extends BaseInstrumentationTestCase<TestSupportActivity> {
+
+    public RemoteActionCompatTest() {
+        super(TestSupportActivity.class);
+    }
+
+    @Test
+    public void testRemoteAction_bundle() throws Throwable {
+        IconCompat icon = IconCompat.createWithContentUri("content://test");
+        String title = "title";
+        String description = "description";
+        PendingIntent action = PendingIntent.getBroadcast(InstrumentationRegistry.getContext(), 0,
+                new Intent("TESTACTION"), 0);
+        RemoteActionCompat reference = new RemoteActionCompat(icon, title, description, action);
+        reference.setEnabled(false);
+        reference.setShouldShowIcon(false);
+
+        RemoteActionCompat result = RemoteActionCompat.createFromBundle(reference.toBundle());
+
+        assertEquals(icon.getUri(), result.getIcon().getUri());
+        assertEquals(title, result.getTitle());
+        assertEquals(description, result.getContentDescription());
+        assertEquals(action.getCreatorPackage(), result.getActionIntent().getCreatorPackage());
+        assertFalse(result.isEnabled());
+        assertFalse(result.shouldShowIcon());
+    }
+
+    @Test
+    public void testRemoteAction_shallowCopy() throws Throwable {
+        IconCompat icon = IconCompat.createWithContentUri("content://test");
+        String title = "title";
+        String description = "description";
+        PendingIntent action = PendingIntent.getBroadcast(InstrumentationRegistry.getContext(), 0,
+                new Intent("TESTACTION"), 0);
+        RemoteActionCompat reference = new RemoteActionCompat(icon, title, description, action);
+        reference.setEnabled(false);
+        reference.setShouldShowIcon(false);
+
+        RemoteActionCompat result = new RemoteActionCompat(reference);
+
+        assertEquals(icon.getUri(), result.getIcon().getUri());
+        assertEquals(title, result.getTitle());
+        assertEquals(description, result.getContentDescription());
+        assertEquals(action.getCreatorPackage(), result.getActionIntent().getCreatorPackage());
+        assertFalse(result.isEnabled());
+        assertFalse(result.shouldShowIcon());
+    }
+}
---
+++ compat/src/main/java/androidx/core/app/RemoteActionCompat.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.app;
+
+import android.app.PendingIntent;
+import android.app.RemoteAction;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.graphics.drawable.IconCompat;
+import androidx.core.os.BuildCompat;
+import androidx.core.util.Preconditions;
+
+/**
+ * Helper for accessing features in {@link android.app.RemoteAction}.
+ */
+public final class RemoteActionCompat {
+
+    private static final String EXTRA_ICON = "icon";
+    private static final String EXTRA_TITLE = "title";
+    private static final String EXTRA_CONTENT_DESCRIPTION = "desc";
+    private static final String EXTRA_ACTION_INTENT = "action";
+    private static final String EXTRA_ENABLED = "enabled";
+    private static final String EXTRA_SHOULD_SHOW_ICON = "showicon";
+
+    private final IconCompat mIcon;
+    private final CharSequence mTitle;
+    private final CharSequence mContentDescription;
+    private final PendingIntent mActionIntent;
+    private boolean mEnabled;
+    private boolean mShouldShowIcon;
+
+    public RemoteActionCompat(@NonNull IconCompat icon, @NonNull CharSequence title,
+            @NonNull CharSequence contentDescription, @NonNull PendingIntent intent) {
+        if (icon == null || title == null || contentDescription == null || intent == null) {
+            throw new IllegalArgumentException(
+                    "Expected icon, title, content description and action callback");
+        }
+        mIcon = icon;
+        mTitle = title;
+        mContentDescription = contentDescription;
+        mActionIntent = intent;
+        mEnabled = true;
+        mShouldShowIcon = true;
+    }
+
+    /**
+     * Constructs a Foo builder using data from {@code other}.
+     */
+    public RemoteActionCompat(@NonNull RemoteActionCompat other) {
+        Preconditions.checkNotNull(other);
+        mIcon = other.mIcon;
+        mTitle = other.mTitle;
+        mContentDescription = other.mContentDescription;
+        mActionIntent = other.mActionIntent;
+        mEnabled = other.mEnabled;
+        mShouldShowIcon = other.mShouldShowIcon;
+    }
+
+    /**
+     * Creates an RemoteActionCompat from a RemoteAction.
+     */
+    @RequiresApi(26)
+    @NonNull
+    public static RemoteActionCompat createFromRemoteAction(@NonNull RemoteAction remoteAction) {
+        Preconditions.checkNotNull(remoteAction);
+        RemoteActionCompat action = new RemoteActionCompat(
+                IconCompat.createFromIcon(remoteAction.getIcon()), remoteAction.getTitle(),
+                remoteAction.getContentDescription(), remoteAction.getActionIntent());
+        action.setEnabled(remoteAction.isEnabled());
+        if (BuildCompat.isAtLeastP()) {
+            action.setShouldShowIcon(remoteAction.shouldShowIcon());
+        }
+        return action;
+    }
+
+    /**
+     * Sets whether this action is enabled.
+     */
+    public void setEnabled(boolean enabled) {
+        mEnabled = enabled;
+    }
+
+    /**
+     * Return whether this action is enabled.
+     */
+    public boolean isEnabled() {
+        return mEnabled;
+    }
+
+    /**
+     * Sets whether the icon should be shown.
+     */
+    public void setShouldShowIcon(boolean shouldShowIcon) {
+        mShouldShowIcon = shouldShowIcon;
+    }
+
+    /**
+     * Return whether the icon should be shown.
+     */
+    public boolean shouldShowIcon() {
+        return mShouldShowIcon;
+    }
+
+    /**
+     * Return an icon representing the action.
+     */
+    public @NonNull IconCompat getIcon() {
+        return mIcon;
+    }
+
+    /**
+     * Return an title representing the action.
+     */
+    public @NonNull CharSequence getTitle() {
+        return mTitle;
+    }
+
+    /**
+     * Return a content description representing the action.
+     */
+    public @NonNull CharSequence getContentDescription() {
+        return mContentDescription;
+    }
+
+    /**
+     * Return the action intent.
+     */
+    public @NonNull PendingIntent getActionIntent() {
+        return mActionIntent;
+    }
+
+    /**
+     * Convert this compat object to {@link RemoteAction} object.
+     *
+     * @return {@link RemoteAction} object
+     */
+    @RequiresApi(26)
+    @NonNull
+    public RemoteAction toRemoteAction() {
+        RemoteAction action = new RemoteAction(mIcon.toIcon(), mTitle, mContentDescription,
+                mActionIntent);
+        action.setEnabled(isEnabled());
+        if (BuildCompat.isAtLeastP()) {
+            action.setShouldShowIcon(shouldShowIcon());
+        }
+        return action;
+    }
+
+    /**
+     * Adds this Icon to a Bundle that can be read back with the same parameters
+     * to {@link #createFromBundle(Bundle)}.
+     */
+    public Bundle toBundle() {
+        Bundle bundle = new Bundle();
+        bundle.putBundle(EXTRA_ICON, mIcon.toBundle());
+        bundle.putCharSequence(EXTRA_TITLE, mTitle);
+        bundle.putCharSequence(EXTRA_CONTENT_DESCRIPTION, mContentDescription);
+        bundle.putParcelable(EXTRA_ACTION_INTENT, mActionIntent);
+        bundle.putBoolean(EXTRA_ENABLED, mEnabled);
+        bundle.putBoolean(EXTRA_SHOULD_SHOW_ICON, mShouldShowIcon);
+        return bundle;
+    }
+
+    /**
+     * Extracts an icon from a bundle that was added using {@link #toBundle()}.
+     */
+    @Nullable
+    public static RemoteActionCompat createFromBundle(@NonNull Bundle bundle) {
+        RemoteActionCompat action = new RemoteActionCompat(
+                IconCompat.createFromBundle(bundle.getBundle(EXTRA_ICON)),
+                bundle.getCharSequence(EXTRA_TITLE),
+                bundle.getCharSequence(EXTRA_CONTENT_DESCRIPTION),
+                bundle.<PendingIntent>getParcelable(EXTRA_ACTION_INTENT));
+        action.setEnabled(bundle.getBoolean(EXTRA_ENABLED));
+        action.setShouldShowIcon(bundle.getBoolean(EXTRA_SHOULD_SHOW_ICON));
+        return action;
+    }
+}
