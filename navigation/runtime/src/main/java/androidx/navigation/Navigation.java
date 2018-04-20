3e4c2e8ed42ed4f3d4e0772951b329c0c07413dc
Ian Lake <ilake@google.com>
2018-04-20 09:38:32 -0700

    API Council feedback: NonNull findNavController
    Integrate API Council feedback that
    getNavController() methods should be removed as
    it is not an O(1) operation and that
    findNavController() should return non-null or throw
    (essentially taking the behavior of the old
    get() methods).
    
    Removed navController Kotlin property extensions,
    relying instead on the more explicit findNavController()
    extension methods.
    
    Test: Updated tests pass
    BUG: 78344602
    Change-Id: I5b32fe0f6bb2291f3e3542b97cba1a133bb85868

--- navigation/runtime/src/main/java/androidx/navigation/Navigation.java
+++ navigation/runtime/src/main/java/androidx/navigation/Navigation.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.ViewParent;
 
@@ -48,28 +49,13 @@ public class Navigation {
      * @param activity The Activity hosting the view
      * @param viewId The id of the view to search from
      * @return the {@link NavController} associated with the view referenced by id
-     */
-    @Nullable
-    public static NavController findNavController(@NonNull Activity activity, @IdRes int viewId) {
-        return Navigation.findNavController(activity.findViewById(viewId));
-    }
-
-    /**
-     * Gets the {@link NavController} given the id of a View and its containing
-     * {@link Activity}. This is a convenience wrapper around {@link #getNavController(View)}.
-     *
-     * <p>This method will locate the {@link NavController} associated with this view.
-     * This is automatically populated for the id of a {@link NavHost} and its children.</p>
-     *
-     * @param activity The Activity hosting the view
-     * @param viewId The id of the view to search from
-     * @return the {@link NavController} associated with the view referenced by id
      * @throws IllegalStateException if the given viewId does not correspond with a
      * {@link NavHost} or is not within a NavHost.
      */
     @NonNull
-    public static NavController getNavController(@NonNull Activity activity, @IdRes int viewId) {
-        NavController navController = findNavController(activity, viewId);
+    public static NavController findNavController(@NonNull Activity activity, @IdRes int viewId) {
+        View view = ActivityCompat.requireViewById(activity, viewId);
+        NavController navController = findViewNavController(view);
         if (navController == null) {
             throw new IllegalStateException("Activity " + activity
                     + " does not have a NavController set on " + viewId);
@@ -87,36 +73,12 @@ public class Navigation {
      *
      * @param view the view to search from
      * @return the locally scoped {@link NavController} to the given view
-     */
-    @Nullable
-    public static NavController findNavController(@NonNull View view) {
-        while (view != null) {
-            NavController controller = getViewNavController(view);
-            if (controller != null) {
-                return controller;
-            }
-            ViewParent parent = view.getParent();
-            view = parent instanceof View ? (View) parent : null;
-        }
-        return null;
-    }
-
-    /**
-     * Gets the {@link NavController} given a local {@link View}.
-     *
-     * <p>This method will locate the {@link NavController} associated with this view.
-     * This is automatically populated for views that are managed by a {@link NavHost}
-     * and is intended for use by various {@link android.view.View.OnClickListener listener}
-     * interfaces.</p>
-     *
-     * @param view the view to search from
-     * @return the locally scoped {@link NavController} to the given view
      * @throws IllegalStateException if the given view does not correspond with a
      * {@link NavHost} or is not within a NavHost.
      */
     @NonNull
-    public static NavController getNavController(@NonNull View view) {
-        NavController navController = findNavController(view);
+    public static NavController findNavController(@NonNull View view) {
+        NavController navController = findViewNavController(view);
         if (navController == null) {
             throw new IllegalStateException("View " + view + " does not have a NavController set");
         }
@@ -153,28 +115,47 @@ public class Navigation {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getNavController(view).navigate(resId, args);
+                findNavController(view).navigate(resId, args);
             }
         };
     }
 
     /**
      * Associates a NavController with the given View, allowing developers to use
-     * {@link #getNavController(View)} and {@link #getNavController(Activity, int)} with that
+     * {@link #findNavController(View)} and {@link #findNavController(Activity, int)} with that
      * View or any of its children to retrieve the NavController.
      * <p>
      * This is generally called for you by the hosting {@link NavHost}.
      * @param view View that should be associated with the given NavController
      * @param controller The controller you wish to later retrieve via
-     *                   {@link #getNavController(View)}
+     *                   {@link #findNavController(View)}
      */
     public static void setViewNavController(@NonNull View view,
             @Nullable NavController controller) {
         view.setTag(R.id.nav_controller_view_tag, controller);
     }
 
+    /**
+     * Recurse up the view hierarchy, looking for the NavController
+     * @param view the view to search from
+     * @return the locally scoped {@link NavController} to the given view, if found
+     */
+    @Nullable
+    private static NavController findViewNavController(@NonNull View view) {
+        while (view != null) {
+            NavController controller = getViewNavController(view);
+            if (controller != null) {
+                return controller;
+            }
+            ViewParent parent = view.getParent();
+            view = parent instanceof View ? (View) parent : null;
+        }
+        return null;
+    }
+
     @SuppressWarnings("unchecked")
-    static NavController getViewNavController(View view) {
+    @Nullable
+    private static NavController getViewNavController(@NonNull View view) {
         Object tag = view.getTag(R.id.nav_controller_view_tag);
         NavController controller = null;
         if (tag instanceof WeakReference) {
