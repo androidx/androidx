75cfe07dc33d7f621da177d52dfc10ac22666a9c
Dongwon Kang <dwkang@google.com>
2018-05-03 16:43:41 -0700

    Properly implement setMediaPlayer2EventCallback
    Test: ./gradlew media:connectedCheck
    Bug: 63934228
    Change-Id: I054728f64c3cec865311817136387dfb9e070c50

--- media/src/main/java/androidx/media/MediaPlayer2Impl.java
+++ media/src/main/java/androidx/media/MediaPlayer2Impl.java
@@ -146,12 +146,10 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
     private final Object mLock = new Object();
     //--- guarded by |mLock| start
     private AudioAttributesCompat mAudioAttributes;
-    private ArrayList<Pair<Executor, MediaPlayer2EventCallback>> mMp2EventCallbackRecords =
-            new ArrayList<>();
+    private Pair<Executor, MediaPlayer2EventCallback> mMp2EventCallbackRecord;
     private ArrayMap<PlayerEventCallback, Executor> mPlayerEventCallbackMap =
             new ArrayMap<>();
-    private ArrayList<Pair<Executor, DrmEventCallback>> mDrmEventCallbackRecords =
-            new ArrayList<>();
+    private Pair<Executor, DrmEventCallback> mDrmEventCallbackRecord;
     private MediaPlayerInterfaceImpl mMediaPlayerInterfaceImpl;
     //--- guarded by |mLock| end
 
@@ -876,9 +874,9 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
         mPlayer.reset();
         synchronized (mLock) {
             mAudioAttributes = null;
-            mMp2EventCallbackRecords.clear();
+            mMp2EventCallbackRecord = null;
             mPlayerEventCallbackMap.clear();
-            mDrmEventCallbackRecords.clear();
+            mDrmEventCallbackRecord = null;
         }
     }
 
@@ -1156,7 +1154,7 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
                     "Illegal null Executor for the MediaPlayer2EventCallback");
         }
         synchronized (mLock) {
-            mMp2EventCallbackRecords.add(new Pair(executor, eventCallback));
+            mMp2EventCallbackRecord = new Pair(executor, eventCallback);
         }
     }
 
@@ -1166,7 +1164,7 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
     @Override
     public void clearMediaPlayer2EventCallback() {
         synchronized (mLock) {
-            mMp2EventCallbackRecords.clear();
+            mMp2EventCallbackRecord = null;
         }
     }
 
@@ -1210,7 +1208,7 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
                     "Illegal null Executor for the MediaPlayer2EventCallback");
         }
         synchronized (mLock) {
-            mDrmEventCallbackRecords.add(new Pair(executor, eventCallback));
+            mDrmEventCallbackRecord = new Pair(executor, eventCallback);
         }
     }
 
@@ -1220,7 +1218,7 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
     @Override
     public void clearDrmEventCallback() {
         synchronized (mLock) {
-            mDrmEventCallbackRecords.clear();
+            mDrmEventCallbackRecord = null;
         }
     }
 
@@ -1461,11 +1459,11 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
     }
 
     private void notifyMediaPlayer2Event(final Mp2EventNotifier notifier) {
-        List<Pair<Executor, MediaPlayer2EventCallback>> records;
+        final Pair<Executor, MediaPlayer2EventCallback> record;
         synchronized (mLock) {
-            records = new ArrayList<>(mMp2EventCallbackRecords);
+            record = mMp2EventCallbackRecord;
         }
-        for (final Pair<Executor, MediaPlayer2EventCallback> record : records) {
+        if (record != null) {
             record.first.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -1494,11 +1492,11 @@ public final class MediaPlayer2Impl extends MediaPlayer2 {
     }
 
     private void notifyDrmEvent(final DrmEventNotifier notifier) {
-        List<Pair<Executor, DrmEventCallback>> records;
+        final Pair<Executor, DrmEventCallback> record;
         synchronized (mLock) {
-            records = new ArrayList<>(mDrmEventCallbackRecords);
+            record = mDrmEventCallbackRecord;
         }
-        for (final Pair<Executor, DrmEventCallback> record : records) {
+        if (record != null) {
             record.first.execute(new Runnable() {
                 @Override
                 public void run() {
