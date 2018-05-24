fcdab929ee70dbbd9b173e10ef95bc1b0ac4fb63
Tony Mantler <nicoya@google.com>
2018-05-24 15:16:51 -0700

    Fix PinPickerTest flake
    Apparently Espresso doesn't wait for UI actions to complete before returning.
    
    Bug: 80258109
    Test: This is the test
    Change-Id: I918cd0c0111836d9df4dd7e27b8c458017660095

--- leanback/src/androidTest/java/androidx/leanback/widget/picker/PinPickerTest.java
+++ leanback/src/androidTest/java/androidx/leanback/widget/picker/PinPickerTest.java
@@ -29,13 +29,21 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.KeyEvent;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.leanback.test.R;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @RunWith(AndroidJUnit4.class)
 public class PinPickerTest {
@@ -55,18 +63,26 @@ public class PinPickerTest {
 
     @Test
     @LargeTest
-    public void keyInputTest() {
+    public void keyInputTest() throws Exception {
+        final CompletableFuture<String> futurePin = new CompletableFuture<>();
         getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
                 mPinPicker.resetPin();
+                mPinPicker.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        futurePin.complete(mPinPicker.getPin());
+                    }
+                });
             }
         });
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_1));
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_2));
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_3));
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_4));
-        assertThat("keyboard input should set pin", mPinPicker.getPin(), is("1234"));
+
+        assertThat("keyboard input should set pin", futurePin.get(5, TimeUnit.SECONDS), is("1234"));
 
         getInstrumentation().runOnMainSync(new Runnable() {
             @Override
@@ -79,11 +95,18 @@ public class PinPickerTest {
 
     @Test
     @LargeTest
-    public void dpadInputTest() {
+    public void dpadInputTest() throws Exception {
+        final CompletableFuture<String> futurePin = new CompletableFuture<>();
         getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
                 mPinPicker.resetPin();
+                mPinPicker.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        futurePin.complete(mPinPicker.getPin());
+                    }
+                });
             }
         });
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_DPAD_DOWN));
@@ -100,6 +123,46 @@ public class PinPickerTest {
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_DPAD_DOWN));
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_DPAD_DOWN));
         onView(withId(R.id.test_picker)).perform(pressKey(KeyEvent.KEYCODE_DPAD_CENTER));
-        assertThat("dpad input should set pin", mPinPicker.getPin(), is("1234"));
+
+        assertThat("dpad input should set pin", futurePin.get(5, TimeUnit.SECONDS), is("1234"));
+    }
+
+    private static class CompletableFuture<V> implements Future<V> {
+
+        private volatile V mValue;
+        private CountDownLatch mLatch = new CountDownLatch(1);
+
+        public void complete(V value) {
+            mValue = value;
+            mLatch.countDown();
+        }
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isCancelled() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isDone() {
+            return mLatch.getCount() == 0;
+        }
+
+        @Override
+        public V get() throws InterruptedException, ExecutionException {
+            mLatch.await();
+            return mValue;
+        }
+
+        @Override
+        public V get(long timeout, @NonNull TimeUnit unit)
+                throws InterruptedException, ExecutionException, TimeoutException {
+            mLatch.await(timeout, unit);
+            return mValue;
+        }
     }
 }
