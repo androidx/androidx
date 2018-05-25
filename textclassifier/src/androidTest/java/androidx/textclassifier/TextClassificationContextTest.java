ca3bd88d32f4f5a7a921488185d20f1e5ecead86
Tony Mak <tonymak@google.com>
2018-05-25 09:58:38 +0800

    Port various classes for session-aware TC (Part 2/ N)
    1. Port SelectionEvent
    2. Port TextClassificationContext
    3. Port TextClassificationManager
    4. Port TextClassificationSessionId
    
    TODO: Wire up these classes in TextClassifier
    
    Test: ./gradlew textclassifier:connectedCheck --info --daemon
    
    Bug: 72307474
    
    Change-Id: I5ec7ac59627fd3411b6abda23d96145073340af3

---
+++ textclassifier/src/androidTest/java/androidx/textclassifier/TextClassificationContextTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.textclassifier;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.os.Bundle;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Instrumentation unit tests for {@link TextClassificationContext}. */
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class TextClassificationContextTest {
+    private static final String PKG_NAME = "pkg_name";
+    private static final String WIDGET_TYPE = TextClassifier.WIDGET_TYPE_TEXTVIEW;
+    private static final String WIDGET_VERSION = "version";
+
+    @Test
+    public void testToBundle() {
+        TextClassificationContext textClassificationContext =
+                new TextClassificationContext.Builder(PKG_NAME, WIDGET_TYPE)
+                        .setWidgetVersion(WIDGET_VERSION)
+                        .build();
+        Bundle bundle = textClassificationContext.toBundle();
+        TextClassificationContext restored = TextClassificationContext.createFromBundle(bundle);
+        assertThat(restored.getPackageName()).isEqualTo(PKG_NAME);
+        assertThat(restored.getWidgetType()).isEqualTo(WIDGET_TYPE);
+        assertThat(restored.getWidgetVersion()).isEqualTo(WIDGET_VERSION);
+    }
+}
