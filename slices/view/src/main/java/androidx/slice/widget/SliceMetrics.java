92a864504a47ede3d58d719bcf600324d516d6d1
Steven Wu <stwu@google.com>
2018-05-30 15:03:35 -0400

    Isolate framework slice metrics from slice view class.
    Test: ./gradlew slice-view:connectedCheck
    Change-Id: Ifd436fda88586f60364441de772a4e38ca9c20e8
    Fixes: 80426223

---
+++ slices/view/src/main/java/androidx/slice/widget/SliceMetrics.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.slice.widget;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RestrictTo;
+import androidx.core.os.BuildCompat;
+
+/**
+ * @hide
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+class SliceMetrics {
+
+    public static @Nullable SliceMetrics getInstance(@NonNull Context context, @NonNull Uri uri) {
+        if (BuildCompat.isAtLeastP()) {
+            return new SliceMetricsWrapper(context, uri);
+        } else {
+            return null;
+        }
+    }
+
+    protected void logVisible() {}
+
+    protected void logHidden() {}
+
+    protected void logTouch(int actionType, @NonNull Uri subSlice) {}
+}
