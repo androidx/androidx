e800f4493f416ea0f993733ea3dc7abae306e2ee
Kevin Chyn <kchyn@google.com>
2018-07-24 11:05:18 -0700

    Add getters to PromptInfo, remove BIOMETRIC_ prefix from errors
    Fixes: 111785106
    
    Test: ./gradlew updateApi
    Test: frameworks/support/gradlew -p frameworks/support :biometric:lintDebug
    Test: frameworks/support/gradlew -p frameworks/support :support-biometric-demos:lintDebug
    Test: ./gradlew buildOnServer
    Change-Id: Ica1dc4cc5b8557bceb94124e080d2bdbe6694d13

--- biometric/src/main/java/androidx/biometrics/BiometricConstants.java
+++ biometric/src/main/java/androidx/biometrics/BiometricConstants.java
@@ -28,44 +28,44 @@ public interface BiometricConstants {
     /**
      * The hardware is unavailable. Try again later.
      */
-    int BIOMETRIC_ERROR_HW_UNAVAILABLE = 1;
+    int ERROR_HW_UNAVAILABLE = 1;
 
     /**
      * Error state returned when the sensor was unable to process the current image.
      */
-    int BIOMETRIC_ERROR_UNABLE_TO_PROCESS = 2;
+    int ERROR_UNABLE_TO_PROCESS = 2;
 
     /**
      * Error state returned when the current request has been running too long. This is intended to
      * prevent programs from waiting for the biometric sensor indefinitely. The timeout is platform
      * and sensor-specific, but is generally on the order of 30 seconds.
      */
-    int BIOMETRIC_ERROR_TIMEOUT = 3;
+    int ERROR_TIMEOUT = 3;
 
     /**
      * Error state returned for operations like enrollment; the operation cannot be completed
      * because there's not enough storage remaining to complete the operation.
      */
-    int BIOMETRIC_ERROR_NO_SPACE = 4;
+    int ERROR_NO_SPACE = 4;
 
     /**
      * The operation was canceled because the biometric sensor is unavailable. For example, this may
      * happen when the user is switched, the device is locked or another pending operation prevents
      * or disables it.
      */
-    int BIOMETRIC_ERROR_CANCELED = 5;
+    int ERROR_CANCELED = 5;
 
     /**
      * @hide
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY)
-    int BIOMETRIC_ERROR_UNABLE_TO_REMOVE = 6;
+    int ERROR_UNABLE_TO_REMOVE = 6;
 
     /**
      * The operation was canceled because the API is locked out due to too many attempts.
      * This occurs after 5 failed attempts, and lasts for 30 seconds.
      */
-    int BIOMETRIC_ERROR_LOCKOUT = 7;
+    int ERROR_LOCKOUT = 7;
 
     /**
      * Hardware vendors may extend this list if there are conditions that do not fall under one of
@@ -75,40 +75,40 @@ public interface BiometricConstants {
      * error message string if they happen, but are advised not to rely on the message id since they
      * will be device and vendor-specific
      */
-    int BIOMETRIC_ERROR_VENDOR = 8;
+    int ERROR_VENDOR = 8;
 
     /**
-     * The operation was canceled because BIOMETRIC_ERROR_LOCKOUT occurred too many times.
+     * The operation was canceled because ERROR_LOCKOUT occurred too many times.
      * Biometric authentication is disabled until the user unlocks with strong authentication
      * (PIN/Pattern/Password)
      */
-    int BIOMETRIC_ERROR_LOCKOUT_PERMANENT = 9;
+    int ERROR_LOCKOUT_PERMANENT = 9;
 
     /**
      * The user canceled the operation. Upon receiving this, applications should use alternate
      * authentication (e.g. a password). The application should also provide the means to return to
      * biometric authentication, such as a "use <biometric>" button.
      */
-    int BIOMETRIC_ERROR_USER_CANCELED = 10;
+    int ERROR_USER_CANCELED = 10;
 
     /**
      * The user does not have any biometrics enrolled.
      */
-    int BIOMETRIC_ERROR_NO_BIOMETRICS = 11;
+    int ERROR_NO_BIOMETRICS = 11;
 
     /**
      * The device does not have a biometric sensor.
      */
-    int BIOMETRIC_ERROR_HW_NOT_PRESENT = 12;
+    int ERROR_HW_NOT_PRESENT = 12;
 
     /**
      * The user pressed the negative button.
      */
-    int BIOMETRIC_ERROR_NEGATIVE_BUTTON = 13;
+    int ERROR_NEGATIVE_BUTTON = 13;
 
     /**
      * @hide
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY)
-    int BIOMETRIC_ERROR_VENDOR_BASE = 1000;
+    int ERROR_VENDOR_BASE = 1000;
 }
