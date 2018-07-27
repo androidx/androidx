aosp-mirror/platform_frameworks_support

Implements "passing" consumed scroll distances during nested scroll pass.
Implement "passing" consumed scroll distances from nested scrolling parent
back to child in dispatchedNestedScroll and onNestedScroll.  This enables
the parent to inform the child of scroll distances that were not consumed
by the parent during final dispatchNestedScroll and onNestedScroll calls,
therefore allowing the child to do things like draw the overscroll edge
glow effect.

Bug: 69326291, 70870377
Test: androidx.core.view.NestedScrollingChildHelperTest
      androidx.core.view.NestedScrollingHelperIntegrationTest
      androidx.core.view.ViewCompatTest
      androidx.core.view.ViewParentCompatTest
      androidx.core.widget.NestedScrollViewInteractionTest
      androidx.core.widget.NestedScrollViewNestedScrollingParent2Test
      androidx.core.widget.NestedScrollViewNestedScrollingParent3Test
      androidx.core.widget.NestedScrollViewTest

Change-Id: Ia5519ba83d216db8aab58b211408d300e59832f4

compat/src/androidTest/java/androidx/core/view/ViewParentCompatTest.java
@@ -1,0 +1,200 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.core.view;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import android.support.v4.BaseInstrumentationTestCase;
+import android.view.View;
+import android.view.ViewParent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.filters.MediumTest;
+import androidx.test.filters.SdkSuppress;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class ViewParentCompatTest extends BaseInstrumentationTestCase<ViewCompatActivity> {
+
+    private View mView;
+
+    public ViewParentCompatTest() {
+        super(ViewCompatActivity.class);
+    }
+
+    @Before
+    public void setup() {
+        mView = new View(mActivityTestRule.getActivity());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNestedScrollingParent3_callsCorrectMethod() {
+        final NestedScrollingParent3Impl nestedScrollingParent3Impl =
+                mock(NestedScrollingParent3Impl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParent3Impl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParent3Impl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+        verify(nestedScrollingParent3Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+        verify(nestedScrollingParent3Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNestedScrollingParent2_callsCorrectMethod() {
+        final NestedScrollingParent2Impl nestedScrollingParent2Impl =
+                mock(NestedScrollingParent2Impl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParent2Impl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParent2Impl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH);
+        verify(nestedScrollingParent2Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNscTouchTypeNotTouch_callsNothing() {
+        final NestedScrollingParentImpl nestedScrollingParentImpl =
+                mock(NestedScrollingParentImpl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParentImpl,
+                mView,
+                11,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_NON_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParentImpl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = 21)
+    public void  dispatchNestedScroll_viewIsNotASupportNestedScrollingParent_callsCorrectMethod() {
+        final ViewParentImpl viewParentImpl = mock(ViewParentImpl.class);
+
+        ViewParentCompat.onNestedScroll(
+                viewParentImpl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{5, 6});
+
+        verify(viewParentImpl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4);
+    }
+
+    public abstract static class ViewParentImpl implements ViewParent {
+
+    }
+
+    public abstract static class NestedScrollingParentImpl extends ViewParentImpl
+            implements NestedScrollingParent{
+
+        @Override
+        public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed) {
+
+        }
+    }
+
+    public abstract static class NestedScrollingParent2Impl extends NestedScrollingParentImpl
+            implements NestedScrollingParent2 {
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type) {
+
+        }
+    }
+
+    public abstract static class NestedScrollingParent3Impl extends NestedScrollingParent2Impl
+            implements NestedScrollingParent3 {
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type, @Nullable int[] consumed) {
+        }
+    }
+}

compat/src/androidTest/java/androidx/core/widget/NestedScrollViewInteractionTest.java
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.widget;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+import android.content.Context;
+import android.support.v4.BaseInstrumentationTestCase;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.TestContentViewActivity;
+import androidx.core.view.NestedScrollingChild3;
+import androidx.core.view.NestedScrollingParent3;
+import androidx.core.view.ViewCompat;
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class NestedScrollViewInteractionTest extends
+        BaseInstrumentationTestCase<TestContentViewActivity> {
+
+    private NestedScrollingSpyView mParentSpy;
+    private NestedScrollView mNestedScrollView;
+
+    public NestedScrollViewInteractionTest() {
+        super(TestContentViewActivity.class);
+    }
+
+    @Before
+    public void setup() {
+
+        // Create views
+
+        View child = new View(mActivityTestRule.getActivity());
+        child.setMinimumWidth(1000);
+        child.setMinimumHeight(1100);
+
+        mNestedScrollView = new NestedScrollView(mActivityTestRule.getActivity());
+        mNestedScrollView.setMinimumWidth(1000);
+        mNestedScrollView.setMinimumHeight(1000);
+
+        mParentSpy = Mockito.spy(new NestedScrollingSpyView(mActivityTestRule.getActivity()));
+        mParentSpy.setMinimumWidth(1000);
+        mParentSpy.setMinimumHeight(1000);
+
+        // Create view hierarchy
+
+        mNestedScrollView.addView(child);
+        mParentSpy.addView(mNestedScrollView);
+
+        //  Measure and layout
+
+        int measureSpecWidth =
+                View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY);
+        int measureSpecHeight =
+                View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY);
+        mParentSpy.measure(measureSpecWidth, measureSpecHeight);
+        mParentSpy.layout(0, 0, 1000, 1000);
+
+        // Enable nested scrolling
+
+        doReturn(true)
+                .when(mParentSpy)
+                .onStartNestedScroll(any(View.class), any(View.class), anyInt(), anyInt());
+    }
+
+    @Test
+    public void uiFingerScroll_scrollsBeyondLimit_remainderPassedToParent() {
+        int touchSlop = ViewConfiguration.get(mActivityTestRule.getActivity()).getScaledTouchSlop();
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent move =
+                MotionEvent.obtain(0, 100, MotionEvent.ACTION_MOVE, 500, 300 - touchSlop, 0);
+
+        mParentSpy.dispatchTouchEvent(down);
+        mParentSpy.dispatchTouchEvent(move);
+
+        verify(mParentSpy).onNestedScroll(mNestedScrollView, 0, 100, 0, 100, ViewCompat.TYPE_TOUCH,
+                new int[]{0, 0});
+    }
+
+    @Test
+    public void uiFingerScroll_scrollsWithinLimit_remainderPassedToParent() {
+        int touchSlop = ViewConfiguration.get(mActivityTestRule.getActivity()).getScaledTouchSlop();
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent move =
+                MotionEvent.obtain(0, 100, MotionEvent.ACTION_MOVE, 500, 450 - touchSlop, 0);
+
+        mParentSpy.dispatchTouchEvent(down);
+        mParentSpy.dispatchTouchEvent(move);
+
+        verify(mParentSpy).onNestedScroll(mNestedScrollView, 0, 50, 0, 0, ViewCompat.TYPE_TOUCH,
+                new int[]{0, 0});
+    }
+
+    public class NestedScrollingSpyView extends FrameLayout implements NestedScrollingChild3,
+            NestedScrollingParent3 {
+
+        public NestedScrollingSpyView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes,
+                int type) {
+            return false;
+        }
+
+        @Override
+        public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes,
+                int type) {
+
+        }
+
+        @Override
+        public void onStopNestedScroll(@NonNull View target, int type) {
+
+        }
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type) {
+
+        }
+
+        @Override
+        public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed,
+                int type) {
+
+        }
+
+        @Override
+        public boolean startNestedScroll(int axes, int type) {
+            return false;
+        }
+
+        @Override
+        public void stopNestedScroll(int type) {
+
+        }
+
+        @Override
+        public boolean hasNestedScrollingParent(int type) {
+            return false;
+        }
+
+        @Override
+        public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed, @Nullable int[] offsetInWindow, int type) {
+            return false;
+        }
+
+        @Override
+        public boolean dispatchNestedPreScroll(int dx, int dy, @Nullable int[] consumed,
+                @Nullable int[] offsetInWindow, int type) {
+            return false;
+        }
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
+        }
+
+        @Override
+        public void dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed, @Nullable int[] offsetInWindow, int type,
+                @NonNull int[] consumed) {
+        }
+    }
+}

androidx/androidx

Bug: 69326291

compat/src/androidTest/java/androidx/core/view/ViewParentCompatTest.java
@@ -1,0 +1,196 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.core.view;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import android.view.View;
+import android.view.ViewParent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.MediumTest;
+import androidx.test.filters.SdkSuppress;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class ViewParentCompatTest {
+
+    private View mView;
+
+    @Before
+    public void setup() {
+        mView = new View(InstrumentationRegistry.getContext());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNestedScrollingParent3_callsCorrectMethod() {
+        final NestedScrollingParent3Impl nestedScrollingParent3Impl =
+                mock(NestedScrollingParent3Impl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParent3Impl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParent3Impl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+        verify(nestedScrollingParent3Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+        verify(nestedScrollingParent3Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNestedScrollingParent2_callsCorrectMethod() {
+        final NestedScrollingParent2Impl nestedScrollingParent2Impl =
+                mock(NestedScrollingParent2Impl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParent2Impl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParent2Impl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH);
+        verify(nestedScrollingParent2Impl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    public void  dispatchNestedScroll_viewIsNscTouchTypeNotTouch_callsNothing() {
+        final NestedScrollingParentImpl nestedScrollingParentImpl =
+                mock(NestedScrollingParentImpl.class);
+
+        ViewParentCompat.onNestedScroll(
+                nestedScrollingParentImpl,
+                mView,
+                11,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_NON_TOUCH,
+                new int[]{6, 7});
+
+        verify(nestedScrollingParentImpl, never()).onNestedScroll(
+                any(View.class),
+                anyInt(),
+                anyInt(),
+                anyInt(),
+                anyInt());
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = 21)
+    public void  dispatchNestedScroll_viewIsNotASupportNestedScrollingParent_callsCorrectMethod() {
+        final ViewParentImpl viewParentImpl = mock(ViewParentImpl.class);
+
+        ViewParentCompat.onNestedScroll(
+                viewParentImpl,
+                mView,
+                1,
+                2,
+                3,
+                4,
+                ViewCompat.TYPE_TOUCH,
+                new int[]{5, 6});
+
+        verify(viewParentImpl).onNestedScroll(
+                mView,
+                1,
+                2,
+                3,
+                4);
+    }
+
+    public abstract static class ViewParentImpl implements ViewParent {
+
+    }
+
+    public abstract static class NestedScrollingParentImpl extends ViewParentImpl
+            implements NestedScrollingParent{
+
+        @Override
+        public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed) {
+
+        }
+    }
+
+    public abstract static class NestedScrollingParent2Impl extends NestedScrollingParentImpl
+            implements NestedScrollingParent2 {
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type) {
+
+        }
+    }
+
+    public abstract static class NestedScrollingParent3Impl extends NestedScrollingParent2Impl
+            implements NestedScrollingParent3 {
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type, @Nullable int[] consumed) {
+        }
+    }
+}

compat/src/androidTest/java/androidx/core/widget/NestedScrollViewInteractionTest.java
@@ -1,0 +1,193 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.widget;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.NestedScrollingChild3;
+import androidx.core.view.NestedScrollingParent3;
+import androidx.core.view.ViewCompat;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class NestedScrollViewInteractionTest {
+
+    private NestedScrollingSpyView mParentSpy;
+    private NestedScrollView mNestedScrollView;
+
+    @Before
+    public void setup() {
+
+        // Create views
+
+        View child = new View(InstrumentationRegistry.getContext());
+        child.setMinimumWidth(1000);
+        child.setMinimumHeight(1100);
+
+        mNestedScrollView = new NestedScrollView(InstrumentationRegistry.getContext());
+        mNestedScrollView.setMinimumWidth(1000);
+        mNestedScrollView.setMinimumHeight(1000);
+
+        mParentSpy = Mockito.spy(new NestedScrollingSpyView(InstrumentationRegistry.getContext()));
+        mParentSpy.setMinimumWidth(1000);
+        mParentSpy.setMinimumHeight(1000);
+
+        // Create view hierarchy
+
+        mNestedScrollView.addView(child);
+        mParentSpy.addView(mNestedScrollView);
+
+        //  Measure and layout
+
+        int measureSpecWidth =
+                View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY);
+        int measureSpecHeight =
+                View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY);
+        mParentSpy.measure(measureSpecWidth, measureSpecHeight);
+        mParentSpy.layout(0, 0, 1000, 1000);
+
+        // Enable nested scrolling
+
+        doReturn(true)
+                .when(mParentSpy)
+                .onStartNestedScroll(any(View.class), any(View.class), anyInt(), anyInt());
+    }
+
+    @Test
+    public void uiFingerScroll_scrollsBeyondLimit_remainderPassedToParent() {
+        int touchSlop =
+                ViewConfiguration.get(InstrumentationRegistry.getContext()).getScaledTouchSlop();
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent move =
+                MotionEvent.obtain(0, 100, MotionEvent.ACTION_MOVE, 500, 300 - touchSlop, 0);
+
+        mParentSpy.dispatchTouchEvent(down);
+        mParentSpy.dispatchTouchEvent(move);
+
+        verify(mParentSpy).onNestedScroll(mNestedScrollView, 0, 100, 0, 100, ViewCompat.TYPE_TOUCH,
+                new int[]{0, 0});
+    }
+
+    @Test
+    public void uiFingerScroll_scrollsWithinLimit_remainderPassedToParent() {
+        int touchSlop =
+                ViewConfiguration.get(InstrumentationRegistry.getContext()).getScaledTouchSlop();
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent move =
+                MotionEvent.obtain(0, 100, MotionEvent.ACTION_MOVE, 500, 450 - touchSlop, 0);
+
+        mParentSpy.dispatchTouchEvent(down);
+        mParentSpy.dispatchTouchEvent(move);
+
+        verify(mParentSpy).onNestedScroll(mNestedScrollView, 0, 50, 0, 0, ViewCompat.TYPE_TOUCH,
+                new int[]{0, 0});
+    }
+
+    public class NestedScrollingSpyView extends FrameLayout implements NestedScrollingChild3,
+            NestedScrollingParent3 {
+
+        public NestedScrollingSpyView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes,
+                int type) {
+            return false;
+        }
+
+        @Override
+        public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes,
+                int type) {
+
+        }
+
+        @Override
+        public void onStopNestedScroll(@NonNull View target, int type) {
+
+        }
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type) {
+
+        }
+
+        @Override
+        public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed,
+                int type) {
+
+        }
+
+        @Override
+        public boolean startNestedScroll(int axes, int type) {
+            return false;
+        }
+
+        @Override
+        public void stopNestedScroll(int type) {
+
+        }
+
+        @Override
+        public boolean hasNestedScrollingParent(int type) {
+            return false;
+        }
+
+        @Override
+        public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed, @Nullable int[] offsetInWindow, int type) {
+            return false;
+        }
+
+        @Override
+        public boolean dispatchNestedPreScroll(int dx, int dy, @Nullable int[] consumed,
+                @Nullable int[] offsetInWindow, int type) {
+            return false;
+        }
+
+        @Override
+        public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                int dxUnconsumed, int dyUnconsumed, int type, @NonNull int[] consumed) {
+        }
+
+        @Override
+        public void dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
+                int dyUnconsumed, @Nullable int[] offsetInWindow, int type,
+                @NonNull int[] consumed) {
+        }
+    }
+}