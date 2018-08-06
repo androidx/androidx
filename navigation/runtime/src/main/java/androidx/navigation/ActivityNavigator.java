aosp-mirror/platform_frameworks_support

Add missing nullability annotations to navigation
Enable lint check to ensure public API is fully annotated.

Test: ./gradlew :na:na-co:linD :na:na-fr:linD :na:na-ru:linD :na:na-ui:linD :na:na-te:linD
Change-Id: I87b408e844e16e040ff6b1c2431aa0da332132b9

navigation/runtime/src/main/java/androidx/navigation/ActivityNavigator.java
@@ -213,7 +213,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setIntent(Intent intent) {
+        public Destination setIntent(@NonNull Intent intent) {
             mIntent = intent;
             return this;
         }
@@ -234,7 +234,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setComponentName(ComponentName name) {
+        public Destination setComponentName(@NonNull ComponentName name) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -260,7 +260,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setAction(String action) {
+        public Destination setAction(@NonNull String action) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -291,7 +291,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setData(Uri data) {
+        public Destination setData(@NonNull Uri data) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -322,7 +322,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setDataPattern(String dataPattern) {
+        public Destination setDataPattern(@NonNull String dataPattern) {
             mDataPattern = dataPattern;
             return this;
         }

androidx/androidx

navigation/runtime/src/main/java/androidx/navigation/ActivityNavigator.java
@@ -213,7 +213,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setIntent(Intent intent) {
+        public Destination setIntent(@Nullable Intent intent) {
             mIntent = intent;
             return this;
         }
@@ -234,7 +234,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setComponentName(ComponentName name) {
+        public Destination setComponentName(@Nullable ComponentName name) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -260,7 +260,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setAction(String action) {
+        public Destination setAction(@Nullable String action) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -291,7 +291,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setData(Uri data) {
+        public Destination setData(@Nullable Uri data) {
             if (mIntent == null) {
                 mIntent = new Intent();
             }
@@ -322,7 +322,7 @@ public class ActivityNavigator extends Navigator<ActivityNavigator.Destination> 
          * @return this {@link Destination}
          */
         @NonNull
-        public Destination setDataPattern(String dataPattern) {
+        public Destination setDataPattern(@Nullable String dataPattern) {
             mDataPattern = dataPattern;
             return this;
         }