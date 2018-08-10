2de179f0af58f9e0b8e30defd47c97d878a52337
Chris Craik <ccraik@google.com>
2018-08-10 10:47:32 -0700

    Move Paging room sample code out of room integration testapp
    This allows the Room testapp to go back to tests-only, except for its service.
    
    Test: ./gradlew room:integ:testapp:cC
    Test: Ran paging samples
    Fixes: 77486557
    
    Change-Id: I96015e144eaab0017c7a8c103630feba9f3fbac2

--- paging/integration-tests/testapp/src/main/java/androidx/paging/integration/testapp/room/PagedListCustomerAdapter.java
+++ paging/integration-tests/testapp/src/main/java/androidx/paging/integration/testapp/room/PagedListCustomerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.room.integration.testapp;
+package androidx.paging.integration.testapp.room;
 
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -23,9 +23,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.paging.PagedList;
 import androidx.paging.PagedListAdapter;
+import androidx.paging.integration.testapp.R;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.room.integration.testapp.database.Customer;
-import androidx.room.integration.testapp.database.LastNameAscCustomerDataSource;
 
 /**
  * Sample adapter which uses a AsyncPagedListDiffer.
