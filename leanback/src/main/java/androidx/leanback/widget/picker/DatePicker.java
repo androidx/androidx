aosp-mirror/platform_frameworks_support

Add setDate(long) in DatePicker
Add this method as per suggestion from api review.

Bug: 109734722
Bug: 74391173

Test: SupportLeanbackDemos app -> Date picker, Time picker, Pin picker activities
Change-Id: I9582092d12d21cde3969b117611d9b11276f943a

leanback/src/main/java/androidx/leanback/widget/picker/DatePicker.java
@@ -408,6 +408,19 @@ public class DatePicker extends Picker {
     }
 
     /**
+     * Update the current date in milliseconds since January 1, 1970 00:00:00 in
+     * {@link TimeZone#getDefault()} time zone.
+     *
+     * @param timeInMilliseconds current date value in milliseconds.
+     */
+    public void setDate(long timeInMilliseconds) {
+        Calendar tempDate = Calendar.getInstance(mConstant.locale);
+        tempDate.setTimeInMillis(timeInMilliseconds);
+        setDate(tempDate.get(Calendar.YEAR), tempDate.get(Calendar.MONTH),
+                tempDate.get(Calendar.DAY_OF_MONTH), false);
+    }
+
+    /**
      * Update the current date.
      *
      * @param year The year.

androidx/androidx

Bug: 113331337

leanback/src/main/java/androidx/leanback/widget/picker/DatePicker.java
@@ -408,6 +408,18 @@ public class DatePicker extends Picker {
     }
 
     /**
+     * Update the current date in milliseconds since January 1, 1970 00:00:00 in
+     * {@link TimeZone#getDefault()} time zone.
+     *
+     * @param timeInMilliseconds current date value in milliseconds.
+     */
+    public void setDate(long timeInMilliseconds) {
+        mTempDate.setTimeInMillis(timeInMilliseconds);
+        setDate(mTempDate.get(Calendar.YEAR), mTempDate.get(Calendar.MONTH),
+                mTempDate.get(Calendar.DAY_OF_MONTH), false);
+    }
+
+    /**
      * Update the current date.
      *
      * @param year The year.