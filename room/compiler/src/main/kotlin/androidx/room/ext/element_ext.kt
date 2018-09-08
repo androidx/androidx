aosp-mirror/platform_frameworks_support

Safer Annotation Reader
Moved only Entity annotation on it

Test: ./gradlew :room:room-compiler:test
Change-Id: Ib005bb36556e7ad81cd298b0e4bf6035255c9c91

room/compiler/src/main/kotlin/androidx/room/ext/element_ext.kt
@@ -22,7 +22,9 @@ import com.google.auto.common.AnnotationMirrors
 import com.google.auto.common.MoreElements
 import com.google.auto.common.MoreTypes
 import me.eugeniomarletti.kotlin.metadata.shadow.load.java.JvmAbi
+import java.lang.reflect.Proxy
 import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.AnnotationMirror
 import javax.lang.model.element.AnnotationValue
 import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
@@ -96,6 +98,74 @@ fun TypeElement.getAllAbstractMethodsIncludingSupers(): Set<ExecutableElement> {
     }
 }
 
+interface ClassGetter {
+    fun callClassFun(methodName: String): TypeMirror?
+    fun <T : Annotation> callNestedAnnotationsFun(methodName: String): Array<AnnotationBox<T>>
+}
+
+class AnnotationBox<T : Annotation>(private val obj: Any) : ClassGetter by (obj as ClassGetter) {
+    @Suppress("UNCHECKED_CAST")
+    val value: T = obj as T
+}
+
+fun <T : Annotation> AnnotationMirror.asAnnotation(cl: Class<T>): AnnotationBox<T> {
+    if (!cl.isAnnotation) {
+        throw IllegalArgumentException("$cl is not annotation")
+    }
+    val map = cl.declaredMethods.associate { method ->
+        val value = AnnotationMirrors.getAnnotationValue(this, method.name)
+        val returnType = method.returnType
+        val defaultValue = method.defaultValue
+
+        val result: Any? = when {
+            returnType == Boolean::class.java -> value.getAsBoolean(defaultValue as Boolean)
+            returnType == String::class.java -> value.getAsString(defaultValue as String)
+            returnType == Array<String>::class.java -> value.getAsStringList().toTypedArray()
+            returnType == Class::class.java -> {
+                try {
+                    value.toClassType()
+                } catch (notPresent: TypeNotPresentException) {
+                    null
+                }
+            }
+            returnType == Int::class.java -> value.getAsInt(defaultValue as Int?)
+            returnType.isArray && returnType.componentType.isAnnotation -> {
+                @Suppress("UNCHECKED_CAST")
+                ListVisitor(returnType.componentType as Class<out Annotation>).visit(value)
+            }
+            else -> throw UnsupportedOperationException("$returnType isn't supported")
+        }
+        method.name to result
+    }
+    return AnnotationBox(Proxy.newProxyInstance(cl.classLoader,
+            arrayOf(cl, ClassGetter::class.java)) { _, method, args ->
+        when (method.name) {
+            ClassGetter::callClassFun.name -> map[args[0]]
+            "callNestedAnnotationsFun" -> map[args[0]]
+            else -> map[method.name]
+        }
+    })
+}
+
+fun <T : Annotation> TypeElement.toAnnotationBox(cl: Class<T>) =
+        MoreElements.getAnnotationMirror(this, cl).orNull()?.asAnnotation(cl)
+
+private class ListVisitor<T : Annotation>(private val annotationClass: Class<T>)
+    : SimpleAnnotationValueVisitor6<Array<AnnotationBox<T>>, Void?>() {
+    override fun visitArray(
+        values: MutableList<out AnnotationValue>?,
+        void: Void?
+    ): Array<AnnotationBox<T>> {
+        val visitor = AnnotationClassVisitor(annotationClass)
+        return values?.mapNotNull { visitor.visit(it) }?.toTypedArray() ?: emptyArray()
+    }
+}
+
+private class AnnotationClassVisitor<T : Annotation>(private val annotationClass: Class<T>)
+    : SimpleAnnotationValueVisitor6<AnnotationBox<T>?, Void?>() {
+    override fun visitAnnotation(a: AnnotationMirror?, v: Void?) = a?.asAnnotation(annotationClass)
+}
+
 // code below taken from dagger2
 // compiler/src/main/java/dagger/internal/codegen/ConfigurationAnnotations.java
 private val TO_LIST_OF_TYPES = object
@@ -138,14 +208,14 @@ fun AnnotationValue.toClassType(): TypeMirror? {
 fun TypeMirror.isCollection(): Boolean {
     return MoreTypes.isType(this) &&
             (MoreTypes.isTypeOf(java.util.List::class.java, this) ||
-            MoreTypes.isTypeOf(java.util.Set::class.java, this))
+                    MoreTypes.isTypeOf(java.util.Set::class.java, this))
 }
 
 fun Element.getAnnotationValue(annotation: Class<out Annotation>, fieldName: String): Any? {
     return MoreElements.getAnnotationMirror(this, annotation)
             .orNull()?.let {
-        AnnotationMirrors.getAnnotationValue(it, fieldName)?.value
-    }
+                AnnotationMirrors.getAnnotationValue(it, fieldName)?.value
+            }
 }
 
 private val ANNOTATION_VALUE_TO_INT_VISITOR = object : SimpleAnnotationValueVisitor6<Int?, Void>() {
@@ -295,6 +365,6 @@ fun Element.findKotlinDefaultImpl(typeUtils: Types): Element? {
     return innerClass.enclosedElements.find {
         it.kind == ElementKind.METHOD && it.simpleName == this.simpleName &&
                 paramsMatch(MoreElements.asExecutable(this).parameters,
-                MoreElements.asExecutable(it).parameters)
+                        MoreElements.asExecutable(it).parameters)
     }
 }

room/compiler/src/main/kotlin/androidx/room/processor/EntityProcessor.kt
@@ -16,27 +16,22 @@
 
 package androidx.room.processor
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Fts3
 import androidx.room.Fts4
-import androidx.room.ext.getAsBoolean
-import androidx.room.ext.getAsInt
-import androidx.room.ext.getAsString
-import androidx.room.ext.getAsStringList
+import androidx.room.ext.AnnotationBox
 import androidx.room.ext.hasAnyOf
-import androidx.room.ext.toType
-import androidx.room.vo.Entity
 import androidx.room.vo.ForeignKeyAction
 import androidx.room.vo.Index
 import com.google.auto.common.AnnotationMirrors.getAnnotationValue
 import javax.lang.model.element.AnnotationMirror
-import javax.lang.model.element.AnnotationValue
 import javax.lang.model.element.Name
 import javax.lang.model.element.TypeElement
 import javax.lang.model.type.TypeMirror
-import javax.lang.model.util.SimpleAnnotationValueVisitor6
 
 interface EntityProcessor {
-    fun process(): Entity
+    fun process(): androidx.room.vo.Entity
 
     companion object {
         fun extractTableName(element: TypeElement, annotation: AnnotationMirror): String {
@@ -48,37 +43,24 @@ interface EntityProcessor {
             }
         }
 
-        fun extractIndices(
-            annotation: AnnotationMirror,
-            tableName: String
-        ): List<IndexInput> {
-            val arrayOfIndexAnnotations = getAnnotationValue(annotation, "indices")
-            return INDEX_LIST_VISITOR.visit(arrayOfIndexAnnotations, tableName)
-        }
-
-        private val INDEX_LIST_VISITOR = object
-            : SimpleAnnotationValueVisitor6<List<IndexInput>, String>() {
-            override fun visitArray(
-                values: MutableList<out AnnotationValue>?,
-                tableName: String
-            ): List<IndexInput> {
-                return values?.mapNotNull {
-                    INDEX_VISITOR.visit(it, tableName)
-                } ?: emptyList()
+        fun extractTableName(element: TypeElement, annotation: Entity): String {
+            return if (annotation.tableName == "") {
+                element.simpleName.toString()
+            } else {
+                annotation.tableName
             }
         }
 
-        private val INDEX_VISITOR = object : SimpleAnnotationValueVisitor6<IndexInput?, String>() {
-            override fun visitAnnotation(a: AnnotationMirror?, tableName: String): IndexInput? {
-                val fieldInput = getAnnotationValue(a, "value").getAsStringList()
-                val unique = getAnnotationValue(a, "unique").getAsBoolean(false)
-                val nameValue = getAnnotationValue(a, "name").getAsString("")
-                val name = if (nameValue == null || nameValue == "") {
-                    createIndexName(fieldInput, tableName)
+        fun extractIndices(annotation: AnnotationBox<Entity>, tableName: String): List<IndexInput> {
+            return annotation.callNestedAnnotationsFun<androidx.room.Index>("indices").map {
+                val indexAnnotation = it.value
+                val nameValue = indexAnnotation.name
+                val name = if (nameValue == "") {
+                    createIndexName(indexAnnotation.value.asList(), tableName)
                 } else {
                     nameValue
                 }
-                return IndexInput(name, unique, fieldInput)
+                IndexInput(name, indexAnnotation.unique, indexAnnotation.value.asList())
             }
         }
 
@@ -86,45 +68,22 @@ interface EntityProcessor {
             return Index.DEFAULT_PREFIX + tableName + "_" + columnNames.joinToString("_")
         }
 
-        fun extractForeignKeys(annotation: AnnotationMirror): List<ForeignKeyInput> {
-            val arrayOfForeignKeyAnnotations = getAnnotationValue(annotation, "foreignKeys")
-            return FOREIGN_KEY_LIST_VISITOR.visit(arrayOfForeignKeyAnnotations)
-        }
-
-        private val FOREIGN_KEY_LIST_VISITOR = object
-            : SimpleAnnotationValueVisitor6<List<ForeignKeyInput>, Void?>() {
-            override fun visitArray(
-                values: MutableList<out AnnotationValue>?,
-                void: Void?
-            ): List<ForeignKeyInput> {
-                return values?.mapNotNull {
-                    FOREIGN_KEY_VISITOR.visit(it)
-                } ?: emptyList()
-            }
-        }
-
-        private val FOREIGN_KEY_VISITOR = object : SimpleAnnotationValueVisitor6<ForeignKeyInput?,
-                Void?>() {
-            override fun visitAnnotation(a: AnnotationMirror?, void: Void?): ForeignKeyInput? {
-                val entityClass = try {
-                    getAnnotationValue(a, "entity").toType()
-                } catch (notPresent: TypeNotPresentException) {
-                    return null
+        fun extractForeignKeys(annotation: AnnotationBox<Entity>): List<ForeignKeyInput> {
+            return annotation.callNestedAnnotationsFun<ForeignKey>("foreignKeys")
+                    .mapNotNull { annotationBox ->
+                val foreignKey = annotationBox.value
+                val parent = annotationBox.callClassFun("entity")
+                if (parent != null) {
+                    ForeignKeyInput(
+                            parent = parent,
+                            parentColumns = foreignKey.parentColumns.asList(),
+                            childColumns = foreignKey.childColumns.asList(),
+                            onDelete = ForeignKeyAction.fromAnnotationValue(foreignKey.onDelete),
+                            onUpdate = ForeignKeyAction.fromAnnotationValue(foreignKey.onUpdate),
+                            deferred = foreignKey.deferred)
+                } else {
+                    null
                 }
-                val parentColumns = getAnnotationValue(a, "parentColumns").getAsStringList()
-                val childColumns = getAnnotationValue(a, "childColumns").getAsStringList()
-                val onDeleteInput = getAnnotationValue(a, "onDelete").getAsInt()
-                val onUpdateInput = getAnnotationValue(a, "onUpdate").getAsInt()
-                val deferred = getAnnotationValue(a, "deferred").getAsBoolean(true)
-                val onDelete = ForeignKeyAction.fromAnnotationValue(onDeleteInput)
-                val onUpdate = ForeignKeyAction.fromAnnotationValue(onUpdateInput)
-                return ForeignKeyInput(
-                        parent = entityClass,
-                        parentColumns = parentColumns,
-                        childColumns = childColumns,
-                        onDelete = onDelete,
-                        onUpdate = onUpdate,
-                        deferred = deferred)
             }
         }
     }

androidx/androidx

room/compiler/src/main/kotlin/androidx/room/ext/element_ext.kt
@@ -22,7 +22,9 @@ import com.google.auto.common.AnnotationMirrors
 import com.google.auto.common.MoreElements
 import com.google.auto.common.MoreTypes
 import me.eugeniomarletti.kotlin.metadata.shadow.load.java.JvmAbi
+import java.lang.reflect.Proxy
 import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.AnnotationMirror
 import javax.lang.model.element.AnnotationValue
 import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
@@ -96,6 +98,79 @@ fun TypeElement.getAllAbstractMethodsIncludingSupers(): Set<ExecutableElement> {
     }
 }
 
+interface ClassGetter {
+    fun getAsTypeMirror(methodName: String): TypeMirror?
+    fun <T : Annotation> getAsAnnotationBox(methodName: String): Array<AnnotationBox<T>>
+}
+
+/**
+ * Class that helps to read values from annotations. Simple types as string, int, lists can
+ * be read from [value]. If you need to read classes or another annotations from annotation use
+ * [getAsTypeMirror] and [getAsAnnotationBox] correspondingly.
+ */
+class AnnotationBox<T : Annotation>(private val obj: Any) : ClassGetter by (obj as ClassGetter) {
+    @Suppress("UNCHECKED_CAST")
+    val value: T = obj as T
+}
+
+fun <T : Annotation> AnnotationMirror.box(cl: Class<T>): AnnotationBox<T> {
+    if (!cl.isAnnotation) {
+        throw IllegalArgumentException("$cl is not annotation")
+    }
+    val map = cl.declaredMethods.associate { method ->
+        val value = AnnotationMirrors.getAnnotationValue(this, method.name)
+        val returnType = method.returnType
+        val defaultValue = method.defaultValue
+
+        val result: Any? = when {
+            returnType == Boolean::class.java -> value.getAsBoolean(defaultValue as Boolean)
+            returnType == String::class.java -> value.getAsString(defaultValue as String?)
+            returnType == Array<String>::class.java -> value.getAsStringList().toTypedArray()
+            returnType == Class::class.java -> {
+                try {
+                    value.toClassType()
+                } catch (notPresent: TypeNotPresentException) {
+                    null
+                }
+            }
+            returnType == Int::class.java -> value.getAsInt(defaultValue as Int?)
+            returnType.isArray && returnType.componentType.isAnnotation -> {
+                @Suppress("UNCHECKED_CAST")
+                ListVisitor(returnType.componentType as Class<out Annotation>).visit(value)
+            }
+            else -> throw UnsupportedOperationException("$returnType isn't supported")
+        }
+        method.name to result
+    }
+    return AnnotationBox(Proxy.newProxyInstance(cl.classLoader,
+            arrayOf(cl, ClassGetter::class.java)) { _, method, args ->
+        when (method.name) {
+            ClassGetter::getAsTypeMirror.name -> map[args[0]]
+            "getAsAnnotationBox" -> map[args[0]]
+            else -> map[method.name]
+        }
+    })
+}
+
+fun <T : Annotation> TypeElement.toAnnotationBox(cl: Class<T>) =
+        MoreElements.getAnnotationMirror(this, cl).orNull()?.box(cl)
+
+private class ListVisitor<T : Annotation>(private val annotationClass: Class<T>)
+    : SimpleAnnotationValueVisitor6<Array<AnnotationBox<T>>, Void?>() {
+    override fun visitArray(
+        values: MutableList<out AnnotationValue>?,
+        void: Void?
+    ): Array<AnnotationBox<T>> {
+        val visitor = AnnotationClassVisitor(annotationClass)
+        return values?.mapNotNull { visitor.visit(it) }?.toTypedArray() ?: emptyArray()
+    }
+}
+
+private class AnnotationClassVisitor<T : Annotation>(private val annotationClass: Class<T>)
+    : SimpleAnnotationValueVisitor6<AnnotationBox<T>?, Void?>() {
+    override fun visitAnnotation(a: AnnotationMirror?, v: Void?) = a?.box(annotationClass)
+}
+
 // code below taken from dagger2
 // compiler/src/main/java/dagger/internal/codegen/ConfigurationAnnotations.java
 private val TO_LIST_OF_TYPES = object
@@ -138,14 +213,14 @@ fun AnnotationValue.toClassType(): TypeMirror? {
 fun TypeMirror.isCollection(): Boolean {
     return MoreTypes.isType(this) &&
             (MoreTypes.isTypeOf(java.util.List::class.java, this) ||
-            MoreTypes.isTypeOf(java.util.Set::class.java, this))
+                    MoreTypes.isTypeOf(java.util.Set::class.java, this))
 }
 
 fun Element.getAnnotationValue(annotation: Class<out Annotation>, fieldName: String): Any? {
     return MoreElements.getAnnotationMirror(this, annotation)
             .orNull()?.let {
-        AnnotationMirrors.getAnnotationValue(it, fieldName)?.value
-    }
+                AnnotationMirrors.getAnnotationValue(it, fieldName)?.value
+            }
 }
 
 private val ANNOTATION_VALUE_TO_INT_VISITOR = object : SimpleAnnotationValueVisitor6<Int?, Void>() {
@@ -295,6 +370,6 @@ fun Element.findKotlinDefaultImpl(typeUtils: Types): Element? {
     return innerClass.enclosedElements.find {
         it.kind == ElementKind.METHOD && it.simpleName == this.simpleName &&
                 paramsMatch(MoreElements.asExecutable(this).parameters,
-                MoreElements.asExecutable(it).parameters)
+                        MoreElements.asExecutable(it).parameters)
     }
 }

room/compiler/src/main/kotlin/androidx/room/processor/EntityProcessor.kt
@@ -16,27 +16,22 @@
 
 package androidx.room.processor
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Fts3
 import androidx.room.Fts4
-import androidx.room.ext.getAsBoolean
-import androidx.room.ext.getAsInt
-import androidx.room.ext.getAsString
-import androidx.room.ext.getAsStringList
+import androidx.room.ext.AnnotationBox
 import androidx.room.ext.hasAnyOf
-import androidx.room.ext.toType
-import androidx.room.vo.Entity
 import androidx.room.vo.ForeignKeyAction
 import androidx.room.vo.Index
 import com.google.auto.common.AnnotationMirrors.getAnnotationValue
 import javax.lang.model.element.AnnotationMirror
-import javax.lang.model.element.AnnotationValue
 import javax.lang.model.element.Name
 import javax.lang.model.element.TypeElement
 import javax.lang.model.type.TypeMirror
-import javax.lang.model.util.SimpleAnnotationValueVisitor6
 
 interface EntityProcessor {
-    fun process(): Entity
+    fun process(): androidx.room.vo.Entity
 
     companion object {
         fun extractTableName(element: TypeElement, annotation: AnnotationMirror): String {
@@ -48,37 +43,24 @@ interface EntityProcessor {
             }
         }
 
-        fun extractIndices(
-            annotation: AnnotationMirror,
-            tableName: String
-        ): List<IndexInput> {
-            val arrayOfIndexAnnotations = getAnnotationValue(annotation, "indices")
-            return INDEX_LIST_VISITOR.visit(arrayOfIndexAnnotations, tableName)
-        }
-
-        private val INDEX_LIST_VISITOR = object
-            : SimpleAnnotationValueVisitor6<List<IndexInput>, String>() {
-            override fun visitArray(
-                values: MutableList<out AnnotationValue>?,
-                tableName: String
-            ): List<IndexInput> {
-                return values?.mapNotNull {
-                    INDEX_VISITOR.visit(it, tableName)
-                } ?: emptyList()
+        fun extractTableName(element: TypeElement, annotation: Entity): String {
+            return if (annotation.tableName == "") {
+                element.simpleName.toString()
+            } else {
+                annotation.tableName
             }
         }
 
-        private val INDEX_VISITOR = object : SimpleAnnotationValueVisitor6<IndexInput?, String>() {
-            override fun visitAnnotation(a: AnnotationMirror?, tableName: String): IndexInput? {
-                val fieldInput = getAnnotationValue(a, "value").getAsStringList()
-                val unique = getAnnotationValue(a, "unique").getAsBoolean(false)
-                val nameValue = getAnnotationValue(a, "name").getAsString("")
-                val name = if (nameValue == null || nameValue == "") {
-                    createIndexName(fieldInput, tableName)
+        fun extractIndices(annotation: AnnotationBox<Entity>, tableName: String): List<IndexInput> {
+            return annotation.getAsAnnotationBox<androidx.room.Index>("indices").map {
+                val indexAnnotation = it.value
+                val nameValue = indexAnnotation.name
+                val name = if (nameValue == "") {
+                    createIndexName(indexAnnotation.value.asList(), tableName)
                 } else {
                     nameValue
                 }
-                return IndexInput(name, unique, fieldInput)
+                IndexInput(name, indexAnnotation.unique, indexAnnotation.value.asList())
             }
         }
 
@@ -86,45 +68,22 @@ interface EntityProcessor {
             return Index.DEFAULT_PREFIX + tableName + "_" + columnNames.joinToString("_")
         }
 
-        fun extractForeignKeys(annotation: AnnotationMirror): List<ForeignKeyInput> {
-            val arrayOfForeignKeyAnnotations = getAnnotationValue(annotation, "foreignKeys")
-            return FOREIGN_KEY_LIST_VISITOR.visit(arrayOfForeignKeyAnnotations)
-        }
-
-        private val FOREIGN_KEY_LIST_VISITOR = object
-            : SimpleAnnotationValueVisitor6<List<ForeignKeyInput>, Void?>() {
-            override fun visitArray(
-                values: MutableList<out AnnotationValue>?,
-                void: Void?
-            ): List<ForeignKeyInput> {
-                return values?.mapNotNull {
-                    FOREIGN_KEY_VISITOR.visit(it)
-                } ?: emptyList()
-            }
-        }
-
-        private val FOREIGN_KEY_VISITOR = object : SimpleAnnotationValueVisitor6<ForeignKeyInput?,
-                Void?>() {
-            override fun visitAnnotation(a: AnnotationMirror?, void: Void?): ForeignKeyInput? {
-                val entityClass = try {
-                    getAnnotationValue(a, "entity").toType()
-                } catch (notPresent: TypeNotPresentException) {
-                    return null
+        fun extractForeignKeys(annotation: AnnotationBox<Entity>): List<ForeignKeyInput> {
+            return annotation.getAsAnnotationBox<ForeignKey>("foreignKeys")
+                    .mapNotNull { annotationBox ->
+                val foreignKey = annotationBox.value
+                val parent = annotationBox.getAsTypeMirror("entity")
+                if (parent != null) {
+                    ForeignKeyInput(
+                            parent = parent,
+                            parentColumns = foreignKey.parentColumns.asList(),
+                            childColumns = foreignKey.childColumns.asList(),
+                            onDelete = ForeignKeyAction.fromAnnotationValue(foreignKey.onDelete),
+                            onUpdate = ForeignKeyAction.fromAnnotationValue(foreignKey.onUpdate),
+                            deferred = foreignKey.deferred)
+                } else {
+                    null
                 }
-                val parentColumns = getAnnotationValue(a, "parentColumns").getAsStringList()
-                val childColumns = getAnnotationValue(a, "childColumns").getAsStringList()
-                val onDeleteInput = getAnnotationValue(a, "onDelete").getAsInt()
-                val onUpdateInput = getAnnotationValue(a, "onUpdate").getAsInt()
-                val deferred = getAnnotationValue(a, "deferred").getAsBoolean(true)
-                val onDelete = ForeignKeyAction.fromAnnotationValue(onDeleteInput)
-                val onUpdate = ForeignKeyAction.fromAnnotationValue(onUpdateInput)
-                return ForeignKeyInput(
-                        parent = entityClass,
-                        parentColumns = parentColumns,
-                        childColumns = childColumns,
-                        onDelete = onDelete,
-                        onUpdate = onUpdate,
-                        deferred = deferred)
             }
         }
     }