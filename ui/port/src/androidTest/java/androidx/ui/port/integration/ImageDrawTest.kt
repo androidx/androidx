56d4656bfeb3ebf92ecce144b5257873c51ad6b8
Mihai Popa <popam@google.com>
2018-09-10 13:03:24 +0100

    Another integration test
    The CL is adding one more integration test. The test is drawing a blue
    rectangle bitmap with aspect ratio 2, and then we ensure that:
    - the drawing contains exactly one rectangle, with no gaps (i.e.
    non-blue pixels) and with valid shape
    - the rectangle preserves the aspect ratio
    - there is no blue pixel arbitrarily drawn outside the rectangle
    
    Test: ran the test
    Change-Id: I95493e6e8ce14d482137a0718b9aef2a67c77e85

--- ui/port/src/androidTest/java/androidx/ui/port/integration/ImageDrawTest.kt
+++ ui/port/src/androidTest/java/androidx/ui/port/integration/ImageDrawTest.kt
@@ -14,13 +14,14 @@
  * limitations under the License.
  */
 
-package androidx.ui.port
+package androidx.ui.port.integration
 
 import android.app.Activity
 import android.app.Instrumentation
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
 import android.view.View
 import android.view.View.MeasureSpec.AT_MOST
 import androidx.test.InstrumentationRegistry
@@ -36,13 +37,15 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import androidx.testutils.PollingCheck
+import androidx.ui.port.R
+import androidx.ui.widgets.framework.Widget
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
-import kotlin.math.abs
+import kotlin.math.min
 
 @RunWith(JUnit4::class)
 @SmallTest
-class IntegrationTest {
+class ImageDrawTest {
     @get:Rule
     public val activityTestRule = ActivityTestRule<TestActivity>(TestActivity::class.java)
 
@@ -56,12 +59,61 @@ class IntegrationTest {
         instrumentation = InstrumentationRegistry.getInstrumentation()
     }
 
-    private val image by lazy {
+    private val blueRectangleImage by lazy {
+        val activitySmallerDimension =
+                min(activity.window.decorView.width, activity.window.decorView.height)
+        // Make the bitmap's width twice its height.
+        val bitmapHeight = activitySmallerDimension / 4
+        val bitmapWidth = 2 * bitmapHeight
+        Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888).apply {
+            eraseColor(Color.BLUE)
+        }
+    }
+
+    private val fourQuadrantsImage by lazy {
         BitmapFactory.decodeResource(
                 activity.resources,
                 R.drawable.four_quadrants,
                 BitmapFactory.Options().apply { inScaled = false }
         )
+    }
+
+    /**
+     * The test draws a bitmap representing a completely blue rectangle with the property that
+     * width = 2 * height. The test then looks at the resulting drawing, and checks that it
+     * contains a valid/well formed rectangle of blue pixels, which preserves the aspect ratio
+     * of 2, and that there is no other blue pixel in the drawing outside this rectangle.
+     */
+    @Test
+    fun testBitmapAspectRatioIsPreserved() {
+        val bitmap = drawWidgetToBitmap(
+                RawImage(
+                        image = Image(blueRectangleImage),
+                        key = Key.createKey("blue rectangle image")
+                )
+        )
+
+        // Get the minimum enclosing rectangle of the blue pixels in the drawing.
+        (0 until bitmap.width).asSequence().flatMap { i -> (0 until bitmap.height).asSequence()
+                .filter { j -> bitmap.getPixel(i, j) == Color.BLUE }.map { j -> Pair(i, j) } }
+                .toList().run {
+                    val x1 = minBy { it.first }?.first ?: 0
+                    val y1 = minBy { it.second }?.second ?: 0
+                    val x2 = maxBy { it.first }?.first ?: 0
+                    val y2 = maxBy { it.second }?.second ?: 0
+
+                    val originalRatio = 1f * blueRectangleImage.width / blueRectangleImage.height
+                    val currentRatio = 1f * (x2 - x1) / (y2 - y1)
+                    // Make sure the aspect ratio is preserved.
+                    assertEquals(originalRatio, currentRatio, 0.1f)
+
+                    // Check that the rectangle is completely blue.
+                    (x1..x2).forEach { x ->
+                        (y1..y2).forEach { y ->
+                            assertEquals(Color.BLUE, bitmap.getPixel(x, y))
+                        }
+                    }
+                }
     }
 
     /**
@@ -73,47 +125,13 @@ class IntegrationTest {
      * filtered.
      */
     @Test
-    fun test() {
-        // Craneview containing a jetpack image.
-        lateinit var craneView: CraneView
-        // A canvas where we will draw the craneView.
-        lateinit var canvas: Canvas
-        // A bitmap backing the above canvas.
-        lateinit var bitmap: Bitmap
-
-        // Initialize craneView & measure.
-        instrumentation.runOnMainSync {
-            val widget = RawImage(
-                    image = Image(image),
-                    key = Key.createKey("four quadrants image")
-            )
-
-            // Measure.
-            craneView = CraneView(activity, widget)
-            craneView.measure(
-                    View.MeasureSpec.makeMeasureSpec(activity.window.decorView.width, AT_MOST),
-                    View.MeasureSpec.makeMeasureSpec(activity.window.decorView.height, AT_MOST)
-            )
-
-            // Initialize the canvas and its backing bitmap.
-            bitmap = Bitmap.createBitmap(
-                    craneView.measuredWidth,
-                    craneView.measuredHeight,
-                    image.config
-            )
-            canvas = Canvas(bitmap)
-        }
-
-        // Layout. Doing this in a separate runOnMainSync, as during measurement we schedule
-        // a frame in a post'd job on the main thread, and we want that to be run before.
-        instrumentation.runOnMainSync {
-            craneView.layout(0, 0, craneView.measuredWidth, craneView.measuredHeight)
-        }
-
-        // Draw.
-        instrumentation.runOnMainSync {
-            craneView.draw(canvas)
-        }
+    fun testBitmapStructureIsPreserved() {
+        val bitmap = drawWidgetToBitmap(
+                RawImage(
+                        image = Image(fourQuadrantsImage),
+                        key = Key.createKey("four quadrants image")
+                )
+        )
 
         // With some tolerance, make sure the drawing still has four quadrants.
         // (we need some error acceptance as the bitmap is scaled, filtered, etc. before being
@@ -130,6 +148,46 @@ class IntegrationTest {
             val epsilon = frequentPixelsSum * 0.01f // 1% of the solid color area
             take(4).forEach { assertEquals(quarterPixels, it.second.toFloat(), epsilon) }
         }
+    }
+
+    private fun drawWidgetToBitmap(widget: Widget): Bitmap {
+        // Craneview containing the widget to be drawn.
+        lateinit var craneView: CraneView
+        // A canvas where we will draw the craneView.
+        lateinit var canvas: Canvas
+        // A bitmap backing the above canvas.
+        lateinit var bitmap: Bitmap
+
+        // Initialize craneView & measure.
+        instrumentation.runOnMainSync {
+            // Measure.
+            craneView = CraneView(activity, widget)
+            craneView.measure(
+                    View.MeasureSpec.makeMeasureSpec(activity.window.decorView.width, AT_MOST),
+                    View.MeasureSpec.makeMeasureSpec(activity.window.decorView.height, AT_MOST)
+            )
+
+            // Initialize the canvas and its backing bitmap.
+            bitmap = Bitmap.createBitmap(
+                    craneView.measuredWidth,
+                    craneView.measuredHeight,
+                    Bitmap.Config.ARGB_8888
+            )
+            canvas = Canvas(bitmap)
+        }
+
+        // Layout. Doing this in a separate runOnMainSync, as during measurement we schedule
+        // a frame in a post'd job on the main thread, and we want that to be run before.
+        instrumentation.runOnMainSync {
+            craneView.layout(0, 0, craneView.measuredWidth, craneView.measuredHeight)
+        }
+
+        // Draw.
+        instrumentation.runOnMainSync {
+            craneView.draw(canvas)
+        }
+
+        return bitmap
     }
 
     private fun Bitmap.pixelCount(): MutableMap<Int, Int> {
