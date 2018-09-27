aosp-mirror/platform_frameworks_support

Call Scene's exit action when use TransitionManager.go() with null transition
Bug: 65536124

When we pass null for TransitionManager.go() there is a special logic where we forgot to call previousScene.exit(), only newscene.enter() is called

Test: added unit tests and tested manually
Change-Id: Ibd4d5a2cc17bbfe4aaa586357446da28d4b355af

transition/src/main/java/androidx/transition/TransitionManager.java
@@ -170,6 +170,12 @@ public class TransitionManager {
 
         if (!sPendingTransitions.contains(sceneRoot)) {
             if (transition == null) {
+                // Notify previous scene that it is being exited
+                Scene previousScene = Scene.getCurrentScene(sceneRoot);
+                if (previousScene != null) {
+                    previousScene.exit();
+                }
+
                 scene.enter();
             } else {
                 sPendingTransitions.add(sceneRoot);

androidx/androidx

transition/src/main/java/androidx/transition/TransitionManager.java
@@ -169,7 +169,13 @@ public class TransitionManager {
         final ViewGroup sceneRoot = scene.getSceneRoot();
 
         if (!sPendingTransitions.contains(sceneRoot)) {
+            Scene oldScene = Scene.getCurrentScene(sceneRoot);
             if (transition == null) {
+                // Notify old scene that it is being exited
+                if (oldScene != null) {
+                    oldScene.exit();
+                }
+
                 scene.enter();
             } else {
                 sPendingTransitions.add(sceneRoot);
@@ -177,7 +183,6 @@ public class TransitionManager {
                 Transition transitionClone = transition.clone();
                 transitionClone.setSceneRoot(sceneRoot);
 
-                Scene oldScene = Scene.getCurrentScene(sceneRoot);
                 if (oldScene != null && oldScene.isCreatedFromLayoutResource()) {
                     transitionClone.setCanRemoveViews(true);
                 }