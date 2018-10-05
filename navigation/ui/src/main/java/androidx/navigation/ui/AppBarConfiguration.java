5fd27fdcff532538fbc99f2932e318f9bd600910
Ian Lake <ilake@google.com>
2018-10-05 13:37:26 -0700

    Allow a set of top level destinations in NavigationUI
    Rather than only allowing the start destination
    of the graph to be a top level destination (i.e.,
    a destination where the Up button does not appear),
    allow developers to specify exactly what destinations
    they want to appear as top level destinations.
    
    The typical use cases include:
    - all destinations in a navigation drawer
    - all destinations in a bottom nav
    - The start destination of a mandatory sign in flow
    
    The default is still the start destination of your
    navigation graph as the only top level destination.
    
    Test: new AppBarConfigurationTest, changes to testapp
    BUG: 117333663
    Change-Id: Iac8773cf0d5ddcd258a9029a51c6f6735d800384

--- navigation/ui/src/main/java/androidx/navigation/ui/AppBarConfiguration.java
+++ navigation/ui/src/main/java/androidx/navigation/ui/AppBarConfiguration.java
@@ -20,6 +20,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 
+import androidx.navigation.NavGraph;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Configuration options for {@link NavigationUI} methods that interact with implementations of the
  * app bar pattern such as {@link android.support.v7.widget.Toolbar},
@@ -28,12 +33,27 @@ import android.support.v4.widget.DrawerLayout;
  */
 public class AppBarConfiguration {
 
+    @NonNull
+    private final Set<Integer> mTopLevelDestinations;
     @Nullable
     private final DrawerLayout mDrawerLayout;
 
     @SuppressWarnings("WeakerAccess") /* synthetic access */
-    AppBarConfiguration(@Nullable DrawerLayout drawerLayout) {
+    AppBarConfiguration(@NonNull Set<Integer> topLevelDestinations,
+            @Nullable DrawerLayout drawerLayout) {
+        mTopLevelDestinations = topLevelDestinations;
         mDrawerLayout = drawerLayout;
+    }
+
+    /**
+     * The set of destinations by id considered at the top level of your information hierarchy.
+     * The Up button will not be displayed when on these destinations.
+     *
+     * @return The set of top level destinations by id.
+     */
+    @NonNull
+    public Set<Integer> getTopLevelDestinations() {
+        return mTopLevelDestinations;
     }
 
     /**
@@ -50,8 +70,50 @@ public class AppBarConfiguration {
      * The Builder class for constructing new {@link AppBarConfiguration} instances.
      */
     public static class Builder {
+        @NonNull
+        private final Set<Integer> mTopLevelDestinations = new HashSet<>();
+
         @Nullable
         private DrawerLayout mDrawerLayout;
+
+        /**
+         * Create a new Builder whose only top level destination is the start destination
+         * of the given {@link NavGraph}. The Up button will not be displayed when on the
+         * start destination of the graph.
+         *
+         * @param navGraph The NavGraph whose start destination should be considered the only
+         *                 top level destination. The Up button will not be displayed when on the
+         *                 start destination of the graph.
+         */
+        public Builder(@NonNull NavGraph navGraph) {
+            mTopLevelDestinations.add(NavigationUI.findStartDestination(navGraph).getId());
+        }
+
+        /**
+         * Create a new Builder with a specific set of top level destinations. The Up button will
+         * not be displayed when on these destinations.
+         *
+         * @param topLevelDestinationIds The set of destinations by id considered at the top level
+         *                               of your information hierarchy. The Up button will not be
+         *                               displayed when on these destinations.
+         */
+        public Builder(@NonNull int... topLevelDestinationIds) {
+            for (int destinationId : topLevelDestinationIds) {
+                mTopLevelDestinations.add(destinationId);
+            }
+        }
+
+        /**
+         * Create a new Builder with a specific set of top level destinations. The Up button will
+         * not be displayed when on these destinations.
+         *
+         * @param topLevelDestinationIds The set of destinations by id considered at the top level
+         *                               of your information hierarchy. The Up button will not be
+         *                               displayed when on these destinations.
+         */
+        public Builder(@NonNull Set<Integer> topLevelDestinationIds) {
+            mTopLevelDestinations.addAll(topLevelDestinationIds);
+        }
 
         /**
          * Display the Navigation button as a drawer symbol when it is not being shown as an
@@ -72,7 +134,7 @@ public class AppBarConfiguration {
          */
         @NonNull
         public AppBarConfiguration build() {
-            return new AppBarConfiguration(mDrawerLayout);
+            return new AppBarConfiguration(mTopLevelDestinations, mDrawerLayout);
         }
     }
 }
