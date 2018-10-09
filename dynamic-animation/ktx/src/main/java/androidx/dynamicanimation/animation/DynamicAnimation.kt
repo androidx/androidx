ee1aad3102e42a0b1d3d700ffed8c9cdad88b04b
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-10-09 16:35:17 +0530

    ktx module for dynamicanimation
    - Module setup
    - Extensions for dynamic animation API
    
    Extensions update
    
    - Removed extensions for instantiating SpringAnimation and attaching SpringForce.
    - Merged two extensions for creating SpringAnimation into one with default value for finalPosition.
    
    Test: Not done yet because extensions need to be finalise.
    
    Change-Id: I863f73500187e537e8bbb13d4d5236b5756afc96

---
+++ dynamic-animation/ktx/src/main/java/androidx/dynamicanimation/animation/DynamicAnimation.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.dynamicanimation.animation
+
+import android.view.View
+
+/**
+ * Creates [FlingAnimation] for view.
+ *
+ * @param property View property to be animated.
+ * @return [FlingAnimation]
+ */
+inline fun <K : View> K.flingAnimationOf(property: FloatPropertyCompat<K>): FlingAnimation {
+    return FlingAnimation(this, property)
+}
+
+/**
+ * Creates [SpringAnimation] for view.
+ * If finalPosition is not [Float.NaN] then create [SpringAnimation] with
+ * [SpringForce.mFinalPosition].
+ *
+ * @param property View property to be animated.
+ * @param finalPosition [SpringForce.mFinalPosition] Final position of spring.
+ * @return [SpringAnimation]
+ */
+inline fun <K : View> K.springAnimationOf(
+    property: FloatPropertyCompat<K>,
+    finalPosition: Float = Float.NaN
+): SpringAnimation {
+    return if (finalPosition.isNaN()) {
+        SpringAnimation(this, property)
+    } else {
+        SpringAnimation(this, property, finalPosition)
+    }
+}
+
+/**
+ * Updates or applies spring force properties like [SpringForce.mDampingRatio],
+ * [SpringForce.mFinalPosition] and stiffness on SpringAnimation.
+ *
+ * If [SpringAnimation.mSpring] is null in case [SpringAnimation] is created without final position
+ * it will be created and attached to [SpringAnimation]
+ *
+ * @param func lambda with receiver on [SpringForce]
+ * @return [SpringAnimation]
+ */
+inline fun SpringAnimation.withSpringForceProperties(
+    func: SpringForce.() -> Unit
+): SpringAnimation {
+    if (spring == null) {
+        spring = SpringForce()
+    }
+    spring.func()
+    return this
+}
