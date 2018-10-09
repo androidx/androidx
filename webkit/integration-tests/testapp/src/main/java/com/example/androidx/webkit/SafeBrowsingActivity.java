aosp-mirror/platform_frameworks_support

[WebView Support Library] initial demo app
This adds an initial demo app. This is a ListView which points to other
Activities (each of which may similarly point to other Activities, or
may actually exercise a feature).

So far, none of these activities exercise any WebView features, other
than the MainActivity which adds the WebView version to its title (using
WebViewCompat#getCurrentWebViewPackage).

This creates an empty SafeBrowsingActivity for the sake of populating
the top-level ListView with something. We'll later populate
SafeBrowsingActivity with a similar ListView for each specific Safe
Browsing edge case.

Bug: 78154299
Test: ./gradlew :webkit-demos:assembleDebug :webkit-demos:installDebug \
    && adb shell monkey -p "com.example.androidx.webkit" \
    -c android.intent.category.LAUNCHER 1

Change-Id: I67724ff344d96b2576366f039591a22dd4c1eb3b

samples/WebkitDemos/src/main/java/com/example/androidx/webkit/SafeBrowsingActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.webkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * An {@link Activity} to exercise Safe Browsing functionality.
+ */
+public class SafeBrowsingActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO(ntfschr): actually do something eventually.
+        setContentView(R.layout.activity_safe_browsing);
+    }
+}

androidx/androidx

webkit/integration-tests/testapp/src/main/java/com/example/androidx/webkit/SafeBrowsingActivity.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.webkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An {@link Activity} to exercise Safe Browsing functionality.
+ */
+public class SafeBrowsingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO(ntfschr): actually do something eventually.
+        setContentView(R.layout.activity_safe_browsing);
+        setTitle(R.string.safebrowsing_activity_title);
+        WebkitHelpers.appendWebViewVersionToTitle(this);
+    }
+}
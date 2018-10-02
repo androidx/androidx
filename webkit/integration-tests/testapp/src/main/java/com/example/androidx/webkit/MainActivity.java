29bbb1e602fa60abc9164c3898cc2cf1a43aeebb
Nate Fischer <ntfschr@google.com>
2018-10-01 19:23:43 -0700

    [WebView Support Library] initial demo app
    This adds an initial demo app. This is a ListView which points to other
    Activities (each of which may similarly point to other Activities, or
    may actually exercise a feature).
    
    So far, none of these activities exercise any WebView features, other
    than the MainActivity which adds the WebView version to its title (using
    WebViewCompat#getCurrentWebViewPackage).
    
    This creates an empty SafeBrowsingActivity for the sake of populating
    the top-level ListView with something. We'll later populate
    SafeBrowsingActivity with a similar ListView for each specific Safe
    Browsing edge case.
    
    Bug: 78154299
    Test: ./gradlew :webkit-demos:assembleDebug :webkit-demos:installDebug \
        && adb shell monkey -p "com.example.androidx.webkit" \
        -c android.intent.category.LAUNCHER 1
    
    Change-Id: I67724ff344d96b2576366f039591a22dd4c1eb3b

---
+++ webkit/integration-tests/testapp/src/main/java/com/example/androidx/webkit/MainActivity.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.webkit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An {@link Activity} for exercising various WebView functionality. This Activity is a {@link
+ * ListView} which starts other Activities, each of which may similarly be a ListView, or may
+ * actually exercise specific {@link android.webkit.WebView} features.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        WebkitHelpers.appendWebViewVersionToTitle(this);
+
+        final Context activityContext = this;
+        MenuListView listView = findViewById(R.id.top_level_list);
+        MenuListView.MenuItem[] menuItems = new MenuListView.MenuItem[] {
+                new MenuListView.MenuItem(
+                        getResources().getString(R.string.safebrowsing_activity_title),
+                        new Intent(activityContext, SafeBrowsingActivity.class)),
+        };
+        listView.setItems(menuItems);
+    }
+}
