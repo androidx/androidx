aosp-mirror/platform_frameworks_support

Add color space conversion extensions to colors
Test: androidx.core.graphics.ColorTest#colorConversion
Change-Id: I5477127989176e4e529bf6d0e25b6e3a49ac492e

core/ktx/src/androidTest/java/androidx/core/graphics/ColorTest.kt
@@ -125,5 +125,34 @@ class ColorTest {
         assertEquals(0.75f, a, 1e-2f)
     }
 
+    @SdkSuppress(minSdkVersion = 26)
+    @Test fun colorConversion() {
+        val bt2020 = ColorSpace.get(ColorSpace.Named.BT2020)
+
+        // Test color ints
+        val c1 = 0x7f102030 to ColorSpace.Named.BT2020
+        assertEquals(bt2020, c1.colorSpace)
+
+        val c2 = 0x7f102030 to bt2020
+        assertEquals(bt2020, c2.colorSpace)
+
+        // Test color longs
+        val colorLong =
+                Color.pack(1.0f, 0.0f, 0.0f, 1.0f, ColorSpace.get(ColorSpace.Named.DISPLAY_P3))
+        val c3 = colorLong to ColorSpace.Named.BT2020
+        assertEquals(bt2020, c3.colorSpace)
+
+        val c4 = colorLong to bt2020
+        assertEquals(bt2020, c4.colorSpace)
+
+        // Test colors
+        val color = Color.valueOf(colorLong)
+        val c5 = color to ColorSpace.Named.BT2020
+        assertEquals(bt2020, c5.colorSpace)
+
+        val c6 = color to bt2020
+        assertEquals(bt2020, c6.colorSpace)
+    }
+
     @Test fun stringToColorInt() = assertEquals(Color.GREEN, "#00ff00".toColorInt())
 }

core/ktx/src/main/java/androidx/core/graphics/Color.kt
@@ -345,6 +345,73 @@ inline val @receiver:ColorLong Long.isWideGamut get() = Color.isWideGamut(this)
 inline val @receiver:ColorLong Long.colorSpace: ColorSpace get() = Color.colorSpace(this)
 
 /**
+ * Converts the color int receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorInt, ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorInt Int.to(colorSpace: ColorSpace.Named) =
+        Color.convert(this, ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color int receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorInt, colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorInt Int.to(colorSpace: ColorSpace) = Color.convert(this, colorSpace)
+
+/**
+ * Converts the color long receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorLong, ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorLong Long.to(colorSpace: ColorSpace.Named) =
+        Color.convert(this, ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color long receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorLong, colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorLong Long.to(colorSpace: ColorSpace) =
+        Color.convert(this, colorSpace)
+
+/**
+ * Converts the color receiver to a color in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * myColor.convert(ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+inline infix fun Color.to(colorSpace: ColorSpace.Named) = convert(ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color receiver to a color in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * myColor.convert(colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+inline infix fun Color.to(colorSpace: ColorSpace) = convert(colorSpace)
+
+/**
  * Return a corresponding [Int] color of this [String].
  *
  * Supported formats are:

androidx/androidx

core/ktx/src/androidTest/java/androidx/core/graphics/ColorTest.kt
@@ -125,5 +125,34 @@ class ColorTest {
         assertEquals(0.75f, a, 1e-2f)
     }
 
+    @SdkSuppress(minSdkVersion = 26)
+    @Test fun colorConversion() {
+        val bt2020 = ColorSpace.get(ColorSpace.Named.BT2020)
+
+        // Test color ints
+        val c1 = 0x7f102030 convertTo ColorSpace.Named.BT2020
+        assertEquals(bt2020, c1.colorSpace)
+
+        val c2 = 0x7f102030 convertTo bt2020
+        assertEquals(bt2020, c2.colorSpace)
+
+        // Test color longs
+        val colorLong =
+                Color.pack(1.0f, 0.0f, 0.0f, 1.0f, ColorSpace.get(ColorSpace.Named.DISPLAY_P3))
+        val c3 = colorLong convertTo ColorSpace.Named.BT2020
+        assertEquals(bt2020, c3.colorSpace)
+
+        val c4 = colorLong convertTo bt2020
+        assertEquals(bt2020, c4.colorSpace)
+
+        // Test colors
+        val color = Color.valueOf(colorLong)
+        val c5 = color convertTo ColorSpace.Named.BT2020
+        assertEquals(bt2020, c5.colorSpace)
+
+        val c6 = color convertTo bt2020
+        assertEquals(bt2020, c6.colorSpace)
+    }
+
     @Test fun stringToColorInt() = assertEquals(Color.GREEN, "#00ff00".toColorInt())
 }

core/ktx/src/main/java/androidx/core/graphics/Color.kt
@@ -345,6 +345,74 @@ inline val @receiver:ColorLong Long.isWideGamut get() = Color.isWideGamut(this)
 inline val @receiver:ColorLong Long.colorSpace: ColorSpace get() = Color.colorSpace(this)
 
 /**
+ * Converts the color int receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorInt, ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorInt Int.convertTo(colorSpace: ColorSpace.Named) =
+        Color.convert(this, ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color int receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorInt, colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorInt Int.convertTo(colorSpace: ColorSpace) =
+        Color.convert(this, colorSpace)
+
+/**
+ * Converts the color long receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorLong, ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorLong Long.convertTo(colorSpace: ColorSpace.Named) =
+        Color.convert(this, ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color long receiver to a color long in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * Color.convert(myColorLong, colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+@ColorLong
+inline infix fun @receiver:ColorLong Long.convertTo(colorSpace: ColorSpace) =
+        Color.convert(this, colorSpace)
+
+/**
+ * Converts the color receiver to a color in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * myColor.convert(ColorSpace.get(colorSpace))
+ * ```
+ */
+@RequiresApi(26)
+inline infix fun Color.convertTo(colorSpace: ColorSpace.Named) = convert(ColorSpace.get(colorSpace))
+
+/**
+ * Converts the color receiver to a color in the specified color space. This is
+ * equivalent to calling:
+ * ```
+ * myColor.convert(colorSpace)
+ * ```
+ */
+@RequiresApi(26)
+inline infix fun Color.convertTo(colorSpace: ColorSpace) = convert(colorSpace)
+
+/**
  * Return a corresponding [Int] color of this [String].
  *
  * Supported formats are: