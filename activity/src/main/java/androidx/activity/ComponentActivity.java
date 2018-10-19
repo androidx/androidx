3fc4bbd1b33a51d611ec5b517c1d69de4d4f82f8
Ian Lake <ilake@google.com>
2018-10-19 13:32:26 -0700

    Add a composable interface for ComponentActivity#onBackPressed
    Allow any class with a reference to a ComponentActivity
    to add/remove an OnBackPressedCallback interface for
    intercepting and handling the onBackPressed callback in
    the ComponentActivity.
    
    Each add call is associated with a LifecycleOwner (either
    implicitly with the Activity or explicitly) which ensures
    that callbacks are only called when the Lifecycle is started
    and ensures that the callbacks are automatically cleaned up if their
    associated Lifecycle is destroyed. This prevents memory leaks
    and makes it appropriate for use in child lifecycles such as
    those of a Fragment.
    
    Test: new ComponentActivityOnBackPressedTest tests
    BUG: 117990885
    Change-Id: I2b283b8a084b5ddec3801d645afb2c6bc5c02657

--- activity/src/main/java/androidx/activity/ComponentActivity.java
+++ activity/src/main/java/androidx/activity/ComponentActivity.java
@@ -29,6 +29,9 @@ import androidx.lifecycle.ReportFragment;
 import androidx.lifecycle.ViewModelStore;
 import androidx.lifecycle.ViewModelStoreOwner;
 
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * Base class for activities that enables composition of higher level components.
  * <p>
@@ -49,6 +52,10 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
 
     // Lazily recreated from NonConfigurationInstances by getViewModelStore()
     private ViewModelStore mViewModelStore;
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    final CopyOnWriteArrayList<LifecycleAwareOnBackPressedCallback> mOnBackPressedCallbacks =
+            new CopyOnWriteArrayList<>();
 
     public ComponentActivity() {
         getLifecycle().addObserver(new GenericLifecycleObserver() {
@@ -166,5 +173,155 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
             }
         }
         return mViewModelStore;
+    }
+
+    /**
+     * Called when the activity has detected the user's press of the back
+     * key. Any {@link OnBackPressedCallback} added via
+     * {@link #addOnBackPressedCallback(LifecycleOwner, OnBackPressedCallback)} will be given a
+     * chance to handle the back button before the default behavior of
+     * {@link android.app.Activity#onBackPressed()} is invoked.
+     *
+     * @see #addOnBackPressedCallback(LifecycleOwner, OnBackPressedCallback)
+     */
+    @Override
+    public void onBackPressed() {
+        for (OnBackPressedCallback onBackPressedCallback : mOnBackPressedCallbacks) {
+            if (onBackPressedCallback.handleOnBackPressed()) {
+                return;
+            }
+        }
+        // If none of the registered OnBackPressedCallbacks handled the back button,
+        // delegate to the super implementation
+        super.onBackPressed();
+    }
+
+    /**
+     * Add a new {@link OnBackPressedCallback}. Callbacks are invoked in order of recency, so
+     * this newly added {@link OnBackPressedCallback} will be the first callback to receive a
+     * callback if {@link #onBackPressed()} is called. Only if this callback returns
+     * <code>false</code> from its {@link OnBackPressedCallback#handleOnBackPressed()} will any
+     * previously added callback be called.
+     * <p>
+     * This is the equivalent of passing this {@link ComponentActivity} to
+     * {@link #addOnBackPressedCallback(LifecycleOwner, OnBackPressedCallback)} and ensures that
+     * the {@link OnBackPressedCallback#handleOnBackPressed()} callback will only be called
+     * if this {@link ComponentActivity} is at least {@link Lifecycle.State#STARTED}. You can
+     * remove the callback prior to the destruction of your activity by calling
+     * {@link #removeOnBackPressedCallback(OnBackPressedCallback)}.
+     *
+     * @param onBackPressedCallback The callback to add
+     *
+     * @see #onBackPressed()
+     * @see #removeOnBackPressedCallback(OnBackPressedCallback)
+     */
+    public void addOnBackPressedCallback(@NonNull OnBackPressedCallback onBackPressedCallback) {
+        addOnBackPressedCallback(this, onBackPressedCallback);
+    }
+
+    /**
+     * Add a new {@link OnBackPressedCallback}. Callbacks are invoked in order of recency, so
+     * this newly added {@link OnBackPressedCallback} will be the first callback to receive a
+     * callback if {@link #onBackPressed()} is called. Only if this callback returns
+     * <code>false</code> from its {@link OnBackPressedCallback#handleOnBackPressed()} will any
+     * previously added callback be called.
+     * <p>
+     * The {@link OnBackPressedCallback#handleOnBackPressed()} callback will only be called if the
+     * given {@link LifecycleOwner} is at least {@link Lifecycle.State#STARTED}. When the
+     * {@link LifecycleOwner} is {@link Lifecycle.State#DESTROYED destroyed}, it will automatically
+     * be removed from the list of callbacks. The only time you would need to manually call
+     * {@link #removeOnBackPressedCallback(OnBackPressedCallback)} is if you'd like to remove the
+     * callback prior to destruction of the associated lifecycle.
+     *
+     * @param owner The LifecycleOwner which controls when the callback should be invoked
+     * @param onBackPressedCallback The callback to add
+     *
+     * @see #onBackPressed()
+     * @see #removeOnBackPressedCallback(OnBackPressedCallback)
+     */
+    public void addOnBackPressedCallback(@NonNull LifecycleOwner owner,
+            @NonNull OnBackPressedCallback onBackPressedCallback) {
+        if (owner.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
+            // Already destroyed, nothing to do
+            return;
+        }
+        // Add new callbacks to the front of the list so that
+        // the most recently added callbacks get priority
+        mOnBackPressedCallbacks.add(0, new LifecycleAwareOnBackPressedCallback(
+                owner.getLifecycle(), onBackPressedCallback));
+    }
+
+    /**
+     * Remove a previously
+     * {@link #addOnBackPressedCallback(LifecycleOwner, OnBackPressedCallback) added}
+     * {@link OnBackPressedCallback} instance. The callback won't be called for any future
+     * {@link #onBackPressed()} calls, but may still receive a callback if this method is called
+     * during the dispatch of an ongoing {@link #onBackPressed()} call.
+     * <p>
+     * This call is usually not necessary as callbacks will be automatically removed when their
+     * associated {@link LifecycleOwner} is {@link Lifecycle.State#DESTROYED destroyed}.
+     *
+     * @param onBackPressedCallback The callback to remove
+     * @see #addOnBackPressedCallback(LifecycleOwner, OnBackPressedCallback)
+     */
+    public void removeOnBackPressedCallback(@NonNull OnBackPressedCallback onBackPressedCallback) {
+        Iterator<LifecycleAwareOnBackPressedCallback> iterator =
+                mOnBackPressedCallbacks.iterator();
+        LifecycleAwareOnBackPressedCallback callbackToRemove = null;
+        while (iterator.hasNext()) {
+            LifecycleAwareOnBackPressedCallback callback = iterator.next();
+            if (callback.getOnBackPressedCallback().equals(onBackPressedCallback)) {
+                callbackToRemove = callback;
+                break;
+            }
+        }
+        if (callbackToRemove != null) {
+            callbackToRemove.onRemoved();
+            mOnBackPressedCallbacks.remove(callbackToRemove);
+        }
+    }
+
+    private class LifecycleAwareOnBackPressedCallback implements
+            OnBackPressedCallback,
+            GenericLifecycleObserver {
+        private final Lifecycle mLifecycle;
+        private final OnBackPressedCallback mOnBackPressedCallback;
+
+        LifecycleAwareOnBackPressedCallback(@NonNull Lifecycle lifecycle,
+                @NonNull OnBackPressedCallback onBackPressedCallback) {
+            mLifecycle = lifecycle;
+            mOnBackPressedCallback = onBackPressedCallback;
+            mLifecycle.addObserver(this);
+        }
+
+        Lifecycle getLifecycle() {
+            return mLifecycle;
+        }
+
+        OnBackPressedCallback getOnBackPressedCallback() {
+            return mOnBackPressedCallback;
+        }
+
+        @Override
+        public boolean handleOnBackPressed() {
+            if (mLifecycle.getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+                return mOnBackPressedCallback.handleOnBackPressed();
+            }
+            return false;
+        }
+
+        @Override
+        public void onStateChanged(LifecycleOwner source, Lifecycle.Event event) {
+            if (event == Lifecycle.Event.ON_DESTROY) {
+                synchronized (mOnBackPressedCallbacks) {
+                    mLifecycle.removeObserver(this);
+                    mOnBackPressedCallbacks.remove(this);
+                }
+            }
+        }
+
+        public void onRemoved() {
+            mLifecycle.removeObserver(this);
+        }
     }
 }
