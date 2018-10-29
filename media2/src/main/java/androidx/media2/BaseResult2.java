efc24726f0a8a47d18cd5a514d1a69bd33bfe0f6
Jaewan Kim <jaewan@google.com>
2018-10-29 18:04:18 +0900

    Clarify that PlayerResult#getCompletionTime() uses elapsedRealtime()
    Bug: 118402866
    Test: $ ./gradlew media2:check
    Change-Id: I721d154759e5faa30ccb9357c2245d64228dd6c0

--- media2/src/main/java/androidx/media2/BaseResult2.java
+++ media2/src/main/java/androidx/media2/BaseResult2.java
@@ -78,6 +78,11 @@ interface BaseResult2 {
     int getResultCode();
 
     // Subclasses should write its own documentation.
+    // Should use SystemClock#elapsedRealtime() instead of System#currentTimeMillis() because
+    //    1. System#currentTimeMillis() can be unexpectedly set by System#setCurrentTimeMillis() or
+    //       changes in the timezone. So receiver cannot know when the command is finished.
+    //    2. For matching the timestamp with the PlaybackState(Compat) which uses
+    //       SystemClock#elapsedRealtime().
     long getCompletionTime();
 
     // Subclasses should write its own documentation.
