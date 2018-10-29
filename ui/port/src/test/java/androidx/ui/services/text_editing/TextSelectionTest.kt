142eacd31ae8a6d0a7c5e3baaedea2c02bf37e3e
Haoyu Zhang <haoyuchang@google.com>
2018-10-29 14:41:21 -0700

    Update class TextSelection
    Bug: 113814835
    Test: ./gradlew ui-port:test
    Change-Id: I4af60624c743912a575fa889aa47e832c3802d69

---
+++ ui/port/src/test/java/androidx/ui/services/text_editing/TextSelectionTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.services.text_editing
+
+import androidx.ui.engine.text.TextAffinity
+import androidx.ui.engine.text.TextPosition
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class TextSelectionTest {
+    @Test
+    fun collapsed() {
+        val offset = 10
+        val textAffinity = TextAffinity.upstream
+        val textSelection = TextSelection.collapsed(offset, textAffinity)
+
+        assertEquals(offset, textSelection.baseOffset)
+        assertEquals(offset, textSelection.extentOffset)
+        assertEquals(textAffinity, textSelection.affinity)
+        assertEquals(false, textSelection.isDirectional)
+    }
+
+    @Test
+    fun fromPosition() {
+        val offset = 20
+        val textAffinity = TextAffinity.downstream
+        val pos = TextPosition(offset, textAffinity)
+        val textSelection = TextSelection.fromPosition(pos)
+
+        assertEquals(offset, textSelection.baseOffset)
+        assertEquals(offset, textSelection.extentOffset)
+        assertEquals(textAffinity, textSelection.affinity)
+        assertEquals(false, textSelection.isDirectional)
+    }
+}
