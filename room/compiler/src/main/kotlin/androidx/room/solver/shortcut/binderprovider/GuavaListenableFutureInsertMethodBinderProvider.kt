c6ef3e2ebec135a8b0a1495fee98e2a70b454486
Daniel Santiago Rivera <danysantiago@google.com>
2018-11-12 11:42:26 -0800

    Support ListenableFuture<T> as return type for shortcut methods.
    Created method binder providers and method binder for supporting
    ListenableFuture as return types in DAO methods with @Insert, @Update
    and @Delete.
    
    Bug: 119418331
    Test: ./gradlew :room:integration-tests:testapp:assembleAndroidTest
    Change-Id: I1f1e36e6e7fad68b035df81587b3bee97f2c3d1d

---
+++ room/compiler/src/main/kotlin/androidx/room/solver/shortcut/binderprovider/GuavaListenableFutureInsertMethodBinderProvider.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.shortcut.binderprovider
+
+import androidx.room.ext.GuavaUtilConcurrentTypeNames
+import androidx.room.ext.RoomGuavaTypeNames
+import androidx.room.ext.typeName
+import androidx.room.processor.Context
+import androidx.room.processor.ProcessorErrors
+import androidx.room.solver.shortcut.binder.GuavaListenableFutureInsertMethodBinder
+import androidx.room.solver.shortcut.binder.InsertMethodBinder
+import androidx.room.vo.ShortcutQueryParameter
+import javax.lang.model.type.DeclaredType
+
+/**
+ * Provider for Guava ListenableFuture binders.
+ */
+class GuavaListenableFutureInsertMethodBinderProvider(
+    private val context: Context
+) : InsertMethodBinderProvider {
+
+    private val hasGuavaRoom by lazy {
+        context.processingEnv.elementUtils
+            .getTypeElement(RoomGuavaTypeNames.GUAVA_ROOM.toString()) != null
+    }
+
+    override fun matches(declared: DeclaredType): Boolean =
+        declared.typeArguments.size == 1 &&
+                context.processingEnv.typeUtils.erasure(declared).typeName() ==
+                GuavaUtilConcurrentTypeNames.LISTENABLE_FUTURE
+
+    override fun provide(
+        declared: DeclaredType,
+        params: List<ShortcutQueryParameter>
+    ): InsertMethodBinder {
+        if (!hasGuavaRoom) {
+            context.logger.e(ProcessorErrors.MISSING_ROOM_GUAVA_ARTIFACT)
+        }
+
+        val typeArg = declared.typeArguments.first()
+        val adapter = context.typeAdapterStore.findInsertAdapter(typeArg, params)
+        return GuavaListenableFutureInsertMethodBinder(typeArg, adapter)
+    }
+}
