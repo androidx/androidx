e9a77682aefb6bbf71ded4de0a759d2e8b5dfa44
Yuki Hamada <hummer@google.com>
2018-11-13 23:02:04 +0000

    Revert "Revert "Fix a crash bug in FragmentScenario#recreate.""
    This reverts commit a7e4ae221c330c4502154a8efdbcc4b78ca84109.
    
    Reason for revert: Revert the revert with resolving the merge conflict with change 812913.
    
    Test: ./gradlew :fragment-testing:connectedAndroidTest
    Change-Id: I61e06986b0da3c802a48ef90ac5d38cb20f166ac

---
+++ fragment/testing/src/androidTest/java/androidx/fragment/app/testing/NoDefaultConstructorFragment.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.fragment.app.testing;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentFactory;
+
+/**
+ * A minimum example fragment which doesn't have default constructor. You have to use
+ * {@link FragmentFactory} to instantiate this fragment.
+ */
+public class NoDefaultConstructorFragment extends StateRecordingFragment {
+
+    @NonNull private final String mConstructorArg;
+
+    public NoDefaultConstructorFragment(@NonNull String arg) {
+        this.mConstructorArg = arg;
+    }
+
+    @NonNull
+    public String getConstructorArg() {
+        return mConstructorArg;
+    }
+}
