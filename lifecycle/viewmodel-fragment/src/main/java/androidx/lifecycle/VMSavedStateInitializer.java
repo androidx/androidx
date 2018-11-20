d724640b0970071a5243b2cc3faaee09f24d5b0d
Sergey Vasilinets <sergeyv@google.com>
2018-11-20 14:06:33 +0000

    Split savedstate module to android / non-android modules
    "savedstate-common" module is android-free, so if in the future
    android-free replacement for Bundle happens, it will be possible to have
    savedstate module without knowledge about Bundle.
    
    "savedstate-bundle" module is android specfic.
    
    Also change package names:
    in savedstate-common: androidx.savedstate
    in savedstate-bundle: androidx.savedstate.bundle
    
    No changes to API in terms of adding / removing / modifying methods.
    
    Test: ./gradlew :activity:cC :fragment:cC
           :savedstate:savedstate-bundle:cC :savedstate:savedstate-common:cC
    Change-Id: I87bdc92e927362f06fcb4567237a3d9ff35d3da2

--- lifecycle/viewmodel-fragment/src/main/java/androidx/lifecycle/VMSavedStateInitializer.java
+++ lifecycle/viewmodel-fragment/src/main/java/androidx/lifecycle/VMSavedStateInitializer.java
@@ -25,6 +25,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.savedstate.SavedStateRegistry;
 
 class VMSavedStateInitializer implements Application.ActivityLifecycleCallbacks {
 
--- lifecycle/viewmodel-savedstate/src/main/java/androidx/lifecycle/SavedStateHandle.java
+++ lifecycle/viewmodel-savedstate/src/main/java/androidx/lifecycle/SavedStateHandle.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.savedstate.SavedStateRegistry;
 
 /**
  * A handle to saved state passed down to {@link ViewModel}
