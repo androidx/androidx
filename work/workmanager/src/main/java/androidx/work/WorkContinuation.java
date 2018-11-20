a10fa366e80f69aa2b571a4754bcb8971a0613b4
Sumir Kataria <sumir@google.com>
2018-11-20 14:43:13 -0800

    The Great Documentation Update, part 2: WorkContinuation.
    I'm not updating docs on a couple of methods because they will
    be removed.
    
    Test: genDocs.
    Change-Id: Id9cceac075a2435c2b349f9eccdc29f10922b294

--- work/workmanager/src/main/java/androidx/work/WorkContinuation.java
+++ work/workmanager/src/main/java/androidx/work/WorkContinuation.java
@@ -15,7 +15,9 @@
  */
 package androidx.work;
 
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
@@ -27,17 +29,49 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * An opaque class that allows you to chain together {@link OneTimeWorkRequest}.
+ * A class that allows you to chain together {@link OneTimeWorkRequest}s.  WorkContinuations allow
+ * the user to create arbitrary acyclic graphs of work dependencies.  You can add dependent work to
+ * a WorkContinuation by invoking {@link #then(OneTimeWorkRequest)} or its variants.  This returns a
+ * new WorkContinuation.
+ * <p>
+ * To construct more complex graphs, {@link WorkContinuation#combine(List)} or its
+ * variants can be used to return a WorkContinuation with the input WorkContinuations as
+ * prerequisites.  To create a graph like this:
+ *
+ * <pre>
+ *     A       C
+ *     |       |
+ *     B       D
+ *     |       |
+ *     +-------+
+ *         |
+ *         E    </pre>
+ *
+ * you would write the following:
+ *
+ * <pre>
+ * {@code
+ *  WorkContinuation left = workManager.beginWith(A).then(B);
+ *  WorkContinuation right = workManager.beginWith(C).then(D);
+ *  WorkContinuation final = WorkContinuation.combine(Arrays.asList(left, right));
+ *  final.enqueue();}</pre>
+ *
+ * Not that enqueuing a WorkContinuation enqueues all previously-unenqueued prerequisites.  You must
+ * call {@link #enqueue()} to inform WorkManager to actually enqueue the work graph.  As usual,
+ * enqueues are asynchronous - you can observe or block on the returned {@link Operation} if you
+ * need to be informed about its completion.
+ * <p>
+ * Because of the fluent nature of this class, its existence should be invisible in most cases.
  */
 public abstract class WorkContinuation {
 
     /**
      * Adds new {@link OneTimeWorkRequest} items that depend on the successful completion of
-     * all previously added {@link OneTimeWorkRequest}.
+     * all previously added {@link OneTimeWorkRequest}s.
      *
-     * @param work One or more {@link OneTimeWorkRequest} to add to the {@link WorkContinuation}
+     * @param work One or more {@link OneTimeWorkRequest}s to add as dependents
      * @return A {@link WorkContinuation} that allows for further chaining of dependent
-     *         {@link OneTimeWorkRequest}
+     *         {@link OneTimeWorkRequest}s
      */
     public final @NonNull WorkContinuation then(@NonNull OneTimeWorkRequest work) {
         return then(Collections.singletonList(work));
@@ -45,29 +79,31 @@ public abstract class WorkContinuation {
 
     /**
      * Adds new {@link OneTimeWorkRequest} items that depend on the successful completion
-     * of all previously added {@link OneTimeWorkRequest}.
+     * of all previously added {@link OneTimeWorkRequest}s.
      *
-     * @param work One or more {@link OneTimeWorkRequest} to add to the {@link WorkContinuation}
+     * @param work One or more {@link OneTimeWorkRequest} to add as dependents
      * @return A {@link WorkContinuation} that allows for further chaining of dependent
-     *         {@link OneTimeWorkRequest}
+     *         {@link OneTimeWorkRequest}s
      */
     public abstract @NonNull WorkContinuation then(@NonNull List<OneTimeWorkRequest> work);
 
     /**
-     * Returns a {@link LiveData} list of {@link WorkInfo} that provides information about work,
-     * their progress, and any resulting output.  If state or outputs of any of the jobs in this
-     * chain changes, any attached {@link android.arch.lifecycle.Observer}s will trigger.
+     * Returns a {@link LiveData} list of {@link WorkInfo}s that provide information about the
+     * status of each {@link OneTimeWorkRequest} in this {@link WorkContinuation}, as well as their
+     * prerequisites.  If the state or outputs of any of the work changes, any attached
+     * {@link android.arch.lifecycle.Observer}s will trigger.
      *
-     * @return A {@link LiveData} containing a list of {@link WorkInfo}es
+     * @return A {@link LiveData} containing a list of {@link WorkInfo}s; you must use
+     *         {@link LiveData#observe(LifecycleOwner, Observer)} to receive updates
      */
     public abstract @NonNull LiveData<List<WorkInfo>> getWorkInfosLiveData();
 
     /**
-     * Returns a {@link ListenableFuture} of a {@link List} of {@link WorkInfo} that provides
-     * information about work, their progress, and any resulting output in the
-     * {@link WorkContinuation}.
+     * Returns a {@link ListenableFuture} of a {@link List} of {@link WorkInfo}s that provides
+     * information about the status of each {@link OneTimeWorkRequest} in this
+     * {@link WorkContinuation}, as well as their prerequisites.
      *
-     * @return A {@link  ListenableFuture} of a {@link List} of {@link WorkInfo}es
+     * @return A {@link  ListenableFuture} of a {@link List} of {@link WorkInfo}s
      */
     public abstract @NonNull ListenableFuture<List<WorkInfo>> getWorkInfos();
 
@@ -79,7 +115,26 @@ public abstract class WorkContinuation {
     public abstract @NonNull Operation enqueue();
 
     /**
-     * Combines multiple {@link WorkContinuation}s to allow for complex chaining.
+     * Combines multiple {@link WorkContinuation}s as prerequisites for a new WorkContinuation to
+     * allow for complex chaining.  For example, to create a graph like this:
+     *
+     * <pre>
+     *     A       C
+     *     |       |
+     *     B       D
+     *     |       |
+     *     +-------+
+     *         |
+     *         E    </pre>
+     *
+     * you would write the following:
+     *
+     * <pre>
+     * {@code
+     *  WorkContinuation left = workManager.beginWith(A).then(B);
+     *  WorkContinuation right = workManager.beginWith(C).then(D);
+     *  WorkContinuation final = WorkContinuation.combine(left, right);
+     *  final.enqueue();}</pre>
      *
      * @param continuations Two or more {@link WorkContinuation}s that are prerequisites for the
      *                      return value
@@ -90,7 +145,26 @@ public abstract class WorkContinuation {
     }
 
     /**
-     * Combines multiple {@link WorkContinuation}s to allow for complex chaining.
+     * Combines multiple {@link WorkContinuation}s as prerequisites for a new WorkContinuation to
+     * allow for complex chaining.  For example, to create a graph like this:
+     *
+     * <pre>
+     *     A       C
+     *     |       |
+     *     B       D
+     *     |       |
+     *     +-------+
+     *         |
+     *         E    </pre>
+     *
+     * you would write the following:
+     *
+     * <pre>
+     * {@code
+     *  WorkContinuation left = workManager.beginWith(A).then(B);
+     *  WorkContinuation right = workManager.beginWith(C).then(D);
+     *  WorkContinuation final = WorkContinuation.combine(Arrays.asList(left, right));
+     *  final.enqueue();}</pre>
      *
      * @param continuations One or more {@link WorkContinuation}s that are prerequisites for the
      *                      return value
