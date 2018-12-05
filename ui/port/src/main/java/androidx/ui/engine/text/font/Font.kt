9077e14519e7badfb39e2e7ca0e03d6c43bdbaec
Siyamed Sinir <siyamed@google.com>
2018-12-05 15:15:57 -0800

    Add font config classes
    Test: gradle ui-port:test
    
    Bug: 113815795
    Change-Id: I17867c9a996d25a92f8778b54411e89a09d67edc

---
+++ ui/port/src/main/java/androidx/ui/engine/text/font/Font.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.engine.text.font
+
+import androidx.ui.engine.text.FontStyle
+import androidx.ui.engine.text.FontWeight
+
+data class Font(
+    val name: String,
+    // TODO(Migration/siyamed): add test
+    val weight: FontWeight = FontWeight.w400,
+    // TODO(Migration/siyamed): add test
+    val style: FontStyle = FontStyle.normal,
+    // TODO(Migration/siyamed): add test
+    val ttcIndex: Int = 0,
+    // TODO(Migration/siyamed): add test
+    val fontVariationSettings: String = ""
+) {
+    init {
+        assert(name.isNotEmpty()) { "Font name cannot be empty" }
+    }
+}
---
+++ ui/port/src/test/java/androidx/ui/engine/text/font/FontFamilyTest.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.ui.engine.text.font
+
+import androidx.ui.engine.text.FontStyle
+import androidx.ui.engine.text.FontWeight
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class FontFamilyTest {
+
+    @Test(expected = AssertionError::class)
+    fun `cannot be instantiated with empty font list`() {
+        FontFamily(listOf())
+    }
+
+    @Test
+    fun `two equal family declarations are equal`() {
+        val fontFamily = FontFamily(
+            Font(
+                name = "fontName",
+                weight = FontWeight.w900,
+                style = FontStyle.italic,
+                ttcIndex = 1,
+                fontVariationSettings = "'wdth' 150"
+            )
+        )
+
+        val otherFontFamily = FontFamily(
+            Font(
+                name = "fontName",
+                weight = FontWeight.w900,
+                style = FontStyle.italic,
+                ttcIndex = 1,
+                fontVariationSettings = "'wdth' 150"
+            )
+        )
+
+        assertThat(fontFamily).isEqualTo(otherFontFamily)
+    }
+
+    @Test
+    fun `two non equal family declarations are not equal`() {
+        val fontFamily = FontFamily(
+            Font(
+                name = "fontName",
+                weight = FontWeight.w900,
+                style = FontStyle.italic,
+                ttcIndex = 1,
+                fontVariationSettings = "'wdth' 150"
+            )
+        )
+
+        val otherFontFamily = FontFamily(
+            Font(
+                name = "fontName",
+                weight = FontWeight.w900,
+                style = FontStyle.italic,
+                ttcIndex = 1,
+                fontVariationSettings = "'wdth' 151" // this is different
+            )
+        )
+
+        assertThat(fontFamily).isNotEqualTo(otherFontFamily)
+    }
+}
