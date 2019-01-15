edf39633483f8cd1b482cbc8b3217cbad59f2e71
Clara Bayarri <clarabayarri@google.com>
2019-01-15 12:35:59 +0000

    Draw a checkbox
    This CL adds code to draw the three states of a checkbox
    Note it does not handle input or animations yet.
    
    Bug: 117213759
    Test: manual
    Change-Id: I20b0c08929ba164ddca65e39310f128dd8593e6f

--- samples/UiMaterialDemos/src/main/java/androidx/ui/material/demos/SelectionControlsActivity.kt
+++ samples/UiMaterialDemos/src/main/java/androidx/ui/material/demos/SelectionControlsActivity.kt
@@ -18,9 +18,19 @@ package androidx.ui.material.demos
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.ui.baseui.selection.ToggleableState
 import androidx.ui.core.CraneWrapper
+import androidx.ui.core.adapter.MeasureBox
+import androidx.ui.core.div
+import androidx.ui.core.dp
+import androidx.ui.core.hasBoundedHeight
+import androidx.ui.core.hasBoundedWidth
+import androidx.ui.core.min
+import androidx.ui.core.tightConstraints
 import androidx.ui.material.Checkbox
 import androidx.ui.material.MaterialTheme
+import androidx.ui.painting.Color
+import com.google.r4a.Composable
 import com.google.r4a.setContent
 
 open class SelectionControlsActivity : Activity() {
@@ -29,8 +39,44 @@ open class SelectionControlsActivity : Activity() {
         setContent {
             <CraneWrapper>
                 <MaterialTheme>
-                    <Checkbox />
+                    <TmpLayout />
                 </MaterialTheme>
             </CraneWrapper> }
     }
+}
+
+@Composable
+fun TmpLayout() {
+    <MeasureBox> constraints, measureOperations ->
+        val width = if (constraints.hasBoundedWidth) {
+            constraints.maxWidth
+        } else {
+            constraints.minWidth
+        }
+
+        val height = if (constraints.hasBoundedHeight) {
+            constraints.maxHeight
+        } else {
+            constraints.minHeight
+        }
+
+        val measurables = measureOperations.collect {
+            <Checkbox value=ToggleableState.CHECKED/>
+            <Checkbox value=ToggleableState.UNCHECKED/>
+            <Checkbox value=ToggleableState.INDETERMINATE/>
+            <Checkbox color=Color(0xffff0000.toInt()) />
+        }
+
+        val size = min(width, height)
+        val rectSize = size / 2
+        measureOperations.layout(size, size) {
+            val placeables = measurables.map {
+                it.measure(tightConstraints(rectSize, rectSize))
+            }
+            placeables[0].place(0.dp, 0.dp)
+            placeables[1].place(rectSize, 0.dp)
+            placeables[2].place(0.dp, rectSize)
+            placeables[3].place(rectSize, rectSize)
+        }
+    </MeasureBox>
 }
--- ui/material/src/main/java/androidx/ui/material/Checkbox.kt
+++ ui/material/src/main/java/androidx/ui/material/Checkbox.kt
@@ -17,31 +17,44 @@
 package androidx.ui.material
 
 import androidx.ui.baseui.selection.Toggleable
+import androidx.ui.baseui.selection.ToggleableState
 import androidx.ui.core.adapter.Draw
 import androidx.ui.core.adapter.MeasureBox
 import androidx.ui.core.dp
-import androidx.ui.engine.geometry.Rect
+import androidx.ui.core.min
+import androidx.ui.core.toPx
+import androidx.ui.engine.geometry.Offset
+import androidx.ui.engine.geometry.RRect
+import androidx.ui.engine.geometry.Radius
+import androidx.ui.engine.geometry.shrink
+import androidx.ui.painting.Canvas
 import androidx.ui.painting.Color
 import androidx.ui.painting.Paint
+import androidx.ui.painting.PaintingStyle
+import androidx.ui.painting.Path
 import com.google.r4a.Component
-import com.google.r4a.Composable
+import com.google.r4a.composer
 
 // TODO(clara): This should not be a class once R4A bug is fixed
 class Checkbox : Component() {
 
     // TODO(clara): remove this default
     var color: Color? = null
+    var value: ToggleableState = ToggleableState.CHECKED
 
     override fun compose() {
         <Toggleable>
+            val strokeWidthPx = strokeWidth.toPx(composer.composer.context)
             <MeasureBox> constraints, measureOperations ->
-                // TODO(clara): Use constraints
-                val calculatedWidth = 42.dp
                 measureOperations.collect {
                     <Colors.Consumer> colors ->
-                        <DrawCheckbox color=(color ?: colors.secondary)/>
+                        <DrawCheckbox color=(color ?: colors.secondary)
+                                      value
+                                      strokeWidth=strokeWidthPx/>
                     </Colors.Consumer>
                 }
+                val calculatedWidth = min(
+                    min(constraints.maxHeight, constraints.maxWidth), checkboxSize)
                 measureOperations.layout(calculatedWidth, calculatedWidth) {
                     // No children to place
                 }
@@ -50,12 +63,66 @@ class Checkbox : Component() {
     }
 }
 
-@Composable
-internal fun DrawCheckbox(color: Color) {
-    val paint = Paint()
-    paint.color = color
+internal fun DrawCheckbox(value: ToggleableState, color: Color, strokeWidth: Float) {
+    val radius = Radius.circular(radiusSize.toPx(composer.composer.context))
     <Draw> canvas, parentSize ->
-        canvas.drawRect(Rect(0f, 0f, parentSize.width, parentSize.height), paint)
-    // TODO(clara): Actually draw a checkbox
+        val outer = RRect(0f, 0f, parentSize.width, parentSize.height, radius)
+        if (value == ToggleableState.CHECKED) {
+            drawChecked(canvas, outer, color, strokeWidth)
+        } else if (value == ToggleableState.UNCHECKED) {
+            // TODO(clara): Where does this color come from?
+            drawUnchecked(canvas, outer, color, strokeWidth)
+        } else { // Indeterminate
+            drawIndeterminate(canvas, outer, color, strokeWidth)
+        }
     </Draw>
 }
+
+internal fun drawUnchecked(canvas: Canvas, outer: RRect, color: Color, strokeWidth: Float) {
+    val paint = Paint()
+    paint.strokeWidth = strokeWidth
+    paint.color = Color(0x42000000.toInt()) // grey for now
+    val inner = outer.shrink(paint.strokeWidth)
+    canvas.drawDRRect(outer, inner, paint)
+}
+
+internal fun drawChecked(canvas: Canvas, outer: RRect, color: Color, strokeWidth: Float) {
+    val paint = Paint()
+    paint.strokeWidth = strokeWidth
+    paint.color = color
+    // Background
+    canvas.drawRRect(outer, paint)
+
+    // Check
+    paint.color = Color(0xFFFFFFFF.toInt())
+    paint.style = PaintingStyle.stroke
+    val path = Path()
+    val width = outer.width
+    val start = Offset(width * 0.15f, width * 0.45f)
+    val mid = Offset(width * 0.4f, width * 0.7f)
+    val end = Offset(width * 0.85f, width * 0.25f)
+    path.moveTo(start.dx, start.dy)
+    path.lineTo(mid.dx, mid.dy)
+    path.lineTo(end.dx, end.dy)
+    canvas.drawPath(path, paint)
+}
+
+internal fun drawIndeterminate(canvas: Canvas, outer: RRect, color: Color, strokeWidth: Float) {
+    val paint = Paint()
+    paint.strokeWidth = strokeWidth
+    paint.color = color
+    // Background
+    canvas.drawRRect(outer, paint)
+
+    // Dash
+    paint.color = Color(0xFFFFFFFF.toInt())
+    val width = outer.width
+    val start = Offset(width * 0.2f, width * 0.5f)
+    val end = Offset(width * 0.8f, width * 0.5f)
+    canvas.drawLine(start, end, paint)
+}
+
+internal val checkboxSize = 24.dp
+// TODO(clara): I made these values up, not in spec
+internal val strokeWidth = 2.5.dp
+internal val radiusSize = 4.dp
