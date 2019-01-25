aosp-mirror/platform_frameworks_support

Deprecate OnConflictStrategy.ROLLBACK and FAIL
OnConflictStrategy.ROLLBACK was never working properly. On conflict, it
always fails with SQLiteException since Room tries to end the
transaction which has already been rolled back by the conflict. In
addition, framework has a bug in handling of transaction around ROLLBACK
(b/120397728), so it is impossible to make this work as expected.

Instead, ABORT has always worked like ROLLBACK. This is because the
INSERT throws SQLiteConstraintException on conflict, so the wrapping
transaction is ended without being marked as successful and thus rolled
back. We could change this to work more like SQLite's original ABORT
(keep preceding changes in the transaction), but it is too risky to
change the behavior of ABORT since it is the default option of @Insert
and @Update.  This CL just clarifies the behavior in Javadoc.

OnConflictStrategy.FAIL is no different from ABORT because of the
wrapping transaction. We should just remove it for clarity.

REPLACE and IGNORE are working fine.

Bug: 117266738
Test: OnConflictStrategyTest
Change-Id: Id62920d531afe4644d9d37ffc823a65132b54b4f

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/test/OnConflictStrategyTest.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.testapp.test;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.test.filters.MediumTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class OnConflictStrategyTest {
+
+    @Database(version = 1, entities = {Animal.class}, exportSchema = false)
+    public abstract static class OnConflictStrategyDatabase extends RoomDatabase {
+        abstract AnimalDao animal();
+    }
+
+    @Entity
+    public static class Animal {
+        @PrimaryKey
+        public final long id;
+        public final String name;
+
+        Animal(long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Dao
+    public interface AnimalDao {
+
+        @Insert(/* onConflict = OnConflictStrategy.ABORT */)
+        void insertOrAbort(Iterable<Animal> animals);
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        void insertOrReplace(Iterable<Animal> animals);
+
+        @Insert(onConflict = OnConflictStrategy.IGNORE)
+        void insertOrIgnore(Iterable<Animal> animals);
+
+        @Query("SELECT name FROM Animal")
+        List<String> allNames();
+    }
+
+    @Test
+    public void insertOrAbort() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        try {
+            db.animal().insertOrAbort(Arrays.asList(
+                    new Animal(1, "Dog"),
+                    new Animal(2, "Cat"),
+                    new Animal(2, "Bird"),
+                    new Animal(3, "Monkey")));
+            fail("Was expecting an exception");
+        } catch (SQLiteConstraintException e) {
+            assertThat(e.getMessage(), containsString("Animal.id"));
+        }
+        assertThat(db.animal().allNames(), are(/* empty */));
+    }
+
+    @Test
+    public void insertOrReplace() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        db.animal().insertOrReplace(Arrays.asList(
+                new Animal(1, "Dog"),
+                new Animal(2, "Cat"),
+                new Animal(2, "Bird"),
+                new Animal(3, "Monkey")));
+        assertThat(db.animal().allNames(), are("Dog", "Bird", "Monkey"));
+    }
+
+    @Test
+    public void insertOrIgnore() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        db.animal().insertOrIgnore(Arrays.asList(
+                new Animal(1, "Dog"),
+                new Animal(2, "Cat"),
+                new Animal(2, "Bird"),
+                new Animal(3, "Monkey")));
+        assertThat(db.animal().allNames(), are("Dog", "Cat", "Monkey"));
+    }
+
+    @SafeVarargs
+    private final <E> Matcher<Collection<E>> are(E... args) {
+        return allOf((Matcher<? super Collection<E>>) hasSize(args.length), hasItems(args));
+    }
+
+    private OnConflictStrategyDatabase openDatabase() {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        return Room.inMemoryDatabaseBuilder(context, OnConflictStrategyDatabase.class).build();
+    }
+
+}

androidx/androidx

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/test/OnConflictStrategyTest.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.testapp.test;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.test.filters.MediumTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class OnConflictStrategyTest {
+
+    @Database(version = 1, entities = {Animal.class}, exportSchema = false)
+    public abstract static class OnConflictStrategyDatabase extends RoomDatabase {
+        abstract AnimalDao animal();
+    }
+
+    @Entity
+    public static class Animal {
+        @PrimaryKey
+        public final long id;
+        public final String name;
+
+        Animal(long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Dao
+    public interface AnimalDao {
+
+        @Insert(/* onConflict = OnConflictStrategy.ABORT */)
+        void insertOrAbort(Iterable<Animal> animals);
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        void insertOrReplace(Iterable<Animal> animals);
+
+        @Insert(onConflict = OnConflictStrategy.IGNORE)
+        void insertOrIgnore(Iterable<Animal> animals);
+
+        @Query("SELECT name FROM Animal")
+        List<String> allNames();
+    }
+
+    @Test
+    public void insertOrAbort() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        try {
+            db.animal().insertOrAbort(Arrays.asList(
+                    new Animal(1, "Dog"),
+                    new Animal(2, "Cat"),
+                    new Animal(2, "Bird"),
+                    new Animal(3, "Monkey")));
+            fail("Was expecting an exception");
+        } catch (SQLiteConstraintException e) {
+            assertThat(e.getMessage(), is(notNullValue()));
+        }
+        assertThat(db.animal().allNames(), are(/* empty */));
+    }
+
+    @Test
+    public void insertOrReplace() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        db.animal().insertOrReplace(Arrays.asList(
+                new Animal(1, "Dog"),
+                new Animal(2, "Cat"),
+                new Animal(2, "Bird"),
+                new Animal(3, "Monkey")));
+        assertThat(db.animal().allNames(), are("Dog", "Bird", "Monkey"));
+    }
+
+    @Test
+    public void insertOrIgnore() {
+        final OnConflictStrategyDatabase db = openDatabase();
+        db.animal().insertOrIgnore(Arrays.asList(
+                new Animal(1, "Dog"),
+                new Animal(2, "Cat"),
+                new Animal(2, "Bird"),
+                new Animal(3, "Monkey")));
+        assertThat(db.animal().allNames(), are("Dog", "Cat", "Monkey"));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <E> Matcher<Collection<E>> are(E... args) {
+        return allOf((Matcher<? super Collection<E>>) hasSize(args.length), hasItems(args));
+    }
+
+    private OnConflictStrategyDatabase openDatabase() {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        return Room.inMemoryDatabaseBuilder(context, OnConflictStrategyDatabase.class).build();
+    }
+
+}