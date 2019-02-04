3c819141d3be0a6194a6d02e5642942ac15d86af
Daniel Santiago Rivera <danysantiago@google.com>
2019-02-04 10:39:54 -0800

    Use JetBrains's Kotlin metadata library.
    Use kotlinx-metadata-jvm to read Kotlin's Metadata annotation
    information. This library has less of an API surface than kotlin-metdata
    meaning KotlinClassMetadataUtils only exposes what Room needs. However
    it does allows us to fix the JVM descriptors (JvmDescriptorUtils) that
    are needed to match annotation processing elements to kotlin metadata
    elements.
    
    Bug: 123767877
    Test: ./gradlew room:room-compiler:test \
                    room:integration-tests:room-testapp-kotlin:cC \
                    room:integration-tests:room-testapp:cC
    
    Change-Id: Iddbe3968865b32168144294e55765a77320a40da

--- room/integration-tests/kotlintestapp/src/androidTest/java/androidx/room/integration/kotlintestapp/dao/BooksDao.kt
+++ room/integration-tests/kotlintestapp/src/androidTest/java/androidx/room/integration/kotlintestapp/dao/BooksDao.kt
@@ -25,6 +25,7 @@ import androidx.room.RawQuery
 import androidx.room.Transaction
 import androidx.room.TypeConverters
 import androidx.room.Update
+import androidx.room.integration.kotlintestapp.vo.AnswerConverter
 import androidx.room.integration.kotlintestapp.vo.Author
 import androidx.room.integration.kotlintestapp.vo.Book
 import androidx.room.integration.kotlintestapp.vo.BookAuthor
@@ -45,7 +46,7 @@ import io.reactivex.Single
 import java.util.Date
 
 @Dao
-@TypeConverters(DateConverter::class)
+@TypeConverters(DateConverter::class, AnswerConverter::class)
 interface BooksDao {
 
     @Insert
@@ -330,4 +331,12 @@ interface BooksDao {
 
     @Query("SELECT dateOfBirth FROM author WHERE authorId = :authorId")
     suspend fun getAuthorDateOfBirths(authorId: String): Date
+
+    // see: b/123767877, suspend function with inner class as parameter issues.
+    @Query("SELECT 0 FROM book WHERE bookId = :param")
+    suspend fun getZero(param: AnswerConverter.Answer): Int
+
+    // see: b/123767877, suspend function with inner class as parameter issues.
+    @Query("SELECT 'YES' FROM book")
+    suspend fun getAnswer(): AnswerConverter.Answer
 }
