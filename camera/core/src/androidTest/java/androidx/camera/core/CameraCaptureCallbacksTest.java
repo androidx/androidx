e85de57582d2ad8c94bd45cfe9793c1fbf0b9f7d
Trevor McGuire <trevormcguire@google.com>
2019-02-15 20:20:24 -0800

    Rename *AndroidTest to *Test in camera tests
     Other support libraries don't use the *AndroidTest naming scheme, so
     they've been renamed for consistency.
    
    Bug: 123998662
    Test: Tests all still pass.
    Change-Id: I5ef04fd9c71bc79ee5973eaf91c40fed7b7bd28b

---
+++ camera/core/src/androidTest/java/androidx/camera/core/CameraCaptureCallbacksTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.core;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public final class CameraCaptureCallbacksTest {
+
+    @Test
+    public void comboCallbackInvokesConstituentCallbacks() {
+        CameraCaptureCallback callback0 = Mockito.mock(CameraCaptureCallback.class);
+        CameraCaptureCallback callback1 = Mockito.mock(CameraCaptureCallback.class);
+        CameraCaptureCallback comboCallback =
+                CameraCaptureCallbacks.createComboCallback(callback0, callback1);
+        CameraCaptureResult result = Mockito.mock(CameraCaptureResult.class);
+        CameraCaptureFailure failure = new CameraCaptureFailure(CameraCaptureFailure.Reason.ERROR);
+
+        comboCallback.onCaptureCompleted(result);
+        verify(callback0, times(1)).onCaptureCompleted(result);
+        verify(callback1, times(1)).onCaptureCompleted(result);
+
+        comboCallback.onCaptureFailed(failure);
+        verify(callback0, times(1)).onCaptureFailed(failure);
+        verify(callback1, times(1)).onCaptureFailed(failure);
+    }
+}
