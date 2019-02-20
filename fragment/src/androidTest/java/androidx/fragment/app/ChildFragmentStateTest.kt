ecc662b822cc374fbaf558d2f66cdcba46e9498b
jbwoods <jbwoods@google.com>
2019-02-20 14:58:59 -0800

    Convert ChildFragmentStateTest to Kotlin
    Test: ran tests in class
    BUG: 124515054
    Change-Id: I88c94f2cff4053590d44c0e907838422fa5e97bf

---
+++ fragment/src/androidTest/java/androidx/fragment/app/ChildFragmentStateTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.fragment.app
+
+import androidx.fragment.app.test.FragmentTestActivity
+import androidx.fragment.app.test.FragmentTestActivity.ParentFragment
+import androidx.test.annotation.UiThreadTest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.google.common.truth.Truth.assertThat
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ChildFragmentStateTest {
+    @get:Rule
+    var activityRule = ActivityTestRule(FragmentTestActivity::class.java)
+
+    @Test
+    @UiThreadTest
+    fun testChildFragmentOrdering() {
+        val parent = ParentFragment()
+        val fm = activityRule.activity.supportFragmentManager
+        fm.beginTransaction().add(parent, "parent").commit()
+        fm.executePendingTransactions()
+        assertThat(parent.wasAttachedInTime).isTrue()
+    }
+}
