7ecf5125dd0410c0a34fe100c3f95c116d049597
Louis Pullen-Freilich <lpf@google.com>
2019-02-25 18:52:05 +0000

    Initial Progress Indicators
    Adds determinate and indeterminate linear and circular indicators
    
    Bug: b/113857929
    Test: ProgressIndicatorTest
    Change-Id: Ibc07e83a1c3e26ae589015dc5b73ecfe5b150bac

---
+++ ui/material/integration-tests/material-demos/src/main/java/androidx/ui/material/demos/ProgressIndicatorActivity.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.material.demos
+
+import CircularProgressIndicator
+import LinearProgressIndicator
+import android.app.Activity
+import android.os.Bundle
+import android.os.Handler
+import androidx.ui.core.CraneWrapper
+import androidx.ui.layout.FlexColumn
+import androidx.ui.layout.MainAxisAlignment.Companion.SpaceEvenly
+import androidx.ui.layout.Row
+import androidx.ui.material.MaterialTheme
+import androidx.ui.painting.Color
+import androidx.ui.painting.Color.Companion.fromARGB
+import androidx.compose.Composable
+import androidx.compose.Model
+import androidx.compose.composer
+import androidx.compose.onActive
+import androidx.compose.onDispose
+import androidx.compose.setContent
+import androidx.compose.unaryPlus
+
+class ProgressIndicatorActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CraneWrapper {
+                MaterialTheme {
+                    ProgressIndicatorDemo()
+                }
+            }
+        }
+    }
+}
+
+@Model
+private class ProgressState {
+    var progress = 0f
+    var cycle = 1
+
+    fun generateColor(): Color {
+        return when (cycle) {
+            1 -> fromARGB(255, 255, 0, 0)
+            2 -> fromARGB(255, 0, 255, 0)
+            3 -> fromARGB(255, 0, 0, 255)
+            // unused
+            else -> fromARGB(255, 0, 0, 0)
+        }
+    }
+
+    fun start() {
+        handler.postDelayed(updateProgress, 400)
+    }
+
+    fun stop() {
+        handler.removeCallbacks(updateProgress)
+    }
+
+    var handler = Handler()
+    var updateProgress: Runnable = object : Runnable {
+        override fun run() {
+            if (progress == 1f) {
+                cycle++
+                if (cycle > 3) {
+                    cycle = 1
+                }
+                progress = 0f
+            } else {
+                progress += 0.25f
+            }
+            handler.postDelayed(this, 400)
+        }
+    }
+}
+
+@Composable
+private fun ProgressIndicatorDemo(state: ProgressState = ProgressState()) {
+
+    +onActive { state.start() }
+    +onDispose { state.stop() }
+
+    FlexColumn {
+        expanded(flex = 1f) {
+            Row(mainAxisAlignment = SpaceEvenly) {
+                // Determinate indicators
+                LinearProgressIndicator(progress = state.progress)
+                CircularProgressIndicator(progress = state.progress)
+            }
+            Row(mainAxisAlignment = SpaceEvenly) {
+                // Fancy colours!
+                LinearProgressIndicator(progress = (state.progress), color = state.generateColor())
+                CircularProgressIndicator(
+                    progress = (state.progress),
+                    color = state.generateColor()
+                )
+            }
+            Row(mainAxisAlignment = SpaceEvenly) {
+                // Indeterminate indicators
+                LinearProgressIndicator()
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
---
+++ ui/material/src/androidTest/java/androidx/ui/material/ProgressIndicatorTest.kt
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.ui.material
+
+import CircularProgressIndicator
+import LinearProgressIndicator
+import androidx.compose.Model
+import androidx.test.filters.LargeTest
+import androidx.ui.core.OnChildPositioned
+import androidx.ui.core.PxSize
+import androidx.ui.core.TestTag
+import androidx.ui.core.dp
+import androidx.ui.core.round
+import androidx.ui.core.withDensity
+import androidx.ui.layout.Container
+import androidx.ui.layout.DpConstraints
+import androidx.ui.test.android.AndroidUiTestRunner
+import androidx.ui.test.assertIsVisible
+import androidx.ui.test.assertValueEquals
+import androidx.ui.test.findByTag
+import com.google.common.truth.Truth
+import androidx.compose.composer
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@Model
+private class State {
+    var progress = 0f
+}
+
+@LargeTest
+@RunWith(JUnit4::class)
+class ProgressIndicatorTest : AndroidUiTestRunner() {
+
+    private val LargeConstraints = DpConstraints(maxWidth = 5000.dp, maxHeight = 5000.dp)
+
+    private val ExpectedLinearWidth = 240.dp
+    private val ExpectedLinearHeight = 4.dp
+
+    private val ExpectedCircularDiameter = 48.dp
+
+    @Test
+    fun determinateLinearProgressIndicator_Progress() {
+        val tag = "linear"
+        val state = State()
+
+        setMaterialContent {
+            TestTag(tag = tag) {
+                LinearProgressIndicator(progress = state.progress)
+            }
+        }
+
+        findByTag(tag)
+            .assertIsVisible()
+            .assertValueEquals("0.0")
+
+        runOnUiAndWaitForRecompose {
+            state.progress = 0.5f
+        }
+
+        findByTag(tag)
+            .assertIsVisible()
+            .assertValueEquals("0.5")
+    }
+
+    @Test
+    fun determinateLinearProgressIndicator_Size() {
+        var size: PxSize? = null
+        setMaterialContent {
+            Container(constraints = LargeConstraints) {
+                OnChildPositioned(onPositioned = { position ->
+                    size = position.size
+                }) {
+                    LinearProgressIndicator(progress = 0f)
+                }
+            }
+        }
+        withDensity(density) {
+            Truth.assertThat(size?.width?.round()).isEqualTo(ExpectedLinearWidth.toIntPx())
+            Truth.assertThat(size?.height?.round()).isEqualTo(ExpectedLinearHeight.toIntPx())
+        }
+    }
+
+    @Test
+    fun indeterminateLinearProgressIndicator_Size() {
+        var size: PxSize? = null
+        setMaterialContent {
+            Container(constraints = LargeConstraints) {
+                OnChildPositioned(onPositioned = { position ->
+                    size = position.size
+                }) {
+                    LinearProgressIndicator()
+                }
+            }
+        }
+        withDensity(density) {
+            Truth.assertThat(size?.width?.round()).isEqualTo(ExpectedLinearWidth.toIntPx())
+            Truth.assertThat(size?.height?.round()).isEqualTo(ExpectedLinearHeight.toIntPx())
+        }
+    }
+
+    @Test
+    fun determinateCircularProgressIndicator_Progress() {
+        val tag = "circular"
+        val state = State()
+
+        setMaterialContent {
+            TestTag(tag = tag) {
+                CircularProgressIndicator(progress = state.progress)
+            }
+        }
+
+        findByTag(tag)
+            .assertIsVisible()
+            .assertValueEquals("0.0")
+
+        runOnUiAndWaitForRecompose {
+            state.progress = 0.5f
+        }
+
+        findByTag(tag)
+            .assertIsVisible()
+            .assertValueEquals("0.5")
+    }
+
+    @Test
+    fun determinateCircularProgressIndicator_Size() {
+        var size: PxSize? = null
+        setMaterialContent {
+            Container(constraints = LargeConstraints) {
+                OnChildPositioned(onPositioned = { position ->
+                    size = position.size
+                }) {
+                    CircularProgressIndicator(progress = 0f)
+                }
+            }
+        }
+        withDensity(density) {
+            Truth.assertThat(size?.width?.round()).isEqualTo(ExpectedCircularDiameter.toIntPx())
+            Truth.assertThat(size?.height?.round()).isEqualTo(ExpectedCircularDiameter.toIntPx())
+        }
+    }
+
+    @Test
+    fun indeterminateCircularProgressIndicator_Size() {
+        var size: PxSize? = null
+        setMaterialContent {
+            Container(constraints = LargeConstraints) {
+                OnChildPositioned(onPositioned = { position ->
+                    size = position.size
+                }) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+        withDensity(density) {
+            Truth.assertThat(size?.width?.round()).isEqualTo(ExpectedCircularDiameter.toIntPx())
+            Truth.assertThat(size?.height?.round())
+                .isEqualTo(ExpectedCircularDiameter.toIntPx())
+        }
+    }
+}
