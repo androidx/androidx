aosp-mirror/platform_frameworks_support

Introduce SavedStateRegistry.runOnNextRecreation
It allows you to run "lambda" on a restart of your component
Test: ComponentActivityRunOnNextRecreateTest

Change-Id: I70a1ad574655937c9dfbc3cd4137183a86386967

activity/src/main/java/androidx/activity/ComponentActivity.java
@@ -61,7 +61,7 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
 
     private final LifecycleRegistry mLifecycleRegistry = new LifecycleRegistry(this);
     private final SavedStateRegistryMaster mSavedStateRegistryMaster =
-            new SavedStateRegistryMaster();
+            SavedStateRegistryMaster.create(this);
 
     // Lazily recreated from NonConfigurationInstances by getViewModelStore()
     private ViewModelStore mViewModelStore;

fragment/src/main/java/androidx/fragment/app/Fragment.java
@@ -247,7 +247,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
     @Nullable FragmentViewLifecycleOwner mViewLifecycleOwner;
     MutableLiveData<LifecycleOwner> mViewLifecycleOwnerLiveData = new MutableLiveData<>();
 
-    SavedStateRegistryMaster mSavedStateRegistryMaster = new SavedStateRegistryMaster();
+    SavedStateRegistryMaster mSavedStateRegistryMaster = SavedStateRegistryMaster.create(this);
 
     /**
      * {@inheritDoc}
@@ -1843,7 +1843,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
      */
     void initState() {
         initLifecycle();
-        mSavedStateRegistryMaster = new SavedStateRegistryMaster();
+        mSavedStateRegistryMaster = SavedStateRegistryMaster.create(this);
         mWho = UUID.randomUUID().toString();
         mAdded = false;
         mRemoving = false;

androidx/androidx

bug: 123650887

activity/src/main/java/androidx/activity/ComponentActivity.java
@@ -61,7 +61,7 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
 
     private final LifecycleRegistry mLifecycleRegistry = new LifecycleRegistry(this);
     private final SavedStateRegistryController mSavedStateRegistryController =
-            new SavedStateRegistryController();
+            SavedStateRegistryController.create(this);
 
     // Lazily recreated from NonConfigurationInstances by getViewModelStore()
     private ViewModelStore mViewModelStore;

fragment/src/main/java/androidx/fragment/app/Fragment.java
@@ -248,7 +248,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
     @Nullable FragmentViewLifecycleOwner mViewLifecycleOwner;
     MutableLiveData<LifecycleOwner> mViewLifecycleOwnerLiveData = new MutableLiveData<>();
 
-    SavedStateRegistryController mSavedStateRegistryController = new SavedStateRegistryController();
+    SavedStateRegistryController mSavedStateRegistryController;
 
     // Cache the ContentView layoutIds for Fragments.
     private static final HashMap<Class, Integer> sAnnotationIds = new HashMap<>();
@@ -434,6 +434,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
 
     private void initLifecycle() {
         mLifecycleRegistry = new LifecycleRegistry(this);
+        mSavedStateRegistryController = SavedStateRegistryController.create(this);
         if (Build.VERSION.SDK_INT >= 19) {
             mLifecycleRegistry.addObserver(new GenericLifecycleObserver() {
                 @Override
@@ -1851,7 +1852,6 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
      */
     void initState() {
         initLifecycle();
-        mSavedStateRegistryController = new SavedStateRegistryController();
         mWho = UUID.randomUUID().toString();
         mAdded = false;
         mRemoving = false;