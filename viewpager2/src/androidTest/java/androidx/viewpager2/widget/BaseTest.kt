a2cb4bc2e71308358df8c03d4d359c9ef3ab46a1
Jelle Fresen <jellefresen@google.com>
2019-03-06 18:59:54 +0000

    Add offscreenPageLimit to ViewPager2
    Uses calculateExtraLayoutSpace to add pages that are off the screen to
    the view hierarchy.
    
    Added OffscreenPageLimitTest to verify that the number of pages on
    either side of the current viewport corresponds to the
    offscreenPageLimit at all times.
    
    Also added a new method requestTransform(), similar to requestLayout(),
    that triggers the registered PageTransformer on demand.
    
    Test: ./gradlew viewpager2:connectedCheck
    Change-Id: I8c9b488bb0c49a04dd0a39b2f0d8d7ea66005cdc

--- viewpager2/src/androidTest/java/androidx/viewpager2/widget/BaseTest.kt
+++ viewpager2/src/androidTest/java/androidx/viewpager2/widget/BaseTest.kt
@@ -264,9 +264,10 @@ open class BaseTest {
     }
 
     fun Context.setAdapterSync(adapterProvider: AdapterProvider) {
-        val waitForRenderLatch = viewPager.addWaitForLayoutChangeLatch()
+        lateinit var waitForRenderLatch: CountDownLatch
 
-        runOnUiThread {
+        activityTestRule.runOnUiThread {
+            waitForRenderLatch = viewPager.addWaitForLayoutChangeLatch()
             viewPager.adapter = adapterProvider(activity)
         }
 
@@ -323,15 +324,6 @@ open class BaseTest {
         return latch
     }
 
-    val ViewPager2.pageSize: Int
-        get() {
-            return if (orientation == ORIENTATION_HORIZONTAL) {
-                measuredWidth - paddingLeft - paddingRight
-            } else {
-                measuredHeight - paddingTop - paddingBottom
-            }
-        }
-
     val ViewPager2.recyclerView: RecyclerView
         get() {
             return getChildAt(0) as RecyclerView
@@ -350,7 +342,11 @@ open class BaseTest {
      * 3. Expected text is displayed
      * 4. Internal activity state is valid (as per activity self-test)
      */
-    fun Context.assertBasicState(pageIx: Int, value: String = pageIx.toString()) {
+    fun Context.assertBasicState(
+        pageIx: Int,
+        value: String = pageIx.toString(),
+        performSelfCheck: Boolean = true
+    ) {
         assertThat<Int>(
             "viewPager.getCurrentItem() should return $pageIx",
             viewPager.currentItem, equalTo(pageIx)
@@ -360,7 +356,8 @@ open class BaseTest {
             matches(withText(value))
         )
 
-        if (viewPager.adapter is SelfChecking) {
+        // TODO(b/130153109): Wire offscreenPageLimit into FragmentAdapter, remove performSelfCheck
+        if (performSelfCheck && viewPager.adapter is SelfChecking) {
             (viewPager.adapter as SelfChecking).selfCheck()
         }
     }
