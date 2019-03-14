628cb530595c1d903295558965423e18deaa1f14
Andrey Kulikov <andreykulikov@google.com>
2019-03-14 16:12:49 +0000

    Fix Animations demo after gestures behavioral update.
    Now we need to have a MeasureBox inside PressGestureDetector, not outside.
    Also fixed an infinite loop if you wrap <Draw> inside <PressGestureDetector>.
    
    Bug: n/a
    Test: Sample app is working now
    Change-Id: Ie03b03725df20937bfdf8fdb4e7c44f1834ed701

--- ui/platform/src/main/java/androidx/ui/core/pointerinput/PointerInputEventProcessor.kt
+++ ui/platform/src/main/java/androidx/ui/core/pointerinput/PointerInputEventProcessor.kt
@@ -258,6 +258,10 @@ internal class PointerInputEventProcessor(val root: LayoutNode) {
                     hitTestOnDescendants(child, newOffset, hitPointerInputNodes)
                     child = null
                 }
+                else -> {
+                    // other nodes like DrawNode has no descendants. stopping the while loop.
+                    child = null
+                }
             }
         }
         return nodeHit
