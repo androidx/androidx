ec088a43ebfc8c32abfc9e631f08a50d1d3085b0
Daniel Santiago Rivera <danysantiago@google.com>
2019-03-19 08:51:53 -0700

    Use a sneaky throw in runInTransaction(Callable)
    Using's Java8 sneaky throw technique allows us to not have to wrap
    checked exceptions in RuntimeExceptions, meaning callers with exception
    catches don't need to unwrap the exception.
    
    Since this is only a Java8 compiler technique (bytecode doesn't
    distinguish between unchecked and checked exceptions), then we
    circumvent bumping Room's minSdk by simply jarring the unpublished
    room-common-java8 library.
    
    Bug: 128623748
    Test: SneakyThrowTests in API 15
    Change-Id: I1611bd776d1f3030b95ba57586d89a7d1742fbe8

---
+++ room/integration-tests/kotlintestapp/src/androidTest/java/androidx/room/integration/kotlintestapp/test/SneakyThrowTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.kotlintestapp.test
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.json.JSONException
+import org.json.JSONObject
+import org.junit.Assert.fail
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.Callable
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class SneakyThrowTest : TestDatabaseTest() {
+
+    @Test
+    fun testCheckedException() {
+        try {
+            database.runInTransaction(Callable<String> {
+                val json = JSONObject()
+                json.getString("key") // method declares that it throws JSONException
+            })
+            fail("runInTransaction should have thrown an exception")
+        } catch (ex: JSONException) {
+            // no-op on purpose
+        }
+    }
+}
