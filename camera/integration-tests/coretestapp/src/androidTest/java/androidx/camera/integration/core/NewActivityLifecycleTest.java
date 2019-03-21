05ca2ac5f210a154e9aba21bf65a61ba4cd0bdaf
weginlee <weginlee@google.com>
2019-03-21 15:10:57 +0800

    Refine AppLifecycleTest and remove UseCaseButtonsStressTest
        1. Refine the AppLifecycleTest to focus on 2 basic life cyle tests,
        and exit each test safely to make next test run successfully.
    
        2. Add NewActivityLifecycleTest to test new Activity life cycle. It
        will fail because of b/128299086 and keep this monitoring.
    
        3. Remove UseCaseButtonsStressTest. In current stage, the kind of
        test is not to the point for stability.
    
    patch set 2
        1. Rename AppLifeCycleTest to ExistingActivityLifecycleTest
        2. Rename 2 method names.
    
    Bug: 129022102
    Test: Manually verify tests 10x on Samsung J7 Prime and Pixel 3
    Change-Id: Ie99db4774976edad508704cec922d7d916be7332

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/NewActivityLifecycleTest.java
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/NewActivityLifecycleTest.java
@@ -27,7 +27,11 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.camera.integration.core.idlingresource.ElapsedTimeIdlingResource;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -36,17 +40,19 @@ import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.Until;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-// Test application lifecycle when using CameraX.
+// Test new activity lifecycle when using CameraX.
 @RunWith(AndroidJUnit4.class)
 @SmallTest
-public final class AppLifecycleTest {
+public final class NewActivityLifecycleTest {
     private static final String BASIC_SAMPLE_PACKAGE = "androidx.camera.integration.core";
     private static final int LAUNCH_TIMEOUT_MS = 5000;
+    private static final int IDLE_TIMEOUT_MS = 1000;
 
     private final UiDevice mDevice =
             UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
@@ -56,13 +62,13 @@ public final class AppLifecycleTest {
             .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
 
     @Rule
-    public GrantPermissionRule cameraPermissionRule =
+    public GrantPermissionRule mCameraPermissionRule =
             GrantPermissionRule.grant(android.Manifest.permission.CAMERA);
     @Rule
-    public GrantPermissionRule storagePermissionRule =
+    public GrantPermissionRule mStoragePermissionRule =
             GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
     @Rule
-    public GrantPermissionRule audioPermissionRule =
+    public GrantPermissionRule mAudioPermissionRule =
             GrantPermissionRule.grant(android.Manifest.permission.RECORD_AUDIO);
 
     @Before
@@ -71,17 +77,15 @@ public final class AppLifecycleTest {
         returnHomeScreen();
     }
 
-
-    // Test that the application starts and reaches displaying a TextureView.
-    @Test
-    public void startupAndDisplayTextureView() {
-        mContext.startActivity(mIntent);
-        waitUntilTextureViewIsReady();
+    @After
+    public void tearDown() {
+        returnHomeScreen();
     }
 
-    // Run the app, press home, then restart the app, always creating new instance.
+    // Starts the activity, returns to the home screen to pause the activity, starts the activity
+    // with a flag which generates the new instance of the activity.
     @Test
-    public void startCoreTestTwiceAlwaysNewInstance() {
+    public void startCoreTestTwiceAlwaysWithNewInstance() {
         mContext.startActivity(mIntent);
         waitUntilTextureViewIsReady();
 
@@ -92,35 +96,8 @@ public final class AppLifecycleTest {
         newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(newIntent);
         waitUntilTextureViewIsReady();
-    }
 
-    // Run the app, press home, then restart the app, clearing the previous task when restarting.
-    // The behavior is the same to simulate press back key.
-    @Test
-    public void startCoreTestTwiceClearingPreviousInstance() {
-        mContext.startActivity(mIntent);
-        waitUntilTextureViewIsReady();
-
-        returnHomeScreen();
-
-        // Clear out any previous instances.
-        mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        mContext.startActivity(mIntent);
-        waitUntilTextureViewIsReady();
-    }
-
-
-    // Run the app, press home, then restart the app, and in practically simulate app lifecycle
-    // state when pressing home key.
-    @Test
-    public void startCoreTestTwiceToSimulatePauseResume() {
-        mContext.startActivity(mIntent);
-        waitUntilTextureViewIsReady();
-
-        returnHomeScreen();
-
-        mContext.startActivity(mIntent);
-        waitUntilTextureViewIsReady();
+        waitForIdlingRegistryAndPressBackButton();
     }
 
     private void waitUntilTextureViewIsReady() {
@@ -131,6 +108,16 @@ public final class AppLifecycleTest {
     private void returnHomeScreen() {
         mDevice.pressHome();
         mDevice.wait(Until.hasObject(By.pkg(mLauncherPackageName).depth(0)), LAUNCH_TIMEOUT_MS);
+    }
+
+    private void waitForIdlingRegistryAndPressBackButton() {
+        IdlingResource idlingResource = new ElapsedTimeIdlingResource(IDLE_TIMEOUT_MS);
+        IdlingRegistry.getInstance().register(idlingResource);
+        Espresso.onIdle();
+        IdlingRegistry.getInstance().unregister(idlingResource);
+
+        // Finish the activity finally.
+        mDevice.pressBack();
     }
 
 }
