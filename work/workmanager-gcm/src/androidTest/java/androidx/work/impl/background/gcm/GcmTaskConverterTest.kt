aosp-mirror/platform_frameworks_support

GCMNetworkManager based scheduler
Test: Unit tests for task converstion.
      Ran integration tests.

Change-Id: I1c8be9a81d4937d44c77c8a1fc5c6b2d0fe5cacc

work/workmanager-gcm/src/androidTest/java/androidx/work/impl/background/gcm/GcmTaskConverterTest.kt
@@ -1,0 +1,214 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.work.impl.background.gcm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.filters.SmallTest
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.impl.WorkManagerImpl
+import androidx.work.impl.background.gcm.GcmTaskConverter.EXECUTION_WINDOW_SIZE
+import com.google.android.gms.gcm.Task
+import org.hamcrest.Matchers.greaterThan
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.spy
+import java.util.concurrent.TimeUnit
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class GcmTaskConverterTest {
+
+    lateinit var mTaskConverter: GcmTaskConverter
+
+    @Before
+    fun setUp() {
+        mTaskConverter = spy(GcmTaskConverter())
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay() {
+        val request = OneTimeWorkRequestBuilder<TestWorker>().build()
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay_withConstraints() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.METERED)
+            .setRequiresCharging(true)
+            .build()
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_CONNECTED)
+        assertEquals(task.requiresCharging, true)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay_withConstraints2() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_UNMETERED)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    fun testOneTimeRequest_hasInitialDelay() {
+        val initialDelay = 10L
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setInitialDelay(initialDelay, TimeUnit.SECONDS)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        val expected = request.workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    fun testOneTimeWorkRequest_backedOff() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>().build()
+        val workSpec = request.workSpec
+        workSpec.runAttemptCount = 1
+        val expected = workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_firstRun() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = PeriodicWorkRequestBuilder<TestWorker>(15L, TimeUnit.MINUTES)
+            .build()
+
+        val expected = request.workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+        val task = mTaskConverter.convert(request.workSpec)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_withFlex_firstRun() {
+        val request = PeriodicWorkRequestBuilder<TestWorker>(
+            15L, TimeUnit.MINUTES, 5, TimeUnit.MINUTES
+        ).build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertThat(task.windowStart, greaterThan(0L)) // should be in the future
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_withFlex_nextRun() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = PeriodicWorkRequestBuilder<TestWorker>(
+            15L, TimeUnit.MINUTES, 5, TimeUnit.MINUTES
+        ).build()
+
+        request.workSpec.periodStartTime = now
+        val expected = TimeUnit.MINUTES.toSeconds(15L)
+
+        val task = mTaskConverter.convert(request.workSpec)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, expected)
+        assertEquals(task.windowEnd, expected + EXECUTION_WINDOW_SIZE)
+    }
+
+    private fun offset(expected: Long, now: Long): Long {
+        val delta = Math.max(expected - now, 0)
+        return TimeUnit.SECONDS.convert(delta, TimeUnit.MILLISECONDS)
+    }
+}

work/workmanager-gcm/src/main/java/androidx/work/impl/background/gcm/GcmTaskConverter.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.work.impl.background.gcm;
+
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.impl.model.WorkSpec;
+
+import com.google.android.gms.gcm.OneoffTask;
+import com.google.android.gms.gcm.Task;
+
+
+/**
+ * Converts a {@link androidx.work.impl.model.WorkSpec} to a {@link Task}.
+ */
+public class GcmTaskConverter {
+
+    /**
+     * Determines the size of the execution window.
+     * windowEnd = windowStart + EXECUTION_WINDOW_SIZE
+     */
+    @VisibleForTesting
+    public static final long EXECUTION_WINDOW_SIZE = 5L;
+
+    OneoffTask convert(@NonNull WorkSpec workSpec) {
+        OneoffTask.Builder builder = new OneoffTask.Builder();
+        builder.setService(WorkManagerGcmService.class)
+                .setTag(workSpec.id)
+                .setUpdateCurrent(true)
+                .setPersisted(false);
+
+        // Next run time is in seconds.
+        long now = SECONDS.convert(now(), MILLISECONDS);
+        long nextRunTimeInSeconds = SECONDS.convert(workSpec.calculateNextRunTime(), MILLISECONDS);
+        // GCMNetworkManager needs the execution window to be relative from the present.
+        long offset = Math.max(nextRunTimeInSeconds - now, 0);
+        builder.setExecutionWindow(offset, offset + EXECUTION_WINDOW_SIZE);
+
+        applyConstraints(builder, workSpec);
+        return builder.build();
+    }
+
+
+    /**
+     * Returns the current time in milliseconds.
+     */
+    @VisibleForTesting
+    public long now() {
+        return System.currentTimeMillis();
+    }
+
+    private static Task.Builder applyConstraints(
+            @NonNull Task.Builder builder,
+            @NonNull WorkSpec workSpec) {
+
+        // Apply defaults
+        builder.setRequiresCharging(false);
+        builder.setRequiredNetwork(Task.NETWORK_STATE_ANY);
+
+        if (workSpec.hasConstraints()) {
+            Constraints constraints = workSpec.constraints;
+
+            // Network Constraints
+            NetworkType networkType = constraints.getRequiredNetworkType();
+            switch (networkType) {
+                case METERED:
+                case NOT_ROAMING:
+                case CONNECTED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_CONNECTED);
+                    break;
+                case UNMETERED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_UNMETERED);
+                    break;
+                case NOT_REQUIRED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_ANY);
+            }
+
+            // Charging constraints
+            if (constraints.requiresCharging()) {
+                builder.setRequiresCharging(true);
+            } else {
+                builder.setRequiresCharging(false);
+            }
+
+            // No direct support for requires battery not low, and requires storage not low.
+            // Using ConstraintTrackingWorker for such use cases.
+
+        }
+
+        return builder;
+    }
+}

androidx/androidx

work/workmanager-gcm/src/androidTest/java/androidx/work/impl/background/gcm/GcmTaskConverterTest.kt
@@ -1,0 +1,216 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.work.impl.background.gcm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.filters.SmallTest
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.impl.WorkManagerImpl
+import androidx.work.impl.background.gcm.GcmTaskConverter.EXECUTION_WINDOW_SIZE_IN_SECONDS
+import com.google.android.gms.gcm.Task
+import org.hamcrest.Matchers.greaterThan
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.spy
+import java.util.concurrent.TimeUnit
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class GcmTaskConverterTest {
+
+    lateinit var mTaskConverter: GcmTaskConverter
+
+    @Before
+    fun setUp() {
+        mTaskConverter = spy(GcmTaskConverter())
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay() {
+        val request = OneTimeWorkRequestBuilder<TestWorker>().build()
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay_withConstraintNetworkConnected() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.METERED)
+            .setRequiresCharging(true)
+            .build()
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_CONNECTED)
+        assertEquals(task.requiresCharging, true)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    fun testOneTimeRequest_noInitialDelay_withConstraintNetworkUnMetered() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_UNMETERED)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, 0L)
+        assertEquals(task.windowEnd, 0L + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    fun testOneTimeRequest_hasInitialDelay() {
+        val initialDelay = 10L
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setInitialDelay(initialDelay, TimeUnit.SECONDS)
+            .build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        val expected = request.workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    fun testOneTimeWorkRequest_backedOff() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = OneTimeWorkRequestBuilder<TestWorker>()
+            .setInitialRunAttemptCount(1)
+            .build()
+
+        val workSpec = request.workSpec
+        val expected = workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_firstRun() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = PeriodicWorkRequestBuilder<TestWorker>(15L, TimeUnit.MINUTES)
+            .build()
+
+        val expected = request.workSpec.calculateNextRunTime()
+        val offset = offset(expected, now)
+        val task = mTaskConverter.convert(request.workSpec)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, offset)
+        assertEquals(task.windowEnd, offset + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_withFlex_firstRun() {
+        val request = PeriodicWorkRequestBuilder<TestWorker>(
+            15L, TimeUnit.MINUTES, 5, TimeUnit.MINUTES
+        ).build()
+
+        val task = mTaskConverter.convert(request.workSpec)
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertThat(task.windowStart, greaterThan(0L)) // should be in the future
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = WorkManagerImpl.MAX_PRE_JOB_SCHEDULER_API_LEVEL)
+    fun testPeriodicWorkRequest_withFlex_nextRun() {
+        val now = System.currentTimeMillis()
+        `when`(mTaskConverter.now()).thenReturn(now)
+
+        val request = PeriodicWorkRequestBuilder<TestWorker>(
+            15L, TimeUnit.MINUTES, 5, TimeUnit.MINUTES
+        ).build()
+
+        request.workSpec.periodStartTime = now
+        val expected = TimeUnit.MINUTES.toSeconds(15L)
+
+        val task = mTaskConverter.convert(request.workSpec)
+
+        assertEquals(task.serviceName, WorkManagerGcmService::class.java.name)
+        assertEquals(task.isPersisted, false)
+        assertEquals(task.isUpdateCurrent, true)
+        assertEquals(task.requiredNetwork, Task.NETWORK_STATE_ANY)
+        assertEquals(task.requiresCharging, false)
+        assertEquals(task.windowStart, expected)
+        assertEquals(task.windowEnd, expected + EXECUTION_WINDOW_SIZE_IN_SECONDS)
+    }
+
+    private fun offset(expected: Long, now: Long): Long {
+        val delta = Math.max(expected - now, 0)
+        return TimeUnit.SECONDS.convert(delta, TimeUnit.MILLISECONDS)
+    }
+}

work/workmanager-gcm/src/main/java/androidx/work/impl/background/gcm/GcmTaskConverter.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.work.impl.background.gcm;
+
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.impl.model.WorkSpec;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.OneoffTask;
+import com.google.android.gms.gcm.Task;
+
+
+/**
+ * Converts a {@link androidx.work.impl.model.WorkSpec} to a {@link Task}.
+ */
+public class GcmTaskConverter {
+
+    /**
+     * This is referring to the size of the execution window in seconds. {@link GcmNetworkManager}
+     * requires that we specify a window of time relative to {@code now} where a {@link Task}
+     * is eligible to run. This window is
+     * [{@code windowStart}, {@code windowStart + EXECUTION_WINDOW_SIZE_IN_SECONDS}) .
+     *
+     * {@code windowStart} is based on {@code workSpec.calculateNextRunTime()} .
+     */
+    @VisibleForTesting
+    public static final long EXECUTION_WINDOW_SIZE_IN_SECONDS = 5L;
+
+    OneoffTask convert(@NonNull WorkSpec workSpec) {
+        OneoffTask.Builder builder = new OneoffTask.Builder();
+        builder.setService(WorkManagerGcmService.class)
+                .setTag(workSpec.id)
+                .setUpdateCurrent(true)
+                .setPersisted(false);
+
+        // Next run time is in seconds.
+        long now = SECONDS.convert(now(), MILLISECONDS);
+        long nextRunTimeInSeconds = SECONDS.convert(workSpec.calculateNextRunTime(), MILLISECONDS);
+        // GCMNetworkManager needs the execution window to be relative from the present.
+        long offset = Math.max(nextRunTimeInSeconds - now, 0);
+        builder.setExecutionWindow(offset, offset + EXECUTION_WINDOW_SIZE_IN_SECONDS);
+
+        applyConstraints(builder, workSpec);
+        return builder.build();
+    }
+
+
+    /**
+     * Returns the current time in milliseconds.
+     */
+    @VisibleForTesting
+    public long now() {
+        return System.currentTimeMillis();
+    }
+
+    private static Task.Builder applyConstraints(
+            @NonNull Task.Builder builder,
+            @NonNull WorkSpec workSpec) {
+
+        // Apply defaults
+        builder.setRequiresCharging(false);
+        builder.setRequiredNetwork(Task.NETWORK_STATE_ANY);
+
+        if (workSpec.hasConstraints()) {
+            Constraints constraints = workSpec.constraints;
+
+            // Network Constraints
+            NetworkType networkType = constraints.getRequiredNetworkType();
+            switch (networkType) {
+                case METERED:
+                case NOT_ROAMING:
+                case CONNECTED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_CONNECTED);
+                    break;
+                case UNMETERED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_UNMETERED);
+                    break;
+                case NOT_REQUIRED:
+                    builder.setRequiredNetwork(Task.NETWORK_STATE_ANY);
+            }
+
+            // Charging constraints
+            if (constraints.requiresCharging()) {
+                builder.setRequiresCharging(true);
+            } else {
+                builder.setRequiresCharging(false);
+            }
+
+            // No direct support for requires battery not low, and requires storage not low.
+            // Using ConstraintTrackingWorker for such use cases.
+
+        }
+
+        return builder;
+    }
+}