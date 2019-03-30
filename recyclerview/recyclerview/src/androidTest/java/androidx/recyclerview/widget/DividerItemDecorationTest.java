eb269fbe4ccba7d34874fc26d24246221eb186ce
Alex Saveau <saveau.alexandre@gmail.com>
2019-03-29 19:15:11 -0700

    Add API to retrieve DividerItemDecoration drawable
    Test: N/A
    Fix: 124539354
    Change-Id: Icd47441595a4d384697d18e35c6e612b1b943c8e
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- recyclerview/recyclerview/src/androidTest/java/androidx/recyclerview/widget/DividerItemDecorationTest.java
+++ recyclerview/recyclerview/src/androidTest/java/androidx/recyclerview/widget/DividerItemDecorationTest.java
@@ -16,6 +16,9 @@
 
 package androidx.recyclerview.widget;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.ContextThemeWrapper;
@@ -50,6 +53,21 @@ public class DividerItemDecorationTest {
                 DividerItemDecoration.HORIZONTAL);
         rv.addItemDecoration(decoration);
         rv.layout(0, 0, 1000, 1000);
+        assertNull(decoration.getDrawable());
+        decoration.onDraw(new Canvas(), rv, null);
+    }
+
+    @Test
+    public void testNonNullListDivider() {
+        final Context context = ApplicationProvider.getApplicationContext();
+        RecyclerView rv = new RecyclerView(context);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new MyAdapter(STRINGS));
+        DividerItemDecoration decoration = new DividerItemDecoration(
+                context, DividerItemDecoration.HORIZONTAL);
+        rv.addItemDecoration(decoration);
+        rv.layout(0, 0, 1000, 1000);
+        assertNotNull(decoration.getDrawable());
         decoration.onDraw(new Canvas(), rv, null);
     }
 
