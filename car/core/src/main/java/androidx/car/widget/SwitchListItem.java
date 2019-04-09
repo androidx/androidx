f9a371311cfcafc31c0ce59ca2b0678632c9de32
Ram Parameswaran <rampara@google.com>
2019-04-09 16:04:35 -0700

    Deprecate setSwitchState() method which is superseded by setChecked().
    Bug: 130213082
    Test: ./gradlew :car:connectedCheck
    
    Change-Id: I549bef9c0f0acd8129a8fdd673196b3adf41bc73

--- car/core/src/main/java/androidx/car/widget/SwitchListItem.java
+++ car/core/src/main/java/androidx/car/widget/SwitchListItem.java
@@ -53,18 +53,18 @@ import java.util.List;
  *
  * <p>An item visually composes of 3 parts; each part may contain multiple views.
  * <ul>
- *     <li>{@code Primary Action}: represented by an icon of following types.
- *     <ul>
- *         <li>Primary Icon - icon size could be large or small.
- *         <li>No Icon - no icon is shown.
- *         <li>Empty Icon - {@code Text} offsets start space as if there was an icon.
- *     </ul>
- *     <li>{@code Text}: supports any combination of the following text views.
- *     <ul>
- *         <li>Title
- *         <li>Body
- *     </ul>
- *     <li>{@code Supplemental Action}: represented by {@link Switch}.
+ * <li>{@code Primary Action}: represented by an icon of following types.
+ * <ul>
+ * <li>Primary Icon - icon size could be large or small.
+ * <li>No Icon - no icon is shown.
+ * <li>Empty Icon - {@code Text} offsets start space as if there was an icon.
+ * </ul>
+ * <li>{@code Text}: supports any combination of the following text views.
+ * <ul>
+ * <li>Title
+ * <li>Body
+ * </ul>
+ * <li>{@code Supplemental Action}: represented by {@link Switch}.
  * </ul>
  *
  * <p>{@code SwitchListItem} binds data to {@link ViewHolder} based on components selected.
@@ -78,7 +78,8 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
     @IntDef({
             PRIMARY_ACTION_ICON_SIZE_SMALL, PRIMARY_ACTION_ICON_SIZE_MEDIUM,
             PRIMARY_ACTION_ICON_SIZE_LARGE})
-    private @interface PrimaryActionIconSize {}
+    private @interface PrimaryActionIconSize {
+    }
 
     /**
      * Small sized icon is the mostly commonly used size. It's the same as supplemental action icon.
@@ -99,7 +100,8 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
     @IntDef({
             PRIMARY_ACTION_TYPE_NO_ICON, PRIMARY_ACTION_TYPE_EMPTY_ICON,
             PRIMARY_ACTION_TYPE_ICON})
-    private @interface PrimaryActionType {}
+    private @interface PrimaryActionType {
+    }
 
     private static final int PRIMARY_ACTION_TYPE_NO_ICON = 0;
     private static final int PRIMARY_ACTION_TYPE_EMPTY_ICON = 1;
@@ -111,9 +113,11 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
 
     private final List<ViewBinder<ViewHolder>> mBinders = new ArrayList<>();
 
-    @PrimaryActionType private int mPrimaryActionType = PRIMARY_ACTION_TYPE_NO_ICON;
+    @PrimaryActionType
+    private int mPrimaryActionType = PRIMARY_ACTION_TYPE_NO_ICON;
     private Drawable mPrimaryActionIconDrawable;
-    @PrimaryActionIconSize private int mPrimaryActionIconSize = PRIMARY_ACTION_ICON_SIZE_SMALL;
+    @PrimaryActionIconSize
+    private int mPrimaryActionIconSize = PRIMARY_ACTION_ICON_SIZE_SMALL;
 
     private CharSequence mTitle;
     private CharSequence mBody;
@@ -202,9 +206,9 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
      * Sets {@code Primary Action} to be represented by an icon.
      *
      * @param drawable the Drawable to set.
-     * @param size small/medium/large. Available as {@link #PRIMARY_ACTION_ICON_SIZE_SMALL},
-     *             {@link #PRIMARY_ACTION_ICON_SIZE_MEDIUM},
-     *             {@link #PRIMARY_ACTION_ICON_SIZE_LARGE}.
+     * @param size     small/medium/large. Available as {@link #PRIMARY_ACTION_ICON_SIZE_SMALL},
+     *                 {@link #PRIMARY_ACTION_ICON_SIZE_MEDIUM},
+     *                 {@link #PRIMARY_ACTION_ICON_SIZE_LARGE}.
      */
     public void setPrimaryActionIcon(@NonNull Drawable drawable, @PrimaryActionIconSize int size) {
         mPrimaryActionType = PRIMARY_ACTION_TYPE_ICON;
@@ -217,9 +221,9 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
      * Sets {@code Primary Action} to be represented by an icon.
      *
      * @param iconResId the resource identifier of the drawable.
-     * @param size small/medium/large. Available as {@link #PRIMARY_ACTION_ICON_SIZE_SMALL},
-     *             {@link #PRIMARY_ACTION_ICON_SIZE_MEDIUM},
-     *             {@link #PRIMARY_ACTION_ICON_SIZE_LARGE}.
+     * @param size      small/medium/large. Available as {@link #PRIMARY_ACTION_ICON_SIZE_SMALL},
+     *                  {@link #PRIMARY_ACTION_ICON_SIZE_MEDIUM},
+     *                  {@link #PRIMARY_ACTION_ICON_SIZE_LARGE}.
      */
     public void setPrimaryActionIcon(@DrawableRes int iconResId, @PrimaryActionIconSize int size) {
         setPrimaryActionIcon(getContext().getDrawable(iconResId), size);
@@ -275,8 +279,19 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
      * Sets the state of {@code Switch}.
      *
      * @param isChecked sets the "checked/unchecked, namely on/off" state of switch.
+     * @deprecated Use {@link #setChecked(boolean)} to set checked state of {@code Switch}.
      */
+    @Deprecated
     public void setSwitchState(boolean isChecked) {
+        setChecked(isChecked);
+    }
+
+    /**
+     * Sets whether the switch is checked.
+     *
+     * @param isChecked {@code true} to check the switch or {@code false} to uncheck it.
+     */
+    public void setChecked(boolean isChecked) {
         if (mSwitchChecked == isChecked) {
             return;
         }
@@ -623,7 +638,7 @@ public class SwitchListItem extends ListItem<SwitchListItem.ViewHolder> {
 
             // Each line groups relevant child views in an effort to help keep this view array
             // updated with actual child views in the ViewHolder.
-            mWidgetViews = new View[] {
+            mWidgetViews = new View[]{
                     mPrimaryIcon,
                     mTitle, mBody,
                     mSwitch, mSwitchDivider,
