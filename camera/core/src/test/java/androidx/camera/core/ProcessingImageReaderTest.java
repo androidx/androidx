7d3d67dc8b050a3899c4be7c8e416b2e581ff69b
Trevor McGuire <trevormcguire@google.com>
2019-04-09 17:23:01 -0700

    Convert ProcessingImageReaderTest to Robolectric
     This was failing due to an unresolved class when running on device. We
     will need to watch out for more failures like this to ensure it is not
     just an instrumentation error.
    
     This doesn't need to run on device. Can run much faster in a
     robolectric test.
    
     Removed background thread/semaphore/sleep to run faster.
    
     Changed to @SmallTest.
    
    Test: ./gradlew bOS
    Change-Id: I9b934bd251bb75f6b0cf316c7c86536046e8b9d0

--- camera/core/src/test/java/androidx/camera/core/ProcessingImageReaderTest.java
+++ camera/core/src/test/java/androidx/camera/core/ProcessingImageReaderTest.java
@@ -18,38 +18,37 @@ package androidx.camera.core;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import static java.lang.Thread.sleep;
-
 import android.os.Handler;
-import android.os.HandlerThread;
+import android.os.Looper;
 import android.view.Surface;
 
 import androidx.camera.testing.fakes.FakeCaptureStage;
 import androidx.camera.testing.fakes.FakeImageInfo;
 import androidx.camera.testing.fakes.FakeImageProxy;
 import androidx.camera.testing.fakes.FakeImageReaderProxy;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.MediumTest;
+import androidx.test.filters.SmallTest;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.internal.DoNotInstrument;
+import org.robolectric.shadows.ShadowLooper;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
 
-@MediumTest
-@RunWith(AndroidJUnit4.class)
+@SmallTest
+@RunWith(RobolectricTestRunner.class)
+@DoNotInstrument
 public final class ProcessingImageReaderTest {
     private static final int CAPTURE_ID_0 = 0;
     private static final int CAPTURE_ID_1 = 1;
     private static final long TIMESTAMP_0 = 0L;
     private static final long TIMESTAMP_1 = 1000L;
-    private final CaptureStage mCaptureStage0 = new FakeCaptureStage(CAPTURE_ID_0, null);
-    private final CaptureStage mCaptureStage1 = new FakeCaptureStage(CAPTURE_ID_1, null);
-    private final FakeImageReaderProxy mImageReaderProxy = new FakeImageReaderProxy();
-    private final CaptureBundle mCaptureBundle = new CaptureBundle();
-    private final CaptureProcessor mCaptureProcessor = new CaptureProcessor() {
+    private static final CaptureProcessor NOOP_PROCESSOR = new CaptureProcessor() {
         @Override
         public void onOutputSurface(Surface surface, int imageFormat) {
 
@@ -60,22 +59,25 @@ public final class ProcessingImageReaderTest {
 
         }
     };
-    private final Semaphore mSemaphore = new Semaphore(0);
-    private ProcessingImageReader mProcessingImageReader;
-    private HandlerThread mBackgroundThread;
-    private Handler mBackgroundHandler;
+
+    private final CaptureStage mCaptureStage0 = new FakeCaptureStage(CAPTURE_ID_0, null);
+    private final CaptureStage mCaptureStage1 = new FakeCaptureStage(CAPTURE_ID_1, null);
+    private final FakeImageReaderProxy mImageReaderProxy = new FakeImageReaderProxy();
+    private CaptureBundle mCaptureBundle;
+    private Handler mMainHandler;
 
     @Before
     public void setUp() {
-        mBackgroundThread = new HandlerThread("CallbackThread");
-        mBackgroundThread.start();
-        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+        mMainHandler = new Handler(Looper.getMainLooper());
+        mCaptureBundle = new CaptureBundle();
+        mCaptureBundle.addCaptureStage(mCaptureStage0);
+        mCaptureBundle.addCaptureStage(mCaptureStage1);
     }
 
     @Test
-    public void canSetFuturesInSettableImageProxyBundle() throws InterruptedException {
-        mCaptureBundle.addCaptureStage(mCaptureStage0);
-        mCaptureBundle.addCaptureStage(mCaptureStage1);
+    public void canSetFuturesInSettableImageProxyBundle()
+            throws InterruptedException, TimeoutException, ExecutionException {
+        final AtomicReference<ImageProxyBundle> bundleRef = new AtomicReference<>();
         // Sets the callback from ProcessingImageReader to start processing
         CaptureProcessor captureProcessor = new CaptureProcessor() {
             @Override
@@ -85,28 +87,28 @@ public final class ProcessingImageReaderTest {
 
             @Override
             public void process(ImageProxyBundle bundle) {
-                try {
-                    // CaptureProcessor.process should be called once all ImageProxies on the
-                    // initial lists are ready. Then checks if the output has matched timestamp.
-                    assertThat(bundle.getImageProxy(CAPTURE_ID_0).get(0,
-                            TimeUnit.SECONDS).getTimestamp()).isEqualTo(TIMESTAMP_0);
-                    assertThat(bundle.getImageProxy(CAPTURE_ID_1).get(0,
-                            TimeUnit.SECONDS).getTimestamp()).isEqualTo(TIMESTAMP_1);
-                } catch (Exception e) {
-
-                }
-                mSemaphore.release();
+                bundleRef.set(bundle);
             }
         };
-        mProcessingImageReader = new ProcessingImageReader(
-                mImageReaderProxy, mBackgroundHandler, mCaptureBundle, captureProcessor);
+        new ProcessingImageReader(
+                mImageReaderProxy, mMainHandler, mCaptureBundle, captureProcessor);
 
         // Feeds ImageProxy with all capture id on the initial list.
         triggerImageAvailable(CAPTURE_ID_0, TIMESTAMP_0);
-        sleep(500);
         triggerImageAvailable(CAPTURE_ID_1, TIMESTAMP_1);
 
-        mSemaphore.acquire();
+        // Ensure all posted tasks finish running
+        ShadowLooper.runUiThreadTasks();
+
+        ImageProxyBundle bundle = bundleRef.get();
+        assertThat(bundle).isNotNull();
+
+        // CaptureProcessor.process should be called once all ImageProxies on the
+        // initial lists are ready. Then checks if the output has matched timestamp.
+        assertThat(bundle.getImageProxy(CAPTURE_ID_0).get(0,
+                TimeUnit.SECONDS).getTimestamp()).isEqualTo(TIMESTAMP_0);
+        assertThat(bundle.getImageProxy(CAPTURE_ID_1).get(0,
+                TimeUnit.SECONDS).getTimestamp()).isEqualTo(TIMESTAMP_1);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -116,12 +118,10 @@ public final class ProcessingImageReaderTest {
         // Creates a ProcessingImageReader with maximum Image number smaller than CaptureBundle
         // size.
         ((FakeImageReaderProxy) imageReaderProxy).setMaxImages(1);
-        mCaptureBundle.addCaptureStage(mCaptureStage0);
-        mCaptureBundle.addCaptureStage(mCaptureStage1);
 
         // Expects to throw exception when creating ProcessingImageReader.
-        mProcessingImageReader = new ProcessingImageReader(imageReaderProxy, mBackgroundHandler,
-                mCaptureBundle, mCaptureProcessor);
+        new ProcessingImageReader(imageReaderProxy, mMainHandler,
+                mCaptureBundle, NOOP_PROCESSOR);
     }
 
     private void triggerImageAvailable(int captureId, long timestamp) {
