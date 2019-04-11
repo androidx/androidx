5080b4ab47c42ea27d4ec0f2a8239d4ac6c513cb
shepshapard <shepshapard@google.com>
2019-04-11 12:19:43 -0400

    Remove PressGestureDetector.kt in framework-adapter
    Bug: 130221868
    Test: Manually tested, everything still works.
    
    Change-Id: Ica6d3531d8b487a941f7a80280feb502e0da0ff5

--- ui/material/src/main/java/androidx/ui/baseui/selection/Toggleable.kt
+++ ui/material/src/main/java/androidx/ui/baseui/selection/Toggleable.kt
@@ -16,7 +16,7 @@
 
 package androidx.ui.baseui.selection
 
-import androidx.ui.core.adapter.PressGestureDetector
+import androidx.ui.core.gesture.PressGestureDetector
 import androidx.ui.core.adapter.Semantics
 import com.google.r4a.Children
 import com.google.r4a.Composable
