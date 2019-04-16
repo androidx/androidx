2d016f347e34308aefd72511019dc07a05137523
Trevor McGuire <trevormcguire@google.com>
2019-04-16 15:57:30 -0700

    Temporarily suppress coretestapp flaky tests
     Suppress for now, will be actually fixed in a later CL
    
    Bug: 130580574
    Test: None
    Change-Id: I35bb991dbc06dbf0612eb5b006f3b3a862c491d7

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/ToggleButtonUITest.java
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/ToggleButtonUITest.java
@@ -34,6 +34,7 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.FlakyTest;
 import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -47,6 +48,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /** Test toggle buttons in CoreTestApp. */
+@FlakyTest(bugId = 130580574)
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public final class ToggleButtonUITest {
