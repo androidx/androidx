e128705fd2524480a7364a9fa223c88b8907925e
Emilie Roberts <hadrosaur@google.com>
2019-04-25 15:17:10 -0700

    Initial commit for AOSP merge
    Test: Open app in portrait mode
    Test: Click 3-dot menu, select "Output logcat"
    Test: Click "Single test", "Single capture", and "Begin test"
    Test: Rotate to landscape mode, ensure text remains
    Test: Click "Multiple tests" and "Begin test"
    Test: Click 3-dot menu, select "Delete images"
    Test: Click 3-dot menu, select "Delete CSV logs"
    Change-Id: I08d9dcd4ee1f52b3cdf325a26cb408afdef4417a

---
+++ camera/integration-tests/timingtestapp/src/main/java/androidx/camera/integration/antelope/MultiTestSettingsFragment.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.integration.antelope
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+/**
+ * Fragment that shows the settings for the "Multiple tests" option
+ */
+class MultiTestSettingsFragment
+    : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.multi_test_settings, rootKey)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+}
