aosp-mirror/platform_frameworks_support

Add hidden MediaController#setSurface(Surface)
Bug: 130515451
Test: ./gradlew -p media2 check connectedCheck &&
      ./media2/session/version-compat-tests/runtest.sh 1
Change-Id: Ia88f55492131502c6d360a58bc7deafbef78d8bd

media2/session/src/androidTest/java/androidx/media2/session/MediaController_SurfaceTest.java
@@ -1,0 +1,146 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media2.session;
+
+import static android.content.Context.KEYGUARD_SERVICE;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import android.app.Instrumentation;
+import android.app.KeyguardManager;
+import android.os.Build;
+import android.os.Process;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests {@link MediaController#setSurface(Surface)}.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MediaController_SurfaceTest extends MediaSessionTestBase {
+    private static final String TAG = "MC_SurfaceTest";
+
+    private Instrumentation mInstrumentation;
+    private SurfaceActivity mActivity;
+    private MockPlayer mPlayer;
+    private MediaSession mSession;
+    private MediaController mController;
+
+    @Rule
+    public ActivityTestRule<SurfaceActivity> mActivityRule =
+            new ActivityTestRule<>(SurfaceActivity.class);
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+        mActivity = mActivityRule.getActivity();
+
+        setKeepScreenOn();
+
+        mPlayer = new MockPlayer(0);
+        mSession = new MediaSession.Builder(mContext, mPlayer)
+                .setSessionCallback(sHandlerExecutor, new MediaSession.SessionCallback() {
+                    @Override
+                    public SessionCommandGroup onConnect(
+                            @NonNull MediaSession session,
+                            @NonNull MediaSession.ControllerInfo controller) {
+                        if (Process.myUid() == controller.getUid()) {
+                            return super.onConnect(session, controller);
+                        }
+                        return null;
+                    }
+                })
+                .setId(TAG)
+                .build();
+        mController = createController(mSession.getToken());
+    }
+
+    @After
+    @Override
+    public void cleanUp() throws Exception {
+        super.cleanUp();
+
+        mSession.close();
+    }
+
+    @Test
+    public void testSetSurface() throws Exception {
+        prepareLooper();
+
+        // Set
+        final Surface testSurface = mActivity.getSurfaceHolder().getSurface();
+        SessionResult result = mController.setSurface(testSurface)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertEquals(testSurface, mPlayer.mSurface);
+
+        // Reset
+        result = mController.setSurface(null)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertNull(mPlayer.mSurface);
+    }
+
+    private void setKeepScreenOn() throws Exception {
+        try {
+            setKeepScreenOnOrThrow();
+        } catch (Throwable tr) {
+            throw new Exception(tr);
+        }
+    }
+
+    private void setKeepScreenOnOrThrow() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= 27) {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    mActivity.setTurnScreenOn(true);
+                    mActivity.setShowWhenLocked(true);
+                    KeyguardManager keyguardManager = (KeyguardManager)
+                            mInstrumentation.getTargetContext().getSystemService(KEYGUARD_SERVICE);
+                    keyguardManager.requestDismissKeyguard(mActivity, null);
+                } else {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                            | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                }
+            }
+        });
+        mInstrumentation.waitForIdleSync();
+    }
+}

media2/session/version-compat-tests/common/src/main/aidl/androidx/media2/test/common/IRemoteMediaSession.aidl
@@ -49,6 +49,7 @@ interface IRemoteMediaSession {
     void notifyCurrentMediaItemChanged(String sessionId, int index);
     void notifyAudioAttributesChanged(String sessionId, in ParcelImpl attrs);
     void notifyVideoSizeChanged(String sessionId, in ParcelImpl videoSize);
+    boolean getSurfaceExists(String sessionId);
 
     void setPlaylist(String sessionId, in List<ParcelImpl> playlist);
     void createAndSetDummyPlaylist(String sessionId, int size);

media2/session/version-compat-tests/current/client/src/androidTest/java/androidx/media2/test/client/RemoteMediaSession.java
@@ -505,6 +505,10 @@ public class RemoteMediaSession {
                 Log.e(TAG, "Failed to call setVideoSize()");
             }
         }
+
+        public boolean getSurfaceExists() throws RemoteException {
+            return mBinder.getSurfaceExists(mSessionId);
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////

media2/session/version-compat-tests/current/client/src/androidTest/java/androidx/media2/test/client/tests/MediaController_SurfaceTest.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media2.test.client.tests;
+
+import static android.content.Context.KEYGUARD_SERVICE;
+
+import static androidx.media2.test.common.CommonConstants.DEFAULT_TEST_NAME;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.app.Instrumentation;
+import android.app.KeyguardManager;
+import android.os.Build;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import androidx.media2.session.MediaController;
+import androidx.media2.session.SessionResult;
+import androidx.media2.test.client.RemoteMediaSession;
+import androidx.media2.test.client.SurfaceActivity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests {@link MediaController#setSurface(Surface)}.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MediaController_SurfaceTest extends MediaSessionTestBase {
+    private static final String TAG = "MC_SurfaceTest";
+
+    private Instrumentation mInstrumentation;
+    private SurfaceActivity mActivity;
+    private RemoteMediaSession mRemoteSession;
+
+    @Rule
+    public ActivityTestRule<SurfaceActivity> mActivityRule =
+            new ActivityTestRule<>(SurfaceActivity.class);
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+        mActivity = mActivityRule.getActivity();
+
+        setKeepScreenOn();
+
+        mRemoteSession = new RemoteMediaSession(DEFAULT_TEST_NAME, mContext, null);
+    }
+
+    @After
+    @Override
+    public void cleanUp() throws Exception {
+        super.cleanUp();
+
+        mRemoteSession.cleanUp();
+    }
+
+    @Test
+    public void testSetSurface() throws Exception {
+        prepareLooper();
+        MediaController controller = createController(mRemoteSession.getToken());
+
+        // Set
+        final Surface testSurface = mActivity.getSurfaceHolder().getSurface();
+        SessionResult result = controller.setSurface(testSurface)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertTrue(mRemoteSession.getMockPlayer().getSurfaceExists());
+
+        // Reset
+        result = controller.setSurface(null)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertFalse(mRemoteSession.getMockPlayer().getSurfaceExists());
+    }
+
+    private void setKeepScreenOn() throws Exception {
+        try {
+            setKeepScreenOnOrThrow();
+        } catch (Throwable tr) {
+            throw new Exception(tr);
+        }
+    }
+
+    private void setKeepScreenOnOrThrow() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= 27) {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    mActivity.setTurnScreenOn(true);
+                    mActivity.setShowWhenLocked(true);
+                    KeyguardManager keyguardManager = (KeyguardManager)
+                            mInstrumentation.getTargetContext().getSystemService(KEYGUARD_SERVICE);
+                    keyguardManager.requestDismissKeyguard(mActivity, null);
+                } else {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                            | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                }
+            }
+        });
+        mInstrumentation.waitForIdleSync();
+    }
+}

media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MediaSessionProviderService.java
@@ -527,5 +527,12 @@ public class MediaSessionProviderService extends Service {
             VideoSize videoSizeObj = MediaParcelUtils.fromParcelable(videoSize);
             player.notifyVideoSizeChanged(videoSizeObj);
         }
+
+        @Override
+        public boolean getSurfaceExists(String sessionId) {
+            MediaSession session = mSessionMap.get(sessionId);
+            MockPlayer player = (MockPlayer) session.getPlayer();
+            return player.getSurfaceExists();
+        }
     }
 }

media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MockPlayer.java
@@ -16,7 +16,10 @@
 
 package androidx.media2.test.service;
 
+import android.view.Surface;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
 import androidx.media.AudioAttributesCompat;
 import androidx.media2.common.MediaItem;
@@ -61,6 +64,7 @@ public class MockPlayer extends SessionPlayer {
     public @RepeatMode int mRepeatMode = -1;
     public @ShuffleMode int mShuffleMode = -1;
     public VideoSize mVideoSize = new VideoSize(0, 0);
+    public Surface mSurface;
 
     public boolean mSetPlaylistCalled;
     public boolean mUpdatePlaylistMetadataCalled;
@@ -522,5 +526,16 @@ public class MockPlayer extends SessionPlayer {
                 }
             });
         }
+    }
+
+    @Override
+    @NonNull
+    public ListenableFuture<PlayerResult> setSurfaceInternal(@Nullable Surface surface) {
+        mSurface = surface;
+        return new SyncListenableFuture(mCurrentMediaItem);
+    }
+
+    public boolean getSurfaceExists() {
+        return mSurface != null;
     }
 }

androidx/androidx

media2/session/src/androidTest/java/androidx/media2/session/MediaController_SurfaceTest.java
@@ -1,0 +1,146 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media2.session;
+
+import static android.content.Context.KEYGUARD_SERVICE;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import android.app.Instrumentation;
+import android.app.KeyguardManager;
+import android.os.Build;
+import android.os.Process;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests {@link MediaController#setSurface(Surface)}.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MediaController_SurfaceTest extends MediaSessionTestBase {
+    private static final String TAG = "MC_SurfaceTest";
+
+    private Instrumentation mInstrumentation;
+    private SurfaceActivity mActivity;
+    private MockPlayer mPlayer;
+    private MediaSession mSession;
+    private MediaController mController;
+
+    @Rule
+    public ActivityTestRule<SurfaceActivity> mActivityRule =
+            new ActivityTestRule<>(SurfaceActivity.class);
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+        mActivity = mActivityRule.getActivity();
+
+        setKeepScreenOn();
+
+        mPlayer = new MockPlayer(0);
+        mSession = new MediaSession.Builder(mContext, mPlayer)
+                .setSessionCallback(sHandlerExecutor, new MediaSession.SessionCallback() {
+                    @Override
+                    public SessionCommandGroup onConnect(
+                            @NonNull MediaSession session,
+                            @NonNull MediaSession.ControllerInfo controller) {
+                        if (Process.myUid() == controller.getUid()) {
+                            return super.onConnect(session, controller);
+                        }
+                        return null;
+                    }
+                })
+                .setId(TAG)
+                .build();
+        mController = createController(mSession.getToken());
+    }
+
+    @After
+    @Override
+    public void cleanUp() throws Exception {
+        super.cleanUp();
+
+        mSession.close();
+    }
+
+    @Test
+    public void testSetSurface() throws Exception {
+        prepareLooper();
+
+        // Set
+        final Surface testSurface = mActivity.getSurfaceHolder().getSurface();
+        SessionResult result = mController.setSurface(testSurface)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertSame(testSurface, mPlayer.mSurface);
+
+        // Reset
+        result = mController.setSurface(null).get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertNull(mPlayer.mSurface);
+    }
+
+    private void setKeepScreenOn() throws Exception {
+        try {
+            setKeepScreenOnOrThrow();
+        } catch (Throwable tr) {
+            throw new Exception(tr);
+        }
+    }
+
+    private void setKeepScreenOnOrThrow() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= 27) {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    mActivity.setTurnScreenOn(true);
+                    mActivity.setShowWhenLocked(true);
+                    KeyguardManager keyguardManager = (KeyguardManager)
+                            mInstrumentation.getTargetContext().getSystemService(KEYGUARD_SERVICE);
+                    keyguardManager.requestDismissKeyguard(mActivity, null);
+                } else {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                            | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                }
+            }
+        });
+        mInstrumentation.waitForIdleSync();
+    }
+}

media2/session/version-compat-tests/common/src/main/aidl/androidx/media2/test/common/IRemoteMediaSession.aidl
@@ -49,6 +49,7 @@ interface IRemoteMediaSession {
     void notifyCurrentMediaItemChanged(String sessionId, int index);
     void notifyAudioAttributesChanged(String sessionId, in ParcelImpl attrs);
     void notifyVideoSizeChanged(String sessionId, in ParcelImpl videoSize);
+    boolean surfaceExists(String sessionId);
 
     void setPlaylist(String sessionId, in List<ParcelImpl> playlist);
     void createAndSetDummyPlaylist(String sessionId, int size);

media2/session/version-compat-tests/current/client/src/androidTest/java/androidx/media2/test/client/RemoteMediaSession.java
@@ -505,6 +505,10 @@ public class RemoteMediaSession {
                 Log.e(TAG, "Failed to call notifyVideoSizeChanged()");
             }
         }
+
+        public boolean surfaceExists() throws RemoteException {
+            return mBinder.surfaceExists(mSessionId);
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////

media2/session/version-compat-tests/current/client/src/androidTest/java/androidx/media2/test/client/tests/MediaController_SurfaceTest.java
@@ -1,0 +1,134 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media2.test.client.tests;
+
+import static android.content.Context.KEYGUARD_SERVICE;
+
+import static androidx.media2.test.common.CommonConstants.DEFAULT_TEST_NAME;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.app.Instrumentation;
+import android.app.KeyguardManager;
+import android.os.Build;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import androidx.media2.session.MediaController;
+import androidx.media2.session.SessionResult;
+import androidx.media2.test.client.RemoteMediaSession;
+import androidx.media2.test.client.SurfaceActivity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests {@link MediaController#setSurface(Surface)}.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MediaController_SurfaceTest extends MediaSessionTestBase {
+    private static final String TAG = "MC_SurfaceTest";
+
+    private Instrumentation mInstrumentation;
+    private SurfaceActivity mActivity;
+    private RemoteMediaSession mRemoteSession;
+
+    @Rule
+    public ActivityTestRule<SurfaceActivity> mActivityRule =
+            new ActivityTestRule<>(SurfaceActivity.class);
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+        mActivity = mActivityRule.getActivity();
+
+        setKeepScreenOn();
+
+        mRemoteSession = new RemoteMediaSession(DEFAULT_TEST_NAME, mContext, null);
+    }
+
+    @After
+    @Override
+    public void cleanUp() throws Exception {
+        super.cleanUp();
+
+        mRemoteSession.cleanUp();
+    }
+
+    @Test
+    public void testSetSurface() throws Exception {
+        prepareLooper();
+        MediaController controller = createController(mRemoteSession.getToken());
+
+        // Set
+        final Surface testSurface = mActivity.getSurfaceHolder().getSurface();
+        SessionResult result = controller.setSurface(testSurface)
+                .get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertTrue(mRemoteSession.getMockPlayer().surfaceExists());
+
+        // Reset
+        result = controller.setSurface(null).get(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertEquals(SessionResult.RESULT_SUCCESS, result.getResultCode());
+        assertFalse(mRemoteSession.getMockPlayer().surfaceExists());
+    }
+
+    private void setKeepScreenOn() throws Exception {
+        try {
+            setKeepScreenOnOrThrow();
+        } catch (Throwable tr) {
+            throw new Exception(tr);
+        }
+    }
+
+    private void setKeepScreenOnOrThrow() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= 27) {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    mActivity.setTurnScreenOn(true);
+                    mActivity.setShowWhenLocked(true);
+                    KeyguardManager keyguardManager = (KeyguardManager)
+                            mInstrumentation.getTargetContext().getSystemService(KEYGUARD_SERVICE);
+                    keyguardManager.requestDismissKeyguard(mActivity, null);
+                } else {
+                    mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                            | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                }
+            }
+        });
+        mInstrumentation.waitForIdleSync();
+    }
+}

media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MediaSessionProviderService.java
@@ -527,5 +527,12 @@ public class MediaSessionProviderService extends Service {
             VideoSize videoSizeObj = MediaParcelUtils.fromParcelable(videoSize);
             player.notifyVideoSizeChanged(videoSizeObj);
         }
+
+        @Override
+        public boolean surfaceExists(String sessionId) {
+            MediaSession session = mSessionMap.get(sessionId);
+            MockPlayer player = (MockPlayer) session.getPlayer();
+            return player.surfaceExists();
+        }
     }
 }

media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MockPlayer.java
@@ -16,7 +16,10 @@
 
 package androidx.media2.test.service;
 
+import android.view.Surface;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
 import androidx.media.AudioAttributesCompat;
 import androidx.media2.common.MediaItem;
@@ -61,6 +64,7 @@ public class MockPlayer extends SessionPlayer {
     public @RepeatMode int mRepeatMode = -1;
     public @ShuffleMode int mShuffleMode = -1;
     public VideoSize mVideoSize = new VideoSize(0, 0);
+    public Surface mSurface;
 
     public boolean mSetPlaylistCalled;
     public boolean mUpdatePlaylistMetadataCalled;
@@ -522,5 +526,16 @@ public class MockPlayer extends SessionPlayer {
                 }
             });
         }
+    }
+
+    @Override
+    @NonNull
+    public ListenableFuture<PlayerResult> setSurfaceInternal(@Nullable Surface surface) {
+        mSurface = surface;
+        return new SyncListenableFuture(mCurrentMediaItem);
+    }
+
+    public boolean surfaceExists() {
+        return mSurface != null;
     }
 }