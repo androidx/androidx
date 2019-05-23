0c2a7091f663f8e853290ecb2a84c08fc7adac86
shepshapard <shepshapard@google.com>
2019-05-23 11:21:54 -0700

    Narrowing RestrictTo scope accross androidx.recyclerview.
    Countered the overly wide aosp/900913 by narrowing back to
    proper levels of restriction.
    
    Verified that no current or previous versions of other
    AndroidX libraries have ever accessed what was previously
    accessible and is no longer.  Did so by updating local
    prebuilts, unzipping all jars and aars, and carefully
    grepping to look for usages.  Also verified all current
    code doesn't access anything that is now more
    restricted.
    
    Bug: None.
    Test: None.
    
    Change-Id: Ife3249c54e4435bf85aaa99c9cf44a4bbde97dfc

--- recyclerview/selection/src/main/java/androidx/recyclerview/selection/ItemDetailsLookup.java
+++ recyclerview/selection/src/main/java/androidx/recyclerview/selection/ItemDetailsLookup.java
@@ -16,7 +16,7 @@
 
 package androidx.recyclerview.selection;
 
-import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
+import static androidx.annotation.RestrictTo.Scope.LIBRARY;
 
 import android.view.MotionEvent;
 
@@ -72,7 +72,7 @@ public abstract class ItemDetailsLookup<K> {
      * @return true if there is an item w/ a stable ID at the event coordinates.
      * @hide
      */
-    @RestrictTo(LIBRARY_GROUP_PREFIX)
+    @RestrictTo(LIBRARY)
     protected boolean overItemWithSelectionKey(@NonNull MotionEvent e) {
         return overItem(e) && hasSelectionKey(getItemDetails(e));
     }
