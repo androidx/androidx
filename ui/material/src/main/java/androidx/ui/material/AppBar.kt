edcdd8c6c9bbb59e2429eb80fc72a3a9325f82b2
Clara Bayarri <clarabayarri@google.com>
2019-05-31 18:03:28 +0100

    TopAppBar with slots
    This CL introduces the "slot" idea to TopAppBar, allowing the
    consumer to provide the content for the 3 sections and default
    components that implement them.
    
    Test: TopAppBarUiTest
    Bug: 113855136
    Change-Id: I67bb6548e0558cd046605ea04c68ff60acc5ac3d

--- ui/material/src/main/java/androidx/ui/material/AppBar.kt
+++ ui/material/src/main/java/androidx/ui/material/AppBar.kt
@@ -21,6 +21,7 @@ import androidx.compose.Composable
 import androidx.compose.composer
 import androidx.compose.unaryPlus
 import androidx.ui.baseui.ColoredRect
+import androidx.ui.core.CurrentTextStyleProvider
 import androidx.ui.core.Dp
 import androidx.ui.core.Semantics
 import androidx.ui.core.Text
@@ -33,34 +34,84 @@ import androidx.ui.layout.WidthSpacer
 import androidx.ui.material.surface.Surface
 import androidx.ui.graphics.Color
 import androidx.ui.layout.EdgeInsets
+import androidx.ui.layout.MainAxisSize
 
 /**
  * A Top App Bar displays information and actions relating to the current screen and is placed at
  * the top of the screen.
+ * This version of the TopAppBar will produce a default bar with a navigation leading icon, an
+ * optional title and a set of menu icons.
+ *
+ * Example usage:
+ *     TopAppBar(
+ *         title = "Title",
+ *         color = +themeColor{ secondary }
+ *     )
  *
  * @param title An optional title to display
- * @param color An optional color for the App Bar. By default
- * [androidx.ui.material.MaterialColors.primary] will be used.
+ * @param color An optional color for the App Bar. By default [MaterialColors.primary] will be used.
  * @param icons An optional list of icons to display on the App Bar.
  */
 @Composable
 fun TopAppBar(
     title: String? = null,
     color: Color = +themeColor { primary },
-    // TODO: replace with icons?
+    // TODO: work on menus
     icons: List<Dp> = emptyList()
+) {
+    TopAppBar(
+        color = color,
+        leadingIcon = { AppBarLeadingIcon() },
+        titleTextLabel = {
+            if (title != null) {
+                TopAppBarTitleTextLabel(title)
+            }
+        },
+        trailingIcons = { TopAppBarTrailingIcons(icons) }
+    )
+}
+
+/**
+ * A Top App Bar displays information and actions relating to the current screen and is placed at
+ * the top of the screen.
+ *
+ * Example usage:
+ *     TopAppBar(
+ *         color = +themeColor{ secondary },
+ *         leadingIcon = { MyNavIcon() },
+ *         titleTextLabel = { Text(text = "Title") },
+ *         trailingIcons = { TopAppBarTrailingIcons(icons) }
+ *     )
+ *
+ * @param color An optional color for the App Bar. By default [MaterialColors.primary] will be used.
+ * @param leadingIcon A composable lambda to be inserted in the Leading Icon space. This is usually
+ * a navigation icon. A standard implementation is provided by [AppBarLeadingIcon].
+ * @param titleTextLabel A composable lambda to be inserted in the title space. This is usually a
+ * [Text] element. A standard implementation is provided by [TopAppBarTitleTextLabel]. Default text
+ * styling [MaterialTypography.h6] will be used.
+ * @param trailingIcons A composable lambda to be inserted at the end of the bar, usually containing
+ * a collection of menu icons. A standard implementation is provided by [TopAppBarTrailingIcons].
+ */
+@Composable
+fun TopAppBar(
+    color: Color = +themeColor { primary },
+    leadingIcon: @Composable() () -> Unit,
+    titleTextLabel: @Composable() () -> Unit,
+    trailingIcons: @Composable() () -> Unit
 ) {
     AppBar(color) {
         FlexRow(mainAxisAlignment = MainAxisAlignment.SpaceBetween) {
             inflexible {
-                LeadingIcon()
+                leadingIcon()
                 WidthSpacer(width = 32.dp)
             }
             expanded(flex = 1f) {
-                TitleTextLabel(title)
+                CurrentTextStyleProvider(value = +themeTextStyle { h6 }) {
+                    titleTextLabel()
+                }
             }
             inflexible {
-                TrailingIcons(icons)
+                trailingIcons()
             }
         }
     }
@@ -68,6 +119,9 @@ fun TopAppBar(
 
 /**
  * An empty App Bar that expands to the parent's width.
+ *
+ * For an App Bar that follows Material spec guidelines to be placed on the top of the screen, see
+ * [TopAppBar].
  */
 @Composable
 fun AppBar(color: Color, @Children children: @Composable() () -> Unit) {
@@ -82,44 +136,79 @@ fun AppBar(color: Color, @Children children: @Composable() () -> Unit) {
     }
 }
 
-// TODO: Expose as a public API piece?
+/**
+ * A component that displays a leading icon for an App Bar following Material spec guidelines.
+ *
+ * @see [AppBar]
+ * @see [TopAppBar]
+ */
 @Composable
-internal fun LeadingIcon() {
+fun AppBarLeadingIcon() {
     // TODO: Replace with real icon button
     Semantics(testTag = "Leading icon") {
         FakeIcon(24.dp)
     }
 }
 
+/**
+ * A component that displays a title as a [Text] element for placement within a Top App Bar
+ * following Material spec guidelines.
+ *
+ * @see [TopAppBar]
+ *
+ * @param title A title String to display
+ */
 @Composable
-internal fun TitleTextLabel(title: String?) {
-    if (title != null) {
-        val style = +themeTextStyle { h6 }
-        Semantics(testTag = "Title text label") {
-            Text(text = title, style = style)
-        }
-    }
+fun TopAppBarTitleTextLabel(title: String) {
+    Text(text = title)
 }
 
+/**
+ * A component that displays a set of menu icons for placement within a Top App Bar following
+ * Material spec guidelines.
+ *
+ * @see [TopAppBar]
+ *
+ * @param icons A list of icons to display
+ */
 @Composable
-internal fun TrailingIcons(icons: List<Dp>) {
-    if (icons.isNotEmpty()) {
-        Row {
-            icons.forEachIndexed { index, dp ->
-                if (index < MaxIconsInTopAppBar) {
-                    if (index > 0) {
-                        WidthSpacer(width = 24.dp)
-                    }
-                    Semantics(testTag = "Trailing icon") {
-                        FakeIcon(dp)
-                    }
-                } else if (index == MaxIconsInTopAppBar) {
-                    // Overflow menu
-                    WidthSpacer(width = 24.dp)
-                    Semantics(testTag = "Overflow icon") {
-                        FakeIcon(12.dp)
-                    }
-                }
+fun TopAppBarTrailingIcons(icons: List<Dp>) {
+    TrailingIcons(
+        numIcons = icons.size,
+        maxIcons = MaxIconsInTopAppBar,
+        icons = { index ->
+            Semantics(testTag = "Trailing icon") {
+                // TODO: Replace with real icon button
+                FakeIcon(icons[index])
+            }
+        },
+        overflowIcon = {
+            Semantics(testTag = "Overflow icon") {
+                FakeIcon(12.dp)
+            }
+        }
+    )
+}
+
+// TODO: make public
+@Composable
+internal fun TrailingIcons(
+    numIcons: Int,
+    maxIcons: Int,
+    icons: @Composable() (index: Int) -> Unit,
+    overflowIcon: @Composable() () -> Unit
+) {
+    if (numIcons > 0) {
+        Row(mainAxisSize = MainAxisSize.Min) {
+            val needsOverflow = numIcons > maxIcons
+            val iconsToDisplay = if (needsOverflow) maxIcons else numIcons
+            for (index in 0 until iconsToDisplay) {
+                WidthSpacer(width = 24.dp)
+                icons(index)
+            }
+            if (needsOverflow) {
+                WidthSpacer(width = 24.dp)
+                overflowIcon()
             }
         }
     }
@@ -133,4 +222,5 @@ internal fun FakeIcon(size: Dp) {
 
 private val RegularHeight = 56.dp
 private val Padding = 16.dp
-private const val MaxIconsInTopAppBar = 2
+// TODO: IR compiler bug avoids this being const
+private val MaxIconsInTopAppBar = 2
