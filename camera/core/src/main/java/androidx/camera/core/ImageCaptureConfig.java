08f6ec962260ec043d470d7c82aaa6fcbfbc3213
fungja <fungja@google.com>
2019-06-10 12:25:07 -0700

    Write better comments for what "Target" means
    Clarify that the aspect ratio is used as a hint and apps should always
    check results.
    Clarify that target refers to each output, naming specifics in each use
    case.
    
    Bug: 121268550
    Test:  ./gradlew :camera:camera-core:lint
    Change-Id: I666f39337fa5cbcf06294dd7b97d29e8eb4be909

--- camera/core/src/main/java/androidx/camera/core/ImageCaptureConfig.java
+++ camera/core/src/main/java/androidx/camera/core/ImageCaptureConfig.java
@@ -769,6 +769,13 @@ public final class ImageCaptureConfig
          * the provided {@link Rational} corresponds to the width, and the denominator corresponds
          * to the height.
          *
+         * <p>The target aspect ratio is used as a hint when determining the resulting output aspect
+         * ratio which may differ from the request, possibly due to device constraints.
+         * Application code should check the resulting output's resolution.
+         *
+         * <p>For ImageCapture, the outputs are the {@link ImageProxy} or the File passed to image
+         * capture listeners.
+         *
          * @param aspectRatio A {@link Rational} representing the ratio of the target's width and
          *                    height.
          * @return The current Builder.
