2534c731cc31a615e4584d74327521ec8c5a8aa7
jbwoods <jbwoods@google.com>
2019-06-20 12:42:37 -0700

    Draw exit animations first in FragmentContainerView
    ViewGroups always draw exiting view animations last. For Fragments, this
    means that the exiting Fragment is always on top of all others. There
    can be no interaction with entering fragments, and there is the
    possiblity of a "pop" when the exit animation finishes to reveal the
    Fragment underneath.
    
    FragmentContainerView overrides the dispatchDraw and drawChild method to
    force all disappearing children to be drawn first. It overrides public
    methods from ViewGroup to build the proper list of transitioning views
    and disappearing child views.
    
    Test: Added units tests, tested visually in app, ./gradlew checkApi
    BUG: 37036000
    Change-Id: I054eaf4ae234258bc7abec22c4c8e559a2c6be71

--- fragment/fragment/src/androidTest/java/androidx/fragment/app/FragmentContainerViewTest.kt
+++ fragment/fragment/src/androidTest/java/androidx/fragment/app/FragmentContainerViewTest.kt
@@ -17,11 +17,19 @@
 package androidx.fragment.app
 
 import android.animation.LayoutTransition
+import android.content.Context
+import android.graphics.Canvas
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.test.FragmentTestActivity
 import androidx.fragment.test.R
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import androidx.test.rule.ActivityTestRule
+import androidx.testutils.waitForExecution
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
 import org.junit.Before
@@ -82,5 +90,201 @@ class FragmentContainerViewTest {
         assertWithMessage("Fragment View should be a FragmentContainerView")
             .that(fragment.view)
             .isInstanceOf(FragmentContainerView::class.java)
+    }
+
+    @Test
+    fun removeViewAt() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeViewAt(0)
+
+        assertThat(view.childCount).isEqualTo(1)
+        assertThat(view.getChildAt(0)).isEqualTo(childView2)
+    }
+
+    @Test
+    fun removeViewInLayout() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeViewInLayout(childView1)
+
+        assertThat(view.childCount).isEqualTo(1)
+        assertThat(view.getChildAt(0)).isEqualTo(childView2)
+    }
+
+    @Test
+    fun removeView() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeView(childView1)
+
+        assertThat(view.getChildAt(0)).isEqualTo(childView2)
+    }
+
+    @Test
+    fun removeViews() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeViews(1, 1)
+
+        assertThat(view.childCount).isEqualTo(1)
+    }
+
+    @Test
+    fun removeViewsInLayout() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeViewsInLayout(1, 1)
+
+        assertThat(view.childCount).isEqualTo(1)
+    }
+
+    @Test
+    fun removeAllViewsInLayout() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeAllViewsInLayout()
+
+        assertThat(view.childCount).isEqualTo(0)
+    }
+
+    // removeDetachedView should not actually remove the view
+    @Test
+    fun removeDetachedView() {
+        val context = activityRule.activity.applicationContext
+        val view = FragmentContainerView(context)
+
+        val childView1 = FragmentContainerView(context)
+        val childView2 = FragmentContainerView(context)
+
+        view.addView(childView1)
+        view.addView(childView2)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+
+        view.removeDetachedView(childView1, false)
+
+        assertThat(view.childCount).isEqualTo(2)
+        assertThat(view.getChildAt(1)).isEqualTo(childView2)
+    }
+
+    // Disappearing child views should be drawn first before other child views.
+    @Test
+    fun drawDisappearingChildViewsFirst() {
+        val fm = activityRule.activity.supportFragmentManager
+
+        val fragment1 = ChildViewFragment()
+        val fragment2 = ChildViewFragment()
+
+        fm.beginTransaction()
+            .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            .replace(R.id.fragment_container_view, fragment1)
+            .commit()
+        activityRule.waitForExecution()
+
+        val frag1View = fragment1.mView as ChildView
+
+        fm.beginTransaction()
+            .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            .replace(R.id.fragment_container_view, fragment2)
+            .commit()
+        activityRule.waitForExecution()
+
+        val frag2View = fragment2.mView as ChildView
+
+        assertThat(frag1View.drawTime).isLessThan(frag2View.drawTime)
+    }
+
+    class ChildViewFragment : StrictViewFragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ) = ChildView(context)
+    }
+
+    class ChildView(context: Context?) : View(context) {
+        var drawTime = 0L
+
+        override fun onDraw(canvas: Canvas?) {
+            super.onDraw(canvas)
+            drawTime = System.nanoTime()
+        }
+    }
+}
+
+class FragmentContainerTestView : FragmentContainerView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    override fun removeView(view: View) {
+        view.invalidate()
+        super.removeView(view)
     }
 }
