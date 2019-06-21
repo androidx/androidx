b4eddfd7b427a4a334dceb6e0fb600de38ccdd4f
Ian Lake <ilake@google.com>
2019-06-21 09:59:11 -0700

    Make SavedStateViewModelFactory the default factory
    For both ComponentActivity and Fragment,
    use SavedStateViewModelFactory as the default factory
    for constructing ViewModels.
    
    As this is a strict superset of ViewModel constructors
    now supported, any existing code assuming a ViewModel
    or AndroidViewModel will continue to function.
    
    Test: updated tests
    BUG: 135716331
    Change-Id: I86ef27e21c6d90f169217956baf9813224df4616

--- activity/activity/src/main/java/androidx/activity/ComponentActivity.java
+++ activity/activity/src/main/java/androidx/activity/ComponentActivity.java
@@ -35,6 +35,7 @@ import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LifecycleRegistry;
 import androidx.lifecycle.ReportFragment;
+import androidx.lifecycle.SavedStateViewModelFactory;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStore;
 import androidx.lifecycle.ViewModelStoreOwner;
@@ -67,6 +68,7 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
 
     // Lazily recreated from NonConfigurationInstances by getViewModelStore()
     private ViewModelStore mViewModelStore;
+    private ViewModelProvider.Factory mDefaultFactory;
 
     private final OnBackPressedDispatcher mOnBackPressedDispatcher =
             new OnBackPressedDispatcher(new Runnable() {
@@ -275,6 +277,13 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
         return mViewModelStore;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The extras of {@link #getIntent()} when this is first called will be used as
+     * the defaults to any {@link androidx.lifecycle.SavedStateHandle} passed to a view model
+     * created using this factory.</p>
+     */
     @NonNull
     @Override
     public ViewModelProvider.Factory getDefaultViewModelProviderFactory() {
@@ -282,7 +291,13 @@ public class ComponentActivity extends androidx.core.app.ComponentActivity imple
             throw new IllegalStateException("Your activity is not yet attached to the "
                     + "Application instance. You can't request ViewModel before onCreate call.");
         }
-        return ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
+        if (mDefaultFactory == null) {
+            mDefaultFactory = new SavedStateViewModelFactory(
+                    getApplication(),
+                    this,
+                    getIntent() != null ? getIntent().getExtras() : null);
+        }
+        return mDefaultFactory;
     }
 
     /**
