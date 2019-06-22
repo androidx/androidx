adaf9f88b659243461b45911c07def9dcb0c5765
Ram Parameswaran <rampara@google.com>
2019-06-21 20:02:45 -0700

    Unify button margin and padding styling for all car dialogs.
    Bug: 135766522
    Test: Verify on SupportCarDemo's car dialog activities.
    
    Change-Id: Id8ec18c3e99e7859a446cc6ee27b338df1f6d041

--- car/core/src/main/java/androidx/car/app/CarSingleChoiceDialog.java
+++ car/core/src/main/java/androidx/car/app/CarSingleChoiceDialog.java
@@ -19,6 +19,7 @@ package androidx.car.app;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -133,33 +134,45 @@ public final class CarSingleChoiceDialog extends Dialog {
     }
 
     private void initializeButtons() {
-        boolean isButtonPresent = false;
         Window window = getWindow();
-        Button positiveButtonView = window.findViewById(R.id.positive_button);
-        if (!TextUtils.isEmpty(mPositiveButtonText)) {
-            isButtonPresent = true;
-            positiveButtonView.setText(mPositiveButtonText);
-            positiveButtonView.setOnClickListener(v -> {
-                if (mOnClickListener != null) {
-                    mOnClickListener.onClick(this, mSelectedItem);
-                }
-                dismiss();
-            });
-        } else {
-            positiveButtonView.setVisibility(View.GONE);
-        }
+        Resources res = getContext().getResources();
 
+        Button positiveButtonView = window.findViewById(R.id.positive_button);
+        positiveButtonView.setText(mPositiveButtonText);
+        positiveButtonView.setOnClickListener(v -> {
+            if (mOnClickListener != null) {
+                mOnClickListener.onClick(this, mSelectedItem);
+            }
+            dismiss();
+        });
+
+        int buttonOffset = res.getDimensionPixelSize(R.dimen.car_padding_4)
+                - res.getDimensionPixelSize(R.dimen.car_padding_2);
+
+        ViewGroup.MarginLayoutParams positiveButtonLayoutParams =
+                (ViewGroup.MarginLayoutParams) positiveButtonView.getLayoutParams();
         Button negativeButtonView = window.findViewById(R.id.negative_button);
+
         if (!TextUtils.isEmpty(mNegativeButtonText)) {
-            isButtonPresent = true;
             negativeButtonView.setText(mNegativeButtonText);
             negativeButtonView.setOnClickListener(v -> dismiss());
+
+            ViewGroup.MarginLayoutParams negativeButtonLayoutParams =
+                    (ViewGroup.MarginLayoutParams) negativeButtonView.getLayoutParams();
+
+            int buttonSpacing = res.getDimensionPixelSize(R.dimen.car_padding_2);
+
+            positiveButtonLayoutParams.setMarginStart(buttonSpacing);
+            positiveButtonView.requestLayout();
+
+            negativeButtonLayoutParams.setMarginStart(buttonOffset);
+            negativeButtonLayoutParams.setMarginEnd(buttonSpacing);
+            negativeButtonView.requestLayout();
         } else {
             negativeButtonView.setVisibility(View.GONE);
-        }
 
-        if (!isButtonPresent) {
-            window.findViewById(R.id.button_panel).setVisibility(View.GONE);
+            positiveButtonLayoutParams.setMarginStart(buttonOffset);
+            positiveButtonView.requestLayout();
         }
     }
 
