e7725b9670a1fd5d1e8b962357da6c0a19f1e8c8
Chuck Jazdzewski <chuckj@google.com>
2019-06-24 08:51:16 -0700

    Add a tooling API for collecting information needed by developer tools
    Test: new tests
    Change-Id: Ief01ad5f07b074b6af10e0efb775bf9f68b814a5

---
+++ ui/ui-tooling/src/androidTest/java/androidx/ui/tooling/InspectableTests.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.tooling
+
+import androidx.compose.SlotTable
+import androidx.compose.composer
+import androidx.test.filters.SmallTest
+import androidx.ui.core.dp
+import androidx.ui.core.px
+import androidx.ui.foundation.ColoredRect
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@SmallTest
+@RunWith(JUnit4::class)
+class InspectableTests : ToolingTest() {
+    @Test
+    fun simpleInspection() {
+        show {
+            Inspectable {
+                Column {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                }
+            }
+        }
+
+        // Should be able to find the group for this test
+        val group = tables.findGroupForFile("InspectableTests")
+        assertNotNull(group)
+
+        // The group should have a non-empty bounding box
+        assertEquals(0.px, group!!.box.top)
+        assertEquals(0.px, group.box.left)
+        assertNotEquals(0.px, group.box.right)
+        assertNotEquals(0.px, group.box.bottom)
+    }
+
+    @Test
+    fun inInspectionMode() {
+        var displayed = false
+        show {
+            Inspectable {
+                Column {
+                    InInspectionModeOnly {
+                        ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                        displayed = true
+                    }
+                }
+            }
+        }
+
+        assertTrue(displayed)
+    }
+
+    @Test
+    fun notInInspectionMode() {
+        var displayed = false
+        show {
+            Column {
+                InInspectionModeOnly {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                    displayed = true
+                }
+            }
+        }
+
+        assertFalse(displayed)
+    }
+}
+
+fun Iterable<SlotTable>.findGroupForFile(fileName: String) =
+    map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+fun SlotTable.findGroupForFile(fileName: String) = asTree().findGroupForFile(fileName)
+fun Group.findGroupForFile(fileName: String): Group? {
+    val position = position
+    if (position != null && position.contains(fileName)) return this
+    return children.map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+}
