983f94e6cb7478c6220c35d3d01202ee92d9fc8c
Jin Seok Park <jinpark@google.com>
2019-06-25 15:08:45 +0900

    Extract and set metadata in integration app
    Bug: 135249615
    Test: ./gradlew :media2:integration-tests:testapp:installDebug
    Change-Id: I08a00dd3becb0298f871bca7bdcf9023f4031f87

--- media2/integration-tests/testapp/src/main/java/androidx/media2/integration/testapp/VideoSessionService.java
+++ media2/integration-tests/testapp/src/main/java/androidx/media2/integration/testapp/VideoSessionService.java
@@ -16,7 +16,12 @@
 
 package androidx.media2.integration.testapp;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -40,6 +45,7 @@ public class VideoSessionService extends MediaSessionService {
 
     private MediaPlayer mMediaPlayer;
     private MediaSession mMediaSession;
+    private UriMediaItem mCurrentItem;
     private AudioAttributesCompat mAudioAttributes;
 
     @Override
@@ -99,17 +105,102 @@ public class VideoSessionService extends MediaSessionService {
         @Override
         public MediaItem onCreateMediaItem(@NonNull MediaSession session,
                 @NonNull MediaSession.ControllerInfo controller, @NonNull String mediaId) {
+            // TODO: Need to check if current media item uri is equal to the given media id.
             MediaMetadata metadata = new MediaMetadata.Builder()
                     .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, mediaId)
                     .build();
-            UriMediaItem mediaItem = new UriMediaItem.Builder(Uri.parse(mediaId))
+            mCurrentItem = new UriMediaItem.Builder(Uri.parse(mediaId))
                     .setMetadata(metadata)
                     .build();
+            MetadataExtractTask task = new MetadataExtractTask(mCurrentItem,
+                    VideoSessionService.this);
+            task.execute();
             // TODO: Temporary fix for multiple calls of setMediaItem not working properly.
             //  (b/135728285)
             mMediaPlayer.reset();
             mMediaPlayer.setAudioAttributes(mAudioAttributes);
-            return mediaItem;
+            return mCurrentItem;
+        }
+    }
+
+    private class MetadataExtractTask extends AsyncTask<Void, Void, MediaMetadata> {
+        private MediaItem mItem;
+        private Context mContext;
+
+        MetadataExtractTask(MediaItem mediaItem, Context context) {
+            mItem = mediaItem;
+            mContext = context;
+        }
+
+        @Override
+        protected MediaMetadata doInBackground(Void... params) {
+            return extractMetadata(mItem);
+        }
+
+        @Override
+        protected void onPostExecute(MediaMetadata metadata) {
+            if (metadata != null) {
+                mItem.setMetadata(metadata);
+            }
+        }
+
+        MediaMetadata extractMetadata(MediaItem mediaItem) {
+            MediaMetadataRetriever retriever = null;
+            try {
+                if (mediaItem == null) {
+                    return null;
+                } else if (mediaItem instanceof UriMediaItem) {
+                    Uri uri = ((UriMediaItem) mediaItem).getUri();
+                    retriever = new MediaMetadataRetriever();
+                    retriever.setDataSource(mContext, uri);
+                }
+            } catch (IllegalArgumentException e) {
+                retriever = null;
+            }
+
+            // Do not extract metadata of a media item which is not the current item.
+            if (mediaItem != mCurrentItem) {
+                if (retriever != null) {
+                    retriever.release();
+                }
+                return null;
+            }
+            String title = extractString(retriever, MediaMetadataRetriever.METADATA_KEY_TITLE);
+            String musicArtistText = extractString(retriever,
+                    MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            Bitmap musicAlbumBitmap = extractAlbumArt(retriever);
+
+            if (retriever != null) {
+                retriever.release();
+            }
+
+            // Set duration and title values as MediaMetadata for MediaControlView
+            MediaMetadata.Builder builder = new MediaMetadata.Builder(mCurrentItem.getMetadata());
+
+            builder.putString(MediaMetadata.METADATA_KEY_TITLE, title);
+            builder.putString(MediaMetadata.METADATA_KEY_ARTIST, musicArtistText);
+            builder.putString(
+                    MediaMetadata.METADATA_KEY_MEDIA_ID, mediaItem.getMediaId());
+            builder.putLong(MediaMetadata.METADATA_KEY_PLAYABLE, 1);
+            builder.putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, musicAlbumBitmap);
+            return builder.build();
+        }
+
+        private String extractString(MediaMetadataRetriever retriever, int intKey) {
+            if (retriever != null) {
+                return retriever.extractMetadata(intKey);
+            }
+            return null;
+        }
+
+        private Bitmap extractAlbumArt(MediaMetadataRetriever retriever) {
+            if (retriever != null) {
+                byte[] album = retriever.getEmbeddedPicture();
+                if (album != null) {
+                    return BitmapFactory.decodeByteArray(album, 0, album.length);
+                }
+            }
+            return null;
         }
     }
 }
