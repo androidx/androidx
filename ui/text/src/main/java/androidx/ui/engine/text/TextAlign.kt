a332796d5e4dec4050da1276aafc548a5f11725d
haoyu <haoyuchang@google.com>
2019-07-01 16:32:59 -0700

    Fix TextAlign Doc
    Bug: 126008308
    Test: ./gradlew test
    Change-Id: I066a8707956aa2c81bf2d57741fda9695ae829f7

--- ui/text/src/main/java/androidx/ui/engine/text/TextAlign.kt
+++ ui/text/src/main/java/androidx/ui/engine/text/TextAlign.kt
@@ -39,18 +39,18 @@ enum class TextAlign {
     /**
      * Align the text on the leading edge of the container.
      *
-     * For left-to-right text ([TextDirection.ltr]), this is the left edge.
+     * For left-to-right text ([TextDirection.Ltr]), this is the left edge.
      *
-     * For right-to-left text ([TextDirection.rtl]), this is the right edge.
+     * For right-to-left text ([TextDirection.Rtl]), this is the right edge.
      */
     Start,
 
     /**
      * Align the text on the trailing edge of the container.
      *
-     * For left-to-right text ([TextDirection.ltr]), this is the right edge.
+     * For left-to-right text ([TextDirection.Ltr]), this is the right edge.
      *
-     * For right-to-left text ([TextDirection.rtl]), this is the left edge.
+     * For right-to-left text ([TextDirection.Rtl]), this is the left edge.
      */
     End
 }
