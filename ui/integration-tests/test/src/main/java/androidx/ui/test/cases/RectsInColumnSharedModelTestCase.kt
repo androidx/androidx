aosp-mirror/platform_frameworks_support

Add more benchmarks.
A littbe bit of refactoring and better modularization.

Also adds Goerge's benchmarks and provides more abstractions so we can
reuse for future benchmarks.

This will get little simpler once Benchmark lib will allow to measure
sub-sections / slices so we can measure compose, measure, layout and
draw in one test case.

I have also added new benchmark where each ColoredRect has its own model
which does not invalidate the whole loop. Recomposition times there are
3x better.

Test: Added
Bug: 127479944
Change-Id: I2639aaefa3669009640023ea878f89d53f04f9cf

ui/integration-tests/src/main/java/androidx/ui/test/cases/RectsInColumnSharedModelTestCase.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.test.cases
+
+import android.app.Activity
+import androidx.compose.composer
+import androidx.compose.FrameManager
+import androidx.compose.Model
+import androidx.ui.baseui.ColoredRect
+import androidx.ui.core.dp
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.test.ComposeMaterialIntoActivity
+import androidx.ui.test.ComposeTestCase
+
+@Model
+private class RectanglesInColumnTestCaseColorModel(var color: Color)
+
+/**
+ * Test case that puts the given amount of rectangles into a column layout and makes changes by
+ * modifying the color used in the model.
+ *
+ * Note: Rectangle are created in for loop that reference a single model. Currently it will happen
+ * that the whole loop has to be re-run when model changes.
+ */
+class RectsInColumnSharedModelTestCase(
+    activity: Activity,
+    private val amountOfRectangles: Int
+) : ComposeTestCase(activity) {
+
+    private val model = RectanglesInColumnTestCaseColorModel(Color.Black)
+
+    override fun runSetup() {
+        compositionContext = ComposeMaterialIntoActivity(activity) {
+            Column {
+                repeat(amountOfRectangles) { i ->
+                    if (i == 0) {
+                        ColoredRect(color = model.color, width = 100.dp, height = 50.dp)
+                    } else {
+                        ColoredRect(color = Color.Green, width = 100.dp, height = 50.dp)
+                    }
+                }
+            }
+        }!!
+        FrameManager.nextFrame()
+
+        view = activity.findViewById(android.R.id.content)
+
+        measure()
+        layout()
+        drawSlow()
+    }
+
+    fun toggleState() {
+        if (model.color == Color.Purple) {
+            model.color = Color.Blue
+        } else {
+            model.color = Color.Purple
+        }
+        FrameManager.nextFrame()
+    }
+}

androidx/androidx

ui/integration-tests/test/src/main/java/androidx/ui/test/cases/RectsInColumnSharedModelTestCase.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.test.cases
+
+import android.app.Activity
+import androidx.compose.composer
+import androidx.compose.FrameManager
+import androidx.compose.Model
+import androidx.ui.core.dp
+import androidx.ui.foundation.ColoredRect
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.test.ComposeMaterialIntoActivity
+import androidx.ui.test.ComposeTestCase
+
+@Model
+private class RectanglesInColumnTestCaseColorModel(var color: Color)
+
+/**
+ * Test case that puts the given amount of rectangles into a column layout and makes changes by
+ * modifying the color used in the model.
+ *
+ * Note: Rectangle are created in for loop that reference a single model. Currently it will happen
+ * that the whole loop has to be re-run when model changes.
+ */
+class RectsInColumnSharedModelTestCase(
+    activity: Activity,
+    private val amountOfRectangles: Int
+) : ComposeTestCase(activity) {
+
+    private val model = RectanglesInColumnTestCaseColorModel(Color.Black)
+
+    override fun runSetup() {
+        compositionContext = ComposeMaterialIntoActivity(activity) {
+            Column {
+                repeat(amountOfRectangles) { i ->
+                    if (i == 0) {
+                        ColoredRect(color = model.color, width = 100.dp, height = 50.dp)
+                    } else {
+                        ColoredRect(color = Color.Green, width = 100.dp, height = 50.dp)
+                    }
+                }
+            }
+        }!!
+        FrameManager.nextFrame()
+
+        view = activity.findViewById(android.R.id.content)
+
+        measure()
+        layout()
+        drawSlow()
+    }
+
+    fun toggleState() {
+        if (model.color == Color.Purple) {
+            model.color = Color.Blue
+        } else {
+            model.color = Color.Purple
+        }
+        FrameManager.nextFrame()
+    }
+}