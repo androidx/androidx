fc68cce560d858696ae15f0983684d3223b10d4f
Calin Tataru <calintat@google.com>
2019-07-15 18:47:16 +0100

    Add column sizing options to Table
    This adds a parameter for specifying how each column
    should be sized. The following options are currently
    supported: wrap, flex, fixed width, and fraction.
    
    Bug: 136648288
    Test: Ran own demo (in ui-layout-demos)
    Change-Id: If935c55bb917c5087dd7ff0d28fcea0b5874c87b

--- ui/ui-layout/integration-tests/layout-demos/src/main/java/androidx/ui/layout/demos/TableActivity.kt
+++ ui/ui-layout/integration-tests/layout-demos/src/main/java/androidx/ui/layout/demos/TableActivity.kt
@@ -21,8 +21,11 @@ import android.os.Bundle
 import androidx.compose.composer
 import androidx.compose.setContent
 import androidx.ui.core.CraneWrapper
-import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.graphics.Color
 import androidx.ui.layout.Table
+import androidx.ui.layout.TableColumnWidth
+import androidx.ui.layout.samples.SizedRectangle
 
 class TableActivity : Activity() {
 
@@ -30,13 +33,65 @@ class TableActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContent {
             CraneWrapper {
-                Table {
-                    for (i in 0 until 5) {
-                        tableRow {
-                            for (j in 0 until 7) {
-                                Text("$i, $j")
-                            }
+                Table(
+                    columnWidth = { columnIndex ->
+                        when (columnIndex) {
+                            0 -> TableColumnWidth.Wrap
+                            1 -> TableColumnWidth.Flex(flex = 1f)
+                            2 -> TableColumnWidth.Flex(flex = 2f)
+                            3 -> TableColumnWidth.Fixed(width = 50.dp)
+                            4 -> TableColumnWidth.Fixed(width = 50.dp)
+                            else -> TableColumnWidth.Fraction(fraction = 0.25f)
                         }
+                    }
+                ) {
+                    tableRow {
+                        SizedRectangle(Color.Red, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Green, height = 25.dp)
+                        SizedRectangle(Color.Blue, height = 25.dp)
+                        SizedRectangle(Color.Cyan, height = 25.dp)
+                        SizedRectangle(Color.Magenta, height = 25.dp)
+                        SizedRectangle(Color.Yellow, height = 25.dp)
+                    }
+                    tableRow {
+                        SizedRectangle(Color.Green, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Blue, height = 25.dp)
+                        SizedRectangle(Color.Cyan, height = 25.dp)
+                        SizedRectangle(Color.Magenta, height = 25.dp)
+                        SizedRectangle(Color.Yellow, height = 25.dp)
+                        SizedRectangle(Color.Red, height = 25.dp)
+                    }
+                    tableRow {
+                        SizedRectangle(Color.Blue, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Cyan, height = 25.dp)
+                        SizedRectangle(Color.Magenta, height = 25.dp)
+                        SizedRectangle(Color.Yellow, height = 25.dp)
+                        SizedRectangle(Color.Red, height = 25.dp)
+                        SizedRectangle(Color.Green, height = 25.dp)
+                    }
+                    tableRow {
+                        SizedRectangle(Color.Cyan, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Magenta, height = 25.dp)
+                        SizedRectangle(Color.Yellow, height = 25.dp)
+                        SizedRectangle(Color.Red, height = 25.dp)
+                        SizedRectangle(Color.Green, height = 25.dp)
+                        SizedRectangle(Color.Blue, height = 25.dp)
+                    }
+                    tableRow {
+                        SizedRectangle(Color.Magenta, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Yellow, height = 25.dp)
+                        SizedRectangle(Color.Red, height = 25.dp)
+                        SizedRectangle(Color.Green, height = 25.dp)
+                        SizedRectangle(Color.Blue, height = 25.dp)
+                        SizedRectangle(Color.Cyan, height = 25.dp)
+                    }
+                    tableRow {
+                        SizedRectangle(Color.Yellow, height = 25.dp, width = 25.dp)
+                        SizedRectangle(Color.Red, height = 25.dp)
+                        SizedRectangle(Color.Green, height = 25.dp)
+                        SizedRectangle(Color.Blue, height = 25.dp)
+                        SizedRectangle(Color.Cyan, height = 25.dp)
+                        SizedRectangle(Color.Magenta, height = 25.dp)
                     }
                 }
             }
