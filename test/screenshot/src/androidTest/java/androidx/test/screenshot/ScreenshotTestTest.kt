704a0dbe89da75f83b5a4d0aac963fd12c7afdf4
Cătălin Tudor <catalintudor@google.com>
2019-07-16 16:14:06 +0100

    Added screenshot test rule
    Deleted the screenshot annotation for now as using a custom runner
    or stacking annotations or modifying the runner didn't seem feasible
    for now.
    
    Created a screenshot test rule that checks the device is a cuttlefish
    device.
    
    Bug: 126350937
    Test: ./gradlew test-screenshot:connectedCheck --info --daemon -Pandroid.testInstrumentationRunnerArguments.class=androidx.test.screenshot.ScreenshotTestTest
    Change-Id: I73c9bd9cdc6832db2e6cf4ba92d2e28435caa3a7

--- test/screenshot/src/androidTest/java/androidx/test/screenshot/ScreenshotTestTest.kt
+++ test/screenshot/src/androidTest/java/androidx/test/screenshot/ScreenshotTestTest.kt
@@ -19,15 +19,18 @@ package androidx.test.screenshot
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ScreenshotTestTest {
 
+    @get:Rule
+    val screenshotTestRule = ScreenshotTestRule()
+
     @Test
     @SmallTest
-    @ScreenshotTest
     fun screenshotTestTest_dummy() {
         assertTrue(true)
     }
