b341558861bf59cfdceb5dd5ad34a6837f755043
Nader Jawad <njawad@google.com>
2019-08-01 16:59:41 -0700

    Updated vector graphics APIs to consume both Dp and Px
    Created additional overload for DrawVector to consume the default
    width and height parameters as Dp.
    Modified the child composable to provide the converted viewport
    dimensions as pixels on the virtual canvas for consumpation
    within the vector graphic definition.
    
    Test: Re-ran VectorGraphicsActivity
    Change-Id: I7e2829e80a8f0ec66790cc24e81130637a10970d

--- ui/ui-framework/src/main/java/androidx/ui/core/vectorgraphics/VectorCompose.kt
+++ ui/ui-framework/src/main/java/androidx/ui/core/vectorgraphics/VectorCompose.kt
@@ -21,67 +21,108 @@ import androidx.compose.Composable
 import androidx.compose.composer
 import androidx.compose.compositionReference
 import androidx.compose.memo
-import androidx.compose.onDispose
 import androidx.compose.onPreCommit
 import androidx.compose.unaryPlus
-
+import androidx.ui.core.Dp
 import androidx.ui.core.Draw
 import androidx.ui.core.Px
+import androidx.ui.core.withDensity
 import androidx.ui.graphics.vectorgraphics.Brush
-import androidx.ui.graphics.vectorgraphics.BrushType
-import androidx.ui.graphics.vectorgraphics.DefaultAlpha
-import androidx.ui.graphics.vectorgraphics.DefaultGroupName
-import androidx.ui.graphics.vectorgraphics.DefaultPathName
-import androidx.ui.graphics.vectorgraphics.DefaultPivotX
-import androidx.ui.graphics.vectorgraphics.DefaultPivotY
-import androidx.ui.graphics.vectorgraphics.DefaultRotation
-import androidx.ui.graphics.vectorgraphics.DefaultScaleX
-import androidx.ui.graphics.vectorgraphics.DefaultScaleY
-import androidx.ui.graphics.vectorgraphics.DefaultStrokeLineCap
-import androidx.ui.graphics.vectorgraphics.DefaultStrokeLineJoin
-import androidx.ui.graphics.vectorgraphics.DefaultStrokeLineMiter
-import androidx.ui.graphics.vectorgraphics.DefaultStrokeLineWidth
-import androidx.ui.graphics.vectorgraphics.DefaultTranslationX
-import androidx.ui.graphics.vectorgraphics.DefaultTranslationY
+import androidx.ui.vector.BrushType
+import androidx.ui.vector.DefaultAlpha
+import androidx.ui.vector.DefaultGroupName
+import androidx.ui.vector.DefaultPathName
+import androidx.ui.vector.DefaultPivotX
+import androidx.ui.vector.DefaultPivotY
+import androidx.ui.vector.DefaultRotation
+import androidx.ui.vector.DefaultScaleX
+import androidx.ui.vector.DefaultScaleY
+import androidx.ui.vector.DefaultStrokeLineCap
+import androidx.ui.vector.DefaultStrokeLineJoin
+import androidx.ui.vector.DefaultStrokeLineMiter
+import androidx.ui.vector.DefaultStrokeLineWidth
+import androidx.ui.vector.DefaultTranslationX
+import androidx.ui.vector.DefaultTranslationY
 import androidx.ui.graphics.vectorgraphics.EmptyBrush
-import androidx.ui.graphics.vectorgraphics.EmptyPath
-import androidx.ui.graphics.vectorgraphics.PathData
-import androidx.ui.graphics.vectorgraphics.VectorComponent
-import androidx.ui.graphics.vectorgraphics.GroupComponent
-import androidx.ui.graphics.vectorgraphics.PathComponent
-import androidx.ui.graphics.vectorgraphics.createPath
+import androidx.ui.vector.EmptyPath
+import androidx.ui.vector.PathData
+import androidx.ui.vector.VectorComponent
+import androidx.ui.vector.GroupComponent
+import androidx.ui.vector.PathComponent
+import androidx.ui.vector.createPath
 import androidx.ui.graphics.vectorgraphics.obtainBrush
 import androidx.ui.painting.StrokeCap
 import androidx.ui.painting.StrokeJoin
 import androidx.ui.vector.VectorScope
 import androidx.ui.vector.composeVector
 import androidx.ui.vector.disposeVector
-import java.util.Vector
 
+/**
+ * Sentinel value used to indicate that a dimension is not provided
+ */
+private const val unset: Float = -1.0f
 
+/**
+ * Draw a vector graphic with the provided width, height and viewport dimensions
+ * [defaultWidth] Intrinsic width of the Vector in [Dp]
+ * [defaultHeight] Intrinsic height of hte Vector in [Dp]
+ * [vectorWidth] Width of the viewport space. The viewport is the virtual canvas where paths are drawn on.
+ *  This parameter is optional. Not providing it will use the [defaultWidth] converted to [Px]
+ * [vectorHeight] Height of hte viewport space. The viewport is the virtual canvas where paths are drawn on.
+ *  This parameter is optional. Not providing it will use the [defaultHeight] converted to [Px]
+ */
 @Composable
 fun DrawVector(
-    viewportWidth: Float,
-    viewportHeight: Float,
-    defaultWidth: Px = Px(viewportWidth),
-    defaultHeight: Px = Px(viewportHeight),
+    defaultWidth: Dp,
+    defaultHeight: Dp,
+    viewportWidth: Float = unset,
+    viewportHeight: Float = unset,
     name: String = "",
-    children: @Composable() VectorScope.() -> Unit
+    @Children children: @Composable() VectorScope.(viewportWidth: Float, viewportHeight: Float) -> Unit
 ) {
-    val vector = +memo(name, viewportWidth, viewportHeight) {
-        VectorComponent(
-            name,
-            viewportWidth,
-            viewportHeight,
-            defaultWidth,
-            defaultHeight
-        )
-    }
+    val widthPx = +withDensity { defaultWidth.toPx() }
+    val heightPx = +withDensity { defaultHeight.toPx() }
+
+    val vpWidth = if (viewportWidth == unset) widthPx.value else viewportWidth
+    val vpHeight = if (viewportHeight == unset) heightPx.value else viewportHeight
+    DrawVector(widthPx, heightPx, vpWidth, vpHeight, name, children)
+}
+
+/**
+ * Draw a vector graphic with the provided width, height and viewport dimensions
+ * [defaultWidth] Intrinsic width of the Vector in [Px]
+ * [defaultHeight] Intrinsic height of hte Vector in [Px]
+ * [vectorWidth] Width of the viewport space. The viewport is the virtual canvas where paths are drawn on.
+ *  This parameter is optional. Not providing it will use the [defaultWidth]
+ * [vectorHeight] Height of hte viewport space. The viewport is the virtual canvas where paths are drawn on.
+ *  This parameter is optional. Not providing it will use the [defaultHeight]
+ */
+@Composable
+fun DrawVector(
+    defaultWidth: Px,
+    defaultHeight: Px,
+    viewportWidth: Float = defaultWidth.value,
+    viewportHeight: Float = defaultHeight.value,
+    name: String = "",
+    @Children children: @Composable() VectorScope.(viewportWidth: Float, viewportHeight: Float) -> Unit
+) {
+    val vector =
+        +memo(name, viewportWidth, viewportHeight) {
+            VectorComponent(
+                name,
+                viewportWidth,
+                viewportHeight,
+                defaultWidth,
+                defaultHeight
+            )
+        }
 
     val ref = +compositionReference()
     composeVector(vector, ref, children)
     +onPreCommit(vector) {
-        onDispose { disposeVector(vector, ref) }
+        onDispose {
+            disposeVector(vector, ref)
+        }
     }
 
     Draw { canvas, _ ->
