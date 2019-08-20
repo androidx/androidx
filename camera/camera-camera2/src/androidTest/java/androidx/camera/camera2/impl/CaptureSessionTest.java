967ed32ce464e02da7399a6d3d86ff2cb6ad79fa
Xi Zhang <xizh@google.com>
2019-08-20 15:11:01 -0700

    Fix crash on releasing CaptureSession.
    Client's test started to crash after Aug 12th release. It happens on real device too when exiting the app. The crash is a NPE from within camera2.
    
    The flakiness was introduced in aosp/980247 where we call abortCaptures() right before calling close(). The theory is that calling abortCaptures() sends ERROR_CAMERA_BUFFER which cleans Surfaces, but it could have already been cleared by close() in a race condition.
    
    The workaround is to call close() in the onReady() callback instead of calling it directly after abortCaptures().
    
    Bug: 139448807
    Test: ./gradlew camera:connectedCheck
    Change-Id: I9ec57190953a2b2a1ad3bf95cc583ab7b153212d

--- camera/camera-camera2/src/androidTest/java/androidx/camera/camera2/impl/CaptureSessionTest.java
+++ camera/camera-camera2/src/androidTest/java/androidx/camera/camera2/impl/CaptureSessionTest.java
@@ -228,6 +228,16 @@ public final class CaptureSessionTest {
     }
 
     @Test
+    public void releaseUnopenedSessionWithAbortInFlightCaptures() {
+        CaptureSession captureSession = createCaptureSession(mTestParameters0);
+        captureSession.setSessionConfig(mTestParameters0.mSessionConfig);
+
+        captureSession.release(/*abortInFlightCaptures=*/true);
+
+        assertThat(captureSession.getState()).isEqualTo(State.RELEASED);
+    }
+
+    @Test
     public void closeOpenedSession() throws CameraAccessException {
         CaptureSession captureSession = createCaptureSession(mTestParameters0);
         captureSession.setSessionConfig(mTestParameters0.mSessionConfig);
