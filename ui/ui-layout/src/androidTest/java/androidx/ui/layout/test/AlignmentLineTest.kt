623dc27921ba37ce77ed4bb4407f8c546c65337d
Mihai Popa <popam@google.com>
2019-08-29 15:36:37 +0100

    Add CenterAlignmentLine layout widget
    The CL adds the CenterAlignmentLine layout widget, which can be used to
    center a child according to an alignment line. The layout expands to
    fill the available space in the axis of the alignment line, and wraps
    content in the opposite axis.
    
    Bug: 140215951
    Test: ran AlignmentLineTest
    Change-Id: I6f45abb253f57879ad27821be34ddbea44eb4eb5

--- ui/ui-layout/src/androidTest/java/androidx/ui/layout/test/AlignmentLineTest.kt
+++ ui/ui-layout/src/androidTest/java/androidx/ui/layout/test/AlignmentLineTest.kt
@@ -41,6 +41,7 @@ import androidx.ui.layout.Align
 import androidx.ui.layout.Alignment
 import androidx.ui.layout.AlignmentLineOffset
 import androidx.ui.layout.AspectRatio
+import androidx.ui.layout.CenterAlignmentLine
 import androidx.ui.layout.ConstrainedBox
 import androidx.ui.layout.Container
 import androidx.ui.layout.DpConstraints
@@ -311,5 +312,133 @@ class AlignmentLineTest : LayoutTest() {
             }
         }
         Assert.assertTrue(latch.await(1, TimeUnit.SECONDS))
+    }
+
+    @Test
+    fun testCenterAlignmentLine_vertical_keepsCrossAxisMinConstraints() = withDensity(density) {
+        val testLine = VerticalAlignmentLine(::min)
+        val latch = CountDownLatch(1)
+        val minHeight = 10.dp
+        show {
+            Wrap {
+                ConstrainedBox(DpConstraints(minHeight = minHeight)) {
+                    CenterAlignmentLine(testLine) {
+                        WithConstraints { constraints ->
+                            Assert.assertEquals(minHeight.toIntPx(), constraints.minHeight)
+                            latch.countDown()
+                        }
+                    }
+                }
+            }
+        }
+        Assert.assertTrue(latch.await(1, TimeUnit.SECONDS))
+    }
+
+    @Test
+    fun testCenterAlignmentLine_horizontal_keepsCrossAxisMinConstraints() = withDensity(density) {
+        val testLine = HorizontalAlignmentLine(::min)
+        val latch = CountDownLatch(1)
+        val minWidth = 10.dp
+        show {
+            Wrap {
+                ConstrainedBox(DpConstraints(minWidth = minWidth)) {
+                    CenterAlignmentLine(testLine) {
+                        WithConstraints { constraints ->
+                            Assert.assertEquals(minWidth.toIntPx(), constraints.minWidth)
+                            latch.countDown()
+                        }
+                    }
+                }
+            }
+        }
+        Assert.assertTrue(latch.await(1, TimeUnit.SECONDS))
+    }
+
+    @Test
+    fun testCenterAlignmentLine_vertical_withInfiniteWidth() = withDensity(density) {
+        val layoutLatch = CountDownLatch(2)
+        val testLine = VerticalAlignmentLine(::min)
+        val childWidth = 30.dp
+        val childHeight = 40.dp
+        val lineDp = 10.dp
+
+        val centerSize = Ref<PxSize>()
+        val childPosition = Ref<PxPosition>()
+        show {
+            Layout({
+                CenterAlignmentLine(testLine) {
+                    SaveLayoutInfo(centerSize, Ref(), layoutLatch)
+                    Layout({ SaveLayoutInfo(Ref(), childPosition, layoutLatch) }) { _, _ ->
+                        layout(
+                            childWidth.toIntPx(),
+                            childHeight.toIntPx(),
+                            testLine to lineDp.toIntPx()
+                        ) { }
+                    }
+                }
+            }) { measurables, _ ->
+                val placeable = measurables.first().measure(Constraints()) // Infinite measuring
+                layout(0.ipx, 0.ipx) {
+                    placeable.place(0.ipx, 0.ipx)
+                }
+            }
+        }
+        Assert.assertTrue(layoutLatch.await(1, TimeUnit.SECONDS))
+
+        Assert.assertNotNull(centerSize.value)
+        Assert.assertEquals(
+            ((childWidth.toIntPx() - lineDp.toIntPx()) * 2).toPx(),
+            centerSize.value!!.width
+        )
+        Assert.assertEquals(childHeight.toIntPx().toPx(), centerSize.value!!.height)
+        Assert.assertNotNull(childPosition.value)
+        Assert.assertEquals(
+            (childWidth.toIntPx() - lineDp.toIntPx() * 2).toPx(), childPosition.value!!.x
+        )
+        Assert.assertEquals(0.px, childPosition.value!!.y)
+    }
+
+    @Test
+    fun testCenterAlignmentLine_horizontal_withInfiniteHeight() = withDensity(density) {
+        val layoutLatch = CountDownLatch(2)
+        val testLine = HorizontalAlignmentLine(::min)
+        val childWidth = 30.dp
+        val childHeight = 40.dp
+        val lineDp = 10.dp
+
+        val centerSize = Ref<PxSize>()
+        val childPosition = Ref<PxPosition>()
+        show {
+            Layout({
+                CenterAlignmentLine(testLine) {
+                    SaveLayoutInfo(centerSize, Ref(), layoutLatch)
+                    Layout({ SaveLayoutInfo(Ref(), childPosition, layoutLatch) }) { _, _ ->
+                        layout(
+                            childWidth.toIntPx(),
+                            childHeight.toIntPx(),
+                            testLine to lineDp.toIntPx()
+                        ) { }
+                    }
+                }
+            }) { measurables, _ ->
+                val placeable = measurables.first().measure(Constraints()) // Infinite measuring
+                layout(0.ipx, 0.ipx) {
+                    placeable.place(0.ipx, 0.ipx)
+                }
+            }
+        }
+        Assert.assertTrue(layoutLatch.await(1, TimeUnit.SECONDS))
+
+        Assert.assertNotNull(centerSize.value)
+        Assert.assertEquals(childWidth.toIntPx().toPx(), centerSize.value!!.width)
+        Assert.assertEquals(
+            ((childHeight.toIntPx() - lineDp.toIntPx()) * 2).toPx(),
+            centerSize.value!!.height
+        )
+        Assert.assertNotNull(childPosition.value)
+        Assert.assertEquals(0.px, childPosition.value!!.x)
+        Assert.assertEquals(
+            (childHeight.toIntPx() - lineDp.toIntPx() * 2).toPx(), childPosition.value!!.y
+        )
     }
 }
--- ui/ui-layout/src/main/java/androidx/ui/layout/AlignmentLine.kt
+++ ui/ui-layout/src/main/java/androidx/ui/layout/AlignmentLine.kt
@@ -21,11 +21,15 @@ import androidx.compose.composer
 import androidx.ui.core.AlignmentLine
 import androidx.ui.core.Dp
 import androidx.ui.core.HorizontalAlignmentLine
+import androidx.ui.core.IntPxSize
 import androidx.ui.core.Layout
+import androidx.ui.core.coerceAtLeast
 import androidx.ui.core.coerceIn
 import androidx.ui.core.dp
 import androidx.ui.core.ipx
+import androidx.ui.core.isFinite
 import androidx.ui.core.looseMin
+import androidx.ui.core.max
 
 /**
  * Layout widget that takes a child and tries to position it within itself according to
@@ -75,6 +79,74 @@ fun AlignmentLineOffset(
             val x = if (alignmentLine.horizontal) 0.ipx else paddingBefore
             val y = if (alignmentLine.horizontal) paddingBefore else 0.ipx
             placeable.place(x, y)
+        }
+    }
+}
+
+/**
+ * Layout widget that takes a child and positions it within itself such that the specified
+ * alignment line is centered. The layout will expand to fill the available space in the
+ * alignment line axis. If infinite space is available, the layout will wrap the child and
+ * add the least possible amount of padding such that the centering will work, assuming that
+ * the child provides the alignment line. The layout will wrap the child in the axis opposite
+ * to the alignment line axis. If the child does not provide the specified alignment line, the
+ * child will be centered in the parent (same behavior as [Center]). Similarly, if the child
+ * decides to be smaller than the min constraints of the layout in the axis opposite to the
+ * alignment line axis, the child will be centered with respect to that axis. To make the
+ * layout expand to fill the available space in the axis opposite to the alignment line,
+ * consider wrapping this widget in an [Align].
+ *
+ * @param alignmentLine the alignment line to be centered in the container
+ *
+ * @see Center
+ * @see Align
+ */
+@Composable
+fun CenterAlignmentLine(alignmentLine: AlignmentLine, children: @Composable() () -> Unit) {
+    Layout(children) { measurables, constraints ->
+        require(measurables.isNotEmpty()) { "No child found in CenterAlignmentLine" }
+        val placeable = measurables.first().measure(
+            // Loose constraints perpendicular on the alignment line.
+            if (alignmentLine.horizontal) constraints.copy(minHeight = 0.ipx)
+            else constraints.copy(minWidth = 0.ipx)
+        )
+        // TODO(popam): This is a workaround for b/140231932
+        placeable[alignmentLine]
+        // Expand in the direction of the alignment line and wrap in the other direction.
+        val containerWidth =
+            if (alignmentLine.horizontal) placeable.width
+            else if (constraints.maxWidth.isFinite()) constraints.maxWidth
+            else { // Wrap ourselves around the content such that the alignment line is centered.
+                val linePosition = placeable[alignmentLine]
+                if (linePosition != null) max(linePosition, placeable.width - linePosition) * 2
+                else placeable.width
+            }
+        val containerHeight =
+            if (!alignmentLine.horizontal) placeable.height
+            else if (constraints.maxHeight.isFinite()) constraints.maxHeight
+            else { // Wrap ourselves around the content such that the alignment line is centered.
+                val linePosition = placeable[alignmentLine]
+                if (linePosition != null) max(linePosition, placeable.height - linePosition) * 2
+                else placeable.height
+            }
+        layout(containerWidth, containerHeight) {
+            // Center child in cross axis or if alignment line is null.
+            val centeredPosition = Alignment.Center.align(
+                IntPxSize(containerWidth - placeable.width, containerHeight - placeable.height)
+            )
+            val childX = if (alignmentLine.horizontal) {
+                centeredPosition.x
+            } else {
+                val linePosition = placeable[alignmentLine] ?: placeable.width / 2
+                containerWidth / 2 - linePosition
+            }
+            val childY = if (alignmentLine.horizontal) {
+                val linePosition = placeable[alignmentLine] ?: placeable.height / 2
+                containerHeight / 2 - linePosition
+            } else {
+                centeredPosition.y
+            }
+            placeable.place(childX, childY)
         }
     }
 }
