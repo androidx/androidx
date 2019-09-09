91864b1958acbce678e4804ebc65ca4c90838b3c
Kirill Grouchnikov <kirillg@google.com>
2019-09-09 17:17:31 -0400

    Add MathUtils.clamp for longs
    Bug: 122595279
    Test: ./gradlew :core:core:connectedCheck --info --daemon -Pandroid.testInstrumentationRunnerArguments.class=androidx.core.math.MathUtilsTest
    Change-Id: I87307d93fa36bd14f5d6f03124f51fb282c4ee4f

--- core/core/src/main/java/androidx/core/math/MathUtils.java
+++ core/core/src/main/java/androidx/core/math/MathUtils.java
@@ -85,4 +85,25 @@ public class MathUtils {
         }
         return value;
     }
+
+    /**
+     * This method takes a numerical value and ensures it fits in a given numerical range. If the
+     * number is smaller than the minimum required by the range, then the minimum of the range will
+     * be returned. If the number is higher than the maximum allowed by the range then the maximum
+     * of the range will be returned.
+     *
+     * @param value the value to be clamped.
+     * @param min minimum resulting value.
+     * @param max maximum resulting value.
+     *
+     * @return the clamped value.
+     */
+    public static long clamp(long value, long min, long max) {
+        if (value < min) {
+            return min;
+        } else if (value > max) {
+            return max;
+        }
+        return value;
+    }
 }
