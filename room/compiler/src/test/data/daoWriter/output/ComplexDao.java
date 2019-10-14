f3132ea237f2cbc84543317a0d05e6bfc04f6e56
Aurimas Liutikas <aurimas@google.com>
2019-10-14 13:25:08 -0700

    Attempt to update to JDK 11.
    Also updated to errorprone usages as a java plugin.
    
    Test: ./gradlew assembleDebug
    
    Change-Id: I6b558ec6e89dd1c67fff6fd3a5638779d9313f3d

--- room/compiler/src/test/data/daoWriter/output/ComplexDao.java
+++ room/compiler/src/test/data/daoWriter/output/ComplexDao.java
@@ -19,7 +19,7 @@ import java.lang.SuppressWarnings;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import javax.annotation.Generated;
+import javax.annotation.processing.Generated;
 
 @Generated("androidx.room.RoomProcessor")
 @SuppressWarnings({"unchecked", "deprecation"})
