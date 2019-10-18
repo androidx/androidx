d6a27db9745957abfa59ef775513d6ef9244efe2
Ian Lake <ilake@google.com>
2019-10-18 15:55:46 -0700

    Fix/suppress deprecated API usage in appcompat-resources
    Test: ./gradlew :appcompat:appcompat-resources:compileReleaseJavaWithJavac
    Change-Id: I4452efd9e0f661352766797df05609b30a315345

--- appcompat/resources/src/main/java/androidx/appcompat/graphics/drawable/DrawableContainer.java
+++ appcompat/resources/src/main/java/androidx/appcompat/graphics/drawable/DrawableContainer.java
@@ -154,6 +154,7 @@ class DrawableContainer extends Drawable implements Drawable.Callback {
         return mAlpha;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void setDither(boolean dither) {
         if (mDrawableContainerState.mDither != dither) {
@@ -400,6 +401,7 @@ class DrawableContainer extends Drawable implements Drawable.Callback {
         return changed;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public int getOpacity() {
         return mCurrDrawable == null || !mCurrDrawable.isVisible() ? PixelFormat.TRANSPARENT :
@@ -487,6 +489,7 @@ class DrawableContainer extends Drawable implements Drawable.Callback {
      *
      * @param d The drawable to initialize.
      */
+    @SuppressWarnings("deprecation")
     private void initializeDrawableForDisplay(Drawable d) {
         if (mBlockInvalidateCallback == null) {
             mBlockInvalidateCallback = new BlockInvalidateCallback();
@@ -1079,6 +1082,7 @@ class DrawableContainer extends Drawable implements Drawable.Callback {
         /**
          * @return the resolved opacity of all child drawables.
          */
+        @SuppressWarnings("deprecation")
         public final int getOpacity() {
             if (mCheckedOpacity) {
                 return mOpacity;
