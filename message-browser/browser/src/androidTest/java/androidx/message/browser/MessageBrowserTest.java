3043850cbe7fdae269d4783f5010e7dc7b428c39
Sungsoo Lim <sungsoo@google.com>
2019-10-25 11:03:38 +0900

    Allow multi connection of MessageLibraryService
    Bug: 142760721
    Test: ./gradlew message:connectedCheck
    Change-Id: Id60f6cbf4267bf11b30b82bd5aa21780817654b7

--- message-browser/browser/src/androidTest/java/androidx/message/browser/MessageBrowserTest.java
+++ message-browser/browser/src/androidTest/java/androidx/message/browser/MessageBrowserTest.java
@@ -30,6 +30,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -94,8 +96,7 @@ public class MessageBrowserTest extends MessageBrowserTestBase {
         final CountDownLatch latch = new CountDownLatch(1);
         MessageBrowser.BrowserCallback callback = new MessageBrowser.BrowserCallback() {
             // TODO(sungsoo): check onDisconnect instead.
-            public void onConnected(@NonNull MessageBrowser browser,
-                    @NonNull MessageCommandGroup allowedCommands) {
+            public void onConnected(@NonNull MessageBrowser browser) {
                 latch.countDown();
             }
         };
@@ -106,5 +107,72 @@ public class MessageBrowserTest extends MessageBrowserTestBase {
         MessageBrowser browser = mMsgBrowserBuilder.build();
         assertFalse(latch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
         browser.close();
+    }
+
+    @Test
+    public void testMultiConnection() throws Exception {
+        BrowserCallbackForTestMultiConnection callback =
+                new BrowserCallbackForTestMultiConnection();
+        mMsgBrowserBuilder.setBrowserCallback(sHandlerExecutor, callback);
+        MessageBrowser browser1 = mMsgBrowserBuilder.build();
+        MessageBrowser browser2 = mMsgBrowserBuilder.build();
+        callback.setBrowsers(browser1, browser2);
+        assertTrue(callback.connectedLatch1.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
+        assertTrue(callback.connectedLatch2.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        browser1.close();
+        assertTrue(callback.disconnectedLatch1.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
+        browser2.close();
+        assertTrue(callback.disconnectedLatch2.await(TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private class BrowserCallbackForTestMultiConnection extends MessageBrowser.BrowserCallback {
+        public MessageBrowser browser1;
+        public final CountDownLatch connectedLatch1 = new CountDownLatch(1);
+        public final CountDownLatch disconnectedLatch1 = new CountDownLatch(1);
+
+        public MessageBrowser browser2;
+        public final CountDownLatch connectedLatch2 = new CountDownLatch(1);
+        public final CountDownLatch disconnectedLatch2 = new CountDownLatch(1);
+
+        private List<MessageBrowser> mConnectedBrowsers;
+
+        public void setBrowsers(MessageBrowser browser1, MessageBrowser browser2) {
+            this.browser1 = browser1;
+            this.browser2 = browser2;
+
+            if (mConnectedBrowsers != null) {
+                for (MessageBrowser browser : mConnectedBrowsers) {
+                    if (browser == this.browser1) {
+                        connectedLatch1.countDown();
+                    } else if (browser == this.browser2) {
+                        connectedLatch2.countDown();
+                    }
+                }
+            }
+            mConnectedBrowsers = null;
+        }
+
+        public void onConnected(@NonNull MessageBrowser browser,
+                @NonNull MessageCommandGroup allowedCommands) {
+            if (browser == browser1) {
+                connectedLatch1.countDown();
+            } else if (browser == browser2) {
+                connectedLatch2.countDown();
+            } else {
+                if (mConnectedBrowsers != null) {
+                    mConnectedBrowsers = new ArrayList<>();
+                }
+                mConnectedBrowsers.add(browser);
+            }
+        }
+
+        public void onDisconnected(@NonNull MessageBrowser browser) {
+            if (browser == browser1) {
+                disconnectedLatch1.countDown();
+            } else if (browser == browser2) {
+                disconnectedLatch2.countDown();
+            }
+        }
     }
 }
