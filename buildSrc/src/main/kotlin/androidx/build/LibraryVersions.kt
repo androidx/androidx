45b11795d6737dd540ccedaec026dc34397f1ba6
Nick Anthony <nickanthony@google.com>
2019-10-28 21:32:19 -0400

    Update lifecycle-viewmodel-savedstate to 1.0.0-rc02
    Test: gw bOS
    Change-Id: Ibaf8b33d3252e43fe655b2ca0a25beaf80136b84
    (cherry picked from commit 3240568ec0a7765296965b7cfbcc257ea1f10ecb)

--- buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -71,7 +71,7 @@ object LibraryVersions {
     val LOCALBROADCASTMANAGER = Version("1.1.0-alpha02")
     val LIFECYCLE = Version("2.2.0-rc02")
     val LIFECYCLE_EXTENSIONS = LIFECYCLE
-    val LIFECYCLES_SAVEDSTATE = Version("1.0.0-rc01")
+    val LIFECYCLES_SAVEDSTATE = Version("1.0.0-rc02")
     val LOADER = Version("1.2.0-alpha01")
     val MEDIA = Version("1.2.0-alpha01")
     val MEDIA2_COMMON = Version("1.1.0-alpha01")
