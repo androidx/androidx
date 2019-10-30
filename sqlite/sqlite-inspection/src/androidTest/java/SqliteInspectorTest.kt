c59c3ec6a5b4c7a03527a30228c71653872ae131
Jakub Gielzak <jgielzak@google.com>
2019-10-30 16:19:44 +0000

    Skeleton for testing database tracking
    To be followed-up by addressed TODO items.
    
    Bug: 143216096
    Test: ./gradlew :sqlite:sqlite-inspection:cC
    
    Change-Id: I462aa0c8e7052d2da2b59ff53fe1f3f81655ca58

--- sqlite/sqlite-inspection/src/androidTest/java/SqliteInspectorTest.kt
+++ sqlite/sqlite-inspection/src/androidTest/java/SqliteInspectorTest.kt
@@ -16,12 +16,20 @@
 
 package androidx.sqlite.inspection
 
+import android.database.sqlite.SQLiteDatabase
+import androidx.inspection.InspectorEnvironment
+import androidx.inspection.InspectorEnvironment.EntryHook
+import androidx.inspection.InspectorEnvironment.ExitHook
 import androidx.inspection.testing.InspectorTester
 import androidx.sqlite.inspection.SqliteInspectorProtocol.Command
+import androidx.sqlite.inspection.SqliteInspectorProtocol.Response
 import androidx.sqlite.inspection.SqliteInspectorProtocol.TrackDatabasesCommand
+import androidx.sqlite.inspection.SqliteInspectorProtocol.TrackDatabasesResponse
+import androidx.sqlite.inspection.SqliteInspectorTest.FakeInspectorEnvironment.RegisterHookEntry.EntryHookEntry
+import androidx.sqlite.inspection.SqliteInspectorTest.FakeInspectorEnvironment.RegisterHookEntry.ExitHookEntry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -33,22 +41,120 @@ import org.junit.runner.RunWith
 class SqliteInspectorTest {
     @Test
     fun test_basic_proto() {
-        val command = Command.newBuilder().setTrackDatabases(
-            TrackDatabasesCommand.getDefaultInstance()
-        ).build()
+        val command = createTrackDatabasesCommand()
 
         val commandBytes = command.toByteArray()
-        Truth.assertThat(commandBytes).isNotEmpty()
+        assertThat(commandBytes).isNotEmpty()
 
         val commandBack = Command.parseFrom(commandBytes)
-        Truth.assertThat(commandBack).isEqualTo(command)
+        assertThat(commandBack).isEqualTo(command)
     }
 
     @Test
     fun test_basic_inject() = runBlocking {
         val inspectorTester = InspectorTester(SqliteInspectorFactory.SQLITE_INSPECTOR_ID)
         // no crash means the inspector was successfully injected
-        Truth.assertThat(inspectorTester.channel.isEmpty).isTrue()
+        assertThat(inspectorTester.channel.isEmpty).isTrue()
         inspectorTester.dispose()
+    }
+
+    @Test
+    fun test_track_databases() = runBlocking {
+        val environment = FakeInspectorEnvironment()
+        val inspectorTester =
+            InspectorTester(SqliteInspectorFactory.SQLITE_INSPECTOR_ID, environment)
+        assertThat(environment.consumeRegisteredHooks()).isEmpty()
+
+        // evaluate response
+        val responseBytes = inspectorTester.sendCommand(createTrackDatabasesCommand().toByteArray())
+        assertThat(responseBytes).isNotEmpty()
+        val response = Response.parseFrom(responseBytes)
+        assertThat(response).isEqualTo(createTrackDatabasesResponse())
+
+        // evaluate registered hooks
+        val hookEntries = environment.consumeRegisteredHooks()
+        assertThat(hookEntries).hasSize(1)
+        hookEntries.first().apply {
+            assertThat(originClass.name).isEqualTo(SQLiteDatabase::class.java.name)
+            assertThat(originMethod).isEqualTo(SqliteInspector.sOpenDatabaseCommandSignature)
+            assertThat(this).isInstanceOf(ExitHookEntry::class.java)
+            // TODO: verify newly-open-database events sent (by calling the registered hook)
+        }
+
+        // TODO: verify already-open-database events sent
+
+        assertThat(environment.consumeRegisteredHooks()).isEmpty()
+        inspectorTester.dispose()
+    }
+
+    private fun createTrackDatabasesCommand(): Command =
+        Command.newBuilder().setTrackDatabases(TrackDatabasesCommand.getDefaultInstance()).build()
+
+    private fun createTrackDatabasesResponse(): Response =
+        Response.newBuilder().setTrackDatabases(TrackDatabasesResponse.getDefaultInstance()).build()
+
+    /**
+     * Fake inspector environment with the following behaviour:
+     * - [findInstances] returns pre-registered values from [registerInstancesToFind].
+     * - [registerEntryHook] and [registerExitHook] record the calls which can later be
+     * retrieved in [consumeRegisteredHooks].
+     */
+    private class FakeInspectorEnvironment : InspectorEnvironment {
+        private val instancesToFind = mutableListOf<Any>()
+        private val registeredHooks = mutableListOf<RegisterHookEntry>()
+
+        fun registerInstancesToFind(instances: List<Any>) {
+            instancesToFind.addAll(instances)
+        }
+
+        fun unregisterInstancesToFind(instances: List<Any>) {
+            instancesToFind.removeAll(instances)
+        }
+
+        /**
+         *  Returns instances pre-registered in [registerInstancesToFind].
+         *  By design crashes in case of the wrong setup - indicating an issue with test code.
+         */
+        @Suppress("UNCHECKED_CAST")
+        // TODO: implement actual findInstances behaviour
+        override fun <T : Any?> findInstances(clazz: Class<T>): MutableList<T> =
+            instancesToFind.map { it as T }.toMutableList()
+
+        override fun registerEntryHook(
+            originClass: Class<*>,
+            originMethod: String,
+            entryHook: EntryHook
+        ) {
+            // TODO: implement actual registerEntryHook behaviour
+            registeredHooks.add(EntryHookEntry(originClass, originMethod, entryHook))
+        }
+
+        override fun <T : Any?> registerExitHook(
+            originClass: Class<*>,
+            originMethod: String,
+            exitHook: ExitHook<T>
+        ) {
+            // TODO: implement actual registerExitHook behaviour
+            registeredHooks.add(ExitHookEntry(originClass, originMethod, exitHook))
+        }
+
+        fun consumeRegisteredHooks(): List<RegisterHookEntry> =
+            registeredHooks.toList().also {
+                registeredHooks.clear()
+            }
+
+        sealed class RegisterHookEntry(val originClass: Class<*>, val originMethod: String) {
+            class ExitHookEntry(
+                originClass: Class<*>,
+                originMethod: String,
+                val exitHook: ExitHook<*>
+            ) : RegisterHookEntry(originClass, originMethod)
+
+            class EntryHookEntry(
+                originClass: Class<*>,
+                originMethod: String,
+                val entryHook: EntryHook
+            ) : RegisterHookEntry(originClass, originMethod)
+        }
     }
 }
--- sqlite/sqlite-inspection/src/main/java/androidx/sqlite/inspection/SqliteInspector.java
+++ sqlite/sqlite-inspection/src/main/java/androidx/sqlite/inspection/SqliteInspector.java
@@ -16,21 +16,80 @@
 
 package androidx.sqlite.inspection;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.inspection.Connection;
 import androidx.inspection.Inspector;
+import androidx.inspection.InspectorEnvironment;
+import androidx.sqlite.inspection.SqliteInspectorProtocol.Command;
+import androidx.sqlite.inspection.SqliteInspectorProtocol.TrackDatabasesResponse;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Inspector to work with SQLite databases
  */
 final class SqliteInspector extends Inspector {
+    // TODO: identify all SQLiteDatabase openDatabase methods
+    @VisibleForTesting static String sOpenDatabaseCommandSignature = "openDatabase"
+            + "("
+            + "Ljava/io/File;"
+            + "Landroid/database/sqlite/SQLiteDatabase$OpenParams;"
+            + ")"
+            + "Landroid/database/sqlite/SQLiteDatabase;";
 
-    SqliteInspector(@NonNull Connection connection) {
+    private final AtomicInteger mNextId = new AtomicInteger(1);
+    private final ConcurrentHashMap<Integer, SQLiteDatabase> mDatabases = new ConcurrentHashMap<>();
+
+    private final InspectorEnvironment mEnvironment;
+
+    SqliteInspector(@NonNull Connection connection, InspectorEnvironment environment) {
         super(connection);
+        mEnvironment = environment;
     }
 
     @Override
     public void onReceiveCommand(@NonNull byte[] data, @NonNull CommandCallback callback) {
+        try {
+            Command command = Command.parseFrom(data);
+            if (command.hasTrackDatabases()) {
+                onTrackDatabases(callback);
+            } else {
+                // TODO: handle unrecognised command
+            }
+        } catch (InvalidProtocolBufferException exception) {
+            // TODO: decide on error handling strategy
+        }
+    }
 
+    private void onTrackDatabases(CommandCallback callback) {
+        callback.reply(SqliteInspectorProtocol.Response.newBuilder().setTrackDatabases(
+                TrackDatabasesResponse.getDefaultInstance()).build().toByteArray());
+
+        mEnvironment.registerExitHook(
+                SQLiteDatabase.class,
+                sOpenDatabaseCommandSignature,
+                new InspectorEnvironment.ExitHook<SQLiteDatabase>() {
+                    @Override
+                    public SQLiteDatabase onExit(SQLiteDatabase database) {
+                        onDatabaseAdded(database);
+                        return database;
+                    }
+                });
+
+        List<SQLiteDatabase> instances = mEnvironment.findInstances(SQLiteDatabase.class);
+        for (SQLiteDatabase instance : instances) {
+            onDatabaseAdded(instance);
+        }
+    }
+
+    private void onDatabaseAdded(SQLiteDatabase database) {
+        // TODO: implement onDatabaseAdded
     }
 }
