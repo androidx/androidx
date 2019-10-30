fa740a0f77af38056bfb4f6e69b71b08c98b830a
Trevor McGuire <trevormcguire@google.com>
2019-10-30 15:02:31 -0700

    Add internal method to add CameraIdFilter to CameraSelector
     Adds appendFilter() method to CameraSelector.Builder which will add
     filters to the CameraSelector.
    
     Adds tests for both CameraSelectorTest and CameraSelectorUtilTest to
     ensure all filters are accounted for.
    
    Bug: 142839697
    Test: ./gradlew camera:camera-core:test
    
    Change-Id: I05d80c09266ac2b69d97bf41f835e8f3e4c306b6

--- camera/camera-core/src/test/java/androidx/camera/core/impl/utils/CameraSelectorUtilTest.java
+++ camera/camera-core/src/test/java/androidx/camera/core/impl/utils/CameraSelectorUtilTest.java
@@ -18,9 +18,16 @@ package androidx.camera.core.impl.utils;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import android.os.Build;
 
 import androidx.camera.core.CameraDeviceConfig;
+import androidx.camera.core.CameraIdFilter;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.LensFacing;
 import androidx.test.filters.SmallTest;
@@ -31,11 +38,16 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.annotation.internal.DoNotInstrument;
 
+import java.util.Collections;
+import java.util.Set;
+
 @SmallTest
 @RunWith(RobolectricTestRunner.class)
 @DoNotInstrument
 @Config(minSdk = Build.VERSION_CODES.LOLLIPOP)
 public class CameraSelectorUtilTest {
+
+    private static final Set<String> SINGLE_ID_SET = Collections.singleton("0");
 
     @Test
     public void convertedCameraDeviceConfig_hasFrontLensFacing() {
@@ -64,5 +76,34 @@ public class CameraSelectorUtilTest {
                 CameraSelectorUtil.toCameraDeviceConfig(cameraSelector);
 
         convertedConfig.getCameraIdFilter();
+    }
+
+    public void convertedCameraDeviceConfig_containsAllFilters() {
+        CameraIdFilter filter0 = createPassThroughMockFilter();
+        CameraIdFilter filter1 = createPassThroughMockFilter();
+        CameraIdFilter filter2 = createPassThroughMockFilter();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .appendFilter(filter0)
+                .appendFilter(filter1)
+                .appendFilter(filter2)
+                .build();
+
+        CameraDeviceConfig convertedConfig =
+                CameraSelectorUtil.toCameraDeviceConfig(cameraSelector);
+
+        CameraIdFilter combinedFilter = convertedConfig.getCameraIdFilter();
+
+        combinedFilter.filter(SINGLE_ID_SET);
+
+        verify(filter0, atLeastOnce()).filter(any());
+        verify(filter1, atLeastOnce()).filter(any());
+        verify(filter2, atLeastOnce()).filter(any());
+    }
+
+    private CameraIdFilter createPassThroughMockFilter() {
+        CameraIdFilter mockFilter = mock(CameraIdFilter.class);
+        when(mockFilter.filter(any())).then(i -> i.getArguments()[0]);
+        return mockFilter;
     }
 }
