47aba066308be6c8435d83ec0276353ddcb6df86
Jin Seok Park <jinpark@google.com>
2019-11-12 19:49:17 +0900

    Change constructor for Exif data only streams
    Also improved Javadoc for constructors, including specifying
    when exceptions are thrown.
    
    Bug: 142347585, Bug: 144321109
    Test: ./gradlew :exifinterface:updateApi
    Change-Id: Ib56ac04cc3ad5c93b0a2aa41c3173a6131339b5d

--- exifinterface/src/androidTest/java/androidx/exifinterface/media/ExifInterfaceTest.java
+++ exifinterface/src/androidTest/java/androidx/exifinterface/media/ExifInterfaceTest.java
@@ -944,7 +944,8 @@ public class ExifInterfaceTest {
         fis.read(exifBytes);
 
         ByteArrayInputStream bin = new ByteArrayInputStream(exifBytes);
-        ExifInterface exifInterface = ExifInterface.fromStandalone(bin);
+        ExifInterface exifInterface =
+                new ExifInterface(bin, ExifInterface.STREAM_TYPE_EXIF_DATA_ONLY);
         compareWithExpectedValue(exifInterface, expectedValue, verboseTag, true);
     }
 
