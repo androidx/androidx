2c776c6313f7e12c0f311afafd1e8f9965edd868
Jelle Fresen <jellefresen@google.com>
2019-11-15 16:52:39 +0000

    Add swipe tests to ScrollerTest
    Showcases the new Actions on SemanticsNodeInteraction:
    - doSwipeUp()
    - doSwipeDown()
    - doSwipeLeft()
    - doSwipeRight()
    
    Bug: 144488855
    Test: ./gradlew ui:ui-foundation:cC
    Change-Id: Ia200ad897f0a93c9b2f7f0259fdf2d67a9b9bbd8

--- ui/ui-foundation/src/androidTest/java/androidx/ui/foundation/ScrollerTest.kt
+++ ui/ui-foundation/src/androidTest/java/androidx/ui/foundation/ScrollerTest.kt
@@ -17,6 +17,7 @@ package androidx.ui.foundation
 
 import android.graphics.Bitmap
 import android.os.Handler
+import android.os.Looper
 import android.view.PixelCopy
 import android.view.View
 import android.view.ViewGroup
@@ -27,15 +28,21 @@ import androidx.test.filters.SdkSuppress
 import androidx.test.filters.SmallTest
 import androidx.ui.core.Alignment
 import androidx.ui.core.AndroidComposeView
+import androidx.ui.core.Dp
 import androidx.ui.core.Draw
 import androidx.ui.core.IntPx
+import androidx.ui.core.Px
+import androidx.ui.core.TestTag
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.ipx
 import androidx.ui.core.px
 import androidx.ui.core.setContent
 import androidx.ui.core.toPx
 import androidx.ui.core.toRect
 import androidx.ui.core.withDensity
+import androidx.ui.foundation.shape.DrawShape
+import androidx.ui.foundation.shape.RectangleShape
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.Paint
 import androidx.ui.graphics.PaintingStyle
@@ -46,12 +53,22 @@ import androidx.ui.layout.ConstrainedBox
 import androidx.ui.layout.Container
 import androidx.ui.layout.DpConstraints
 import androidx.ui.layout.Row
+import androidx.ui.semantics.Semantics
+import androidx.ui.test.GestureScope
+import androidx.ui.test.SemanticsNodeInteraction
 import androidx.ui.test.android.AndroidComposeTestRule
 import androidx.ui.test.assertIsDisplayed
 import androidx.ui.test.assertIsNotDisplayed
 import androidx.ui.test.createComposeRule
+import androidx.ui.test.doGesture
 import androidx.ui.test.doScrollTo
+import androidx.ui.test.findByTag
 import androidx.ui.test.findByText
+import androidx.ui.test.sendSwipeDown
+import androidx.ui.test.sendSwipeLeft
+import androidx.ui.test.sendSwipeRight
+import androidx.ui.test.sendSwipeUp
+import com.google.common.truth.Truth.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -279,6 +296,51 @@ class ScrollerTest {
             .assertIsDisplayed()
     }
 
+    @Test
+    fun verticalScroller_swipeUp_swipeDown() {
+        swipeScrollerAndBack(true, GestureScope::sendSwipeUp, GestureScope::sendSwipeDown)
+    }
+
+    @Test
+    fun horizontalScroller_swipeLeft_swipeRight() {
+        swipeScrollerAndBack(false, GestureScope::sendSwipeLeft, GestureScope::sendSwipeRight)
+    }
+
+    private fun swipeScrollerAndBack(
+        isVertical: Boolean,
+        firstSwipe: GestureScope.() -> Unit,
+        secondSwipe: GestureScope.() -> Unit
+    ) {
+        val scrollerPosition = ScrollerPosition()
+
+        createScrollableContent(isVertical, scrollerPosition = scrollerPosition)
+        assertThat(scrollerPosition.getValueOnUiThread()).isEqualTo(0.px)
+
+        findByTag("scroller")
+            .doGesture { firstSwipe() }
+            .awaitScrollAnimation(scrollerPosition)
+
+        val scrolledValue = scrollerPosition.getValueOnUiThread()
+        assertThat(scrolledValue).isGreaterThan(0.px)
+
+        findByTag("scroller")
+            .doGesture { secondSwipe() }
+            .awaitScrollAnimation(scrollerPosition)
+
+        assertThat(scrollerPosition.getValueOnUiThread()).isLessThan(scrolledValue)
+    }
+
+    private fun ScrollerPosition.getValueOnUiThread(): Px {
+        var value = 0.px
+        val latch = CountDownLatch(1)
+        composeTestRule.runOnUiThread {
+            value = this.value
+            latch.countDown()
+        }
+        latch.await()
+        return value
+    }
+
     private fun composeVerticalScroller(
         scrollerPosition: ScrollerPosition = ScrollerPosition(),
         width: IntPx = defaultCrossAxisSize,
@@ -413,7 +475,10 @@ class ScrollerTest {
 
     private fun createScrollableContent(
         isVertical: Boolean,
-        itemCount: Int = 100
+        itemCount: Int = 100,
+        width: Dp = 100.dp,
+        height: Dp = 100.dp,
+        scrollerPosition: ScrollerPosition = ScrollerPosition()
     ) {
         composeTestRule.setContent {
             val content = @Composable {
@@ -421,16 +486,25 @@ class ScrollerTest {
                     Text(text = "$it")
                 }
             }
-            if (isVertical) {
-                VerticalScroller {
-                    Column {
-                        content()
-                    }
-                }
-            } else {
-                HorizontalScroller {
-                    Row {
-                        content()
+            Align(alignment = Alignment.TopLeft) {
+                Container(width = width, height = height) {
+                    DrawShape(RectangleShape, Color.White)
+                    TestTag("scroller") {
+                        Semantics {
+                            if (isVertical) {
+                                VerticalScroller(scrollerPosition) {
+                                    Column {
+                                        content()
+                                    }
+                                }
+                            } else {
+                                HorizontalScroller(scrollerPosition) {
+                                    Row {
+                                        content()
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -462,6 +536,27 @@ class ScrollerTest {
         assertTrue(latch.await(1, TimeUnit.SECONDS))
         assertEquals(PixelCopy.SUCCESS, copyResult)
         return dest
+    }
+
+    private fun SemanticsNodeInteraction.awaitScrollAnimation(
+        scroller: ScrollerPosition
+    ): SemanticsNodeInteraction {
+        if (!scroller.holder.animatedFloat.isRunning) {
+            return this
+        }
+        val latch = CountDownLatch(1)
+        val handler = Handler(Looper.getMainLooper())
+        handler.post(object : Runnable {
+            override fun run() {
+                if (scroller.holder.animatedFloat.isRunning) {
+                    handler.post(this)
+                } else {
+                    latch.countDown()
+                }
+            }
+        })
+        latch.await()
+        return this
     }
 
     // TODO(malkov): ALL below is copypaste from LayoutTest as this test in ui-foundation now
