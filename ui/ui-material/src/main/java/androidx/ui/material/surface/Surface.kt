e7ac85a0d78ef97ea5e4cd7c1b09684dac80b801
Louis Pullen-Freilich <lpf@google.com>
2019-11-26 17:52:03 +0000

    Adds contentColor metadata to foundation
    contentColor represents the color of typography and
    iconography in a given subtree. This is set by Surface
    to correspond to the `on` color if Surface's color is a color
    in the theme.
    
    If not, Surface will use the existing value of contentColor(),
    which defaults to black if not set by any other component.
    
    Bug: b/145355526
    Test: SurfaceColorsTest
    
    Change-Id: Icd0743ece7c29e24e8af341b47659425e30eb838

--- ui/ui-material/src/main/java/androidx/ui/material/surface/Surface.kt
+++ ui/ui-material/src/main/java/androidx/ui/material/surface/Surface.kt
@@ -16,9 +16,7 @@
 
 package androidx.ui.material.surface
 
-import androidx.compose.Ambient
 import androidx.compose.Composable
-import androidx.compose.ambient
 import androidx.compose.unaryPlus
 import androidx.ui.core.Clip
 import androidx.ui.core.CurrentTextStyleProvider
@@ -30,14 +28,16 @@ import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.ipx
 import androidx.ui.engine.geometry.Shape
-import androidx.ui.foundation.shape.border.Border
+import androidx.ui.foundation.ProvideContentColor
+import androidx.ui.foundation.contentColor
 import androidx.ui.foundation.shape.DrawShape
 import androidx.ui.foundation.shape.RectangleShape
+import androidx.ui.foundation.shape.border.Border
 import androidx.ui.foundation.shape.border.DrawBorder
 import androidx.ui.graphics.Color
 import androidx.ui.material.ColorPalette
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.textColorForBackground
+import androidx.ui.material.contentColorFor
 import androidx.ui.text.TextStyle
 
 /**
@@ -50,30 +50,41 @@ import androidx.ui.text.TextStyle
  *
  * 3) Borders: If [shape] has a border, then it will also be drawn.
  *
- *
  * Material surface is the central metaphor in material design. Each surface
  * exists at a given elevation, which influences how that piece of surface
  * visually relates to other surfaces and how that surface casts shadows.
  *
- * The text color for inner [Text] components will try to match the correlated color
- * for the background [color]. For example, on [ColorPalette.surface] background
- * [ColorPalette.onSurface] will be used for text. To modify these default style
- * values use [CurrentTextStyleProvider] or provide direct styling to your components.
- * @see textColorForBackground
+ * [contentColor] is the preferred color for any children inside this surface - any [Text] inside
+ * this Surface will use this color by default.
+ *
+ * If no [contentColor] is set, this surface will try and match its background color to a color
+ * defined in the theme [ColorPalette], and return the corresponding `onFoo` color. For example,
+ * if the [color] of this surface is [ColorPalette.surface], [contentColor] will be set to
+ * [ColorPalette.onSurface]. If [color] is not part of the theme palette, [contentColor] will keep
+ * the same value set above this Surface.
+ *
+ * To modify these default style values used by text, use [CurrentTextStyleProvider] or explicitly
+ * pass a new [TextStyle] to your text.
+ *
+ * To manually retrieve the content color inside a surface, use [contentColor].
  *
  * @param modifier Modifier to be applied to the layout corresponding to the surface
  * @param shape Defines the surface's shape as well its shadow. A shadow is only
  *  displayed if the [elevation] is greater than zero.
  * @param color The background color. Use [Color.Transparent] to have no color.
+ * @param contentColor The preferred content color provided by this Surface to its children.
+ * Defaults to either the matching `onFoo` color for [color], or if [color] is not a color from
+ * the theme, this will keep the same value set above this Surface.
  * @param border Optional border to draw on top of the shape.
  * @param elevation The z-coordinate at which to place this surface. This controls
- *  the size of the shadow below the surface.
+ * the size of the shadow below the surface.
  */
 @Composable
 fun Surface(
     modifier: Modifier = Modifier.None,
     shape: Shape = RectangleShape,
     color: Color = (+MaterialTheme.colors()).surface,
+    contentColor: Color = (+MaterialTheme.colors()).contentColorFor(color) ?: contentColor(),
     border: Border? = null,
     elevation: Dp = 0.dp,
     children: @Composable() () -> Unit
@@ -84,27 +95,13 @@ fun Surface(
         }
         DrawShape(shape = shape, color = color)
         Clip(shape = shape) {
-            val background = if (color.alpha > 0f) color else +ambient(CurrentBackground)
-            CurrentBackground.Provider(background) {
-                CurrentTextStyleProvider(
-                    value = TextStyle(color = +textColorForBackground(color)),
-                    children = children
-                )
-            }
+            ProvideContentColor(contentColor, children)
         }
         if (border != null) {
             DrawBorder(shape = shape, border = border)
         }
     }
 }
-
-/**
- * An ambient to store the current background. Usually provided by [Surface] component.
- *
- * Other components can read it to apply some logic to automatically change icons, text
- * or Ripple color based on the current background.
- */
-val CurrentBackground = Ambient.of { Color.Transparent }
 
 /**
  * A simple layout which just reserves a space for a [Surface].
