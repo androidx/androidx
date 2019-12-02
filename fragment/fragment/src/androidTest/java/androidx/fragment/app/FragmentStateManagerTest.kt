4a2e625f3a6887a737081bce3abd58f975c6f2f0
Ian Lake <ilake@google.com>
2019-12-02 13:33:22 -0800

    Dispatch manager state changes to FragmentStateManagers
    Instead of assuming that a FragmentStateManager
    can go up to RESUMED by default, dispatch the
    FragmentManager's state changes to each
    FragmentStateManager and use that to set
    the maximum state of the FragmentStateManager.
    
    The one exception is when using the <fragment>
    tag, which allows Fragments to move to CREATED
    before the FragmentManager itself moves to
    CREATED.
    
    Test: new tests pass, all existing tests pass
    BUG: 139536619
    Change-Id: I748bb0afc8ffe956dcd6fa695437a0a0d54e8762

--- fragment/fragment/src/androidTest/java/androidx/fragment/app/FragmentStateManagerTest.kt
+++ fragment/fragment/src/androidTest/java/androidx/fragment/app/FragmentStateManagerTest.kt
@@ -73,4 +73,66 @@ class FragmentStateManagerTest {
         assertThat(restoredFragment.mSavedFragmentState)
             .isNotNull()
     }
+
+    @Test
+    fun testSetFragmentManagerState() {
+        val fragment = StrictFragment()
+        val fragmentStateManager = FragmentStateManager(dispatcher, fragment)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.INITIALIZING)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.CREATED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.CREATED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.ACTIVITY_CREATED)
+        // Ensure that moving the FragmentManager's state isn't enough to move beyond CREATED
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.CREATED)
+        // Add the Fragment so that it is allowed to move beyond CREATED
+        fragment.mAdded = true
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.ACTIVITY_CREATED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.STARTED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.STARTED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.RESUMED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.RESUMED)
+
+        // Test downward changes
+        fragmentStateManager.setFragmentManagerState(Fragment.STARTED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.STARTED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.ACTIVITY_CREATED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.ACTIVITY_CREATED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.CREATED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.CREATED)
+
+        fragmentStateManager.setFragmentManagerState(Fragment.INITIALIZING)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.INITIALIZING)
+    }
+
+    @Test
+    fun testInflatedFragmentIsCreated() {
+        val fragment = StrictFragment()
+        val fragmentStateManager = FragmentStateManager(dispatcher, fragment)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.INITIALIZING)
+
+        // Now fake that the Fragment has been added using the <fragment> tag
+        fragment.mFromLayout = true
+        fragment.mInLayout = true
+        // And confirm that FragmentStateManager allows it to move to CREATED
+        // despite never calling setFragmentManagerState(Fragment.CREATED)
+        assertThat(fragmentStateManager.computeMaxState())
+            .isEqualTo(Fragment.CREATED)
+    }
 }
