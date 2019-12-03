27c1b27bb8f6b3153443aa88c684b244c1f401e2
Matthew Fraschilla <fraschilla@google.com>
2019-12-03 12:52:25 -0800

    Create new NavHostController for Testing
    Certain elements of navigation cannot be tested easily since they are
    private or final fields which cannot be mocked. This creates a new
    test specific NavController that, for now, simply exposes the backstack.
    
    Test: added TestNavHostControllerTest
    Change-Id: I18c9a56d8510b2979640e9eebf8a496877f36569

---
+++ navigation/navigation-testing/src/androidTest/java/androidx/navigation/testing/TestNavHostControllerTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.navigation.testing
+
+import androidx.navigation.testing.test.R
+import androidx.navigation.TestNavHostController
+import androidx.navigation.plusAssign
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.testutils.TestNavigator
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class TestNavHostControllerTest {
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setUp() {
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext()).apply {
+            navigatorProvider += TestNavigator()
+            setGraph(R.navigation.test_graph)
+        }
+    }
+
+    @Test
+    fun testStartBackStack() {
+        val backStack = navController.backStack
+        assertThat(backStack).hasSize(2)
+        assertThat(backStack[0].destination.id).isEqualTo(R.id.test_graph)
+        assertThat(backStack[1].destination.id).isEqualTo(R.id.start_test)
+    }
+
+    @Test
+    fun testNavigateBackStack() {
+        navController.navigate(R.id.second)
+        val backStack = navController.backStack
+        assertThat(backStack).hasSize(3)
+        assertThat(backStack[2].destination.id).isEqualTo(R.id.second_test)
+    }
+}
