135c822af1ef7e2443fb48e1ad197433f9891b3e
Ben Weiss <benweiss@google.com>
2019-12-12 14:01:56 +0000

    Clean up DefaultProgressFragment state display
    Display state in title rather than module name.
    Make code more Kotlin idiomatic.
    
    Bug: 143874673
    Test: ./gradlew :navigation:navigation-dynamic-features-fragment:connectedCheck
    Change-Id: I9419e96551a74ee4a1fc61dcf10fd1ce68d66656

--- navigation/navigation-dynamic-features-fragment/src/main/java/androidx/navigation/dynamicfeatures/fragment/ui/DefaultProgressFragment.kt
+++ navigation/navigation-dynamic-features-fragment/src/main/java/androidx/navigation/dynamicfeatures/fragment/ui/DefaultProgressFragment.kt
@@ -23,6 +23,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.navigation.dynamicfeatures.fragment.R
 import com.google.android.play.core.splitinstall.model.SplitInstallErrorCode
 
@@ -41,42 +42,52 @@ class DefaultProgressFragment :
         private const val PROGRESS_MAX = 100
     }
 
+    private lateinit var title: TextView
     private lateinit var moduleName: TextView
     private lateinit var progressBar: ProgressBar
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        moduleName = view.findViewById(R.id.module_name)
-        progressBar = view.findViewById(R.id.installation_progress)
-        progressBar.isIndeterminate = true
-        progressBar.max = PROGRESS_MAX
-
-        val activityIcon = view.findViewById<ImageView>(R.id.progress_icon)
-        val icon = try {
-            requireActivity().packageManager.getActivityIcon(
-                ComponentName(requireContext(), requireActivity().javaClass))
-        } catch (e: PackageManager.NameNotFoundException) {
-            requireActivity().packageManager.defaultActivityIcon
+        with(view) {
+            title = findViewById(R.id.progress_title)
+            moduleName = findViewById(R.id.module_name)
+            progressBar = findViewById<ProgressBar>(R.id.installation_progress)
+            setActivityIcon(findViewById(R.id.progress_icon))
         }
-        activityIcon.setImageDrawable(icon)
+    }
+
+    private fun setActivityIcon(activityIcon: ImageView) {
+        with(requireContext().packageManager) {
+            val icon = try {
+                getActivityIcon(ComponentName(requireContext(), requireActivity().javaClass))
+            } catch (e: PackageManager.NameNotFoundException) {
+                defaultActivityIcon
+            }
+            activityIcon.setImageDrawable(icon)
+        }
     }
 
     override fun onProgress(status: Int, bytesDownloaded: Long, bytesTotal: Long) {
-        progressBar.visibility = View.VISIBLE
-        if (bytesTotal > 0) {
-            progressBar.isIndeterminate = false
-            progressBar.progress = (PROGRESS_MAX * bytesDownloaded / bytesTotal).toInt()
-        } else {
-            progressBar.isIndeterminate = true
+        with(progressBar) {
+            visibility = View.VISIBLE
+            if (bytesTotal == 0L) {
+                isIndeterminate = true
+            } else {
+                progress = (PROGRESS_MAX * bytesDownloaded / bytesTotal).toInt()
+                isIndeterminate = false
+            }
         }
     }
 
     override fun onCancelled() {
-        progressBar.visibility = View.INVISIBLE
-        moduleName.setText(R.string.installation_cancelled)
+        displayErrorState(R.string.installation_cancelled)
     }
 
     override fun onFailed(@SplitInstallErrorCode errorCode: Int) {
+        displayErrorState(R.string.installation_failed)
+    }
+
+    private fun displayErrorState(@StringRes text: Int) {
+        title.setText(text)
         progressBar.visibility = View.INVISIBLE
-        moduleName.setText(R.string.installation_failed)
     }
 }
