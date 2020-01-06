64a809819de2a0190e772676b915febcde437289
Jakub Gielzak <jgielzak@google.com>
2020-01-06 19:41:05 +0000

    Test: union of two queries with different columns
    Added a test that combines results of two queries with different column
    names. The result set of the union uses columns from the first query.
    
    Bug: 143216096
    Test: ./gradlew :sqlite:sqlite-inspection:cC
    
    Change-Id: Ic49b479c03c058624a1ab98848229c85a19f89b5

--- sqlite/sqlite-inspection/src/androidTest/java/androidx/sqlite/inspection/test/QueryTest.kt
+++ sqlite/sqlite-inspection/src/androidTest/java/androidx/sqlite/inspection/test/QueryTest.kt
@@ -32,6 +32,7 @@ import org.junit.runner.RunWith
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
+// TODO: add tests for invalid queries: union of unequal number of columns, syntax error, etc.
 class QueryTest {
     @get:Rule
     val testEnvironment = SqliteInspectorTestEnvironment()
@@ -39,41 +40,124 @@ class QueryTest {
     @get:Rule
     val temporaryFolder = TemporaryFolder()
 
+    private val table1: Table = Table(
+        "table1",
+        Column("t", "TEXT"),
+        Column("nu", "NUMERIC"),
+        Column("i", "INTEGER"),
+        Column("r", "REAL"),
+        Column("b", "BLOB")
+    )
+
+    private val table2: Table = Table(
+        "table2",
+        Column("id", "INTEGER"),
+        Column("name", "TEXT")
+    )
+
+    private val database = Database("db1", table1, table2)
+
+    /** Query verifying type affinity behaviour */
     @Test
-    fun test_query() = runBlocking {
-        // TODO: add tests for invalid queries
-        // TODO: add tests spanning multiple tables (e.g. union of two queries)
-        // TODO: split into smaller tests
+    fun test_valid_query_type_affinity_cases() {
+        val values = listOf(
+            table1 to repeat5("'abc'"),
+            table1 to repeat5("'500.0'"),
+            table1 to repeat5("500.0"),
+            table1 to repeat5("500"),
+            table1 to repeat5("x'0500'"),
+            table1 to repeat5("NULL"),
 
+            table2 to arrayOf("1", "'A'"),
+            table2 to arrayOf("2", "'B'"),
+            table2 to arrayOf("3", "'C'"),
+            table2 to arrayOf("4", "'D'")
+        )
+
+        val query = "select * from ${table1.name}"
+
+        val expectedValues = listOf(
+            repeat5("abc").toList(),
+            listOf("500.0", 500, 500, 500.0f, "500.0"), // text|integer|integer|float|text
+            listOf("500.0", 500, 500, 500.0f, 500.0f), // text|integer|integer|float|float
+            listOf("500", 500, 500, 500.0f, 500), // text|integer|integer|float|integer
+            listOf(*repeat5(arrayOf<Any?>(5.toByte(), 0.toByte()))), // blob|blob|blob|blob|blob
+            listOf(*repeat5<Any?>(null)) // null|null|null|null|null
+        )
+
+        val expectedTypes = listOf(
+            repeat5("text").toList(),
+            listOf("text", "integer", "integer", "float", "text"),
+            listOf("text", "integer", "integer", "float", "float"),
+            listOf("text", "integer", "integer", "float", "integer"),
+            listOf("blob", "blob", "blob", "blob", "blob"),
+            listOf("null", "null", "null", "null", "null")
+        )
+
+        val expectedColumnNames = table1.columns.map { it.name }
+
+        test_valid_query(
+            database,
+            values,
+            query,
+            expectedValues,
+            expectedTypes,
+            expectedColumnNames
+        )
+    }
+
+    /** Union of two queries (different column names) resulting in using first query columns. */
+    @Test
+    fun test_valid_query_two_table_union() {
+        val values = listOf(
+            table1 to repeat5("'abc'"),
+            table1 to repeat5("'xyz'"),
+            table2 to arrayOf("1", "'A'"),
+            table2 to arrayOf("2", "'B'")
+        )
+
+        // query construction
+        val columns1 = table1.columns.take(2).map { it.name }
+        val columns2 = table2.columns.take(2).map { it.name }
+        val column11 = columns1.first()
+        val column12 = columns1.last()
+        val column21 = columns2.first()
+        val column22 = columns2.last()
+        val query1 = "select $column11, $column12 from ${table1.name} where $column11 is 'abc'"
+        val query2 = "select $column21, $column22 from ${table2.name} where $column22 is 'B'"
+        val query = "$query1 union $query2"
+
+        val expectedValues = listOf(
+            listOf(2, "B"),
+            listOf("abc", "abc")
+        )
+
+        val expectedTypes = listOf(
+            listOf("integer", "text"),
+            listOf("text", "text")
+        )
+
+        test_valid_query(
+            database,
+            values,
+            query,
+            expectedValues,
+            expectedTypes,
+            expectedColumnNames = columns1
+        )
+    }
+
+    private fun test_valid_query(
+        database: Database,
+        values: List<Pair<Table, Array<String>>>,
+        query: String,
+        expectedValues: List<List<Any?>>,
+        expectedTypes: List<List<String>>,
+        expectedColumnNames: List<String>
+    ) = runBlocking {
         // given
-        val table1 = Table(
-            "table1",
-            Column("t", "TEXT"),
-            Column("nu", "NUMERIC"),
-            Column("i", "INTEGER"),
-            Column("r", "REAL"),
-            Column("b", "BLOB")
-        )
-
-        val table2 = Table(
-            "table2",
-            Column("id", "INTEGER"),
-            Column("name", "TEXT")
-        )
-
-        val database = Database("db1", table1, table2)
-        val databaseInstance = database.createInstance(temporaryFolder).apply {
-            insertValues(table1, *repeat5("'500.0'"))
-            insertValues(table1, *repeat5("500.0"))
-            insertValues(table1, *repeat5("500"))
-            insertValues(table1, *repeat5("x'0500'"))
-            insertValues(table1, *repeat5("NULL"))
-
-            insertValues(table2, "1", "'A'")
-            insertValues(table2, "2", "'B'")
-            insertValues(table2, "3", "'C'")
-            insertValues(table2, "4", "'D'")
-        }
+        val databaseInstance = database.createInstance(temporaryFolder)
+        values.forEach { (table, values) -> databaseInstance.insertValues(table, *values) }
 
         testEnvironment.registerAlreadyOpenDatabases(
             listOf(
@@ -85,24 +169,6 @@ class QueryTest {
         testEnvironment.sendCommand(createTrackDatabasesCommand())
         val databaseId = testEnvironment.awaitDatabaseOpenedEvent(database.name).databaseId
 
-        val query = "select * from ${table1.name}"
-
-        val expectedValues = listOf(
-            listOf("500.0", 500, 500, 500.0f, "500.0"), // text|integer|integer|float|text
-            listOf("500.0", 500, 500, 500.0f, 500.0f), // text|integer|integer|float|float
-            listOf("500", 500, 500, 500.0f, 500), // text|integer|integer|float|integer
-            listOf(*repeat5(arrayOf<Any?>(5.toByte(), 0.toByte()))), // blob|blob|blob|blob|blob
-            listOf(*repeat5<Any?>(null)) // null|null|null|null|null
-        )
-
-        val expectedTypes = listOf(
-            listOf("text", "integer", "integer", "float", "text"),
-            listOf("text", "integer", "integer", "float", "float"),
-            listOf("text", "integer", "integer", "float", "integer"),
-            listOf("blob", "blob", "blob", "blob", "blob"),
-            listOf("null", "null", "null", "null", "null")
-        )
-
         // when
         testEnvironment.sendCommand(createQueryTableCommand(databaseId, query)).let { response ->
             // then
@@ -111,7 +177,7 @@ class QueryTest {
                     row.valuesList.forEachIndexed { colIx, cell ->
                         assertThat(cell.value).isEqualTo(expectedValues[rowIx][colIx])
                         assertThat(cell.type).isEqualTo(expectedTypes[rowIx][colIx])
-                        assertThat(cell.columnName).isEqualTo(table1.columns[colIx].name)
+                        assertThat(cell.columnName).isEqualTo(expectedColumnNames[colIx])
                     }
                 }
             }
