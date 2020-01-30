44a1751fc9e00e2b32178a552a7919c6d0e6b25b
Jeff Gaston <jeffrygaston@google.com>
2020-01-30 14:40:46 -0500

    Saving build durations as numbers instead of formatted timestamps
    Now, a build metrics file might look like this:
    
      {"configuration_duration": 0.774, "task_execution_duration": 86.44, "total_cpu": 1072.12}
    
    Bug: 148526619
    Test: DIST_DIR=../../out/dist ./busytown/androidx.sh
    Change-Id: Ia13004c6f933db9018ef111d355e95e9ef17ac89

--- busytown/impl/parse_profile_html.py
+++ busytown/impl/parse_profile_html.py
@@ -53,12 +53,34 @@ def parse(inputProfilePath, interestingKeys):
       line = line.strip()
       lineText = line.replace("<td>", "").replace('<td class="numeric">', "").replace("</td>", "")
       if currentKey is not None:
-        values[currentKey] = lineText
+        values[currentKey] = parseDuration(lineText)
       if lineText in interestingKeys:
         currentKey = lineText
       else:
         currentKey = None
     return values
+
+# Given a duration such as 1h20m02.5s, returns a number like ((1*60)+20)*60+2=4802.5
+def parseDuration(durationText):
+  originalDurationText = durationText
+  secondsText = "0"
+  minutesText = "0"
+  hoursText = "0"
+  daysText = "0"
+  if "d" in durationText:
+   daysText, durationText = durationText.split("d")
+  if "h" in durationText:
+    hoursText, durationText = durationText.split("h")
+  if "m" in durationText:
+    minutesText, durationText = durationText.split("m")
+  if "s" in durationText:
+    secondsText, durationText = durationText.split("s")
+  if durationText != "":
+    raise Exception("Failed to parse '" + durationText + "'")
+  try:
+    return (((float(daysText) * 24 + float(hoursText)) * 60) + float(minutesText)) * 60 + float(secondsText)
+  except ValueError as e:
+    raise ValueError("Failed to parse '" + durationText + "'")
 
 if __name__ == "__main__":
   main()
