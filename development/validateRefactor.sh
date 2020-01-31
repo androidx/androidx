fd45035fada03e1f8b377d5a5c27743c9760e8b4
Jeff Gaston <jeffrygaston@google.com>
2020-01-31 16:39:32 -0500

    validateRefactor.sh support for multiple repositories
    in preparation to test changes to Dokka
    
    Bug: 148450617
    
    Test: ./development/validateRefactor.sh HEAD^
    Test: ./development/validateRefactor.sh tools/repohooks:HEAD^ HEAD^
    
    Change-Id: Id48bf2d234fc3d10ff5af3a53145d2ed670fdb49

--- development/validateRefactor.sh
+++ development/validateRefactor.sh
@@ -19,38 +19,110 @@ set -e
 supportRoot="$(cd $(dirname $0)/.. && pwd)"
 checkoutRoot="$(cd ${supportRoot}/../.. && pwd)"
 
+function die() {
+  echo "$@" >&2
+  exit 1
+}
+
 function usage() {
-  echo "usage: $0 <git treeish>"
-  echo
-  echo "For example, $0 HEAD^"
-  echo
-  echo "Validates that libraries built from <git treeish>* are the same as the build outputs at HEAD."
-  echo "This can be used to validate that a refactor did not change the outputs."
-  echo
-  echo "* A git treeish is what you type when you run 'git checkout <git treeish>'"
-  echo "  See also https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddeftree-ishatree-ishalsotreeish ."
+  violation="$1"
+  die "
+  Usage: $0 <git treeish>
+         $0 <path>:<git treeish> <path>:<git treeish>
+
+  Validates that libraries built from the given versions are the same as
+  the build outputs built at HEAD. This can be used to validate that a refactor
+  did not change the outputs. If a git treeish is given with no path, the path is considered to be frameworks/support
+
+  Example: $0 HEAD^
+  Example: $0 prebuilts/androidx/external:HEAD^ frameworks/support:work^
+
+  * A git treeish is what you type when you run 'git checkout <git treeish>'
+    See also https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddeftree-ishatree-ishalsotreeish .
+  "
   return 1
 }
 
-oldCommit="$1"
-if [ "$oldCommit" == "" ]; then
+# Fills in a default repository path of "frameworks/support:" for any args that don't specify
+# their repository. Given an input of: "work^ prebuilts/androidx/external:HEAD^", should return
+# "frameworks/support:work^ prebuilts/androidx/external:HEAD^".
+function expandCommitArgs() {
+  inputSpecs="$@"
+  outputSpecs=""
+  for spec in $inputSpecs; do
+    if echo "$spec" | grep -v ":" >/dev/null; then
+      spec="frameworks/support:$spec"
+    fi
+    outputSpecs="$outputSpecs $spec"
+  done
+  echo $outputSpecs
+}
+
+# Given a list of paths like "frameworks/support prebuilts/androidx/external",
+# runs `git checkout -` in each
+function uncheckout() {
+  repositoryPaths="$@"
+  for repositoryPath in $repositoryPaths; do
+    echoAndDo git -C "$checkoutRoot/$repositoryPath" checkout -
+  done
+}
+# Given a list of version specs like "a/b:c d/e:f", returns just the paths: "a/b d/e"
+function getParticipatingProjectPaths() {
+  specs="$@"
+  result=""
+  for arg in $specs; do
+    echo parsing $arg >&2
+    repositoryPath="$(echo $arg | sed 's|\([^:]*\):\([^:]*\)|\1|')"
+    otherVersion="$(echo $arg | sed 's|\([^:]*\):\([^:]*\)|\2|')"
+    if [ "$otherVersion" != "HEAD" ]; then
+      result="$result $repositoryPath"
+    fi
+  done
+  echo $result
+}
+# Given a list of paths, returns a string containing the currently checked-out version of each
+function getCurrentCommits() {
+  repositoryPaths="$@"
+  result=""
+  for repositoryPath in $repositoryPaths; do
+    currentVersion="$(cd $checkoutRoot/$repositoryPath && git log -1 --format=%H)"
+    result="$result $repositoryPath:$currentVersion"
+  done
+  echo $result
+}
+function echoAndDo() {
+  echo "$*"
+  eval "$*"
+}
+# Given a list of version specs like "a/b:c d/e:f", checks out the appropriate version in each
+# In this example it would be `cd a/b && git checkout e` and `cd e/e && git checkout f`
+function checkout() {
+  versionSpecs="$1"
+  for versionSpec in $versionSpecs; do
+    project="$(echo $versionSpec | sed 's|\([^:]*\):\([^:]*\)|\1|')"
+    ref="$(echo     $versionSpec | sed 's|\([^:]*\):\([^:]*\)|\2|')"
+    echo "checking out $ref in project $project"
+    echoAndDo git -C "$checkoutRoot/$project" checkout "$ref"
+  done
+}
+function doBuild() {
+  echoAndDo ./gradlew createArchive
+  echoAndDo unzip "${tempOutPath}/dist/top-of-tree-m2repository-all-0.zip" -d "${tempOutPath}/dist/top-of-tree-m2repository-all-0.unzipped"
+}
+
+oldCommits="$(expandCommitArgs $@)"
+projectPaths="$(getParticipatingProjectPaths $oldCommits)"
+echo old commits: $oldCommits
+if [ "$oldCommits" == "" ]; then
   usage
 fi
-newCommit="$(git log -1 --format=%H)"
+newCommits="$(getCurrentCommits $projectPaths)"
+echo new commits: $newCommits
 
 oldOutPath="${checkoutRoot}/out-old"
 newOutPath="${checkoutRoot}/out-new"
 tempOutPath="${checkoutRoot}/out"
 
-function echoAndDo() {
-  echo "$*"
-  eval "$*"
-}
-
-function doBuild() {
-  ./gradlew createArchive
-  unzip "${tempOutPath}/dist/top-of-tree-m2repository-all-0.zip" -d "${tempOutPath}/dist/top-of-tree-m2repository-all-0.unzipped"
-}
 
 rm -rf "$oldOutPath" "$newOutPath" "$tempOutPath"
 
@@ -58,17 +130,17 @@ echo building new commit
 doBuild
 mv "$tempOutPath" "$newOutPath"
 
-
 echo building previous commit
-echoAndDo git checkout "$oldCommit"
+
+checkout "$oldCommits"
 if doBuild; then
   echo previous build succeeded
 else
   echo previous build failed
-  git checkout -
+  uncheckout "$projectPaths"
   exit 1
 fi
-git checkout -
+uncheckout "$projectPaths"
 mv "$tempOutPath" "$oldOutPath"
 
 echo
