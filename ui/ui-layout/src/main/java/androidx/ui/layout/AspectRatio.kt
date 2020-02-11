47f09b55fbd7564e11e6a1db11fab60d593748e9
Anastasia Soboleva <soboleva@google.com>
2020-02-11 15:36:51 +0000

    Remove AspectRatio, FlexColumn/Row and Spacing
    Bug: 149197328, 143753123, 149198808, 145599478
    Test: layout, material and framework tests passed
    Relnote: "Removed AspectRatio composable in favor of modifier. Deleted
    obsolete FlexColumn, FlexRow composables and Spacing modifier"
    Change-Id: Iec8a7b87c9130310a9d80ab82cd166738c99a3df

--- ui/ui-layout/src/main/java/androidx/ui/layout/AspectRatio.kt
+++ ui/ui-layout/src/main/java/androidx/ui/layout/AspectRatio.kt
@@ -17,9 +17,7 @@
 package androidx.ui.layout
 
 import androidx.annotation.FloatRange
-import androidx.compose.Composable
 import androidx.ui.core.Constraints
-import androidx.ui.core.Layout
 import androidx.ui.core.LayoutModifier
 import androidx.ui.core.Measurable
 import androidx.ui.core.satisfiedBy
@@ -81,61 +79,6 @@ data class LayoutAspectRatio(
     override fun Density.maxIntrinsicHeightOf(measurable: Measurable, width: IntPx): IntPx {
         return if (width == IntPx.Infinity) measurable.maxIntrinsicHeight(width)
         else width / aspectRatio
-    }
-}
-
-/**
- * Layout composable that attempts to size itself and a potential layout child to match a specified
- * aspect ratio. The composable will try to match one of the incoming constraints, in the following
- * order: maxWidth, maxHeight, minWidth, minHeight. The size in the other dimension will then be
- * computed according to the aspect ratio. Note that the provided aspectRatio will always
- * correspond to the width/height ratio.
- *
- * If a valid size that satisfies the constraints is found this way, the composable will size itself to
- * this size. If a child is present, this will be measured with tight constraints to match the size.
- * If no valid size is found, the aspect ratio will not be satisfied, and the composable will
- * wrap its child, which is measured with the same constraints. If there is no child, the composable
- * will size itself to the incoming min constraints.
- *
- * Example usage:
- *     AspectRatio(2f) {
- *         DrawRectangle(color = Color.Blue)
- *         Container(padding = EdgeInsets(20.dp)) {
- *             SizedRectangle(color = Color.Black)
- *         }
- *     }
- * The AspectRatio composable will make the Container have the width twice its height.
- */
-@Deprecated(
-    "Use AspectRatio layout modifier instead.",
-    ReplaceWith("modifier = LayoutAspectRatio(aspectRatio)"),
-    DeprecationLevel.WARNING
-)
-@Composable
-fun AspectRatio(
-    aspectRatio: Float,
-    children: @Composable() () -> Unit
-) {
-    require(aspectRatio > 0) {
-        "Received aspect ratio value $aspectRatio is expected to be positive non-zero."
-    }
-    Layout(children) { measurables, constraints ->
-        val size = constraints.findSizeWith(aspectRatio)
-
-        val measurable = measurables.firstOrNull()
-        val childConstraints = if (size != null) {
-            Constraints.fixed(size.width, size.height)
-        } else {
-            constraints
-        }
-        val placeable = measurable?.measure(childConstraints)
-
-        layout(
-            size?.width ?: placeable?.width ?: constraints.minWidth,
-            size?.height ?: placeable?.height ?: constraints.minHeight
-        ) {
-            placeable?.place(0.ipx, 0.ipx)
-        }
     }
 }
 
