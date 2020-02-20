cdb78ac4088894cc2873a79423a5213ffae092ce
Matvei Malkov <malkov@google.com>
2020-02-20 19:28:38 +0000

    Clean up material demos
    Material demos were growing uncontrollably for past few month, a lot of
    good stuff happened. This CL groups some of similar things (buttons and
    FABs) and delete some really misc demos
    
    Bug: n/a
    Test: material tests should pass
    Change-Id: Ibaf014b180ce4ffed565d36539eba9e9a7e2d25a

--- ui/ui-material/integration-tests/material-demos/src/main/java/androidx/ui/material/demos/ButtonActivity.kt
+++ ui/ui-material/integration-tests/material-demos/src/main/java/androidx/ui/material/demos/ButtonActivity.kt
@@ -34,39 +34,126 @@ package androidx.ui.material.demos
 
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.foundation.Border
+import androidx.ui.foundation.shape.GenericShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.Image
+import androidx.ui.graphics.imageFromResource
 import androidx.ui.layout.Arrangement
-import androidx.ui.layout.Center
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutHeight
+import androidx.ui.layout.LayoutPadding
+import androidx.ui.layout.LayoutSize
+import androidx.ui.layout.LayoutWidth
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacer
 import androidx.ui.material.Button
+import androidx.ui.material.FloatingActionButton
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.OutlinedButton
 import androidx.ui.material.samples.ButtonSample
+import androidx.ui.material.samples.IconButtonSample
+import androidx.ui.material.samples.IconToggleButtonSample
 import androidx.ui.material.samples.OutlinedButtonSample
 import androidx.ui.material.samples.TextButtonSample
+import androidx.ui.unit.dp
 
 class ButtonActivity : MaterialDemoActivity() {
 
+    private val DefaultSpace = 20.dp
+
     @Composable
     override fun materialContent() {
-        Center {
-            Column(LayoutHeight.Fill, arrangement = Arrangement.SpaceEvenly) {
-                ButtonSample()
+        val icon = imageFromResource(resources, R.drawable.ic_favorite)
 
-                OutlinedButtonSample()
+        Column(LayoutPadding(10.dp)) {
+            Buttons()
+            Spacer(LayoutHeight(DefaultSpace))
+            Fabs(icon)
+            Spacer(LayoutHeight(DefaultSpace))
+            IconButtons()
+            Spacer(LayoutHeight(DefaultSpace))
+            CustomShapeButton()
+        }
+    }
 
-                TextButtonSample()
+    @Composable
+    private fun Buttons() {
+        Text("Buttons")
+        Spacer(LayoutHeight(DefaultSpace))
+        Row(LayoutWidth.Fill, arrangement = Arrangement.SpaceEvenly) {
+            ButtonSample()
+            OutlinedButtonSample()
+            TextButtonSample()
+        }
 
-                Button(backgroundColor = MaterialTheme.colors().secondary, onClick = {}) {
-                    Text("Secondary Color")
-                }
+        Spacer(LayoutHeight(DefaultSpace))
 
-                // TODO(Andrey): Disabled button has wrong bg and text color for now.
-                // Need to figure out where will we store their styling. Not a part of
-                // ColorPalette right now and specs are not clear about this.
-                Button {
-                    Text("DISABLED. TODO")
-                }
+        Row(LayoutWidth.Fill, arrangement = Arrangement.SpaceEvenly) {
+            Button(backgroundColor = MaterialTheme.colors().secondary, onClick = {}) {
+                Text("Secondary Color")
+            }
+            // TODO(Andrey): Disabled button has wrong bg and text color for now.
+            // Need to figure out where will we store their styling. Not a part of
+            // ColorPalette right now and specs are not clear about this.
+            Button {
+                Text("DISABLED. TODO")
             }
         }
+    }
+
+    @Composable
+    private fun Fabs(icon: Image) {
+        Text("Floating action buttons")
+        Spacer(LayoutHeight(DefaultSpace))
+
+        Row(LayoutWidth.Fill, arrangement = Arrangement.SpaceEvenly) {
+            FloatingActionButton(
+                icon = icon,
+                onClick = {}
+            )
+            FloatingActionButton(
+                text = "EXTENDED",
+                onClick = {}
+            )
+            FloatingActionButton(
+                icon = icon,
+                text = "ADD TO FAVS",
+                color = MaterialTheme.colors().error,
+                onClick = {}
+            )
+        }
+    }
+
+    @Composable
+    private fun IconButtons() {
+        Text("Icon buttons")
+        Spacer(LayoutHeight(DefaultSpace))
+
+        Row {
+            IconButtonSample()
+            IconToggleButtonSample()
+        }
+    }
+
+    @Composable
+    private fun CustomShapeButton() {
+        Text("Custom shape button")
+        Spacer(LayoutHeight(DefaultSpace))
+        OutlinedButton(
+            modifier = LayoutSize(100.dp),
+            onClick = {},
+            shape = TriangleShape,
+            backgroundColor = Color.Yellow,
+            border = Border(size = 2.dp, color = Color.Black)
+        ) {
+            Text("Ok")
+        }
+    }
+
+    private val TriangleShape = GenericShape { size ->
+        moveTo(size.width.value / 2f, 0f)
+        lineTo(size.width.value, size.height.value)
+        lineTo(0f, size.height.value)
     }
 }
