aca3d331ac3a7282cd75cbefdf2a5460181a2096
husaynhakeem <husaynhakeem@google.com>
2020-02-21 12:06:58 -0800

    Fix RotationTransformTest.
    RotationTransformTest was setting the screen orientation by calling setRequestedOrientation() on FakeActivity. The reversePortrait() test was failing, setRequestedOrientation(REVERSE_PORTRAIT) wasn’t rotating the display by 180 degrees. I read that this might be because the activity needs a certain screenOrientation flag to be set to sensorFull in the manifest to support all orientations.
    
    This CL tests RotationTransform in an other way, and makes sure the degrees it returns are correct.
    
    Bug: b/149937115
    Test: RotationTransformTest.java
    Change-Id: I4d231217ed00bb1db21f4540aece0c809264dc32

---
+++ camera/camera-view/src/test/java/androidx/camera/view/preview/transform/RotationTransformTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.view.preview.transform;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.os.Build;
+import android.view.Display;
+import android.view.Surface;
+import android.view.View;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.annotation.internal.DoNotInstrument;
+
+@SmallTest
+@RunWith(RobolectricTestRunner.class)
+@DoNotInstrument
+@Config(minSdk = Build.VERSION_CODES.LOLLIPOP)
+public class RotationTransformTest {
+
+    @Test
+    public void rotation_0() {
+        final View view = setUpView(Surface.ROTATION_0);
+        assertThat(RotationTransform.getRotationDegrees(view)).isEqualTo(0);
+    }
+
+    @Test
+    public void rotation_90() {
+        final View view = setUpView(Surface.ROTATION_90);
+        assertThat(RotationTransform.getRotationDegrees(view)).isEqualTo(90);
+    }
+
+    @Test
+    public void rotation_180() {
+        final View view = setUpView(Surface.ROTATION_180);
+        assertThat(RotationTransform.getRotationDegrees(view)).isEqualTo(180);
+    }
+
+    @Test
+    public void rotation_270() {
+        final View view = setUpView(Surface.ROTATION_270);
+        assertThat(RotationTransform.getRotationDegrees(view)).isEqualTo(270);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void unknown_rotation() {
+        final View view = setUpView(-1);
+        RotationTransform.getRotationDegrees(view);
+    }
+
+    private View setUpView(int rotation) {
+        final View view = mock(View.class);
+        final Display display = mock(Display.class);
+        when(view.getDisplay()).thenReturn(display);
+        when(display.getRotation()).thenReturn(rotation);
+        return view;
+    }
+}
