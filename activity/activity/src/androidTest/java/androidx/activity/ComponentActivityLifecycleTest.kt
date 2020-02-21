9d198991185b2196abb251043660cc0f4da0af6f
Ian Lake <ilake@google.com>
2020-02-21 13:51:22 -0800

    Remove reliance on Mockito and Espresso from tests
    Remove reliance on Mockito and unused Espresso
    dependency from activity tests.
    
    Test: tests still pass
    Change-Id: I796e85b52daed0d78db50a0ff4a8626aa3ef18f2

--- activity/activity/src/androidTest/java/androidx/activity/ComponentActivityLifecycleTest.kt
+++ activity/activity/src/androidTest/java/androidx/activity/ComponentActivityLifecycleTest.kt
@@ -19,16 +19,18 @@ package androidx.activity
 import android.os.Bundle
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.testutils.withActivity
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import org.mockito.Mockito.verifyNoMoreInteractions
+
+internal enum class LifecycleSource {
+    ACTIVITY,
+    ACTIVITY_CALLBACK
+}
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -37,81 +39,68 @@ class ComponentActivityLifecycleTest {
     @Test
     @Throws(Throwable::class)
     fun testLifecycleObserver() {
-        lateinit var activity: LifecycleComponentActivity
-        lateinit var activityCallbackLifecycleOwner: LifecycleOwner
-        lateinit var lifecycleObserver: LifecycleEventObserver
+        lateinit var events: List<Pair<LifecycleSource, Lifecycle.Event>>
         ActivityScenario.launch(LifecycleComponentActivity::class.java).use { scenario ->
-            activity = scenario.withActivity { this }
-            activityCallbackLifecycleOwner = activity.activityCallbackLifecycleOwner
-            lifecycleObserver = activity.lifecycleObserver
+            events = scenario.withActivity { this.events }
         }
 
         // The Activity's lifecycle callbacks should fire first,
         // followed by the activity's lifecycle observers
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_CREATE)
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_CREATE)
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_START)
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_START)
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_RESUME)
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_RESUME)
-        // Now the order reverses as things unwind
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_PAUSE)
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_PAUSE)
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_STOP)
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_STOP)
-        verify(lifecycleObserver)
-                .onStateChanged(activity, Lifecycle.Event.ON_DESTROY)
-        verify(lifecycleObserver)
-                .onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_DESTROY)
-        verifyNoMoreInteractions(lifecycleObserver)
+        assertThat(events)
+            .containsExactly(
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_CREATE,
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_CREATE,
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_START,
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_START,
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_RESUME,
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_RESUME,
+                // Now the order reverses as things unwind
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_PAUSE,
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_PAUSE,
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_STOP,
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_STOP,
+                LifecycleSource.ACTIVITY to Lifecycle.Event.ON_DESTROY,
+                LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_DESTROY
+            ).inOrder()
     }
 }
 
 class LifecycleComponentActivity : ComponentActivity() {
-    val activityCallbackLifecycleOwner: LifecycleOwner = mock(LifecycleOwner::class.java)
-    val lifecycleObserver: LifecycleEventObserver = mock(LifecycleEventObserver::class.java)
+    internal val events = mutableListOf<Pair<LifecycleSource, Lifecycle.Event>>()
 
     init {
-        lifecycle.addObserver(lifecycleObserver)
+        lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            events.add(LifecycleSource.ACTIVITY to event)
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_CREATE)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_CREATE)
     }
 
     override fun onStart() {
         super.onStart()
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_START)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_START)
     }
 
     override fun onResume() {
         super.onResume()
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_RESUME)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_RESUME)
     }
 
     override fun onPause() {
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_PAUSE)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_PAUSE)
         super.onPause()
     }
 
     override fun onStop() {
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_STOP)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_STOP)
         super.onStop()
     }
 
     override fun onDestroy() {
-        lifecycleObserver.onStateChanged(activityCallbackLifecycleOwner, Lifecycle.Event.ON_DESTROY)
+        events.add(LifecycleSource.ACTIVITY_CALLBACK to Lifecycle.Event.ON_DESTROY)
         super.onDestroy()
     }
 }
