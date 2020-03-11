235864448d4fda178ac08628c9db4ef126e9f4a8
Louis Pullen-Freilich <lpf@google.com>
2020-03-11 18:02:47 +0000

    The great demo migration
    - Provides common demo library for defining categories and demos
    - Rebuilds the base demo activity fully inside Compose, with built-in theme settings & system dark theme support
    
    Test: ./gradlew ui:integration-tests:demos:installDebug
    
    Change-Id: Ic3cdd619e444f6dd6e2d5c5dd24167d3d55f3292

---
+++ ui/integration-tests/demos/common/src/main/java/androidx/ui/demos/common/Demo.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.demos.common
+
+import android.app.Activity
+import androidx.compose.Composable
+import kotlin.reflect.KClass
+
+/**
+ * Generic demo with a [title] that will be displayed in the list of demos.
+ */
+sealed class Demo(val title: String)
+
+/**
+ * Demo that launches an [Activity] when selected.
+ *
+ * This should only be used for demos that need to customize the activity, the large majority of
+ * demos should just use [ComposableDemo] instead.
+ *
+ * @property activityClass the KClass (Foo::class) of the activity that will be launched when
+ * this demo is selected.
+ */
+class ActivityDemo <T : Activity> (title: String, val activityClass: KClass<T>) : Demo(title)
+
+/**
+ * Demo that displays [Composable] [content] when selected.
+ */
+class ComposableDemo(title: String, val content: @Composable() () -> Unit) : Demo(title)
+
+/**
+ * A category of [Demo]s, that will display a list of [demos] when selected.
+ */
+class DemoCategory(title: String, val demos: List<Demo>) : Demo(title)
