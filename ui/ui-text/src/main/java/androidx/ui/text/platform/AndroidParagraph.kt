93d87ceb70be17aefb7c84f510631c969e814bc6
Nader Jawad <njawad@google.com>
2020-03-18 17:30:41 -0700

    Created Path Interface
    Updated Path to remove hard dependency
    on android.graphics.Path. Replaced
    with Path interface and moved the implementation
    to AndroidPath to match Canvas and AndroidCanvas
    implementations.
    
    Test: Re-ran tests for all effected modules,
    ui-graphics, ui-text, ui-platform
    
    Change-Id: I673fa96267c4c9db7a975a56ea5c71a9ee695e1c

--- ui/ui-text/src/main/java/androidx/ui/text/platform/AndroidParagraph.kt
+++ ui/ui-text/src/main/java/androidx/ui/text/platform/AndroidParagraph.kt
@@ -34,6 +34,7 @@ import androidx.text.style.PlaceholderSpan
 import androidx.ui.geometry.Rect
 import androidx.ui.graphics.Canvas
 import androidx.ui.graphics.Path
+import androidx.ui.graphics.asComposePath
 import androidx.ui.text.AnnotatedString
 import androidx.ui.text.Paragraph
 import androidx.ui.text.ParagraphConstraints
@@ -223,7 +224,7 @@ internal class AndroidParagraph constructor(
         }
         val path = android.graphics.Path()
         layout.getSelectionPath(start, end, path)
-        return Path(path)
+        return path.asComposePath()
     }
 
     override fun getCursorRect(offset: Int): Rect {
