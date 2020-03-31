4ab866f1c89db7a9f7bc7b7d150c3e5503c4c462
Jeremy Woods <jbwoods@google.com>
2020-03-30 20:53:21 -0700

    Add support for passing results between Fragments
    Currently if developers would like to pass data between fragments that
    are in the same FragmentManager, they use the target fragment APIs.
    Using target fragments requires direct access to a fragment instance,
    which can be dangerous since the state of the target fragment is
    unknown.
    
    This change offers APIs to set a result on a Fragment and handle how
    that result is used at the proper time in the Fragment's lifecycle. The
    same usage pattern will work for passing data between any two fragments,
    including DialogFragments, and fragments used in Navigation.
    
    This change also includes a -ktx extension function to ensure kotlin
    users can pass in the FragmentResultListener as a hanging lambda.
    
    Test: added FragmentResultTest and FragmentResultOwnerTest
    Bug: 149787344
    RelNote: "Added support for passing results between two Fragments. This
    works for hierarchy fragments (parent child), DialogFragments, and
    fragments in Navigation."
    
    Change-Id: I0d9a1f1a66b872eb5ca08f132e4f7838abaad962

---
+++ fragment/fragment/src/androidTest/java/androidx/fragment/app/FragmentResultTest.kt
@@ -0,0 +1,245 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.fragment.app
+
+import android.os.Bundle
+import androidx.fragment.app.test.FragmentTestActivity
+import androidx.fragment.test.R
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.testutils.withActivity
+import com.google.common.truth.Truth.assertWithMessage
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class FragmentResultTest {
+
+    @Test
+    fun testReplaceResult() {
+        with(ActivityScenario.launch(FragmentTestActivity::class.java)) {
+            val fm = withActivity {
+                setContentView(R.layout.simple_container)
+                supportFragmentManager
+            }
+            val fragment1 = ResultFragment()
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment1)
+                .commit()
+            executePendingTransactions()
+
+            val fragment2 = StrictFragment()
+
+            fm.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment2)
+                .addToBackStack(null)
+                .commit()
+            executePendingTransactions()
+
+            val resultBundle = Bundle()
+            val expectedResult = "resultGood"
+            resultBundle.putString("bundleKey", expectedResult)
+
+            fm.setResult("requestKey", resultBundle)
+
+            assertWithMessage("The result is not set")
+                .that(fragment1.actualResult)
+                .isNull()
+
+            withActivity {
+                fm.popBackStackImmediate()
+            }
+
+            assertWithMessage("The result is incorrect")
+                .that(fragment1.actualResult)
+                .isEqualTo(expectedResult)
+        }
+    }
+
+    @Test
+    fun testSetResultWhileResumed() {
+        with(ActivityScenario.launch(FragmentTestActivity::class.java)) {
+            val fm = withActivity {
+                setContentView(R.layout.simple_container)
+                supportFragmentManager
+            }
+            val fragment1 = ResultFragment()
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment1)
+                .commit()
+            executePendingTransactions()
+
+            val fragment2 = StrictFragment()
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment2)
+                .commit()
+            executePendingTransactions()
+
+            val resultBundle = Bundle()
+            val expectedResult = "resultGood"
+            resultBundle.putString("bundleKey", expectedResult)
+
+            fm.setResult("requestKey", resultBundle)
+
+            assertWithMessage("The result is incorrect")
+                .that(fragment1.actualResult)
+                .isEqualTo(expectedResult)
+        }
+    }
+
+    @Test
+    fun testStoredSetResultWhileResumed() {
+        with(ActivityScenario.launch(FragmentTestActivity::class.java)) {
+            val fm = withActivity {
+                setContentView(R.layout.simple_container)
+                supportFragmentManager
+            }
+            val fragment1 = StrictFragment()
+            var actualResult: String? = null
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment1)
+                .commit()
+            executePendingTransactions()
+
+            val resultBundle = Bundle()
+            val expectedResult = "resultGood"
+            resultBundle.putString("bundleKey", expectedResult)
+
+            fm.setResult("requestKey", resultBundle)
+
+            fm.setResultListener("requestKey", fragment1, FragmentResultListener
+            { _, bundle -> actualResult = bundle.getString("bundleKey") })
+
+            assertWithMessage("The result is incorrect")
+                .that(actualResult)
+                .isEqualTo(expectedResult)
+        }
+    }
+
+    @Test
+    fun testReplaceResultSavedRestore() {
+        with(ActivityScenario.launch(FragmentTestActivity::class.java)) {
+            var fm = withActivity {
+                setContentView(R.layout.simple_container)
+                supportFragmentManager
+            }
+            var fragment1 = ResultFragment()
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment1, "fragment1")
+                .commit()
+            executePendingTransactions()
+
+            val fragment2 = StrictFragment()
+
+            fm.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment2)
+                .addToBackStack(null)
+                .commit()
+            executePendingTransactions()
+
+            val resultBundle = Bundle()
+            val expectedResult = "resultGood"
+            resultBundle.putString("bundleKey", expectedResult)
+
+            fm.setResult("requestKey", resultBundle)
+
+            assertWithMessage("The result is not set")
+                .that(fragment1.actualResult)
+                .isNull()
+
+            recreate()
+
+            fm = withActivity { supportFragmentManager }
+
+            withActivity {
+                fm.popBackStackImmediate()
+            }
+
+            fragment1 = fm.findFragmentByTag("fragment1") as ResultFragment
+
+            assertWithMessage("The result is incorrect")
+                .that(fragment1.actualResult)
+                .isEqualTo(expectedResult)
+        }
+    }
+
+    @Test
+    fun testChildFragmentResult() {
+        with(ActivityScenario.launch(FragmentTestActivity::class.java)) {
+            val fm = withActivity {
+                setContentView(R.layout.simple_container)
+                supportFragmentManager
+            }
+            val parent = ParentResultFragment()
+
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, parent)
+                .commit()
+            executePendingTransactions()
+
+            val child = ChildResultFragment()
+
+            parent.childFragmentManager.beginTransaction()
+                .add(child, "child")
+                .commit()
+            executePendingTransactions()
+
+            assertWithMessage("The result is incorrect")
+                .that(parent.actualResult)
+                .isEqualTo("resultGood")
+        }
+    }
+}
+
+class ResultFragment : StrictFragment() {
+    var actualResult: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        parentFragmentManager.setResultListener("requestKey", this, FragmentResultListener
+        { _, bundle -> actualResult = bundle.getString("bundleKey") })
+    }
+}
+
+class ParentResultFragment : StrictFragment() {
+    var actualResult: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        childFragmentManager.setResultListener("requestKey", this, FragmentResultListener
+        { _, bundle -> actualResult = bundle.getString("bundleKey") })
+    }
+}
+
+class ChildResultFragment : StrictFragment() {
+    override fun onStart() {
+        super.onStart()
+        val resultBundle = Bundle().apply {
+            putString("bundleKey", "resultGood")
+        }
+        parentFragmentManager.setResult("requestKey", resultBundle)
+    }
+}
