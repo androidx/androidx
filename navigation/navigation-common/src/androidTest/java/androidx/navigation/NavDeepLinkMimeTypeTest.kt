1b403fbbb5085738d7fd2f203ecd3df43523f502
Jeremy Woods <jbwoods@google.com>
2020-04-06 17:56:22 -0700

    Add action and mimetype support for NavDeepLink
    Currently only Uris are supported for navigation deeplinks. We should
    add support for intent actions and mimetypes. This will allow for
    additional flexibility when using navigation.
    
    This change makes NavDeepLink public and adds a complimentary
    NavDeepLinkRequest class.
    
    Test: NavDeepLinkActionTest and NavDeepLinkMimeTypeTest
    Bug: 136573074
    Bug: 135334841
    Change-Id: I2cbebdd314e25254dd3a713490b02d07ab8dacd8

---
+++ navigation/navigation-common/src/androidTest/java/androidx/navigation/NavDeepLinkMimeTypeTest.kt
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.navigation
+
+import android.net.Uri
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
+import org.junit.Assert.fail
+import org.junit.Test
+
+@SmallTest
+class NavDeepLinkMimeTypeTest {
+    companion object {
+        private const val DEEP_LINK_EXACT_HTTPS = "https://www.example.com"
+    }
+
+    @Test
+    fun deepLinkInvalidMimeType() {
+        val mimeType = "noSlash"
+        try {
+            NavDeepLink(null, null, mimeType)
+            fail("NavDeepLink must throw")
+        } catch (e: IllegalArgumentException) {
+            assertThat(e)
+                .hasMessageThat().contains(
+                    "The given mimeType $mimeType does not match to required \"type/subtype\" " +
+                            "format"
+                )
+        }
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatch() {
+        val mimeType = "*/*"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, mimeType)))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchWithUri() {
+        val mimeType = "*/*"
+        val deepLink = NavDeepLink(DEEP_LINK_EXACT_HTTPS, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                Uri.parse(DEEP_LINK_EXACT_HTTPS), null, mimeType)))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchDeepLinkWildCard() {
+        val mimeType = "*/*"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "type/subtype")))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchTypeDeepLinkWildCard() {
+        val mimeType = "*/subtype"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "type/subtype")))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchSubtypeDeepLinkWildCard() {
+        val mimeType = "type/*"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "type/subtype")))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchRequestWildCard() {
+        val mimeType = "type/subtype"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "*/*")))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchSubtypeRequestWildCard() {
+        val mimeType = "type/subtype"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "type/*")))
+            .isTrue()
+    }
+
+    @Test
+    fun deepLinkMimeTypeMatchTypeRequestWildCard() {
+        val mimeType = "type/subtype"
+        val deepLink = NavDeepLink(null, null, mimeType)
+
+        assertWithMessage("The mimeTypes should match")
+            .that(deepLink.matches(NavDeepLinkRequest(
+                null, null, "*/subtype")))
+            .isTrue()
+    }
+}
