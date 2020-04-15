0421f7126ccc9799b339c53343adee1c77b06185
Amaury Medeiros <amaurym@google.com>
2020-04-15 14:53:18 +0100

    Support TransitionAnimation tracking in ui-tooling
    Make sure that TransitionAnimations are parsed into ComposeAnimations and
    notified to Android Studio when subscribing to the PreviewAnimationClock.
    This way, the Animation Inspector will be able to track and display these
    animations and their corresponding states.
    
    From the Animation Inspector in Android Studio, users will be able to see
    and play around with all the TransitionAnimation in the same @Composable
    tree, setting the from and to states of each one individually and analyzing
    how they animate simultaneously by manipulating a timeline that will control
    the PreviewAnimationClock time. In addition to that, users will be able to
    inspect the PropKeys being animated as well as their value at a given time.
    
    The main changes made to PreviewAnimationClock to achieve that were:
    
    * notifySubscribe and notifyUnsubscribe now receive a ComposeAnimation
      object instead of a AnimationClockObserver, making it much easier to
      handle in Android Studio.
    * Parse TransitionAnimationClockObserver into ComposeAnimation in
      notifySubscribe to notify Android Studio when TransitionAnimations are
      subscribed.
    
    In addition to that, the following new APIs had to be added to make it
    possible to track and manipulate TransitionAnimations:
    
    * updateSeekableAnimation: updates the cached SeekableAnimation associated
      with the given ComposeAnimation. The SeekableAnimation is used to get the
      animation properties in setClockTime.
    * updateAnimationStates: updates the from and to states of the actual
      TransitionAnimations using the values from the cached SeekableAnimations,
      and resets the clock so all the animations go to the initial state.
    * getMaxDuration: returns the time of the longest TransitionAnimation being
      tracked, taking into account the current from and to states.
    * getAnimatedProperties: receives a ComposeAnimation and returns a list of
      label -> value pairs corresponding to an animated property (e.g. PropKey)
      label and its value at the current time.
    
    Relnote: N/A
    Test: PreviewAnimationClockTest
    Bug: 160267340
    Bug: 157896171
    Change-Id: Ie8e05b5996fc541868a9fbf8aacdca361b667ea0

--- ui/ui-tooling/src/androidTest/java/androidx/ui/tooling/preview/animation/PreviewAnimationClockTest.kt
+++ ui/ui-tooling/src/androidTest/java/androidx/ui/tooling/preview/animation/PreviewAnimationClockTest.kt
@@ -17,12 +17,32 @@
 package androidx.ui.tooling.preview.animation
 
 import androidx.animation.AnimationClockObserver
+import androidx.animation.FloatPropKey
+import androidx.animation.InternalAnimationApi
+import androidx.animation.LinearEasing
+import androidx.animation.TransitionAnimation
+import androidx.animation.transitionDefinition
+import androidx.animation.tween
+import androidx.ui.animation.ColorPropKey
+import androidx.ui.animation.tooling.ComposeAnimation
+import androidx.ui.animation.tooling.ComposeAnimationType
+import androidx.ui.graphics.Color
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
+@OptIn(InternalAnimationApi::class)
 class PreviewAnimationClockTest {
+
+    private lateinit var testClock: TestPreviewAnimationClock
+
+    @Before
+    fun setUp() {
+        testClock = TestPreviewAnimationClock()
+    }
+
     @Test
     fun setClockTimeIsRelative() {
         val previewAnimationClock = PreviewAnimationClock(100)
@@ -31,33 +51,242 @@ class PreviewAnimationClockTest {
     }
 
     @Test
-    fun subscribeAndUnsubscribeShouldNotify() {
+    fun subscribeAndUnsubscribeTransitionAnimationShouldNotify() {
+        val anim = TransitionAnimation(offsetDef, testClock)
+        assertEquals(0, testClock.subscribeCount)
+        assertEquals(0, testClock.unsubscribeCount)
+
+        // Force subscription
+        anim.toState(Offset.O2)
+        assertEquals(1, testClock.subscribeCount)
+
+        // Force unsubscription.
+        anim.snapToState(Offset.O1)
+        assertEquals(1, testClock.unsubscribeCount)
+
+        val subscribedAnimation = testClock.subscribedAnimation
+        assertEquals(subscribedAnimation, testClock.unsubscribedAnimation)
+
+        // Check the animation is a non-monotonic TransitionAnimation
+        assertEquals(ComposeAnimationType.TRANSITION_ANIMATION, subscribedAnimation.getType())
+        val animation = subscribedAnimation.getAnimation() as TransitionAnimation<*>
+        assertEquals(anim, animation)
+        assertFalse(animation.monotonic)
+        val states = subscribedAnimation.getStates()
+        assertEquals(2, states.size)
+        assertTrue(states.contains(Offset.O1))
+        assertTrue(states.contains(Offset.O2))
+    }
+
+    @Test
+    fun unsupportedObserverShouldNotNotify() {
         val observer = object : AnimationClockObserver {
             override fun onAnimationFrame(frameTimeMillis: Long) {
                 // Do nothing
             }
         }
 
-        val previewAnimationClock = TestPreviewAnimationClock()
-        assertFalse(previewAnimationClock.subscribed)
-        previewAnimationClock.subscribe(observer)
-        assertTrue(previewAnimationClock.subscribed)
+        assertEquals(0, testClock.subscribeCount)
+        assertEquals(0, testClock.notifySubscribeCount)
+        testClock.subscribe(observer)
+        assertEquals(1, testClock.subscribeCount)
+        assertEquals(0, testClock.notifySubscribeCount)
 
-        assertFalse(previewAnimationClock.unsubscribed)
-        previewAnimationClock.unsubscribe(observer)
-        assertTrue(previewAnimationClock.unsubscribed)
+        assertEquals(0, testClock.unsubscribeCount)
+        assertEquals(0, testClock.notifyUnsubscribeCount)
+        testClock.unsubscribe(observer)
+        assertEquals(1, testClock.unsubscribeCount)
+        assertEquals(0, testClock.notifyUnsubscribeCount)
+    }
+
+    @Test
+    fun disposeClearsCachedAnimations() {
+        setUpOffsetScenario()
+
+        assertFalse(testClock.observersToAnimations.isEmpty())
+        assertFalse(testClock.seekableAnimations.isEmpty())
+
+        testClock.dispose()
+
+        assertTrue(testClock.observersToAnimations.isEmpty())
+        assertTrue(testClock.seekableAnimations.isEmpty())
+    }
+
+    @Test
+    fun updateSeekableAnimationModifiesCachedValue() {
+        val seekableAnimation = testClock.seekableAnimations[setUpRotationColorScenario()]!!
+
+        assertEquals(RotationColor.RC1, seekableAnimation.fromState)
+        assertEquals(RotationColor.RC3, seekableAnimation.toState)
+    }
+
+    @Test
+    fun updateAnimationStatesDoesNotResubscribe() {
+        setUpRotationColorScenario()
+
+        // The animation in the scenario should have been subscribed and properly notified.
+        assertEquals(1, testClock.subscribeCount)
+        assertEquals(1, testClock.notifySubscribeCount)
+
+        testClock.updateAnimationStates()
+        // subscribe is called when updating the states, but since it follows an unsubscription,
+        // we skip the method behavior and, for instance, don't notify the subscription again.
+        assertEquals(2, testClock.subscribeCount)
+        assertEquals(1, testClock.notifySubscribeCount)
+    }
+
+    @Test
+    fun updateAnimationStatesResetsClock() {
+        setUpRotationColorScenario()
+        testClock.setClockTime(123)
+        assertEquals(123, testClock.clock.clockTimeMillis)
+
+        testClock.updateAnimationStates()
+        assertEquals(0, testClock.clock.clockTimeMillis)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @Test
+    fun updateAnimationStatesUpdatesAllTransitionAnimations() {
+        val rotationAnimation =
+            setUpRotationColorScenario().getAnimation() as TransitionAnimation<RotationColor>
+        val offsetAnimation = setUpOffsetScenario().getAnimation() as TransitionAnimation<Offset>
+
+        testClock.updateAnimationStates()
+
+        // offset animates from O1 (0) and rotation animates from RC1 (0)
+        assertEquals(0f, offsetAnimation[offset], eps)
+        assertEquals(0f, rotationAnimation[rotation], eps)
+        // Animations take 800ms and 1000ms, so setting the clock to 1000 should make both finish
+        testClock.setClockTime(1000)
+        // offset animates to O2 (100) and rotation animates to RC3 (360)
+        assertEquals(100f, offsetAnimation[offset], eps)
+        assertEquals(360f, rotationAnimation[rotation], eps)
+    }
+
+    @Test
+    fun getAnimatedPropertiesReturnsValuesAtCurrentTime() {
+        val rotationAnimation = setUpRotationColorScenario()
+        val offsetAnimation = setUpOffsetScenario()
+        testClock.setClockTime(200)
+        var animatedProperties = testClock.getAnimatedProperties(rotationAnimation)
+
+        val color = animatedProperties.single { it.first == "borderColor" }
+        // We're animating from RC1 (Red) to RC3 (Green). Since there is no transition defined
+        // for the color property, we snap to the end state.
+        assertEquals(Color.Green, color.second)
+
+        val rotation = animatedProperties.single { it.first == "myRotation" }
+        // We're animating from RC1 (0 degrees) to RC3 (360 degrees). There is a transition of
+        // 1000ms defined for the rotation, and we set the clock to 20% of this time.
+        assertEquals(72f, rotation.second as Float, eps)
+
+        animatedProperties = testClock.getAnimatedProperties(offsetAnimation)
+        val offset = animatedProperties.single()
+        // We're animating from O1 (0) to O2 (100). There is a transition of 800ms defined for
+        // the offset, and we set the clock to 25% of this time.
+        assertEquals(25f, offset.second as Float, eps)
+    }
+
+    @Test
+    fun maxDurationReturnsLongestDuration() {
+        // When there are no animations, we should return an invalid duration.
+        assertTrue(testClock.getMaxDuration() < 0)
+        setUpRotationColorScenario() // 1000ms
+        setUpOffsetScenario() // 800ms
+
+        assertEquals(1000, testClock.getMaxDuration())
+    }
+
+    // Sets up a transition animation scenario, going from RotationColor.RC1 to RotationColor.RC3.
+    private fun setUpRotationColorScenario(): ComposeAnimation {
+        TransitionAnimation(rotationColorDef, testClock).toState(RotationColor.RC2)
+        val composeAnimation = testClock.observersToAnimations.values.single {
+            it.getStates().contains(RotationColor.RC1)
+        }
+        testClock.updateSeekableAnimation(composeAnimation, RotationColor.RC1, RotationColor.RC3)
+        return composeAnimation
+    }
+
+    // Sets up a transition animation scenario, going from from Offset.O1 to Offset.O2.
+    private fun setUpOffsetScenario(): ComposeAnimation {
+        TransitionAnimation(offsetDef, testClock).toState(Offset.O2)
+        val composeAnimation = testClock.observersToAnimations.values.single {
+            it.getStates().contains(Offset.O1)
+        }
+        testClock.updateSeekableAnimation(composeAnimation, Offset.O1, Offset.O2)
+        return composeAnimation
     }
 
     private inner class TestPreviewAnimationClock : PreviewAnimationClock(0) {
-        var subscribed = false
-        var unsubscribed = false
+        lateinit var subscribedAnimation: ComposeAnimation
+        lateinit var unsubscribedAnimation: ComposeAnimation
+        var subscribeCount = 0
+        var notifySubscribeCount = 0
+        var unsubscribeCount = 0
+        var notifyUnsubscribeCount = 0
 
-        override fun notifySubscribe(observer: AnimationClockObserver) {
-            subscribed = true
+        override fun notifySubscribe(animation: ComposeAnimation) {
+            subscribedAnimation = animation
+            notifySubscribeCount++
         }
 
-        override fun notifyUnsubscribe(observer: AnimationClockObserver) {
-            unsubscribed = true
+        override fun notifyUnsubscribe(animation: ComposeAnimation) {
+            unsubscribedAnimation = animation
+            notifyUnsubscribeCount++
         }
+
+        override fun subscribe(observer: AnimationClockObserver) {
+            super.subscribe(observer)
+            subscribeCount++
+        }
+
+        override fun unsubscribe(observer: AnimationClockObserver) {
+            super.unsubscribe(observer)
+            unsubscribeCount++
+        }
+    }
+}
+
+private enum class Offset { O1, O2 }
+
+private enum class RotationColor { RC1, RC2, RC3 }
+
+private val rotation = FloatPropKey(label = "myRotation")
+private val offset = FloatPropKey(label = "myOffset")
+private val color = ColorPropKey(label = "borderColor")
+
+private val offsetDef = transitionDefinition {
+    state(Offset.O1) {
+        this[offset] = 0f
+    }
+    state(Offset.O2) {
+        this[offset] = 100f
+    }
+
+    transition {
+        offset using tween(durationMillis = 800, easing = LinearEasing)
+    }
+}
+
+private const val eps = 0.00001f
+
+private val rotationColorDef = transitionDefinition {
+    state(RotationColor.RC1) {
+        this[rotation] = 0f
+        this[color] = Color.Red
+    }
+    state(RotationColor.RC2) {
+        this[rotation] = 180f
+        this[color] = Color.Blue
+    }
+
+    state(RotationColor.RC3) {
+        this[rotation] = 360f
+        this[color] = Color.Green
+    }
+
+    transition {
+        rotation using tween(durationMillis = 1000, easing = LinearEasing)
     }
 }
