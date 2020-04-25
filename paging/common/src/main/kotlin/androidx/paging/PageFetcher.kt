be1814d5382c6bdc21fa9281ecf9f33e90dd0329
Dustin Lam <dustinlam@google.com>
2020-04-24 19:45:57 -0700

    Move RemoteMediatorAccessor into its own file and add tests
    Fixes: 153916065
    Fixes: 155105280
    Test: ./gradlew paging:paging-common:test
    Change-Id: I381c5d3f7eb488e9ba5a0fc80a42acb54a836752

--- paging/common/src/main/kotlin/androidx/paging/PageFetcher.kt
+++ paging/common/src/main/kotlin/androidx/paging/PageFetcher.kt
@@ -16,17 +16,9 @@
 
 package androidx.paging
 
-import androidx.paging.LoadType.APPEND
-import androidx.paging.LoadType.PREPEND
-import androidx.paging.LoadType.REFRESH
-import androidx.paging.RemoteMediator.InitializeAction
 import androidx.paging.RemoteMediator.InitializeAction.LAUNCH_INITIAL_REFRESH
-import androidx.paging.RemoteMediator.MediatorResult
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.flow.Flow
@@ -37,9 +29,6 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.scan
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 internal class PageFetcher<Key : Any, Value : Any>(
@@ -67,7 +56,7 @@ internal class PageFetcher<Key : Any, Value : Any>(
     val flow: Flow<PagingData<Value>> = channelFlow {
         refreshChannel.asFlow()
             .onStart {
-                emit(remoteMediatorAccessor?.onInitialize() == LAUNCH_INITIAL_REFRESH)
+                emit(remoteMediatorAccessor?.initialize() == LAUNCH_INITIAL_REFRESH)
             }
             .scan(null) { previousGeneration: PageFetcherSnapshot<Key, Value>?,
                           triggerRemoteRefresh ->
@@ -120,65 +109,5 @@ internal class PageFetcher<Key : Any, Value : Any>(
         }
 
         override fun refresh() = this@PageFetcher.refresh()
-    }
-}
-
-/**
- * Usage of [RemoteMediator] within [PageFetcher] and [PageFetcherSnapshot] should always be
- * accessed behind this class, which handles state tracking of active remote jobs.
- */
-internal class RemoteMediatorAccessor<Key : Any, Value : Any>(
-    private val remoteMediator: RemoteMediator<Key, Value>
-) {
-    private val jobsByLoadTypeLock = Mutex()
-    private val jobsByLoadType = HashMap<LoadType, Deferred<MediatorResult>>()
-
-    suspend fun onInitialize(): InitializeAction {
-        return remoteMediator.initialize()
-    }
-
-    /**
-     * Launches a remote load request with the backing [MediatorResult] if no current
-     * [kotlinx.coroutines.Job] for the passed [LoadType] is running, otherwise returns the
-     * result of the existing [kotlinx.coroutines.Job].
-     */
-    internal suspend fun load(
-        scope: CoroutineScope,
-        loadType: LoadType,
-        state: PagingState<Key, Value>
-    ): MediatorResult {
-        val deferred = jobsByLoadTypeLock.withLock {
-            if (jobsByLoadType[loadType]?.isActive != true) {
-                // List of RemoteMediator.load jobs that were registered prior to this one.
-                val existingJobs = jobsByLoadType.values.toList() // Immutable copy.
-                val existingBoundaryJobs = listOfNotNull(
-                    jobsByLoadType[PREPEND],
-                    jobsByLoadType[APPEND]
-                )
-
-                jobsByLoadType[loadType] = scope.async {
-                    if (loadType == REFRESH) {
-                        // Since RemoteMediator is expected to perform writes to the local DB
-                        // in the common case, it's not safe to just cancel and proceed with
-                        // REFRESH here. If we do that, the REFRESH could race with e.g. the
-                        // START job, and it's unsafe for an old START to land in the DB after
-                        // a newer REFRESH. Due to cooperative cancellation, the START job may
-                        // not actually realize it's cancelled before performing its write.
-                        existingBoundaryJobs.forEach { it.cancel() }
-                        existingBoundaryJobs.joinAll()
-                    }
-
-                    // Only allow one active RemoteMediator.load at a time, by joining all jobs
-                    // registered in jobsByLoadType before this one.
-                    existingJobs.forEach { it.join() }
-
-                    remoteMediator.load(loadType, state)
-                }
-            }
-
-            jobsByLoadType.getValue(loadType)
-        }
-
-        return deferred.await()
     }
 }
