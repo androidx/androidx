a1ab18da271236ddcc565fe5648e1dfc7482bc93
Jeremy Woods <jbwoods@google.com>
2020-05-19 17:51:58 -0700

    Add DoubleTransition sample app to Fragment integration
    This app tests what happens when two fragment enter at the same time
    with two different transitions.
    
    Test: ./gradlew bOS
    Change-Id: Ie8990549a40a589abb47e3e5b2033b8fc2e6f31a

---
+++ fragment/integration-tests/testapp/src/main/java/androidx/fragment/testapp/doubleTransitionBug/DoubleTransitionBugActivity.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.fragment.testapp.doubleTransitionBug
+
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.commit
+import androidx.fragment.testapp.R
+import androidx.transition.Fade
+import androidx.transition.Slide
+
+class DoubleTransitionBugActivity : FragmentActivity(R.layout.double_transition_bug_activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.content, Fragment(R.layout.double_transition_bug_fragment_second))
+            }
+        }
+
+        findViewById<Button>(R.id.important_button).setOnClickListener { switchFragment() }
+    }
+
+    private fun switchFragment() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.content)
+
+        currentFragment!!.exitTransition = Fade()
+
+        val second = Fragment(R.layout.double_transition_bug_fragment_second)
+        second.enterTransition = Slide(Gravity.BOTTOM)
+
+        val first = Fragment(R.layout.double_transition_bug_fragment_first)
+        first.enterTransition = Fade().setDuration(5000)
+
+        supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.content, first)
+                .add(R.id.content, second)
+                .hide(currentFragment)
+                .addToBackStack(null)
+                .commit()
+    }
+}
