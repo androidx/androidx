bcae0e9d6cfb969b9f365a1be938c3f67c4decd0
Jin Seok Park <jinpark@google.com>
2020-05-21 14:02:57 +0900

    Add isSelectable to TrackInfo
    Bug: 143152618
    Test: ./media2/session/version-compat-tests/runtest.sh 4
          ./gradlew :media2:media2-player:connectedCheck
    Change-Id: I8663739376c4170051fd9a1d0b5356152270b39a

--- media2/player/src/androidTest/java/androidx/media2/player/MediaPlayerTest.java
+++ media2/player/src/androidTest/java/androidx/media2/player/MediaPlayerTest.java
@@ -806,6 +806,11 @@ public class MediaPlayerTest extends MediaPlayerTestBase {
         readTracks();
         assertEquals(2, mSubtitleTrackInfos.size());
 
+        // Test isSelectable
+        assertFalse(mVideoTrackInfos.get(0).isSelectable());
+        assertTrue(mSubtitleTrackInfos.get(0).isSelectable());
+        assertTrue(mSubtitleTrackInfos.get(1).isSelectable());
+
         mPlayer.reset();
     }
 
@@ -843,6 +848,10 @@ public class MediaPlayerTest extends MediaPlayerTestBase {
         assertEquals(1, mVideoTrackInfos.size());
         assertEquals(1, mAudioTrackInfos.size());
         assertEquals(0, mSubtitleTrackInfos.size());
+
+        // Test isSelectable
+        assertFalse(mVideoTrackInfos.get(0).isSelectable());
+        assertTrue(mAudioTrackInfos.get(0).isSelectable());
 
         // Test getSelectedTrack
         assertEquals(mVideoTrackInfos.get(0),
