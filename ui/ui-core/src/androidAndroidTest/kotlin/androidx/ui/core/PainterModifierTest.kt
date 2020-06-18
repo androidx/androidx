2d7bb9beb5c3ca47b30c5c699bd051f7048bdefa
Nader Jawad <njawad@google.com>
2020-06-17 21:03:24 -0700

    Fixed issue where ContentScale would not influence Image size
    Updated PainterModifier to incorporate
    the ContentScale algorithm in determining
    the corresponding sizing constraints
    
    Also updated PainterModifierTest to avoid
    sampling pixels values on edges which might
    lead to test flakiness on different screen
    densities.
    
    Updated PainterModifierTest to use ComposeTestRule
    instead of ActivityTest
    
    Change-Id: Id3b536944d6f0536ec05ff397f06b190472b4a96
    Fixes: 158559631
    Test: Added test to PainterModifierTest

--- ui/ui-core/src/androidAndroidTest/kotlin/androidx/ui/core/PainterModifierTest.kt
+++ ui/ui-core/src/androidAndroidTest/kotlin/androidx/ui/core/PainterModifierTest.kt
@@ -26,8 +26,7 @@ import androidx.ui.core.test.AtLeastSize
 import androidx.ui.core.test.FixedSize
 import androidx.ui.core.test.Padding
 import androidx.ui.core.test.background
-import androidx.ui.core.test.waitAndScreenShot
-import androidx.ui.framework.test.TestActivity
+import androidx.ui.foundation.Box
 import androidx.ui.geometry.Size
 import androidx.ui.graphics.BlendMode
 import androidx.ui.graphics.Color
@@ -37,8 +36,19 @@ import androidx.ui.graphics.compositeOver
 import androidx.ui.graphics.drawscope.DrawScope
 import androidx.ui.graphics.painter.Painter
 import androidx.ui.graphics.toArgb
+import androidx.ui.layout.height
+import androidx.ui.layout.heightIn
 import androidx.ui.layout.ltr
 import androidx.ui.layout.rtl
+import androidx.ui.layout.width
+import androidx.ui.layout.widthIn
+import androidx.ui.layout.wrapContentHeight
+import androidx.ui.test.assertHeightIsEqualTo
+import androidx.ui.test.assertWidthIsEqualTo
+import androidx.ui.test.captureToBitmap
+import androidx.ui.test.createComposeRule
+import androidx.ui.test.findRoot
+import androidx.ui.unit.dp
 import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -58,16 +68,12 @@ class PainterModifierTest {
     val containerWidth = 100.0f
     val containerHeight = 100.0f
 
-    @Suppress("DEPRECATION")
     @get:Rule
-    val rule = androidx.test.rule.ActivityTestRule<TestActivity>(TestActivity::class.java)
-    private lateinit var activity: TestActivity
+    val rule = createComposeRule()
     private lateinit var drawLatch: CountDownLatch
 
     @Before
     fun setup() {
-        activity = rule.activity
-        activity.hasFocusLatch
         drawLatch = CountDownLatch(1)
     }
 
@@ -75,13 +81,11 @@ class PainterModifierTest {
     @Test
     fun testPainterModifierColorFilter() {
         val paintLatch = CountDownLatch(1)
-        rule.runOnUiThread {
-            activity.setContent {
-                testPainter(
-                    colorFilter = ColorFilter(Color.Cyan, BlendMode.srcIn),
-                    latch = paintLatch
-                )
-            }
+        rule.setContent {
+            testPainter(
+                colorFilter = ColorFilter(Color.Cyan, BlendMode.srcIn),
+                latch = paintLatch
+            )
         }
 
         paintLatch.await(5, TimeUnit.SECONDS)
@@ -98,13 +102,11 @@ class PainterModifierTest {
     @Test
     fun testPainterModifierAlpha() {
         val paintLatch = CountDownLatch(1)
-        rule.runOnUiThread {
-            activity.setContent {
-                testPainter(
-                    alpha = 0.5f,
-                    latch = paintLatch
-                )
-            }
+        rule.setContent {
+            testPainter(
+                alpha = 0.5f,
+                latch = paintLatch
+            )
         }
 
         paintLatch.await(5, TimeUnit.SECONDS)
@@ -132,13 +134,11 @@ class PainterModifierTest {
     @Test
     fun testPainterModifierRtl() {
         val paintLatch = CountDownLatch(1)
-        rule.runOnUiThread {
-            activity.setContent {
-                testPainter(
-                    rtl = true,
-                    latch = paintLatch
-                )
-            }
+        rule.setContent {
+            testPainter(
+                rtl = true,
+                latch = paintLatch
+            )
         }
 
         paintLatch.await(5, TimeUnit.SECONDS)
@@ -156,25 +156,23 @@ class PainterModifierTest {
     fun testPainterAspectRatioMaintainedInSmallerParent() {
         val paintLatch = CountDownLatch(1)
         val containerSizePx = containerWidth.roundToInt() * 3
-        rule.runOnUiThread {
-            activity.setContent {
-                FixedSize(size = containerSizePx, modifier = Modifier.background(Color.White)) {
-                    // Verify that the contents are scaled down appropriately even though
-                    // the Painter's intrinsic width and height is twice that of the component
-                    // it is to be drawn into
-                    Padding(containerWidth.roundToInt()) {
-                        AtLeastSize(size = containerWidth.roundToInt(),
-                            modifier = Modifier.paint(
-                                LatchPainter(
-                                    containerWidth * 2,
-                                    containerHeight * 2,
-                                    paintLatch
-                                ),
-                                alignment = Alignment.Center,
-                                contentScale = ContentScale.Inside
-                            )
-                        ) {
-                        }
+        rule.setContent {
+            FixedSize(size = containerSizePx, modifier = Modifier.background(Color.White)) {
+                // Verify that the contents are scaled down appropriately even though
+                // the Painter's intrinsic width and height is twice that of the component
+                // it is to be drawn into
+                Padding(containerWidth.roundToInt()) {
+                    AtLeastSize(size = containerWidth.roundToInt(),
+                        modifier = Modifier.paint(
+                            LatchPainter(
+                                containerWidth * 2,
+                                containerHeight * 2,
+                                paintLatch
+                            ),
+                            alignment = Alignment.Center,
+                            contentScale = ContentScale.Inside
+                        )
+                    ) {
                     }
                 }
             }
@@ -202,21 +200,19 @@ class PainterModifierTest {
     fun testPainterAlignedBottomRightIfSmallerThanParent() {
         val paintLatch = CountDownLatch(1)
         val containerSizePx = containerWidth.roundToInt() * 2
-        rule.runOnUiThread {
-            activity.setContent {
-                AtLeastSize(size = containerWidth.roundToInt() * 2,
-                    modifier = Modifier.background(Color.White).paint(
-                        LatchPainter(
-                            containerWidth,
-                            containerHeight,
-                            paintLatch
-                        ),
-                        alignment = Alignment.BottomEnd,
-                        contentScale = ContentScale.Inside
-                    )
-                ) {
-                    // Intentionally empty
-                }
+        rule.setContent {
+            AtLeastSize(size = containerWidth.roundToInt() * 2,
+                modifier = Modifier.background(Color.White).paint(
+                    LatchPainter(
+                        containerWidth,
+                        containerHeight,
+                        paintLatch
+                    ),
+                    alignment = Alignment.BottomEnd,
+                    contentScale = ContentScale.Inside
+                )
+            ) {
+                // Intentionally empty
             }
         }
 
@@ -232,8 +228,8 @@ class PainterModifierTest {
         ).apply {
             assertEquals(Color.Red.toArgb(), getPixel(right, bottom))
             assertEquals(Color.Red.toArgb(), getPixel(innerBoxLeft, bottom))
-            assertEquals(Color.Red.toArgb(), getPixel(innerBoxLeft, innerBoxTop))
-            assertEquals(Color.Red.toArgb(), getPixel(right, innerBoxTop))
+            assertEquals(Color.Red.toArgb(), getPixel(innerBoxLeft, innerBoxTop + 1))
+            assertEquals(Color.Red.toArgb(), getPixel(right, innerBoxTop + 1))
 
             assertEquals(Color.White.toArgb(), getPixel(innerBoxLeft - 1, bottom))
             assertEquals(Color.White.toArgb(), getPixel(innerBoxLeft - 1, innerBoxTop - 1))
@@ -245,14 +241,12 @@ class PainterModifierTest {
     @Test
     fun testPainterModifierIntrinsicSize() {
         val paintLatch = CountDownLatch(1)
-        rule.runOnUiThread {
-            activity.setContent {
-                NoMinSizeContainer {
-                    NoIntrinsicSizeContainer(
-                        Modifier.paint(LatchPainter(containerWidth, containerHeight, paintLatch))
-                    ) {
-                        // Intentionally empty
-                    }
+        rule.setContent {
+            NoMinSizeContainer {
+                NoIntrinsicSizeContainer(
+                    Modifier.paint(LatchPainter(containerWidth, containerHeight, paintLatch))
+                ) {
+                    // Intentionally empty
                 }
             }
         }
@@ -279,24 +273,22 @@ class PainterModifierTest {
     fun testPainterIntrinsicSizeDoesNotExceedMax() {
         val paintLatch = CountDownLatch(1)
         val containerSize = containerWidth.roundToInt() / 2
-        rule.runOnUiThread {
-            activity.setContent {
+        rule.setContent {
+            NoIntrinsicSizeContainer(
+                Modifier.background(Color.White) +
+                        FixedSizeModifier(containerWidth.roundToInt())
+            ) {
                 NoIntrinsicSizeContainer(
-                    Modifier.background(Color.White) +
-                            FixedSizeModifier(containerWidth.roundToInt())
+                    AlignTopLeft + FixedSizeModifier(containerSize).paint(
+                        LatchPainter(
+                            containerWidth,
+                            containerHeight,
+                            paintLatch
+                        ),
+                        alignment = Alignment.TopStart
+                    )
                 ) {
-                    NoIntrinsicSizeContainer(
-                        AlignTopLeft + FixedSizeModifier(containerSize).paint(
-                            LatchPainter(
-                                containerWidth,
-                                containerHeight,
-                                paintLatch
-                            ),
-                            alignment = Alignment.TopStart
-                        )
-                    ) {
-                        // Intentionally empty
-                    }
+                    // Intentionally empty
                 }
             }
         }
@@ -327,23 +319,21 @@ class PainterModifierTest {
     fun testPainterNotSizedToIntrinsics() {
         val paintLatch = CountDownLatch(1)
         val containerSize = containerWidth.roundToInt() / 2
-        rule.runOnUiThread {
-            activity.setContent {
+        rule.setContent {
+            NoIntrinsicSizeContainer(
+                Modifier.background(Color.White) +
+                        FixedSizeModifier(containerSize)
+            ) {
                 NoIntrinsicSizeContainer(
-                    Modifier.background(Color.White) +
-                            FixedSizeModifier(containerSize)
+                    FixedSizeModifier(containerSize).paint(
+                        LatchPainter(
+                            containerWidth,
+                            containerHeight,
+                            paintLatch
+                        ),
+                        sizeToIntrinsics = false, alignment = Alignment.TopStart)
                 ) {
-                    NoIntrinsicSizeContainer(
-                        FixedSizeModifier(containerSize).paint(
-                            LatchPainter(
-                                containerWidth,
-                                containerHeight,
-                                paintLatch
-                            ),
-                            sizeToIntrinsics = false, alignment = Alignment.TopStart)
-                    ) {
-                        // Intentionally empty
-                    }
+                    // Intentionally empty
                 }
             }
         }
@@ -365,6 +355,145 @@ class PainterModifierTest {
         }
     }
 
+    @Test
+    fun testPainterFixedHeightScalesDownWidth() {
+        val composableHeightPx = 100f
+        val composableMaxWidthPx = 300f
+        val painterWidth = 400f
+        val painterHeight = 200f
+
+        val density = rule.density.density
+        // The resultant composable should match the height provided in the height modifier
+        // however, despite the width being a maximum of 300 pixels, the composable
+        // should be 200 pixels wide as the painter is scaled down to ensure the height constraint
+        // is satisfied. Because the Painter is twice as tall as the composable, the composable
+        // width should be half that of the painter
+        testPainterScaleMatchesSize(
+            Modifier.height(((composableHeightPx) / density).dp)
+                .widthIn(0.dp, (composableMaxWidthPx / density).dp),
+            ContentScale.Inside,
+            Size(painterWidth, painterHeight),
+            painterWidth / 2,
+            composableHeightPx
+        )
+    }
+
+    @Test
+    fun testPainterFixedWidthScalesDownHeight() {
+        val composableWidthPx = 100f
+        val composableMaxHeightPx = 300f
+        val painterWidth = 400f
+        val painterHeight = 200f
+
+        val density = rule.density.density
+        // The resultant composable should match the height provided in the height modifier
+        // however, despite the width being a maximum of 300 pixels, the resultant composable
+        // should be 200 pixels wide as the painter is scaled down to ensure the height constraint
+        // is satisfied. Because the Painter is twice as tall as the composable, the composable
+        // width should be half that of the painter
+        testPainterScaleMatchesSize(
+            Modifier.width(((composableWidthPx) / density).dp)
+                .heightIn(0.dp, (composableMaxHeightPx / density).dp),
+            ContentScale.Inside,
+            Size(painterWidth, painterHeight),
+            composableWidthPx,
+            painterHeight / 4
+        )
+    }
+
+    @Test
+    fun testPainterFixedDimensionUnchanged() {
+        val painterWidth = 1000f
+        val painterHeight = 375f
+        val density = rule.density.density
+        val composableWidth = 500f
+        val composableHeight = 800f
+        // Because the constraints are tight here, do not attempt to resize the composable
+        // based on the intrinsic dimensions of the Painter
+        testPainterScaleMatchesSize(
+            Modifier.width((composableWidth / density).dp).height((composableHeight / density).dp),
+            ContentScale.Fit,
+            Size(painterWidth, painterHeight),
+            composableWidth,
+            composableHeight
+        )
+    }
+
+    @Test
+    fun testPainterComposableHeightScaledUpWithFixedWidth() {
+        val composableWidthPx = 200f
+        val painterWidth = 100f
+        val painterHeight = 200f
+        // A Painter with ContentScale.FillWidth will scale its content to ensure that the
+        // composable width fills its width constraint. This also scales the height by the
+        // same scale factor. Because the intrinsic width is twice that of the width constraint,
+        // the height should be double that of the intrinsic height of the painter
+        testPainterScaleMatchesSize(
+            Modifier.width((composableWidthPx / rule.density.density).dp).wrapContentHeight(),
+            ContentScale.FillWidth,
+            Size(painterWidth, painterHeight),
+            composableWidthPx,
+            painterHeight * 2
+        )
+    }
+
+    @Test
+    fun testPainterWidthScaledDownWithSmallerHeight() {
+        val composableWidthPx = 200f
+        val painterWidth = 100f
+        val painterHeight = 200f
+        // A Painter with ContentScale.Inside should scale its content down to fit within the
+        // constraints of the composable
+        // In this case a fixed width that is larger than the painter with undefined height
+        // should have the composable width match that of its input and the height match
+        // that of the painter
+        testPainterScaleMatchesSize(
+            Modifier.width((composableWidthPx / rule.density.density).dp).wrapContentHeight(),
+            ContentScale.Inside,
+            Size(painterWidth, painterHeight),
+            composableWidthPx,
+            painterHeight
+        )
+    }
+
+    private fun testPainterScaleMatchesSize(
+        modifier: Modifier,
+        contentScale: ContentScale,
+        painterSize: Size,
+        composableWidthPx: Float,
+        composableHeightPx: Float
+    ) {
+        val paintLatch = CountDownLatch(1)
+
+        var composableWidth = 0f
+        var composableHeight = 0f
+        rule.setContent {
+            composableWidth = composableWidthPx / DensityAmbient.current.density
+            composableHeight = composableHeightPx / DensityAmbient.current.density
+            // Because the painter is told to fit inside the constraints, the width should
+            // match that of the provided fixed width and the height should match that of the
+            // composable as no scaling is being done
+            val painter = object : Painter() {
+                override val intrinsicSize: Size
+                    get() = painterSize
+
+                override fun DrawScope.onDraw() { /* no-op */ }
+            }
+            Box(modifier =
+                Modifier.drawWithContent {
+                    drawContent()
+                    paintLatch.countDown()
+                }.plus(modifier)
+                .paint(painter, contentScale = contentScale)
+            )
+        }
+
+        paintLatch.await()
+        findRoot()
+            .assertWidthIsEqualTo(composableWidth.dp)
+            .assertHeightIsEqualTo(composableHeight.dp)
+    }
+
     @Composable
     private fun testPainter(
         alpha: Float = DefaultAlpha,
@@ -384,7 +513,7 @@ class PainterModifierTest {
     }
 
     private fun obtainScreenshotBitmap(width: Int, height: Int = width): Bitmap {
-        val bitmap = rule.waitAndScreenShot()
+        val bitmap = findRoot().captureToBitmap()
         Assert.assertEquals(width, bitmap.width)
         Assert.assertEquals(height, bitmap.height)
         return bitmap
