854eadb975f17125ad9d074052d1a4e8e9b27e72
Ralston Da Silva <ralu@google.com>
2020-06-25 11:56:35 -0700

    Make KeyEvent an inline class
    Relnote: "KeyEvent is deprecated and replaced by KeyEvent2"
    Bug: 159921580
    Fixes: 159921580
    Test: Verified by running the demo app and udated ProcessKeyInputTest and AndroidProcessKeyInputTest.
    Change-Id: I687305a80a4caff2645d87bfac108205028f9f44

--- ui/ui-core/src/commonMain/kotlin/androidx/ui/core/Owner.kt
+++ ui/ui-core/src/commonMain/kotlin/androidx/ui/core/Owner.kt
@@ -19,7 +19,7 @@ import androidx.ui.autofill.Autofill
 import androidx.ui.autofill.AutofillTree
 import androidx.ui.core.clipboard.ClipboardManager
 import androidx.ui.core.hapticfeedback.HapticFeedback
-import androidx.ui.core.keyinput.KeyEvent
+import androidx.ui.core.keyinput.KeyEvent2
 import androidx.ui.core.semantics.SemanticsOwner
 import androidx.ui.core.texttoolbar.TextToolbar
 import androidx.ui.graphics.Canvas
@@ -142,11 +142,11 @@ interface Owner {
     fun requestFocus(): Boolean
 
     /**
-     * Send this [KeyEvent] to the focused component in this [Owner].
+     * Send this [KeyEvent][KeyEvent2] to the focused component in this [Owner].
      *
      * @return true if the event was consumed. False otherwise.
      */
-    fun sendKeyEvent(keyEvent: KeyEvent): Boolean
+    fun sendKeyEvent(keyEvent: KeyEvent2): Boolean
 
     /**
      * Observing the model reads are temporary disabled during the [block] execution.
