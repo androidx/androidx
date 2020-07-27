8a504815d4d612148316460257a71aacc3c3c70e
Gyumin Sim <gyumin@google.com>
2020-07-28 01:54:34 +0900

    Comply with inclusive language guidance in media
    See https://source.android.com/setup/contribute/respectful-code for
    reference.
    
    #inclusivefixit
    
    Bug: 161896447
    Test: Build
    Change-Id: Ibe2c70ffc979c1ed551694f85622fc7d398dd82b

--- media/media/src/main/java/androidx/media/MediaSessionManagerImplApi28.java
+++ media/media/src/main/java/androidx/media/MediaSessionManagerImplApi28.java
@@ -33,12 +33,12 @@ class MediaSessionManagerImplApi28 extends MediaSessionManagerImplApi21 {
     @Override
     public boolean isTrustedForMediaControl(MediaSessionManager.RemoteUserInfoImpl userInfo) {
         // Don't use framework's isTrustedForMediaControl().
-        // In P, framework's isTrustedForMediaControl() does the sanity check whether the UID, PID,
+        // In P, framework's isTrustedForMediaControl() checks whether the UID, PID,
         // and package name match. In MediaSession/MediaController, Context#getPackageName() is
         // used by MediaController to tell MediaSession the package name.
         // However, UID, PID and Context#getPackageName() may not match if a activity/service runs
         // on the another app's process by specifying android:process in the AndroidManifest.xml.
-        // In that case, sanity check will always fail.
+        // In that case, this check will always fail.
         // Alternative way is to use Context#getOpPackageName() for sending the package name,
         // but it's hidden so we cannot use it.
         return super.isTrustedForMediaControl(userInfo);
