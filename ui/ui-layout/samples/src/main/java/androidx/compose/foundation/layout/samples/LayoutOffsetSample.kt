4044ecfc4e52de0673c395f9d6258486b9ca934e
Anastasia Soboleva <soboleva@google.com>
2020-07-28 15:05:24 +0100

    Add absolute API for offset
    Fixes: 153633345
    Test: LayoutOffsetTest
    Relnote: "Added Modifier.absoluteOffset() and Modifier.absoluteOffsetPx(). Unlike offset modifiers, absolute offset modifiers will not auto-mirror in right-to-left context"
    Change-Id: I3aa2155766e3989cbf703e48f71daaf079a63f8e

--- ui/ui-layout/samples/src/main/java/androidx/compose/foundation/layout/samples/LayoutOffsetSample.kt
+++ ui/ui-layout/samples/src/main/java/androidx/compose/foundation/layout/samples/LayoutOffsetSample.kt
@@ -23,6 +23,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.gesture.tapGestureFilter
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.absoluteOffsetPx
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.offsetPx
@@ -32,7 +34,8 @@ import androidx.compose.ui.unit.dp
 @Sampled
 @Composable
 fun LayoutOffsetModifier() {
-    // This text will be offset (10.dp, 20.dp) from the center of the available space.
+    // This text will be offset (10.dp, 20.dp) from the center of the available space. In the
+    // right-to-left context, the offset will be (-10.dp, 20.dp).
     Text(
         "Layout offset modifier sample",
         Modifier.fillMaxSize()
@@ -43,13 +46,39 @@ fun LayoutOffsetModifier() {
 
 @Sampled
 @Composable
+fun LayoutAbsoluteOffsetModifier() {
+    // This text will be offset (10.dp, 20.dp) from the center of the available space.
+    Text(
+        "Layout offset modifier sample",
+        Modifier.fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .absoluteOffset(10.dp, 20.dp)
+    )
+}
+
+@Sampled
+@Composable
 fun LayoutOffsetPxModifier() {
-    // This text will be offset in steps of 10.dp from the top left of the available space.
+    // This text will be offset in steps of 10.dp from the top left of the available space in
+    // left-to-right context, and from top right in right-to-left context.
     val offset = state { 0f }
     Text(
         "Layout offset modifier sample",
         Modifier
             .tapGestureFilter { offset.value += 10f }
             .offsetPx(offset, offset)
+    )
+}
+
+@Sampled
+@Composable
+fun LayoutAbsoluteOffsetPxModifier() {
+    // This text will be offset in steps of 10.dp from the top left of the available space.
+    val offset = state { 0f }
+    Text(
+        "Layout offset modifier sample",
+        Modifier
+            .tapGestureFilter { offset.value += 10f }
+            .absoluteOffsetPx(offset, offset)
     )
 }
