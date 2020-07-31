7aa8f79a046eb59fe3ec770acb264d2ec64e88f5
Nate Fischer <ntfschr@google.com>
2020-07-31 12:23:52 -0700

    AndroidX Webkit: initial setSafeBrowsingAllowlist
    This adds an initial setSafeBrowsingAllowlist implementation:
    
     * setSafeBrowsingWhitelist plumbs through setSafeBrowsingAllowlist
     * setSafeBrowsingAllowlist prefers the ALLOWLIST boundary interface,
       followed by the setSafeBrowsingWhitelist() platform API, followed by
       the WHITELIST boundary interface
     * Tests and demo app use the new setSafeBrowsingAllowlist method
     * setSafeBrowsingAllowlist accepts a Set<String> instead of
       List<String>, since order and duplicates are not meaningful (nor were
       they meaningful in the old API)
    
    This API is still hidden, but will be unhidden in a follow-up.
    
    Bug: 160326030
    Bug: 161947908
    Test: ./gradlew :webkit:webkit:connectedAndroidTest (on Android N + old
    Test: WebView, Android N + new WebView, & Android Q + old WebView)
    Test: ./gradlew :webkit:webkit:lintDebug
    Change-Id: I1c23b474378a0417896e630534735aefd7ba28c7

--- webkit/webkit/src/main/java/androidx/webkit/WebViewFeature.java
+++ webkit/webkit/src/main/java/androidx/webkit/WebViewFeature.java
@@ -55,6 +55,7 @@ public class WebViewFeature {
             SAFE_BROWSING_ENABLE,
             DISABLED_ACTION_MODE_MENU_ITEMS,
             START_SAFE_BROWSING,
+            SAFE_BROWSING_ALLOWLIST,
             SAFE_BROWSING_WHITELIST,
             SAFE_BROWSING_PRIVACY_POLICY_URL,
             SERVICE_WORKER_BASIC_USAGE,
@@ -142,8 +143,20 @@ public class WebViewFeature {
     /**
      * Feature for {@link #isFeatureSupported(String)}.
      * This feature covers
+     * {@link androidx.webkit.WebViewCompat#setSafeBrowsingAllowlist(List, ValueCallback)}.
+     *
+     * @hide
+     */
+    // TODO(b/160326030): unhide this API and deprecate SAFE_BROWSING_WHITELIST
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public static final String SAFE_BROWSING_ALLOWLIST = "SAFE_BROWSING_ALLOWLIST";
+
+    /**
+     * Feature for {@link #isFeatureSupported(String)}.
+     * This feature covers
      * {@link androidx.webkit.WebViewCompat#setSafeBrowsingWhitelist(List, ValueCallback)}.
      */
+    // TODO(b/160326030): deprecate this API and unhide SAFE_BROWSING_ALLOWLIST
     public static final String SAFE_BROWSING_WHITELIST = "SAFE_BROWSING_WHITELIST";
 
     /**
