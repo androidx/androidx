225575eb22ee3d464c895da00ae2188298224fe4
husaynhakeem <husaynhakeem@google.com>
2020-08-20 11:13:05 -0700

    Fix image analysis unlocked orientation test
    Update the image analysis unlocked orientation test to use UiDevice to rotate the activity under test, and ActivityMonitor to wait for the rotation to complete.
    
    Bug: b/162739395, b/165400904
    Test: ImageAnalysisUnlockedOrientationTest.kt
    Change-Id: Iabe2e2a68d7c4bad251212b29942d15161e87175

--- camera/integration-tests/uiwidgetstestapp/src/androidTest/java/androidx/camera/integration/uiwidgets/rotations/ImageAnalysisBaseTest.kt
+++ camera/integration-tests/uiwidgetstestapp/src/androidTest/java/androidx/camera/integration/uiwidgets/rotations/ImageAnalysisBaseTest.kt
@@ -40,19 +40,20 @@ import java.util.concurrent.TimeUnit
  */
 abstract class ImageAnalysisBaseTest<A : CameraActivity> {
 
+    protected val mDevice: UiDevice =
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
     protected fun setUp(lensFacing: Int) {
         CoreAppTestUtil.assumeCompatibleDevice()
         Assume.assumeTrue(CameraUtil.hasCameraWithLensFacing(lensFacing))
 
         // Clear the device's UI and ensure it's in a natural orientation
         CoreAppTestUtil.clearDeviceUI(InstrumentationRegistry.getInstrumentation())
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        device.setOrientationNatural()
+        mDevice.setOrientationNatural()
     }
 
     protected fun tearDown() {
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        device.unfreezeRotation()
+        mDevice.unfreezeRotation()
     }
 
     protected inline fun <reified A : CameraActivity> verifyRotation(
