31a5fa9bf9d379a775fd5406c4c78d944d2da3e6
Jeff Gaston <jeffrygaston@google.com>
2020-09-15 09:53:56 -0400

    simplify-build-failure.sh adding --clean
    Also attempting to autodetect when cleaning is necessary
    
    Bug: 132727976
    Bug: 167569304
    Test: ./development/simplify-build-failure/simplify-build-failure.sh --task ":core:core:assembleDebug" "Xlint" --clean
    
    Change-Id: Ifbdc2cbaff36b693add384aca7b9c00309781d10

--- development/simplify-build-failure/simplify-build-failure.sh
+++ development/simplify-build-failure/simplify-build-failure.sh
@@ -22,7 +22,7 @@ function usage() {
   echo '  simplify-build-failure.sh'
   echo
   echo 'SYNOPSIS'
-  echo "  $0 (--task <gradle task> <error message> | --command <shell command> ) [--continue] [--limit-to-path <file path>] [--check-lines-in <subfile path>] [--num-jobs <count>]"
+  echo "  $0 (--task <gradle task> <error message> [--clean] | --command <shell command> ) [--continue] [--limit-to-path <file path>] [--check-lines-in <subfile path>] [--num-jobs <count>]"
   echo
   echo DESCRIPTION
   echo '  Searches for a minimal set of files and/or lines required to reproduce a given build failure'
@@ -44,8 +44,12 @@ function usage() {
   echo
   echo '  --check-lines-in <subfile path>'
   echo '    Specifies that individual lines in files in <subfile path> will be considered for removal, too'
+  echo
   echo '  --num-jobs <count>'
   echo '    Specifies the number of jobs to run at once'
+  echo
+  echo '  --clean'
+  echo '    Specifies that each build should start from a consistent state'
   exit 1
 }
 
@@ -68,6 +72,7 @@ resume=false
 subfilePath=""
 limitToPath=""
 numJobs="1"
+clean="false"
 
 export ALLOW_MISSING_PROJECTS=true # so that if we delete entire projects then the AndroidX build doesn't think we made a spelling mistake
 
@@ -109,10 +114,7 @@ while [ "$1" != "" ]; do
     fi
 
     gradleCommand="OUT_DIR=out ./gradlew $gradleTasks > log 2>&1"
-    grepCommand="grep \"$errorMessage\" log"
-    # Sleep in case Gradle fails very quickly
-    # We don't want to run too many Gradle commands in a row or else the daemons might get confused
-    testCommand="cd $commandSubdir && $gradleCommand; sleep 2; $grepCommand"
+    grepCommand="$scriptPath/impl/grepOrTail.sh \"$errorMessage\" log"
     continue
   fi
   if [ "$arg" == "--command" ]; then
@@ -144,14 +146,25 @@ while [ "$1" != "" ]; do
     shift
     continue
   fi
+  if [ "$arg" == "--clean" ]; then
+    clean=true
+    continue
+  fi
   echo "Unrecognized argument '$arg'"
   usage
 done
 
-if [ "$testCommand" == "" ]; then
-  usage
+if [ "$gradleCommand" == "" ]; then
+  if [ "$clean" == "true" ]; then
+    echo "Option --clean requires option --task"
+    usage
+  fi
+  if [ "$testCommand" == "" ]; then
+    usage
+  fi
 fi
 
+# delete temp dir if not resuming
 if [ "$resume" == "true" ]; then
   if [ -d "$tempDir" ]; then
     echo "Not deleting temp dir $tempDir"
@@ -160,34 +173,97 @@ else
   echo "Removing temp dir $tempDir"
   rm "$tempDir" -rf
 fi
-referencePassingDir="$tempDir/base"
-referenceFailingDir="$tempDir/failing"
 
-rm "$referencePassingDir" -rf
-if [ "$limitToPath" != "" ]; then
-  mkdir -p "$(dirname $referencePassingDir)"
-  cp -r "$supportRoot" "$referencePassingDir"
-  rm "$referencePassingDir/$limitToPath" -rf
-else
-  mkdir -p "$referencePassingDir"
+# backup code so user can keep editing
+if [ ! -e "$referenceFailingDir" ]; then
+  echo backup up frameworks/support into "$referenceFailingDir" in case you want to continue to make modifications or run other builds
+  rm "$referenceFailingDir" -rf
+  cp -rT . "$referenceFailingDir"
+  # remove some unhelpful settings
+  sed -i 's/.*Werror.*//' "$referenceFailingDir/buildSrc/build.gradle"
+  sed -i 's/.*Exception.*cannot include.*//' "$referenceFailingDir/settings.gradle"
+  # remove some generated files that we don't want diff-filterer.py to track
+  rm -rf "$referenceFailingDir/.gradle" "$referenceFailingDir/buildSrc/.gradle" "$referenceFailingDir/out"
 fi
 
-if [ "$subfilePath" != "" ]; then
-  if [ ! -e "$subfilePath" ]; then
-    echo "$subfilePath" does not exist
-    exit 1
+# if Gradle tasks are specified, then determine the appropriate shell command
+if [ "$gradleCommand" != "" ]; then
+  # if --clean is specified, then determine whether we can at least start with a minimal out/ dir
+  startingOutDir="$tempDir/failing-out"
+  outTestDir="$tempDir/out-test"
+  if [ "$clean" != "true" ]; then
+    if [ "$resume" == "true" ]; then
+      if [ -e "$startingOutDir" ]; then
+        echo Will clean before each build because that was the previously computed setting
+        clean=true
+      else
+        echo Will not clean before each build because that was the previously computed setting
+      fi
+    else
+      echo Determining whether we must clean before each build
+      rm "$outTestDir" -rf
+      mkdir -p "$tempDir"
+      cp -r "$referenceFailingDir" "$outTestDir"
+      echo Doing first test build
+      if bash -c "cd "$outTestDir/$commandSubdir" && $gradleCommand; $grepCommand"; then
+        echo Reproduced the problem
+      else
+        echo Failed to reproduce the problem
+        exit 1
+      fi
+      echo Doing another test build to determine if cleaning between builds is necessary
+      if bash -c "cd "$outTestDir/$commandSubdir" && $gradleCommand; $grepCommand"; then
+        echo Reproduced the problem even when not starting from a clean out/ dir
+      else
+        echo Did not reproduce the problem when starting from previous out/ dir
+        echo Will have to clean the out/ dir before each build
+        clean=true
+      fi
+    fi
+  fi
+  if [ "$clean" == "true" ]; then
+    if [ -e "$startingOutDir" ]; then
+      echo Reusing existing base out dir of "$startingOutDir"
+    else
+      echo Checking whether we can prepopulate a minimal out/ dir for faster execution
+      rm "$outTestDir" -rf
+      mkdir -p "$tempDir"
+      cp -r "$supportRoot" "$outTestDir"
+      if bash -c "cd "$outTestDir/$commandSubdir" && OUT_DIR=out ./gradlew projects --no-daemon && $gradleCommand; $grepCommand"; then
+        echo Will reuse base out dir of "$startingOutDir"
+        cp -r "$outTestDir/$commandSubdir/out" "$startingOutDir"
+      else
+        echo Will start subsequent builds from empty out dir
+        mkdir -p "$startingOutDir"
+      fi
+    fi
+    gradleCommand="rm out .gradle buildSrc/.gradle -rf && cp -r $startingOutDir out && $gradleCommand --no-daemon"
+  fi
+  # Sleep in case Gradle fails very quickly
+  # We don't want to run too many Gradle commands in a row or else the daemons might get confused
+  testCommand="cd $commandSubdir && $gradleCommand; sleep 2; $grepCommand"
+  if [ "$clean" == true ]; then
+    # reset the out/ dir
+    gradle_prepareState_command="rm out .gradle buildSrc/.gradle -rf && cp -r $startingOutDir out"
+    # update the timestamps on all files in case they affect anything
+    gradle_prepareState_command="$gradle_prepareState_command && find -type f | xargs touch || true"
+
+    gradleCommand="$(echo "$gradleCommand" | sed 's/gradlew/gradlew --no-daemon/')"
+  else
+    gradle_prepareState_command=""
   fi
 fi
+
 
 filtererStep1Work="$tempDir"
 filtererStep1Output="$filtererStep1Work/bestResults"
 fewestFilesOutputPath="$tempDir/fewestFiles"
 if echo "$resume" | grep "true" >/dev/null && stat "$fewestFilesOutputPath" >/dev/null 2>/dev/null; then
-  echo "Skipping first execution of diff-filterer, $fewestFilesOutputPath already exists"
+  echo "Skipping asking diff-filterer for a minimal set of files, $fewestFilesOutputPath already exists"
 else
   if [ "$resume" == "true" ]; then
     if stat "$filtererStep1Output" >/dev/null 2>/dev/null; then
-      echo "Reusing $filtererStep1Output to resume first execution of diff-filterer"
+      echo "Reusing $filtererStep1Output to resume asking diff-filterer for a minimal set of files"
       # Copy the previous results to resume from
       rm "$referenceFailingDir" -rf
       cp -rT "$filtererStep1Output" "$referenceFailingDir"
@@ -195,13 +271,6 @@ else
       echo "Cannot resume previous execution; neither $fewestFilesOutputPath nor $filtererStep1Output exists"
       exit 1
     fi
-  else
-    # make a backup of the code so that the user can still make modifications to the source tree without interfering with diff-filterer.py
-    rm "$referenceFailingDir" -rf
-    cp -rT . "$referenceFailingDir"
-    # remove some unhelpful settings
-    sed -i 's/.*Werror.*//' "$referenceFailingDir/buildSrc/build.gradle"
-    sed -i 's/.*throw new Exception.*Path.*does not exist.*//' "$referenceFailingDir/settings.gradle"
   fi
   echo Running diff-filterer.py once to identify the minimal set of files needed to reproduce the error
   if ./development/file-utils/diff-filterer.py --assume-no-side-effects --work-path $filtererStep1Work --num-jobs "$numJobs" "$referenceFailingDir" "$referencePassingDir" "$testCommand"; then
@@ -239,14 +308,6 @@ else
     # If we're not making changes in buildSrc, then we want to keep the gradle caches around for more speed
     # If we are making changes in buildSrc, then Gradle doesn't necessarily do up-to-date checks correctly, and we want to clear the caches between builds
     filtererOptions="$filtererOptions --assume-no-side-effects"
-  else
-    if [ "$grepCommand" != "" ]; then
-      filtererOptions="$filtererOptions --assume-no-side-effects"
-      # If we're making changes in buildSrc, then we want to make sure that a clean build passes because Gradle doesn't always do up-to-date checks correctly when we're making strange changes in buildSrc
-      # However, the build runs much more quickly when incremental than when clean
-      # So, we first run an incremental build and then if it passes we run a clean build
-      testCommand="cd $commandSubdir && $gradleCommand; $grepCommand && rm log out -rf && $gradleCommand --no-daemon; $grepCommand"
-    fi
   fi
 
   if echo "$resume" | grep true >/dev/null && stat "$noFunctionBodies_output" >/dev/null 2>/dev/null; then
