9c45b8582cf47ae3ea552486605808e914e40f1f
Jon Spivack <spivack@google.com>
2020-10-08 14:39:49 -0700

    FileProvider: Support custom filenames
    The client can pass a custom filename into an overloaded version of getUriForFile. This allows the Uri to have a different display name from the original file without requiring that file to be modified or copied.
    
    Relnote: "Add overload of FileProvider#getUriForFile to support custom
    filenames"
    
    Bug: 160263415
    Test: Manual (Used this to share files via Nearby Sharing)
    Test: FileProviderTest
    Change-Id: Ie870bbb7781edfc4c841bcaeefd55b5e632b3eeb

--- core/core/src/androidTest/java/androidx/core/content/FileProviderTest.java
+++ core/core/src/androidTest/java/androidx/core/content/FileProviderTest.java
@@ -59,6 +59,7 @@ public class FileProviderTest {
     private static final String TEST_FILE = "file.test";
     private static final byte[] TEST_DATA = new byte[] { (byte) 0xf0, 0x00, 0x0d };
     private static final byte[] TEST_DATA_ALT = new byte[] { (byte) 0x33, 0x66 };
+    private static final String TEST_FILE_DISPLAY_NAME = "Test Display Name";
 
     private ContentResolver mResolver;
     private Context mContext;
@@ -257,9 +258,32 @@ public class FileProviderTest {
     }
 
     @Test
+    public void testQueryDisplayName() throws Exception {
+        final File file = new File(mContext.getFilesDir(), TEST_FILE);
+        final Uri uri = stageFileAndGetUri(file, TEST_DATA, TEST_AUTHORITY, TEST_FILE_DISPLAY_NAME);
+
+        Cursor cursor = mResolver.query(uri, null, null, null, null);
+        try {
+            cursor.moveToFirst();
+            assertEquals(TEST_FILE_DISPLAY_NAME,
+                    cursor.getString(cursor.getColumnIndex(DISPLAY_NAME)));
+        } finally {
+            cursor.close();
+        }
+    }
+
+    @Test
     public void testReadFile() throws Exception {
         final File file = new File(mContext.getFilesDir(), TEST_FILE);
         final Uri uri = stageFileAndGetUri(file, TEST_DATA);
+
+        assertContentsEquals(TEST_DATA, uri);
+    }
+
+    @Test
+    public void testReadFileWithCustomDisplayName() throws Exception {
+        final File file = new File(mContext.getFilesDir(), TEST_FILE);
+        final Uri uri = stageFileAndGetUri(file, TEST_DATA, TEST_AUTHORITY, TEST_FILE_DISPLAY_NAME);
 
         assertContentsEquals(TEST_DATA, uri);
     }
@@ -395,10 +419,15 @@ public class FileProviderTest {
     }
 
     private Uri stageFileAndGetUri(File file, byte[] data) throws Exception {
-        return stageFileAndGetUri(file, data, TEST_AUTHORITY);
+        return stageFileAndGetUri(file, data, TEST_AUTHORITY, null);
     }
 
     private Uri stageFileAndGetUri(File file, byte[] data, String authority) throws Exception {
+        return stageFileAndGetUri(file, data, authority, null);
+    }
+
+    private Uri stageFileAndGetUri(File file, byte[] data, String authority, String displayName)
+            throws Exception {
         if (data != null) {
             final FileOutputStream out = new FileOutputStream(file);
             try {
@@ -408,6 +437,10 @@ public class FileProviderTest {
             }
         } else {
             file.delete();
+        }
+
+        if (displayName != null) {
+            return FileProvider.getUriForFile(mContext, authority, file, displayName);
         }
         return FileProvider.getUriForFile(mContext, authority, file);
     }
