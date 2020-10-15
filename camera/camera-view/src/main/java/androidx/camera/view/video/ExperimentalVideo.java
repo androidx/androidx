312f77ca94ed7335490f3651f2099997f4a0d1cc
Trevor McGuire <trevormcguire@google.com>
2020-10-14 21:12:36 -0700

    camera-view experimental video APIs
     Adds @ExperimentalVideo annotation to camera-view. APIs using this
     annotation make use of new experimental video functionality.
    
     Removes API dependencies on VideoCapture which is still restricted and
     will be removed from camera-core shortly.
    
    Relnote: "`@ExperimentalVideo` annotation was introduced to camera-view.
    This annotation marks APIs which expose experimental video functionality
    which is subject to change as the features are fully developed. Any
    method using these APIs should use the `@UseExperimental` annotation
    with `ExperimentalVideo` as the `markerClass`."
    
    Bug: 170763031
    Test: ./gradlew bOS
    Change-Id: I6d72945efd3d1a6c4d9724e905ec38c0aa1c9205

---
+++ camera/camera-view/src/main/java/androidx/camera/view/video/ExperimentalVideo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.view.video;
+
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import androidx.annotation.experimental.Experimental;
+
+import java.lang.annotation.Retention;
+
+/**
+ * Denotes that the annotated method uses the experimental methods which allow for video
+ * recording.
+ *
+ * <p>Video recording API signatures within the {@code camera-view} artifact may change in future
+ * releases, or may be removed altogether.
+ */
+@Retention(CLASS)
+@Experimental
+public @interface ExperimentalVideo {
+}
