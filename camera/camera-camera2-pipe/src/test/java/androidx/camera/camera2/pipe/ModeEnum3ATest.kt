c1504680e9b75c4a17f73fe96037bee078b50ad2
Jelle Fresen <jellefresen@google.com>
2020-10-24 16:44:58 +0100

    Remove test size annotation from host side test
    Bug: 166771118
    Test: ./gradlew lintDebug
    Change-Id: If6d8d4667a34920664090a2caf6540acf70dbbb3

--- camera/camera-camera2-pipe/src/test/java/androidx/camera/camera2/pipe/ModeEnum3ATest.kt
+++ camera/camera-camera2-pipe/src/test/java/androidx/camera/camera2/pipe/ModeEnum3ATest.kt
@@ -18,13 +18,11 @@ package androidx.camera.camera2.pipe
 
 import android.os.Build
 import androidx.camera.camera2.pipe.testing.CameraPipeRobolectricTestRunner
-import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
-@SmallTest
 @RunWith(CameraPipeRobolectricTestRunner::class)
 @Config(minSdk = Build.VERSION_CODES.LOLLIPOP)
 class ModeEnum3ATest {
