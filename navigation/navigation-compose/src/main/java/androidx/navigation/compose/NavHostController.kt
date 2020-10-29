41b9a249e82a06a228e09257a8b880c646eae92f
Jeremy Woods <jbwoods@google.com>
2020-10-29 15:17:30 -0700

    Add support for NavOptions in Navigation compose
    Adding support to add NavOptions to navigate() calls in Navigation
    compose.
    
    Used the new support in the BottomBarNavDemo.
    
    RelNote: "Navigation compose now support NavOptions for using popUpTo
    and launchSingleTop operations"
    Test: NavHostControllerTest and ComposeNavigatorTest
    Bug: 171468994
    
    Change-Id: I8282d4d92eb90b32f4ee5119face3b57920d873b

--- navigation/navigation-compose/src/main/java/androidx/navigation/compose/NavHostController.kt
+++ navigation/navigation-compose/src/main/java/androidx/navigation/compose/NavHostController.kt
@@ -34,6 +34,9 @@ import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.NavGraph
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.navOptions
 import androidx.navigation.navigation
 
 const val KEY_ROUTE = "android-support-nav:controller:route"
@@ -95,9 +98,13 @@ private fun NavControllerSaver(
  * Navigate to a route in the current NavGraph.
  *
  * @param route route for the destination
+ * @param builder DSL for constructing a new [NavOptions]
  */
-public fun NavController.navigate(route: String) {
-    navigate(NavDeepLinkRequest.Builder.fromUri(createRoute(route).toUri()).build())
+public fun NavController.navigate(route: String, builder: NavOptionsBuilder.() -> Unit = {}) {
+    navigate(
+        NavDeepLinkRequest.Builder.fromUri(createRoute(route).toUri()).build(),
+        navOptions(builder)
+    )
 }
 
 /**
