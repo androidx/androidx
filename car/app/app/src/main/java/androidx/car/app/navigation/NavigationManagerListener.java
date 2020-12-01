fbe04307ffac5eeca3188e281f864eb032253b8f
Ram Parameswaran <rampara@google.com>
2020-11-30 18:21:37 -0800

    Decouple lifecycle logic from CarAppService
    Relnote: N/A
    Bug: 171565494
    Test: /gradlew car:app:app:connectedAndroidTest
    
    Change-Id: I07316731afcd3a10334b97a4950ae891e3f65193

--- car/app/app/src/main/java/androidx/car/app/navigation/NavigationManagerListener.java
+++ car/app/app/src/main/java/androidx/car/app/navigation/NavigationManagerListener.java
@@ -16,6 +16,7 @@
 
 package androidx.car.app.navigation;
 
+import androidx.car.app.CarContext;
 import androidx.car.app.navigation.model.Trip;
 
 /**
@@ -38,8 +39,7 @@ public interface NavigationManagerListener {
      * Notifies the app that, from this point onwards, when the user chooses to navigate to a
      * destination, the app should start simulating a drive towards that destination.
      *
-     * <p>This mode should remain active until {@link
-     * androidx.car.app.CarAppService#onCarAppFinished} is called.
+     * <p>This mode should remain active until {@link CarContext#finishCarApp()} is called.
      *
      * <p>This functionality is used to allow verifying the app's navigation capabilities without
      * being in an actual car.
