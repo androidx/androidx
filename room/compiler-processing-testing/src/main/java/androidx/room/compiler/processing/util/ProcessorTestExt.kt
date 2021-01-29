4538d0d5eeffb58453cfbf53a84a55e834ab07d1
Yigit Boyar <yboyar@google.com>
2021-01-29 10:25:51 -0800

    Add ability to disable ksp tests
    Since KSP is under active development, it does not yet use a
    fully stable API from kotlin, causing it to fail when kotlin
    compiler internals change.
    
    Moreover, we use Kotlin Compile Testing to run these tests, hence, it
    sometimes requires updating KCT as well before we can run KSP tests.
    
    This situation causes a problem in AndroidX kotlin version updates as we
    use a single version there and Compose project closely follows latest
    kotlin version.
    
    This CL introduces a compilation time configuration to disable KSP
    tests to unblock kotlin updates. Now the compiler processing testing
    project adds a resource with versions and the library decides whether it
    can run KSP tests or not.
    I've added CompilationCapabilities class to easily check it in tests and
    also updated tests that needs to change when capability changes.
    It is not perfect as we cannot prevent new tests from being added that
    has to run the test but the fix is at least easy using
    Capabilities.assumeKspIsEnabled (it will throw assumption violation
    exception to skip the test if test cannot pass without KSP).
    
    Bug: b/178725084
    Test: TestConfigTest (also run locally when it is disabled)
    Change-Id: I79a5554ed79f5515d16ba4e49ec48738dcfb5d9d

--- room/compiler-processing-testing/src/main/java/androidx/room/compiler/processing/util/ProcessorTestExt.kt
+++ room/compiler-processing-testing/src/main/java/androidx/room/compiler/processing/util/ProcessorTestExt.kt
@@ -51,8 +51,19 @@ private fun runTests(
             false
         }
     }
-    // make sure some tests did run
-    assertThat(runCount).isGreaterThan(0)
+    // make sure some tests did run. Ksp tests might be disabled so if it is the only test given,
+    // ignore the check
+    val minTestCount = when {
+        CompilationTestCapabilities.canTestWithKsp ||
+            (runners.toList() - KspCompilationTestRunner).isNotEmpty() -> {
+            1
+        }
+        else -> {
+            // is ok if we don't run any tests if ksp is disabled and it is the only test
+            0
+        }
+    }
+    assertThat(runCount).isAtLeast(minTestCount)
 }
 
 fun runProcessorTestWithoutKsp(
