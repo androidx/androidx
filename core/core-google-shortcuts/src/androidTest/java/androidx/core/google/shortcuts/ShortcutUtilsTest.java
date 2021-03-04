85b2ecaf414c0b17ce1dbbf853cbae1eedf5c85f
David Dong <ddong@google.com>
2021-03-03 20:10:37 -0500

    Add ShortcutInfoChangeListenerImpl and Trampoline Activity
    currently the Trampoline Activity is just used to generate a valid app index url. But in the future, we plan on using it to support private shortcut intents.
    
    Test: added unit test
    
    Change-Id: I2947293b2b64f19455ba500ab921445c0f1d0f8f

---
+++ core/core-google-shortcuts/src/androidTest/java/androidx/core/google/shortcuts/ShortcutUtilsTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.google.shortcuts;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ShortcutUtilsTest {
+    private static final String TEST_PACKAGE = "com.test.package";
+
+    @Test
+    @SmallTest
+    public void testGetIndexableUrl_returnsTrampolineActivityIntentUriWithIdExtra() {
+        Context context = mock(Context.class);
+        when(context.getPackageName()).thenReturn(TEST_PACKAGE);
+
+        String id = "intentId";
+
+        String url = ShortcutUtils.getIndexableUrl(context, id);
+
+        String expectedUri = String.format("intent:#Intent;component=%s/androidx.core.google"
+                        + ".shortcuts.TrampolineActivity;S.id=%s;end", TEST_PACKAGE, id);
+        assertThat(url).isEqualTo(expectedUri);
+    }
+
+    @Test
+    @SmallTest
+    public void testGetIndexableShortcutUrl_returnsShortcutUrl() throws Exception {
+        Context context = mock(Context.class);
+        Intent intent = Intent.parseUri("http://www.google.com", 0);
+
+        String shortcutUrl = ShortcutUtils.getIndexableShortcutUrl(context, intent);
+
+        String expectedShortcutUrl = "http://www.google.com";
+        assertThat(shortcutUrl).isEqualTo(expectedShortcutUrl);
+    }
+}
---
+++ core/core-google-shortcuts/src/main/java/androidx/core/google/shortcuts/ShortcutUtils.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.google.shortcuts;
+
+import static androidx.annotation.RestrictTo.Scope.LIBRARY;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+
+/**
+ * Utility methods and constants used by the google shortcuts library.
+ *
+ * @hide
+ */
+@RestrictTo(LIBRARY)
+class ShortcutUtils {
+    public static final String SHORTCUT_LABEL_KEY = "shortcutLabel";
+    public static final String SHORTCUT_DESCRIPTION_KEY = "shortcutDescription";
+    public static final String SHORTCUT_URL_KEY = "shortcutUrl";
+    public static final String ID_KEY = "id";
+
+    /**
+     * Generate value for Indexable url field. The url field will not be used for anything other
+     * than referencing the Indexable object. But since it requires that it's openable by the
+     * app, we generate it as an intent that opens the Trampoline Activity.
+     *
+     * @param context the app's context.
+     * @param shortcutId the shortcut id used to generate the url.
+     * @return the indexable url.
+     */
+    public static String getIndexableUrl(@NonNull Context context, @NonNull String shortcutId) {
+        Intent intent = new Intent(context, TrampolineActivity.class);
+        intent.putExtra(ID_KEY, shortcutId);
+
+        return intent.toUri(Intent.URI_INTENT_SCHEME);
+    }
+
+    /**
+     * Generate value for Indexable shortcutUrl field. This field will be used by Google
+     * Assistant to open shortcuts.
+     *
+     * @param context the app's context.
+     * @param shortcutIntent the intent that the shortcut opens.
+     * @return the shortcut url.
+     */
+    public static String getIndexableShortcutUrl(@NonNull Context context,
+            @NonNull Intent shortcutIntent) {
+        // TODO (b/182599835): support private shortcut intents by wrapping it inside an intent
+        //  that launches the trampoline activity.
+        return shortcutIntent.toUri(0);
+    }
+
+    private ShortcutUtils() {}
+}
