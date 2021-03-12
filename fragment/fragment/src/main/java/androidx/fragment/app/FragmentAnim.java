4c702a921f39729d3d8e5af981a787c25b195ff1
Jeremy Woods <jbwoods@google.com>
2021-03-12 15:43:49 -0800

    Ensure fragments animate in the same direction
    When using the new state manager, the last fragment operation should
    always determine the direction of the special effects. So if the
    transactions are forward -> forward -> pop, the special effects should
    all be pop type since the last transaction was a pop.
    
    RelNote: "When running both `popBackStack()` and `commit()` operations
    together, the last operation will now set the direction for all
    animations rather than running some pop animations and some enter
    animations."
    Test: removePopExitAnimation
    Bug: 181142246
    
    Change-Id: I7072e2c8008318ae812ae461a42ddafbbe6055cd

--- fragment/fragment/src/main/java/androidx/fragment/app/FragmentAnim.java
+++ fragment/fragment/src/main/java/androidx/fragment/app/FragmentAnim.java
@@ -43,11 +43,11 @@ class FragmentAnim {
     }
 
     static AnimationOrAnimator loadAnimation(@NonNull Context context,
-            @NonNull Fragment fragment, boolean enter) {
+            @NonNull Fragment fragment, boolean enter, boolean isPop) {
         int transit = fragment.getNextTransition();
-        int nextAnim = fragment.getNextAnim();
-        // Clear the Fragment animation
-        fragment.setNextAnim(0);
+        int nextAnim = getNextAnim(fragment, enter, isPop);
+        // Clear the Fragment animations
+        fragment.setAnimations(0, 0, 0, 0);
         // We do not need to keep up with the removing Fragment after we get its next animation.
         // If transactions do not allow reordering, this will always be true and the visible
         // removing fragment will be cleared. If reordering is allowed, this will only be true
@@ -116,6 +116,22 @@ class FragmentAnim {
             }
         }
         return null;
+    }
+
+    private static int getNextAnim(Fragment fragment, boolean enter, boolean isPop) {
+        if (isPop) {
+            if (enter) {
+                return fragment.getPopEnterAnim();
+            } else {
+                return fragment.getPopExitAnim();
+            }
+        } else {
+            if (enter) {
+                return fragment.getEnterAnim();
+            } else {
+                return fragment.getExitAnim();
+            }
+        }
     }
 
     /**
