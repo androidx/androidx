a93badba5d42f90747d9ab9e015ff89be687ac15
Yang Yu <yanggyyu@google.com>
2021-03-23 15:48:13 -0700

    Update AppSearchCompiler.
    Refactor AppSearchCompiler to extend BasicAnnotationProcessor for
    multi-round process.
    
    BUG: 173639224
    Test: AppSearchCompilerTest
    Change-Id: I723882715ca5c650a8c6442b310787eaeabb1679

--- appsearch/compiler/src/main/java/androidx/appsearch/compiler/AppSearchCompiler.java
+++ appsearch/compiler/src/main/java/androidx/appsearch/compiler/AppSearchCompiler.java
@@ -15,17 +15,22 @@
  */
 package androidx.appsearch.compiler;
 
+import static javax.lang.model.util.ElementFilter.typesIn;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
+
+import com.google.auto.common.BasicAnnotationProcessor;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
-import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedOptions;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -33,21 +38,19 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
+import javax.tools.Diagnostic.Kind;
 
 /** Processes @Document annotations. */
 @SupportedAnnotationTypes({IntrospectionHelper.DOCUMENT_ANNOTATION_CLASS})
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedOptions({AppSearchCompiler.OUTPUT_DIR_OPTION})
-public class AppSearchCompiler extends AbstractProcessor {
+public class AppSearchCompiler extends BasicAnnotationProcessor {
     /**
      * This property causes us to write output to a different folder instead of the usual filer
      * location. It should only be used for testing.
      */
     @VisibleForTesting
     static final String OUTPUT_DIR_OPTION = "AppSearchCompiler.OutputDir";
-
-    private Messager mMessager;
 
     @Override
     @NonNull
@@ -56,73 +59,67 @@ public class AppSearchCompiler extends AbstractProcessor {
     }
 
     @Override
-    public synchronized void init(@NonNull ProcessingEnvironment processingEnvironment) {
-        super.init(processingEnvironment);
-        mMessager = processingEnvironment.getMessager();
+    protected Iterable<? extends Step> steps() {
+        return ImmutableList.of(new AppSearchCompileStep(processingEnv));
     }
 
-    @Override
-    public boolean process(
-            @NonNull Set<? extends TypeElement> set,
-            @NonNull RoundEnvironment roundEnvironment) {
-        try {
-            tryProcess(set, roundEnvironment);
-        } catch (ProcessingException e) {
-            e.printDiagnostic(mMessager);
+    private static final class AppSearchCompileStep implements Step {
+        private final ProcessingEnvironment mProcessingEnv;
+        private final Messager mMessager;
+
+        AppSearchCompileStep(ProcessingEnvironment processingEnv) {
+            mProcessingEnv = processingEnv;
+            mMessager = processingEnv.getMessager();
         }
-        // True means we claimed the annotations. This is true regardless of whether they were
-        // used correctly.
-        return true;
-    }
 
-    private void tryProcess(
-            @NonNull Set<? extends TypeElement> set,
-            @NonNull RoundEnvironment roundEnvironment) throws ProcessingException {
-        if (set.isEmpty()) return;
+        @Override
+        public ImmutableSet<String> annotations() {
+            return ImmutableSet.of(IntrospectionHelper.DOCUMENT_ANNOTATION_CLASS);
+        }
 
-        // Find the TypeElement corresponding to the @Document annotation. We can't use the
-        // annotation class directly because the appsearch project compiles only on Android, but
-        // this annotation processor runs on the host.
-        TypeElement documentAnnotation =
-                findAnnotation(set, IntrospectionHelper.DOCUMENT_ANNOTATION_CLASS);
+        @Override
+        public ImmutableSet<Element> process(
+                ImmutableSetMultimap<String, Element> elementsByAnnotation) {
+            Set<TypeElement> documentElements =
+                    typesIn(elementsByAnnotation.get(
+                            IntrospectionHelper.DOCUMENT_ANNOTATION_CLASS));
+            for (TypeElement document : documentElements) {
+                try {
+                    processDocument(document);
+                } catch (ProcessingException e) {
+                    // Prints error message.
+                    e.printDiagnostic(mMessager);
+                }
+            }
+            // No elements will be passed to next round of processing.
+            return ImmutableSet.of();
+        }
 
-        for (Element element : roundEnvironment.getElementsAnnotatedWith(documentAnnotation)) {
+        private void processDocument(@NonNull TypeElement element) throws ProcessingException {
             if (element.getKind() != ElementKind.CLASS) {
                 throw new ProcessingException(
                         "@Document annotation on something other than a class", element);
             }
-            processDocument((TypeElement) element);
-        }
-    }
 
-    private void processDocument(@NonNull TypeElement element) throws ProcessingException {
-        DocumentModel model = DocumentModel.create(processingEnv, element);
-        CodeGenerator generator = CodeGenerator.generate(processingEnv, model);
-        String outputDir = processingEnv.getOptions().get(OUTPUT_DIR_OPTION);
-        try {
-            if (outputDir == null || outputDir.isEmpty()) {
-                generator.writeToFiler();
-            } else {
-                mMessager.printMessage(
-                        Diagnostic.Kind.NOTE,
-                        "Writing output to \"" + outputDir
-                                + "\" due to the presence of -A" + OUTPUT_DIR_OPTION);
-                generator.writeToFolder(new File(outputDir));
-            }
-        } catch (IOException e) {
-            ProcessingException pe =
-                    new ProcessingException("Failed to write output", model.getClassElement());
-            pe.initCause(e);
-            throw pe;
-        }
-    }
-
-    private TypeElement findAnnotation(Set<? extends TypeElement> set, String name) {
-        for (TypeElement typeElement : set) {
-            if (typeElement.getQualifiedName().contentEquals(name)) {
-                return typeElement;
+            DocumentModel model = DocumentModel.create(mProcessingEnv, element);
+            CodeGenerator generator = CodeGenerator.generate(mProcessingEnv, model);
+            String outputDir = mProcessingEnv.getOptions().get(OUTPUT_DIR_OPTION);
+            try {
+                if (outputDir == null || outputDir.isEmpty()) {
+                    generator.writeToFiler();
+                } else {
+                    mMessager.printMessage(
+                            Kind.NOTE,
+                            "Writing output to \"" + outputDir
+                                    + "\" due to the presence of -A" + OUTPUT_DIR_OPTION);
+                    generator.writeToFolder(new File(outputDir));
+                }
+            } catch (IOException e) {
+                ProcessingException pe =
+                        new ProcessingException("Failed to write output", model.getClassElement());
+                pe.initCause(e);
+                throw pe;
             }
         }
-        return null;
     }
 }
