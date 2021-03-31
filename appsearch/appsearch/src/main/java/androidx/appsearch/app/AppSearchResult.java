cd525fefacbaa712d20457ed323ec8ffca0d65dc
Alexander Dorokhine <adorokhine@google.com>
2021-03-31 01:07:16 -0700

    Fixes and improvements to AppSearchResult.
    * Log NOT_FOUND at a verbose level to reduce log spam for operations
      which are sometimes intentional
    * Map NullPointerException to INTERNAL_ERROR instead of UNKNOWN_ERROR.
      We don't expect NPEs to be generated inside the backend
      implementation. If they are, something has gone wrong internally.
    * Include exception name in the AppSearchResult message. Some exceptions
      (especially NPE) have no message, or a terse and confusing message if
      presented without the context of the originating exception.
    
    Bug: 183550974
    Bug: 181787682
    Test: AppSearchResultTest#testMapNullPointer
    Change-Id: I28d8555c5087657b71e01e6f27cd96c242d2c866

--- appsearch/appsearch/src/main/java/androidx/appsearch/app/AppSearchResult.java
+++ appsearch/appsearch/src/main/java/androidx/appsearch/app/AppSearchResult.java
@@ -36,6 +36,8 @@ import java.lang.annotation.RetentionPolicy;
  * @param <ValueType> The type of result object for successful calls.
  */
 public final class AppSearchResult<ValueType> {
+    private static final String TAG = "AppSearchResult";
+
     /**
      * Result codes from {@link AppSearchSession} methods.
      * @hide
@@ -211,14 +213,22 @@ public final class AppSearchResult<ValueType> {
     @NonNull
     public static <ValueType> AppSearchResult<ValueType> throwableToFailedResult(
             @NonNull Throwable t) {
-        Log.d("AppSearchResult", "Converting throwable to failed result.", t);
+        // Log for traceability. NOT_FOUND is logged at VERBOSE because this error can occur during
+        // the regular operation of the system (b/183550974). Everything else is logged at DEBUG.
+        if (t instanceof AppSearchException
+                && ((AppSearchException) t).getResultCode() == RESULT_NOT_FOUND) {
+            Log.v(TAG, "Converting throwable to failed result: " + t);
+        } else {
+            Log.d(TAG, "Converting throwable to failed result.", t);
+        }
 
         if (t instanceof AppSearchException) {
             return ((AppSearchException) t).toAppSearchResult();
         }
 
+        String exceptionClass = t.getClass().getSimpleName();
         @AppSearchResult.ResultCode int resultCode;
-        if (t instanceof IllegalStateException) {
+        if (t instanceof IllegalStateException || t instanceof NullPointerException) {
             resultCode = AppSearchResult.RESULT_INTERNAL_ERROR;
         } else if (t instanceof IllegalArgumentException) {
             resultCode = AppSearchResult.RESULT_INVALID_ARGUMENT;
@@ -227,6 +237,6 @@ public final class AppSearchResult<ValueType> {
         } else {
             resultCode = AppSearchResult.RESULT_UNKNOWN_ERROR;
         }
-        return AppSearchResult.newFailedResult(resultCode, t.getMessage());
+        return AppSearchResult.newFailedResult(resultCode, exceptionClass + ": " + t.getMessage());
     }
 }
