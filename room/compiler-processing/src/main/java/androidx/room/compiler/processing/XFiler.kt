86343f12bd244efd9350e839ba68936903c500ed
Eli Hart <eli.hart@airbnb.com>
2021-04-06 14:25:59 -0500

    Add enum based filing mode

--- room/compiler-processing/src/main/java/androidx/room/compiler/processing/XFiler.kt
+++ room/compiler-processing/src/main/java/androidx/room/compiler/processing/XFiler.kt
@@ -23,22 +23,27 @@ import com.squareup.kotlinpoet.FileSpec
  * Code generation interface for XProcessing.
  */
 interface XFiler {
+
+    fun write(javaFile: JavaFile, mode: Mode = Mode.Isolating)
+
+    fun write(fileSpec: FileSpec, mode: Mode = Mode.Isolating)
+
     /**
-     * @param aggregating Specifies whether this
-     * file represents aggregating or isolating inputs for incremental build purposes. This does
-     * not apply in Javac processing because aggregating vs isolating is set on the processor
-     * level. For more on KSP's definitions of isolating vs aggregating see the documentation at
+     * Specifies whether a file represents aggregating or isolating inputs for incremental
+     * build purposes. This does not apply in Javac processing because aggregating vs isolating
+     * is set on the processor level. For more on KSP's definitions of isolating vs aggregating
+     * see the documentation at
      * https://github.com/google/ksp/blob/master/docs/incremental.md
      */
-    fun write(javaFile: JavaFile, aggregating: Boolean = false)
-
-    fun write(fileSpec: FileSpec, aggregating: Boolean = false)
+    enum class Mode {
+        Aggregating, Isolating
+    }
 }
 
-fun JavaFile.writeTo(generator: XFiler, aggregating: Boolean = false) {
-    generator.write(this, aggregating)
+fun JavaFile.writeTo(generator: XFiler, mode: XFiler.Mode = XFiler.Mode.Isolating) {
+    generator.write(this, mode)
 }
 
-fun FileSpec.writeTo(generator: XFiler, aggregating: Boolean = false) {
-    generator.write(this, aggregating)
+fun FileSpec.writeTo(generator: XFiler, mode: XFiler.Mode = XFiler.Mode.Isolating) {
+    generator.write(this, mode)
 }
