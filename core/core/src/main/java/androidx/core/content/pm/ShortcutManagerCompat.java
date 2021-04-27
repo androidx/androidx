558b7b3e5a8ef454a52a26ca49e5f526730ce60d
David Dong <ddong@google.com>
2021-04-27 15:59:28 -0400

    remove alpha dependencies on security lib and ToT dependency on core
    Test: unit tests and tested in test app
    Change-Id: Ide92e942b98b76ef77a3715fc49d3f3d1176d47f

--- core/core/src/main/java/androidx/core/content/pm/ShortcutManagerCompat.java
+++ core/core/src/main/java/androidx/core/content/pm/ShortcutManagerCompat.java
@@ -830,7 +830,7 @@ public class ShortcutManagerCompat {
     private static List<ShortcutInfoChangeListener> getShortcutInfoListeners(Context context) {
         if (sShortcutInfoChangeListeners == null) {
             List<ShortcutInfoChangeListener> result = new ArrayList<>();
-            if (Build.VERSION.SDK_INT >= 21) {
+            if (Build.VERSION.SDK_INT >= 23) {
                 PackageManager packageManager = context.getPackageManager();
                 Intent activityIntent = new Intent(SHORTCUT_LISTENER_INTENT_FILTER_ACTION);
                 activityIntent.setPackage(context.getPackageName());
