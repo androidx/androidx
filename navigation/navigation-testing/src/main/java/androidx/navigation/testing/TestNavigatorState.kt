ff325dad7ef332217b2cb9ae1428d24bb10fbc3b
Ian Lake <ilake@google.com>
2021-05-05 14:55:58 -0700

    Ensure TestNavigatorState updates the Lifecycle correctly
    The TestNavigatorState serves the role of
    "NavController" when building isolated tests
    for a Navigator. As such, it owns the logic for
    updating and maintaining the state of each
    NavBackStackEntry added to it.
    
    As such, the TestNavigatorState now updates
    the Lifecycle of each NavBackStackEntry
    as it is added and popped from the state's
    back stack, mirroring the behavior of
    NavController.
    
    Relnote: N/A
    Test: new TestNavigatorStateTest
    BUG: 80029773
    Change-Id: I92b09989a7d9bc63d52747eef40f04d75a43cc0d

--- navigation/navigation-testing/src/main/java/androidx/navigation/testing/TestNavigatorState.kt
+++ navigation/navigation-testing/src/main/java/androidx/navigation/testing/TestNavigatorState.kt
@@ -22,10 +22,15 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.testing.TestLifecycleOwner
+import androidx.navigation.FloatingWindow
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDestination
 import androidx.navigation.NavViewModelStoreProvider
 import androidx.navigation.NavigatorState
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.util.UUID
 
 /**
@@ -36,12 +41,20 @@ import java.util.UUID
  * An optional [context] can be provided to allow for the usages of
  * [androidx.lifecycle.AndroidViewModel] within the created [NavBackStackEntry]
  * instances.
+ *
+ * The [Lifecycle] of all [NavBackStackEntry] instances added to this TestNavigatorState
+ * will be updated as they are added and removed from the state. This work is kicked off
+ * on the [coroutineDispatcher].
  */
 public class TestNavigatorState @JvmOverloads constructor(
-    private val context: Context? = null
+    private val context: Context? = null,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Main.immediate
 ) : NavigatorState() {
 
-    private val lifecycleOwner: LifecycleOwner = TestLifecycleOwner(Lifecycle.State.RESUMED)
+    private val lifecycleOwner: LifecycleOwner = TestLifecycleOwner(
+        Lifecycle.State.RESUMED,
+        coroutineDispatcher
+    )
 
     private val viewModelStoreProvider = object : NavViewModelStoreProvider {
         private val viewModelStores = mutableMapOf<UUID, ViewModelStore>()
@@ -58,4 +71,41 @@ public class TestNavigatorState @JvmOverloads constructor(
     ): NavBackStackEntry = NavBackStackEntry.create(
         context, destination, arguments, lifecycleOwner, viewModelStoreProvider
     )
+
+    override fun add(backStackEntry: NavBackStackEntry) {
+        super.add(backStackEntry)
+        updateMaxLifecycle()
+    }
+
+    override fun pop(popUpTo: NavBackStackEntry, saveState: Boolean) {
+        val beforePopList = backStack.value
+        val poppedList = beforePopList.subList(beforePopList.indexOf(popUpTo), beforePopList.size)
+        super.pop(popUpTo, saveState)
+        updateMaxLifecycle(poppedList)
+    }
+
+    private fun updateMaxLifecycle(poppedList: List<NavBackStackEntry> = emptyList()) {
+        runBlocking(coroutineDispatcher) {
+            // NavBackStackEntry Lifecycles must be updated on the main thread
+            // as per the contract within Lifecycle, so we explicitly swap to the main thread
+            // no matter what CoroutineDispatcher was passed to us.
+            withContext(Dispatchers.Main.immediate) {
+                // Mark all removed NavBackStackEntries as DESTROYED
+                for (entry in poppedList.reversed()) {
+                    entry.maxLifecycle = Lifecycle.State.DESTROYED
+                }
+                // Now go through the current list of destinations, updating their Lifecycle state
+                val currentList = backStack.value
+                var previousEntry: NavBackStackEntry? = null
+                for (entry in currentList.reversed()) {
+                    entry.maxLifecycle = when {
+                        previousEntry == null -> Lifecycle.State.RESUMED
+                        previousEntry.destination is FloatingWindow -> Lifecycle.State.STARTED
+                        else -> Lifecycle.State.CREATED
+                    }
+                    previousEntry = entry
+                }
+            }
+        }
+    }
 }
