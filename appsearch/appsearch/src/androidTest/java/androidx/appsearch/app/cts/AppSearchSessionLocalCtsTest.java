894a04cea84dc060089ddcd7552f9e80d2e00cd7
Alan Viverette <alanv@google.com>
2021-05-11 12:28:19 -0400

    Suppress flaky tests
    Bug: 179770443
    Bug: 187828770
    Bug: 187830562
    Bug: 187831733
    Test: SwipeTest, TransactionExecutorTest, TextSelectionColorScreenshotTest
    Change-Id: Ib49744bc3305121a18c7fe55da90d4dc859fcc4a

--- appsearch/appsearch/src/androidTest/java/androidx/appsearch/app/cts/AppSearchSessionLocalCtsTest.java
+++ appsearch/appsearch/src/androidTest/java/androidx/appsearch/app/cts/AppSearchSessionLocalCtsTest.java
@@ -22,8 +22,11 @@ import androidx.annotation.NonNull;
 import androidx.appsearch.app.AppSearchSession;
 import androidx.appsearch.localstorage.LocalStorage;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.filters.FlakyTest;
 
 import com.google.common.util.concurrent.ListenableFuture;
+
+import org.junit.Test;
 
 import java.util.concurrent.ExecutorService;
 
@@ -43,4 +46,12 @@ public class AppSearchSessionLocalCtsTest extends AppSearchSessionCtsTestBase {
                 new LocalStorage.SearchContext.Builder(context).setDatabaseName(dbName).build(),
                 executor);
     }
+
+    @FlakyTest(bugId = 187831733)
+    @Test
+    @Override
+    public void testCloseAndReopen() throws Exception {
+        super.testCloseAndReopen();
+    }
+
 }
