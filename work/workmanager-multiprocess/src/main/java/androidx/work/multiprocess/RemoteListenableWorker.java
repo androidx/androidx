09c8bdef53c261940bc7223ec9e2f38f8500ad47
Rahul Ravikumar <rahulrav@google.com>
2021-05-14 15:47:14 -0700

    Update `RemoteListenableWorker` documentation.
    * The `Worker` might be interrupted before its execution window lapses when its constraints are unmet.
    
    Fixes: b/187530832
    Test: N/A Docs update
    Change-Id: I5a316d2e4ee28440bf1bc43cf2428c506f1efd1f

--- work/workmanager-multiprocess/src/main/java/androidx/work/multiprocess/RemoteListenableWorker.java
+++ work/workmanager-multiprocess/src/main/java/androidx/work/multiprocess/RemoteListenableWorker.java
@@ -168,8 +168,11 @@ public abstract class RemoteListenableWorker extends ListenableWorker {
      * <p>
      * A ListenableWorker is given a maximum of ten minutes to finish its execution and return a
      * {@code Result}.  After this time has expired, the worker will be signalled to stop and its
-     * {@link ListenableFuture} will be cancelled. Note that the 10 minute execution window also
+     * {@link ListenableFuture} will be cancelled. Note that the ten minute execution window also
      * includes the cost of binding to the remote process.
+     * <p>
+     * The {@link RemoteListenableWorker} will also be signalled to stop when its constraints are
+     * no longer met.
      *
      * @return A {@link ListenableFuture} with the {@code Result} of the computation.  If you
      * cancel this Future, WorkManager will treat this unit of work as a {@code Result#failure()}.
