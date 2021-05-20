89b34bdc389fc28bf2d939f1319c442bd4f70abd
Jim Sproch <jsproch@google.com>
2021-05-19 22:01:48 -0700

    Update wear-compose mpp dependencies
    Change-Id: I09a4e507a43c9028add94d5208ca8faa149efefd

--- wear/compose/material/src/commonMain/kotlin/androidx/wear/compose/material/Chip.kt
+++ wear/compose/material/src/commonMain/kotlin/androidx/wear/compose/material/Chip.kt
@@ -475,7 +475,8 @@ private class DefaultChipColors(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (other == null) return false
+        if (this::class != other::class) return false
 
         other as DefaultChipColors
 
