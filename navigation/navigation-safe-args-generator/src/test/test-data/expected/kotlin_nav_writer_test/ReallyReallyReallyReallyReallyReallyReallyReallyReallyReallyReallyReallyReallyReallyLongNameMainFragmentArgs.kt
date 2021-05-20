08a683eb231e310953da145fbf27482116f1db12
Jeremy Woods <jbwoods@google.com>
2021-05-20 16:32:11 -0700

    Add Kotlin compilation checks for safe args
    We should compile the generated files after each test so we know they
    work.
    
    Test: KotlinNavWriterTest
    Change-Id: Ic57d9eace00ebc63bc8e1d42c5ea3b1958c3855d

--- navigation/navigation-safe-args-generator/src/test/test-data/expected/kotlin_nav_writer_test/ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameMainFragmentArgs.kt
+++ navigation/navigation-safe-args-generator/src/test/test-data/expected/kotlin_nav_writer_test/ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameMainFragmentArgs.kt
@@ -3,18 +3,23 @@ package a.b
 import android.os.Bundle
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavArgs
+import java.lang.IllegalArgumentException
+import kotlin.String
 import kotlin.jvm.JvmStatic
 
 public data class
-    ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs()
-    : NavArgs {
+    ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs(
+  public val main: String,
+) : NavArgs {
   public fun toBundle(): Bundle {
     val result = Bundle()
+    result.putString("main", this.main)
     return result
   }
 
   public fun toSavedStateHandle(): SavedStateHandle {
     val result = SavedStateHandle()
+    result.set("main", this.main)
     return result
   }
 
@@ -23,13 +28,31 @@ public data class
     public fun fromBundle(bundle: Bundle):
         ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs {
       bundle.setClassLoader(ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs::class.java.classLoader)
-      return ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs()
+      val __main : String?
+      if (bundle.containsKey("main")) {
+        __main = bundle.getString("main")
+        if (__main == null) {
+          throw IllegalArgumentException("Argument \"main\" is marked as non-null but was passed a null value.")
+        }
+      } else {
+        throw IllegalArgumentException("Required argument \"main\" is missing and does not have an android:defaultValue")
+      }
+      return ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs(__main)
     }
 
     @JvmStatic
     public fun fromSavedStateHandle(savedStateHandle: SavedStateHandle):
-        ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs
-        =
-        ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs()
+        ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs {
+      val __main : String?
+      if (savedStateHandle.contains("main")) {
+        __main = savedStateHandle["main"]
+        if (__main == null) {
+          throw IllegalArgumentException("Argument \"main\" is marked as non-null but was passed a null value")
+        }
+      } else {
+        throw IllegalArgumentException("Required argument \"main\" is missing and does not have an android:defaultValue")
+      }
+      return ReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongNameFragmentArgs(__main)
+    }
   }
 }
