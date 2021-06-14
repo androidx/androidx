07d7d693542f7e84e06ec78b197247d22359a12d
Jeff Gaston <jeffrygaston@google.com>
2021-06-14 10:50:04 -0400

    Fixing androidx_incremental return code
    Bug: 188565660
    Test: ./busytown/androidx_incremental.sh misspelled && echo ok # and see that 'ok' is not echoed
    
    Change-Id: Idf38f34be3c7e5a19c33e1eab1e64dd203bfaaeb

--- busytown/androidx_incremental.sh
+++ busytown/androidx_incremental.sh
@@ -38,15 +38,20 @@ function zipKotlinMetadata() {
 }
 
 # Run Gradle
+EXIT_VALUE=0
 if impl/build.sh buildOnServer checkExternalLicenses listTaskOutputs validateAllProperties \
     --profile "$@"; then
   echo build succeeded
+  EXIT_VALUE=0
 else
   zipKotlinMetadata
   echo build failed
+  EXIT_VALUE=1
 fi
 
 # Parse performance profile reports (generated with the --profile option above) and re-export the metrics in an easily machine-readable format for tracking
 impl/parse_profile_htmls.sh
 
 echo "Completing $0 at $(date)"
+
+exit "$EXIT_VALUE"
