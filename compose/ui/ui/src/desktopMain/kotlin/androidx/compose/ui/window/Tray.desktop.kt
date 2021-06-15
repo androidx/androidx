vladertel/compose-multiplatform-core

Desktop. Composable Window API. Tray, notifications
- Add composable Tray:
```
Tray(
    icon = icon,
    state = state.tray,
    hint = "Tray",
    menu = {
        Menu("Submenu") {
            Item(
                "item1",
                onClick = {
                    println("item1")
                }
            )
        }

        Separator()

        Item(
            "Exit",
            onClick = { state.window.isOpen = false }
        )
    }
)
```

- Add global property `isTraySupported`. Tray isn't supported on some platforms.
(We should investigate this, because I saw a tray icon on Ubuntu of another application,
but Swing says there is no support)

- Add ability to send notifications. Without tray we cannot do that
(the current workaround in Notifier crashes application on Windows)

Discussion of API and demos: https://android-review.googlesource.com/c/platform/frameworks/support/+/1674523
(See examples: closeToTray, trayAndNotifications)

Test: manual ./gradlew runWindowApi -Pandroidx.compose.multiplatformEnabled=true
Test: see (https://android-review.googlesource.com/c/platform/frameworks/support/+/1674523)
Change-Id: I65ac5dae6f600a9dd8fa197ef95265f9cb50ea5e

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/window/Tray.desktop.kt
@@ -1,0 +1,176 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.window
+
+import androidx.compose.desktop.AppManager
+import androidx.compose.desktop.AppWindow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import java.awt.PopupMenu
+import java.awt.SystemTray
+import java.awt.TrayIcon
+import java.awt.image.BufferedImage
+
+/**
+ * `true` if the platform supports tray icons in the taskbar
+ */
+val isTraySupported get() = SystemTray.isSupported()
+
+// TODO(demin): add mouse click/double-click/right click listeners (can we use PointerInputEvent?)
+/**
+ * Adds tray icon to the platform taskbar if it is supported.
+ *
+ * If tray icon isn't supported by the platform, in the "standard" error output stream
+ * will be printed an error.
+ *
+ * See [isTraySupported] to know if tray icon is supported
+ * (for example to show/hide an option in the application settings)
+ *
+ * This API is experimental and will eventually replace [AppWindow] / [AppManager].
+ *
+ * @param icon Icon of the tray
+ * @param state State to control tray and show notifications
+ * @param hint Hint/tooltip that will be shown to the user
+ * @param menu Context menu of the tray that will be shown to the user on the mouse click (right
+ * click on Windows, left click on macOs).
+ * If it doesn't contain any items then context menu will not be shown.
+ * @param onAction Action performed when user clicks on the tray icon (double click on Windows,
+ * right click on macOs)
+ */
+@ExperimentalComposeUiApi
+@Composable
+fun Tray(
+    icon: BufferedImage,
+    state: TrayState = TrayState(),
+    hint: String? = null,
+    onAction: () -> Unit = {},
+    menu: @Composable MenuScope.() -> Unit = {}
+) {
+    if (!isTraySupported) {
+        DisposableEffect(Unit) {
+            // We should notify developer, but shouldn't throw an exception.
+            // If we would throw an exception, some application wouldn't work on some platforms at
+            // all, if developer doesn't check that application crashes.
+            //
+            // We can do this because we don't return anything in Tray function, and following
+            // code doesn't depend on something that is created/calculated in this function.
+            System.err.println(
+                "Tray is not supported on the current platform. " +
+                    "Use global property `isTraySupported` to check."
+            )
+            onDispose {}
+        }
+        return
+    }
+
+    val currentOnAction by rememberUpdatedState(onAction)
+
+    val tray = remember {
+        TrayIcon(icon).apply {
+            isImageAutoSize = true
+
+            addActionListener {
+                currentOnAction()
+            }
+        }
+    }
+    val popupMenu = remember { PopupMenu() }
+    val currentMenu by rememberUpdatedState(menu)
+
+    SideEffect {
+        if (tray.image != icon) tray.image = icon
+        if (tray.toolTip != hint) tray.toolTip = hint
+    }
+
+    val composition = rememberCompositionContext()
+    val coroutineScope = rememberCoroutineScope()
+
+    DisposableEffect(Unit) {
+        tray.popupMenu = popupMenu
+
+        val menuComposition = popupMenu.setContent(composition) {
+            currentMenu()
+        }
+
+        SystemTray.getSystemTray().add(tray)
+
+        state.notificationFlow
+            .onEach(tray::displayMessage)
+            .launchIn(coroutineScope)
+
+        onDispose {
+            menuComposition.dispose()
+            SystemTray.getSystemTray().remove(tray)
+        }
+    }
+}
+
+/**
+ * Creates a [WindowState] that is remembered across compositions.
+ */
+@Composable
+fun rememberTrayState() = remember {
+    TrayState()
+}
+
+/**
+ * A state object that can be hoisted to control tray and show notifications.
+ *
+ * In most cases, this will be created via [rememberTrayState].
+ */
+class TrayState {
+    private val notificationChannel = Channel<Notification>(0)
+
+    /**
+     * Flow of notifications sent by [sendNotification].
+     * This flow doesn't have a buffer, so all previously sent notifications will not appear in
+     * this flow.
+     */
+    val notificationFlow: Flow<Notification>
+        get() = notificationChannel.receiveAsFlow()
+
+    /**
+     * Send notification to tray. If [TrayState] is attached to [Tray], notification will be sent to
+     * the platform. If [TrayState] is not attached then notification will be lost.
+     */
+    fun sendNotification(notification: Notification) {
+        notificationChannel.offer(notification)
+    }
+}
+
+private fun TrayIcon.displayMessage(notification: Notification) {
+    val messageType = when (notification.type) {
+        Notification.Type.None -> TrayIcon.MessageType.NONE
+        Notification.Type.Info -> TrayIcon.MessageType.INFO
+        Notification.Type.Warning -> TrayIcon.MessageType.WARNING
+        Notification.Type.Error -> TrayIcon.MessageType.ERROR
+    }
+
+    displayMessage(notification.title, notification.message, messageType)
+}

androidx/androidx

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/window/Tray.desktop.kt
@@ -1,0 +1,177 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.window
+
+import androidx.compose.desktop.AppManager
+import androidx.compose.desktop.AppWindow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import java.awt.Image
+import java.awt.PopupMenu
+import java.awt.SystemTray
+import java.awt.TrayIcon
+
+/**
+ * `true` if the platform supports tray icons in the taskbar
+ */
+val isTraySupported get() = SystemTray.isSupported()
+
+// TODO(demin): add mouse click/double-click/right click listeners (can we use PointerInputEvent?)
+/**
+ * Adds tray icon to the platform taskbar if it is supported.
+ *
+ * If tray icon isn't supported by the platform, in the "standard" error output stream
+ * will be printed an error.
+ *
+ * See [isTraySupported] to know if tray icon is supported
+ * (for example to show/hide an option in the application settings)
+ *
+ * This API is experimental and will eventually replace [AppWindow] / [AppManager].
+ *
+ * @param icon Icon of the tray
+ * @param state State to control tray and show notifications
+ * @param hint Hint/tooltip that will be shown to the user
+ * @param menu Context menu of the tray that will be shown to the user on the mouse click (right
+ * click on Windows, left click on macOs).
+ * If it doesn't contain any items then context menu will not be shown.
+ * @param onAction Action performed when user clicks on the tray icon (double click on Windows,
+ * right click on macOs)
+ */
+@Suppress("unused")
+@ExperimentalComposeUiApi
+@Composable
+fun ApplicationScope.Tray(
+    icon: Image,
+    state: TrayState = TrayState(),
+    hint: String? = null,
+    onAction: () -> Unit = {},
+    menu: @Composable MenuScope.() -> Unit = {}
+) {
+    if (!isTraySupported) {
+        DisposableEffect(Unit) {
+            // We should notify developer, but shouldn't throw an exception.
+            // If we would throw an exception, some application wouldn't work on some platforms at
+            // all, if developer doesn't check that application crashes.
+            //
+            // We can do this because we don't return anything in Tray function, and following
+            // code doesn't depend on something that is created/calculated in this function.
+            System.err.println(
+                "Tray is not supported on the current platform. " +
+                    "Use global property `isTraySupported` to check."
+            )
+            onDispose {}
+        }
+        return
+    }
+
+    val currentOnAction by rememberUpdatedState(onAction)
+
+    val tray = remember {
+        TrayIcon(icon).apply {
+            isImageAutoSize = true
+
+            addActionListener {
+                currentOnAction()
+            }
+        }
+    }
+    val popupMenu = remember { PopupMenu() }
+    val currentMenu by rememberUpdatedState(menu)
+
+    SideEffect {
+        if (tray.image != icon) tray.image = icon
+        if (tray.toolTip != hint) tray.toolTip = hint
+    }
+
+    val composition = rememberCompositionContext()
+    val coroutineScope = rememberCoroutineScope()
+
+    DisposableEffect(Unit) {
+        tray.popupMenu = popupMenu
+
+        val menuComposition = popupMenu.setContent(composition) {
+            currentMenu()
+        }
+
+        SystemTray.getSystemTray().add(tray)
+
+        state.notificationFlow
+            .onEach(tray::displayMessage)
+            .launchIn(coroutineScope)
+
+        onDispose {
+            menuComposition.dispose()
+            SystemTray.getSystemTray().remove(tray)
+        }
+    }
+}
+
+/**
+ * Creates a [WindowState] that is remembered across compositions.
+ */
+@Composable
+fun rememberTrayState() = remember {
+    TrayState()
+}
+
+/**
+ * A state object that can be hoisted to control tray and show notifications.
+ *
+ * In most cases, this will be created via [rememberTrayState].
+ */
+class TrayState {
+    private val notificationChannel = Channel<Notification>(0)
+
+    /**
+     * Flow of notifications sent by [sendNotification].
+     * This flow doesn't have a buffer, so all previously sent notifications will not appear in
+     * this flow.
+     */
+    val notificationFlow: Flow<Notification>
+        get() = notificationChannel.receiveAsFlow()
+
+    /**
+     * Send notification to tray. If [TrayState] is attached to [Tray], notification will be sent to
+     * the platform. If [TrayState] is not attached then notification will be lost.
+     */
+    fun sendNotification(notification: Notification) {
+        notificationChannel.trySend(notification)
+    }
+}
+
+private fun TrayIcon.displayMessage(notification: Notification) {
+    val messageType = when (notification.type) {
+        Notification.Type.None -> TrayIcon.MessageType.NONE
+        Notification.Type.Info -> TrayIcon.MessageType.INFO
+        Notification.Type.Warning -> TrayIcon.MessageType.WARNING
+        Notification.Type.Error -> TrayIcon.MessageType.ERROR
+    }
+
+    displayMessage(notification.title, notification.message, messageType)
+}