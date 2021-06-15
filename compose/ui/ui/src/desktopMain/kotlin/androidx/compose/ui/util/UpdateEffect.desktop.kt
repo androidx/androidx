vladertel/compose-multiplatform-core

Desktop. Composable Window API. Window. Helper functions and classes to manipulate state
- add LocalWindow. It is needed because we need to know the owner
of the composition in case we need to show a dialog
(a modal dialog needs to know the parent window to be able to disable it)

- UpdatedEffect. Run callback every time the captured state is changed

- ComponentUpdater. Stores applied state and the next time we need to
apply a new state - compares it with previous and updates a component only if this new state is changed.
Similar to androidx.compose.runtime.Updater, but without depending on the current `composer`.

- Data classes to store the window size/position in Dp units

- Safe functions to change the state of Window

- Add ComposeWindow.isFullscreen/isMaximized/isMinimized

Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
Change-Id: Ic6e00106611647c2c7ade6298fb7457c21b6867d

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/util/UpdateEffect.desktop.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
+import kotlinx.coroutines.channels.Channel
+
+/**
+ * When [UpdateEffect] enters the composition it will call [update] and will capture all state
+ * which is used in this function.
+ *
+ * When any state is changed, [update] will be called again on the next recomposition.
+ *
+ * [update] always be called in UI thread.
+ */
+@Composable
+internal fun UpdateEffect(update: () -> Unit) {
+    val tasks = remember { Channel<() -> Unit>(Channel.RENDEZVOUS) }
+    val currentUpdate by rememberUpdatedState(update)
+
+    LaunchedEffect(Unit) {
+        for (task in tasks) {
+            task()
+        }
+    }
+
+    DisposableEffect(Unit) {
+        val snapshotObserver = SnapshotStateObserver { command ->
+            command()
+        }
+        snapshotObserver.start()
+
+        fun performUpdate() {
+            snapshotObserver.observeReads(
+                Unit,
+                onValueChangedForScope = { tasks.offer(::performUpdate) }
+            ) {
+                currentUpdate()
+            }
+        }
+
+        performUpdate()
+
+        onDispose {
+            snapshotObserver.stop()
+            snapshotObserver.clear()
+        }
+    }
+}

androidx/androidx

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/util/UpdateEffect.desktop.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
+import kotlinx.coroutines.channels.Channel
+
+/**
+ * When [UpdateEffect] enters the composition it will call [update] and will capture all state
+ * which is used in this function.
+ *
+ * When any state is changed, [update] will be called again on the next recomposition.
+ *
+ * [update] always be called in UI thread.
+ */
+@Composable
+internal fun UpdateEffect(update: () -> Unit) {
+    val tasks = remember { Channel<() -> Unit>(Channel.RENDEZVOUS) }
+    val currentUpdate by rememberUpdatedState(update)
+
+    LaunchedEffect(Unit) {
+        for (task in tasks) {
+            task()
+        }
+    }
+
+    DisposableEffect(Unit) {
+        val snapshotObserver = SnapshotStateObserver { command ->
+            command()
+        }
+        snapshotObserver.start()
+
+        fun performUpdate() {
+            snapshotObserver.observeReads(
+                Unit,
+                onValueChangedForScope = { tasks.trySend(::performUpdate) }
+            ) {
+                currentUpdate()
+            }
+        }
+
+        performUpdate()
+
+        onDispose {
+            snapshotObserver.stop()
+            snapshotObserver.clear()
+        }
+    }
+}