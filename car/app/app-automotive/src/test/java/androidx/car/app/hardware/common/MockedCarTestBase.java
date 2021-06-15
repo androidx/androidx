5a5519d1315299fa0c3e55cab9cad674e69efe3f
Kai Wang <kwangsudo@google.com>
2021-06-15 22:38:40 +0000

    Unit tests for AAOS implementation
    Adds unit tests for PropertyManager and PropertyRequestProcessor
    
    Bug: 189229744
    Test: ./gradlew :car:app:app-automotive:testDebugUnitTest
    Change-Id: I892768141cb03d8f9e74ae4bfe043b99a415a240

---
+++ car/app/app-automotive/src/test/java/androidx/car/app/hardware/common/MockedCarTestBase.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.app.hardware.common;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import android.car.Car;
+import android.car.VehiclePropertyIds;
+import android.car.hardware.CarPropertyConfig;
+import android.car.hardware.CarPropertyValue;
+import android.car.hardware.property.CarPropertyManager;
+import android.os.SystemClock;
+
+import androidx.car.app.shadows.car.ShadowCar;
+
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
+import org.robolectric.annotation.internal.DoNotInstrument;
+
+/**
+ * Base class for testing with mocked car.
+ */
+@Config(
+        manifest = Config.NONE,
+        shadows = {ShadowCar.class}
+)
+@DoNotInstrument
+public class MockedCarTestBase {
+    public static final int MODEL_YEAR = 2021;
+    public static final String MODEL_NAME = "car_name";
+    public static final String MODEL_MAKER = "android";
+    public static final boolean FUEL_DOOR_DEFAULT = true;
+
+    @Mock
+    private CarPropertyValue<Integer> mModelYearValueMock;
+    @Mock
+    private CarPropertyValue<String> mModelNameValueMock;
+    @Mock
+    private CarPropertyValue<String> mManufacturerValueMock;
+    @Mock
+    private CarPropertyConfig<Integer> mModelYearConfigMock;
+    @Mock
+    private CarPropertyConfig<String> mModelNameConfigMock;
+    @Mock
+    private CarPropertyConfig<String> mManufacturerConfigMock;
+    @Mock
+    private CarPropertyConfig<Boolean> mFuelDoorConfigMock;
+    @Mock
+    private CarPropertyValue<Boolean> mFuelDoorValueMock;
+    @Mock
+    private Car mCarMock;
+    @Mock
+    private CarPropertyManager mCarPropertyManagerMock;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        // Mock car
+        ShadowCar.setCar(mCarMock);
+        when(mCarMock.getCarManager(anyString())).thenReturn(mCarPropertyManagerMock);
+
+        // Mocks car property manager
+        doReturn(mModelYearConfigMock).when(mCarPropertyManagerMock)
+                .getCarPropertyConfig(eq(VehiclePropertyIds.INFO_MODEL_YEAR));
+        doReturn(mModelYearValueMock).when(mCarPropertyManagerMock).getProperty(
+                any(), eq(VehiclePropertyIds.INFO_MODEL_YEAR), anyInt());
+        doReturn(mManufacturerConfigMock).when(mCarPropertyManagerMock)
+                .getCarPropertyConfig(eq(VehiclePropertyIds.INFO_MAKE));
+        doReturn(mManufacturerValueMock).when(mCarPropertyManagerMock).getProperty(
+                any(), eq(VehiclePropertyIds.INFO_MAKE), anyInt());
+        doReturn(mModelNameConfigMock).when(mCarPropertyManagerMock)
+                .getCarPropertyConfig(eq(VehiclePropertyIds.INFO_MODEL));
+        doReturn(mModelNameValueMock).when(mCarPropertyManagerMock).getProperty(
+                any(), eq(VehiclePropertyIds.INFO_MODEL), anyInt());
+
+        // Sets up property configs
+        when(mModelYearConfigMock.getPropertyType()).thenReturn(Integer.class);
+        when(mModelNameConfigMock.getPropertyType()).thenReturn(String.class);
+        when(mManufacturerConfigMock.getPropertyType()).thenReturn(String.class);
+
+        // Sets up property values
+        when(mModelYearValueMock.getPropertyId()).thenReturn(VehiclePropertyIds.INFO_MODEL_YEAR);
+        when(mModelYearValueMock.getValue()).thenReturn(MODEL_YEAR);
+        when(mModelYearValueMock.getStatus()).thenReturn(CarPropertyValue.STATUS_AVAILABLE);
+        when(mModelYearValueMock.getTimestamp()).thenReturn(SystemClock.elapsedRealtimeNanos());
+        when(mModelNameValueMock.getPropertyId()).thenReturn(VehiclePropertyIds.INFO_MODEL);
+        when(mModelNameValueMock.getValue()).thenReturn(MODEL_NAME);
+        when(mModelNameValueMock.getStatus()).thenReturn(CarPropertyValue.STATUS_ERROR);
+        when(mModelNameValueMock.getTimestamp()).thenReturn(SystemClock.elapsedRealtimeNanos());
+        when(mManufacturerValueMock.getPropertyId()).thenReturn(VehiclePropertyIds.INFO_MAKE);
+        when(mManufacturerValueMock.getValue()).thenReturn(MODEL_MAKER);
+        when(mManufacturerValueMock.getStatus()).thenReturn(CarPropertyValue.STATUS_UNAVAILABLE);
+        when(mManufacturerValueMock.getTimestamp()).thenReturn(SystemClock.elapsedRealtimeNanos());
+
+        // Adds fuel_door config and value for testing permission
+        doReturn(mFuelDoorConfigMock).when(mCarPropertyManagerMock)
+                .getCarPropertyConfig(eq(VehiclePropertyIds.FUEL_DOOR_OPEN));
+        doReturn(mFuelDoorValueMock).when(mCarPropertyManagerMock).getProperty(
+                any(), eq(VehiclePropertyIds.FUEL_DOOR_OPEN), anyInt());
+        when(mFuelDoorConfigMock.getPropertyType()).thenReturn(Boolean.class);
+        when(mFuelDoorValueMock.getPropertyId()).thenReturn(VehiclePropertyIds.FUEL_DOOR_OPEN);
+        when(mFuelDoorValueMock.getValue()).thenReturn(FUEL_DOOR_DEFAULT);
+        when(mFuelDoorValueMock.getStatus()).thenReturn(CarPropertyValue.STATUS_AVAILABLE);
+        when(mFuelDoorValueMock.getTimestamp()).thenReturn(SystemClock.elapsedRealtimeNanos());
+    }
+}
---
+++ car/app/app-automotive/src/test/java/androidx/car/app/hardware/common/PropertyManagerTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.app.hardware.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.assertThrows;
+
+import android.app.Application;
+import android.car.Car;
+import android.car.VehiclePropertyIds;
+import android.util.SparseArray;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.internal.DoNotInstrument;
+import org.robolectric.shadows.ShadowApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@RunWith(RobolectricTestRunner.class)
+@DoNotInstrument
+public class PropertyManagerTest extends MockedCarTestBase {
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private PropertyManager mPropertyManager;
+
+    @Before
+    public void setUp() {
+        super.setUp();
+        MockitoAnnotations.initMocks(this);
+
+        // Sets application's permission
+        Application application = ApplicationProvider.getApplicationContext();
+        ShadowApplication shadowApplication = Shadows.shadowOf(application);
+        shadowApplication.grantPermissions(Car.PERMISSION_CAR_INFO);
+        mPropertyManager = new PropertyManager(ApplicationProvider.getApplicationContext());
+    }
+
+    /**
+     * Tests {@link PropertyManager#submitGetPropertyRequest(List, Executor)} with single request.
+     */
+    @Test
+    public void submitGetPropertyRequestTest_singleProperty() throws Exception {
+        List<GetPropertyRequest> requests = new ArrayList<>();
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.INFO_MODEL_YEAR));
+        ListenableFuture<List<CarPropertyResponse<?>>> future =
+                mPropertyManager.submitGetPropertyRequest(requests, mExecutor);
+
+        List<CarPropertyResponse<?>> responses = future.get();
+        CarPropertyResponse<?> infoYearResponse = responses.get(0);
+
+        assertThat(responses.size()).isEqualTo(requests.size());
+        assertThat(infoYearResponse.getPropertyId()).isEqualTo(VehiclePropertyIds.INFO_MODEL_YEAR);
+        assertThat(infoYearResponse.getValue()).isEqualTo(MODEL_YEAR);
+        assertThat(infoYearResponse.getStatus()).isEqualTo(CarValue.STATUS_SUCCESS);
+    }
+
+    /**
+     * Tests {@link PropertyManager#submitGetPropertyRequest(List, Executor)} multi requests.
+     */
+    @Test
+    public void submitGetPropertyRequestTest_multipleProperties() throws Exception {
+        List<GetPropertyRequest> requests = new ArrayList<>();
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.INFO_MODEL_YEAR));
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.INFO_MODEL));
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.INFO_MAKE));
+        ListenableFuture<List<CarPropertyResponse<?>>> future =
+                mPropertyManager.submitGetPropertyRequest(requests, mExecutor);
+
+        List<CarPropertyResponse<?>> responses = future.get();
+        SparseArray<CarPropertyResponse<?>> responsesMap = getCarPropertyResponseMap(responses);
+        CarPropertyResponse<?> infoMakerResponse = responsesMap.get(VehiclePropertyIds.INFO_MAKE);
+        CarPropertyResponse<?> infoModelResponse = responsesMap.get(VehiclePropertyIds.INFO_MODEL);
+        CarPropertyResponse<?> infoYearResponse =
+                responsesMap.get(VehiclePropertyIds.INFO_MODEL_YEAR);
+
+        assertThat(responses.size()).isEqualTo(requests.size());
+        assertThat(infoMakerResponse.getValue()).isEqualTo(MODEL_MAKER);
+        assertThat(infoMakerResponse.getStatus()).isEqualTo(CarValue.STATUS_UNAVAILABLE);
+        assertThat(infoModelResponse.getValue()).isEqualTo(MODEL_NAME);
+        assertThat(infoModelResponse.getStatus()).isEqualTo(CarValue.STATUS_UNKNOWN);
+        assertThat(infoYearResponse.getValue()).isEqualTo(MODEL_YEAR);
+        assertThat(infoYearResponse.getStatus()).isEqualTo(CarValue.STATUS_SUCCESS);
+    }
+
+    /**
+     * Tests get property without permissions should throw exception
+     */
+    @Test
+    public void submitGetPropertyRequestTest_withoutPermission() {
+        List<GetPropertyRequest> requests = new ArrayList<>();
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.FUEL_DOOR_OPEN));
+
+        assertThrows(SecurityException.class, () ->
+                mPropertyManager.submitGetPropertyRequest(requests, mExecutor));
+    }
+
+    /**
+     * Gets an unimplemented property
+     */
+    @Test
+    public void submitGetPropertyRequestTest_unimplementedProperty() throws Exception {
+        List<GetPropertyRequest> requests = new ArrayList<>();
+        requests.add(GetPropertyRequest.create(VehiclePropertyIds.INFO_FUEL_TYPE));
+
+        ListenableFuture<List<CarPropertyResponse<?>>> future =
+                mPropertyManager.submitGetPropertyRequest(requests,
+                        mExecutor);
+        List<CarPropertyResponse<?>> responses = future.get();
+        CarPropertyResponse<?> response = responses.get(0);
+
+        assertThat(response.getPropertyId()).isEqualTo(VehiclePropertyIds.INFO_FUEL_TYPE);
+        assertThat(responses.size()).isEqualTo(requests.size());
+        assertThat(response.getValue()).isNull();
+        assertThat(response.getStatus()).isEqualTo(CarValue.STATUS_UNIMPLEMENTED);
+    }
+
+    private static SparseArray<CarPropertyResponse<?>> getCarPropertyResponseMap(
+            List<CarPropertyResponse<?>> responses) {
+        SparseArray<CarPropertyResponse<?>> responseSparseArray = new SparseArray<>();
+        for (CarPropertyResponse<?> response : responses) {
+            responseSparseArray.put(response.getPropertyId(), response);
+        }
+        return responseSparseArray;
+    }
+}
