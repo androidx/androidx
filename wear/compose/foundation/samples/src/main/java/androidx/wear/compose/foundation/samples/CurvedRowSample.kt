e95065f0656a28ed41e5b15b879df30f1d709d41
Sergio Sancho <ssancho@google.com>
2021-06-23 11:01:52 +0100

    Add CurvedRow to wear-compose
    A CurvedRow is a Layout that places its children on an arc, similar to
    a row, but with the content drawn in an annulus sector instead of a
    box.
    Parameters can be used to specify which angle will be used as anchor to
    lay down the children, in which direction to laid them down (clockwise
    or counter-clockwise), and how to use the anchor (start there, end there
    or center around there).
    Added a new category for foundation demos and a small screen to the Demo.
    Added a new project for samples to be used in the demo and javadocs.
    
    Test: To be added.
    Relnote: "Add a new class CurvedRow to laid out composables in an arc"
    Bug: 190807301
    
    Change-Id: I29941d2f915c3da65194402367dcd30898a4d0b2

---
+++ wear/compose/foundation/samples/src/main/java/androidx/wear/compose/foundation/samples/CurvedRowSample.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.compose.foundation.samples
+
+import androidx.annotation.Sampled
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.foundation.CurvedRow
+
+@Sampled
+@Composable
+fun SimpleCurvedRow() {
+    CurvedRow(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        BasicText(
+            "Simple",
+            Modifier.background(Color.White).padding(2.dp),
+            TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
+        )
+        Box(
+            modifier = Modifier
+                .size(20.dp)
+                .background(Color.Gray)
+        )
+        BasicText(
+            "CurvedRow",
+            Modifier.background(Color.White).padding(2.dp),
+            TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
+        )
+    }
+}
