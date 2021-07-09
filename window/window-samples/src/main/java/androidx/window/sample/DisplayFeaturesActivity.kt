03c4acf7ce15f8d29fff19c4547127e5751501e3
Kenneth Ford <kennethford@google.com>
2021-07-09 19:54:29 +0000

    Updates our internal sample to remove usages of WindowManager
    Updates our samples to remove WindowManager and WindowBackend usages and
    migrate to the new repo API's. Contributed from pmaggi@ updates on
    Github
    
    Bug: N/A
    Test: Manual
    Change-Id: Ia07a9528a6c3ff9b6edf32045008421b08a4a72f

--- window/window-samples/src/main/java/androidx/window/sample/DisplayFeaturesActivity.kt
+++ window/window-samples/src/main/java/androidx/window/sample/DisplayFeaturesActivity.kt
@@ -21,49 +21,53 @@ import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
-import androidx.core.util.Consumer
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.window.WindowInfoRepo
 import androidx.window.WindowLayoutInfo
-import androidx.window.WindowManager
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 /** Demo activity that shows all display features and current device state on the screen. */
-class DisplayFeaturesActivity : BaseSampleActivity() {
+class DisplayFeaturesActivity : AppCompatActivity() {
 
-    private lateinit var windowManager: WindowManager
     private val stateLog: StringBuilder = StringBuilder()
 
     private val displayFeatureViews = ArrayList<View>()
-    // Store most recent values for the device state and window layout
-    private val stateContainer = StateContainer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_features)
 
-        windowManager = getTestBackend()?.let { backend ->
-            @Suppress("DEPRECATION") // TODO(b/173739071) remove when updating WindowManager
-            WindowManager(this, backend)
-        }
-            ?: WindowManager(this)
+        val windowInfoRepo = WindowInfoRepo.create(this)
 
+        lifecycleScope.launch(Dispatchers.Main) {
+            // The block passed to repeatOnLifecycle is executed when the lifecycle
+            // is at least STARTED and is cancelled when the lifecycle is STOPPED.
+            // It automatically restarts the block when the lifecycle is STARTED again.
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // Safely collect from windowInfoRepo when the lifecycle is STARTED
+                // and stops collection when the lifecycle is STOPPED
+                windowInfoRepo.windowLayoutInfo
+                    .collect { newLayoutInfo ->
+                        // New posture information
+                        updateStateLog(newLayoutInfo)
+                        updateCurrentState(newLayoutInfo)
+                    }
+            }
+        }
         stateLog.clear()
         stateLog.append(getString(R.string.stateUpdateLog)).append("\n")
     }
 
-    override fun onStart() {
-        super.onStart()
-        windowManager.registerLayoutChangeCallback(mainThreadExecutor, stateContainer)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        windowManager.unregisterLayoutChangeCallback(stateContainer)
-    }
-
     /** Updates the device state and display feature positions. */
-    internal fun updateCurrentState() {
+    internal fun updateCurrentState(windowLayoutInfo: WindowLayoutInfo) {
         // Cleanup previously added feature views
         val rootLayout = findViewById<FrameLayout>(R.id.featureContainerLayout)
         for (featureView in displayFeatureViews) {
@@ -74,33 +78,30 @@ class DisplayFeaturesActivity : BaseSampleActivity() {
         // Update the UI with the current state
         val stateStringBuilder = StringBuilder()
 
-        stateContainer.lastLayoutInfo?.let { windowLayoutInfo ->
-            stateStringBuilder.append(getString(R.string.windowLayout))
-                .append(": ")
-                .append(windowLayoutInfo)
+        stateStringBuilder.append(getString(R.string.windowLayout))
+            .append(": ")
 
-            // Add views that represent display features
-            for (displayFeature in windowLayoutInfo.displayFeatures) {
-                val lp = getLayoutParamsForFeatureInFrameLayout(displayFeature, rootLayout)
-                    ?: continue
+        // Add views that represent display features
+        for (displayFeature in windowLayoutInfo.displayFeatures) {
+            val lp = getLayoutParamsForFeatureInFrameLayout(displayFeature, rootLayout)
+                ?: continue
 
-                // Make sure that zero-wide and zero-high features are still shown
-                if (lp.width == 0) {
-                    lp.width = 1
-                }
-                if (lp.height == 0) {
-                    lp.height = 1
-                }
-
-                val featureView = View(this)
-                val color = getColor(R.color.colorFeatureFold)
-                featureView.foreground = ColorDrawable(color)
-
-                rootLayout.addView(featureView, lp)
-                featureView.id = View.generateViewId()
-
-                displayFeatureViews.add(featureView)
+            // Make sure that zero-wide and zero-high features are still shown
+            if (lp.width == 0) {
+                lp.width = 1
             }
+            if (lp.height == 0) {
+                lp.height = 1
+            }
+
+            val featureView = View(this)
+            val color = getColor(R.color.colorFeatureFold)
+            featureView.foreground = ColorDrawable(color)
+
+            rootLayout.addView(featureView, lp)
+            featureView.id = View.generateViewId()
+
+            displayFeatureViews.add(featureView)
         }
 
         findViewById<TextView>(R.id.currentState).text = stateStringBuilder.toString()
@@ -119,15 +120,5 @@ class DisplayFeaturesActivity : BaseSampleActivity() {
         val sdf = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
         val currentDate = sdf.format(Date())
         return currentDate.toString()
-    }
-
-    inner class StateContainer : Consumer<WindowLayoutInfo> {
-        var lastLayoutInfo: WindowLayoutInfo? = null
-
-        override fun accept(newLayoutInfo: WindowLayoutInfo) {
-            updateStateLog(newLayoutInfo)
-            lastLayoutInfo = newLayoutInfo
-            updateCurrentState()
-        }
     }
 }
