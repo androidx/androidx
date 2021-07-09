9386ec04d905a724fa8b46170d7ac9e2f14079bb
Soonil Nagarkar <sooniln@google.com>
2021-07-09 11:53:04 -0700

    Add Math compat functions to MathUtils
    Relnote: Added Math compat functions to MathUtils
    Test: MathUtilsTest
    
    Change-Id: Idb590f4b897a3cdeadbc6e84b3386d0c6031bdf6

--- core/core/src/main/java/androidx/core/math/MathUtils.java
+++ core/core/src/main/java/androidx/core/math/MathUtils.java
@@ -24,6 +24,175 @@ public class MathUtils {
     private MathUtils() {}
 
     /**
+     * See {@link Math#addExact(int, int)}.
+     */
+    public static int addExact(int x, int y) {
+        // copied from Math.java
+        int r = x + y;
+        // HD 2-12 Overflow iff both arguments have the opposite sign of the result
+        if (((x ^ r) & (y ^ r)) < 0) {
+            throw new ArithmeticException("integer overflow");
+        }
+        return r;
+    }
+
+    /**
+     * See {@link Math#addExact(long, long)}.
+     */
+    public static long addExact(long x, long y) {
+        // copied from Math.java
+        long r = x + y;
+        // HD 2-12 Overflow iff both arguments have the opposite sign of the result
+        if (((x ^ r) & (y ^ r)) < 0) {
+            throw new ArithmeticException("long overflow");
+        }
+        return r;
+    }
+
+
+    /**
+     * See {@link Math#subtractExact(int, int)}.
+     */
+    public static int subtractExact(int x, int y) {
+        // copied from Math.java
+        int r = x - y;
+        // HD 2-12 Overflow iff the arguments have different signs and
+        // the sign of the result is different than the sign of x
+        if (((x ^ y) & (x ^ r)) < 0) {
+            throw new ArithmeticException("integer overflow");
+        }
+        return r;
+    }
+
+    /**
+     * See {@link Math#subtractExact(long, long)}.
+     */
+    public static long subtractExact(long x, long y) {
+        // copied from Math.java
+        long r = x - y;
+        // HD 2-12 Overflow iff the arguments have different signs and
+        // the sign of the result is different than the sign of x
+        if (((x ^ y) & (x ^ r)) < 0) {
+            throw new ArithmeticException("long overflow");
+        }
+        return r;
+    }
+
+    /**
+     * See {@link Math#multiplyExact(int, int)}.
+     */
+    public static int multiplyExact(int x, int y) {
+        // copied from Math.java
+        long r = (long) x * (long) y;
+        if ((int) r != r) {
+            throw new ArithmeticException("integer overflow");
+        }
+        return (int) r;
+    }
+
+    /**
+     * See {@link Math#multiplyExact(long, long)}.
+     */
+    public static long multiplyExact(long x, long y) {
+        // copied from Math.java
+        long r = x * y;
+        long ax = Math.abs(x);
+        long ay = Math.abs(y);
+        if (((ax | ay) >>> 31 != 0)) {
+            // Some bits greater than 2^31 that might cause overflow
+            // Check the result using the divide operator
+            // and check for the special case of Long.MIN_VALUE * -1
+            if (((y != 0) && (r / y != x)) || (x == Long.MIN_VALUE && y == -1)) {
+                throw new ArithmeticException("long overflow");
+            }
+        }
+        return r;
+    }
+
+    /**
+     * See {@link Math#incrementExact(int)}.
+     */
+    public static int incrementExact(int a) {
+        // copied from Math.java
+        if (a == Integer.MAX_VALUE) {
+            throw new ArithmeticException("integer overflow");
+        }
+
+        return a + 1;
+    }
+
+    /**
+     * See {@link Math#incrementExact(long)}.
+     */
+    public static long incrementExact(long a) {
+        // copied from Math.java
+        if (a == Long.MAX_VALUE) {
+            throw new ArithmeticException("long overflow");
+        }
+
+        return a + 1L;
+    }
+
+    /**
+     * See {@link Math#decrementExact(int)}.
+     */
+    public static int decrementExact(int a) {
+        // copied from Math.java
+        if (a == Integer.MIN_VALUE) {
+            throw new ArithmeticException("integer overflow");
+        }
+
+        return a - 1;
+    }
+
+    /**
+     * See {@link Math#decrementExact(long)}.
+     */
+    public static long decrementExact(long a) {
+        // copied from Math.java
+        if (a == Long.MIN_VALUE) {
+            throw new ArithmeticException("long overflow");
+        }
+
+        return a - 1L;
+    }
+
+    /**
+     * See {@link Math#negateExact(int)}.
+     */
+    public static int negateExact(int a) {
+        // copied from Math.java
+        if (a == Integer.MIN_VALUE) {
+            throw new ArithmeticException("integer overflow");
+        }
+
+        return -a;
+    }
+
+    /**
+     * See {@link Math#negateExact(long)}.
+     */
+    public static long negateExact(long a) {
+        // copied from Math.java
+        if (a == Long.MIN_VALUE) {
+            throw new ArithmeticException("long overflow");
+        }
+
+        return -a;
+    }
+
+    /**
+     * See {@link Math#toIntExact(long)}.
+     */
+    public static int toIntExact(long value) {
+        // copied from Math.java
+        if ((int) value != value) {
+            throw new ArithmeticException("integer overflow");
+        }
+        return (int) value;
+    }
+
+    /**
      * This method takes a numerical value and ensures it fits in a given numerical range. If the
      * number is smaller than the minimum required by the range, then the minimum of the range will
      * be returned. If the number is higher than the maximum allowed by the range then the maximum
