140b5f19b748524b4e8448328dc262f3f2d10797
Nick Anthony <nickanthony@google.com>
2021-07-15 12:08:50 -0400

    Convert JetpadClient.py to spaces instead of tabs
    Test: cd development/auto-version-updater && ./update_versions_for_release.py 1626886800000
    Change-Id: Ieaf4d533f6cfdbd6969344662d9fd12d088d9437

--- development/JetpadClient.py
+++ development/JetpadClient.py
@@ -25,72 +25,72 @@ def print_e(*args, **kwargs):
 
 
 def getJetpadReleaseInfo(date):
-	try:
-		rawJetpadReleaseOutput = subprocess.check_output('span sql /span/global/androidx-jetpad:prod_instance \"SELECT GroupId, ArtifactId, ReleaseVersion, PreviousReleaseSHA, ReleaseSHA, Path, RequireSameVersionGroupBuild, ReleaseBuildId, ReleaseBranch FROM LibraryReleases WHERE ReleaseDate = %s\"' % date, shell=True)
-	except subprocess.CalledProcessError:
-		print_e("FAIL: Failed to get jetpad release info for  %s. "
-				"This likely means you need to run gcert." %  date)
-		return None
-	rawJetpadReleaseOutputLines = rawJetpadReleaseOutput.splitlines()
-	if len(rawJetpadReleaseOutputLines) <= 2:
-		print_e("Error: Date %s returned zero results from Jetpad.  Please check your date" % args.date)
-		return None
-	jetpadReleaseOutput = iter(rawJetpadReleaseOutputLines)
-	return jetpadReleaseOutput
+    try:
+        rawJetpadReleaseOutput = subprocess.check_output('span sql /span/global/androidx-jetpad:prod_instance \"SELECT GroupId, ArtifactId, ReleaseVersion, PreviousReleaseSHA, ReleaseSHA, Path, RequireSameVersionGroupBuild, ReleaseBuildId, ReleaseBranch FROM LibraryReleases WHERE ReleaseDate = %s\"' % date, shell=True)
+    except subprocess.CalledProcessError:
+        print_e("FAIL: Failed to get jetpad release info for  %s. "
+                "This likely means you need to run gcert." %  date)
+        return None
+    rawJetpadReleaseOutputLines = rawJetpadReleaseOutput.splitlines()
+    if len(rawJetpadReleaseOutputLines) <= 2:
+        print_e("Error: Date %s returned zero results from Jetpad.  Please check your date" % args.date)
+        return None
+    jetpadReleaseOutput = iter(rawJetpadReleaseOutputLines)
+    return jetpadReleaseOutput
 
 def getReleaseInfoObject(date, includeAllCommits, jetpadReleaseInfo):
-	releaseDateTime = datetime.datetime.fromtimestamp(float(date)/1000.0)
-	releaseJsonObject = {}
-	releaseJsonObject["releaseDate"] = "%02d-%02d-%s" % (releaseDateTime.month, releaseDateTime.day, releaseDateTime.year)
-	releaseJsonObject["includeAllCommits"] = includeAllCommits
-	releaseJsonObject["modules"] = {}
-	for line in jetpadReleaseInfo:
-		if "androidx" not in line.decode(): continue
-		# Remove all white space and split line based on '|'
-		artifactIdReleaseLine = line.decode().replace(" ", "").split('|')
-		groupId = artifactIdReleaseLine[1]
-		artifactId = artifactIdReleaseLine[2]
-		version = artifactIdReleaseLine[3]
-		fromSHA = artifactIdReleaseLine[4]
-		untilSHA = artifactIdReleaseLine[5]
-		path = artifactIdReleaseLine[6]
-		if path and path[0] == '/': path = path[1:]
-		requiresSameVersion = False
-		if artifactIdReleaseLine[7] == "true":
-			requiresSameVersion = True
-		buildId = artifactIdReleaseLine[8]
-		branch = artifactIdReleaseLine[9]
-		if groupId in releaseJsonObject["modules"]:
-			releaseJsonObject["modules"][groupId].append({
-				"groupId": groupId,
-				"artifactId": artifactId,
-				"version": version,
-				"fromSHA": fromSHA,
-				"untilSHA": untilSHA,
-				"requiresSameVersion": requiresSameVersion,
-				"path": path,
-				"buildId": buildId,
-				"branch": branch,
-			})
-		else:
-			releaseJsonObject["modules"][groupId] = [{
-				"groupId": groupId,
-				"artifactId": artifactId,
-				"version": version,
-				"fromSHA": fromSHA,
-				"untilSHA": untilSHA,
-				"requiresSameVersion": requiresSameVersion,
-				"path": path,
-				"buildId": buildId,
-				"branch": branch,
-			}]
-	return releaseJsonObject
+    releaseDateTime = datetime.datetime.fromtimestamp(float(date)/1000.0)
+    releaseJsonObject = {}
+    releaseJsonObject["releaseDate"] = "%02d-%02d-%s" % (releaseDateTime.month, releaseDateTime.day, releaseDateTime.year)
+    releaseJsonObject["includeAllCommits"] = includeAllCommits
+    releaseJsonObject["modules"] = {}
+    for line in jetpadReleaseInfo:
+        if "androidx" not in line.decode(): continue
+        # Remove all white space and split line based on '|'
+        artifactIdReleaseLine = line.decode().replace(" ", "").split('|')
+        groupId = artifactIdReleaseLine[1]
+        artifactId = artifactIdReleaseLine[2]
+        version = artifactIdReleaseLine[3]
+        fromSHA = artifactIdReleaseLine[4]
+        untilSHA = artifactIdReleaseLine[5]
+        path = artifactIdReleaseLine[6]
+        if path and path[0] == '/': path = path[1:]
+        requiresSameVersion = False
+        if artifactIdReleaseLine[7] == "true":
+            requiresSameVersion = True
+        buildId = artifactIdReleaseLine[8]
+        branch = artifactIdReleaseLine[9]
+        if groupId in releaseJsonObject["modules"]:
+            releaseJsonObject["modules"][groupId].append({
+                "groupId": groupId,
+                "artifactId": artifactId,
+                "version": version,
+                "fromSHA": fromSHA,
+                "untilSHA": untilSHA,
+                "requiresSameVersion": requiresSameVersion,
+                "path": path,
+                "buildId": buildId,
+                "branch": branch,
+            })
+        else:
+            releaseJsonObject["modules"][groupId] = [{
+                "groupId": groupId,
+                "artifactId": artifactId,
+                "version": version,
+                "fromSHA": fromSHA,
+                "untilSHA": untilSHA,
+                "requiresSameVersion": requiresSameVersion,
+                "path": path,
+                "buildId": buildId,
+                "branch": branch,
+            }]
+    return releaseJsonObject
 
 def getJetpadRelease(date, includeAllCommits):
-	print("Getting the release info from Jetpad...")
-	jetpadReleaseInfo = getJetpadReleaseInfo(date)
-	if not jetpadReleaseInfo:
-		exit(1)
-	print("Successful")
-	return getReleaseInfoObject(date, includeAllCommits, jetpadReleaseInfo)
+    print("Getting the release info from Jetpad...")
+    jetpadReleaseInfo = getJetpadReleaseInfo(date)
+    if not jetpadReleaseInfo:
+        exit(1)
+    print("Successful")
+    return getReleaseInfoObject(date, includeAllCommits, jetpadReleaseInfo)
 
