4707733b5507ed9f11be84ff2f09a7bd5c2eeee3
Brad Corso <bcorso@google.com>
2021-07-30 22:36:34 +0000

    [GH] Add Messager#printMessage() overloads to XMessager
    ## Proposed Changes
    
    Adds [Messager#printMessage()](https://docs.oracle.com/javase/8/docs/api/javax/annotation/processing/Messager.html) overloads to XMessager.
    
    For KSP, there's not a direct equivalent to these overloads since the KspLogger can accept the element/annotation/annotationValue directly without requiring all preceding information. Thus, for KspMessager, we only look at the most specific, non-null parameter given. For example, if element and annotation are given we report on the annotation.
    
    ## Testing
    
    Test: ./gradlew :room:r-c-p:test
    
    ## Issues Fixed
    
    Fixes: [https://issuetracker.google.com/193819242](https://issuetracker.google.com/193819242)
    
    This is an imported pull request from https://github.com/androidx/androidx/pull/209.
    
    Resolves #209
    Github-Pr-Head-Sha: 8c73d7b5c7ab7b211bfc4796edde640178c7e330
    GitOrigin-RevId: f515c4ff8f422e1576423d5b668b96a7629e5417
    Change-Id: Ife376c53563555b7b63db2b4eb3c030e497a8ff5

--- room/room-compiler-processing/src/main/java/androidx/room/compiler/processing/XMessager.kt
+++ room/room-compiler-processing/src/main/java/androidx/room/compiler/processing/XMessager.kt
@@ -23,24 +23,91 @@ import javax.tools.Diagnostic
  */
 abstract class XMessager {
     private val watchers = mutableListOf<XMessager>()
+
     /**
-     * Prints the given [msg] to the logs while also associating it with the given [element].
+     * Prints the given [msg] to the logs while also associating it with the given [element],
+     * [annotation], and [annotationValue].
+     *
+     * @param kind Kind of the message
+     * @param msg The actual message to report to the compiler
+     */
+    fun printMessage(kind: Diagnostic.Kind, msg: String) {
+        printMsg(kind, msg)
+    }
+
+    /**
+     * Prints the given [msg] to the logs while also associating it with the given [element],
+     * [annotation], and [annotationValue].
      *
      * @param kind Kind of the message
      * @param msg The actual message to report to the compiler
      * @param element The element with whom the message should be associated with
      */
-    fun printMessage(kind: Diagnostic.Kind, msg: String, element: XElement? = null) {
+    final fun printMessage(
+        kind: Diagnostic.Kind,
+        msg: String,
+        element: XElement,
+    ) {
+        printMsg(kind, msg, element)
+    }
+
+    /**
+     * Prints the given [msg] to the logs while also associating it with the given [element],
+     * [annotation], and [annotationValue].
+     *
+     * @param kind Kind of the message
+     * @param msg The actual message to report to the compiler
+     * @param element The element with whom the message should be associated with
+     * @param annotation The annotation with whom the msg should be associated with
+     */
+    final fun printMessage(
+        kind: Diagnostic.Kind,
+        msg: String,
+        element: XElement,
+        annotation: XAnnotation,
+    ) {
+        printMsg(kind, msg, element, annotation)
+    }
+
+    /**
+     * Prints the given [msg] to the logs while also associating it with the given [element],
+     * [annotation], and [annotationValue].
+     *
+     * @param kind Kind of the message
+     * @param msg The actual message to report to the compiler
+     * @param element The element with whom the message should be associated with
+     * @param annotation The annotation with whom the msg should be associated with
+     * @param annotationValue The annotation value with whom the msg should be associated with
+     */
+    final fun printMessage(
+        kind: Diagnostic.Kind,
+        msg: String,
+        element: XElement,
+        annotation: XAnnotation,
+        annotationValue: XAnnotationValue
+    ) {
+        printMsg(kind, msg, element, annotation, annotationValue)
+    }
+
+    private fun printMsg(
+        kind: Diagnostic.Kind,
+        msg: String,
+        element: XElement? = null,
+        annotation: XAnnotation? = null,
+        annotationValue: XAnnotationValue? = null
+    ) {
         watchers.forEach {
-            it.printMessage(kind, msg, element)
+            it.printMsg(kind, msg, element, annotation, annotationValue)
         }
-        onPrintMessage(kind, msg, element)
+        onPrintMessage(kind, msg, element, annotation, annotationValue)
     }
 
     protected abstract fun onPrintMessage(
         kind: Diagnostic.Kind,
         msg: String,
-        element: XElement? = null
+        element: XElement? = null,
+        annotation: XAnnotation? = null,
+        annotationValue: XAnnotationValue? = null
     )
 
     fun addMessageWatcher(watcher: XMessager) {
