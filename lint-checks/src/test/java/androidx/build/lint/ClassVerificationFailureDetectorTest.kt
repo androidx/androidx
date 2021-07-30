1e4ad0cfa0c491dbaadb024839984dbc700bf7b6
Tiem Song <tiem@google.com>
2021-07-30 15:49:58 -0700

    Add more lint-check tests for Kotlin sources (part 2)
    The Kotlin sample source files were created using Android Studio's built-in Java-to-Kotlin conversion tool and minimally edited to address compilation issues.
    
    Also add expected lint failures to lint-baseline.xml due to adding source files.
    
    Bug: 188048904
    Test: :lint-checks:test :lint-checks:lint
    Change-Id: Icac620ee3ecd0c01c9aa1eace49702e8c25ae391

--- lint-checks/src/test/java/androidx/build/lint/ClassVerificationFailureDetectorTest.kt
+++ lint-checks/src/test/java/androidx/build/lint/ClassVerificationFailureDetectorTest.kt
@@ -35,7 +35,9 @@ class ClassVerificationFailureDetectorTest : AbstractLintDetectorTest(
     ),
 ) {
 
-    @Ignore // Started failing with AGP 7.1.0-alpha03 upgrade
+    // Started failing with AGP 7.1.0-alpha03 upgrade. When fixing this, also create a Kotlin
+    // version of this test (see b/188048904 for details).
+    @Ignore
     @Test
     fun `Detection of unsafe references in Java sources`() {
         val input = arrayOf(
@@ -125,6 +127,27 @@ Fix for src/androidx/sample/core/widget/ListViewCompat.java line 69: Extract to 
         /* ktlint-enable max-line-length */
 
         check(*input).expect(expected).expectFixDiffs(expectedFix)
+    }
+
+    @Test
+    fun `Detection and auto-fix of unsafe references in real-world Kotlin sources`() {
+        val input = arrayOf(
+            ktSample("androidx.sample.core.widget.ListViewCompatKotlin"),
+        )
+
+        /* ktlint-disable max-line-length */
+        val expected = """
+src/androidx/sample/core/widget/ListViewCompatKotlin.kt:33: Error: This call references a method added in API level 19; however, the containing class androidx.sample.core.widget.ListViewCompatKotlin is reachable from earlier API levels and will fail run-time class verification. [ClassVerificationFailure]
+            listView.scrollListBy(y)
+                     ~~~~~~~~~~~~
+src/androidx/sample/core/widget/ListViewCompatKotlin.kt:58: Error: This call references a method added in API level 19; however, the containing class androidx.sample.core.widget.ListViewCompatKotlin is reachable from earlier API levels and will fail run-time class verification. [ClassVerificationFailure]
+            listView.canScrollList(direction)
+                     ~~~~~~~~~~~~~
+2 errors, 0 warnings
+        """.trimIndent()
+        /* ktlint-enable max-line-length */
+
+        check(*input).expect(expected)
     }
 
     @Test
