d7830ba5bd80a568f4729ae60cf7800fbc19de81
Aurimas Liutikas <aurimas@google.com>
2021-08-10 15:31:46 +0000

    Reenabling running errorprone in androidx-studio-integration
    The issue was fixed with r.android.com/1785267
    
    Change-Id: If6d6b761b96ea49c236c2af0d35120d4128ffd89
    Test: None

--- busytown/androidx-studio-integration.sh
+++ busytown/androidx-studio-integration.sh
@@ -1,5 +1,4 @@
 set -e
 SCRIPT_PATH="$(cd $(dirname $0) && pwd)"
 
-# runErrorProne is disabled due to I77d9800990e2a46648f7ed2713c54398cd798a0d in AGP
-$SCRIPT_PATH/impl/build-studio-and-androidx.sh -Pandroidx.summarizeStderr --no-daemon -Pandroidx.allWarningsAsErrors listTaskOutputs bOS -Pandroidx.verifyUpToDate -x verifyDependencyVersions -x runErrorProne --stacktrace
+$SCRIPT_PATH/impl/build-studio-and-androidx.sh -Pandroidx.summarizeStderr --no-daemon -Pandroidx.allWarningsAsErrors listTaskOutputs bOS -Pandroidx.verifyUpToDate -x verifyDependencyVersions --stacktrace
