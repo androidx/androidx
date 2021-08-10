87fcb4c4e40ae2c1827589b35d6b0753f0129d93
Sanura N'Jaka <sanura@google.com>
2021-08-10 22:59:56 +0000

    Implement MenuHostHelper class
    Implementing the MenuHostHelper class that
    can (and should) be used for implementing
    the MenuHost interface.
    
    RelNote: "Core now included a `MenuHostHelper`
    class that can be used when having a component
    implement `MenuHost`."
    Test: N/A
    
    Change-Id: I74f4ae6ab5ef63c51b4f982b6a334bba5bb0dfc2

---
+++ core/core/src/androidTest/java/androidx/core/view/MenuHostHelperTest.kt
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.view
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.R
+import androidx.core.app.TestActivityWithLifecycle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.testing.TestLifecycleOwner
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.testutils.withActivity
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class MenuHostHelperTest {
+
+    private val context = InstrumentationRegistry.getInstrumentation().context
+    private val menuProvider = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.example_menu, menu)
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            return true
+        }
+    }
+
+    @Test
+    fun addMenuProvider() {
+        with(ActivityScenario.launch(TestActivityWithLifecycle::class.java)) {
+            val toolbar = Toolbar(context)
+            val menuHost = TestMenuHost(toolbar.menu, withActivity { menuInflater })
+
+            menuHost.addMenuProvider(menuProvider)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNotNull()
+        }
+    }
+
+    @Test
+    fun addMenuProviderWithLifecycle() {
+        with(ActivityScenario.launch(TestActivityWithLifecycle::class.java)) {
+            val toolbar = Toolbar(context)
+            val menuHost = TestMenuHost(toolbar.menu, withActivity { menuInflater })
+            val lifecycleOwner = TestLifecycleOwner(Lifecycle.State.STARTED)
+
+            menuHost.addMenuProvider(menuProvider, lifecycleOwner)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNotNull()
+
+            lifecycleOwner.currentState = Lifecycle.State.DESTROYED
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+        }
+    }
+
+    @Test
+    fun addMenuProviderWithLifecycleAndState() {
+        with(ActivityScenario.launch(TestActivityWithLifecycle::class.java)) {
+            val toolbar = Toolbar(context)
+            val menuHost = TestMenuHost(toolbar.menu, withActivity { menuInflater })
+            val lifecycleOwner = TestLifecycleOwner(Lifecycle.State.CREATED)
+
+            menuHost.addMenuProvider(menuProvider, lifecycleOwner, Lifecycle.State.RESUMED)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+
+            lifecycleOwner.currentState = Lifecycle.State.RESUMED
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNotNull()
+
+            lifecycleOwner.currentState = Lifecycle.State.CREATED
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+        }
+    }
+
+    @Test
+    fun removeMenuProvider() {
+        with(ActivityScenario.launch(TestActivityWithLifecycle::class.java)) {
+            val toolbar = Toolbar(context)
+            val menuHost = TestMenuHost(toolbar.menu, withActivity { menuInflater })
+
+            menuHost.addMenuProvider(menuProvider)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNotNull()
+
+            menuHost.removeMenuProvider(menuProvider)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+        }
+    }
+
+    @Test
+    fun multipleMenuProviders() {
+        with(ActivityScenario.launch(TestActivityWithLifecycle::class.java)) {
+            val toolbar = Toolbar(context)
+            val menuHost = TestMenuHost(toolbar.menu, withActivity { menuInflater })
+
+            menuHost.addMenuProvider(menuProvider)
+
+            val menuProvider2 = object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.example_menu2, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return true
+                }
+            }
+            menuHost.addMenuProvider(menuProvider2)
+
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item3)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item4)).isNotNull()
+
+            menuHost.removeMenuProvider(menuProvider)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item3)).isNotNull()
+            assertThat(toolbar.menu.findItem(R.id.item4)).isNotNull()
+
+            menuHost.removeMenuProvider(menuProvider2)
+            assertThat(toolbar.menu.findItem(R.id.item1)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item2)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item3)).isNull()
+            assertThat(toolbar.menu.findItem(R.id.item4)).isNull()
+        }
+    }
+}
---
+++ core/core/src/main/java/androidx/core/view/MenuHostHelper.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.view;
+
+import android.annotation.SuppressLint;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Helper class for implementing {@link MenuHost}.
+ */
+public class MenuHostHelper {
+
+    private final Runnable mOnInvalidateMenuCallback;
+    private final CopyOnWriteArrayList<MenuProvider> mMenuProviders = new CopyOnWriteArrayList<>();
+
+    /**
+     * Construct a new MenuHostHelper.
+     *
+     * @param onInvalidateMenuCallback callback to invalidate the menu
+     *                                 whenever there may be a change to it
+     */
+    public MenuHostHelper(@NonNull Runnable onInvalidateMenuCallback) {
+        mOnInvalidateMenuCallback = onInvalidateMenuCallback;
+    }
+
+    /**
+     * Inflates the entire {@link Menu}, which will include all
+     * {@link MenuItem}s provided by all current {@link MenuProvider}s.
+     *
+     * @param menu         the menu to inflate all the menu items into
+     * @param menuInflater the inflater to be used to inflate the menu
+     */
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        for (MenuProvider menuProvider : mMenuProviders) {
+            menuProvider.onCreateMenu(menu, menuInflater);
+        }
+    }
+
+    /**
+     * Called whenever one of the menu items from any of the current
+     * {@link MenuProvider}s is selected.
+     *
+     * @param item the menu item that was selected
+     * @return {@code true} to indicate the menu processing was consumed
+     * by one of the {@link MenuProvider}s, {@code false} otherwise.
+     */
+    public boolean onMenuItemSelected(@NonNull MenuItem item) {
+        for (MenuProvider menuProvider : mMenuProviders) {
+            if (menuProvider.onMenuItemSelected(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Adds the given {@link MenuProvider} to the helper.
+     *
+     * @param provider the MenuProvider to be added
+     */
+    public void addMenuProvider(@NonNull MenuProvider provider) {
+        mMenuProviders.add(provider);
+        mOnInvalidateMenuCallback.run();
+    }
+
+    /**
+     * Adds the given {@link MenuProvider} to the helper.
+     *
+     * This {@link MenuProvider} will be removed once the given {@link LifecycleOwner}
+     * receives an {@link Lifecycle.Event.ON_DESTROY} event.
+     *
+     * @param provider the MenuProvider to be added
+     * @param owner    the Lifecycle owner whose state will determine the removal of the provider
+     */
+    public void addMenuProvider(@NonNull MenuProvider provider, @NonNull LifecycleOwner owner) {
+        addMenuProvider(provider);
+        Lifecycle lifecycle = owner.getLifecycle();
+        lifecycle.addObserver((LifecycleEventObserver) (source, event) -> {
+            if (event == Lifecycle.Event.ON_DESTROY) {
+                removeMenuProvider(provider);
+            }
+        });
+    }
+
+    /**
+     * Adds the given {@link MenuProvider} to the helper once the given
+     * {@link LifecycleOwner} reaches the given {@link Lifecycle.State}.
+     *
+     * This {@link MenuProvider} will be removed once the given {@link LifecycleOwner}
+     * goes down from the given {@link Lifecycle.State} or receives an
+     * {@link Lifecycle.Event.ON_DESTROY} event.
+     *
+     * @param provider the MenuProvider to be added
+     * @param state    the Lifecycle.State to check for automated addition/removal
+     * @param owner    the Lifecycle owner whose state will determine the removal of the provider
+     */
+    @SuppressLint("LambdaLast")
+    public void addMenuProvider(@NonNull MenuProvider provider, @NonNull LifecycleOwner owner,
+            @NonNull Lifecycle.State state) {
+        Lifecycle lifecycle = owner.getLifecycle();
+        lifecycle.addObserver((LifecycleEventObserver) (source, event) -> {
+            if (event == Lifecycle.Event.upTo(state)) {
+                addMenuProvider(provider);
+            } else if (event == Lifecycle.Event.ON_DESTROY) {
+                removeMenuProvider(provider);
+            } else if (event == Lifecycle.Event.downFrom(state)) {
+                removeMenuProvider(provider);
+            }
+        });
+    }
+
+    /**
+     * Removes the given {@link MenuProvider} from the helper.
+     *
+     * @param provider the MenuProvider to be removed
+     */
+    public void removeMenuProvider(@NonNull MenuProvider provider) {
+        mMenuProviders.remove(provider);
+        mOnInvalidateMenuCallback.run();
+    }
+}
