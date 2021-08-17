9726f78a97a4c974ad4e67e8051e2cf80e776884
Jamie Garside <jgarside@google.com>
2021-08-17 15:52:22 +0100

    Add Row and Column to androidx.glance.
    Test: Added (./gradlew :glance:glance:test)
    Relnote: n/a
    Change-Id: Ibff3a86a75deb071a4dba7570a555b0c84d5b37c

---
+++ glance/glance/src/test/kotlin/androidx/glance/layout/RowTest.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.glance.layout
+
+import androidx.glance.GlanceInternalApi
+import androidx.glance.Modifier
+import androidx.glance.unit.dp
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(GlanceInternalApi::class, ExperimentalCoroutinesApi::class)
+class RowTest {
+    private lateinit var fakeCoroutineScope: TestCoroutineScope
+
+    @Before
+    fun setUp() {
+        fakeCoroutineScope = TestCoroutineScope()
+    }
+
+    @Test
+    fun createComposableRow() = fakeCoroutineScope.runBlockingTest {
+        val root = runTestingComposition {
+            Row {}
+        }
+
+        assertThat(root.children).hasSize(1)
+        assertThat(root.children[0]).isInstanceOf(EmittableRow::class.java)
+        assertThat((root.children[0] as EmittableRow).children).hasSize(0)
+    }
+
+    @Test
+    fun createComposableRowWithParams() = fakeCoroutineScope.runBlockingTest {
+        val root = runTestingComposition {
+            Row(
+                modifier = Modifier.padding(2.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalAlignment = Alignment.End
+            ) {}
+        }
+
+        val innerRow = root.children[0] as EmittableRow
+        val paddingModifier = requireNotNull(innerRow.modifier.findModifier<PaddingModifier>())
+        assertThat(paddingModifier.top).isEqualTo(2.dp)
+        assertThat(innerRow.verticalAlignment).isEqualTo(Alignment.Bottom)
+        assertThat(innerRow.horizontalAlignment).isEqualTo(Alignment.End)
+    }
+
+    @Test
+    fun createComposableRowWithChildren() = fakeCoroutineScope.runBlockingTest {
+        val root = runTestingComposition {
+            Row {
+                Box(contentAlignment = Alignment.BottomCenter) {}
+                Box(contentAlignment = Alignment.TopCenter) {}
+            }
+        }
+
+        val innerRow = root.children[0] as EmittableRow
+        val leafBox0 = innerRow.children[0] as EmittableBox
+        val leafBox1 = innerRow.children[1] as EmittableBox
+
+        assertThat(leafBox0.contentAlignment).isEqualTo(Alignment.BottomCenter)
+        assertThat(leafBox1.contentAlignment).isEqualTo(Alignment.TopCenter)
+    }
+}
