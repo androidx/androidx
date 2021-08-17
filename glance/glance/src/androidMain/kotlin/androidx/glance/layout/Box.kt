ba518726f454e9d00b2e22d0fb206bcd6be51167
Jamie Garside <jgarside@google.com>
2021-08-17 11:59:46 +0100

    Add Box composable, along with its emittable.
    This also adds a testing utility to run a single Composition. This just
    uses some sensible defaults, and isn't really intended for doing more
    than just evaluating a Composition once to see what it generated.
    
    Relnote: n/a
    Test: Tests included (./gradlew	:glance:glance:test).
    Change-Id: I0f4be15799b10081862b0ce337d45670051c37bc

---
+++ glance/glance/src/androidMain/kotlin/androidx/glance/layout/Box.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.glance.layout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
+import androidx.glance.Applier
+import androidx.glance.EmittableWithChildren
+import androidx.glance.GlanceInternalApi
+import androidx.glance.Modifier
+
+@GlanceInternalApi
+class EmittableBox() : EmittableWithChildren() {
+    override var modifier: Modifier = Modifier
+    var contentAlignment: Alignment = Alignment.TopStart
+
+    override fun toString(): String {
+        return "EmittableBox(modifier=$modifier, contentAlignment=$contentAlignment)"
+    }
+}
+
+/**
+ * A layout composable with [content].
+ *
+ * By default, the [Box] will size itself to fit the content, unless a [Dimension] constraint has
+ * been provided. When the children are smaller than the [Box], they will be placed within the box
+ * subject to the [contentAlignment]. When the [content] has more than one layout child, all of
+ * the children will be stacked on top of each other in the composition order.
+ *
+ * @param modifier The modifier to be applied to the layout.
+ * @param contentAlignment The alignment of children within the [Box].
+ * @param content The content inside the [Box].
+ */
+@OptIn(GlanceInternalApi::class)
+@Composable
+fun Box(
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopStart,
+    content: @Composable() () -> Unit
+) {
+    ComposeNode<EmittableBox, Applier>(
+        factory = ::EmittableBox,
+        update = {
+            this.set(modifier) { this.modifier = it }
+            this.set(contentAlignment) { this.contentAlignment = it }
+        },
+        content = content
+    )
+}
