c868d247d41032097e326a0b59c3d49b18bf8270
Ian Lake <ilake@google.com>
2021-08-27 11:01:47 -0700

    Convert ActivityResultContract to Kotlin
    Rewrite ActivityResultContract in Kotlin to
    ensure proper handling of nullable classes for
    the input and output classes.
    
    Test: ./gradlew checkApi
    BUG: 183440304
    Relnote: "The `ActivityResultContract` class has
    been rewritten in Kotlin to ensure that developers
    writing custom contracts in Kotlin can define the
    correct nullability for their input and output
    classes."
    
    Change-Id: I8a8f5f38d3a8d0cb457d876300efc748c299e772

--- activity/activity/src/main/java/androidx/activity/result/contract/ActivityResultContract.kt
+++ activity/activity/src/main/java/androidx/activity/result/contract/ActivityResultContract.kt
@@ -13,74 +13,44 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.activity.result.contract
 
-package androidx.activity.result.contract;
-
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-
-import androidx.activity.result.ActivityResultCaller;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.content.Context
+import android.content.Intent
 
 /**
- * A contract specifying that an activity can be called with an input of type I
- * and produce an output of type O
+ * A contract specifying that an activity can be called with an input of type [I]
+ * and produce an output of type [O].
  *
  * Makes calling an activity for result type-safe.
  *
- * @param <I> input type
- * @param <O> output type
- *
- * @see ActivityResultCaller
+ * @see androidx.activity.result.ActivityResultCaller
  */
-public abstract class ActivityResultContract<I, O> {
+abstract class ActivityResultContract<I, O> {
+    /**
+     * Create an intent that can be used for [android.app.Activity.startActivityForResult].
+     */
+    abstract fun createIntent(context: Context, input: I): Intent
 
-    /** Create an intent that can be used for {@link Activity#startActivityForResult} */
-    public abstract @NonNull Intent createIntent(@NonNull Context context,
-            @SuppressLint("UnknownNullness") I input);
-
-    /** Convert result obtained from {@link Activity#onActivityResult} to O */
-    @SuppressLint("UnknownNullness")
-    public abstract O parseResult(int resultCode, @Nullable Intent intent);
+    /**
+     * Convert result obtained from [android.app.Activity.onActivityResult] to [O].
+     */
+    abstract fun parseResult(resultCode: Int, intent: Intent?): O
 
     /**
      * An optional method you can implement that can be used to potentially provide a result in
      * lieu of starting an activity.
      *
-     * @return the result wrapped in a {@link SynchronousResult} or {@code null} if the call
+     * @return the result wrapped in a [SynchronousResult] or `null` if the call
      * should proceed to start an activity.
      */
-    public @Nullable SynchronousResult<O> getSynchronousResult(
-            @NonNull Context context,
-            @SuppressLint("UnknownNullness") I input) {
-        return null;
+    open fun getSynchronousResult(context: Context, input: I): SynchronousResult<O>? {
+        return null
     }
 
     /**
-     * The wrapper for a result provided in {@link #getSynchronousResult}
-     *
-     * @param <T> type of the result
+     * The wrapper for a result provided in [getSynchronousResult]. This allows differentiating
+     * between a null [T] synchronous result and no synchronous result at all.
      */
-    public static final class SynchronousResult<T> {
-        private final @SuppressLint("UnknownNullness") T mValue;
-
-        /**
-         * Create a new result wrapper
-         *
-         * @param value the result value
-         */
-        public SynchronousResult(@SuppressLint("UnknownNullness") T value) {
-            this.mValue = value;
-        }
-
-        /**
-         * @return the result value
-         */
-        public @SuppressLint("UnknownNullness") T getValue() {
-            return mValue;
-        }
-    }
+    class SynchronousResult<T>(val value: T)
 }
