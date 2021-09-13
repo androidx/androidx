e32d7e45d2b7d50888276ac58767b362bc46c538
Sean McQuillan <seanmcq@google.com>
2021-09-13 13:31:39 -0700

    Fix profile formats for 0.0.9 and 0.0.1
    V0.0.1:
    - Write data in header/body/header/body/header/body format instead of
    incorrect header/header/header/body/body/body when writing multidex
    profiles
    - Use ':' for dex key separator
    
    v0.0.5:
    - Write data in header/body/header/body in multidex
    - Use ':' for dex key separator
    
    v0.0.9:
    - New format, shipped on O_MR1
    - Write data in header/body/header/body in multidex
    - Use '!' for dex key separator
    - Format is the same as 0.1.0 otherwise
    
    In addition, added a requirement that baseline.apk from profgen
    always generateds dexKey with the dex file name and never adds the apk
    name.
    
    Test: ./gradlew :profileinstaller:profileinstaller:test
    Relnote: "Fix profileinstaller transcoding issues on N, O, and O_MR1."
    
    Change-Id: I12d759c4dc1ee570b7321671e3c9502924ed549d

--- profileinstaller/profileinstaller/src/main/java/androidx/profileinstaller/ProfileVersion.java
+++ profileinstaller/profileinstaller/src/main/java/androidx/profileinstaller/ProfileVersion.java
@@ -18,10 +18,23 @@ package androidx.profileinstaller;
 
 import android.os.Build;
 
+import java.util.Arrays;
+
 class ProfileVersion {
     private ProfileVersion() {}
     static final byte[] V010_P = new byte[]{'0', '1', '0', '\0'};
+    static final byte[] V009_O_MR1 = new byte[]{'0', '0', '9', '\0'};
     static final byte[] V005_O = new byte[]{'0', '0', '5', '\0'};
     static final byte[] V001_N = new byte[]{'0', '0', '1', '\0'};
     static final int MIN_SUPPORTED_SDK = Build.VERSION_CODES.N;
+
+    static String dexKeySeparator(byte[] version) {
+        if (Arrays.equals(version, V001_N)) {
+            return ":";
+        } else if (Arrays.equals(version, V005_O)) {
+            return ":";
+        } else {
+            return "!";
+        }
+    }
 }
