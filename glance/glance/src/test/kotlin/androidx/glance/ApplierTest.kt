dba1e19eabde3e94b34918170831b6683e0b6739
Cyrus Boadway <boadway@google.com>
2021-09-29 16:50:26 +0100

    Reset max depth for lazy list children
    Allow EmittableWithChildren to declare that their children do not
    count towards the layout depth, and instead start a new depth counter.
    For example, a RemoteViews list adapter children each have an
    independent depth restriction from the list's layout.
    
    Test: unit test
    Bug: b/200909365
    Change-Id: I8ded2b42e9ea9166b4eb53e4765266ba7293e639

--- glance/glance/src/test/kotlin/androidx/glance/ApplierTest.kt
+++ glance/glance/src/test/kotlin/androidx/glance/ApplierTest.kt
@@ -106,6 +106,28 @@ class ApplierTest {
         )
     }
 
+    @Test
+    fun resetDepth() {
+        val root = RootEmittable(maxDepth = 1)
+        val applier = Applier(root)
+
+        applier.insertTopDown(0, LeafEmittable())
+        val r = ResetsDepthEmittable()
+        applier.insertTopDown(1, r)
+        applier.down(r)
+        val m = MiddleEmittable()
+        applier.insertTopDown(0, m)
+        applier.down(m)
+
+        val ex = assertFailsWith<IllegalArgumentException> {
+            applier.insertTopDown(0, LeafEmittable())
+        }
+
+        assertThat(ex.message).isEqualTo(
+            "Too many embedded views for the current surface. The maximum depth is: 1"
+        )
+    }
+
     private companion object {
         fun updateApplier(applier: Applier) {
             val middle = MiddleEmittable()
@@ -127,5 +149,9 @@ private class MiddleEmittable : EmittableWithChildren() {
 }
 
 private class LeafEmittable : Emittable {
+    override var modifier: Modifier = Modifier
+}
+
+private class ResetsDepthEmittable : EmittableWithChildren(resetsDepthForChildren = true) {
     override var modifier: Modifier = Modifier
 }
