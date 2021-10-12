ec2a0b817020d504fbe6982a05c37db7864e801b
Tiem Song <tiem@google.com>
2021-10-12 15:22:39 -0700

    Exclude lintDebug task from androidx-studio-integration.sh
    Bug: 201364708
    Test: run androidx-studio-integration.sh and verify that there are no
    lint Results
    
    Change-Id: I09ca3b19a5a0ff5474f654a0157d2721613cd6da

--- busytown/androidx-studio-integration.sh
+++ busytown/androidx-studio-integration.sh
@@ -1,4 +1,6 @@
 set -e
 SCRIPT_PATH="$(cd $(dirname $0) && pwd)"
 
-$SCRIPT_PATH/impl/build-studio-and-androidx.sh -Pandroidx.allWarningsAsErrors listTaskOutputs bOS -Pandroidx.verifyUpToDate -x verifyDependencyVersions --stacktrace
+# Exclude lintDebug task, as lint tests are covered by the
+# androidx-studio-integration-lint.sh script
+$SCRIPT_PATH/impl/build-studio-and-androidx.sh -Pandroidx.allWarningsAsErrors listTaskOutputs bOS -Pandroidx.verifyUpToDate -x verifyDependencyVersions -x lintDebug --stacktrace
