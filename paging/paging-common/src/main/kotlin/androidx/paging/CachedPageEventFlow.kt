8eb2a9b2b0c58afeefa4aa2fbbf0b8675cef679c
Yigit Boyar <yboyar@google.com>
2021-10-26 14:42:36 -0700

    Fix race condition in shared paging data
    This CL fixes a race condition in CachedPageEventFlow and also a memory
    leak as a side effect :).
    
    Previously, if source dispatches 2 or more events between the time new
    subscriber connects to the history and connects to the shared flow, it
    could miss events.
    
    An earlier version of this CL does have a repro test but it is removed
    now as this CL completely removes the two flow collection setup that we
    had before (hence the case cannot be reproduced).
    
    Instead, CachedPageEventFlow now uses the onSubscribed extension
    function on SharedFlow to be able atomically inject a new stream.
    onSubscribed is guaranteed to run between the time subscription is
    established and before any other event is dispatched, hence we don't
    have the race anymore.
    
    As part of this change, CachedPageEventFlow will no longer use shareIn,
    which also caused b/203594733. That leak happened because shared flows
    never complete so it kept the CachedPageEventFlow in memory even when we
    didn't use it anymore. Instead, this new implementation simply launches
    a collection job in the given scope and sends the latest event
    via a controlled MutableSharedFlow. The injection of history happens
    on the onSubscribe of that MutableSharedFlow, which gives us an atomic
    ability to inject the history before sending any more events.
    
    This MutableSharedFlow has replay=1 and buffer=UNLIMITED. Buffer is only
    ever used when there is a slow collector, which was still necessary in
    the previous implementation as well.
    
    To cancel the downstream, we still emit a complete event to this Flow.
    Unlike the previous implementation, this is not a `active.flatMap`
    operation anymore, instead we use `tryEmit` and rely on the fact that it
    will never fail in practice (due to UNLIMITED buffer).
    
    Bug: 204228426
    Bug: 203594733
    Test: CachedPageEventFlowTest (the memory leak test is in a followup)
    
    Change-Id: Ib682ea334e061bc5fa850663cc1ebea0c7a7ebbf

--- paging/paging-common/src/main/kotlin/androidx/paging/CachedPageEventFlow.kt
+++ paging/paging-common/src/main/kotlin/androidx/paging/CachedPageEventFlow.kt
@@ -18,21 +18,16 @@ package androidx.paging
 
 import androidx.annotation.VisibleForTesting
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onSubscription
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.flow.withIndex
-import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -48,173 +43,102 @@ internal class CachedPageEventFlow<T : Any>(
     private val pageController = FlattenedPageController<T>()
 
     /**
-     * Flow<PageEvent> can be collected only once so this goes from active to passive only.
+     * Shared flow for downstreams where we dispatch each event coming from upstream.
+     * This only has reply = 1 so it does not keep the previous events. Meanwhile, it still buffers
+     * them for active subscribers.
+     * A final `null` value is emitted as the end of stream message once the job is complete.
      */
-    private val active = MutableStateFlow(true)
-
-    /**
-     * Shared flow for upstream.
-     *
-     * Note that shared flows cannot be completed, hence we use a custom wrapper type
-     * [FlattenedPageEventStorage.UpstreamMessage] to specify end of stream.
-     */
-    private val sharedSrc = active.simpleFlatMapLatest { active ->
-        if (active) {
-            src.withIndex().onEach(pageController::record)
-                .map {
-                    FlattenedPageEventStorage.UpstreamMessage.Value(it)
-                }
-                .onCompletion {
-                    // close it so that we can dispatch the completed message.
-                    this@CachedPageEventFlow.close()
-                }
-        } else {
-            // send a complete message to notify end of stream
-            flowOf(FlattenedPageEventStorage.UpstreamMessage.Completed)
-        }
-    }.shareIn(
-        scope = scope,
-        started = SharingStarted.Lazily, // never stop, we'll be closed manually
-        // Don't replay for events, we have our own condensed replay mechanism that merges events.
-        // We only replay once to account for the Completed terminal event. If we don't, shared flow
-        // would never emit any value hence the upstream would look as if it is open even though
-        // it is not.
-        // note that not replaying does not mean values are not buffered. Buffering still
-        // happens as long as the subscriber has arrived in time before the value is emitted.
-        // [downstreamFlow] takes care of ordering them behind a mutex.
-        // https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/share-in.html
-        replay = 1
+    private val mutableSharedSrc = MutableSharedFlow<IndexedValue<PageEvent<T>>?>(
+        replay = 1,
+        extraBufferCapacity = Channel.UNLIMITED,
+        onBufferOverflow = BufferOverflow.SUSPEND
     )
 
-    fun close() {
-        active.compareAndSet(expect = true, update = false)
+    /**
+     * Shared flow used for downstream which also sends the history. Each downstream connects to
+     * this where it first receives a history event and then any other event that was emitted by
+     * the upstream.
+     */
+    private val sharedForDownstream = mutableSharedSrc.onSubscription {
+        val history = pageController.getStateAsEvents()
+        // start the job if it has not started yet. We do this after capturing the history so that
+        // the first subscriber does not receive any history.
+        job.start()
+        history.forEach {
+            emit(it)
+        }
     }
 
-    val downstreamFlow = simpleChannelFlow<PageEvent<T>> {
-        // get a new snapshot. this will immediately hook us to the upstream channel
-        val snapshot = pageController.createTemporaryDownstream()
-        var lastReceivedHistoryIndex = Int.MIN_VALUE
-        // first, dispatch everything in the snapshot
-        // these are events that sync us to the current state + other events that might be
-        // emitted by the upstream between the time of snapshot and us connecting to the upstream
-        val historyCollection = launch {
-            snapshot.consumeHistory().collect {
-                lastReceivedHistoryIndex = it.index
-                send(it.value)
+    /**
+     * The actual job that collects the upstream.
+     */
+    private val job = scope.launch(start = CoroutineStart.LAZY) {
+        src.withIndex()
+            .collect {
+                mutableSharedSrc.emit(it)
+                pageController.record(it)
             }
+    }.also {
+        it.invokeOnCompletion {
+            // Emit a final `null` message to the mutable shared flow.
+            // Even though, this tryEmit might technically fail, it shouldn't because we have
+            // unlimited buffer in the shared flow.
+            mutableSharedSrc.tryEmit(null)
         }
-        val activeStreamCollection = launch {
-            sharedSrc.takeWhile {
-                it !== FlattenedPageEventStorage.UpstreamMessage.Completed
-            }.onCompletion {
-                // if main upstream finishes, make sure to close the history stream
-                // otherwise it might stay open forever
-                snapshot.close()
-            }.collect {
-                // we got a value from real stream, close history
-                snapshot.close()
-                // wait for it to be done so that all events there are dispatched
-                historyCollection.join()
-                // now if it is new enough, emit it, otherwise, skip it
-                if (it is FlattenedPageEventStorage.UpstreamMessage.Value<*>) {
-                    if (it.event.index > lastReceivedHistoryIndex) {
-                        @Suppress("UNCHECKED_CAST")
-                        send(it.event.value as PageEvent<T>)
-                    }
+    }
+
+    fun close() {
+        job.cancel()
+    }
+
+    val downstreamFlow = flow {
+        // track max event index we've seen to avoid race condition between history and the shared
+        // stream
+        var maxEventIndex = Integer.MIN_VALUE
+        sharedForDownstream
+            .takeWhile {
+                // shared flow cannot finish hence we have a special marker to finish it
+                it != null
+            }
+            .collect { indexedValue ->
+                // we take until null so this cannot be null
+                if (indexedValue!!.index > maxEventIndex) {
+                    emit(indexedValue.value)
+                    maxEventIndex = indexedValue.index
                 }
             }
-        }
-        joinAll(activeStreamCollection, historyCollection)
-    }
-}
-
-/**
- * This intermediate class is used to ensure new downstream does not miss any events.
- *
- * There is a race condition where, while it is collecting the initial state, there might be more
- * events dispatched by the real source which will be missed because new downstream is not
- * subscribed to the real upstream yet.
- *
- * FlattenedPageController tracks these temporary channels and sends each event into them as well
- * so that they are not lost until the new downstream connects to the real upstream.
- * After that connection is established, this channel is closed.
- *
- * Each value is indexed to avoid sending the same event multiple times.
- */
-private class TemporaryDownstream<T : Any> {
-    /**
-     * List of events that sync us to current state + any other events that might arrive while
-     * we are doing the initial sync
-     */
-    private val historyChannel: Channel<IndexedValue<PageEvent<T>>> = Channel(Channel.UNLIMITED)
-
-    fun consumeHistory() = historyChannel.consumeAsFlow()
-
-    /**
-     * Tries to send the value to the history channel.
-     * Returns false if send fails (e.g. channel is already closed)
-     */
-    suspend fun send(event: IndexedValue<PageEvent<T>>): Boolean {
-        return try {
-            historyChannel.send(event)
-            true
-        } catch (closed: ClosedSendChannelException) {
-            false
-        }
-    }
-
-    fun close() {
-        historyChannel.close()
     }
 }
 
 private class FlattenedPageController<T : Any> {
     private val list = FlattenedPageEventStorage<T>()
-    private var snapshots = listOf<TemporaryDownstream<T>>()
     private val lock = Mutex()
     private var maxEventIndex = -1
 
     /**
      * Record the event.
-     * This sends the event into storage but also into any other active TemporaryDownstream.
      */
     suspend fun record(event: IndexedValue<PageEvent<T>>) {
         lock.withLock {
             maxEventIndex = event.index
             list.add(event.value)
-            snapshots = snapshots.filter {
-                it.send(event)
-            }
         }
     }
 
     /**
-     * Creates a temporary downstream which will have events that are necessary to sync the
-     * current state + any other events that might arrive while that sync is in progress.
+     * Create a list of events that represents the current state of the list.
      */
-    suspend fun createTemporaryDownstream(): TemporaryDownstream<T> {
+    suspend fun getStateAsEvents(): List<IndexedValue<PageEvent<T>>> {
         return lock.withLock {
-            // send the current state into the temporary downstream while we are inside the mutex
-            // this ensure that we don't process any new events until we copy all initial events
-            // into the channel of temporary downstream
-            val snap = TemporaryDownstream<T>()
             // condensed events to bring downstream up to the current state
             val catchupEvents = list.getAsEvents()
-            // make sure the indices here match the indices coming from upstream so that if the same
-            // event shows up in both places, we won't dispatch it twice.
-            // we want the last event's index to match the latest event index we've received so
-            // that downstream can ignore that event if they receive it also from the original
-            // upstream.
             val startEventIndex = maxEventIndex - catchupEvents.size + 1
-            catchupEvents.forEachIndexed { index, pageEvent ->
-                snap.send(
-                    IndexedValue(
-                        index = startEventIndex + index,
-                        value = pageEvent
-                    )
+            catchupEvents.mapIndexed { index, pageEvent ->
+                IndexedValue(
+                    index = startEventIndex + index,
+                    value = pageEvent
                 )
             }
-            snap
         }
     }
 }
@@ -317,13 +241,5 @@ internal class FlattenedPageEventStorage<T : Any> {
         }
 
         return events
-    }
-
-    /**
-     * Message type for the shared stream
-     */
-    sealed class UpstreamMessage {
-        object Completed : UpstreamMessage()
-        class Value<T : Any>(val event: IndexedValue<PageEvent<T>>) : UpstreamMessage()
     }
 }
