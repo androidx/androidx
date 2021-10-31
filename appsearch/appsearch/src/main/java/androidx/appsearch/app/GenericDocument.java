2efadbddbe9e4aaf031d83cee1f807b640d47250
Terry Wang <tytytyww@google.com>
2021-10-30 20:06:12 -0700

    Fix NPE when convert Document to proto.
    This change the GenericDocument.getProperty to return an empty array
    rather than null when the property array is empty.
    
    Bug: 204677124
    Test: GenericDocumentToProtoConverterTest
    Change-Id: Idf0383bfbf823f0ad161048eb295294e57593568

--- appsearch/appsearch/src/main/java/androidx/appsearch/app/GenericDocument.java
+++ appsearch/appsearch/src/main/java/androidx/appsearch/app/GenericDocument.java
@@ -275,6 +275,14 @@ public class GenericDocument {
      * one of the typed versions of this method instead, such as {@link #getPropertyString} or
      * {@link #getPropertyStringArray}.
      *
+     * <!--@exportToFramework:ifJetpack()--><!--@exportToFramework:else()
+     *   <p>If the property has been set is an empty {@link GenericDocument}[] or {@code byte[][]},
+     *   this will return an empty {@link GenericDocument}[] or {@code byte[][]} respectively
+     *   starting in {@link android.os.Build.VERSION_CODES#TIRAMISU Android T} and {@code null} in
+     *   earlier versions of Android. For other types it will always return an empty
+     *   array if the property has been set as an empty array.
+     * -->
+     *
      * @param path The path to look for.
      * @return The entry with the given path as an object or {@code null} if there is no such path.
      *   The returned object will be one of the following types: {@code String[]}, {@code long[]},
@@ -298,9 +306,6 @@ public class GenericDocument {
             // entry: BYTE_ARRAY_FIELD -> byte[].
             @SuppressWarnings("unchecked")
             List<Bundle> bundles = (List<Bundle>) rawValue;
-            if (bundles.size() == 0) {
-                return null;
-            }
             byte[][] bytes = new byte[bundles.size()][];
             for (int i = 0; i < bundles.size(); i++) {
                 Bundle bundle = bundles.get(i);
@@ -322,9 +327,6 @@ public class GenericDocument {
             // The underlying Bundle of nested GenericDocuments is packed into a Parcelable array.
             // We must unpack it into GenericDocument instances.
             Parcelable[] bundles = (Parcelable[]) rawValue;
-            if (bundles.length == 0) {
-                return null;
-            }
             GenericDocument[] documents = new GenericDocument[bundles.length];
             for (int i = 0; i < bundles.length; i++) {
                 if (bundles[i] == null) {
@@ -807,6 +809,13 @@ public class GenericDocument {
      *
      * <p>See {@link #getProperty} for a detailed description of the path syntax.
      *
+     * <!--@exportToFramework:ifJetpack()--><!--@exportToFramework:else()
+     *   <p> If the property has been set via  @link Builder#setPropertyBytes} is an empty {@code
+     *   byte[][]}. This will return an empty {@code byte[][]} respectively starting in
+     *   {@link android.os.Build.VERSION_CODES#TIRAMISU Android T} and {@code null} in earlier
+     *   versions of Android.
+     * -->
+     *
      * @param path The path to look for.
      * @return The {@code byte[][]} associated with the given path, or {@code null} if no value is
      * set or the value is of a different type.
@@ -823,6 +832,13 @@ public class GenericDocument {
      * Retrieves a repeated {@link GenericDocument} property by path.
      *
      * <p>See {@link #getProperty} for a detailed description of the path syntax.
+     *
+     * <!--@exportToFramework:ifJetpack()--><!--@exportToFramework:else()
+     *   <p> If the property has been set via {@link Builder#setPropertyDocument} is an empty
+     *   {@link GenericDocument}[], this will return an empty {@link GenericDocument}[] respectively
+     *   starting in {@link android.os.Build.VERSION_CODES#TIRAMISU Android T} and {@code null} in
+     *   earlier versions of Android.
+     * -->
      *
      * @param path The path to look for.
      * @return The {@link GenericDocument}[] associated with the given path, or {@code null} if no
