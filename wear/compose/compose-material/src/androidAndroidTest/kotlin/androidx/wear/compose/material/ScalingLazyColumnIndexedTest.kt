62646651b1bd75c8c664b18626ab87721612d5d6
jnichol <jnichol@google.com>
2021-11-02 12:09:14 +0000

    Add support for indexed items to the ScalingLazyColumn
    Add support for adding items with keys to ScalingLazyColumn.
    Also add convenience methods for adding lists and arrays of items
    
    Test: ./gradlew :wear:compose:compose-material:connectedCheck --info --daemon
    Bug: 201950732
    Relnote: "Adding support to ScalingLazyColumn to allow items to have a key. Also added convenience methods to allow adding of items from Arrays and Lists."
    Change-Id: Ic1f89a6c4ffd58f6e5a043e4148fe6f7fdc130f9

---
+++ wear/compose/compose-material/src/androidAndroidTest/kotlin/androidx/wear/compose/material/ScalingLazyColumnIndexedTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.compose.material
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertTopPositionInRootIsEqualTo
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.unit.dp
+import org.junit.Rule
+import org.junit.Test
+
+class ScalingLazyColumnIndexedTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun scalingLazyColumnShowsIndexedItems() {
+        val items = (1..4).map { it.toString() }
+
+        rule.setContent {
+            ScalingLazyColumn(
+                modifier = Modifier.height(200.dp),
+                scalingParams = ScalingLazyColumnDefaults.scalingParams(edgeScale = 1.0f)
+            ) {
+                itemsIndexed(items) { index, item ->
+                    Spacer(
+                        Modifier.height(101.dp).width(100.dp)
+                            .testTag("$index-$item")
+                    )
+                }
+            }
+        }
+
+        // Fully visible
+        rule.onNodeWithTag("0-1")
+            .assertIsDisplayed()
+
+        // Partially visible
+        rule.onNodeWithTag("1-2")
+            .assertIsDisplayed()
+
+        // Will have been composed but should not be visible
+        rule.onNodeWithTag("2-3")
+            .assertIsNotDisplayed()
+
+        // Should not have been composed
+        rule.onNodeWithTag("3-4")
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun columnWithIndexesComposedWithCorrectIndexAndItem() {
+        val items = (0..1).map { it.toString() }
+
+        rule.setContent {
+            ScalingLazyColumn(Modifier.height(200.dp)) {
+                itemsIndexed(items) { index, item ->
+                    BasicText(
+                        "${index}x$item", Modifier.requiredHeight(100.dp)
+                    )
+                }
+            }
+        }
+
+        rule.onNodeWithText("0x0")
+            .assertTopPositionInRootIsEqualTo(0.dp)
+
+        rule.onNodeWithText("1x1")
+            .assertTopPositionInRootIsEqualTo(104.dp)
+    }
+}
