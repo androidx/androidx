77dc081c46bef6a29d4768fb2b5dd64b0799c3d4
Tiem Song <tiem@google.com>
2021-11-03 14:24:22 -0700

    Auto-fix ClassVerificationIssues in appcompat
    Bug: 204900003
    Test: N/A
    Change-Id: I4a9ca6077cb56fcc99d3deabc1f42ab3d6747feb

--- appcompat/appcompat/src/main/java/androidx/appcompat/widget/TooltipCompat.java
+++ appcompat/appcompat/src/main/java/androidx/appcompat/widget/TooltipCompat.java
@@ -19,8 +19,10 @@ package androidx.appcompat.widget;
 import android.os.Build;
 import android.view.View;
 
+import androidx.annotation.DoNotInline;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 /**
  * Helper class used to emulate the behavior of {@link View#setTooltipText(CharSequence)} prior
@@ -39,11 +41,24 @@ public class TooltipCompat  {
      */
     public static void setTooltipText(@NonNull View view, @Nullable CharSequence tooltipText) {
         if (Build.VERSION.SDK_INT >= 26) {
-            view.setTooltipText(tooltipText);
+            Api26Impl.setTooltipText(view, tooltipText);
         } else {
             TooltipCompatHandler.setTooltipText(view, tooltipText);
         }
     }
 
-    private TooltipCompat() {}
+    private TooltipCompat() {
+    }
+
+    @RequiresApi(26)
+    static class Api26Impl {
+        private Api26Impl() {
+            // This class is not instantiable.
+        }
+
+        @DoNotInline
+        static void setTooltipText(View view, CharSequence tooltipText) {
+            view.setTooltipText(tooltipText);
+        }
+    }
 }
