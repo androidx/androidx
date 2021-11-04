7b01fce5cb2ab2afbc65b38c44347bef0ba7b3b8
Daz DeBoer <daz@gradle.com>
2021-11-04 15:47:17 -0600

    Fixes for code style
    - Prefer " to ' for strings in Groovy
    - Add copyright header to all new files

--- playground-common/playground-plugin/src/main/groovy/androidx/playground/GradleEnterpriseConventionsPlugin.groovy
+++ playground-common/playground-plugin/src/main/groovy/androidx/playground/GradleEnterpriseConventionsPlugin.groovy
@@ -1,4 +1,19 @@
-package androidx.playground
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package androidx.playground
 
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
@@ -6,8 +21,8 @@ import org.gradle.caching.http.HttpBuildCache
 
 class GradleEnterpriseConventionsPlugin implements Plugin<Settings> {
     void apply(Settings settings) {
-        settings.apply(plugin: 'com.gradle.enterprise')
-        settings.apply(plugin: 'com.gradle.common-custom-user-data-gradle-plugin')
+        settings.apply(plugin: "com.gradle.enterprise")
+        settings.apply(plugin: "com.gradle.common-custom-user-data-gradle-plugin")
 
         settings.gradleEnterprise {
             server = "https://ge.androidx.dev"
