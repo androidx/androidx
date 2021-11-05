35c8bb5e0d475be6824174892bd4d14d6011e2f1
jnichol <jnichol@google.com>
2021-11-05 15:08:21 +0000

    Add ScalingLazyItemScope and fillParentMaxSize/fillParentMaxWidth/fillParentMaxHeight modifiers.
    Adds functionality already available in the LazyRow/Column but missing from ScalingLazyColumn.
    
    Test: ./gradlew :wear:compose:compose-material:connectedCheck --info --daemon
    Bug: 205276473
    Relnote: "Added a ScalingLazyItemScope and some new modifiers fillParentMaxSize/fillParentMaxWidth/fillParentMaxHeight to allow list items to be sized based on the size of the parent container. Items can be set to fill all or a fraction of the parents size. This exposes functionality already available in the LazyRow/Column"
    Change-Id: I4612f5088090567fe9fcec598ffbd04642fa9aa5

--- wear/compose/compose-material/src/commonMain/kotlin/androidx/wear/compose/material/ScalingLazyListState.kt
+++ wear/compose/compose-material/src/commonMain/kotlin/androidx/wear/compose/material/ScalingLazyListState.kt
@@ -48,7 +48,6 @@ public fun rememberScalingLazyListState(): ScalingLazyListState {
  */
 @Stable
 public class ScalingLazyListState : ScrollableState {
-
     internal var lazyListState: LazyListState = LazyListState(0, 0)
     internal val extraPaddingInPixels = mutableStateOf<Int?>(null)
     internal val scalingParams = mutableStateOf<ScalingParams?>(null)
@@ -68,6 +67,7 @@ public class ScalingLazyListState : ScrollableState {
             EmptyScalingLazyListLayoutInfo
         } else {
             val visibleItemsInfo = mutableListOf<ScalingLazyListItemInfo>()
+            val centralItemIndex: Int
 
             if (lazyListState.layoutInfo.visibleItemsInfo.isNotEmpty()) {
                 val verticalAdjustment =
@@ -89,7 +89,7 @@ public class ScalingLazyListState : ScrollableState {
                     centerItemInfo
                 )
                 // Go Up
-                val centralItemIndex = centralItem.index
+                centralItemIndex = centralItem.index
                 var nextItemBottomNoPadding = centerItemInfo.offset - gapBetweenItemsPx.value!!
                 val minIndex =
                     lazyListState.layoutInfo.visibleItemsInfo.minOf { it.index }
@@ -142,7 +142,7 @@ public class ScalingLazyListState : ScrollableState {
                 viewportStartOffset = lazyListState.layoutInfo.viewportStartOffset +
                     extraPaddingInPixels.value!!,
                 viewportEndOffset = lazyListState.layoutInfo.viewportEndOffset -
-                    extraPaddingInPixels.value!!
+                    extraPaddingInPixels.value!!,
             )
         }
     }
@@ -169,7 +169,7 @@ public class ScalingLazyListState : ScrollableState {
         /**
          * The default [Saver] implementation for [ScalingLazyListState].
          */
-        val Saver: Saver<ScalingLazyListState, *> = listSaver(
+        val Saver = listSaver<ScalingLazyListState, Int>(
             save = {
                 listOf(
                     it.lazyListState.firstVisibleItemIndex,
