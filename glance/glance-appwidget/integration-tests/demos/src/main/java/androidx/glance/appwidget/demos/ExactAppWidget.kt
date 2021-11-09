1319a98a9b8fab3c87a6e5117a4ac8c0c91b84a0
Pierre Barbier de Reuille <pbdr@google.com>
2021-11-09 10:23:42 +0000

    Adding RoundedCorner modifier for Android S+.
    Bug: 205677727
    Test: demos
    Relnote: n/a
    Change-Id: Ide51dcb07ace1634b84ddb2c617a22dd6c1e7034

--- glance/glance-appwidget/integration-tests/demos/src/main/java/androidx/glance/appwidget/demos/ExactAppWidget.kt
+++ glance/glance-appwidget/integration-tests/demos/src/main/java/androidx/glance/appwidget/demos/ExactAppWidget.kt
@@ -18,15 +18,15 @@ package androidx.glance.appwidget.demos
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
 import androidx.glance.LocalSize
-import androidx.glance.appwidget.unit.ColorProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.background
+import androidx.glance.appwidget.layout.cornerRadius
+import androidx.glance.appwidget.unit.ColorProvider
 import androidx.glance.layout.Column
 import androidx.glance.layout.Text
 import androidx.glance.layout.fillMaxSize
@@ -46,7 +46,8 @@ class ExactAppWidget : GlanceAppWidget() {
             modifier = GlanceModifier
                 .fillMaxSize()
                 .background(day = Color.LightGray, night = Color.DarkGray)
-                .padding(8.dp)
+                .padding(R.dimen.external_padding)
+                .cornerRadius(R.dimen.corner_radius)
         ) {
             Text(
                 context.getString(R.string.exact_widget_title),
