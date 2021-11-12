e0ea00b2ebec400ad214a96880620b7907c6caad
Sergey Vasilinets <sergeyv@google.com>
2021-11-12 14:04:15 +0000

    Workmanager: Remove @Keep annotations
    Test: manual
    fixes: 187446990
    Relnote: NA
    Change-Id: I32fd387c28128e1cf2a4bfa47d30b9b3364fbb3c

--- work/work-runtime/src/main/java/androidx/work/ListenableWorker.java
+++ work/work-runtime/src/main/java/androidx/work/ListenableWorker.java
@@ -16,13 +16,11 @@
 
 package androidx.work;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Network;
 import android.net.Uri;
 
 import androidx.annotation.IntRange;
-import androidx.annotation.Keep;
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -74,8 +72,6 @@ public abstract class ListenableWorker {
      * @param appContext The application {@link Context}
      * @param workerParams Parameters to setup the internal state of this worker
      */
-    @Keep
-    @SuppressLint("BanKeepAnnotation")
     public ListenableWorker(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
         // Actually make sure we don't get nulls.
         if (appContext == null) {
