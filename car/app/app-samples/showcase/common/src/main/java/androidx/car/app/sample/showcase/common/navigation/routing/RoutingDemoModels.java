b8cbac4c81a11763bf8676478747cc75396be6d3
Abhijoy Saha <abhijoy@google.com>
2022-01-11 22:34:04 +0000

    Add option to ActionStrip to allow its actions to ignore fade in/out animation.
    Relnote: "Added methods to allow actions inside action strips
    to ignore fade in/out animation"
    Bug: 207715266
    Test: ActionStripTest
    
    Change-Id: Ie9887faa48645cb4140b101949577c6e830d78f2

--- car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/navigation/routing/RoutingDemoModels.java
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/navigation/routing/RoutingDemoModels.java
@@ -147,7 +147,7 @@ public abstract class RoutingDemoModels {
                         new Action.Builder()
                                 .setTitle("Stop")
                                 .setOnClickListener(onStopNavigation)
-                                .build())
+                                .build(), /* alwaysShow= */ true)
                 .build();
     }
 
