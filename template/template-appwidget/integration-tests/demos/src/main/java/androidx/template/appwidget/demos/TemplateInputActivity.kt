968c61c09b8ae7667aeed3c1f765cf64e815852e
Zoe Page <zoepage@google.com>
2022-01-31 19:36:42 +0000

    Move SingleEntityTemplate toward UX specs
    Updates to the SingleEntityTemplate data blob and layouts, to move
    closer to final specs.
    
    Relnote: Updates to SingleEntityTemplate layouts
    Test: Demo app tested locally
    Change-Id: If925d9d5667fbdbc5f00c8ff1de671f2831b502e

--- template/template-appwidget/integration-tests/demos/src/main/java/androidx/template/appwidget/demos/TemplateInputActivity.kt
+++ template/template-appwidget/integration-tests/demos/src/main/java/androidx/template/appwidget/demos/TemplateInputActivity.kt
@@ -24,10 +24,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -37,10 +41,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.datastore.preferences.core.Preferences
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.ImageProvider
 import androidx.glance.action.ActionParameters
@@ -55,13 +62,19 @@ import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.unit.ColorProvider
 import androidx.template.appwidget.GlanceTemplateAppWidget
 import androidx.template.template.GlanceTemplate
+import androidx.template.template.TemplateImageWithDescription
 import androidx.template.template.SingleEntityTemplate
+import androidx.template.template.TemplateTextButton
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
+/**
+ * Main activity for the template data demo app. Hosts a form for template data to apply to a home
+ * screen widget.
+ */
 class TemplateInputActivity : ComponentActivity() {
-    private val MAX_LONG_HEX = "FFFFFFFF".toLong(radix = 16)
 
     override fun onResume() {
         super.onResume()
@@ -97,48 +110,154 @@ class TemplateInputActivity : ComponentActivity() {
     @Composable
     fun SingleEntityContent() {
         val context: Context = this@TemplateInputActivity
-        var mainText by remember { mutableStateOf(TextFieldValue()) }
+        var titleText by remember { mutableStateOf(TextFieldValue(DEFAULT_TITLE)) }
+        var subtitleText by remember { mutableStateOf(TextFieldValue()) }
+        var bodyText by remember { mutableStateOf(TextFieldValue()) }
+        var colorText by remember { mutableStateOf(TextFieldValue(DEFAULT_BACKGROUND)) }
 
         Column(modifier = Modifier.wrapContentHeight().fillMaxWidth()) {
             // Data input
-            Text("Background color:")
-            // TODO: clearer instructions and more input verification
-            TextField(value = mainText, onValueChange = { mainText = it })
+            Text("Glanceable title:")
+            TextField(
+                value = titleText,
+                singleLine = true,
+                placeholder = { Text(DEFAULT_TITLE) },
+                onValueChange = { titleText = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+            )
+            Text("Glanceable subtitle:", modifier = Modifier.padding(top = 8.dp))
+            TextField(
+                value = subtitleText,
+                singleLine = true,
+                onValueChange = { subtitleText = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+            )
+
+            Text("Glanceable body text:", modifier = Modifier.padding(top = 8.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    value = bodyText,
+                    onValueChange = { bodyText = it },
+                    maxLines = 5,
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    modifier = Modifier.weight(1F),
+                    shape = MaterialTheme.shapes.medium
+                )
+                Button(
+                    onClick = { bodyText = TextFieldValue(DEFAULT_BODY) }
+                ) { Text("Use lorem ipsum") }
+            }
+
+            Text("Background color:", modifier = Modifier.padding(top = 8.dp))
+            Text("Enter an 8-digit hex color code from 0 to FFFFFFFF (default: FFCCCCCC)")
+            Text("Using ARGB format, so the first two digits are the alpha value. From 00 " +
+                "(completely transparent) to FF (completely opaque)")
+            TextField(
+                value = colorText,
+                singleLine = true,
+                placeholder = { Text(DEFAULT_BACKGROUND) },
+                onValueChange = { colorText = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    onButtonClick(context, titleText, subtitleText, bodyText, colorText)
+                    CoroutineScope(SupervisorJob()).launch {
+                        formInputJob?.join()
+                        val prefs = GlanceState.getValue(context, definition = state, appFileKey())
+                        titleText = TextFieldValue(prefs[TitleKey]!!)
+                        subtitleText = TextFieldValue(prefs[SubtitleKey] ?: "")
+                        bodyText = TextFieldValue(prefs[BodyKey] ?: "")
+                        colorText = TextFieldValue(
+                            prefs[BackgroundKey]?.toString(16) ?: DEFAULT_BACKGROUND
+                        )
+                    }
+                })
+            )
 
             Button(
+                modifier = Modifier.padding(top = 8.dp),
                 onClick = {
-                    val longValue = mainText.text.toLong(radix = 16)
-                    if (longValue > MAX_LONG_HEX) {
-                        mainText = TextFieldValue("FFFFFFFF")
-                        Toast.makeText(context, "Value out of range", Toast.LENGTH_SHORT).show()
-                    } else if (longValue < 0) {
-                        mainText = TextFieldValue("00000000")
-                        Toast.makeText(context, "Value out of range", Toast.LENGTH_SHORT).show()
-                    }
-
+                    onButtonClick(context, titleText, subtitleText, bodyText, colorText)
                     CoroutineScope(SupervisorJob()).launch {
-                        // Save input to app datastore
-                        GlanceState.updateValue(context, state, appFileKey()) { prefs ->
-                            prefs.toMutablePreferences().apply {
-                                this[BackgroundKey] = (mainText.text).toLong(radix = 16)
-                            }
-                        }
+                        formInputJob?.join()
+                        val prefs = GlanceState.getValue(context, definition = state, appFileKey())
+                        titleText = TextFieldValue(prefs[TitleKey]!!)
+                        subtitleText = TextFieldValue(prefs[SubtitleKey] ?: "")
+                        bodyText = TextFieldValue(prefs[BodyKey] ?: "")
+                        colorText = TextFieldValue(
+                            prefs[BackgroundKey]?.toString(16) ?: DEFAULT_BACKGROUND
+                        )
                     }
-                    Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
                 }
             ) { Text("Submit") }
         }
     }
+
+    private fun onButtonClick(
+        context: Context,
+        title: TextFieldValue,
+        subtitle: TextFieldValue,
+        body: TextFieldValue,
+        color: TextFieldValue
+    ) {
+        // Validate background color
+        var colorString = color.text
+        if (colorString.isEmpty()) {
+            colorString = DEFAULT_BACKGROUND
+        }
+        if (colorString.length > 8) {
+            colorString = colorString.subSequence(0, 8).toString()
+        }
+        while (colorString.length < 8) {
+            colorString += "0"
+        }
+
+        val longValue = colorString.toLong(radix = 16)
+        if (longValue > COLOR_LONG_MAX_HEX) {
+            colorString = COLOR_STRING_MAX
+            Toast.makeText(context, R.string.background_error_high, Toast.LENGTH_SHORT).show()
+        } else if (longValue < 0) {
+            colorString = COLOR_STRING_MIN
+            Toast.makeText(context, R.string.background_error_low, Toast.LENGTH_SHORT).show()
+        }
+
+        formInputJob = CoroutineScope(SupervisorJob()).launch {
+            // Save input to app datastore
+            GlanceState.updateValue(context, state, appFileKey()) { prefs ->
+                prefs.toMutablePreferences().apply {
+                    this[TitleKey] = title.text.takeIf { it.isNotEmpty() } ?: DEFAULT_TITLE
+                    if (subtitle.text.isEmpty()) {
+                        minusAssign(SubtitleKey)
+                    } else {
+                        this[SubtitleKey] = subtitle.text
+                    }
+                    this[BodyKey] = body.text
+                    this[BackgroundKey] = colorString.toLong(radix = 16)
+                }
+            }
+        }
+        Toast.makeText(context, R.string.template_data_saved_message, Toast.LENGTH_SHORT).show()
+    }
 }
 
-val state: GlanceStateDefinition<Preferences> = PreferencesGlanceStateDefinition
+private const val DEFAULT_TITLE = "Title"
+private const val DEFAULT_BACKGROUND = "FFCCCCCC"
+private const val DEFAULT_BODY = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed " +
+    "do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+private const val COLOR_STRING_MIN = "FF000000"
+private const val COLOR_STRING_MAX = "FFFFFFFF"
+private val COLOR_LONG_MAX_HEX = COLOR_STRING_MAX.toLong(radix = 16)
+private val state: GlanceStateDefinition<Preferences> = PreferencesGlanceStateDefinition
+private var formInputJob: Job? = null
 
 class SingleEntityInputWidgetTemplate : SingleEntityTemplate() {
     override fun getData(state: Any?): Data {
         require(state is Preferences)
+        val title = state[TitleKey] ?: DEFAULT_TITLE
+        val subtitle = state[SubtitleKey] ?: ""
+        val body = state[BodyKey] ?: ""
         val background = state[BackgroundKey]?.let { ColorProvider(Color(it)) }
             ?: ColorProvider(R.color.default_widget_background)
-        return createData(background)
+        return createData(title, subtitle, body, background)
     }
 }
 
@@ -151,18 +270,30 @@ class TemplateInputWidgetReceiver : GlanceAppWidgetReceiver() {
 class TemplateButtonAction : ActionCallback {
     override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
         // Fetch the template and values from the app datastore and apply to the widget
-        val background = GlanceState.getValue(context, state, appFileKey())[BackgroundKey]
-        val templateIndex = GlanceState.getValue(context, state, appFileKey())[TemplateKey] ?: 0
+        val appPrefs = GlanceState.getValue(context, state, appFileKey())
+        val templateIndex = appPrefs[TemplateKey] ?: 0
         template = getTemplate(Templates.values()[templateIndex])
-        val widget = SingleEntityTemplateWidget()
-        if (background != null) {
-            updateAppWidgetState(context, glanceId) { prefs ->
-                prefs.toMutablePreferences().apply {
-                    this[BackgroundKey] = background
-                }
+
+        updateAppWidgetState(context, glanceId) { widgetPrefs ->
+            widgetPrefs.putAll(
+                TitleKey to (appPrefs[TitleKey] ?: DEFAULT_TITLE),
+                BackgroundKey to
+                    (appPrefs[BackgroundKey] ?: DEFAULT_BACKGROUND.toLong(radix = 16))
+            )
+            val subtitleText = appPrefs[SubtitleKey]
+            if (subtitleText == null) {
+                widgetPrefs.minusAssign(SubtitleKey)
+            } else {
+                widgetPrefs[SubtitleKey] = subtitleText
+            }
+            val bodyText = appPrefs[BodyKey]
+            if (bodyText == null) {
+                widgetPrefs.minusAssign(BodyKey)
+            } else {
+                widgetPrefs[BodyKey] = bodyText
             }
         }
-        widget.update(context, glanceId)
+        SingleEntityTemplateWidget().update(context, glanceId)
     }
 }
 
@@ -172,17 +303,37 @@ private enum class Templates {
 }
 
 private val TemplateKey = intPreferencesKey("template_key")
+private val TitleKey = stringPreferencesKey("title_key")
+private val SubtitleKey = stringPreferencesKey("subtitle_key")
+private val BodyKey = stringPreferencesKey("body_key")
 private val BackgroundKey = longPreferencesKey("background_key")
 
 // Template used by the widget, update this to change the widget type
 private var template: GlanceTemplate<*> = SingleEntityInputWidgetTemplate()
 
-private fun createData(background: ColorProvider) = SingleEntityTemplate.Data(
-    "Template example",
-    "Apply",
-    actionRunCallback<TemplateButtonAction>(),
-    ImageProvider(R.drawable.compose),
-    background
+private fun createData(
+    title: String,
+    subtitle: String,
+    body: String,
+    background: ColorProvider
+) = SingleEntityTemplate.Data(
+    header = "Demo",
+    headerIcon = TemplateImageWithDescription(
+        ImageProvider(R.drawable.compose),
+        "Header icon"
+    ),
+    title = title,
+    subtitle = subtitle,
+    bodyText = body,
+    button = TemplateTextButton(
+        actionRunCallback<TemplateButtonAction>(),
+        "Apply"
+    ),
+    mainImage = TemplateImageWithDescription(
+        ImageProvider(R.drawable.compose),
+        "Compose image"
+    ),
+    backgroundColor = background
 )
 
 private fun getTemplate(type: Templates): GlanceTemplate<*> =
