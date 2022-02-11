2ae6b97cc071130e4903b58bda454bcf29bb8e15
Chris Craik <ccraik@google.com>
2022-02-10 16:51:02 -0800

    Use PowerManager for throttle detection when available
    Bug: 217497678
    Fixes: 131755853
    Test: ThrottleDetectorTest - wrapped Pixel 2 in a jacket until it started throttling
    Relnote: "Use PowerManager for thermal throttling detection on Q (API
    29) and higher. This significantly reduces frequency of false
    positives in thermal throttling detection (benchmark retry after 90
    second cooldown), and speeds up benchmarks significantly on user
    builds. It also provides throttle detection even when clocks are
    locked (if they're locked too high for the device's physical
    environment)."
    
    Change-Id: I9c02781c91bb1f646f98d1c84f44fa16c6e5e7ba

--- benchmark/benchmark-common/src/main/java/androidx/benchmark/ThrottleDetector.kt
+++ benchmark/benchmark-common/src/main/java/androidx/benchmark/ThrottleDetector.kt
@@ -16,7 +16,12 @@
 
 package androidx.benchmark
 
+import android.content.Context
 import android.opengl.Matrix
+import android.os.Build
+import android.os.PowerManager
+import androidx.annotation.RequiresApi
+import androidx.test.platform.app.InstrumentationRegistry
 
 internal object ThrottleDetector {
     private var initNs = 0.0
@@ -25,14 +30,14 @@ internal object ThrottleDetector {
      * Copies 400K, 10 times.
      */
     private fun copySomeData() {
-        val a = ByteArray(400000)
-        val b = ByteArray(400000)
-        for (i in 0..9) {
+        val a = ByteArray(400_000)
+        val b = ByteArray(400_000)
+        repeat(10) {
             System.arraycopy(a, 0, b, 0, a.size)
         }
     }
 
-    private fun measureWorkNs(): Double {
+    internal fun measureWorkNs(): Double {
         // Access a non-trivial amount of data to try and 'reset' any cache state.
         // Have observed this to give more consistent performance when clocks are unlocked.
         copySomeData()
@@ -50,10 +55,17 @@ internal object ThrottleDetector {
     }
 
     /**
-     * Called to calculate throttling baseline, will be ignored after first call.
+     * Called to calculate throttling baseline, will be ignored after first call until reset.
+     *
+     * Does nothing if baseline isn't needed
      */
-    fun computeThrottleBaseline() {
-        if (initNs == 0.0) {
+    fun computeThrottleBaselineIfNeeded() {
+        if (Build.VERSION.SDK_INT < 29 && // can't use PowerManager API yet
+            initNs == 0.0 && // first time
+            !CpuInfo.locked && // CPU locked (presumably to stable values), should be no throttling
+            !IsolationActivity.sustainedPerformanceModeInUse && // trust sustained perf
+            !Errors.isEmulator // don't bother with emulators, will always be unpredicatable
+        ) {
             initNs = measureWorkNs()
         }
     }
@@ -71,12 +83,34 @@ internal object ThrottleDetector {
      * of single-threaded CPU work.
      */
     fun isDeviceThermalThrottled(): Boolean {
+        if (Build.VERSION.SDK_INT >= 29) {
+            return Api29Helper.isDeviceThermalThrottled()
+        }
+
         if (initNs == 0.0) {
-            // not initialized, so assume not throttled.
+            // baseline not set, so assume not throttled.
             return false
         }
 
         val workNs = measureWorkNs()
         return workNs > initNs * 1.10
+    }
+
+    @RequiresApi(29)
+    internal object Api29Helper {
+        /**
+         * We don't accept any thermal throttling status over NONE, since we have observed
+         * significant CPU variation even with `THERMAL_STATUS_LIGHT` (see ThrottleDetectorTest).
+         *
+         * On a Pixel 2 running API 30, we've seen variation from 55 - 72ms in the measureWorkNs()
+         * matrix workload.
+         */
+        fun isDeviceThermalThrottled(): Boolean {
+            return (InstrumentationRegistry
+                .getInstrumentation()
+                .context
+                .getSystemService(Context.POWER_SERVICE) as PowerManager)
+                .currentThermalStatus > PowerManager.THERMAL_STATUS_NONE
+        }
     }
 }
