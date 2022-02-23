0e1a2dd7892bd4b65e7d1218935f30cf85602efa
Hao Ke <haok@google.com>
2022-02-23 20:02:33 +0000

    Add compat method for Parcel readParcelableList and readParcelableArray.
    Relnote: Add compat method for Parcel readParcelableList and readParcelableArray.
    Test: ParcelCompatTest
    Bug: 206868182
    Change-Id: I6ecbfa3a56ee0a3cb00f3103b33527bf29497e0a

--- core/core/src/androidTest/java/androidx/core/os/ParcelCompatTest.java
+++ core/core/src/androidTest/java/androidx/core/os/ParcelCompatTest.java
@@ -20,14 +20,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import android.content.pm.Signature;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Parcel;
+import android.os.Parcelable;
 
+import androidx.annotation.RequiresApi;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
@@ -52,6 +59,37 @@ public class ParcelCompatTest {
         p.setDataPosition(0);
         Rect r2 = ParcelCompat.readParcelable(p, Rect.class.getClassLoader(), Rect.class);
         assertEquals(r, r2);
+    }
+
+    @Test
+    public void readParcelableArrayInT() {
+        Parcel p = Parcel.obtain();
+        Signature[] s = {new Signature("1234"),
+                null,
+                new Signature("abcd")
+        };
+        p.writeParcelableArray(s, 0);
+        p.setDataPosition(0);
+        Parcelable[] s1 = ParcelCompat.readParcelableArray(p, Signature.class.getClassLoader(),
+                Signature.class);
+        assertTrue(Arrays.equals(s, s1));
+        p.recycle();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Test
+    public void readParcelableListInT() {
+        final Parcel p = Parcel.obtain();
+        ArrayList<Signature> list = new ArrayList<>();
+        ArrayList<Signature> list1 = new ArrayList<>();
+        list.add(new Signature("1234"));
+        list.add(new Signature("4321"));
+        p.writeParcelableList(list, 0);
+        p.setDataPosition(0);
+        ParcelCompat.readParcelableList(p, list1, Signature.class.getClassLoader(),
+                Signature.class);
+        assertEquals(list, list1);
+        p.recycle();
     }
 
     @Test
