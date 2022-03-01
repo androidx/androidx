ee1b35a776e2035092d925c404805fc7d6429b6b
Hengrui Cao <hengruicao@google.com>
2022-03-01 22:56:51 +0000

    Port sdkservice over to aosp
    Few manual changes:
    - Renamed members to use mCamel convention
    - Added NonNull annotations to few method parameters.
    
    Test: :heath:health-data-client:test
    Bug: 221711464
    Change-Id: I092629f9975d2ff2cf135ea98b6ad5c6dd6d5bb2

---
+++ health/health-data-client/src/test/java/androidx/health/platform/client/impl/permission/token/PermissionTokenManagerTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.health.platform.client.impl.permission.token;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class PermissionTokenManagerTest {
+    @Test
+    public void setToken_getToken() {
+        Context context = ApplicationProvider.getApplicationContext();
+        final String token = "token";
+
+        PermissionTokenManager.setCurrentToken(context, token);
+
+        assertThat(PermissionTokenManager.getCurrentToken(context)).isEqualTo(token);
+    }
+}
