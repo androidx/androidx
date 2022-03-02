d7cfd98efa5608e7e5172f86b81bd3d74efb4b4e
stevebower <stevebower@google.com>
2022-03-02 14:24:09 +0000

    Extend Picker to have a read-only mode
    Added readOnly and label parameters to support a read-only mode for Picker. When readOnly = true, the currently selected option is displayed. There is a slot for a label as well - if that is supplied, the label is also displayed above the currently selected option in read-only mode. This new mode is intended to be used for screens that display multiple Pickers, only one of which is editable at a time, the others being shown as read-only.
    
    Test: androidx.wear.compose.material.test
    
    Relnote: "We have added a read-only mode to Picker, for screens with multiple Pickers where only one Picker is editable at a time. When the Picker is read-only, it displays the currently selected option and a label if one has been provided."
    
    Bug: 222267781
    Change-Id: I879de7842a21d000d5a2554922be976899f930d3

--- wear/compose/compose-material/samples/src/main/java/androidx/wear/compose/material/samples/PickerSample.kt
+++ wear/compose/compose-material/samples/src/main/java/androidx/wear/compose/material/samples/PickerSample.kt
@@ -16,19 +16,33 @@
 
 package androidx.wear.compose.material.samples
 
+import android.view.MotionEvent
 import androidx.annotation.Sampled
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.CompactChip
+import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Picker
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.rememberPickerState
@@ -84,6 +98,70 @@ fun OptionChangePicker() {
                     Text("$it")
                 }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Sampled
+@Composable
+fun DualPicker() {
+    var selectedColumn by remember { mutableStateOf(0) }
+    val textStyle = MaterialTheme.typography.display1
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Picker(
+            readOnly = selectedColumn != 0,
+            state = rememberPickerState(initialNumberOfOptions = 12,
+                initiallySelectedOption = 6),
+            modifier = Modifier.size(64.dp, 100.dp), separation = (-10).dp,
+            readOnlyLabel = {
+                Text(text = "Hour", style = MaterialTheme.typography.caption1,
+                    color = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.align(Alignment.TopCenter).offset(y = 8.dp)
+                )
+            }
+        ) { hour: Int -> Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "%02d".format(hour + 1), style = textStyle,
+                color = if (selectedColumn == 0) MaterialTheme.colors.secondary
+                    else MaterialTheme.colors.onBackground,
+                modifier = Modifier
+                    .align(Alignment.Center).wrapContentSize()
+                    .pointerInteropFilter {
+                    if (it.action == MotionEvent.ACTION_DOWN) selectedColumn = 0
+                    true
+                }
+            ) }
+        }
+        Spacer(Modifier.width(8.dp))
+        Text(text = ":", style = textStyle, color = MaterialTheme.colors.onBackground)
+        Spacer(Modifier.width(8.dp))
+        Picker(
+            readOnly = selectedColumn != 1,
+            state = rememberPickerState(initialNumberOfOptions = 60),
+            modifier = Modifier.size(64.dp, 100.dp), separation = (-10).dp,
+            readOnlyLabel = {
+                Text(text = "Minute", style = MaterialTheme.typography.caption1,
+                    color = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.align(Alignment.TopCenter).offset(y = 8.dp)
+                )
+            }
+        ) { minute: Int -> Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "%02d".format(minute), style = textStyle,
+                color = if (selectedColumn == 1) MaterialTheme.colors.secondary
+                    else MaterialTheme.colors.onBackground,
+                modifier = Modifier
+                    .align(Alignment.Center).wrapContentSize()
+                    .pointerInteropFilter {
+                    if (it.action == MotionEvent.ACTION_DOWN) selectedColumn = 1
+                    true
+                }
+            ) }
         }
     }
 }
