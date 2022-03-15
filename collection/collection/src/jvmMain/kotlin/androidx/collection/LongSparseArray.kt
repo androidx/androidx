acd40afafe15e635383713cb761799ecc01e942e
Dustin Lam <dustinlam@google.com>
2022-03-14 19:38:56 -0700

    Move LongSparseArray extensions into main artifact
    Now that collections is in Kotlin, we can move declarations from -ktx
    into the main artifact directly to avoid collisions between file-level
    classes. Since -ktx has an implementation dependency on the main
    artifact, this is a compatible change even though it removes an entire
    class from the -ktx artifact.
    
    Relnote: "Migrate LongSparseArray extensions into the main artifact,
      androidx.collection:collection. This change makes the pre-existing
      -ktx extensions accessible to Kotlin users without requiring the -ktx
      dependency, while still maintaining compatibility for existing users.
      Maintaining these extensions in a separate -ktx artifact is no longer
      beneficial since the main artifact has moved to Kotlin."
    Test: ./gradlew collection:collection-benchmark:mergeDexReleaseAndroidTest
    Change-Id: I8659ae3f23dd6c568b3a15f84cf1592ce7446774

--- collection/collection/src/jvmMain/kotlin/androidx/collection/LongSparseArray.kt
+++ collection/collection/src/jvmMain/kotlin/androidx/collection/LongSparseArray.kt
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 
-// Note: This only applies to the top level, file-class "LongSparseArraKt" and not the entire file.
-@file:RestrictTo(RestrictTo.Scope.LIBRARY)
-
 package androidx.collection
 
-import androidx.annotation.RestrictTo
 import androidx.collection.internal.EMPTY_LONGS
 import androidx.collection.internal.EMPTY_OBJECTS
 import androidx.collection.internal.binarySearch
 import androidx.collection.internal.idealLongArraySize
+import kotlin.DeprecationLevel.HIDDEN
 
 private val DELETED = Any()
 
@@ -485,4 +482,65 @@ public open class LongSparseArray<E>
             append('}')
         }
     }
+}
+
+/** Returns the number of key/value pairs in the collection. */
+public inline val <T> LongSparseArray<T>.size: Int get() = size()
+
+/** Returns true if the collection contains [key]. */
+@Suppress("NOTHING_TO_INLINE")
+public inline operator fun <T> LongSparseArray<T>.contains(key: Long): Boolean = containsKey(key)
+
+/** Allows the use of the index operator for storing values in the collection. */
+@Suppress("NOTHING_TO_INLINE")
+public inline operator fun <T> LongSparseArray<T>.set(key: Long, value: T): Unit = put(key, value)
+
+/** Creates a new collection by adding or replacing entries from [other]. */
+public operator fun <T> LongSparseArray<T>.plus(other: LongSparseArray<T>): LongSparseArray<T> {
+    val new = LongSparseArray<T>(size() + other.size())
+    new.putAll(this)
+    new.putAll(other)
+    return new
+}
+
+/** Return the value corresponding to [key], or [defaultValue] when not present. */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> LongSparseArray<T>.getOrDefault(key: Long, defaultValue: T): T =
+    get(key, defaultValue)
+
+/** Return the value corresponding to [key], or from [defaultValue] when not present. */
+public inline fun <T> LongSparseArray<T>.getOrElse(key: Long, defaultValue: () -> T): T =
+    get(key) ?: defaultValue()
+
+/** Return true when the collection contains elements. */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> LongSparseArray<T>.isNotEmpty(): Boolean = !isEmpty()
+
+/** Removes the entry for [key] only if it is mapped to [value]. */
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER") // Binary API compatibility.
+@Deprecated(
+    message = "Replaced with member function. Remove extension import!",
+    level = HIDDEN
+)
+public fun <T> LongSparseArray<T>.remove(key: Long, value: T): Boolean = remove(key, value)
+
+/** Performs the given [action] for each key/value entry. */
+public inline fun <T> LongSparseArray<T>.forEach(action: (key: Long, value: T) -> Unit) {
+    for (index in 0 until size()) {
+        action(keyAt(index), valueAt(index))
+    }
+}
+
+/** Return an iterator over the collection's keys. */
+public fun <T> LongSparseArray<T>.keyIterator(): LongIterator = object : LongIterator() {
+    var index = 0
+    override fun hasNext() = index < size()
+    override fun nextLong() = keyAt(index++)
+}
+
+/** Return an iterator over the collection's values. */
+public fun <T> LongSparseArray<T>.valueIterator(): Iterator<T> = object : Iterator<T> {
+    var index = 0
+    override fun hasNext() = index < size()
+    override fun next() = valueAt(index++)
 }
