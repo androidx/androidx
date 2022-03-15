00346bb52f9e00da9e417a6da27ce49482bffc61
Yigit Boyar <yboyar@google.com>
2022-03-14 18:33:23 -0700

    Fix include regex for playground projects
    A change in AOSP broke the project regex parser for settings.gradle.
    
    This CL updates the regex and also adds tests for expected cases.
    
    Bug: n/a
    Test: local + CI

---
+++ playground-common/playground-plugin/src/main/kotlin/androidx/playground/SettingsParser.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.playground
+
+import java.io.File
+
+/**
+ * Helper class to parse the settings.gradle file from the main build and extract a list of
+ * projects.
+ */
+internal object SettingsParser {
+    /**
+     * Match lines that start with includeProject, followed by a require argument for project gradle
+     * path and an optional argument for project file path.
+     */
+    /* ktlint-disable max-line-length */
+    var includeProjectPattern = Regex(
+        """^[\n\r\s]*includeProject\("(?<name>[a-z0-9-:]*)"(,[\n\r\s]*"(?<path>[a-z0-9-/]+))?.*\).*$""",
+        setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE)
+    ).toPattern()
+
+    fun findProjects(
+        settingsFile: File,
+    ): List<IncludedProject> {
+        return findProjects(
+            fileContents = settingsFile.readText(Charsets.UTF_8)
+        )
+    }
+
+    fun findProjects(
+        fileContents: String,
+    ): List<IncludedProject> {
+        val matcher = includeProjectPattern.matcher(fileContents)
+        val includedProjects = mutableListOf<IncludedProject>()
+        while (matcher.find()) {
+            // check if is an include project line, if so, extract project gradle path and
+            // file system path and call the filter
+            val projectGradlePath =
+                matcher.group("name") ?: error("Project gradle path should not be null")
+            val projectFilePath =
+                matcher.group("path") ?: createFilePathFromGradlePath(projectGradlePath)
+            includedProjects.add(IncludedProject(projectGradlePath, projectFilePath))
+        }
+        return includedProjects
+    }
+
+    /**
+     * Converts a gradle path (e.g. :a:b:c) to a file path (a/b/c)
+     */
+    private fun createFilePathFromGradlePath(projectGradlePath: String): String {
+        return projectGradlePath.trimStart(':').replace(':', '/')
+    }
+
+    internal data class IncludedProject(
+        val gradlePath: String,
+        val filePath: String
+    )
+}
---
+++ playground-common/playground-plugin/src/test/kotlin/androidx/playground/SettingsParserTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.playground
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+
+class SettingsParserTest {
+    @Test
+    fun parseProjects() {
+        SettingsParser.includeProjectPattern = Regex(
+            """^[\n\r\s]*includeProject\("(?<name>[a-z0-9-:]*)"(,[\n\r\s]*"(?<path>[a-z0-9-/]+))?.*\).*$""",
+            setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE)
+        ).toPattern()
+        val projects = SettingsParser.findProjects(
+            """
+            includeProject(":no:filepath", [BuildType.MAIN])
+            includeProject(":with:filepath", "some/dir", [BuildType.COMPOSE])
+                includeProject(":has:spaces:before:include", "dir2", [BuildType.MAIN])
+            includeProject(":has:comments:after", "dir3", [BuildType.MAIN]) // some comment
+            // includeProject("commented", "should not be there", [BuildType.MAIN])
+            includeProject("no:build:type")
+            includeProject("no:build:type:with:path", "dir4")
+            """.trimIndent()
+        )
+        Truth.assertThat(
+            projects
+        ).containsExactly(
+            SettingsParser.IncludedProject(
+                gradlePath = ":with:filepath",
+                filePath = "some/dir"
+            ),
+            SettingsParser.IncludedProject(
+                gradlePath = ":no:filepath",
+                filePath = "no/filepath"
+            ),
+            SettingsParser.IncludedProject(
+                gradlePath = ":has:spaces:before:include",
+                filePath = "dir2"
+            ),
+            SettingsParser.IncludedProject(
+                gradlePath = ":has:comments:after",
+                filePath = "dir3"
+            ),
+            SettingsParser.IncludedProject(
+                gradlePath = "no:build:type",
+                filePath = "no/build/type"
+            ),
+            SettingsParser.IncludedProject(
+                gradlePath = "no:build:type:with:path",
+                filePath = "dir4"
+            )
+        )
+    }
+}
