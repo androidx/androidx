843d5ce622a42d916891bde646335f15c0ca70d9
Ember Rose <emberrose@google.com>
2022-03-17 22:33:13 +0000

    Revert "Add a flag to biometric fragments to ensure that the view model provider that was given to the constructor is used when looking up the view model after showing."
    This reverts commit 302e8694a44620a4d3f3345f718adc6561a64da4.
    
    Reason for revert: Presubmit tests broken
    Bug: 225187683
    
    Change-Id: I7f6fc49e84fdfd59a2dc99299aacd7898e95a5c2

--- biometric/biometric/src/main/java/androidx/biometric/FingerprintDialogFragment.java
+++ biometric/biometric/src/main/java/androidx/biometric/FingerprintDialogFragment.java
@@ -58,8 +58,6 @@ import java.lang.annotation.RetentionPolicy;
 public class FingerprintDialogFragment extends DialogFragment {
     private static final String TAG = "FingerprintFragment";
 
-    private static final String ARG_HOST_ACTIVITY = "host_activity";
-
     /**
      * The dialog has not been initialized.
      */
@@ -152,16 +150,8 @@ public class FingerprintDialogFragment extends DialogFragment {
      */
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     @NonNull
-    static FingerprintDialogFragment newInstance(boolean hostedInActivity) {
-        final FingerprintDialogFragment fragment = new FingerprintDialogFragment();
-        final Bundle args = new Bundle();
-        args.putBoolean(ARG_HOST_ACTIVITY, hostedInActivity);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    private boolean isHostedInActivity() {
-        return getArguments().getBoolean(ARG_HOST_ACTIVITY, true);
+    static FingerprintDialogFragment newInstance() {
+        return new FingerprintDialogFragment();
     }
 
     @Override
@@ -259,7 +249,12 @@ public class FingerprintDialogFragment extends DialogFragment {
      * fragment.
      */
     private void connectViewModel() {
-        mViewModel = BiometricPrompt.getViewModel(this, isHostedInActivity());
+        final Context host = BiometricPrompt.getHostActivityOrContext(this);
+        if (host == null) {
+            return;
+        }
+
+        mViewModel = BiometricPrompt.getViewModel(host);
 
         mViewModel.getFingerprintDialogState().observe(this, new Observer<Integer>() {
             @Override
@@ -363,7 +358,8 @@ public class FingerprintDialogFragment extends DialogFragment {
      */
     private int getThemedColorFor(int attr) {
         final Context context = getContext();
-        if (context == null) {
+        final Context host = BiometricPrompt.getHostActivityOrContext(this);
+        if (context == null || host == null) {
             Log.w(TAG, "Unable to get themed color. Context or activity is null.");
             return 0;
         }
@@ -371,7 +367,7 @@ public class FingerprintDialogFragment extends DialogFragment {
         TypedValue tv = new TypedValue();
         Resources.Theme theme = context.getTheme();
         theme.resolveAttribute(attr, tv, true /* resolveRefs */);
-        TypedArray arr = context.obtainStyledAttributes(tv.data, new int[] {attr});
+        TypedArray arr = host.obtainStyledAttributes(tv.data, new int[] {attr});
 
         final int color = arr.getColor(0 /* index */, 0 /* defValue */);
         arr.recycle();
