fb2ccba7f59f1876b39113c77b4742c05c808b96
clarafok <clarafok@google.com>
2022-03-25 13:57:30 -0700

    Converted SavedStateRegistryController to Kotlin
    Part of the kotlinification process of SavedState library.
    
    Test: ./gradlew checkApi all existing test pass
    Test: ./gradlew savedstate:savedstate:cC
    Bug: 220191285
    
    Relnote: "`SavedStateRegistryController` is now in Kotlin"
    Change-Id: Ie3b7373dd457de21f5d544e5278e40318a5c460e

--- savedstate/savedstate/src/main/java/androidx/savedstate/SavedStateRegistryController.kt
+++ savedstate/savedstate/src/main/java/androidx/savedstate/SavedStateRegistryController.kt
@@ -13,99 +13,80 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.savedstate
 
-package androidx.savedstate;
-
-import android.os.Bundle;
-
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Lifecycle;
+import android.os.Bundle
+import androidx.annotation.MainThread
+import androidx.lifecycle.Lifecycle
 
 /**
- * An API for {@link SavedStateRegistryOwner} implementations to control {@link SavedStateRegistry}.
- * <p>
- * {@code SavedStateRegistryOwner} should call {@link #performRestore(Bundle)} to restore state of
- * {@link SavedStateRegistry} and {@link #performSave(Bundle)} to gather SavedState from it.
+ * An API for [SavedStateRegistryOwner] implementations to control [SavedStateRegistry].
+ *
+ * `SavedStateRegistryOwner` should call [performRestore] to restore state of
+ * [SavedStateRegistry] and [performSave] to gather SavedState from it.
  */
-public final class SavedStateRegistryController {
-    private final SavedStateRegistryOwner mOwner;
-    private final SavedStateRegistry mRegistry;
+class SavedStateRegistryController private constructor(private val owner: SavedStateRegistryOwner) {
 
-    private boolean mAttached = false;
+    val savedStateRegistry: SavedStateRegistry = SavedStateRegistry()
 
-    private SavedStateRegistryController(SavedStateRegistryOwner owner) {
-        mOwner = owner;
-        mRegistry = new SavedStateRegistry();
-    }
-
-    /**
-     * Returns controlled {@link SavedStateRegistry}
-     */
-    @NonNull
-    public SavedStateRegistry getSavedStateRegistry() {
-        return mRegistry;
-    }
+    private var attached = false
 
     /**
      * Perform the initial, one time attachment necessary to configure this
-     * {@link SavedStateRegistry}. This must be called when the owner's {@link Lifecycle} is
-     * {@link Lifecycle.State#INITIALIZED} and before you call
-     * {@link #performRestore(Bundle)}.
+     * [SavedStateRegistry]. This must be called when the owner's [Lifecycle] is
+     * [Lifecycle.State.INITIALIZED] and before you call [performRestore].
      */
     @MainThread
-    public void performAttach() {
-        Lifecycle lifecycle = mOwner.getLifecycle();
-        if (lifecycle.getCurrentState() != Lifecycle.State.INITIALIZED) {
-            throw new IllegalStateException("Restarter must be created only during "
-                    + "owner's initialization stage");
+    fun performAttach() {
+        val lifecycle = owner.lifecycle
+        check(lifecycle.currentState == Lifecycle.State.INITIALIZED) {
+            ("Restarter must be created only during owner's initialization stage")
         }
-        lifecycle.addObserver(new Recreator(mOwner));
-        mRegistry.performAttach(lifecycle);
-
-        mAttached = true;
+        lifecycle.addObserver(Recreator(owner))
+        savedStateRegistry.performAttach(lifecycle)
+        attached = true
     }
 
     /**
-     * An interface for an owner of this {@link SavedStateRegistry} to restore saved state.
+     * An interface for an owner of this [SavedStateRegistry] to restore saved state.
      *
      * @param savedState restored state
      */
     @MainThread
-    public void performRestore(@Nullable Bundle savedState) {
+    fun performRestore(savedState: Bundle?) {
         // To support backward compatibility with libraries that do not explicitly
         // call performAttach(), we make sure that work is done here
-        if (!mAttached) {
-            performAttach();
+        if (!attached) {
+            performAttach()
         }
-        Lifecycle lifecycle = mOwner.getLifecycle();
-        if (lifecycle.getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
-            throw new IllegalStateException("performRestore cannot be called when owner "
-                    + " is " + lifecycle.getCurrentState());
+        val lifecycle = owner.lifecycle
+        check(!lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+            ("performRestore cannot be called when owner is ${lifecycle.currentState}")
         }
-        mRegistry.performRestore(savedState);
+        savedStateRegistry.performRestore(savedState)
     }
 
     /**
-     * An interface for an owner of this  {@link SavedStateRegistry}
+     * An interface for an owner of this  [SavedStateRegistry]
      * to perform state saving, it will call all registered providers and
      * merge with unconsumed state.
      *
      * @param outBundle Bundle in which to place a saved state
      */
     @MainThread
-    public void performSave(@NonNull Bundle outBundle) {
-        mRegistry.performSave(outBundle);
+    fun performSave(outBundle: Bundle) {
+        savedStateRegistry.performSave(outBundle)
     }
 
-    /**
-     * Creates a {@link SavedStateRegistryController}.
-     * <p>
-     * It should be called during construction time of {@link SavedStateRegistryOwner}
-     */
-    @NonNull
-    public static SavedStateRegistryController create(@NonNull SavedStateRegistryOwner owner) {
-        return new SavedStateRegistryController(owner);
+    companion object {
+        /**
+         * Creates a [SavedStateRegistryController].
+         *
+         * It should be called during construction time of [SavedStateRegistryOwner]
+         */
+        @JvmStatic
+        fun create(owner: SavedStateRegistryOwner): SavedStateRegistryController {
+            return SavedStateRegistryController(owner)
+        }
     }
 }
