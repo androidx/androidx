61aca7b33219167a7976251f9db6a7cff6b5c3e0
Sergey Vasilinets <sergeyv@google.com>
2022-03-28 00:55:03 +0100

    Migrate IdGenerator to Kotlin
    bug: 209145335
    Test: N/A
    Change-Id: If56685618430df3c0b923aa439cf5c45acf66e1a

--- work/work-runtime/src/main/java/androidx/work/impl/utils/IdGenerator.kt
+++ work/work-runtime/src/main/java/androidx/work/impl/utils/IdGenerator.kt
@@ -13,136 +13,92 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.work.impl.utils
 
-package androidx.work.impl.utils;
-
-import static android.content.Context.MODE_PRIVATE;
-
-import static androidx.work.impl.utils.PreferenceUtils.INSERT_PREFERENCE;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.RestrictTo;
-import androidx.sqlite.db.SupportSQLiteDatabase;
-import androidx.work.impl.WorkDatabase;
-import androidx.work.impl.model.Preference;
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.impl.WorkDatabase
+import androidx.work.impl.model.Preference
+import androidx.work.impl.utils.PreferenceUtils.INSERT_PREFERENCE
+import java.util.concurrent.Callable
 
 /**
- * Generates unique IDs that are persisted in {@link SharedPreferences}.
- *
- * @hide
+ * Generates unique IDs that are persisted in [SharedPreferences].
+ * @param workDatabase The [WorkDatabase] where metadata is persisted.
  */
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-public class IdGenerator {
-
-    /** The initial id used for JobInfos and Alarms. */
-    public static final int INITIAL_ID = 0;
-    public static final String PREFERENCE_FILE_KEY = "androidx.work.util.id";
-    public static final String NEXT_JOB_SCHEDULER_ID_KEY = "next_job_scheduler_id";
-    public static final String NEXT_ALARM_MANAGER_ID_KEY = "next_alarm_manager_id";
-
-    private final WorkDatabase mWorkDatabase;
-
+class IdGenerator(private val workDatabase: WorkDatabase) {
     /**
-     * Constructs a {@link IdGenerator}.
-     *
-     * @param workDatabase The {@link WorkDatabase} where metadata is persisted.
+     * Generates IDs for [android.app.job.JobInfo] jobs given a reserved range.
      */
-    public IdGenerator(@NonNull WorkDatabase workDatabase) {
-        mWorkDatabase = workDatabase;
-    }
-
-    /**
-     * Generates IDs for {@link android.app.job.JobInfo} jobs given a reserved range.
-     */
-    public int nextJobSchedulerIdWithRange(int minInclusive, int maxInclusive) {
-        synchronized (IdGenerator.class) {
-            int id = nextId(NEXT_JOB_SCHEDULER_ID_KEY);
-            if (id < minInclusive || id > maxInclusive) {
+    fun nextJobSchedulerIdWithRange(minInclusive: Int, maxInclusive: Int): Int {
+        return workDatabase.runInTransaction(Callable {
+            var id = workDatabase.nextId(NEXT_JOB_SCHEDULER_ID_KEY)
+            if (id !in minInclusive..maxInclusive) {
                 // outside the range, re-start at minInclusive.
-                id = minInclusive;
-                update(NEXT_JOB_SCHEDULER_ID_KEY, id + 1);
+                id = minInclusive
+                workDatabase.updatePreference(NEXT_JOB_SCHEDULER_ID_KEY, id + 1)
             }
-            return id;
-        }
+            id
+        })
     }
 
     /**
-     * Generates IDs for {@link android.app.AlarmManager} work.
+     * Generates IDs for [android.app.AlarmManager] work.
      */
-    public int nextAlarmManagerId() {
-        synchronized (IdGenerator.class) {
-            return nextId(NEXT_ALARM_MANAGER_ID_KEY);
-        }
+    fun nextAlarmManagerId(): Int {
+        return workDatabase.runInTransaction(Callable {
+            workDatabase.nextId(NEXT_ALARM_MANAGER_ID_KEY)
+        })
     }
+}
 
-    /**
-     * Returns the next available ID based on a persisted counter.
-     * Resets ID counter to 0 when the ID exceeded {@link Integer#MAX_VALUE}.
-     *
-     * @param key String matching the particular counter to retrieve
-     * @return next available id
-     */
-    private int nextId(String key) {
-        mWorkDatabase.beginTransaction();
+private fun WorkDatabase.nextId(key: String): Int {
+    val value = preferenceDao().getLongValue(key)
+    val id = value?.toInt() ?: INITIAL_ID
+    val nextId = if (id == Int.MAX_VALUE) INITIAL_ID else id + 1
+    updatePreference(key, nextId)
+    return id
+}
+
+private fun WorkDatabase.updatePreference(key: String, value: Int) =
+    this.preferenceDao().insertPreference(Preference(key, value.toLong()))
+
+/** The initial id used for JobInfos and Alarms.  */
+const val INITIAL_ID = 0
+const val NEXT_JOB_SCHEDULER_ID_KEY = "next_job_scheduler_id"
+const val NEXT_ALARM_MANAGER_ID_KEY = "next_alarm_manager_id"
+const val PREFERENCE_FILE_KEY = "androidx.work.util.id"
+
+/**
+ * Migrates [IdGenerator] from [android.content.SharedPreferences] to the
+ * [WorkDatabase].
+ *
+ * @param context The application [Context]
+ */
+internal fun migrateLegacyIdGenerator(
+    context: Context,
+    sqLiteDatabase: SupportSQLiteDatabase
+) {
+    val sharedPreferences = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE)
+
+    // Check to see if we have not migrated already.
+    if (sharedPreferences.contains(NEXT_JOB_SCHEDULER_ID_KEY) ||
+        sharedPreferences.contains(NEXT_JOB_SCHEDULER_ID_KEY)
+    ) {
+        val nextJobId = sharedPreferences.getInt(NEXT_JOB_SCHEDULER_ID_KEY, INITIAL_ID)
+        val nextAlarmId = sharedPreferences.getInt(NEXT_ALARM_MANAGER_ID_KEY, INITIAL_ID)
+        sqLiteDatabase.beginTransaction()
         try {
-            Long value = mWorkDatabase.preferenceDao().getLongValue(key);
-            int id = value != null ? value.intValue() : INITIAL_ID;
-            int nextId = (id == Integer.MAX_VALUE) ? INITIAL_ID : id + 1;
-            update(key, nextId);
-            mWorkDatabase.setTransactionSuccessful();
-            return id;
+            sqLiteDatabase.execSQL(INSERT_PREFERENCE, arrayOf(NEXT_JOB_SCHEDULER_ID_KEY, nextJobId))
+            sqLiteDatabase.execSQL(
+                INSERT_PREFERENCE, arrayOf(NEXT_ALARM_MANAGER_ID_KEY, nextAlarmId)
+            )
+            // Cleanup
+            sharedPreferences.edit().clear().apply()
+            sqLiteDatabase.setTransactionSuccessful()
         } finally {
-            mWorkDatabase.endTransaction();
+            sqLiteDatabase.endTransaction()
         }
-
-    }
-
-    private void update(String key, int value) {
-        mWorkDatabase.preferenceDao().insertPreference(new Preference(key, Long.valueOf(value)));
-    }
-
-    /**
-     * Migrates {@link IdGenerator} from {@link android.content.SharedPreferences} to the
-     * {@link WorkDatabase}.
-     *
-     * @param context The application {@link Context}
-     */
-    // TODO(b/141962522): Suppressed during upgrade to AGP 3.6.
-    @SuppressWarnings("IdentityBinaryExpression")
-    public static void migrateLegacyIdGenerator(
-            @NonNull Context context,
-            @NonNull SupportSQLiteDatabase sqLiteDatabase) {
-
-        SharedPreferences sharedPreferences =
-                context.getSharedPreferences(PREFERENCE_FILE_KEY, MODE_PRIVATE);
-
-        // Check to see if we have not migrated already.
-        if (sharedPreferences.contains(NEXT_JOB_SCHEDULER_ID_KEY)
-                || sharedPreferences.contains(NEXT_JOB_SCHEDULER_ID_KEY)) {
-
-            int nextJobId = sharedPreferences.getInt(NEXT_JOB_SCHEDULER_ID_KEY, INITIAL_ID);
-            int nextAlarmId = sharedPreferences.getInt(NEXT_ALARM_MANAGER_ID_KEY, INITIAL_ID);
-
-            sqLiteDatabase.beginTransaction();
-            try {
-                sqLiteDatabase.execSQL(INSERT_PREFERENCE,
-                        new Object[]{NEXT_JOB_SCHEDULER_ID_KEY, nextJobId});
-
-                sqLiteDatabase.execSQL(INSERT_PREFERENCE,
-                        new Object[]{NEXT_ALARM_MANAGER_ID_KEY, nextAlarmId});
-                // Cleanup
-                sharedPreferences.edit()
-                    .clear()
-                    .apply();
-
-                sqLiteDatabase.setTransactionSuccessful();
-            } finally {
-                sqLiteDatabase.endTransaction();
-            }
-        }
-
     }
 }
