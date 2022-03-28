d718f98b9cac7f7644edf5a3cc55e5f1233bfc36
Neda Topoljanac <tneda@google.com>
2022-03-28 15:53:59 +0000

    Refactor Button to use Nullable fields instead of field + boolean
    This is for parameters that don't have to be set by user, but have
    different default values based on the other parameters.
    
    Bug: N/A
    Test: Manualy tested.
    Relnote: "Button refactoring."
    Change-Id: Ib7135e86bf8583e3784cf50e47e63dc99eaa61af

--- wear/tiles/tiles-material/src/main/java/androidx/wear/tiles/material/Button.java
+++ wear/tiles/tiles-material/src/main/java/androidx/wear/tiles/material/Button.java
@@ -90,15 +90,12 @@ public class Button implements LayoutElement {
         @NonNull private CharSequence mContentDescription = "";
         @NonNull private DpProp mSize = DEFAULT_BUTTON_SIZE;
         @Nullable private String mText = null;
-        private @TypographyName int mTypographyName =
-                getDefaultTypographyForSize(DEFAULT_BUTTON_SIZE);
-        private boolean mIsTypographyNameSet = false;
+        @Nullable private Integer mTypographyName = null;
         @Nullable private String mIcon = null;
         @Nullable private DpProp mIconSize = null;
         @Nullable private String mImage = null;
         @NonNull private ButtonColors mButtonColors = PRIMARY_BUTTON_COLORS;
         @ButtonType private int mType = NOT_SET;
-        private boolean mDefaultSize = false;
 
         /**
          * Creates a builder for the {@link Button} from the given content. Custom content should be
@@ -185,7 +182,6 @@ public class Button implements LayoutElement {
             resetContent();
             this.mIcon = resourceId;
             this.mType = ICON;
-            this.mDefaultSize = false;
             this.mIconSize = size;
             return this;
         }
@@ -204,7 +200,6 @@ public class Button implements LayoutElement {
             resetContent();
             this.mIcon = resourceId;
             this.mType = ICON;
-            this.mDefaultSize = true;
             return this;
         }
 
@@ -225,7 +220,6 @@ public class Button implements LayoutElement {
             resetContent();
             this.mText = text;
             this.mType = TEXT;
-            this.mDefaultSize = true;
             return this;
         }
 
@@ -243,9 +237,7 @@ public class Button implements LayoutElement {
             resetContent();
             this.mText = text;
             this.mTypographyName = typographyName;
-            this.mIsTypographyNameSet = true;
             this.mType = TEXT;
-            this.mDefaultSize = false;
             return this;
         }
 
@@ -263,13 +255,12 @@ public class Button implements LayoutElement {
             resetContent();
             this.mImage = resourceId;
             this.mType = IMAGE;
-            this.mDefaultSize = false;
             return this;
         }
 
         private void resetContent() {
             this.mText = null;
-            this.mIsTypographyNameSet = false;
+            this.mTypographyName = null;
             this.mIcon = null;
             this.mImage = null;
             this.mCustomContent = null;
@@ -319,9 +310,9 @@ public class Button implements LayoutElement {
                 case ICON:
                 {
                     DpProp iconSize =
-                            mDefaultSize
-                                    ? ButtonDefaults.recommendedIconSize(mSize)
-                                    : checkNotNull(mIconSize);
+                            mIconSize != null
+                                    ? mIconSize
+                                    : ButtonDefaults.recommendedIconSize(mSize);
                     content =
                             new Image.Builder()
                                     .setResourceId(checkNotNull(mIcon))
@@ -339,7 +330,7 @@ public class Button implements LayoutElement {
                 {
                     @TypographyName
                     int typographyName =
-                            mIsTypographyNameSet
+                            mTypographyName != null
                                     ? mTypographyName
                                     : getDefaultTypographyForSize(mSize);
                     content =
