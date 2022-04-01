018966e693e2dd8e0930ed24eb63e9c851548c6c
clarafok <clarafok@google.com>
2022-04-01 16:44:09 -0700

    Convert ViewTreeSavedStateRegistryOwner to Kotlin
    Deprecated helper function `View.findViewTreeSavedStateRegistryOwner()
    from savedstate-ktx. Replaced with `View.getViewTreeSavedStateRegistryOwner()` from
    `ViewTreeSavedStateRegistryOwner`
    
    Test: ./gradlew checkApi all existing test pass
    
    Bug: 220191285
    
    RelNote: "ViewTreeSavedStateRegistryOwner in SavedState module is now in Kotlin.
    Its new `View.findViewTreeSavedStateRegistryOwner()` has replaced the
    one from View.kt which is now deprecated"
    
    Change-Id: Ib9cbcc89c1973ea3ee9f554797afdb7763d52d52

--- savedstate/savedstate/src/main/java/androidx/savedstate/ViewTreeSavedStateRegistryOwner.kt
+++ savedstate/savedstate/src/main/java/androidx/savedstate/ViewTreeSavedStateRegistryOwner.kt
@@ -13,70 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:JvmName("ViewTreeSavedStateRegistryOwner")
 
-package androidx.savedstate;
+package androidx.savedstate
 
-import android.view.View;
-import android.view.ViewParent;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.view.View
 
 /**
- * Accessors for finding a view tree-local {@link SavedStateRegistryOwner} that allows managing the
- * saving state using {@link SavedStateRegistry} for the given view.
+ * Set the [SavedStateRegistryOwner] responsible for managing the saved state for this [View]
+ * Calls to [get] from this view or descendants will return `owner`.
+ *
+ * This is is automatically set for you in the common cases of using fragments or
+ * ComponentActivity.
+ *
+ *
+ * This should only be called by constructs such as activities or fragments that manage
+ * a view tree and their saved state through a [SavedStateRegistryOwner]. Callers
+ * should only set a [SavedStateRegistryOwner] that will be *stable.* The
+ * associated [SavedStateRegistry] should be cleared if the view tree is removed and is
+ * not guaranteed to later become reattached to a window.
+ *
+ * @param owner The [SavedStateRegistryOwner] responsible for managing the
+ * saved state for the given view
  */
-public final class ViewTreeSavedStateRegistryOwner {
-    private ViewTreeSavedStateRegistryOwner() {
-        // No instances
-    }
+@JvmName("set")
+fun View.setViewTreeSavedStateRegistryOwner(owner: SavedStateRegistryOwner?) {
+    setTag(R.id.view_tree_saved_state_registry_owner, owner)
+}
 
-    /**
-     * Set the {@link SavedStateRegistryOwner} responsible for managing the saved state for the
-     * given {@link View}.
-     * Calls to {@link #get(View)} from this view or descendants will return {@code owner}.
-     *
-     * This is is automatically set for you in the common cases of using fragments or
-     * ComponentActivity.
-     *
-     * <p>This should only be called by constructs such as activities or fragments that manage
-     * a view tree and their saved state through a {@link SavedStateRegistryOwner}. Callers
-     * should only set a {@link SavedStateRegistryOwner} that will be <em>stable.</em> The
-     * associated {@link SavedStateRegistry} should be cleared if the view tree is removed and is
-     * not guaranteed to later become reattached to a window.</p>
-     *
-     * @param view Root view managed by {@link SavedStateRegistryOwner}
-     * @param owner The {@link SavedStateRegistryOwner} responsible for managing the
-     *              saved state for the given view
-     */
-    public static void set(@NonNull View view, @Nullable SavedStateRegistryOwner owner) {
-        view.setTag(R.id.view_tree_saved_state_registry_owner, owner);
-    }
+/**
+ * Retrieve the [SavedStateRegistryOwner] responsible for managing the saved state
+ * for this [View].
+ * This may be used to save or restore the state associated with the view.
+ *
+ * The returned [SavedStateRegistryOwner] is managing all the Views within the Fragment
+ * or Activity this [View] is added to.
 
-    /**
-     * Retrieve the {@link SavedStateRegistryOwner} responsible for managing the saved state
-     * for the given {@link View}.
-     * This may be used to save or restore the state associated with the view.
-     *
-     * The returned {@link SavedStateRegistryOwner} is managing all the Views within the Fragment
-     * or Activity the given {@link View} is added to.
-     *
-     * @param view View to fetch a {@link SavedStateRegistryOwner} for
-     * @return The {@link SavedStateRegistryOwner} responsible for managing the saved state for
-     *         the given view and/or some subset of its ancestors
-     */
-    @Nullable
-    public static SavedStateRegistryOwner get(@NonNull View view) {
-        SavedStateRegistryOwner found = (SavedStateRegistryOwner) view.getTag(
-                R.id.view_tree_saved_state_registry_owner);
-        if (found != null) return found;
-        ViewParent parent = view.getParent();
-        while (found == null && parent instanceof View) {
-            final View parentView = (View) parent;
-            found = (SavedStateRegistryOwner) parentView.getTag(
-                    R.id.view_tree_saved_state_registry_owner);
-            parent = parentView.getParent();
-        }
-        return found;
-    }
+ * @return The [SavedStateRegistryOwner] responsible for managing the saved state for
+ * this view and/or some subset of its ancestors
+ */
+@JvmName("get")
+fun View.findViewTreeSavedStateRegistryOwner(): SavedStateRegistryOwner? {
+    return generateSequence(this) { view ->
+        view.parent as? View
+    }.mapNotNull { view ->
+        view.getTag(R.id.view_tree_saved_state_registry_owner) as? SavedStateRegistryOwner
+    }.firstOrNull()
 }
