899ca7123479a4f5de52a1f37e78535345696f24
stevebower <stevebower@google.com>
2022-04-12 12:10:32 +0100

    Extend SwipeDismissableNavHost to support edge-swiping.
    SwipeToDismissBox supports edge-swiping, by adding Modifier.edgeSwipeToDismiss to composables that support horizontal scrolling. In order to allow such usage within SwipeDismissableNavHost, SwipeToDismissBoxState can now be passed into SwipeDismissableNavHostState (previously, it was created internally).
    
    Test: Run androidx.wear.compose.navigation.test
    Bug: 228336555
    Relnote: "We have added a SwipeDismissableNavHostState parameter to SwipeDismissableNavHost. This supports use of edge-swiping on screens used as navigation destinations, because SwipeToDismissBoxState can now be hoisted and used to initialise both SwipeDismissableNavHostState and Modifier.edgeSwipeToDismiss on screens that require edge-swiping."
    Change-Id: I819f5f6daa7084f7c7b536652937d4d1dbdb7ff8

--- wear/compose/integration-tests/navigation/src/main/java/androidx/wear/compose/integration/navigation/MainActivity.kt
+++ wear/compose/integration-tests/navigation/src/main/java/androidx/wear/compose/integration/navigation/MainActivity.kt
@@ -19,17 +19,27 @@ package androidx.wear.compose.integration.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.CompactChip
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.edgeSwipeToDismiss
+import androidx.wear.compose.material.rememberSwipeToDismissBoxState
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavHostState
 
 class MainActivity : ComponentActivity() {
 
@@ -38,9 +48,11 @@ class MainActivity : ComponentActivity() {
 
         setContent(parent = null) {
             MaterialTheme {
+                val swipeToDismissBoxState = rememberSwipeToDismissBoxState()
                 val navController = rememberSwipeDismissableNavController()
                 SwipeDismissableNavHost(
                     navController = navController,
+                    state = rememberSwipeDismissableNavHostState(swipeToDismissBoxState),
                     startDestination = START
                 ) {
                     composable(START) {
@@ -53,6 +65,11 @@ class MainActivity : ComponentActivity() {
                             CompactChip(
                                 onClick = { navController.navigate(SCREEN2) },
                                 label = { Text("Next screen") },
+                            )
+                            Spacer(modifier = Modifier.fillMaxWidth().height(4.dp))
+                            CompactChip(
+                                onClick = { navController.navigate(EDGE_SWIPE_SCREEN) },
+                                label = { Text("Screen with edge swipe") },
                             )
                         }
                     }
@@ -82,6 +99,20 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     }
+                    composable(EDGE_SWIPE_SCREEN) {
+                        val horizontalScrollState = rememberScrollState(0)
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Text(
+                                modifier = Modifier.align(Alignment.Center)
+                                    .edgeSwipeToDismiss(swipeToDismissBoxState)
+                                    .horizontalScroll(horizontalScrollState),
+                                text = "This text can be scrolled horizontally - " +
+                                    "to dismiss, swipe " +
+                                    "right from the left edge of the screen" +
+                                    " (called Edge Swiping)",
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -91,3 +122,4 @@ class MainActivity : ComponentActivity() {
 private const val START = "start"
 private const val SCREEN2 = "screen2"
 private const val SCREEN3 = "screen3"
+private const val EDGE_SWIPE_SCREEN = "edge_swipe_screen"
