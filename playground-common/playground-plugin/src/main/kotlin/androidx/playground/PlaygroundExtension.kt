88badb30286269f043ea7d9ab7ed8b401d6c7f5b
Aurimas <aurimas@liutikas.net>
2022-04-22 20:28:32 +0000

    [GH] Explicitly specify repositories for plugin resolution
    Explicitly specify repositories for plugin resolution as the default is
    gradlePluginPortal which has unsigned artifacts for kotlinx.serialization
    
    Test: ./gradlew tasks locally
    
    This is an imported pull request from https://github.com/androidx/androidx/pull/372.
    
    Resolves #372
    Github-Pr-Head-Sha: 86004904479d09e4342ba16a2c61b740d6d49abb
    GitOrigin-RevId: aa459cdd42e3bdb88781594a19848f4d042b95e4
    Change-Id: I467548e76ae77c6fe1bd2939aa7b5b2b9166540e

--- playground-common/playground-plugin/src/main/kotlin/androidx/playground/PlaygroundExtension.kt
+++ playground-common/playground-plugin/src/main/kotlin/androidx/playground/PlaygroundExtension.kt
@@ -87,6 +87,12 @@ open class PlaygroundExtension @Inject constructor(
      * @param relativePathToRoot The relative path of the project to the root AndroidX project
      */
     fun setupPlayground(relativePathToRoot: String) {
+        // gradlePluginPortal has a variety of unsigned binaries that have proper signatures
+        // in mavenCentral, so prefer that over gradlePluginPortal.
+        settings.pluginManagement.repositories {
+            it.mavenCentral()
+            it.gradlePluginPortal()
+        }
         val projectDir = settings.rootProject.projectDir
         val supportRoot = File(projectDir, relativePathToRoot).canonicalFile
         this.supportRootDir = supportRoot
