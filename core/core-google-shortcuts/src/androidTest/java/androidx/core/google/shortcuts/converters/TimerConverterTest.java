b3971810013474da772a2987588acbc319c40fcd
David Dong <ddong@google.com>
2022-04-25 16:29:48 -0700

    change firebase-appindexing dependency to the new play-services-appindex
    Test: unit test + manual testing
    Bug: 205890624
    Relnote: "Change firebase-appindexing dependency to the new play-services-appindex. No behavior change. Upgrading to this version of core-google-shortcuts isn't supported if the app has an existing dependency on firebase-appindexing."
    
    Change-Id: Ibc079356bea790e00126d0130c307acfcc823c21

--- core/core-google-shortcuts/src/androidTest/java/androidx/core/google/shortcuts/converters/TimerConverterTest.java
+++ core/core-google-shortcuts/src/androidTest/java/androidx/core/google/shortcuts/converters/TimerConverterTest.java
@@ -29,7 +29,7 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
-import com.google.firebase.appindexing.Indexable;
+import com.google.android.gms.appindex.Indexable;
 
 import org.junit.Before;
 import org.junit.Test;
