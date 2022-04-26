809845bc5df0af02c56acd46373c092d71132eeb
paulmalolepsy <paulmalolepsy@google.com>
2022-04-26 11:53:34 -0700

    common added, with Preferences.kt moved over

---
+++ datastore/datastore-preferences-core/src/jvmMain/kotlin/androidx/datastore/preferences/core/Actual.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.datastore.preferences.core
+
+import java.util.Collections
+import java.util.concurrent.atomic.AtomicBoolean
+
+internal actual fun <K, V> immutableMap(map:Map<K, V>):Map<K, V>
+    = Collections.unmodifiableMap(map)
+
+internal actual fun <T> immutableSet(set:Set<T>):Set<T>
+    = Collections.unmodifiableSet(set)
+
+
+internal actual class AtomicBoolean actual constructor(initialValue: Boolean) {
+    private val delegate:java.util.concurrent.atomic.AtomicBoolean
+
+    actual fun set(value: Boolean) = delegate.set(value)
+
+    actual fun get(): Boolean = delegate.get()
+
+    init {
+        delegate = AtomicBoolean(initialValue)
+    }
+}
