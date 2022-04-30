5ded69636645e2ea50a595303fa00c619ce5def3
Yigit Boyar <yboyar@google.com>
2022-04-29 22:05:25 -0700

    Playground Setup Cleanup
    This CL updates the setup playground script to include verification
    files and also makes playground projects re-link the shared gradle
    folder. This way, any changes in that folder will reflect to all
    playground projects.
    
    I've also added a new script to update all playground projects.
    
    Bug: n/a
    Test: github CI passes
    
    remove version catalog creation from playground plugin
    
    increase metaspace
    
    Update androidx-shared.properties
    
    Update androidx-shared.properties

---
+++ playground-common/update-playground-setups.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Helper script to update all playground setups when we make changes
+# on how playground works (in other words, when we update setup-playground.sh).
+function absPath {
+    python3 -c "import os.path; print(os.path.abspath('$1'))"
+}
+
+WORKING_DIR=$(pwd)
+PLAYGROUND_REL_PATH=$(dirname $0)
+SUPPORT_ROOT_ABS_PATH=$(absPath "$PLAYGROUND_REL_PATH/..")
+PLAYGROUND_ABS_PATH=$(absPath $PLAYGROUND_REL_PATH)
+
+function setupPlayground {
+    echo "setting up playground in $1"
+    cd $1
+    $PLAYGROUND_ABS_PATH/setup-playground.sh
+    echo "finished setting up playground in $1"
+}
+
+PLAYGROUND_SETTINGS_FILES=$(egrep -lir --include=settings.gradle "setupPlayground" $SUPPORT_ROOT_ABS_PATH)
+for SETTINGS_FILE in $PLAYGROUND_SETTINGS_FILES
+do
+    PROJECT_DIR=$(absPath $(dirname $SETTINGS_FILE))
+    setupPlayground $PROJECT_DIR
+done
+cd $WORKING_DIR
