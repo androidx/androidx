f764f3d820ce234950a2f5170b57466d41042178
Leo Huang <leohuang@google.com>
2022-05-10 04:06:05 +0000

    Revert "Fix Async pause behavior of androidx.camera.video.Recorder"
    This reverts commit 67eac559f4ec0157888e621605f136d4f9a5c416.
    
    Reason for revert: Unit test dashboard failure rate increment approaches release build cutoff
    
    Change-Id: I272221daf48c7223b85843da16c66b40c0d13bb2

--- camera/camera-video/src/main/java/androidx/camera/video/Recorder.java
+++ camera/camera-video/src/main/java/androidx/camera/video/Recorder.java
@@ -377,10 +377,6 @@ public final class Recorder implements VideoOutput {
     @SuppressWarnings("WeakerAccess") /* synthetic accessor */
     SourceState mSourceState = SourceState.INACTIVE;
     private ScheduledFuture<?> mSourceNonStreamingTimeout = null;
-    @SuppressWarnings("WeakerAccess") /* synthetic accessor */
-    boolean mAudioPaused = false;
-    @SuppressWarnings("WeakerAccess") /* synthetic accessor */
-    boolean mVideoPaused = false;
     //--------------------------------------------------------------------------------------------//
 
     Recorder(@Nullable Executor executor, @NonNull MediaSpec mediaSpec,
@@ -1601,21 +1597,13 @@ public final class Recorder implements VideoOutput {
                         @ExecutedBy("mSequentialExecutor")
                         @Override
                         public void onEncodeStart() {
-                            mVideoPaused = false;
+                            // No-op.
                         }
 
                         @ExecutedBy("mSequentialExecutor")
                         @Override
                         public void onEncodeStop() {
                             completer.set(null);
-                        }
-
-                        @Override
-                        public void onEncodePaused() {
-                            mVideoPaused = true;
-                            if (!isAudioEnabled() || (isAudioEnabled() && mAudioPaused)) {
-                                onRecordingPaused();
-                            }
                         }
 
                         @ExecutedBy("mSequentialExecutor")
@@ -1704,21 +1692,13 @@ public final class Recorder implements VideoOutput {
                             @ExecutedBy("mSequentialExecutor")
                             @Override
                             public void onEncodeStart() {
-                                mAudioPaused = false;
+                                // No-op.
                             }
 
                             @ExecutedBy("mSequentialExecutor")
                             @Override
                             public void onEncodeStop() {
                                 completer.set(null);
-                            }
-
-                            @Override
-                            public void onEncodePaused() {
-                                mAudioPaused = true;
-                                if (mVideoPaused) {
-                                    onRecordingPaused();
-                                }
                             }
 
                             @ExecutedBy("mSequentialExecutor")
@@ -1871,17 +1851,12 @@ public final class Recorder implements VideoOutput {
                 mAudioEncoder.pause();
             }
             mVideoEncoder.pause();
+
+            mInProgressRecording.updateVideoRecordEvent(VideoRecordEvent.pause(
+                    mInProgressRecording.getOutputOptions(),
+                    getInProgressRecordingStats()));
         }
     }
-
-    @SuppressWarnings("WeakerAccess") /* synthetic accessor */
-    @ExecutedBy("mSequentialExecutor")
-    void onRecordingPaused() {
-        mInProgressRecording.updateVideoRecordEvent(VideoRecordEvent.pause(
-                mInProgressRecording.getOutputOptions(),
-                getInProgressRecordingStats()));
-    }
-
 
     @ExecutedBy("mSequentialExecutor")
     private void resumeInternal(@NonNull RecordingRecord recordingToResume) {
@@ -1889,10 +1864,8 @@ public final class Recorder implements VideoOutput {
         if (mInProgressRecording == recordingToResume && !mInProgressRecordingStopping) {
             if (isAudioEnabled()) {
                 mAudioEncoder.start();
-                mAudioPaused = false;
             }
             mVideoEncoder.start();
-            mVideoPaused = false;
 
             mInProgressRecording.updateVideoRecordEvent(VideoRecordEvent.resume(
                     mInProgressRecording.getOutputOptions(),
