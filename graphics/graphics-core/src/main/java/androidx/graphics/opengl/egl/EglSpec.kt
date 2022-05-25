dad76653066e1452aca6f22adf9c8b74cf00ab39
Nader Jawad <njawad@google.com>
2022-05-24 17:44:36 -0700

    Expose EGLExt methods on EglSpec
    Relnote: "Updated EglSpec interface to expose
    recently introduced methods on EGLExt.
    EglSpec simplifies the usages of these APIs by
    always leveraging the default eglDisplay
    to be consistent with other APIs used within
    EglManager/EglSpec."
    
    Fixes: 233813913
    Test: Updated EglManagerTest to leverage EglSpec
    Change-Id: I72ea40c5d37b40a771a4b4a22f21aab4ebb3a3c7

--- graphics/graphics-core/src/main/java/androidx/graphics/opengl/egl/EglSpec.kt
+++ graphics/graphics-core/src/main/java/androidx/graphics/opengl/egl/EglSpec.kt
@@ -16,11 +16,26 @@
 
 package androidx.graphics.opengl.egl
 
+import android.hardware.HardwareBuffer
 import android.opengl.EGL14
 import android.opengl.EGLConfig
 import android.opengl.EGLContext
 import android.opengl.EGLSurface
+import android.os.Build
 import android.view.Surface
+import androidx.annotation.RequiresApi
+import androidx.hardware.SyncFenceCompat
+import androidx.opengl.EGLExt
+import androidx.opengl.EGLExt.Companion.EGLClientWaitResult
+import androidx.opengl.EGLExt.Companion.EGLSyncAttribute
+import androidx.opengl.EGLExt.Companion.EGL_CONDITION_SATISFIED_KHR
+import androidx.opengl.EGLExt.Companion.EGL_FALSE
+import androidx.opengl.EGLExt.Companion.EGL_FOREVER_KHR
+import androidx.opengl.EGLExt.Companion.EGL_SYNC_FLUSH_COMMANDS_BIT_KHR
+import androidx.opengl.EGLExt.Companion.EGL_TIMEOUT_EXPIRED_KHR
+import androidx.opengl.EGLExt.Companion.eglClientWaitSyncKHR
+import androidx.opengl.EGLImageKHR
+import androidx.opengl.EGLSyncKHR
 
 /**
  * Interface for accessing various EGL facilities independent of EGL versions.
@@ -79,7 +94,7 @@ interface EglSpec {
      * buffers of that pbuffer bound to a texture object have been released. Deferral of
      * surface destruction would still return true as deferral does not indicate a failure condition
      *
-     * @return True if destruction of the EGLSurface was successful, false otherwise
+     * @return `true` if destruction of the EGLSurface was successful, false otherwise
      */
     fun eglDestroySurface(surface: EGLSurface): Boolean
 
@@ -155,7 +170,7 @@ interface EglSpec {
      *
      * @param surface Specifies the EGL drawing surface whose buffers are to be swapped
      *
-     * @return True if swapping of buffers succeeds, false otherwise
+     * @return `true` if swapping of buffers succeeds, false otherwise
      */
     fun eglSwapBuffers(surface: EGLSurface): Boolean
 
@@ -167,7 +182,7 @@ interface EglSpec {
      * @param result Int array to store the result of the query
      * @param offset Index within [result] to store the value of the queried attribute
      *
-     * @return True if the query was completed successfully, false otherwise. If the query
+     * @return `true` if the query was completed successfully, false otherwise. If the query
      * fails, [result] is unmodified
      */
     fun eglQuerySurface(surface: EGLSurface, attribute: Int, result: IntArray, offset: Int): Boolean
@@ -189,6 +204,168 @@ interface EglSpec {
      * error code obtained from [EglSpec.eglGetError]
      */
     fun getErrorMessage(): String = getStatusString(eglGetError())
+
+    /**
+     * Creates an EGLImage from the provided [HardwareBuffer]. This handles
+     * internally creating an EGLClientBuffer and an [EGLImageKHR] from the client buffer.
+     *
+     * @param hardwareBuffer Backing [HardwareBuffer] for the generated EGLImage instance
+     *
+     * @return an [EGLImageKHR] instance representing the [EGLImageKHR] created from the
+     * HardwareBuffer. Because this is created internally through EGL's eglCreateImageKR method,
+     * this has the KHR suffix.
+     */
+    @Suppress("AcronymName")
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun eglCreateImageFromHardwareBuffer(hardwareBuffer: HardwareBuffer): EGLImageKHR?
+
+    /**
+     * Destroy the given [EGLImageKHR] instance. Once destroyed, the image may not be used to
+     * create any additional [EGLImageKHR] target resources within any client API contexts,
+     * although existing [EGLImageKHR] siblings may continue to be used. `true` is returned
+     * if DestroyImageKHR succeeds, `false` indicates failure. This can return `false` if the
+     * corresponding [EGLContext] is not valid.
+     *
+     * See: https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_image_base.txt
+     *
+     * @param image EGLImageKHR to be destroyed
+     *
+     * @return `true` if the destruction of the EGLImageKHR object was successful, `false` otherwise
+     */
+    @Suppress("AcronymName")
+    fun eglDestroyImageKHR(image: EGLImageKHR): Boolean
+
+    /**
+     * Creates a sync object of the specified type associated with the
+     * specified display, and returns a handle to the new object.
+     * The configuration of the returned [EGLSyncKHR] object is specified by the provided
+     * attributes.
+     *
+     * Consumers should ensure that the EGL_KHR_fence_sync EGL extension is supported before
+     * invoking this method otherwise a null EGLSyncFenceKHR object is returned.
+     *
+     * See: https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_fence_sync.txt
+     *
+     * @param type Indicates the type of sync object that is returned
+     * @param attributes Specifies the configuration of the sync object returned
+     *
+     * @return the [EGLSyncKHR] object to be used as a fence or null if this extension
+     * is not supported
+     */
+    @Suppress("AcronymName")
+    fun eglCreateSyncKHR(type: Int, attributes: EglConfigAttributes?): EGLSyncKHR?
+
+    /**
+     * Query attributes of the provided sync object. Accepted attributes to query depend
+     * on the type of sync object. If no errors are generated, this returns true and the
+     * value of the queried attribute is stored in the value array at the offset position.
+     * If this method returns false, the provided value array is unmodified.
+     *
+     * See: https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_fence_sync.txt
+     *
+     * @param sync EGLSyncKHR object to query attributes
+     * @param attribute Corresponding EGLSyncKHR attribute to query on [sync]
+     * @param value Integer array used to store the result of the query
+     * @param offset Index within the value array to store the result of the attribute query
+     *
+     * @return `true` if the attribute was queried successfully, false otherwise. Failure cases
+     * include attempting to call this method on an invalid sync object, or the display provided
+     * not matching the display that was used to create this sync object. Additionally if the
+     * queried attribute is not supported for the sync object, false is returned.
+     */
+    @Suppress("AcronymName")
+    fun eglGetSyncAttribKHR(
+        sync: EGLSyncKHR,
+        @EGLSyncAttribute attribute: Int,
+        value: IntArray,
+        offset: Int
+    ): Boolean
+
+    /**
+     * Destroys the given sync object associated with the specified display
+     *
+     * Consumers should ensure that the EGL_KHR_fence_sync EGL extension is supported before
+     * invoking this method otherwise a null EGLSyncFenceKHR object is returned.
+     * See: https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_fence_sync.txt
+     *
+     * @param sync Fence object to be destroyed
+     *
+     * @return `true` if the [EGLSyncKHR] object was destroyed successfully `false` otherwise. This
+     * can return `false` if the sync object is not a valid sync object for the provided display
+     * or if the display provided in this method does not match the display used to create this
+     * sync in [eglCreateSyncKHR].
+     */
+    @Suppress("AcronymName")
+    fun eglDestroySyncKHR(sync: EGLSyncKHR): Boolean
+
+    /**
+     * Creates a native synchronization fence referenced through a file descriptor
+     * that is associated with an EGL fence sync object.
+     *
+     * See:
+     * https://www.khronos.org/registry/EGL/extensions/ANDROID/EGL_ANDROID_native_fence_sync.txt
+     *
+     * @param sync The [EGLSyncKHR] to fetch the [SyncFenceCompat] from
+     * @return A [SyncFenceCompat] representing the native fence.
+     *  If [sync] is not a valid sync object for display, an invalid [SyncFenceCompat]
+     *  instance is returned and an EGL_BAD_PARAMETER error is generated.
+     *  If the EGL_SYNC_NATIVE_FENCE_FD_ANDROID attribute of [sync] is
+     *  EGL_NO_NATIVE_FENCE_FD_ANDROID, an invalid [SyncFenceCompat] is
+     *  returned and an EGL_BAD_PARAMETER error is generated.
+     *  If the display does not match the display passed to [eglCreateSyncKHR]
+     *  when [sync] was created, the behavior is undefined.
+     */
+    @Suppress("AcronymName")
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun eglDupNativeFenceFDANDROID(sync: EGLSyncKHR): SyncFenceCompat
+
+    /**
+     * Blocks the calling thread until the specified sync object is signalled or until
+     * [timeoutNanos] nanoseconds have passed.
+     * More than one [eglClientWaitSyncKHR] may be outstanding on the same [sync] at any given
+     * time. When there are multiple threads blocked on the same [sync] and the [sync] object
+     * has signalled, all such threads are released, but the order in which they are released is
+     * not defined.
+     *
+     * If the value of [timeoutNanos] is zero, then [eglClientWaitSyncKHR] simply tests the
+     * current status of sync. If the value of [timeoutNanos] is the special value
+     * [EGL_FOREVER_KHR], then [eglClientWaitSyncKHR] does not time out. For all other values,
+     * [timeoutNanos] is adjusted to the closest value allowed by the implementation-dependent
+     * timeout accuracy, which may be substantially longer than one nanosecond.
+     *
+     * [eglClientWaitSyncKHR] returns one of three status values describing the reason for
+     * returning. A return value of [EGL_TIMEOUT_EXPIRED_KHR] indicates that the specified
+     * timeout period expired before [sync] was signalled, or if [timeoutNanos] is zero,
+     * indicates that [sync] is not signaled. A return value of [EGL_CONDITION_SATISFIED_KHR]
+     * indicates that [sync] was signaled before the timeout expired, which includes the case
+     * when [sync] was already signaled when [eglClientWaitSyncKHR] was called. If an error
+     * occurs then an error is generated and [EGL_FALSE] is returned.
+     *
+     * If the sync object being blocked upon will not be signaled in finite time (for example
+     * by an associated fence command issued previously, but not yet flushed to the graphics
+     * pipeline), then [eglClientWaitSyncKHR] may wait forever. To help prevent this behavior,
+     * if the [EGL_SYNC_FLUSH_COMMANDS_BIT_KHR] is set on the flags parameter and the [sync] is
+     * unsignaled when [eglClientWaitSyncKHR] is called, then the equivalent flush will be
+     * performed for the current EGL context before blocking on sync. If no context is
+     * current bound for the API, the [EGL_SYNC_FLUSH_COMMANDS_BIT_KHR] bit is ignored.
+     *
+     * @param sync EGLSyncKHR object to wait on
+     * @param flags Optional flags to provide to handle flushing of pending commands
+     * @param timeoutNanos Optional timeout value to wait before this method returns, measured
+     * in nanoseconds. This value is always consumed as an unsigned long value so even negative
+     * values will be converted to their unsigned equivalent.
+     *
+     * @return Result code indicating the status of the wait request. Either
+     * [EGL_CONDITION_SATISFIED_KHR], if the sync did signal within the specified timeout,
+     * [EGL_TIMEOUT_EXPIRED_KHR] if the sync did not signal within the specified timeout,
+     * or [EGL_FALSE] if an error occurs.
+     */
+    @Suppress("AcronymName")
+    fun eglClientWaitSyncKHR(
+        sync: EGLSyncKHR,
+        flags: Int,
+        timeoutNanos: Long
+    ): @EGLClientWaitResult Int
 
     companion object {
 
@@ -312,7 +489,44 @@ interface EglSpec {
                 }
             }
 
+            @RequiresApi(Build.VERSION_CODES.Q)
+            override fun eglCreateImageFromHardwareBuffer(
+                hardwareBuffer: HardwareBuffer
+            ): EGLImageKHR? =
+                EGLExt.eglCreateImageFromHardwareBuffer(getDefaultDisplay(), hardwareBuffer)
+
+            override fun eglDestroyImageKHR(image: EGLImageKHR): Boolean =
+                EGLExt.eglDestroyImageKHR(getDefaultDisplay(), image)
+
+            override fun eglCreateSyncKHR(
+                type: Int,
+                attributes: EglConfigAttributes?
+            ): EGLSyncKHR? =
+                EGLExt.eglCreateSyncKHR(getDefaultDisplay(), type, attributes)
+
+            override fun eglGetSyncAttribKHR(
+                sync: EGLSyncKHR,
+                attribute: Int,
+                value: IntArray,
+                offset: Int
+            ): Boolean =
+                EGLExt.eglGetSyncAttribKHR(getDefaultDisplay(), sync, attribute, value, offset)
+
+            override fun eglDestroySyncKHR(sync: EGLSyncKHR): Boolean =
+                EGLExt.eglDestroySyncKHR(getDefaultDisplay(), sync)
+
             override fun eglGetError(): Int = EGL14.eglGetError()
+
+            @RequiresApi(Build.VERSION_CODES.KITKAT)
+            override fun eglDupNativeFenceFDANDROID(sync: EGLSyncKHR): SyncFenceCompat =
+                EGLExt.eglDupNativeFenceFDANDROID(getDefaultDisplay(), sync)
+
+            override fun eglClientWaitSyncKHR(
+                sync: EGLSyncKHR,
+                flags: Int,
+                timeoutNanos: Long
+            ): Int =
+                EGLExt.eglClientWaitSyncKHR(getDefaultDisplay(), sync, flags, timeoutNanos)
 
             private fun getDefaultDisplay() = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY)
 
