cd8b8cc73a68e754117cf950d9868a34da82209a
Alan Viverette <alanv@google.com>
2022-06-17 14:43:53 -0400

    Avoid overwriting uiMode on activity or application context
    Fixes an issue where our context wrapper reused the application
    context's backing resource implementation.
    
    We now eagerly use the base context's resources when we don't have
    any configuration override, which avoids reusing the application's
    resources.
    
    As a side-effect, we now get configuration updates that are (correctly)
    missing our overrides. To counteract this, we let the delegate re-apply
    overrides and then we replace the configuration update contents with the
    desired overridden configuration contents.
    
    Bug: 227502879
    Fixes: 236394773
    Test: FilternatorTest
    Test: NavDrawerActivityTest
    Test: entire AppCompat test suite
    Change-Id: Idf9d51987fdf953bcc892970f625f451a07b0d3d

---
+++ appcompat/appcompat/src/androidTest/java/androidx/appcompat/app/g3/FilternatorTestWithCustomDefault.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appcompat.app.g3
+
+import androidx.appcompat.Orientation
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.withOrientation
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.testutils.LifecycleOwnerUtils.waitUntilState
+import androidx.testutils.withActivity
+import com.google.common.truth.Truth.assertThat
+import java.util.concurrent.TimeUnit
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Regression test for b/236394773, adapted from GmsCore's own tests. The activity used for this
+ * test has been modified to always call AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES).
+ *
+ * The primary purpose of this test is to ensure that the application configuration is not
+ * accidentally modified when we modify the activity configuration.
+ */
+@Suppress("SameParameterValue")
+@LargeTest
+@SdkSuppress(minSdkVersion = 18) // UiDevice
+@RunWith(AndroidJUnit4::class)
+class FilternatorTestWithCustomDefault {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(FilternatorActivityWithCustomDefault::class.java)
+
+    private lateinit var uiDevice: UiDevice
+
+    @Before
+    fun setup() {
+        uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+    }
+
+    @After
+    fun teardown() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+    }
+
+    @Test
+    fun testConfigurationUpdatedOnLandscapeMode() {
+        // Wait for the activity to fully start before rotating,
+        // otherwise we won't receive onConfigurationChanged.
+        val activity = activityRule.withActivity { this }
+        waitUntilState(activity, Lifecycle.State.RESUMED)
+
+        // Rotate and wait for the activity to check that
+        // configuration has been properly updated.
+        uiDevice.withOrientation(Orientation.LEFT) {
+            FilternatorActivity.configurationLatch.await(5000, TimeUnit.MILLISECONDS)
+            assertThat(FilternatorActivity.configurationException).isNull()
+        }
+    }
+}
--- appcompat/appcompat/src/main/java/androidx/appcompat/app/AppCompatActivity.java
+++ appcompat/appcompat/src/main/java/androidx/appcompat/app/AppCompatActivity.java
@@ -226,14 +226,16 @@ public class AppCompatActivity extends FragmentActivity implements AppCompatCall
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
-        if (mResources != null) {
-            // The real (and thus managed) resources object was already updated
-            // by ResourcesManager, so pull the current metrics from there.
-            final DisplayMetrics newMetrics = super.getResources().getDisplayMetrics();
-            mResources.updateConfiguration(newConfig, newMetrics);
-        }
-
+        // The delegate may modify the real resources object or the config param to implement its
+        // desired configuration overrides. Let it do it's thing and then use the resulting state.
         getDelegate().onConfigurationChanged(newConfig);
+
+        // Manually propagate configuration changes to our unmanaged resources object.
+        if (mResources != null) {
+            final Configuration currConfig = super.getResources().getConfiguration();
+            final DisplayMetrics currMetrics = super.getResources().getDisplayMetrics();
+            mResources.updateConfiguration(currConfig, currMetrics);
+        }
     }
 
     @Override
