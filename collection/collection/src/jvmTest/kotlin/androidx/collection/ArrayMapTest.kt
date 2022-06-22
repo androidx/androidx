8509651c90d4cc659af5f20743444e2c0e2516d1
Arkadii Ivanov <arkivanov@google.com>
2022-06-22 16:58:06 +0100

    Converted ArrayMapTest to Kotlin
    Test: ArrayMapTest
    Bug: 230860333
    Change-Id: Ic318ed04b519b132f13901b28d1d642a507ddf9a

--- collection/collection/src/jvmTest/kotlin/androidx/collection/ArrayMapTest.kt
+++ collection/collection/src/jvmTest/kotlin/androidx/collection/ArrayMapTest.kt
@@ -13,163 +13,155 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.collection
 
-package androidx.collection;
+import androidx.collection.internal.Lock
+import androidx.collection.internal.synchronized
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.runBlocking
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+internal class ArrayMapTest {
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-import java.util.Arrays;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Map;
-
-@RunWith(JUnit4.class)
-public class ArrayMapTest {
-    ArrayMap<String, String> mMap = new ArrayMap<>();
+    private val map = ArrayMap<String, String>()
 
     /**
      * Attempt to generate a ConcurrentModificationException in ArrayMap.
-     * <p>
+     *
      * ArrayMap is explicitly documented to be non-thread-safe, yet it's easy to accidentally screw
      * this up; ArrayMap should (in the spirit of the core Java collection types) make an effort to
      * catch this and throw ConcurrentModificationException instead of crashing somewhere in its
      * internals.
      */
+    @OptIn(DelicateCoroutinesApi::class) // newFixedThreadPoolContext is delicate in jvm
     @Test
-    public void testConcurrentModificationException() throws Exception {
-        final int testLenMs = 5000;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i = 0;
-                while (mMap != null) {
+    fun testConcurrentModificationException() {
+        var error: Throwable? = null
+        val nThreads = 20
+        var nActiveThreads = 0
+        val lock = Lock()
+        val dispatcher = newFixedThreadPoolContext(nThreads = nThreads, name = "ArraySetTest")
+        val scope = CoroutineScope(dispatcher)
+
+        repeat(nThreads) {
+            scope.launch {
+                lock.synchronized { nActiveThreads++ }
+
+                while (isActive) {
+                    val add = Random.nextBoolean()
                     try {
-                        mMap.put(String.format("key %d", i++), "B_DONT_DO_THAT");
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        fail(e.getMessage());
-                    } catch (ClassCastException e) {
-                        fail(e.getMessage());
-                    } catch (ConcurrentModificationException e) {
-                        System.out.println("[successfully caught CME at put #" + i
-                                + " size=" + (mMap == null ? "??" : String.valueOf(mMap.size()))
-                                + "]");
+                        if (add) {
+                            map[Random.nextLong().toString()] = "B_DONT_DO_THAT"
+                        } else {
+                            map.clear()
+                        }
+                    } catch (e: IndexOutOfBoundsException) {
+                        if (!add) {
+                            error = e
+                            throw e
+                        } // Expected exception otherwise
+                    } catch (e: ClassCastException) {
+                        error = e
+                        throw e
+                    } catch (ignored: ConcurrentModificationException) {
+                        // Expected exception
                     }
                 }
             }
-        }).start();
-        for (int i = 0; i < (testLenMs / 100); i++) {
-            try {
-                Thread.sleep(100);
-                mMap.clear();
-            } catch (InterruptedException e) {
-            } catch (ArrayIndexOutOfBoundsException e) {
-                fail(e.getMessage());
-            } catch (ClassCastException e) {
-                fail(e.getMessage());
-            } catch (ConcurrentModificationException e) {
-                System.out.println(
-                        "[successfully caught CME at clear #"
-                                + i + " size=" + mMap.size() + "]");
-            }
         }
-        mMap = null; // will stop other thread
-        System.out.println();
+
+        runBlocking(Dispatchers.Default) {
+            // Wait until all worker threads are started
+            for (i in 0 until 100) {
+                if (lock.synchronized { nActiveThreads == nThreads }) {
+                    break
+                } else {
+                    delay(timeMillis = 10L)
+                }
+            }
+
+            // Allow the worker threads to run concurrently for some time
+            delay(timeMillis = 100L)
+        }
+
+        scope.cancel()
+        dispatcher.close()
+
+        error?.also { throw it }
     }
 
     /**
      * Check to make sure the same operations behave as expected in a single thread.
      */
     @Test
-    public void testNonConcurrentAccesses() throws Exception {
-        for (int i = 0; i < 100000; i++) {
-            try {
-                mMap.put(String.format("key %d", i++), "B_DONT_DO_THAT");
-                if (i % 200 == 0) {
-                    System.out.print(".");
-                }
-                if (i % 500 == 0) {
-                    mMap.clear();
-                    System.out.print("X");
-                }
-            } catch (ConcurrentModificationException e) {
-                fail(e.getMessage());
+    fun testNonConcurrentAccesses() {
+        repeat(100_000) { i ->
+            map["key %i"] = "B_DONT_DO_THAT"
+            if (i % 500 == 0) {
+                map.clear()
             }
         }
     }
 
     @Test
-    public void testIsSubclassOfSimpleArrayMap() {
-        Object map = new ArrayMap<String, Integer>();
-        assertTrue(map instanceof SimpleArrayMap);
+    fun testIsSubclassOfSimpleArrayMap() {
+        @Suppress("USELESS_IS_CHECK")
+        assertTrue(map is SimpleArrayMap<*, *>)
     }
 
     /**
      * Regression test for ensure capacity: b/224971154
      */
     @Test
-    public void putAll() {
-        ArrayMap<String, String> map = new ArrayMap<>();
-        Map<String, String> otherMap = new HashMap<>();
-        otherMap.put("abc", "def");
-        map.putAll(otherMap);
-        assertEquals(map.size(), 1);
-        assertEquals(map.keyAt(0), "abc");
-        assertEquals(map.valueAt(0), "def");
+    fun putAll() {
+        val otherMap = HashMap<String, String>()
+        otherMap["abc"] = "def"
+        map.putAll(otherMap)
+        assertEquals(1, map.size)
+        assertEquals("abc", map.keyAt(0))
+        assertEquals("def", map.valueAt(0))
     }
 
     @Test
-    public void toArray() {
-        Map<String, Integer> map = new ArrayMap<>();
-        map.put("a", 1);
-        String[] keys = map.keySet().toArray(new String[1]);
-        Integer[] values = map.values().toArray(new Integer[1]);
+    fun keys() {
+        val map = ArrayMap<String, Int>()
 
-        assertEquals(Arrays.toString(keys), "[a]");
-        assertEquals(Arrays.toString(values), "[1]");
+        map["a"] = 1
+        assertEquals(map.keys, setOf("a"))
 
-        // re-do again, it should re-use the same arrays
-        String[] keys2 = map.keySet().toArray(keys);
-        Integer[] values2 = map.values().toArray(values);
+        map["b"] = 2
+        map["c"] = 3
+        assertEquals(map.keys, setOf("a", "b", "c"))
 
-        assertSame(keys2, keys);
-        assertSame(values2, values);
-        assertEquals(Arrays.toString(keys2), "[a]");
-        assertEquals(Arrays.toString(values2), "[1]");
+        map.remove("b")
+        map.remove("c")
+        assertEquals(map.keys, setOf("a"))
+    }
 
-        // add new items
-        map.put("b", 2);
-        map.put("c", 3);
+    @Test
+    fun values() {
+        val map = ArrayMap<String, Int>()
 
-        // now it shouldn't re-use arrays because arrays are too small
-        String[] keys3 = map.keySet().toArray(keys);
-        Integer[] values3 = map.values().toArray(values);
+        map["a"] = 1
+        assertContentEquals(map.values, listOf(1))
 
-        assertNotSame(values3, values);
-        assertNotSame(keys3, keys);
-        assertEquals(Arrays.toString(keys3), "[a, b, c]");
-        assertEquals(Arrays.toString(values3), "[1, 2, 3]");
+        map["b"] = 2
+        map["c"] = 3
+        assertContentEquals(map.values, listOf(1, 2, 3))
 
-
-        map.remove("b");
-        map.remove("c");
-
-        // arrays are big enough, re-use
-        String[] keys4 = map.keySet().toArray(keys3);
-        Integer[] values4 = map.values().toArray(values3);
-
-        assertSame(values4, values3);
-        assertSame(keys4, keys3);
-        // https://docs.oracle.com/javase/8/docs/api/java/util/Set.html#toArray-T:A-
-        // only the next index is nulled in the array, per documentation.
-        assertEquals(Arrays.toString(keys4), "[a, null, c]");
-        assertEquals(Arrays.toString(values4), "[1, null, 3]");
+        map.remove("b")
+        map.remove("c")
+        assertContentEquals(map.values, listOf(1))
     }
 }
