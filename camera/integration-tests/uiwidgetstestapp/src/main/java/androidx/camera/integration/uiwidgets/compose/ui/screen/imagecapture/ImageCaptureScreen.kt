0b7097d3a641ece57a1fd8ad7be95c0b920d8df8
Terence Ho <terenceho@google.com>
2022-06-22 09:40:06 +0000

    Implement ImageCaptureScreen
    Add ImageCaptureScreenStateHolder to handle the states, use cases and
    CameraX implementation details. Update ImageCaptureScreen Composable to
    display PreviewView and CameraControl buttons.
    
    Add Flash for ImageCapture
    
    The State Holder will check if the Camera supports flash. If it does
    not, a placeholder will be rendered. If flash is supported, a toggle is provided to switch the flash modes.
    The State Holder will also decide which icon to render based on the
    flash mode.
    
    Bug: 234080273
    Test: n/a
    Change-Id: I23c6e861a97f39947e86adb005a1b5497e6bb241

--- camera/integration-tests/uiwidgetstestapp/src/main/java/androidx/camera/integration/uiwidgets/compose/ui/screen/imagecapture/ImageCaptureScreen.kt
+++ camera/integration-tests/uiwidgetstestapp/src/main/java/androidx/camera/integration/uiwidgets/compose/ui/screen/imagecapture/ImageCaptureScreen.kt
@@ -16,10 +16,91 @@
 
 package androidx.camera.integration.uiwidgets.compose.ui.screen.imagecapture
 
-import androidx.compose.material.Text
+import android.view.ViewGroup
+import androidx.camera.integration.uiwidgets.compose.ui.screen.components.CameraControlButton
+import androidx.camera.integration.uiwidgets.compose.ui.screen.components.CameraControlButtonPlaceholder
+import androidx.camera.integration.uiwidgets.compose.ui.screen.components.CameraControlRow
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.FlipCameraAndroid
+import androidx.compose.material.icons.sharp.Lens
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun ImageCaptureScreen() {
-    Text("Image Capture Screen")
+fun ImageCaptureScreen(
+    modifier: Modifier = Modifier,
+    stateHolder: ImageCaptureScreenStateHolder = rememberImageCaptureScreenStateHolder()
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val localContext = LocalContext.current
+    val previewView = remember {
+        PreviewView(localContext).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
+            stateHolder.setSurfaceProvider(this.surfaceProvider)
+        }
+    }
+
+    LaunchedEffect(key1 = stateHolder.lensFacing) {
+        stateHolder.startCamera(context = localContext, lifecycleOwner = lifecycleOwner)
+    }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        AndroidView(
+            factory = {
+                previewView
+            }
+        )
+
+        CameraControlRow(modifier = Modifier.align(Alignment.BottomCenter)) {
+            CameraControlButton(
+                imageVector = Icons.Sharp.FlipCameraAndroid,
+                contentDescription = "Toggle Camera Lens",
+            ) {
+                stateHolder.toggleLensFacing()
+            }
+
+            CameraControlButton(
+                imageVector = Icons.Sharp.Lens,
+                contentDescription = "Image Capture",
+                modifier = Modifier
+                    .padding(1.dp)
+                    .border(1.dp, MaterialTheme.colors.onSecondary, CircleShape)
+            ) {
+                stateHolder.takePhoto(localContext)
+            }
+
+            if (stateHolder.hasFlashMode) {
+                CameraControlButton(
+                    imageVector = stateHolder.flashModeIcon,
+                    contentDescription = "Toggle Flash Mode",
+                    modifier = Modifier
+                        .padding(1.dp)
+                        .border(1.dp, MaterialTheme.colors.onSecondary, RectangleShape)
+                ) {
+                    stateHolder.toggleFlashMode()
+                }
+            } else {
+                CameraControlButtonPlaceholder()
+            }
+        }
+    }
 }
