c8e3ec7b7c854b38761ce79d2762af3b691b81bf
Daniel Peykov <peykov@google.com>
2022-06-23 18:17:01 +0000

    Simplify and fix lint issues in UiDeviceTests.java
    * Extract common logic into setUp/tearDown.
    * Refactor into N and M test cases and use SdkSuppress to differentiate.
    
    Bug: 236497717
    Test: ./gradlew :test:uiautomator:uiautomator:lint
    Change-Id: I43f73b1b52d1da8d88a0abcd857c487b38be0122

--- test/uiautomator/uiautomator/src/androidTest/java/androidx/test/uiautomator/UiDeviceTests.java
+++ test/uiautomator/uiautomator/src/androidTest/java/androidx/test/uiautomator/UiDeviceTests.java
@@ -16,80 +16,70 @@
 
 package androidx.test.uiautomator;
 
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import android.app.Instrumentation;
 import android.app.UiAutomation;
 import android.os.Build;
 
+import androidx.test.filters.SdkSuppress;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class UiDeviceTests {
 
-    @Test
-    public void testGetUiAutomation_withDefaultFlags() {
-        // Get the default flags
-        final int defaultFlags = Configurator.getInstance().getUiAutomationFlags();
+    private Instrumentation mMockInstrumentation;
+    private UiDevice mDevice;
+    private int mDefaultFlags;
 
-        // Setup mocks
-        Instrumentation mockInstrumentation = Mockito.mock(Instrumentation.class);
-
-        // Can't mock UiAutomation, so return a real instance
+    @Before
+    public void setUp() {
+        mMockInstrumentation = mock(Instrumentation.class);
         UiAutomation automation = InstrumentationRegistry.getInstrumentation().getUiAutomation();
-        doReturn(automation).when(mockInstrumentation).getUiAutomation(anyInt());
-        doReturn(automation).when(mockInstrumentation).getUiAutomation();
-
-        // Get a UiDevice instance to test and use it to obtain a UiAutomation instance
-        UiDevice device = Mockito.spy(new UiDevice(mockInstrumentation));
-        UiAutomation uiAutomation = device.getUiAutomation();
-
-        // Verify that the UiAutomation instance was obtained with the right arguments
-        if (UiDevice.API_LEVEL_ACTUAL > Build.VERSION_CODES.M) {
-            // On N+, we should obtain a UiAutomation instance using the default flags
-            Mockito.verify(mockInstrumentation, atLeastOnce()).getUiAutomation(eq(defaultFlags));
-        } else {
-            // Prior to N, flags should be ignored
-            Mockito.verify(mockInstrumentation, atLeastOnce()).getUiAutomation();
+        doReturn(automation).when(mMockInstrumentation).getUiAutomation();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            doReturn(automation).when(mMockInstrumentation).getUiAutomation(anyInt());
         }
+        mDevice = new UiDevice(mMockInstrumentation);
+        mDefaultFlags = Configurator.getInstance().getUiAutomationFlags();
+    }
+
+    @After
+    public void tearDown() {
+        Configurator.getInstance().setUiAutomationFlags(mDefaultFlags);
     }
 
     @Test
+    @SdkSuppress(maxSdkVersion = Build.VERSION_CODES.M)
+    public void testGetUiAutomation_withoutFlags() {
+        mDevice.getUiAutomation();
+        // Verify that the UiAutomation instance was obtained without flags (prior to N).
+        verify(mMockInstrumentation, atLeastOnce()).getUiAutomation();
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.N)
+    public void testGetUiAutomation_withDefaultFlags() {
+        mDevice.getUiAutomation();
+        // Verify that the UiAutomation instance was obtained with default flags (N+).
+        verify(mMockInstrumentation, atLeastOnce()).getUiAutomation(eq(mDefaultFlags));
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.N)
     public void testGetUiAutomation_withCustomFlags() {
-        // Remember the default flags
-        final int defaultFlags = Configurator.getInstance().getUiAutomationFlags();
-
-        // Set some custom flags
-        final int flags = 5;
-        Configurator.getInstance().setUiAutomationFlags(flags);
-
-        // Setup mocks
-        Instrumentation mockInstrumentation = Mockito.mock(Instrumentation.class);
-
-        // Can't mock UiAutomation, so return a real instance
-        UiAutomation automation = InstrumentationRegistry.getInstrumentation().getUiAutomation();
-        doReturn(automation).when(mockInstrumentation).getUiAutomation(anyInt());
-        doReturn(automation).when(mockInstrumentation).getUiAutomation();
-
-        // Get a UiDevice instance to test and use it to obtain a UiAutomation instance
-        UiDevice device = Mockito.spy(new UiDevice(mockInstrumentation));
-        UiAutomation uiAutomation = device.getUiAutomation();
-
-        // Verify that the UiAutomation instance was obtained with the right arguments
-        if (UiDevice.API_LEVEL_ACTUAL > Build.VERSION_CODES.M) {
-            // On N+, we should obtain a UiAutomation instance using the custom flags
-            Mockito.verify(mockInstrumentation, atLeastOnce()).getUiAutomation(eq(flags));
-        } else {
-            // Prior to N, flags should be ignored
-            Mockito.verify(mockInstrumentation, atLeastOnce()).getUiAutomation();
-        }
-
-        // Reset the flags
-        Configurator.getInstance().setUiAutomationFlags(defaultFlags);
+        int customFlags = 5;
+        Configurator.getInstance().setUiAutomationFlags(customFlags);
+        mDevice.getUiAutomation();
+        // Verify that the UiAutomation instance was obtained with custom flags (N+).
+        verify(mMockInstrumentation, atLeastOnce()).getUiAutomation(eq(customFlags));
     }
 }
