c5d7599ea955bf40e8d57e7e6a964a237f89b178
David Saff <saff@google.com>
2022-07-11 15:28:09 -0400

    In integration tests, can apply AndroidXPlugin using plugin {} syntax
    Bug: 233089408
    Test: ./gradlew :buildSrc-tests:test --tests androidx.build.*
    
    Change-Id: I1e386bf34e8c6e2c25fb075fda5dadce5e3af5ea

--- buildSrc-tests/src/test/kotlin/androidx/build/buildFiles.kt
+++ buildSrc-tests/src/test/kotlin/androidx/build/buildFiles.kt
@@ -25,6 +25,7 @@ fun AndroidXPluginTestContext.writeBuildFiles(vararg projects: AndroidXSelfTestP
 fun AndroidXPluginTestContext.writeBuildFiles(projects: List<AndroidXSelfTestProject>) {
     writeRootSettingsFile(projects.map { it.gradlePath })
     writeRootBuildFile()
+    writeApplyPluginScript()
 
     File(supportRoot, "libraryversions.toml").writeText(
         """|[groups]
@@ -60,4 +61,17 @@ fun AndroidXPluginTestContext.writeRootSettingsFile(projectPaths: List<String>) 
         }
     }
     File(setup.rootDir, "settings.gradle").writeText(settingsString)
+}
+
+fun AndroidXPluginTestContext.writeApplyPluginScript() {
+    setup.rootDir.resolve("buildSrc/apply").also { it.mkdirs() }
+        .resolve("applyAndroidXImplPlugin.gradle").writeText(
+        """|import androidx.build.AndroidXImplPlugin
+           |buildscript {
+           |  ${setup.repositories}
+           |  $buildScriptDependencies
+           |}
+           |apply plugin: AndroidXImplPlugin
+           |""".trimMargin()
+    )
 }
