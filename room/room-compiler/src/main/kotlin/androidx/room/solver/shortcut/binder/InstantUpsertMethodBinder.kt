d45552645a81c99ed9c25cbba72c14b951c79ae0
yizhenz <yizhenz@google.com>
2022-07-15 20:37:33 +0000

    Creating the binders for upsert.
    Bug: 117855270
    Test: N/A
    Change-Id: I1f3330774951c09cbfec34c05beb3da4727c123e

--- room/room-compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/InstantUpsertMethodBinder.kt
+++ room/room-compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/InstantUpsertMethodBinder.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,42 +16,28 @@
 
 package androidx.room.solver.shortcut.binder
 
+import androidx.room.ext.N
 import androidx.room.solver.CodeGenScope
 import androidx.room.solver.shortcut.result.UpsertMethodAdapter
 import androidx.room.vo.ShortcutQueryParameter
+import androidx.room.writer.DaoWriter
 import com.squareup.javapoet.FieldSpec
 import com.squareup.javapoet.TypeSpec
-/**
- * Connects the upsert method, the database and the [UpsertMethodAdapter].
- */
-abstract class UpsertMethodBinder(val adapter: UpsertMethodAdapter?) {
 
-    /**
-     * Received the upsert method parameters, the upsertion adapters and generations the code that
-     * runs the upsert and returns the result.
-     *
-     * For example, for the DAO method:
-     * ```
-     * @Upsert
-     * fun addPublishers(vararg publishers: Publisher): List<Long>
-     * ```
-     * The following code will be generated:
-     *
-     * ```
-     * __db.beginTransaction();
-     * try {
-     *  List<Long> _result = __upsertionAdapterOfPublisher.upsertAndReturnIdsList(publishers);
-     *  __db.setTransactionSuccessful();
-     *  return _result;
-     * } finally {
-     *  __db.endTransaction();
-     * }
-     * ```
-     */
-    abstract fun convertAndReturn(
+/**
+ * Binder that knows how to write instant (blocking) upsert methods.
+ */
+class InstantUpsertMethodBinder(adapter: UpsertMethodAdapter?) : UpsertMethodBinder(adapter) {
+
+    override fun convertAndReturn(
         parameters: List<ShortcutQueryParameter>,
         upsertionAdapters: Map<String, Pair<FieldSpec, TypeSpec>>,
         dbField: FieldSpec,
         scope: CodeGenScope
-    )
+    ) {
+        scope.builder().apply {
+            addStatement("$N.assertNotSuspendingTransaction()", DaoWriter.dbField)
+        }
+        // TODO: createUpsertionMethodBody
+    }
 }
