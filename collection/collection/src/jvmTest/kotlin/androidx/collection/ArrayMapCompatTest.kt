51bb23efac837de5015cc3b74aa28ab973f0ffcf
Arkadii Ivanov <arkivanov@google.com>
2022-07-15 11:06:02 +0100

    Converted ArrayMapCompatTest to Kotlin
    Bug: 230860333
    Test: ArrayMapCompatTest
    Change-Id: I932d1c556bee2d3e75b06b2bad51811eb0e13842

--- collection/collection/src/jvmTest/kotlin/androidx/collection/ArrayMapCompatTest.kt
+++ collection/collection/src/jvmTest/kotlin/androidx/collection/ArrayMapCompatTest.kt
@@ -13,122 +13,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package androidx.collection;
+package androidx.collection
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-
-@RunWith(JUnit4.class)
-public class ArrayMapCompatTest {
+internal class ArrayMapCompatTest {
 
     @Test
-    public void testCanNotIteratePastEnd_entrySetIterator() {
-        Map<String, String> map = new ArrayMap<>();
-        map.put("key 1", "value 1");
-        map.put("key 2", "value 2");
-        Set<Map.Entry<String, String>> expectedEntriesToIterate = new HashSet<>(Arrays.asList(
-                entryOf("key 1", "value 1"),
-                entryOf("key 2", "value 2")
-        ));
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+    fun testCanNotIteratePastEnd_entrySetIterator() {
+        val map = ArrayMap<String, String>()
+        map["key 1"] = "value 1"
+        map["key 2"] = "value 2"
+        val iterator = map.entries.iterator()
 
         // Assert iteration over the expected two entries in any order
-        assertTrue(iterator.hasNext());
-        Map.Entry<String, String> firstEntry = copyOf(iterator.next());
-        assertTrue(expectedEntriesToIterate.remove(firstEntry));
+        assertTrue(iterator.hasNext())
+        assertEquals("key 1" to "value 1", iterator.next().toPair())
+        assertTrue(iterator.hasNext())
+        assertEquals("key 2" to "value 2", iterator.next().toPair())
+        assertFalse(iterator.hasNext())
 
-        assertTrue(iterator.hasNext());
-        Map.Entry<String, String> secondEntry = copyOf(iterator.next());
-        assertTrue(expectedEntriesToIterate.remove(secondEntry));
-
-        assertFalse(iterator.hasNext());
-
-        try {
-            iterator.next();
-            fail();
-        } catch (NoSuchElementException expected) {
-        }
-    }
-
-    private static <K, V> Map.Entry<K, V> entryOf(K key, V value) {
-        return new AbstractMap.SimpleEntry<>(key, value);
-    }
-
-    private static <K, V> Map.Entry<K, V> copyOf(Map.Entry<K, V> entry) {
-        return entryOf(entry.getKey(), entry.getValue());
+        assertFailsWith<NoSuchElementException> { iterator.next() }
     }
 
     @Test
-    public void testCanNotIteratePastEnd_keySetIterator() {
-        Map<String, String> map = new ArrayMap<>();
-        map.put("key 1", "value 1");
-        map.put("key 2", "value 2");
-        Set<String> expectedKeysToIterate = new HashSet<>(Arrays.asList("key 1", "key 2"));
-        Iterator<String> iterator = map.keySet().iterator();
+    fun testCanNotIteratePastEnd_keySetIterator() {
+        val map = ArrayMap<String, String>()
+        map["key 1"] = "value 1"
+        map["key 2"] = "value 2"
+        val iterator = map.keys.iterator()
 
         // Assert iteration over the expected two keys in any order
-        assertTrue(iterator.hasNext());
-        String firstKey = iterator.next();
-        assertTrue(expectedKeysToIterate.remove(firstKey));
+        assertTrue(iterator.hasNext())
+        assertEquals("key 1", iterator.next())
+        assertTrue(iterator.hasNext())
+        assertEquals("key 2", iterator.next())
+        assertFalse(iterator.hasNext())
 
-        assertTrue(iterator.hasNext());
-        String secondKey = iterator.next();
-        assertTrue(expectedKeysToIterate.remove(secondKey));
-
-        assertFalse(iterator.hasNext());
-
-        try {
-            iterator.next();
-            fail();
-        } catch (NoSuchElementException expected) {
-        }
+        assertFailsWith<NoSuchElementException> { iterator.next() }
     }
 
     @Test
-    public void testCanNotIteratePastEnd_valuesIterator() {
-        Map<String, String> map = new ArrayMap<>();
-        map.put("key 1", "value 1");
-        map.put("key 2", "value 2");
-        Set<String> expectedValuesToIterate = new HashSet<>(Arrays.asList("value 1", "value 2"));
-        Iterator<String> iterator = map.values().iterator();
+    fun testCanNotIteratePastEnd_valuesIterator() {
+        val map = ArrayMap<String, String>()
+        map["key 1"] = "value 1"
+        map["key 2"] = "value 2"
+        val iterator = map.values.iterator()
 
         // Assert iteration over the expected two values in any order
-        assertTrue(iterator.hasNext());
-        String firstValue = iterator.next();
-        assertTrue(expectedValuesToIterate.remove(firstValue));
+        assertTrue(iterator.hasNext())
+        assertEquals("value 1", iterator.next())
+        assertTrue(iterator.hasNext())
+        assertEquals("value 2", iterator.next())
+        assertFalse(iterator.hasNext())
 
-        assertTrue(iterator.hasNext());
-        String secondValue = iterator.next();
-        assertTrue(expectedValuesToIterate.remove(secondValue));
-
-        assertFalse(iterator.hasNext());
-
-        try {
-            iterator.next();
-            fail();
-        } catch (NoSuchElementException expected) {
-        }
+        assertFailsWith<NoSuchElementException> { iterator.next() }
     }
 
-    @Test public void toArray() {
-        Map<String, String> map = new ArrayMap<>();
-        map.put("key 1", "value 1");
-        map.put("key 2", "value 2");
-        Object[] array = map.entrySet().toArray();
-        assertEquals(2, array.length);
+    @Test
+    fun toArray() {
+        val map = ArrayMap<String, String>()
+        map["key 1"] = "value 1"
+        map["key 2"] = "value 2"
+        val array = map.entries.toTypedArray()
+        assertEquals(2, array.size)
     }
 }
