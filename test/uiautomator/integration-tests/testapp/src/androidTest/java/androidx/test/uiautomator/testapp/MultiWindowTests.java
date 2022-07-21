9ddc7cf4723340af8586084d9ae502e4ab7772e0
Daniel Peykov <peykov@google.com>
2022-07-21 20:44:34 +0000

    Fix flaky MultiWindow/UiDevice tests and improve coverage
    * PiP testing was found to be slow/flaky on some devices. This change
      simplifies it (fewer steps that can flake) and increases its timeout.
    * Also consolidates/refactors split-screen tests to lower their
      minSdkVersion, and replaces the flaky "focused" assertion in
      testDumpWindowHierarchy.
    
    Bug: 239438915
    Test: ./gradlew :test:uiautomator:integration-tests:t:cAT
    Change-Id: I685037f2e0a8e072573783920b3dead80ab8cc88

--- test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/MultiWindowTests.java
+++ test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/MultiWindowTests.java
@@ -16,12 +16,14 @@
 
 package androidx.test.uiautomator.testapp;
 
+import static android.accessibilityservice.AccessibilityService.GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN;
 import static android.content.Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT;
 import static android.content.Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 import static androidx.test.uiautomator.testapp.SplitScreenTestActivity.WINDOW_ID;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -31,6 +33,7 @@ import android.os.SystemClock;
 
 import androidx.test.filters.LargeTest;
 import androidx.test.filters.SdkSuppress;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.BySelector;
 import androidx.test.uiautomator.UiObject2;
@@ -38,48 +41,50 @@ import androidx.test.uiautomator.Until;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 /** Integration tests for multi-window support. */
 @LargeTest
 public class MultiWindowTests extends BaseTest {
 
-    private static final long TIMEOUT_MS = 10_000;
+    private static final long TIMEOUT_MS = 30_000;
     private static final long DELAY_MS = 5_000;
 
     @Test
     @SdkSuppress(minSdkVersion = 21)
-    public void testHasStatusBar() {
+    public void testMultiWindow_statusBar() {
+        // Can locate objects outside of current context.
         assertTrue(mDevice.hasObject(By.res("com.android.systemui", "status_bar")));
     }
 
     @Test
     @SdkSuppress(minSdkVersion = 24)
-    public void testPictureInPicture() {
+    public void testMultiWindow_pictureInPicture() {
         BySelector defaultMode = By.res(TEST_APP, "pip_mode").text("Default Mode");
         BySelector pipMode = By.res(TEST_APP, "pip_mode").text("PiP Mode");
 
         // Create window in PiP mode and verify its location (bounds correctly calculated).
         launchTestActivity(PictureInPictureTestActivity.class);
-        mDevice.findObject(By.res(TEST_APP, "pip_button")).click();
+        assertTrue(mDevice.hasObject(defaultMode));
+        mDevice.pressHome();
         SystemClock.sleep(DELAY_MS); // Wait for the PiP window to settle.
         int width = mDevice.getDisplayWidth();
         int height = mDevice.getDisplayHeight();
         Rect bottomHalf = new Rect(0, height / 2, width, height);
         UiObject2 pipWindow = mDevice.wait(Until.findObject(pipMode), TIMEOUT_MS);
+        assertNotNull("Timed out waiting for PiP window", pipWindow);
         assertTrue(bottomHalf.contains(pipWindow.getVisibleBounds()));
-
-        // Create window in default mode and verify that both windows are present and searchable.
-        launchTestActivity(PictureInPictureTestActivity.class,
-                new Intent().setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_MULTIPLE_TASK));
-        SystemClock.sleep(DELAY_MS); // Wait for the PiP window to settle.
-        assertTrue(mDevice.wait(Until.hasObject(pipMode), TIMEOUT_MS));
-        assertTrue(mDevice.hasObject(defaultMode));
     }
 
     @Test
-    @SdkSuppress(minSdkVersion = 32)
-    public void testSplitScreen() {
+    @SdkSuppress(minSdkVersion = 31)
+    public void testMultiWindow_splitScreen() {
+        // Launch two split-screen activities with different IDs.
         launchTestActivity(SplitScreenTestActivity.class,
                 new Intent().setFlags(DEFAULT_FLAGS).putExtra(WINDOW_ID, "first"));
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                .performGlobalAction(GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN);
         launchTestActivity(SplitScreenTestActivity.class,
                 new Intent().setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_LAUNCH_ADJACENT
                         | FLAG_ACTIVITY_MULTIPLE_TASK).putExtra(WINDOW_ID, "second"));
@@ -91,10 +96,38 @@ public class MultiWindowTests extends BaseTest {
         UiObject2 secondWindow = mDevice.findObject(By.res(TEST_APP, "window_id").text("second"));
         assertNotNull(secondWindow);
 
-        // Window IDs are centered in each window (bounds correctly calculated).
+        // Window IDs are centered in each window (bounds correctly calculated; order independent).
         int width = mDevice.getDisplayWidth();
         int height = mDevice.getDisplayHeight();
-        assertTrue(firstWindow.getVisibleBounds().contains(width / 2, height / 4));
-        assertTrue(secondWindow.getVisibleBounds().contains(width / 2, 3 * height / 4));
+        List<UiObject2> windows = Arrays.asList(firstWindow, secondWindow);
+        assertTrue(windows.stream().anyMatch(
+                w -> w.getVisibleBounds().contains(width / 2, height / 4)));
+        assertTrue(windows.stream().anyMatch(
+                w -> w.getVisibleBounds().contains(width / 2, 3 * height / 4)));
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = 31)
+    public void testMultiWindow_click() {
+        // Launch two split-screen activities with buttons.
+        launchTestActivity(UiDeviceTestClickActivity.class);
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                .performGlobalAction(GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN);
+        launchTestActivity(UiDeviceTestClickActivity.class,
+                new Intent().setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_LAUNCH_ADJACENT
+                        | FLAG_ACTIVITY_MULTIPLE_TASK));
+        SystemClock.sleep(DELAY_MS); // Wait for the windows to settle.
+
+        // Click a button in the middle of each activity.
+        int width = mDevice.getDisplayWidth();
+        int height = mDevice.getDisplayHeight();
+        mDevice.click(width / 2, height / 4);
+        mDevice.click(width / 2, 3 * height / 4);
+
+        // Verify that both buttons were clicked.
+        List<UiObject2> buttons = mDevice.findObjects(By.res(TEST_APP, "button"));
+        assertEquals(2, buttons.size());
+        assertEquals("I've been clicked!", buttons.get(0).getText());
+        assertEquals("I've been clicked!", buttons.get(1).getText());
     }
 }
