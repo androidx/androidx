929c1c67c6c54020d7a49d951d7baed1ae938306
Shivam Goel <shivamgoel@google.com>
2022-08-10 15:57:38 -0700

    New maven project for async layout inflater factory.
    Test: Unit tests have been added.
    Relnote: "Allows adding Factory2 and callback executor for
    AsyncLayoutInflater."
    
    Change-Id: I4ecf819f9249b887cbd27f1843b517a5ac9d4158

--- asynclayoutinflater/asynclayoutinflater/src/androidTest/java/androidx/asynclayoutinflater/view/AsyncLayoutInflaterTest.java
+++ asynclayoutinflater/asynclayoutinflater/src/androidTest/java/androidx/asynclayoutinflater/view/AsyncLayoutInflaterTest.java
@@ -15,6 +15,8 @@
  */
 package androidx.asynclayoutinflater.view;
 
+import android.os.Build;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -22,6 +24,7 @@ import androidx.asynclayoutinflater.test.R;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.MediumTest;
+import androidx.test.filters.SdkSuppress;
 
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
@@ -34,20 +37,19 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
 
 @MediumTest
 @RunWith(AndroidJUnit4.class)
 @SuppressWarnings("deprecation")
 public class AsyncLayoutInflaterTest {
-
     private static final String BG_THREAD_NAME = "bg-async-thread";
-
     @Rule
     public ActivityScenarioRule<TestActivity> testActivityRule = new ActivityScenarioRule<>(
             TestActivity.class);
-
     Executor mBackgroundExecutor;
     AsyncLayoutInflater mAsyncLayoutInflater;
+    AsyncLayoutInflater mAsyncLayoutInflaterAppCompat;
     TestActivity mActivity;
 
     @Before
@@ -55,6 +57,7 @@ public class AsyncLayoutInflaterTest {
         testActivityRule.getScenario().onActivity(activity -> {
             mActivity = activity;
             mAsyncLayoutInflater = activity.getAsyncLayoutInflater();
+            mAsyncLayoutInflaterAppCompat = activity.getAsyncLayoutInflaterAppCompat();
         });
         mBackgroundExecutor = Executors.newSingleThreadExecutor(
                 new ThreadFactoryBuilder().setNameFormat(BG_THREAD_NAME).build());
@@ -70,5 +73,65 @@ public class AsyncLayoutInflaterTest {
         View inflatedView = LayoutInflater.from(mActivity).inflate(R.layout.test_button, null,
                 false);
         Assert.assertNotSame(asyncInflatedView.getClass(), inflatedView.getClass());
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.LOLLIPOP)
+    public void correctAsyncInflaterView_forButton() throws Exception {
+        SettableFuture<View> asyncInflatedViewFuture = SettableFuture.create();
+        mAsyncLayoutInflaterAppCompat.inflate(R.layout.test_button, null,
+                /* callbackExecutor= */ null,
+                (view, resId, parent) -> {
+                    asyncInflatedViewFuture.set(view);
+                });
+        View asyncInflatedView = asyncInflatedViewFuture.get();
+        View inflatedView = LayoutInflater.from(mActivity).inflate(R.layout.test_button, null,
+                false);
+        Assert.assertSame(asyncInflatedView.getClass(), inflatedView.getClass());
+    }
+
+    @Test
+    public void inflateCallback_onMainThread() throws Exception {
+        AtomicReference<Thread> callbackThread = new AtomicReference<>(null);
+        SettableFuture<View> asyncInflatedViewFuture = SettableFuture.create();
+        mAsyncLayoutInflaterAppCompat.inflate(R.layout.test_button, null,
+                /* callbackExecutor= */ null,
+                (view, resId, parent) -> {
+                    callbackThread.set(Thread.currentThread());
+                    asyncInflatedViewFuture.set(view);
+                });
+        asyncInflatedViewFuture.get();
+        Assert.assertSame(callbackThread.get(), Looper.getMainLooper().getThread());
+    }
+
+    @Test
+    public void inflateCallback_onBackgroundThread() throws Exception {
+        AtomicReference<Thread> callbackThread = new AtomicReference<>(null);
+        SettableFuture<View> asyncInflatedViewFuture = SettableFuture.create();
+        mAsyncLayoutInflaterAppCompat.inflate(R.layout.test_button, null,
+                mBackgroundExecutor,
+                (view, resId, parent) -> {
+                    callbackThread.set(Thread.currentThread());
+                    asyncInflatedViewFuture.set(view);
+                });
+        asyncInflatedViewFuture.get();
+        // Not called on main thread.
+        Assert.assertNotSame(callbackThread.get(), Looper.getMainLooper().getThread());
+        Assert.assertEquals(callbackThread.get().getName(), BG_THREAD_NAME);
+    }
+
+    @Test
+    public void inflateCallback_onBackgroundThread_onAsyncInflationFailure() throws Exception {
+        AtomicReference<Thread> callbackThread = new AtomicReference<>(null);
+        SettableFuture<View> asyncInflatedViewFuture = SettableFuture.create();
+        mAsyncLayoutInflaterAppCompat.inflate(R.layout.fail_async_text_view, null,
+                mBackgroundExecutor,
+                (view, resId, parent) -> {
+                    callbackThread.set(Thread.currentThread());
+                    asyncInflatedViewFuture.set(view);
+                });
+        asyncInflatedViewFuture.get();
+        Assert.assertNotSame(callbackThread.get(), Looper.getMainLooper().getThread());
+        Assert.assertEquals(callbackThread.get().getName(), BG_THREAD_NAME);
     }
 }
