ec46efb56ecc93f26260d41546ee45bfb1ef7cd6
“riyaghai” <riyaghai@google.com>
2022-08-18 06:43:56 +0000

    Modifying locales related tests in appcompat in accordance with the sdk version bump to 33.
    Tests have been modified to have minSdkVersion as 32 (which was set to
    31 earlier).
    
    Tests using isAtLeastT() have been modified to use the version code for
    Tiramisu.
    
    Test: ./gradlew appcompat:appcompat:connectedCheck
    
    Bug: 223775393
    Bug: 218430372
    Change-Id: Ife0e6329ac8985d20134d780b5b78441634c8dba

--- appcompat/appcompat/src/androidTest/java/androidx/appcompat/app/LocalesUpdateTestCase.kt
+++ appcompat/appcompat/src/androidTest/java/androidx/appcompat/app/LocalesUpdateTestCase.kt
@@ -34,9 +34,7 @@ import org.junit.Rule
 import org.junit.Test
 
 @LargeTest
-// TODO(b/218430372): Modify SdkSuppress annotation in tests for backward compatibility of
-//  setApplicationLocales
-@SdkSuppress(maxSdkVersion = 31)
+@SdkSuppress(maxSdkVersion = 32)
 class LocalesUpdateTestCase() {
     @get:Rule
     val rule = LocalesActivityTestRule(LocalesUpdateActivity::class.java)
