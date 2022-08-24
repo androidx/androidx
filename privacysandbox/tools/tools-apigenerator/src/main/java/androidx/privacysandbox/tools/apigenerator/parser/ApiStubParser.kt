9995c622fb9cd8cc0c9573c75c06ff756ca5f811
Lucas Tenorio <ltenorio@google.com>
2022-08-24 11:16:00 +0100

    Add basic parser to Privacy Sandbox API generator.
    This class converts SDK API bytecode stubs and their Kotlin metadata
    into Privacy Sandbox API model classes. The classes will later be used
    to generate SDK API wrapper sources.
    
    In this basic implementation, we're just reading the appropriate
    annotations and copying the package and class names of the target
    interfaces.
    
    TODO:
    - Method, parameter and type parsing.
    - More robust tests around Kotlin metadata handling.
    
    Test: ./gradlew :privacysandbox:tools:tools-apigenerator:test
    Change-Id: I839ceda37b5a794ddcc4e87f170c108bb51f3b21

---
+++ privacysandbox/tools/tools-apigenerator/src/main/java/androidx/privacysandbox/tools/apigenerator/parser/ApiStubParser.kt
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.privacysandbox.tools.apigenerator.parser
+
+import androidx.privacysandbox.tools.PrivacySandboxService
+import androidx.privacysandbox.tools.core.AnnotatedInterface
+import androidx.privacysandbox.tools.core.ParsedApi
+import java.io.File
+import kotlinx.metadata.Flag
+import kotlinx.metadata.KmClass
+import kotlinx.metadata.jvm.KotlinClassHeader
+import kotlinx.metadata.jvm.KotlinClassMetadata
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassReader.SKIP_CODE
+import org.objectweb.asm.ClassReader.SKIP_DEBUG
+import org.objectweb.asm.ClassReader.SKIP_FRAMES
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.AnnotationNode
+import org.objectweb.asm.tree.ClassNode
+
+internal object ApiStubParser {
+    /**
+     * Parses the API annotated by a Privacy Sandbox SDK from its compiled classes.
+     *
+     * @param stubClassPath Set of compiled SDK classes. It's expected that at least one of those
+     *      files should be a valid Kotlin interface annotated with @PrivacySandboxService.
+     */
+    internal fun parse(stubClassPath: Set<File>): ParsedApi {
+        val services = stubClassPath.asSequence()
+            .filter { it.extension == "class" }
+            .map { it.inputStream().use { inputStream -> toClassNode(inputStream.readBytes()) } }
+            .filter { it.visibleAnnotationsWithType<PrivacySandboxService>().isNotEmpty() }
+            .map(ApiStubParser::parseClass)
+            .toSet()
+        if (services.isEmpty()) throw IllegalArgumentException(
+            "Unable to find valid interfaces annotated with @PrivacySandboxService."
+        )
+        return ParsedApi(services)
+    }
+
+    private fun toClassNode(classContents: ByteArray): ClassNode {
+        val reader = ClassReader(classContents)
+        val classNode = ClassNode(Opcodes.ASM9)
+        reader.accept(classNode, SKIP_CODE or SKIP_DEBUG or SKIP_FRAMES)
+        return classNode
+    }
+
+    private fun parseClass(classNode: ClassNode): AnnotatedInterface {
+        val kotlinMetadata = parseKotlinMetadata(classNode)
+
+        // Package names are separated with slashes and nested classes are separated with dots.
+        // (e.g com/example/OuterClass.InnerClass).
+        val (packageName, className) = kotlinMetadata.name.split('/').run {
+            dropLast(1).joinToString(separator = ".") to last()
+        }
+
+        if (!Flag.Class.IS_INTERFACE(kotlinMetadata.flags)) {
+            throw IllegalArgumentException(
+                "$packageName.$className is not a Kotlin interface but it's annotated with " +
+                    "@PrivacySandboxService."
+            )
+        }
+
+        if (className.contains('.')) {
+            throw IllegalArgumentException(
+                "$packageName.$className is an inner interface so it can't be annotated with " +
+                    "@PrivacySandboxService."
+            )
+        }
+
+        return AnnotatedInterface(className, packageName)
+    }
+
+    private fun parseKotlinMetadata(classNode: ClassNode): KmClass {
+        val metadataValues =
+            classNode.visibleAnnotationsWithType<Metadata>().firstOrNull()?.attributeMap
+                ?: throw IllegalArgumentException(
+                    "Missing Kotlin metadata annotation in ${classNode.name}. " +
+                        "Is this a valid Kotlin class?"
+                )
+
+        // ASM models annotation attributes as flat List<Objects>, so the unchecked cast is
+        // inevitable when some of these objects have type parameters, like the lists below.
+        @Suppress("UNCHECKED_CAST")
+        val header = KotlinClassHeader(
+            kind = metadataValues["k"] as Int?,
+            metadataVersion = (metadataValues["mv"] as? List<Int>?)?.toIntArray(),
+            data1 = (metadataValues["d1"] as? List<String>?)?.toTypedArray(),
+            data2 = (metadataValues["d2"] as? List<String>?)?.toTypedArray(),
+            extraInt = metadataValues["xi"] as? Int?,
+            packageName = metadataValues["pn"] as? String?,
+            extraString = metadataValues["xs"] as? String?,
+        )
+
+        return when (val metadata = KotlinClassMetadata.read(header)) {
+            is KotlinClassMetadata.Class -> metadata.toKmClass()
+            else -> throw IllegalArgumentException(
+                "Unable to parse Kotlin metadata from ${classNode.name}. " +
+                    "Is this a valid Kotlin class?"
+            )
+        }
+    }
+}
+
+inline fun <reified T> ClassNode.visibleAnnotationsWithType(): List<AnnotationNode> {
+    return (visibleAnnotations ?: listOf<AnnotationNode>())
+        .filter { Type.getDescriptor(T::class.java) == it?.desc }
+        .filterNotNull()
+}
+
+/** Map of annotation attributes. This is a convenience wrapper around [AnnotationNode.values]. */
+val AnnotationNode.attributeMap: Map<String, Any>
+    get() {
+        values ?: return mapOf()
+        val attributes = mutableMapOf<String, Any>()
+        for (i in 0 until values.size step 2) {
+            attributes[values[i] as String] = values[i + 1]
+        }
+        return attributes
+    }
---
+++ privacysandbox/tools/tools-apigenerator/src/test/java/androidx/privacysandbox/tools/apigenerator/parser/ApiStubParserTest.kt
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.privacysandbox.tools.apigenerator.parser
+
+import androidx.privacysandbox.tools.core.AnnotatedInterface
+import androidx.room.compiler.processing.util.Source
+import androidx.room.compiler.processing.util.compiler.TestCompilationArguments
+import androidx.room.compiler.processing.util.compiler.compile
+import androidx.testutils.assertThrows
+import com.google.common.truth.Truth.assertThat
+import java.nio.file.Files
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ApiStubParserTest {
+    @Test
+    fun annotatedInterface_isParsed() {
+        val source = Source.kotlin(
+            "com/mysdk/MySdk.kt", """
+                    package com.mysdk
+                    import androidx.privacysandbox.tools.PrivacySandboxService
+                    @PrivacySandboxService
+                    interface MySdk
+                """
+        )
+
+        assertThat(compileAndParseApi(source))
+            .containsExactly(AnnotatedInterface("MySdk", "com.mysdk"))
+    }
+
+    @Test
+    fun nonAnnotatedClasses_areSafelyIgnored() {
+        val interfaces = compileAndParseApi(
+            Source.kotlin(
+                "com/mysdk/MySdk.kt", """
+                    package com.mysdk
+                    import androidx.privacysandbox.tools.PrivacySandboxService
+                    @PrivacySandboxService
+                    interface MySdk
+                """
+            ), Source.kotlin(
+                "com/mysdk/NonAnnotatedInterface.kt", """
+                    package com.mysdk
+                    interface NonAnnotatedInterface
+                """
+            ), Source.kotlin(
+                "com/mysdk/NonAnnotatedClass.kt", """
+                    package com.mysdk
+                    class NonAnnotatedClass
+                """
+            ), Source.java(
+                "com/mysdk/NonAnnotatedJavaClass.java", """
+                    package com.mysdk;
+                    class NonAnnotatedJavaClass {}
+                """
+            )
+        )
+
+        assertThat(interfaces).containsExactly(AnnotatedInterface("MySdk", "com.mysdk"))
+    }
+
+    @Test
+    fun annotatedInterfaceWithEmptyPackageName_isHandledSafely() {
+        val source = Source.kotlin(
+            "MySdk.kt", """
+                    import androidx.privacysandbox.tools.PrivacySandboxService
+                    @PrivacySandboxService
+                    interface MySdk
+                """
+        )
+
+        assertThat(compileAndParseApi(source)).containsExactly(AnnotatedInterface("MySdk", ""))
+    }
+
+    @Test
+    fun nonKotlinAnnotatedInterface_throws() {
+        val source = Source.java(
+            "com/mysdk/MySdk.java", """
+                    package com.mysdk;
+                    import androidx.privacysandbox.tools.PrivacySandboxService;
+                    @PrivacySandboxService
+                    interface MySdk {}
+                """
+        )
+
+        assertThrows<IllegalArgumentException> {
+            compileAndParseApi(source)
+        }.hasMessageThat().contains(
+            "Missing Kotlin metadata annotation in com/mysdk/MySdk. Is this a valid Kotlin class?"
+        )
+    }
+
+    @Test
+    fun annotatedKotlinClass_throws() {
+        val source = Source.kotlin(
+            "com/mysdk/MySdk.kt", """
+                    package com.mysdk
+                    import androidx.privacysandbox.tools.PrivacySandboxService
+                    @PrivacySandboxService
+                    class MySdk
+                """
+        )
+
+        assertThrows<IllegalArgumentException> {
+            compileAndParseApi(source)
+        }.hasMessageThat().contains(
+            "com.mysdk.MySdk is not a Kotlin interface but it's annotated with " +
+                "@PrivacySandboxService"
+        )
+    }
+
+    @Test
+    fun annotatedKotlinInnerInterface_throws() {
+        val source = Source.kotlin(
+            "com/mysdk/OuterMySdk.kt", """
+                    package com.mysdk
+                    import androidx.privacysandbox.tools.PrivacySandboxService
+                    interface OuterMySdk {
+                      @PrivacySandboxService
+                      interface InnerMySdk
+                    }
+                """
+        )
+
+        assertThrows<IllegalArgumentException> {
+            compileAndParseApi(source)
+        }.hasMessageThat().contains(
+            "com.mysdk.OuterMySdk.InnerMySdk is an inner interface so it can't be annotated with " +
+                "@PrivacySandboxService"
+        )
+    }
+
+    @Test
+    fun missingAnnotatedInterface_throws() {
+        val source = Source.kotlin(
+            "com/mysdk/MySdk.kt", """
+                    package com.mysdk
+                    interface MySdk
+                """
+        )
+
+        assertThrows<IllegalArgumentException> {
+            compileAndParseApi(source)
+        }.hasMessageThat().contains(
+            "Unable to find valid interfaces annotated with @PrivacySandboxService."
+        )
+    }
+
+    private fun compileAndParseApi(vararg sources: Source): Set<AnnotatedInterface> {
+        val result = compile(
+            Files.createTempDirectory("test").toFile(), TestCompilationArguments(
+                sources = sources.asList(),
+            )
+        )
+        assertThat(result.success).isTrue()
+        val stubClassPath = result.outputClasspath.flatMap { it.walk() }.toSet()
+        return ApiStubParser.parse(stubClassPath).services
+    }
+}
