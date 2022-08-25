d5aef61a6fe51dcffb0fe4732659b3833f69395e
WenHung_Teng <wenhungteng@google.com>
2022-08-25 15:55:49 +0800

    Update core test app tests to reset the cameraImpl
    For Parameterized test with camera-camera2/camera-pipe,
    Tests (using CameraXActivity) will shut down camerax in
    the @After block, and it will require to force config
    the cameraImpl every time when start the test Activity.
    
    Add an extra Intent key to ignore the configured
    implementation type, so it will be able to configure
    the camera implement again when next time to start the
    Activity.
    
    Bug: 243758509
    Test: TakePictureTest, ToggleButtonUITest,
    ExistingActivityLifecycleTest
    
    Change-Id: Idf674d9f90145d59a9d7311c6c15d79468571b5a

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/BasicUITest.kt
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/BasicUITest.kt
@@ -89,6 +89,7 @@ class BasicUITest(
         CameraXActivity::class.java
     ).apply {
         putExtra(CameraXActivity.INTENT_EXTRA_CAMERA_IMPLEMENTATION, cameraConfig)
+        putExtra(CameraXActivity.INTENT_EXTRA_CAMERA_IMPLEMENTATION_NO_HISTORY, true)
     }
 
     @Before
