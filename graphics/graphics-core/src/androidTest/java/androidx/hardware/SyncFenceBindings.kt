ed9a83a5559ed942916ad9bd287a052d80c0bc9d
Nader Jawad <njawad@google.com>
2022-08-26 23:13:31 -0700

    Refactor SyncFence.cpp to leverage dynamic linking
    Workaround linking issue when attempting to leverage
    libsync.so in the NDK. Use dlopen/dlsym to resolve
    sync_file_info and sync_file_info_free APIs to
    avoid null resolution of method symbols.
    
    Created SyncFenceTest to front load more tests into
    presubmit in addition to existing EGLManagerTests which
    are more end to end with EGL setup.
    
    Relnote: "Added finalizer to SyncFence as well as fixing
    constant usage within nWait native implementation to
    use 0 instead of JNI_OK.
    
    Updated RequiresApi for SyncFence#getSignalTime to Android
    O as it relies on NDK APIs that were introduced in API level
    26
    "
    
    Fixes: 242317731
    Test: Re-enabled testEglDupNativeFenceFDANDROIDawaitForever
    and introduced additional SyncFenceTests
    
    Change-Id: Ic8ecdb141e6e6cb8da23df653174ef14165686dc

---
+++ graphics/graphics-core/src/androidTest/java/androidx/hardware/SyncFenceBindings.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.hardware
+
+/**
+ * Helper class of jni bindings to verify dlopen/dlsym behavior to resolve sync_info_file and
+ * sync_info_file_free methods
+ */
+internal class SyncFenceBindings private constructor() {
+    companion object {
+
+        external fun nResolveSyncFileInfo(): Boolean
+
+        external fun nResolveSyncFileInfoFree(): Boolean
+
+        init {
+            System.loadLibrary("sync-fence")
+        }
+    }
+}
