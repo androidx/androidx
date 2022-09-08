6bc03ad6ba36fd98754c06946ce7837d9195f050
Fred Sladkey <fsladkey@google.com>
2022-09-08 12:55:23 +0000

    Revert "Add simulator test tasks to bOS for KMP builds."
    This reverts commit a5e8417c418c16f3cb3910bbb08c7806ae150db5.
    
    Reason for revert: Intermittent infra issues are causing build to fail occasionally and need to be resolved before re-landing.
    
    Change-Id: Ic65c6f61e61ad7f82da1c6075ff11d53a8eef707

--- busytown/androidx_sandbox.sh
+++ busytown/androidx_sandbox.sh
@@ -9,4 +9,4 @@ export ANDROIDX_PROJECTS=KMP
 export USE_ANDROIDX_REMOTE_BUILD_CACHE=false
 
 # run build in a sandbox
-../development/sandbox/run-without-network.sh impl/build.sh buildOnServer --no-configuration-cache --no-daemon -Pandroidx.displayTestOutput=false -Pandroidx.runKmpUnitTestsWithBuildOnServerTask=true
+../development/sandbox/run-without-network.sh impl/build.sh buildOnServer --no-configuration-cache --no-daemon
