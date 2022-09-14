91e14f289b259bc5c8cea085a6ad93cd35f55549
Rahul Ravikumar <rahulrav@google.com>
2022-09-14 13:40:29 -0700

    Fix XCode project generation to ensure gradle tasks run before benchmarks.
    Test: Tested locally
    Change-Id: I7cbfdb9f0346f191a9787397e46352f64f09d7ca

--- benchmark/benchmark-darwin-samples-xcode/xcodegenw.sh
+++ benchmark/benchmark-darwin-samples-xcode/xcodegenw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Generates the XCode project to run / debug benchmark tests.
 
+rm -rf benchmark-darwin-sample-xcode.xcodeproj
 xcodegen --spec xcodegen-project.yml
 open benchmark-darwin-sample-xcode.xcodeproj
