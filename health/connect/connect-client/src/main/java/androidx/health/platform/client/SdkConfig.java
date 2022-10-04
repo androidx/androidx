f128279065c5aff04f7d18249501228012a320c1
Hengrui CAO <hengruicao@google.com>
2022-10-04 11:10:19 +0100

    Version bump post alpha06 release.
    Test: N/A
    Relnote: N/A
    Change-Id: Ib366a13eccbc6963ccc5bb557c0ec9f93d21d316

--- health/connect/connect-client/src/main/java/androidx/health/platform/client/SdkConfig.java
+++ health/connect/connect-client/src/main/java/androidx/health/platform/client/SdkConfig.java
@@ -25,7 +25,7 @@ import androidx.annotation.RestrictTo;
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 public final class SdkConfig {
     // should be increased everytime a new SDK is released
-    public static final int SDK_VERSION = 6;
+    public static final int SDK_VERSION = 7;
 
     private SdkConfig() {}
 }
