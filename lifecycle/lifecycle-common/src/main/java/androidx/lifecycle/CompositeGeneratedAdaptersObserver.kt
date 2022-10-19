caea9b3830e7959ee3731d586f0faa0ae1676474
sanura <sanura@google.com>
2022-10-19 21:36:19 +0000

    Converted CompositeGeneratedAdaptersObserver to Kotlin
    Part of the kotlinification process of Lifecycle library.
    
    Test: ./gradlew checkApi
    Bug: 240298691
    
    Relnote: "`CompositeGeneratedAdaptersObserver` is now in Kotlin"
    Change-Id: I0fd68bf090846a23586dc46fc4b8e73bc58a56ac

--- lifecycle/lifecycle-common/src/main/java/androidx/lifecycle/CompositeGeneratedAdaptersObserver.kt
+++ lifecycle/lifecycle-common/src/main/java/androidx/lifecycle/CompositeGeneratedAdaptersObserver.kt
@@ -13,28 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.lifecycle
 
-package androidx.lifecycle;
-
-
-import androidx.annotation.NonNull;
-
-class CompositeGeneratedAdaptersObserver implements LifecycleEventObserver {
-
-    private final GeneratedAdapter[] mGeneratedAdapters;
-
-    CompositeGeneratedAdaptersObserver(GeneratedAdapter[] generatedAdapters) {
-        mGeneratedAdapters = generatedAdapters;
-    }
-
-    @Override
-    public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
-        MethodCallsLogger logger = new MethodCallsLogger();
-        for (GeneratedAdapter mGenerated: mGeneratedAdapters) {
-            mGenerated.callMethods(source, event, false, logger);
+internal class CompositeGeneratedAdaptersObserver(
+    private val generatedAdapters: Array<GeneratedAdapter>
+) :
+    LifecycleEventObserver {
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        val logger = MethodCallsLogger()
+        for (adapter in generatedAdapters) {
+            adapter.callMethods(source, event, false, logger)
         }
-        for (GeneratedAdapter mGenerated: mGeneratedAdapters) {
-            mGenerated.callMethods(source, event, true, logger);
+        for (adapter in generatedAdapters) {
+            adapter.callMethods(source, event, true, logger)
         }
     }
 }
