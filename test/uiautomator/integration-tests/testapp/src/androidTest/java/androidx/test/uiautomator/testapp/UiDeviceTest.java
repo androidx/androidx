6b5b1953f0c27963557aaea2b4e68108b93b3d1c
Aurimas Liutikas <aurimas@google.com>
2022-10-24 11:42:52 -0700

    Revert "Add tests for `dumpWindowHierarchy(*)`."
    This reverts commit 9c5c2a0ffe068bb16eebac5ef7debd11bac60042.
    
    Reason for revert: Causing test instability
    Bug: 255298365
    
    Change-Id: I8e4bc1bb4784b1462cade39146dc34e1e2b02fd8

--- test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/UiDeviceTest.java
+++ test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/UiDeviceTest.java
@@ -43,7 +43,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -387,53 +386,23 @@ public class UiDeviceTest extends BaseTest {
         assertTrue(mDevice.isNaturalOrientation());
     }
 
-    @Test
-    public void testDumpWindowHierarchy_withString() throws Exception {
-        launchTestActivity(MainActivity.class);
-        File outFile = mTmpDir.newFile();
-        mDevice.dumpWindowHierarchy(outFile.getAbsolutePath());
+    /* TODO(b/235841020): Implement these tests, and the tests for exceptions of each tested method.
 
-        // Verify that a valid XML file was generated and that node attributes are correct.
-        Document xml = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(outFile);
-        validateMainActivityXml(xml);
-    }
+    public void testDumpWindowHierarchy_withString() {}
+
+    public void testDumpWindowHierarchy_withFile() {} // already added
+
+    public void testDumpWindowHierarchy_withOutputStream() {}
+    */
 
     @Test
-    public void testDumpWindowHierarchy_withFile() throws Exception {
+    public void testDumpWindowHierarchy() throws Exception {
         launchTestActivity(MainActivity.class);
         File outFile = mTmpDir.newFile();
         mDevice.dumpWindowHierarchy(outFile);
 
         // Verify that a valid XML file was generated and that node attributes are correct.
         Document xml = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(outFile);
-        validateMainActivityXml(xml);
-    }
-
-    @Test
-    public void testDumpWindowHierarchy_withOutputStream() throws Exception {
-        launchTestActivity(MainActivity.class);
-        File outFile = mTmpDir.newFile();
-        FileOutputStream outStream = new FileOutputStream(outFile);
-        mDevice.dumpWindowHierarchy(outStream);
-
-        Document xml = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(outFile);
-        validateMainActivityXml(xml);
-    }
-
-    /* TODO(b/235841020): Implement these tests, and the tests for exceptions of each tested method.
-
-    public void testWaitForWindowUpdate() {}
-
-    public void testTakeScreenshot() {} // already added
-
-    public void testTakeScreenshot_withScaleAndQuality() {} // already added
-
-    public void testGetLauncherPackageName() {}
-
-    public void testExecuteShellCommand() {} // already added
-    */
-
-    private static void validateMainActivityXml(Document xml) throws Exception {
         Element element = (Element) XPathFactory.newInstance().newXPath()
                 .compile("//hierarchy//*/node[@resource-id='" + TEST_APP + ":id/button']")
                 .evaluate(xml, XPathConstants.NODE);
@@ -457,4 +426,17 @@ public class UiDeviceTest extends BaseTest {
         assertEquals("true", element.getAttribute("visible-to-user"));
         assertNotNull(element.getAttribute("bounds"));
     }
+
+    /* TODO(b/235841020): Implement these tests, and the tests for exceptions of each tested method.
+
+    public void testWaitForWindowUpdate() {}
+
+    public void testTakeScreenshot() {} // already added
+
+    public void testTakeScreenshot_withScaleAndQuality() {} // already added
+
+    public void testGetLauncherPackageName() {}
+
+    public void testExecuteShellCommand() {} // already added
+    */
 }
