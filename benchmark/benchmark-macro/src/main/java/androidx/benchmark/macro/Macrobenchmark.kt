a10d2ed3051551b7a74038bdcd58c0d272f12f5e
Chris Craik <ccraik@google.com>
2022-11-04 18:22:04 +0000

    Revert "New APIs PerfettoTrace.record and PerfettoTraceRule"
    This reverts commit 5462b6f43ac00e551e9ef074461cdf0999b478fd.
    
    Reason for revert: breaks running benchmarks locally
    
    Change-Id: I331e1abdd3681e8234e751558b66cbca55994d28

--- benchmark/benchmark-macro/src/main/java/androidx/benchmark/macro/Macrobenchmark.kt
+++ benchmark/benchmark-macro/src/main/java/androidx/benchmark/macro/Macrobenchmark.kt
@@ -208,9 +208,9 @@ private fun macrobenchmark(
                     setupBlock(scope)
                 }
 
-                val iterString = iteration.toString().padStart(3, '0')
                 val tracePath = perfettoCollector.record(
-                    fileLabel = "${uniqueName}_iter$iterString",
+                    benchmarkName = uniqueName,
+                    iteration = iteration,
 
                     /**
                      * Prior to API 24, every package name was joined into a single setprop which
