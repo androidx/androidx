0fcf693f18ae7b701975cf0296fc5bc1d71e3b69
sanura <sanura@google.com>
2022-11-08 00:27:16 +0000

    Converted LifecycleEventObserver to Kotlin
    Part of the kotlinification process of Lifecycle library.
    
    RelNote: "`LifecycleEventObserver` is now in Kotlin"
    Test: ./gradlew checkApi
    Bug: 240298691
    Change-Id: I60034ada57f8a23c65f7bdbecdf2792f59b77733

--- lifecycle/lifecycle-common/src/main/java/androidx/lifecycle/LifecycleEventObserver.kt
+++ lifecycle/lifecycle-common/src/main/java/androidx/lifecycle/LifecycleEventObserver.kt
@@ -13,29 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package androidx.lifecycle;
-
-import androidx.annotation.NonNull;
+package androidx.lifecycle
 
 /**
  * Class that can receive any lifecycle change and dispatch it to the receiver.
- * <p>
+ *
  * If a class implements both this interface and
- * {@link androidx.lifecycle.DefaultLifecycleObserver}, then
- * methods of {@code DefaultLifecycleObserver} will be called first, and then followed by the call
- * of {@link LifecycleEventObserver#onStateChanged(LifecycleOwner, Lifecycle.Event)}
- * <p>
- * If a class implements this interface and in the same time uses {@link OnLifecycleEvent}, then
+ * [androidx.lifecycle.DefaultLifecycleObserver], then
+ * methods of `DefaultLifecycleObserver` will be called first, and then followed by the call
+ * of [LifecycleEventObserver.onStateChanged]
+ *
+ * If a class implements this interface and in the same time uses [OnLifecycleEvent], then
  * annotations will be ignored.
  */
-@SuppressWarnings("LambdaLast")
-public interface LifecycleEventObserver extends LifecycleObserver {
+public fun interface LifecycleEventObserver : LifecycleObserver {
     /**
      * Called when a state transition event happens.
      *
      * @param source The source of the event
      * @param event The event
      */
-    void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event);
+    public fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event)
 }
