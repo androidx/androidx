5248e303189651705e0188242694c84f09c3bb35
Jeff Gaston <jeffrygaston@google.com>
2022-11-18 15:14:21 -0500

    merging docs build times into the main build metrics file
    to make it easier to find them
    
    Bug: 217178435
    Test: Treehugger runs busytown/*.sh
    
    Change-Id: I2101357f1a64fe7feb83e2e527a818823e751a37

--- busytown/androidx.sh
+++ busytown/androidx.sh
@@ -28,7 +28,7 @@ else
 
   # Parse performance profile reports (generated with the --profile option above) and re-export
   # the metrics in an easily machine-readable format for tracking
-  impl/parse_profile_htmls.sh
+  impl/parse_profile_data.sh
 fi
 
 echo "Completing $0 at $(date) with exit value $EXIT_VALUE"
