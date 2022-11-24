d7cadb0aee1659f9a0a1d7c1e5a0ae028b89a279
WenHung_Teng <wenhungteng@google.com>
2022-11-24 16:49:32 +0800

    Parameterized Camera2InteropIntegrationTest
    Run test on both camera-camera2 & camera-pipe config.
    The DeviceStateCallback is not ported, ignore the
    cameraDeviceListener_receivesClose_afterUnbindAll test
    for camera-pipe.
    
    Bug: 239153537
    Test: Camera2InteropIntegrationTest
    Change-Id: I50853da662afb1e7860a909c4becc118fc487042

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/util/CameraPipeUtil.kt
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/util/CameraPipeUtil.kt
@@ -17,11 +17,19 @@
 package androidx.camera.integration.core.util
 
 import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CaptureRequest
 import androidx.annotation.OptIn
+import androidx.camera.camera2.interop.Camera2CameraControl
+import androidx.camera.camera2.interop.Camera2CameraInfo
 import androidx.camera.camera2.interop.Camera2Interop
+import androidx.camera.camera2.interop.CaptureRequestOptions
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 import androidx.camera.camera2.pipe.integration.CameraPipeConfig
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraInfo
 import androidx.camera.core.ExtendableBuilder
+import com.google.common.util.concurrent.ListenableFuture
 
 object CameraPipeUtil {
 
@@ -42,6 +50,68 @@ object CameraPipeUtil {
                 .setSessionCaptureCallback(captureCallback)
         } else {
             Camera2Interop.Extender(builder).setSessionCaptureCallback(captureCallback)
+        }
+    }
+
+    @kotlin.OptIn(
+        androidx.camera.camera2.pipe.integration.interop.ExperimentalCamera2Interop::class
+    )
+    @OptIn(markerClass = [ExperimentalCamera2Interop::class])
+    @JvmStatic
+    fun <T> setDeviceStateCallback(
+        implName: String,
+        builder: ExtendableBuilder<T>,
+        stateCallback: CameraDevice.StateCallback
+    ) {
+        if (implName == CameraPipeConfig::class.simpleName) {
+            androidx.camera.camera2.pipe.integration.interop.Camera2Interop.Extender(
+                builder
+            ).setDeviceStateCallback(stateCallback)
+        } else {
+            Camera2Interop.Extender(builder).setDeviceStateCallback(stateCallback)
+        }
+    }
+
+    @kotlin.OptIn(
+        androidx.camera.camera2.pipe.integration.interop.ExperimentalCamera2Interop::class
+    )
+    @OptIn(markerClass = [ExperimentalCamera2Interop::class])
+    @JvmStatic
+    fun getCameraId(implName: String, cameraInfo: CameraInfo): String {
+        return if (implName == CameraPipeConfig::class.simpleName) {
+            androidx.camera.camera2.pipe.integration.interop.Camera2CameraInfo.from(
+                cameraInfo
+            ).getCameraId()
+        } else {
+            Camera2CameraInfo.from(cameraInfo).cameraId
+        }
+    }
+
+    @kotlin.OptIn(
+        androidx.camera.camera2.pipe.integration.interop.ExperimentalCamera2Interop::class
+    )
+    @OptIn(markerClass = [ExperimentalCamera2Interop::class])
+    @JvmStatic
+    fun setRequestOptions(
+        implName: String,
+        cameraControl: CameraControl,
+        parameter: Map<CaptureRequest.Key<Int>, Int>
+    ): ListenableFuture<Void?> {
+        return if (implName == CameraPipeConfig::class.simpleName) {
+            androidx.camera.camera2.pipe.integration.interop.Camera2CameraControl.from(
+                cameraControl
+            ).setCaptureRequestOptions(
+                androidx.camera.camera2.pipe.integration.interop.CaptureRequestOptions.Builder()
+                    .apply {
+                        parameter.forEach { (key, value) -> setCaptureRequestOption(key, value) }
+                    }.build()
+            )
+        } else {
+            Camera2CameraControl.from(cameraControl).setCaptureRequestOptions(
+                CaptureRequestOptions.Builder().apply {
+                    parameter.forEach { (key, value) -> setCaptureRequestOption(key, value) }
+                }.build()
+            )
         }
     }
 }
