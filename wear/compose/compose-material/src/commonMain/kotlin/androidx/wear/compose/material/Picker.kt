ac8b37a47434283025d83bb30028ee53f26d7b61
stevebower <stevebower@google.com>
2022-12-05 18:39:17 +0000

    Fix jittery font when scrolling Picker
    There has been a long-standing bug in the Picker component that scaled text has a jitter (typically seen as moving slightly horizontally during the scroll). Since aosp/2277787, we can now specify a compositing strategy on Modifier.graphicsLayer which addresses this problem by forcing offscreen rendering. The graphics layer is used further down the stack in ScalingLazyColumn.
    
    Relnote: "We have fixed a text jittering issue seen in the Picker component by leveraging the new compositing strategy added to Modifier.graphicsLayer."
    
    Bug: 251185997
    Bug: 223384589
    Test: ./gradlew :wear:compose:compose-material:connectedCheck --info --daemon
    Change-Id: I993029ef37f128cbb568d4a9b80b8bf8f3534882

--- wear/compose/compose-material/src/commonMain/kotlin/androidx/wear/compose/material/Picker.kt
+++ wear/compose/compose-material/src/commonMain/kotlin/androidx/wear/compose/material/Picker.kt
@@ -48,7 +48,9 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.focused
@@ -184,7 +186,11 @@ public fun Picker(
             content = {
                 items(state.numberOfItems()) { ix ->
                     with(pickerScope) {
-                        option((ix + state.optionsOffset) % state.numberOfOptions)
+                        Box(Modifier.graphicsLayer {
+                            compositingStrategy = CompositingStrategy.Offscreen
+                        }) {
+                            option((ix + state.optionsOffset) % state.numberOfOptions)
+                        }
                     }
                 }
             },
