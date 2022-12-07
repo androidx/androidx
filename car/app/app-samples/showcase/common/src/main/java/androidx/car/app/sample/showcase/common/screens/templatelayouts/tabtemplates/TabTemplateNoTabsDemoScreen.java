5f7a46696a6cd3d0bfe32cbf1e0a8290a5ab25db
Yiqun Wu <yiqunw@google.com>
2022-12-07 13:23:36 +0800

    Add tab template demo screens
    Add one tab demo screen with 4 tabs: message, pane, list and grid tabs.
    Add one tab demo screen with 2 tabs: loading state and search tab.
    Add one tab demo screen without tabs.
    Screenshots:
    https://hsv.googleplex.com/6527246455013376
    https://hsv.googleplex.com/6186061634994176
    https://hsv.googleplex.com/5662232575737856
    https://hsv.googleplex.com/5050944241795072
    https://hsv.googleplex.com/4742767184969728
    https://hsv.googleplex.com/5783473546592256
    https://hsv.googleplex.com/6562040974934016
    https://hsv.googleplex.com/4976697880346624
    https://hsv.googleplex.com/5084435876675584
    https://hsv.googleplex.com/6517745987354624
    
    Bug: 237312967
    Test: manually
    Change-Id: I20ae67f8bb3de935cc35fe3df78abc7842886b3d

---
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/templatelayouts/tabtemplates/TabTemplateNoTabsDemoScreen.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.app.sample.showcase.common.screens.templatelayouts.tabtemplates;
+
+import static androidx.car.app.model.Action.APP_ICON;
+
+import android.annotation.SuppressLint;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.CarContext;
+import androidx.car.app.CarToast;
+import androidx.car.app.Screen;
+import androidx.car.app.model.TabTemplate;
+import androidx.car.app.model.Template;
+
+/**
+ * Creates a screen that demonstrates usage of the full screen {@link TabTemplate} without tabs.
+ */
+public final class TabTemplateNoTabsDemoScreen extends Screen {
+    private TabTemplate.Builder mTabTemplateBuilder;
+
+    public TabTemplateNoTabsDemoScreen(@NonNull CarContext carContext) {
+        super(carContext);
+    }
+
+    @NonNull
+    @Override
+    public Template onGetTemplate() {
+        mTabTemplateBuilder = new TabTemplate.Builder(new TabTemplate.TabCallback() {
+            @SuppressLint("SyntheticAccessor")
+            @Override
+            public void onTabSelected(@NonNull String tabContentId) {
+                // No-op
+            }
+        })
+                .setHeaderAction(APP_ICON)
+                .setLoading(true);
+
+        new Handler(getCarContext().getMainLooper()).postDelayed(() -> {
+            getScreenManager().pop();
+            CarToast.makeText(getCarContext(), "Back to previous screen",
+                    CarToast.LENGTH_SHORT).show();
+        }, 5000);
+        return mTabTemplateBuilder.build();
+    }
+
+}
