013ec849eb342d1658b3879c3c9cef59a09d44e3
sanura <sanura@google.com>
2022-12-15 03:14:29 +0000

    Convert LifecycleService to Kotlin
    Part of the kotlin conversion process for the Lifecycle library.
    
    RelNote: "`LifecycleService` is now in Kotlin"
    Test: ./gradlew checkApi
    Bug: 240298691
    Change-Id: I160d7e0be9d70207038f35a19cb05e5fcf8d25b2

--- lifecycle/lifecycle-service/src/main/java/androidx/lifecycle/LifecycleService.kt
+++ lifecycle/lifecycle-service/src/main/java/androidx/lifecycle/LifecycleService.kt
@@ -13,67 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.lifecycle
 
-package androidx.lifecycle;
-
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
-import androidx.annotation.CallSuper;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.annotation.CallSuper
 
 /**
- * A Service that is also a {@link LifecycleOwner}.
+ * A Service that is also a [LifecycleOwner].
  */
-public class LifecycleService extends Service implements LifecycleOwner {
+open class LifecycleService : Service(), LifecycleOwner {
 
-    private final ServiceLifecycleDispatcher mDispatcher = new ServiceLifecycleDispatcher(this);
+    private val dispatcher = ServiceLifecycleDispatcher(this)
 
     @CallSuper
-    @Override
-    public void onCreate() {
-        mDispatcher.onServicePreSuperOnCreate();
-        super.onCreate();
+    override fun onCreate() {
+        dispatcher.onServicePreSuperOnCreate()
+        super.onCreate()
     }
 
     @CallSuper
-    @Nullable
-    @Override
-    public IBinder onBind(@NonNull Intent intent) {
-        mDispatcher.onServicePreSuperOnBind();
-        return null;
+    override fun onBind(intent: Intent): IBinder? {
+        dispatcher.onServicePreSuperOnBind()
+        return null
     }
 
-    @SuppressWarnings("deprecation")
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
     @CallSuper
-    @Override
-    public void onStart(@Nullable Intent intent, int startId) {
-        mDispatcher.onServicePreSuperOnStart();
-        super.onStart(intent, startId);
+    override fun onStart(intent: Intent?, startId: Int) {
+        dispatcher.onServicePreSuperOnStart()
+        super.onStart(intent, startId)
     }
 
     // this method is added only to annotate it with @CallSuper.
-    // In usual service super.onStartCommand is no-op, but in LifecycleService
-    // it results in mDispatcher.onServicePreSuperOnStart() call, because
+    // In usual Service, super.onStartCommand is no-op, but in LifecycleService
+    // it results in dispatcher.onServicePreSuperOnStart() call, because
     // super.onStartCommand calls onStart().
     @CallSuper
-    @Override
-    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
     }
 
     @CallSuper
-    @Override
-    public void onDestroy() {
-        mDispatcher.onServicePreSuperOnDestroy();
-        super.onDestroy();
+    override fun onDestroy() {
+        dispatcher.onServicePreSuperOnDestroy()
+        super.onDestroy()
     }
 
-    @Override
-    @NonNull
-    public Lifecycle getLifecycle() {
-        return mDispatcher.getLifecycle();
+    override fun getLifecycle(): Lifecycle {
+        return dispatcher.lifecycle
     }
 }
