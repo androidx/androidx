eaf88cdd85fd372351a6a5aa6ebd32f184417dd9
Fedor Kudasov <kudasov@google.com>
2022-12-19 13:19:53 +0000

    Add wear tiles preview adapter
    Bug: 227474520
    Relnote: New module with wear tiles tolling is added
    Test: TileServiceViewAdapterTest
    Change-Id: I2d7155beb26a7379d3dfd83a0dbea9ec1df68e08

---
+++ wear/tiles/tiles-tooling/src/main/java/androidx/wear/tiles/tooling/TileServiceViewAdapter.kt
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.tiles.tooling
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import androidx.wear.tiles.DeviceParametersBuilders
+import androidx.wear.tiles.LayoutElementBuilders
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.ResourceBuilders
+import androidx.wear.tiles.StateBuilders
+import androidx.wear.tiles.TileBuilders
+import androidx.wear.tiles.TileService
+import androidx.wear.tiles.TimelineBuilders
+import androidx.wear.tiles.renderer.TileRenderer
+import androidx.wear.tiles.timeline.TilesTimelineCache
+import com.google.common.util.concurrent.ListenableFuture
+import java.lang.reflect.Method
+import java.util.concurrent.TimeUnit
+import kotlin.math.roundToInt
+
+private const val TOOLS_NS_URI = "http://schemas.android.com/tools"
+
+/**
+ * A method extending functionality of [Class.getDeclaredMethod] allowing to finding the methods
+ * (including non-public) declared in the superclasses as well.
+ */
+internal fun Class<out Any>.findMethod(
+    name: String,
+    vararg parameterTypes: Class<out Any>
+): Method {
+    var currentClass: Class<out Any>? = this
+    while (currentClass != null) {
+        try {
+            return currentClass.getDeclaredMethod(name, *parameterTypes)
+        } catch (_: NoSuchMethodException) { }
+        currentClass = currentClass.superclass
+    }
+    val methodSignature = "$name(${parameterTypes.joinToString { ", " }})"
+    throw NoSuchMethodException(
+        "Could not find method $methodSignature neither in $this nor in its superclasses.")
+}
+
+/**
+ * View adapter that renders a class inheriting [TileService]. The class is found by reading the
+ * `tools:tileServiceName` attribute that contains the FQCN.
+ */
+internal class TileServiceViewAdapter(context: Context, attrs: AttributeSet) :
+    FrameLayout(context, attrs) {
+    init {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet) {
+        val tileServiceName = attrs.getAttributeValue(TOOLS_NS_URI, "tileServiceName") ?: return
+
+        init(tileServiceName)
+    }
+
+    @SuppressLint("BanUncheckedReflection")
+    @Suppress("UNCHECKED_CAST")
+    internal fun init(tileServiceName: String) {
+        val tileServiceClass = Class.forName(tileServiceName)
+
+        // val tileService = <TileServiceClassName>()
+        val tileService = tileServiceClass.getConstructor().newInstance() as TileService
+
+        // tileService.attachBaseContext(context)
+        val attachBaseContextMethod =
+            tileServiceClass
+                .findMethod("attachBaseContext", Context::class.java)
+                .apply { isAccessible = true }
+        attachBaseContextMethod.invoke(tileService, context)
+
+        val deviceParams = context.buildDeviceParameters()
+        val tileRequest = RequestBuilders.TileRequest
+            .Builder()
+            .setState(StateBuilders.State.Builder().build())
+            .setDeviceParameters(deviceParams)
+            .build()
+
+        // val tile = tileService.onTileRequest(tileRequest)
+        val onTileRequestMethod =
+            tileServiceClass
+                .findMethod("onTileRequest", RequestBuilders.TileRequest::class.java)
+                .apply { isAccessible = true }
+        val tile =
+            (onTileRequestMethod.invoke(tileService, tileRequest) as
+                ListenableFuture<TileBuilders.Tile>).get(1, TimeUnit.SECONDS)
+
+        val resourceRequest = RequestBuilders.ResourcesRequest
+            .Builder()
+            .setVersion(tile.resourcesVersion)
+            .setDeviceParameters(deviceParams)
+            .build()
+
+        // val resources = tileService.onResourcesRequest(resourceRequest).get(1, TimeUnit.SECONDS)
+        val onResourcesRequestMethod =
+            tileServiceClass
+                .findMethod("onResourcesRequest", RequestBuilders.ResourcesRequest::class.java)
+                .apply { isAccessible = true }
+        val resources =
+            (onResourcesRequestMethod.invoke(tileService, resourceRequest) as
+                ListenableFuture<ResourceBuilders.Resources>).get(1, TimeUnit.SECONDS)
+
+        val layout = tile.timeline?.getCurrentLayout()
+        if (layout != null) {
+            val renderer = TileRenderer(
+                context,
+                layout,
+                resources,
+                ContextCompat.getMainExecutor(context)
+            ) { }
+            renderer.inflate(this)?.apply {
+                (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
+            }
+        }
+    }
+}
+
+internal fun TimelineBuilders.Timeline?.getCurrentLayout(): LayoutElementBuilders.Layout? {
+    val now = System.currentTimeMillis()
+    return this?.let {
+        val cache = TilesTimelineCache(it)
+        cache.findTimelineEntryForTime(now) ?: cache.findClosestTimelineEntry(now)
+    }?.layout
+}
+
+/**
+ * Creates an instance of [DeviceParametersBuilders.DeviceParameters] from the [Context].
+ */
+internal fun Context.buildDeviceParameters(): DeviceParametersBuilders.DeviceParameters {
+    val displayMetrics = resources.displayMetrics
+    val isScreenRound = resources.configuration.isScreenRound
+    return DeviceParametersBuilders.DeviceParameters.Builder()
+        .setScreenWidthDp(
+            (displayMetrics.widthPixels / displayMetrics.density).roundToInt())
+        .setScreenHeightDp(
+            (displayMetrics.heightPixels / displayMetrics.density).roundToInt())
+        .setScreenDensity(displayMetrics.density)
+        .setScreenShape(
+            if (isScreenRound) DeviceParametersBuilders.SCREEN_SHAPE_ROUND
+            else DeviceParametersBuilders.SCREEN_SHAPE_RECT
+        )
+        .setDevicePlatform(DeviceParametersBuilders.DEVICE_PLATFORM_WEAR_OS)
+        .build()
+}
---
+++ wear/tiles/tiles-tooling/src/test/java/androidx/wear/tiles/tooling/FindMethodTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.tiles.tooling
+
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+
+@Suppress("unused")
+open class A {
+    open fun aOpen(): String { return "aOpen" }
+
+    fun aFinal(): String { return "aFinal" }
+
+    protected open fun aOpenProtected(): String { return "aOpenProtected" }
+
+    protected fun aFinalProtected(): String { return "aFinalProtected" }
+
+    protected open fun aOpenProtected2(): String { return "aOpenProtected2" }
+}
+
+@Suppress("unused")
+class B : A() {
+    override fun aOpen(): String { return "bOpen" }
+
+    override fun aOpenProtected(): String { return "bOpenProtected" }
+}
+
+class FindMethodTest {
+    @Test
+    fun testFindMethod() {
+        val a = A()
+        assertEquals("aOpen", A::class.java.findMethod("aOpen").invoke(a))
+        assertEquals("aFinal", A::class.java.findMethod("aFinal").invoke(a))
+        assertEquals("aOpenProtected", A::class.java.findMethod("aOpenProtected").invoke(a))
+        assertEquals("aFinalProtected", A::class.java.findMethod("aFinalProtected").invoke(a))
+        try {
+            A::class.java.findMethod("noSuchMethod")
+            fail()
+        } catch (_: NoSuchMethodException) { }
+
+        val b = B()
+        assertEquals("bOpen", B::class.java.findMethod("aOpen").invoke(b))
+        assertEquals("bOpenProtected", B::class.java.findMethod("aOpenProtected").invoke(b))
+        assertEquals("aFinal", B::class.java.findMethod("aFinal").invoke(b))
+        assertEquals("aOpenProtected2", B::class.java.findMethod("aOpenProtected2").invoke(b))
+    }
+}
