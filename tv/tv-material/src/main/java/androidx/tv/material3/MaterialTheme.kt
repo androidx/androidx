0151b4b2c1a51a61c64d4da7fe2bb979f4be9bff
Shubham Singh <shubhammsingh@google.com>
2022-12-19 17:01:02 +0530

    Introducing Theming in TV Compose
    The following have been forked from androidx.compose.material3
    
    1. MaterialTheme
    2. ColorScheme
    3. Shapes
    4. Tokens
    
    Not importing RippleTheme as we do not intend to have the ripple effect on TV
    
    Test: Tests are added in the upcoming Surface CL.
    Relnote: "Introducing Theming in TV Compose
    
    The following have been forked from androidx.compose.material3
    
    1. MaterialTheme
    2. ColorScheme
    3. Shapes
    4. Tokens
    "
    
    Change-Id: Ifeb74ccfeced09ea6000a8f114a4396d0f66ea6c

---
+++ tv/tv-material/src/main/java/androidx/tv/material3/MaterialTheme.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.tv.material3
+
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.tv.material.ExperimentalTvMaterialApi
+
+/**
+ * Material Theming refers to the customization of your Material Design app to better reflect your
+ * product’s brand.
+ *
+ * Material components such as Button and Checkbox use values provided here when retrieving
+ * default values.
+ *
+ * All values may be set by providing this component with the [colorScheme][ColorScheme],
+ * [typography][Typography] attributes. Use this to configure the overall
+ * theme of elements within this MaterialTheme.
+ *
+ * Any values that are not set will inherit the current value from the theme, falling back to the
+ * defaults if there is no parent MaterialTheme. This allows using a MaterialTheme at the top
+ * of your application, and then separate MaterialTheme(s) for different screens / parts of your
+ * UI, overriding only the parts of the theme definition that need to change.
+ *
+ * @param colorScheme A complete definition of the Material Color theme for this hierarchy
+ * @param shapes A set of corner shapes to be used as this hierarchy's shape system
+ * @param content The composable content that will be displayed with this theme
+ */
+@ExperimentalTvMaterialApi
+@Composable
+fun MaterialTheme(
+    colorScheme: ColorScheme = MaterialTheme.colorScheme,
+    shapes: Shapes = MaterialTheme.shapes,
+    content: @Composable () -> Unit
+) {
+    val rememberedColorScheme = remember {
+        // Explicitly creating a new object here so we don't mutate the initial [colorScheme]
+        // provided, and overwrite the values set in it.
+        colorScheme.copy()
+    }.apply {
+        updateColorSchemeFrom(colorScheme)
+    }
+    val selectionColors = rememberTextSelectionColors(rememberedColorScheme)
+    CompositionLocalProvider(
+        LocalColorScheme provides rememberedColorScheme,
+        LocalShapes provides shapes,
+        LocalTextSelectionColors provides selectionColors,
+        content = content
+    )
+}
+
+/**
+ * Contains functions to access the current theme values provided at the call site's position in
+ * the hierarchy.
+ */
+object MaterialTheme {
+    /**
+     * Retrieves the current [ColorScheme] at the call site's position in the hierarchy.
+     */
+    @ExperimentalTvMaterialApi
+    val colorScheme: ColorScheme
+        @Composable
+        @ReadOnlyComposable
+        @SuppressWarnings("HiddenTypeParameter", "UnavailableSymbol")
+        get() = LocalColorScheme.current
+
+    /**
+     * Retrieves the current [Shapes] at the call site's position in the hierarchy.
+     */
+    val shapes: Shapes
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalShapes.current
+}
+
+@OptIn(ExperimentalTvMaterialApi::class)
+@Composable
+/*@VisibleForTesting*/
+internal fun rememberTextSelectionColors(colorScheme: ColorScheme): TextSelectionColors {
+    val primaryColor = colorScheme.primary
+    return remember(primaryColor) {
+        TextSelectionColors(
+            handleColor = primaryColor,
+            backgroundColor = primaryColor.copy(alpha = TextSelectionBackgroundOpacity)
+        )
+    }
+}
+
+/*@VisibleForTesting*/
+internal const val TextSelectionBackgroundOpacity = 0.4f
