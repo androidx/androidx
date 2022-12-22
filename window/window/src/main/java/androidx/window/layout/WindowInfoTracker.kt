1978fd79975963e1982c851f998f442fbfe0e2f0
An An Yu <ananyu@google.com>
2022-12-22 18:22:51 +0000

    Add UiContext support to java WindowInfoTracker
    Bug:237342281
    Test: window:window-java:cAT
    Relnote: "Add a new method WindowInfoTrackerCallbackAdapter#addWindowLayoutInfo(context)
    and more documentation to make it available to extension developers."
    
    Change-Id: Ib3f8ac7ec62531ddfefb469d4cfe53c6710c48fb

--- window/window/src/main/java/androidx/window/layout/WindowInfoTracker.kt
+++ window/window/src/main/java/androidx/window/layout/WindowInfoTracker.kt
@@ -41,7 +41,13 @@ interface WindowInfoTracker {
      *
      * This method exports the same content as
      * [WindowLayoutInfo.windowLayoutInfo(activity: Activity)], but also supports non-Activity
-     * windows to receive [WindowLayoutInfo] updates.
+     * windows to receive [WindowLayoutInfo] updates. A [WindowLayoutInfo] value should be published
+     * when [DisplayFeature] have changed, but the behavior is ultimately decided by the hardware
+     * implementation. It is recommended to test the following scenarios:
+     *
+     *  * Values are emitted immediately after subscribing to this function.
+     *  * There is a long delay between subscribing and receiving the first value.
+     *  * Never receiving a value after subscription.
      *
      * A derived class may throw NotImplementedError if this method is not overridden.
      * Obtaining a [WindowInfoTracker] through [WindowInfoTracker.getOrCreate] guarantees having a
@@ -67,9 +73,14 @@ interface WindowInfoTracker {
      * contains a [List] of [DisplayFeature] that intersect the associated [android.view.Window].
      *
      * The first [WindowLayoutInfo] will not be emitted until [Activity.onStart] has been called.
-     * which values you receive and when is device dependent. It is recommended to test scenarios
-     * where there is a long delay between subscribing and receiving the first value or never
-     * receiving a value, since it may differ between hardware implementations.
+     * which values you receive and when is device dependent.
+     *
+     * It is recommended to test the following scenarios since behaviors may differ between hardware
+     * implementations:
+     *
+     *  * Values are emitted immediately after subscribing to this function.
+     *  * There is a long delay between subscribing and receiving the first value.
+     *  * Never receiving a value after subscription.
      *
      * Since the information is associated to the [Activity] you should not retain the [Flow] across
      * [Activity] recreations. Doing so can result in unpredictable behavior such as a memory leak
