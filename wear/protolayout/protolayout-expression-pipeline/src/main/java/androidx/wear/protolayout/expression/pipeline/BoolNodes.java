64a69afb1bbc5845a90f794b33f115fe71cfdd31
Neda Topoljanac <tneda@google.com>
2022-12-23 23:50:12 +0000

    Add helper classes for each expression type
    Bug: 263020163
    Test: TBA
    Relnote: "Helper classes for each expression type."
    Change-Id: I77965df05080f1b1d355776451f4f8b284cd3b1b

---
+++ wear/protolayout/protolayout-expression-pipeline/src/main/java/androidx/wear/protolayout/expression/pipeline/BoolNodes.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.protolayout.expression.pipeline;
+
+import android.util.Log;
+
+import androidx.annotation.UiThread;
+import androidx.wear.protolayout.expression.proto.DynamicProto;
+import androidx.wear.protolayout.expression.proto.DynamicProto.ComparisonFloatOp;
+import androidx.wear.protolayout.expression.proto.DynamicProto.ComparisonInt32Op;
+import androidx.wear.protolayout.expression.proto.DynamicProto.StateBoolSource;
+import androidx.wear.protolayout.expression.proto.FixedProto.FixedBool;
+
+/** Dynamic data nodes which yield boleans. */
+class BoolNodes {
+    private BoolNodes() {}
+
+    /** Dynamic boolean node that has a fixed value. */
+    static class FixedBoolNode implements DynamicDataSourceNode<Boolean> {
+        private final boolean mValue;
+        private final DynamicTypeValueReceiver<Boolean> mDownstream;
+
+        FixedBoolNode(FixedBool protoNode, DynamicTypeValueReceiver<Boolean> downstream) {
+            mValue = protoNode.getValue();
+            mDownstream = downstream;
+        }
+
+        @Override
+        @UiThread
+        public void preInit() {
+            mDownstream.onPreUpdate();
+        }
+
+        @Override
+        @UiThread
+        public void init() {
+            mDownstream.onData(mValue);
+        }
+
+        @Override
+        @UiThread
+        public void destroy() {}
+    }
+
+    /** Dynamic boolean node that gets value from the state. */
+    static class StateBoolNode extends StateSourceNode<Boolean> {
+        StateBoolNode(
+                ObservableStateStore stateStore,
+                StateBoolSource protoNode,
+                DynamicTypeValueReceiver<Boolean> downstream) {
+            super(
+                    stateStore,
+                    protoNode.getSourceKey(),
+                    se -> se.getBoolVal().getValue(),
+                    downstream);
+        }
+    }
+
+    /** Dynamic boolean node that gets value from comparing two integers. */
+    static class ComparisonInt32Node extends DynamicDataBiTransformNode<Integer, Integer, Boolean> {
+        private static final String TAG = "ComparisonInt32Node";
+
+        ComparisonInt32Node(
+                ComparisonInt32Op protoNode, DynamicTypeValueReceiver<Boolean> downstream) {
+            super(
+                    downstream,
+                    (lhs, rhs) -> {
+                        int unboxedLhs = lhs;
+                        int unboxedRhs = rhs;
+
+                        switch (protoNode.getOperationType()) {
+                            case COMPARISON_OP_TYPE_EQUALS:
+                                return unboxedLhs == unboxedRhs;
+                            case COMPARISON_OP_TYPE_NOT_EQUALS:
+                                return unboxedLhs != unboxedRhs;
+                            case COMPARISON_OP_TYPE_LESS_THAN:
+                                return unboxedLhs < unboxedRhs;
+                            case COMPARISON_OP_TYPE_LESS_THAN_OR_EQUAL_TO:
+                                return unboxedLhs <= unboxedRhs;
+                            case COMPARISON_OP_TYPE_GREATER_THAN:
+                                return unboxedLhs > unboxedRhs;
+                            case COMPARISON_OP_TYPE_GREATER_THAN_OR_EQUAL_TO:
+                                return unboxedLhs >= unboxedRhs;
+                            default:
+                                Log.e(TAG, "Unknown operation type in ComparisonInt32Node");
+                                return false;
+                        }
+                    });
+        }
+    }
+
+    /** Dynamic boolean node that gets value from comparing two floats. */
+    static class ComparisonFloatNode extends DynamicDataBiTransformNode<Float, Float, Boolean> {
+        private static final String TAG = "ComparisonFloatNode";
+        public static final float EPSILON = 1e-6f;
+
+        ComparisonFloatNode(
+                ComparisonFloatOp protoNode, DynamicTypeValueReceiver<Boolean> downstream) {
+            super(
+                    downstream,
+                    (lhs, rhs) -> {
+                        float unboxedLhs = lhs;
+                        float unboxedRhs = rhs;
+
+                        switch (protoNode.getOperationType()) {
+                            case COMPARISON_OP_TYPE_EQUALS:
+                                return equalFloats(unboxedLhs, unboxedRhs);
+                            case COMPARISON_OP_TYPE_NOT_EQUALS:
+                                return !equalFloats(unboxedLhs, unboxedRhs);
+                            case COMPARISON_OP_TYPE_LESS_THAN:
+                                return (unboxedLhs < unboxedRhs)
+                                        && !equalFloats(unboxedLhs, unboxedRhs);
+                            case COMPARISON_OP_TYPE_LESS_THAN_OR_EQUAL_TO:
+                                return (unboxedLhs < unboxedRhs)
+                                        || equalFloats(unboxedLhs, unboxedRhs);
+                            case COMPARISON_OP_TYPE_GREATER_THAN:
+                                return (unboxedLhs > unboxedRhs)
+                                        && !equalFloats(unboxedLhs, unboxedRhs);
+                            case COMPARISON_OP_TYPE_GREATER_THAN_OR_EQUAL_TO:
+                                return (unboxedLhs > unboxedRhs)
+                                        || equalFloats(unboxedLhs, unboxedRhs);
+                            default:
+                                Log.e(TAG, "Unknown operation type in ComparisonInt32Node");
+                                return false;
+                        }
+                    });
+        }
+
+        private static boolean equalFloats(float lhs, float rhs) {
+            return Math.abs(lhs - rhs) < EPSILON;
+        }
+    }
+
+    /** Dynamic boolean node that gets opposite value from another boolean node. */
+    static class NotBoolOp extends DynamicDataTransformNode<Boolean, Boolean> {
+        NotBoolOp(DynamicTypeValueReceiver<Boolean> downstream) {
+            super(downstream, b -> !b);
+        }
+    }
+
+    /** Dynamic boolean node that gets value from logical operation. */
+    static class LogicalBoolOp extends DynamicDataBiTransformNode<Boolean, Boolean, Boolean> {
+        private static final String TAG = "LogicalBooleanOp";
+
+        LogicalBoolOp(
+                DynamicProto.LogicalBoolOp protoNode,
+                DynamicTypeValueReceiver<Boolean> downstream) {
+            super(
+                    downstream,
+                    (a, b) -> {
+                        switch (protoNode.getOperationType()) {
+                            case LOGICAL_OP_TYPE_AND:
+                                return a && b;
+                            case LOGICAL_OP_TYPE_OR:
+                                return a || b;
+                            default:
+                                Log.e(TAG, "Unknown operation type in LogicalBoolOp");
+                                return false;
+                        }
+                    });
+        }
+    }
+}
---
+++ wear/protolayout/protolayout-expression-pipeline/src/main/java/androidx/wear/protolayout/expression/pipeline/ColorNodes.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.protolayout.expression.pipeline;
+
+import static androidx.wear.protolayout.expression.pipeline.AnimationsHelper.applyAnimationSpecToAnimator;
+
+import android.animation.ValueAnimator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
+import androidx.wear.protolayout.expression.proto.AnimationParameterProto.AnimationSpec;
+import androidx.wear.protolayout.expression.proto.DynamicProto.AnimatableFixedColor;
+import androidx.wear.protolayout.expression.proto.DynamicProto.StateColorSource;
+import androidx.wear.protolayout.expression.proto.FixedProto.FixedColor;
+
+/** Dynamic data nodes which yield colors. */
+class ColorNodes {
+    private ColorNodes() {}
+
+    /** Dynamic color node that has a fixed value. */
+    static class FixedColorNode implements DynamicDataSourceNode<Integer> {
+        private final int mValue;
+        private final DynamicTypeValueReceiver<Integer> mDownstream;
+
+        FixedColorNode(FixedColor protoNode, DynamicTypeValueReceiver<Integer> downstream) {
+            this.mValue = protoNode.getArgb();
+            this.mDownstream = downstream;
+        }
+
+        @Override
+        @UiThread
+        public void preInit() {
+            mDownstream.onPreUpdate();
+        }
+
+        @Override
+        @UiThread
+        public void init() {
+            mDownstream.onData(mValue);
+        }
+
+        @Override
+        public void destroy() {}
+    }
+
+    /** Dynamic color node that gets value from the platform source. */
+    static class StateColorSourceNode extends StateSourceNode<Integer> {
+        StateColorSourceNode(
+                ObservableStateStore observableStateStore,
+                StateColorSource protoNode,
+                DynamicTypeValueReceiver<Integer> downstream) {
+            super(
+                    observableStateStore,
+                    protoNode.getSourceKey(),
+                    se -> se.getColorVal().getArgb(),
+                    downstream);
+        }
+    }
+
+    /** Dynamic color node that gets animatable value from fixed source. */
+    static class AnimatableFixedColorNode extends AnimatableNode
+            implements DynamicDataSourceNode<Integer> {
+
+        private final AnimatableFixedColor mProtoNode;
+        private final DynamicTypeValueReceiver<Integer> mDownstream;
+
+        AnimatableFixedColorNode(
+                AnimatableFixedColor protoNode,
+                DynamicTypeValueReceiver<Integer> mDownstream,
+                QuotaManager quotaManager) {
+            super(quotaManager);
+            this.mProtoNode = protoNode;
+            this.mDownstream = mDownstream;
+        }
+
+        @Override
+        @UiThread
+        public void preInit() {
+            mDownstream.onPreUpdate();
+        }
+
+        @Override
+        @UiThread
+        public void init() {
+            ValueAnimator animator =
+                    ValueAnimator.ofArgb(mProtoNode.getFromArgb(), mProtoNode.getToArgb());
+            animator.addUpdateListener(a -> mDownstream.onData((Integer) a.getAnimatedValue()));
+
+            applyAnimationSpecToAnimator(animator, mProtoNode.getSpec());
+
+            mQuotaAwareAnimator.updateAnimator(animator);
+            startOrSkipAnimator();
+        }
+
+        @Override
+        @UiThread
+        public void destroy() {
+            mQuotaAwareAnimator.stopAnimator();
+        }
+    }
+
+    /** Dynamic color node that gets animatable value from dynamic source. */
+    static class DynamicAnimatedColorNode extends AnimatableNode
+            implements DynamicDataNode<Integer> {
+
+        final DynamicTypeValueReceiver<Integer> mDownstream;
+        private final DynamicTypeValueReceiver<Integer> mInputCallback;
+
+        @Nullable Integer mCurrentValue = null;
+        int mPendingCalls = 0;
+
+        // Static analysis complains about calling methods of parent class AnimatableNode under
+        // initialization but mInputCallback is only used after the constructor is finished.
+        @SuppressWarnings("method.invocation.invalid")
+        DynamicAnimatedColorNode(
+                DynamicTypeValueReceiver<Integer> downstream,
+                @NonNull AnimationSpec spec,
+                QuotaManager quotaManager) {
+            super(quotaManager);
+            this.mDownstream = downstream;
+            this.mInputCallback =
+                    new DynamicTypeValueReceiver<Integer>() {
+                        @Override
+                        public void onPreUpdate() {
+                            mPendingCalls++;
+
+                            if (mPendingCalls == 1) {
+                                mDownstream.onPreUpdate();
+
+                                mQuotaAwareAnimator.resetAnimator();
+                            }
+                        }
+
+                        @Override
+                        public void onData(@NonNull Integer newData) {
+                            if (mPendingCalls > 0) {
+                                mPendingCalls--;
+                            }
+
+                            if (mPendingCalls == 0) {
+                                if (mCurrentValue == null) {
+                                    mCurrentValue = newData;
+                                    mDownstream.onData(mCurrentValue);
+                                } else {
+                                    ValueAnimator animator =
+                                            ValueAnimator.ofArgb(mCurrentValue, newData);
+
+                                    applyAnimationSpecToAnimator(animator, spec);
+                                    animator.addUpdateListener(
+                                            a -> {
+                                                if (mPendingCalls == 0) {
+                                                    mCurrentValue = (Integer) a.getAnimatedValue();
+                                                    mDownstream.onData(mCurrentValue);
+                                                }
+                                            });
+
+                                    mQuotaAwareAnimator.updateAnimator(animator);
+                                    startOrSkipAnimator();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onInvalidated() {
+                            if (mPendingCalls > 0) {
+                                mPendingCalls--;
+                            }
+
+                            if (mPendingCalls == 0) {
+                                mCurrentValue = null;
+                                mDownstream.onInvalidated();
+                            }
+                        }
+                    };
+        }
+
+        public DynamicTypeValueReceiver<Integer> getInputCallback() {
+            return mInputCallback;
+        }
+    }
+}
