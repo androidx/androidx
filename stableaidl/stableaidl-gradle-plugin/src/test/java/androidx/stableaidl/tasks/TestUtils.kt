09784f3b7e748fde7c372c5087ee67e2a7c66a67
Alan Viverette <alanv@google.com>
2023-01-04 14:47:01 -0500

    Avoid using internal AGP APIs in Stable AIDL plugin
    Package headers using an AAR transformation, rather than obtaining the
    directory from AGP's AidlCompile task.
    
    Obtain the AIDL executable and framework paths by constructing them
    ourselves, rather than pulling them from AGP's internal classes.
    
    Bug: 253427301
    Test: StableAidlPackageApiTest
    Change-Id: Ia7cac47bdef33ab933361e577a38fc349666f85e

---
+++ stableaidl/stableaidl-gradle-plugin/src/test/java/androidx/stableaidl/tasks/TestUtils.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.stableaidl.tasks
+
+import java.io.File
+import java.util.zip.ZipFile
+
+internal fun createFile(name: String, parent: File): File {
+    val newFile = parent.resolve(name)
+    newFile.parentFile.mkdirs()
+    newFile.createNewFile()
+    return newFile
+}
+
+internal fun ZipFile.getEntryNames(): List<String> {
+    val flattened = mutableListOf<String>()
+    entries().iterator().forEach { entry ->
+        flattened += entry.name
+    }
+    return flattened
+}
