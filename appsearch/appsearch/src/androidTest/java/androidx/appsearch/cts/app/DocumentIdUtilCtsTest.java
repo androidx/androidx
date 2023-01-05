cafd0103a9d8d5b89d4d5ed476a97b3a7e248c32
Alex Saveliev <alexsav@google.com>
2023-01-04 16:54:29 -0800

    Moves DocumentIdUtilCtsTest to cts folder
    This is needed for API coverage
    
    Test: DocumentIdUtilCtsTest.java
    Bug: 264468097
    Change-Id: I50b7532824da92d37b3a964d53d84e4851f80086

--- appsearch/appsearch/src/androidTest/java/androidx/appsearch/cts/app/DocumentIdUtilCtsTest.java
+++ appsearch/appsearch/src/androidTest/java/androidx/appsearch/cts/app/DocumentIdUtilCtsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,11 @@
  * limitations under the License.
  */
 
-package androidx.appsearch.app;
+package androidx.appsearch.cts.app;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import androidx.appsearch.app.GenericDocument;
 import androidx.appsearch.util.DocumentIdUtil;
 
 import org.junit.Test;
