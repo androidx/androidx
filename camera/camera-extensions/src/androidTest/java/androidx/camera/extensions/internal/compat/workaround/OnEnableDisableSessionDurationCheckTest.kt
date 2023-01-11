7c3e8e19a5be571f88085f0a4653130cbb7a6410
Scott Nien <scottnien@google.com>
2023-01-11 23:17:30 +0800

    [Extensions] Fixed surface abandoned crash when pause/resume too quickly on Samsung Z Fold2
    This cl is similar as the reverted CL aosp/2207335 as I think it
    is still necessary to phase in to make the stability on par
    with Camera2 Extensions. We enable the workaround only
    on Samsung devices.
    
    In camera2 Extensions, all camera event will be passed through
    AIDL which has the extra delay so it avoids the crash without
    any efforts.
    
    Root cause:
    On Samsung Z Fold2, when Extensions are enabled, if pause / resume
    camera too quickly, it might cause surface abandoned crash.
    
    Samsung will start the initialization at onEnableSession,
    and release the resource at onDisableSession. The issue is
    caused by onDisableSession being invoked too early (within
    50ms) and could destroy the ImageWriter surface while it
    is still used in the initialization.
    
    The workaround is to ensure onDisableSession being called in
    at least 100ms after onEnableSession. It will sleep the
    camera thread for a while to achieve this. Though the CL
    could cause some delay when shuting down the camera,
    it is not the normal user operation to close the camera
    and open the camera quickly within 100ms. This normally
    happens in auto testing. So the impacts for the user
    is almost none.
    
    Relnote: "Fixed the Extensions crash that happens on some Samsung
    devices when pausing/resuming the app too quickly"
    Bug: 264502486
    Bug: 245226085
    Test: extensions/OpenCloseCaptureSessionStressTest, OnEnableDisableSessionDurationCheckTest
    
    Change-Id: Iaca262a499d7e8e72c5072ea7063943dd291b4f2

---
+++ camera/camera-extensions/src/androidTest/java/androidx/camera/extensions/internal/compat/workaround/OnEnableDisableSessionDurationCheckTest.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.extensions.internal.compat.workaround
+
+import android.os.SystemClock
+import androidx.camera.extensions.internal.compat.workaround.OnEnableDisableSessionDurationCheck.MIN_DURATION_FOR_ENABLE_DISABLE_SESSION
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.filters.SmallTest
+import com.google.common.collect.Range
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 21)
+class OnEnableDisableSessionDurationCheckTest {
+    companion object {
+        const val TOLERANCE = 15L
+    }
+
+    @Test
+    fun enabled_ensureMinimalDuration() {
+        // Arrange
+        val check = OnEnableDisableSessionDurationCheck(/* enabledMinimumDuration */true)
+
+        val duration = 40L
+        // Act
+        val startTime = SystemClock.elapsedRealtime()
+        check.onEnableSessionInvoked()
+        Thread.sleep(duration)
+        check.onDisableSessionInvoked()
+        val endTime = SystemClock.elapsedRealtime()
+
+        // Assert
+        assertThat((endTime - startTime))
+            .isIn(
+                Range.closed(
+                    MIN_DURATION_FOR_ENABLE_DISABLE_SESSION,
+                    MIN_DURATION_FOR_ENABLE_DISABLE_SESSION + TOLERANCE
+                ))
+    }
+
+    @Test
+    fun enabled_doNotWaitExtraIfDurationExceeds() {
+        // 1. Arrange
+        val check = OnEnableDisableSessionDurationCheck(/* enabledMinimumDuration */true)
+
+        // make the duration of onEnable to onDisable to be the minimal duration.
+        val duration = MIN_DURATION_FOR_ENABLE_DISABLE_SESSION
+
+        // 2. Act
+        val startTime = SystemClock.elapsedRealtime()
+        check.onEnableSessionInvoked()
+        // make the duration of onEnable to onDisable to be the minimal duration.
+        Thread.sleep(duration)
+        check.onDisableSessionInvoked()
+        val endTime = SystemClock.elapsedRealtime()
+
+        // 3. Assert: no extra time waited.
+        assertThat((endTime - startTime))
+            .isLessThan(
+                duration + TOLERANCE
+            )
+    }
+
+    @Test
+    fun disabled_doNotWait() {
+        // 1. Arrange
+        val check = OnEnableDisableSessionDurationCheck(/* enabledMinimumDuration */ false)
+
+        // 2. Act
+        val startTime = SystemClock.elapsedRealtime()
+        check.onEnableSessionInvoked()
+        check.onDisableSessionInvoked()
+        val endTime = SystemClock.elapsedRealtime()
+
+        // 3. Assert
+        assertThat((endTime - startTime))
+            .isLessThan(TOLERANCE)
+    }
+}
