a6ec8cc8df1fd6e8a4407ad15569d77914e9adb3
Ian Lake <ilake@google.com>
2023-01-25 00:05:20 +0000

    Move OnBackPressedDispatcher extension to main artifact
    As the main activity artifact is automatically included
    whenever a developer depends on activity-ktx and these are
    in an atomic library group, we can move the Kotlin
    extension from activity-ktx to activity itself now that
    the class itself has been moved to Kotlin.
    
    Relnote: n/a
    Test: ./gradlew checkApi
    BUG: 257291701
    Change-Id: I13482de2e25bd6e084f4b299dddd43b2bcf7a4c7

--- activity/activity/src/main/java/androidx/activity/OnBackPressedDispatcher.kt
+++ activity/activity/src/main/java/androidx/activity/OnBackPressedDispatcher.kt
@@ -287,3 +287,31 @@ class OnBackPressedDispatcher @JvmOverloads constructor(
         }
     }
 }
+
+/**
+ * Create and add a new [OnBackPressedCallback] that calls [onBackPressed] in
+ * [OnBackPressedCallback.handleOnBackPressed].
+ *
+ * If an [owner] is specified, the callback will only be added when the Lifecycle is
+ * [androidx.lifecycle.Lifecycle.State.STARTED].
+ *
+ * A default [enabled] state can be supplied.
+ */
+@Suppress("RegistrationName")
+fun OnBackPressedDispatcher.addCallback(
+    owner: LifecycleOwner? = null,
+    enabled: Boolean = true,
+    onBackPressed: OnBackPressedCallback.() -> Unit
+): OnBackPressedCallback {
+    val callback = object : OnBackPressedCallback(enabled) {
+        override fun handleOnBackPressed() {
+            onBackPressed()
+        }
+    }
+    if (owner != null) {
+        addCallback(owner, callback)
+    } else {
+        addCallback(callback)
+    }
+    return callback
+}
