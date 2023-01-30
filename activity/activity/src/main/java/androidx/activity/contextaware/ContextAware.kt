e4a7c287c679487bb576d4865be865b4364dc0c6
Ian Lake <ilake@google.com>
2023-01-30 23:37:12 +0000

    Move ContextAware Kotlin extension to activity artifact
    Migrate the ContextAware.withContextAvailable extension
    to the main activity artifact to ensure there is only one
    ContextAware.kt class.
    
    Relnote: "The `ContextAware.withContextAvailable` Kotlin
    extension has been moved from `activity-ktx` to `activity`."
    Test: moved tests still work
    BUG: 257291701
    
    Change-Id: Iada9260aa0d006aeea976d08713e171285848d7f

--- activity/activity/src/main/java/androidx/activity/contextaware/ContextAware.kt
+++ activity/activity/src/main/java/androidx/activity/contextaware/ContextAware.kt
@@ -16,6 +16,7 @@
 package androidx.activity.contextaware
 
 import android.content.Context
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 /**
  * A `ContextAware` class is associated with a [Context] sometime after
@@ -59,4 +60,34 @@ interface ContextAware {
      * @see addOnContextAvailableListener
      */
     fun removeOnContextAvailableListener(listener: OnContextAvailableListener)
+}
+
+/**
+ * Run [onContextAvailable] when the [Context] becomes available and
+ * resume with the result.
+ *
+ * If the [Context] is already available, [onContextAvailable] will be
+ * synchronously called on the current coroutine context. Otherwise,
+ * [onContextAvailable] will be called on the UI thread immediately when
+ * the Context becomes available.
+ */
+suspend inline fun <R> ContextAware.withContextAvailable(
+    crossinline onContextAvailable: (@JvmSuppressWildcards Context) -> @JvmSuppressWildcards R
+): @JvmSuppressWildcards R {
+    val availableContext = peekAvailableContext()
+    return if (availableContext != null) {
+        onContextAvailable(availableContext)
+    } else {
+        suspendCancellableCoroutine { co ->
+            val listener = object : OnContextAvailableListener {
+                override fun onContextAvailable(context: Context) {
+                    co.resumeWith(runCatching { onContextAvailable(context) })
+                }
+            }
+            addOnContextAvailableListener(listener)
+            co.invokeOnCancellation {
+                removeOnContextAvailableListener(listener)
+            }
+        }
+    }
 }
