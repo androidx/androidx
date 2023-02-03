d067df615215d9e233eed5f59bd09819c0795ee4
Aurimas Liutikas <aurimas@google.com>
2023-02-03 11:56:06 -0800

    Add ability to specify additional device test apk keys
    It will be used by webkit tests to preinstall chrome for testing.
    
    Test: ./gradlew webkit:webkit:GenerateTestConfiguration
          ../../out/androidx/build/test-xml-configs/webkit-webkitdebugAndroidTest.json
          contains the new key
    
    Change-Id: I2b1fda8ada4d29f296fc313af6068c4724c79039

--- buildSrc-tests/src/test/kotlin/androidx/build/testConfiguration/AndroidTestConfigBuilderTest.kt
+++ buildSrc-tests/src/test/kotlin/androidx/build/testConfiguration/AndroidTestConfigBuilderTest.kt
@@ -87,6 +87,36 @@ class AndroidTestConfigBuilderTest {
                       "key": "notAnnotation",
                       "value": "androidx.test.filters.FlakyTest"
                     }
+                  ],
+                  "additionalApkKeys": []
+                }
+            """.trimIndent()
+            )
+        )
+    }
+
+    @Test
+    fun testJsonAgainstGoldenAdditionalApkKey() {
+        builder.additionalApkKeys(listOf("customKey"))
+        MatcherAssert.assertThat(
+            builder.buildJson(),
+            CoreMatchers.`is`("""
+                {
+                  "name": "placeHolderAndroidTest.xml",
+                  "minSdkVersion": "15",
+                  "testSuiteTags": [
+                    "placeholder_tag"
+                  ],
+                  "testApk": "placeholder.apk",
+                  "testApkSha256": "123456",
+                  "instrumentationArgs": [
+                    {
+                      "key": "notAnnotation",
+                      "value": "androidx.test.filters.FlakyTest"
+                    }
+                  ],
+                  "additionalApkKeys": [
+                    "customKey"
                   ]
                 }
             """.trimIndent()
@@ -118,7 +148,8 @@ class AndroidTestConfigBuilderTest {
                       "key": "androidx.benchmark.dryRunMode.enable",
                       "value": "true"
                     }
-                  ]
+                  ],
+                  "additionalApkKeys": []
                 }
             """.trimIndent()
             )
@@ -147,7 +178,8 @@ class AndroidTestConfigBuilderTest {
                       "key": "notAnnotation",
                       "value": "androidx.test.filters.FlakyTest"
                     }
-                  ]
+                  ],
+                  "additionalApkKeys": []
                 }
             """.trimIndent()
             )
