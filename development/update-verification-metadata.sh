eaac29c028f5430fd962834c1100247b4828714a
Jeff Gaston <jeffrygaston@google.com>
2023-02-07 15:11:00 -0500

    update-verification-metadata.sh no longer excluding certain parts of the file
    This change should allow the behavior of update-verification-metadata.sh to be clearer
    
    Excluding certain parts of the file shouldn't be needed anymore now that https://github.com/gradle/gradle/issues/20260 is resolved
    
    Bug: 268231467
    
    Test: development/update-verification-metadata.sh
    Test: Treehugger runs busytown/*.sh
    
    Change-Id: Icf359da8d137406cc3a2fe7fa755c86314e20a29

--- development/update-verification-metadata.sh
+++ development/update-verification-metadata.sh
@@ -17,32 +17,13 @@ function regenerateVerificationMetadata() {
   echo "regenerating verification metadata and keyring"
   # regenerate metadata
   # Need to run a clean build, https://github.com/gradle/gradle/issues/19228
-  runGradle --write-verification-metadata pgp,sha256 --export-keys --dry-run --clean bOS
-  # extract and keep only the <trusted-keys> section
-  WORK_DIR=gradle/update-keys-temp
-  rm -rf "$WORK_DIR"
-  mkdir -p "$WORK_DIR"
-
-  # extract the middle of the new file, https://github.com/gradle/gradle/issues/18569
-  grep -B 10000 "<trusted-keys>" gradle/verification-metadata.dryrun.xml > "$WORK_DIR/new.head"
-  grep -A 10000 "</trusted-keys>" gradle/verification-metadata.dryrun.xml > "$WORK_DIR/new.tail"
-  numTopLines="$(cat "$WORK_DIR/new.head" | wc -l)"
-  numTopLinesPlus1="$(($numTopLines + 1))"
-  numBottomLines="$(cat "$WORK_DIR/new.tail" | wc -l)"
-  numLines="$(cat gradle/verification-metadata.dryrun.xml | wc -l)"
-  numMiddleLines="$(($numLines - $numTopLines - $numBottomLines))"
-  # also remove 'version=' lines, https://github.com/gradle/gradle/issues/20192
-  cat gradle/verification-metadata.dryrun.xml | tail -n "+$numTopLinesPlus1" | head -n "$numMiddleLines" | sed 's/ version="[^"]*"//' > "$WORK_DIR/new.middle"
-
-  # extract the top and bottom of the old file
-  grep -B 10000 "<trusted-keys>" gradle/verification-metadata.xml > "$WORK_DIR/old.head"
-  grep -A 10000 "</trusted-keys>" gradle/verification-metadata.xml > "$WORK_DIR/old.tail"
+  runGradle --stacktrace --write-verification-metadata pgp,sha256 --export-keys --dry-run --clean bOS
 
   # update verification metadata file
-  cat "$WORK_DIR/old.head" "$WORK_DIR/new.middle" "$WORK_DIR/old.tail" > gradle/verification-metadata.xml
+  # also remove 'version=' lines, https://github.com/gradle/gradle/issues/20192
+  cat gradle/verification-metadata.dryrun.xml | sed 's/ \(trusted-key.*\)version="[^"]*"/\1/' > gradle/verification-metadata.xml
 
   # remove temporary files
-  rm -rf "$WORK_DIR"
   rm -f gradle/verification-keyring-dryrun.gpg
   rm -f gradle/verification-keyring-dryrun.keys
   rm -f gradle/verification-metadata.dryrun.xml
@@ -50,4 +31,4 @@ function regenerateVerificationMetadata() {
 regenerateVerificationMetadata
 
 echo
-echo "Done. Please check that these changes look correct ('git diff')"
+echo 'Done. Please check that these changes look correct (`git diff`)'
