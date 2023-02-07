17765877ebe625e01bc22da7f9785ba3b5ecb6ad
Daniel Peykov <peykov@google.com>
2023-02-07 17:37:07 +0000

    Further improve test compatibility with older APIs
    * Avoid testing quick settings when it is not reliably present.
    * Wait for focused/checked/selected status changes to occur.
    * Use `pm list packages` to reliably test shell commands.
    
    Bug: 266744346
    Bug: 266744658
    Test: heavy presubmit
    Change-Id: If73c0985ec62712ce3ae5e4b40f8b8a6ef96dd32

--- test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/UiDeviceTest.java
+++ test/uiautomator/integration-tests/testapp/src/androidTest/java/androidx/test/uiautomator/testapp/UiDeviceTest.java
@@ -280,6 +280,7 @@ public class UiDeviceTest extends BaseTest {
     }
 
     @Test
+    @SdkSuppress(minSdkVersion = 21) // Quick settings menu might not be present prior to API 21.
     public void testOpenQuickSettings() {
         mDevice.openQuickSettings();
 
