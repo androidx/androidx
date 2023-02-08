89b4c45ba74dfb3eb7a702c4534140074e8f3cd5
Chris Craik <ccraik@google.com>
2023-02-08 13:41:13 -0800

    Support cmd package compile --reset on Android 14
    Fixes: 249143766
    Test: TrivialStartupBenchmark
    Test: CompilationModeTest
    Relnote: "On Android 14+, Macrobenchmark no longer reinstalls target
    applications to reset compilation state, thanks to a new platform
    feature. Previously it was necessary to have a rooted device, or to
    deal with all application state (e.g. login) being removed before each
    benchmark runs."
    
    Also adds clarifying error to internal legacy metric failure occurring
    on Android 14.
    
    Change-Id: I9b08cfecc36735d057b388e75d1c9b50b53ed083

--- benchmark/benchmark-macro/src/main/java/androidx/benchmark/macro/CompilationMode.kt
+++ benchmark/benchmark-macro/src/main/java/androidx/benchmark/macro/CompilationMode.kt
@@ -29,6 +29,7 @@ import androidx.benchmark.macro.CompilationMode.Ignore
 import androidx.benchmark.macro.CompilationMode.None
 import androidx.benchmark.macro.CompilationMode.Partial
 import androidx.benchmark.userspaceTrace
+import androidx.core.os.BuildCompat
 import androidx.profileinstaller.ProfileInstallReceiver
 import org.junit.AssumptionViolatedException
 
@@ -72,6 +73,7 @@ import org.junit.AssumptionViolatedException
  * to compile the target app).
  */
 sealed class CompilationMode {
+    @androidx.annotation.OptIn(markerClass = [BuildCompat.PrereleaseSdkCheck::class])
     internal fun resetAndCompile(
         packageName: String,
         killProcessBlock: () -> Unit,
@@ -88,7 +90,7 @@ sealed class CompilationMode {
                     // The flag `enablePackageReset` can be set to `true` on `userdebug` builds in
                     // order to speed-up the profile reset. When set to false, reset is performed
                     // uninstalling and reinstalling the app.
-                    if (Shell.isSessionRooted()) {
+                    if (BuildCompat.isAtLeastU() || Shell.isSessionRooted()) {
                         // Package reset enabled
                         Log.d(TAG, "Re-compiling $packageName")
                         // cmd package compile --reset returns a "Success" or a "Failure" to stdout.
@@ -102,7 +104,7 @@ sealed class CompilationMode {
                             "Unable to recompile $packageName ($output)"
                         }
                     } else {
-                        // User builds. Kick off a full uninstall-reinstall
+                        // User builds pre-U. Kick off a full uninstall-reinstall
                         Log.d(TAG, "Reinstalling $packageName")
                         reinstallPackage(packageName)
                     }
