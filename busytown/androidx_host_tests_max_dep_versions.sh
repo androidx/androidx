b114219b2bc5188755adff847a950f9e6fc473fe
Fred Sladkey <fsladkey@google.com>
2023-02-14 13:24:55 -0500

    Enable native KMP targets by default.
    Bug: 269250443
    Test: Existing builds succeed
    Change-Id: I8e8b9eccc3eddd3caa23d7c100db10a82504468b

--- busytown/androidx_host_tests_max_dep_versions.sh
+++ busytown/androidx_host_tests_max_dep_versions.sh
@@ -7,7 +7,6 @@ cd "$(dirname $0)"
 
 impl/build.sh test linuxX64Test -Pandroidx.useMaxDepVersions \
     -Pandroidx.displayTestOutput=false \
-    -Pandroidx.enabled.kmp.target.platforms=+native \
     -Pandroidx.ignoreTestFailures "$@"
 
 echo "Completing $0 at $(date)"
