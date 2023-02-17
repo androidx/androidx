4500df27e38149a50c3e627f8fa2bbae86ce73e0
Marcello Albano <maralb@google.com>
2023-02-17 20:56:51 +0000

    Revert "Revert "Baseline Profile Gradle Plugin - Part 2""
    This reverts commit 89f99445d0e38f97c5ea801f49994f5bd7b77af7.
    
    Reason for revert: Reapplying after bump to AGP 8.0-beta3
    
    Change-Id: Iaeb3845912ea67bd966146e86271677add4892c8

--- benchmark/baseline-profiles-gradle-plugin/src/main/kotlin/androidx/baselineprofiles/gradle/producer/BaselineProfilesProducerPlugin.kt
+++ benchmark/baseline-profiles-gradle-plugin/src/main/kotlin/androidx/baselineprofiles/gradle/producer/BaselineProfilesProducerPlugin.kt
@@ -21,9 +21,13 @@ import androidx.baselineprofiles.gradle.utils.ATTRIBUTE_CATEGORY_BASELINE_PROFIL
 import androidx.baselineprofiles.gradle.utils.ATTRIBUTE_FLAVOR
 import androidx.baselineprofiles.gradle.utils.BUILD_TYPE_BASELINE_PROFILE_PREFIX
 import androidx.baselineprofiles.gradle.utils.CONFIGURATION_NAME_BASELINE_PROFILES
+import androidx.baselineprofiles.gradle.utils.INTERMEDIATES_BASE_FOLDER
 import androidx.baselineprofiles.gradle.utils.camelCase
+import androidx.baselineprofiles.gradle.utils.checkAgpVersion
 import androidx.baselineprofiles.gradle.utils.createBuildTypeIfNotExists
 import androidx.baselineprofiles.gradle.utils.createNonObfuscatedBuildTypes
+import androidx.baselineprofiles.gradle.utils.isGradleSyncRunning
+import com.android.build.api.dsl.TestBuildType
 import com.android.build.api.variant.TestAndroidComponentsExtension
 import com.android.build.gradle.TestExtension
 import org.gradle.api.GradleException
@@ -36,18 +40,46 @@ import org.gradle.api.attributes.Category
  * This is the producer plugin for baseline profile generation. In order to generate baseline
  * profiles three plugins are needed: one is applied to the app or the library that should consume
  * the baseline profile when building (consumer), one is applied to the project that should supply
- * the apk under test (build provider) and the last one is applied to a library module containing
+ * the apk under test (build provider) and the last one is applied to a test module containing
  * the ui test that generate the baseline profile on the device (producer).
  */
 class BaselineProfilesProducerPlugin : Plugin<Project> {
 
+    companion object {
+        private const val COLLECT_TASK_NAME = "collect"
+    }
+
     override fun apply(project: Project) {
+        var foundTestPlugin = false
         project.pluginManager.withPlugin("com.android.test") {
+            foundTestPlugin = true
             configureWithAndroidPlugin(project = project)
+        }
+
+        // Only used to verify that the android test plugin has been applied.
+        project.afterEvaluate {
+            if (!project.isGradleSyncRunning()) {
+                if (!foundTestPlugin) {
+                    throw IllegalStateException(
+                        """
+                    The module ${project.name} does not have the `com.android.test` plugin
+                    applied. The `androidx.baselineprofiles.producer` plugin supports only android
+                    test modules. Please review your build.gradle to ensure this plugin is applied
+                    to the correct module.
+                    """.trimIndent()
+                    )
+                }
+                project.logger.debug(
+                    "[BaselineProfilesProducerPlugin] afterEvaluate check: app plugin was applied"
+                )
+            }
         }
     }
 
     private fun configureWithAndroidPlugin(project: Project) {
+
+        // Checks that the required AGP version is applied to this project.
+        project.checkAgpVersion()
 
         // Prepares extensions used by the plugin
         val baselineProfilesExtension =
@@ -62,42 +94,51 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
         testExtension.experimentalProperties["android.experimental.self-instrumenting"] = true
 
         // Creates the new build types to match the build provider. Note that release does not
-        // exist by default so we need to create nonObfuscatedRelease and map it manually to
+        // exist by default so we need to create nonMinifiedRelease and map it manually to
         // `release`. All the existing build types beside `debug`, that is the default one, are
         // added manually in the configuration so we can assume they've been added for the purpose
-        // of generating baseline profiles. We don't need to create a nonObfuscated build type from
-        // `debug`.
+        // of generating baseline profiles. We don't need to create a nonMinified build type from
+        // `debug` since there will be no matching configuration with the apk provider module.
 
         val nonObfuscatedReleaseName = camelCase(BUILD_TYPE_BASELINE_PROFILE_PREFIX, "release")
         val extendedTypeToOriginalTypeMapping = mutableMapOf(nonObfuscatedReleaseName to "release")
 
         testAndroidComponent.finalizeDsl { ext ->
 
+            // The test build types need to be debuggable and have the same singing config key to
+            // be installed. We also disable the test coverage tracking since it's not important
+            // here.
+            val configureBlock: TestBuildType.() -> (Unit) = {
+                isDebuggable = true
+                enableAndroidTestCoverage = false
+                enableUnitTestCoverage = false
+                signingConfig = ext.buildTypes.getByName("debug").signingConfig
+                matchingFallbacks += listOf("release")
+            }
+
+            // The variant names are used by the test module to request a specific apk artifact to
+            // the under test app module (using configuration attributes). This is all handled by
+            // the com.android.test plugin, as long as both modules have the same variants.
+            // Unfortunately the test module cannot determine which variants are present in the
+            // under test app module. As a result we need to replicate the same build types and
+            // flavors, so that the same variant names are created.
             createNonObfuscatedBuildTypes(
                 project = project,
                 extension = ext,
                 extendedBuildTypeToOriginalBuildTypeMapping = extendedTypeToOriginalTypeMapping,
+                configureBlock = configureBlock,
                 filterBlock = {
-                    // TODO: Which build types to skip. In theory we want to skip only debug because
-                    //  it's the default one. All the ones that have been manually added should be
-                    //  considered for this.
+                    // All the build types that have been added to the test module be extended.
+                    // This is because we can't know here which ones are actually release in the
+                    // under test module.
                     it.name != "debug"
                 },
-                configureBlock = {
-                    enableAndroidTestCoverage = false
-                    enableUnitTestCoverage = false
-                },
             )
-
             createBuildTypeIfNotExists(
                 project = project,
                 extension = ext,
                 buildTypeName = nonObfuscatedReleaseName,
-                configureBlock = {
-                    enableAndroidTestCoverage = false
-                    enableUnitTestCoverage = false
-                    matchingFallbacks += listOf("release")
-                }
+                configureBlock = configureBlock
             )
         }
 
@@ -119,9 +160,13 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
 
                 // Creating configurations only for the extended build types.
                 if (it.buildType == null ||
-                    it.buildType !in extendedTypeToOriginalTypeMapping.keys) {
+                    it.buildType !in extendedTypeToOriginalTypeMapping.keys
+                ) {
                     return@onVariants
                 }
+
+                val flavorName =
+                    if (it.flavorName == null || it.flavorName!!.isEmpty()) null else it.flavorName
 
                 // Creates the configuration to handle this variant. Note that in the attributes
                 // to match the configuration we use the original build type without `nonObfuscated`.
@@ -129,7 +174,7 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
                 val configurationName = createBaselineProfileConfigurationForVariant(
                     project = project,
                     variantName = it.name,
-                    flavorName = it.flavorName ?: "",
+                    flavorName = flavorName,
                     originalBuildTypeName = originalBuildTypeName
                 )
 
@@ -138,7 +183,7 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
                     createTasksForVariant(
                         project = project,
                         variantName = it.name,
-                        flavorName = it.flavorName ?: "",
+                        flavorName = flavorName,
                         configurationName = configurationName,
                         baselineProfilesExtension = baselineProfilesExtension
                     )
@@ -159,7 +204,7 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
     private fun createTasksForVariant(
         project: Project,
         variantName: String,
-        flavorName: String,
+        flavorName: String?,
         configurationName: String,
         baselineProfilesExtension: BaselineProfilesProducerExtension
     ) {
@@ -178,44 +223,67 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
         // The test task runs the ui tests
         val testTasks = devices.map {
             try {
-                project.tasks.named(camelCase(it, variantName, "androidTest"))
+                project.tasks.named(camelCase(it, variantName, "androidTest")).apply {
+                    configure { t ->
+                        // TODO: this is a bit hack-ish but we can rewrite if we decide to keep the
+                        //  configuration [BaselineProfilesProducerExtension.enableEmulatorDisplay]
+                        if (t.hasProperty("enableEmulatorDisplay")) {
+                            t.setProperty(
+                                "enableEmulatorDisplay",
+                                baselineProfilesExtension.enableEmulatorDisplay
+                            )
+                        }
+                    }
+                }
             } catch (e: UnknownTaskException) {
+
+                // If gradle is syncing don't throw any exception and simply stop here. This plugin
+                // will fail at build time instead. This allows not breaking project sync in ide.
+                if (project.isGradleSyncRunning()) {
+                    return
+                }
+
                 throw GradleException(
                     """
                     It wasn't possible to determine the test task for managed device `$it`.
-                    Please check the managed devices specified in the baseline profiles configuration.
+                    Please check the managed devices specified in the baseline profiles
+                    configuration.
                 """.trimIndent(), e
                 )
             }
         }
 
-        // Merge result protos task
-        val mergeResultProtosTask = project.tasks.named(
-            camelCase("merge", variantName, "testResultProtos")
-        )
-
         // The collect task collects the baseline profile files from the ui test results
         val collectTaskProvider = project.tasks.register(
-            camelCase("collect", variantName, "BaselineProfiles"),
+            camelCase(COLLECT_TASK_NAME, variantName, "BaselineProfiles"),
             CollectBaselineProfilesTask::class.java
         ) {
 
             // Test tasks have to run before collect
-            it.dependsOn(testTasks, mergeResultProtosTask)
+            it.dependsOn(testTasks)
+
+            // Merge result protos task may not exist depending on gradle version
+            val mergeTaskName = camelCase("merge", variantName, "testResultProtos")
+            try {
+                it.dependsOn(project.tasks.named(mergeTaskName))
+            } catch (e: UnknownTaskException) {
+                // Nothing to do.
+                project.logger.info("Task $mergeTaskName does not exist.")
+            }
 
             // Sets flavor name
             it.outputFile.set(
                 project
                     .layout
                     .buildDirectory
-                    .file("intermediates/baselineprofiles/$flavorName/baseline-prof.txt")
+                    .file("$INTERMEDIATES_BASE_FOLDER/$flavorName/baseline-prof.txt")
             )
 
             // Sets the connected test results location, if tests are supposed to run also on
             // connected devices.
             if (shouldExpectConnectedOutput) {
                 it.connectedAndroidTestOutputDir.set(
-                    if (flavorName.isEmpty()) {
+                    if (flavorName == null) {
                         project.layout.buildDirectory
                             .dir("outputs/androidTest-results/connected")
                     } else {
@@ -229,7 +297,7 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
             // also on managed devices.
             if (shouldExpectManagedOutput) {
                 it.managedAndroidTestOutputDir.set(
-                    if (flavorName.isEmpty()) {
+                    if (flavorName == null) {
                         project.layout.buildDirectory.dir(
                             "outputs/androidTest-results/managedDevice"
                         )
@@ -253,7 +321,7 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
     private fun createBaselineProfileConfigurationForVariant(
         project: Project,
         variantName: String,
-        flavorName: String,
+        flavorName: String?,
         originalBuildTypeName: String,
     ): String {
         val configurationName =
@@ -264,6 +332,8 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
                 isCanBeResolved = false
                 isCanBeConsumed = true
                 attributes {
+
+                    // Main specialized attribute
                     it.attribute(
                         Category.CATEGORY_ATTRIBUTE,
                         project.objects.named(
@@ -271,14 +341,14 @@ class BaselineProfilesProducerPlugin : Plugin<Project> {
                             ATTRIBUTE_CATEGORY_BASELINE_PROFILE
                         )
                     )
-                    it.attribute(
-                        ATTRIBUTE_BUILD_TYPE,
-                        originalBuildTypeName
-                    )
-                    it.attribute(
-                        ATTRIBUTE_FLAVOR,
-                        flavorName
-                    )
+
+                    // Build type
+                    it.attribute(ATTRIBUTE_BUILD_TYPE, originalBuildTypeName)
+
+                    // Flavor if existing
+                    if (flavorName != null) {
+                        it.attribute(ATTRIBUTE_FLAVOR, flavorName)
+                    }
                 }
             }
         return configurationName
