b24ab9af990550cc30a6621261e1d5e554957f79
Ianna Li <iannali@google.com>
2023-02-17 17:51:01 -0800

    Check in remaining Safety classes.
    Test: n/a
    Relnote: n/a
    Bug: 267679434
    Change-Id: Iba413fa7c1bb1851f1bd5db83d160e5463c4b9ef

--- appactions/interaction/interaction-capabilities-safety/src/main/java/androidx/appactions/interaction/capabilities/safety/StartSafetyCheck.kt
+++ appactions/interaction/interaction-capabilities-safety/src/main/java/androidx/appactions/interaction/capabilities/safety/StartSafetyCheck.kt
@@ -22,59 +22,161 @@ import androidx.appactions.interaction.capabilities.core.BaseSession
 import androidx.appactions.interaction.capabilities.core.impl.BuilderOf
 import androidx.appactions.interaction.capabilities.core.impl.converters.TypeConverters
 import androidx.appactions.interaction.capabilities.core.impl.spec.ActionSpecBuilder
+import androidx.appactions.interaction.capabilities.core.properties.SimpleProperty
 import androidx.appactions.interaction.capabilities.core.task.impl.AbstractTaskUpdater
 import androidx.appactions.interaction.capabilities.core.values.GenericErrorStatus
+import androidx.appactions.interaction.capabilities.core.values.SafetyCheck
 import androidx.appactions.interaction.capabilities.core.values.SuccessStatus
 import androidx.appactions.interaction.capabilities.core.values.executionstatus.ActionNotInProgress
 import androidx.appactions.interaction.capabilities.core.values.executionstatus.NoInternetConnection
+import androidx.appactions.interaction.capabilities.safety.executionstatus.EmergencySharingInProgress
 import androidx.appactions.interaction.capabilities.safety.executionstatus.SafetyAccountNotLoggedIn
 import androidx.appactions.interaction.capabilities.safety.executionstatus.SafetyFeatureNotOnboarded
 import androidx.appactions.interaction.proto.ParamValue
 import com.google.protobuf.Struct
 import com.google.protobuf.Value
+import java.time.Duration
+import java.time.ZonedDateTime
 import java.util.Optional
 
-/** StopSafetyCheck.kt in interaction-capabilities-safety */
-private const val CAPABILITY_NAME = "actions.intent.STOP_SAFETY_CHECK"
+/** StartSafetyCheck.kt in interaction-capabilities-safety */
+private const val CAPABILITY_NAME = "actions.intent.START_SAFETY_CHECK"
 
 private val ACTION_SPEC =
     ActionSpecBuilder.ofCapabilityNamed(CAPABILITY_NAME)
-        .setDescriptor(StopSafetyCheck.Property::class.java)
-        .setArgument(StopSafetyCheck.Argument::class.java, StopSafetyCheck.Argument::Builder)
-        .setOutput(StopSafetyCheck.Output::class.java)
+        .setDescriptor(StartSafetyCheck.Property::class.java)
+        .setArgument(StartSafetyCheck.Argument::class.java, StartSafetyCheck.Argument::Builder)
+        .setOutput(StartSafetyCheck.Output::class.java)
+        .bindStructParameter(
+            "safetyCheck.duration",
+            StartSafetyCheck.Property::duration.getter,
+            StartSafetyCheck.Argument.Builder::setDuration,
+            TypeConverters::toDuration
+        )
+        .bindStructParameter(
+            "safetyCheck.checkInTime",
+            StartSafetyCheck.Property::checkInTime.getter,
+            StartSafetyCheck.Argument.Builder::setCheckInTime,
+            TypeConverters::toZonedDateTime
+        )
+        .bindOptionalOutput(
+            "safetyCheck",
+            StartSafetyCheck.Output::safetyCheck.getter,
+            TypeConverters::toParamValue
+        )
         .bindOptionalOutput(
             "executionStatus",
-            StopSafetyCheck.Output::executionStatus.getter,
-            StopSafetyCheck.ExecutionStatus::toParamValue
+            StartSafetyCheck.Output::executionStatus.getter,
+            StartSafetyCheck.ExecutionStatus::toParamValue
         )
         .build()
 
 // TODO(b/267806701): Add capability factory annotation once the testing library is fully migrated.
-class StopSafetyCheck private constructor() {
+class StartSafetyCheck private constructor() {
     // TODO(b/267805819): Update to include the SessionBuilder once Session API is ready.
     class CapabilityBuilder :
         CapabilityBuilderBase<
             CapabilityBuilder, Property, Argument, Output, Confirmation, TaskUpdater, Session
             >(ACTION_SPEC) {
         override fun build(): ActionCapability {
-            super.setProperty(Property())
+            // TODO(b/268369632): No-op remove empty property builder after Property od removed
+            super.setProperty(Property.Builder().build())
             return super.build()
         }
     }
 
     // TODO(b/268369632): Remove Property from public capability APIs.
-    class Property internal constructor()
+    class Property internal constructor(
+        val duration: Optional<SimpleProperty>,
+        val checkInTime: Optional<SimpleProperty>
+    ) {
+        override fun toString(): String {
+            return "Property(duration=$duration, checkInTime=$checkInTime)"
+        }
 
-    class Argument internal constructor() {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Property
+
+            if (duration != other.duration) return false
+            if (checkInTime != other.checkInTime) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = duration.hashCode()
+            result = 31 * result + checkInTime.hashCode()
+            return result
+        }
+
+        class Builder {
+            private var duration: SimpleProperty? = null
+
+            private var checkInTime: SimpleProperty? = null
+
+            fun setDuration(duration: SimpleProperty): Builder =
+                apply { this.duration = duration }
+
+            fun setCheckInTime(checkInTime: SimpleProperty): Builder =
+                apply { this.checkInTime = checkInTime }
+
+            fun build(): Property =
+                Property(Optional.ofNullable(duration), Optional.ofNullable(checkInTime))
+        }
+    }
+
+    class Argument internal constructor(
+        val duration: Optional<Duration>,
+        val checkInTime: Optional<ZonedDateTime>
+    ) {
+        override fun toString(): String {
+            return "Argument(duration=$duration, checkInTime=$checkInTime)"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Argument
+
+            if (duration != other.duration) return false
+            if (checkInTime != other.checkInTime) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = duration.hashCode()
+            result = 31 * result + checkInTime.hashCode()
+            return result
+        }
+
         class Builder : BuilderOf<Argument> {
-            override fun build(): Argument = Argument()
+            private var duration: Duration? = null
+
+            private var checkInTime: ZonedDateTime? = null
+
+            fun setDuration(duration: Duration): Builder =
+                apply { this.duration = duration }
+
+            fun setCheckInTime(checkInTime: ZonedDateTime): Builder =
+                apply { this.checkInTime = checkInTime }
+
+            override fun build(): Argument =
+                Argument(Optional.ofNullable(duration), Optional.ofNullable(checkInTime))
         }
     }
 
     // TODO(b/267533126): Remove the use of optional once ActionSpecBuilder supports nullability.
-    class Output internal constructor(val executionStatus: Optional<ExecutionStatus>) {
+    class Output internal constructor(
+        val safetyCheck: Optional<SafetyCheck>,
+        val executionStatus: Optional<ExecutionStatus>
+    ) {
         override fun toString(): String {
-            return "Output(executionStatus=$executionStatus)"
+            return "Output(safetyCheck=$safetyCheck, executionStatus=$executionStatus)"
         }
 
         override fun equals(other: Any?): Boolean {
@@ -83,22 +185,31 @@ class StopSafetyCheck private constructor() {
 
             other as Output
 
+            if (safetyCheck != other.safetyCheck) return false
             if (executionStatus != other.executionStatus) return false
 
             return true
         }
 
         override fun hashCode(): Int {
-            return executionStatus.hashCode()
+            var result = safetyCheck.hashCode()
+            result = 31 * result + executionStatus.hashCode()
+            return result
         }
 
         class Builder {
+            private var safetyCheck: SafetyCheck? = null
+
             private var executionStatus: ExecutionStatus? = null
+
+            fun setSafetyCheck(safetyCheck: SafetyCheck): Builder =
+                apply { this.safetyCheck = safetyCheck }
 
             fun setExecutionStatus(executionStatus: ExecutionStatus): Builder =
                 apply { this.executionStatus = executionStatus }
 
-            fun build(): Output = Output(Optional.ofNullable(executionStatus))
+            fun build(): Output =
+                Output(Optional.ofNullable(safetyCheck), Optional.ofNullable(executionStatus))
         }
     }
 
@@ -106,6 +217,7 @@ class StopSafetyCheck private constructor() {
         private var successStatus: SuccessStatus? = null
         private var genericErrorStatus: GenericErrorStatus? = null
         private var actionNotInProgress: ActionNotInProgress? = null
+        private var emergencySharingInProgress: EmergencySharingInProgress? = null
         private var safetyAccountNotLoggedIn: SafetyAccountNotLoggedIn? = null
         private var safetyFeatureNotOnboarded: SafetyFeatureNotOnboarded? = null
         private var noInternetConnection: NoInternetConnection? = null
@@ -120,6 +232,10 @@ class StopSafetyCheck private constructor() {
 
         constructor(actionNotInProgress: ActionNotInProgress) {
             this.actionNotInProgress = actionNotInProgress
+        }
+
+        constructor(emergencySharingInProgress: EmergencySharingInProgress) {
+            this.emergencySharingInProgress = emergencySharingInProgress
         }
 
         constructor(safetyAccountNotLoggedIn: SafetyAccountNotLoggedIn) {
@@ -144,6 +260,9 @@ class StopSafetyCheck private constructor() {
             }
             if (actionNotInProgress != null) {
                 status = actionNotInProgress.toString()
+            }
+            if (emergencySharingInProgress != null) {
+                status = emergencySharingInProgress.toString()
             }
             if (safetyAccountNotLoggedIn != null) {
                 status = safetyAccountNotLoggedIn.toString()
