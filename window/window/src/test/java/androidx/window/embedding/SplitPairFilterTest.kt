76376c7ca5ec1f1e268ef05d7a5fd3b8c647a532
Louis Chang <louischang@google.com>
2023-02-21 03:49:33 +0000

    Moving SplitPairFilter to JVM tests
    And adding more unit tests.
    
    Fix: 269360941
    Test: SplitPairFilterTest
    Change-Id: Ieb100fe7cbf00930d1037c07079c6e1bf94de0e7

--- window/window/src/test/java/androidx/window/embedding/SplitPairFilterTest.kt
+++ window/window/src/test/java/androidx/window/embedding/SplitPairFilterTest.kt
@@ -20,12 +20,15 @@ import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import com.google.common.truth.Truth.assertWithMessage
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
-import org.junit.Before
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class SplitPairFilterTest {
     private val intent1 = Intent()
     private val intent2 = Intent()
@@ -38,10 +41,74 @@ class SplitPairFilterTest {
         on { componentName } doReturn COMPONENT_2
     }
 
-    @Before
-    fun setUp() {
-        intent1.component = COMPONENT_1
-        intent2.component = COMPONENT_2
+    @Test(expected = IllegalArgumentException::class)
+    fun packageNameMustNotBeEmpty_primary() {
+        val emptyPackageComponent = ComponentName(EMPTY, FAKE_CLASS)
+        SplitPairFilter(emptyPackageComponent, COMPONENT_1, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun packageNameMustNotBeEmpty_secondary() {
+        val emptyPackageComponent = ComponentName(EMPTY, FAKE_CLASS)
+        SplitPairFilter(COMPONENT_1, emptyPackageComponent, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun classNameMustNotBeEmpty_primary() {
+        val emptyClassComponent = ComponentName(FAKE_PACKAGE, EMPTY)
+        SplitPairFilter(emptyClassComponent, COMPONENT_1, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun classNameMustNotBeEmpty_secondary() {
+        val emptyClassComponent = ComponentName(FAKE_PACKAGE, EMPTY)
+        SplitPairFilter(COMPONENT_1, emptyClassComponent, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun packageNameCannotContainWildcard_primary() {
+        val wildcardPackageComponent = ComponentName(FAKE_PACKAGE_WILDCARD_INSIDE, FAKE_CLASS)
+        SplitPairFilter(wildcardPackageComponent, COMPONENT_1, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun packageNameCannotContainWildcard_secondary() {
+        val wildcardPackageComponent = ComponentName(FAKE_PACKAGE_WILDCARD_INSIDE, FAKE_CLASS)
+        SplitPairFilter(COMPONENT_1, wildcardPackageComponent, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun classNameCannotContainWildcard_primary() {
+        val wildcardInsideClassComponent = ComponentName(FAKE_PACKAGE, FAKE_CLASS_WILDCARD_INSIDE)
+        SplitPairFilter(wildcardInsideClassComponent, COMPONENT_1, null)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun classNameCannotContainWildcard_secondary() {
+        val wildcardInsideClassComponent = ComponentName(FAKE_PACKAGE, FAKE_CLASS_WILDCARD_INSIDE)
+        SplitPairFilter(COMPONENT_1, wildcardInsideClassComponent, null)
+    }
+
+    @Test
+    fun sameComponentName() {
+        val splitPairFilter = SplitPairFilter(COMPONENT_1, COMPONENT_2, INTENT_ACTION)
+        assertEquals(COMPONENT_1.packageName, splitPairFilter.primaryActivityName.packageName)
+        assertEquals(COMPONENT_1.className, splitPairFilter.primaryActivityName.className)
+        assertEquals(
+            COMPONENT_2.packageName,
+            splitPairFilter.secondaryActivityName.packageName
+        )
+        assertEquals(COMPONENT_2.className, splitPairFilter.secondaryActivityName.className)
+        assertEquals(INTENT_ACTION, splitPairFilter.secondaryActivityIntentAction)
+    }
+
+    @Test
+    fun equalsImpliesSameHashCode() {
+        val first = SplitPairFilter(COMPONENT_1, COMPONENT_2, INTENT_ACTION)
+        val second = SplitPairFilter(COMPONENT_1, COMPONENT_2, INTENT_ACTION)
+
+        assertEquals(first, second)
+        assertEquals(first.hashCode(), second.hashCode())
     }
 
     @Test
@@ -51,6 +118,8 @@ class SplitPairFilterTest {
             COMPONENT_2,
             null /* secondaryActivityIntentAction */
         )
+        intent1.component = COMPONENT_1
+        intent2.component = COMPONENT_2
 
         assertWithMessage("#matchesActivityPair must be true because intents match")
             .that(filter.matchesActivityPair(activity1, activity2)).isTrue()
@@ -74,14 +143,16 @@ class SplitPairFilterTest {
 
     @Test
     fun testMatch_WithAction() {
-        val filter = SplitPairFilter(COMPONENT_1, WILDCARD, ACTION)
+        val filter = SplitPairFilter(COMPONENT_1, WILDCARD_COMPONENT, INTENT_ACTION)
+        intent1.component = COMPONENT_1
+        intent2.component = COMPONENT_2
 
         assertWithMessage("#matchesActivityPair must be false because intent has no action")
             .that(filter.matchesActivityPair(activity1, activity2)).isFalse()
         assertWithMessage("#matchesActivityIntentPair must be false because intent has no action")
             .that(filter.matchesActivityIntentPair(activity1, intent2)).isFalse()
 
-        intent2.action = ACTION
+        intent2.action = INTENT_ACTION
 
         assertWithMessage("#matchesActivityPair must be true because intent.action matches")
             .that(filter.matchesActivityPair(activity1, activity2)).isTrue()
@@ -93,11 +164,12 @@ class SplitPairFilterTest {
     fun testMatch_WithIntentPackage() {
         val filter = SplitPairFilter(
             COMPONENT_1,
-            CLASS_WILDCARD,
+            WILDCARD_CLASS_COMPONENT,
             null /* secondaryActivityIntentAction */
         )
+        intent1.component = COMPONENT_1
         intent2.component = null
-        intent2.`package` = CLASS_WILDCARD.packageName
+        intent2.`package` = WILDCARD_CLASS_COMPONENT.packageName
         doReturn(COMPONENT_1).whenever(activity2).componentName
 
         assertWithMessage("#matchesActivityPair must be true because intent.package matches")
@@ -120,8 +192,8 @@ class SplitPairFilterTest {
     @Test
     fun testMatch_EmptyIntentWithWildcard() {
         val filter = SplitPairFilter(
-            WILDCARD,
-            WILDCARD,
+            WILDCARD_COMPONENT,
+            WILDCARD_COMPONENT,
             null /* secondaryActivityIntentAction */
         )
         intent1.component = null
@@ -134,10 +206,16 @@ class SplitPairFilterTest {
     }
 
     companion object {
-        private const val ACTION = "action.test"
+        private const val FAKE_PACKAGE: String = "fake.package"
+        private const val FAKE_PACKAGE_WILDCARD_INSIDE = "fake.*.package"
+        private const val FAKE_CLASS: String = "fake.class.test"
+        private const val FAKE_CLASS_WILDCARD_INSIDE = "fake.*.class"
+
+        private const val EMPTY: String = ""
+        private const val INTENT_ACTION = "fake.action"
         private val COMPONENT_1 = ComponentName("a.b.c", "a.b.c.TestActivity")
         private val COMPONENT_2 = ComponentName("d.e.f", "d.e.f.TestActivity")
-        private val WILDCARD = ComponentName("*", "*")
-        private val CLASS_WILDCARD = ComponentName("d.e.f", "*")
+        private val WILDCARD_CLASS_COMPONENT = ComponentName(FAKE_PACKAGE, "*")
+        private val WILDCARD_COMPONENT = ComponentName("*", "*")
     }
 }
