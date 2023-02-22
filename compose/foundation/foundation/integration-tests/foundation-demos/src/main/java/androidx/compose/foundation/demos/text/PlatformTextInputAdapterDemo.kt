b7c21c9e53a07b6aa7b6184a674970980b21417e
Halil Ozercan <halilibo@google.com>
2023-02-22 17:14:12 +0000

    WIP BasicTextInputPlugin, BasicTextField2, TextFieldState
    Adding a new TextInputPlugin to use with the new BasicTextField2. AndroidTextInputAdapter provides the necessary wiring to start a new input session and dispose it. Created InputConnections are not managed, but created input connections read and write to currently active input session through AndroidTextInputAdapter.
    
    New text input sessions have a one-to-one relation with a `TextFieldState` to make sure that a single input session only modifies the given state. If TextFieldState argument reference changes for a TextField, text input session should be restarted.
    
    Also added a very simple first implementation of BasicTextField2. This composable uses TextMeasurer for text layout instead of TextDelegate. It also means that we would like graduate TextMeasurer in the next release.
    
    Test: N/A
    Change-Id: Ia5b167241eccc724153446e977a50c702d8d8120

--- compose/foundation/foundation/integration-tests/foundation-demos/src/main/java/androidx/compose/foundation/demos/text/PlatformTextInputAdapterDemo.kt
+++ compose/foundation/foundation/integration-tests/foundation-demos/src/main/java/androidx/compose/foundation/demos/text/PlatformTextInputAdapterDemo.kt
@@ -34,6 +34,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.demos.text.WackyTextInputPlugin.createAdapter
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.BasicText
@@ -68,7 +69,7 @@ private const val TAG = "WackyInput"
 
 @Composable
 fun PlatformTextInputAdapterDemo() {
-    Column {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Row {
             var value by remember { mutableStateOf("") }
             Text("Standard text field: ")
