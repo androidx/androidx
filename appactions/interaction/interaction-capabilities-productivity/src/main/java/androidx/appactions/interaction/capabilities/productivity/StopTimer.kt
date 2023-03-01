ee472b0dc5bd09a89ef2b75c7a0eecdcebee8fe1
Ianna Li <iannali@google.com>
2023-02-28 18:31:35 -0800

    Check in Productivity capabilities w/o session api.
    Test: n/a
    Relnote: n/a
    Bug: 267814075
    Change-Id: I7145cd760f89ff5f38b7be240541fc872f6639e4

---
+++ appactions/interaction/interaction-capabilities-productivity/src/main/java/androidx/appactions/interaction/capabilities/productivity/StopTimer.kt
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appactions.interaction.capabilities.productivity
+
+import androidx.appactions.interaction.capabilities.core.ActionCapability
+import androidx.appactions.interaction.capabilities.core.BaseSession
+import androidx.appactions.interaction.capabilities.core.CapabilityBuilderBase
+import androidx.appactions.interaction.capabilities.core.impl.BuilderOf
+import androidx.appactions.interaction.capabilities.core.impl.converters.TypeConverters
+import androidx.appactions.interaction.capabilities.core.impl.spec.ActionSpecBuilder
+import androidx.appactions.interaction.capabilities.core.properties.SimpleProperty
+import androidx.appactions.interaction.capabilities.core.task.impl.AbstractTaskUpdater
+import androidx.appactions.interaction.capabilities.core.values.GenericErrorStatus
+import androidx.appactions.interaction.capabilities.core.values.SuccessStatus
+import androidx.appactions.interaction.capabilities.core.values.Timer
+import androidx.appactions.interaction.proto.ParamValue
+import com.google.protobuf.Struct
+import com.google.protobuf.Value
+import java.util.Optional
+
+/** StopTimer.kt in interaction-capabilities-productivity */
+private const val CAPABILITY_NAME = "actions.intent.STOP_TIMER"
+
+private val ACTION_SPEC = ActionSpecBuilder.ofCapabilityNamed(CAPABILITY_NAME)
+    .setDescriptor(StopTimer.Property::class.java)
+    .setArgument(StopTimer.Argument::class.java, StopTimer.Argument::Builder)
+    .setOutput(StopTimer.Output::class.java).bindRepeatedStructParameter(
+        "timer",
+        { property -> Optional.ofNullable(property.timerList) },
+        StopTimer.Argument.Builder::setTimerList,
+        TypeConverters::toTimer
+    ).bindOptionalOutput(
+        "executionStatus",
+        { output -> Optional.ofNullable(output.executionStatus) },
+        StopTimer.ExecutionStatus::toParamValue
+    ).build()
+
+// TODO(b/267806701): Add capability factory annotation once the testing library is fully migrated.
+class StopTimer private constructor() {
+
+    class CapabilityBuilder :
+        CapabilityBuilderBase<
+            CapabilityBuilder, Property, Argument, Output, Confirmation, TaskUpdater, Session
+            >(ACTION_SPEC) {
+        override fun build(): ActionCapability {
+            super.setProperty(Property.Builder().build())
+            return super.build()
+        }
+    }
+
+    // TODO(b/268369632): Remove Property from public capability APIs.
+    class Property internal constructor(
+        val timerList: SimpleProperty?
+    ) {
+        override fun toString(): String {
+            return "Property(timerList=$timerList}"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Property
+
+            if (timerList != other.timerList) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return timerList.hashCode()
+        }
+
+        class Builder {
+            private var timerList: SimpleProperty? = null
+
+            fun setTimerList(timerList: SimpleProperty): Builder =
+                apply { this.timerList = timerList }
+
+            fun build(): Property = Property(timerList)
+        }
+    }
+
+    class Argument internal constructor(
+        val timerList: List<Timer>?
+    ) {
+        override fun toString(): String {
+            return "Argument(timerList=$timerList)"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Argument
+
+            if (timerList != other.timerList) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return timerList.hashCode()
+        }
+
+        class Builder : BuilderOf<Argument> {
+            private var timerList: List<Timer>? = null
+
+            fun setTimerList(timerList: List<Timer>): Builder = apply { this.timerList = timerList }
+
+            override fun build(): Argument = Argument(timerList)
+        }
+    }
+
+    class Output internal constructor(val executionStatus: ExecutionStatus?) {
+        override fun toString(): String {
+            return "Output(executionStatus=$executionStatus)"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Output
+
+            if (executionStatus != other.executionStatus) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return executionStatus.hashCode()
+        }
+
+        class Builder {
+            private var executionStatus: ExecutionStatus? = null
+
+            fun setExecutionStatus(executionStatus: ExecutionStatus): Builder =
+                apply { this.executionStatus = executionStatus }
+
+            fun build(): Output = Output(executionStatus)
+        }
+    }
+
+    class ExecutionStatus {
+        private var successStatus: SuccessStatus? = null
+        private var genericErrorStatus: GenericErrorStatus? = null
+
+        constructor(successStatus: SuccessStatus) {
+            this.successStatus = successStatus
+        }
+
+        constructor(genericErrorStatus: GenericErrorStatus) {
+            this.genericErrorStatus = genericErrorStatus
+        }
+
+        internal fun toParamValue(): ParamValue {
+            var status: String = ""
+            if (successStatus != null) {
+                status = successStatus.toString()
+            }
+            if (genericErrorStatus != null) {
+                status = genericErrorStatus.toString()
+            }
+            val value: Value = Value.newBuilder().setStringValue(status).build()
+            return ParamValue.newBuilder().setStructValue(
+                Struct.newBuilder().putFields(TypeConverters.FIELD_NAME_TYPE, value).build(),
+            ).build()
+        }
+    }
+
+    class Confirmation internal constructor()
+
+    class TaskUpdater internal constructor() : AbstractTaskUpdater()
+
+    sealed interface Session : BaseSession<Argument, Output>
+}
