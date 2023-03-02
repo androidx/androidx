4f0753b62d3b824391628e7fa0ab799dcdaa3b3b
Diego Vela <diegovela@google.com>
2023-03-02 18:08:55 +0000

    Remove Background color API.
    Remove Color API for beta release while we handle feedback.
    
    The beta has not actually gone out.  API changes have been approved by
    API council.
    
    Relnote: Remove background color API for stable.
    
    Bug: 267637107
    Test: n/a
    Merged-In: I34c3ecf8bbb63a0229633268c58a67da9dc51642
    Change-Id: I34c3ecf8bbb63a0229633268c58a67da9dc51642

--- window/window/src/androidTest/java/androidx/window/embedding/EmbeddingAdapterTest.kt
+++ window/window/src/androidTest/java/androidx/window/embedding/EmbeddingAdapterTest.kt
@@ -20,7 +20,6 @@ import androidx.window.extensions.embedding.ActivityStack as OEMActivityStack
 import androidx.window.extensions.embedding.SplitAttributes as OEMSplitAttributes
 import androidx.window.extensions.embedding.SplitInfo as OEMSplitInfo
 import android.app.Activity
-import android.graphics.Color
 import androidx.window.WindowTestUtils
 import androidx.window.core.ExtensionsUtil
 import androidx.window.core.PredicateAdapter
@@ -61,7 +60,6 @@ class EmbeddingAdapterTest {
             SplitAttributes.Builder()
                 .setSplitType(SplitType.SPLIT_TYPE_EQUAL)
                 .setLayoutDirection(SplitAttributes.LayoutDirection.LOCALE)
-                .setAnimationBackgroundColor(SplitAttributes.BackgroundColor.DEFAULT)
                 .build()
         )
         assertEquals(listOf(expectedSplitInfo), adapter.translate(listOf(oemSplitInfo)))
@@ -124,7 +122,6 @@ class EmbeddingAdapterTest {
             OEMSplitAttributes.Builder()
                 .setSplitType(OEMSplitAttributes.SplitType.HingeSplitType(RatioSplitType(0.5f)))
                 .setLayoutDirection(TOP_TO_BOTTOM)
-                .setAnimationBackgroundColor(Color.YELLOW)
                 .build(),
         )
         val expectedSplitInfo = SplitInfo(
@@ -133,7 +130,6 @@ class EmbeddingAdapterTest {
             SplitAttributes.Builder()
                 .setSplitType(SPLIT_TYPE_HINGE)
                 .setLayoutDirection(SplitAttributes.LayoutDirection.TOP_TO_BOTTOM)
-                .setAnimationBackgroundColor(SplitAttributes.BackgroundColor.color(Color.YELLOW))
                 .build()
         )
         assertEquals(listOf(expectedSplitInfo), adapter.translate(listOf(oemSplitInfo)))
