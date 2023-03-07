2f1c31bb416f68646266ffdf2dbc0beea723f2a9
Aurimas Liutikas <aurimas@google.com>
2023-03-07 18:32:54 +0000

    Revert "Introduce Toggleable Surface in Compose for TV"
    This reverts commit df0c3c406433624051efd2a549b90ae46fb46116.
    
    Reason for revert: broke SurfaceTest tests
    Bug: 270998525
    Change-Id: I7cb9a27f22ec1768f4b95b21be2e1b1dea30450b

--- tv/tv-material/src/main/java/androidx/tv/material3/Surface.kt
+++ tv/tv-material/src/main/java/androidx/tv/material3/Surface.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -52,7 +53,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.tv.material3.tokens.Elevation
 import kotlinx.coroutines.launch
 
 /**
@@ -82,6 +82,7 @@ import kotlinx.coroutines.launch
  * @param content defines the [Composable] content inside the surface
  */
 @ExperimentalTvMaterial3Api
+@NonRestartableComposable
 @Composable
 fun Surface(
     onClick: () -> Unit,
@@ -104,8 +105,10 @@ fun Surface(
             enabled = enabled,
             onClick = onClick,
             interactionSource = interactionSource,
+            value = false,
+            onValueChanged = null
         ),
-        checked = false,
+        selected = false,
         enabled = enabled,
         tonalElevation = tonalElevation,
         shape = ClickableSurfaceDefaults.shape(
@@ -149,121 +152,11 @@ fun Surface(
     )
 }
 
-/**
- * The Surface is a building block component that will be used for any focusable
- * element on TV such as buttons, cards, navigation, etc.
- *
- * This version of Surface is responsible for a toggling its checked state as well as everything
- * else that a regular Surface does:
- *
- * This version of surface will react to the check toggles, calling
- * [onCheckedChange] lambda, updating the [interactionSource] when [PressInteraction] occurs, and
- * showing ripple indication in response to press events. If you don't need check
- * handling, consider using a Surface function that doesn't require [onCheckedChange] param.
- *
- * To manually retrieve the content color inside a surface, use [LocalContentColor].
- *
- * @param checked whether or not this Surface is toggled on or off
- * @param onCheckedChange callback to be invoked when the toggleable Surface is clicked
- * @param modifier Modifier to be applied to the layout corresponding to the surface
- * @param enabled Controls the enabled state of the surface. When `false`, this Surface will not be
- * clickable or focusable.
- * @param tonalElevation When [color] is [ColorScheme.surface], a higher the elevation will result
- * in a darker color in light theme and lighter color in dark theme.
- * @param shape Defines the surface's shape.
- * @param color Color to be used on background of the Surface
- * @param contentColor The preferred content color provided by this Surface to its children.
- * @param scale Defines size of the Surface relative to its original size.
- * @param border Defines a border around the Surface.
- * @param glow Diffused shadow to be shown behind the Surface.
- * @param interactionSource the [MutableInteractionSource] representing the stream of [Interaction]s
- * for this Surface. You can create and pass in your own remembered [MutableInteractionSource] if
- * you want to observe [Interaction]s and customize the appearance / behavior of this Surface in
- * different [Interaction]s.
- * @param content defines the [Composable] content inside the surface
- */
-@ExperimentalTvMaterial3Api
-@Composable
-fun Surface(
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    tonalElevation: Dp = Elevation.Level0,
-    shape: ToggleableSurfaceShape = ToggleableSurfaceDefaults.shape(),
-    color: ToggleableSurfaceColor = ToggleableSurfaceDefaults.color(),
-    contentColor: ToggleableSurfaceColor = ToggleableSurfaceDefaults.contentColor(),
-    scale: ToggleableSurfaceScale = ToggleableSurfaceDefaults.scale(),
-    border: ToggleableSurfaceBorder = ToggleableSurfaceDefaults.border(),
-    glow: ToggleableSurfaceGlow = ToggleableSurfaceDefaults.glow(),
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    content: @Composable (BoxScope.() -> Unit)
-) {
-    val focused by interactionSource.collectIsFocusedAsState()
-    val pressed by interactionSource.collectIsPressedAsState()
-
-    SurfaceImpl(
-        modifier = modifier.tvToggleable(
-            enabled = enabled,
-            checked = checked,
-            onCheckedChange = onCheckedChange,
-            interactionSource = interactionSource,
-        ),
-        checked = checked,
-        enabled = enabled,
-        tonalElevation = tonalElevation,
-        shape = ToggleableSurfaceDefaults.shape(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            shape = shape
-        ),
-        color = ToggleableSurfaceDefaults.color(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            color = color
-        ),
-        contentColor = ToggleableSurfaceDefaults.color(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            color = contentColor
-        ),
-        scale = ToggleableSurfaceDefaults.scale(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            scale = scale
-        ),
-        border = ToggleableSurfaceDefaults.border(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            border = border
-        ),
-        glow = ToggleableSurfaceDefaults.glow(
-            enabled = enabled,
-            focused = focused,
-            pressed = pressed,
-            selected = checked,
-            glow = glow
-        ),
-        interactionSource = interactionSource,
-        content = content
-    )
-}
-
 @ExperimentalTvMaterial3Api
 @Composable
 private fun SurfaceImpl(
     modifier: Modifier,
-    checked: Boolean,
+    selected: Boolean,
     enabled: Boolean,
     shape: Shape,
     color: Color,
@@ -282,7 +175,7 @@ private fun SurfaceImpl(
         enabled = enabled,
         focused = focused,
         pressed = pressed,
-        selected = checked
+        selected = selected
     )
 
     val absoluteElevation = LocalAbsoluteTonalElevation.current + tonalElevation
@@ -365,25 +258,34 @@ private fun SurfaceImpl(
 
 /**
  * This modifier handles click, press, and focus events for a TV composable.
- * @param enabled decides whether [onClick] is executed
+ * @param enabled decides whether [onClick] or [onValueChanged] is executed
  * @param onClick executes the provided lambda
+ * @param value differentiates whether the current item is selected or unselected
+ * @param onValueChanged executes the provided lambda while returning the inverse state of [value]
  * @param interactionSource used to emit [PressInteraction] events
  */
 private fun Modifier.tvClickable(
     enabled: Boolean,
     onClick: (() -> Unit)?,
+    value: Boolean,
+    onValueChanged: ((Boolean) -> Unit)?,
     interactionSource: MutableInteractionSource
 ) = this
     .handleDPadEnter(
         enabled = enabled,
         interactionSource = interactionSource,
-        onClick = onClick
+        onClick = onClick,
+        value = value,
+        onValueChanged = onValueChanged
     )
     .focusable(interactionSource = interactionSource)
     .semantics(mergeDescendants = true) {
         onClick {
             onClick?.let { nnOnClick ->
                 nnOnClick()
+                return@onClick true
+            } ?: onValueChanged?.let { nnOnValueChanged ->
+                nnOnValueChanged(!value)
                 return@onClick true
             }
             false
@@ -393,59 +295,20 @@ private fun Modifier.tvClickable(
         }
     }
 
-/**
- * This modifier handles click, press, and focus events for a TV composable.
- * @param enabled decides whether [onCheckedChange] is executed
- * @param checked differentiates whether the current item is checked or unchecked
- * @param onCheckedChange executes the provided lambda while returning the inverse state of
- * [checked]
- */
-private fun Modifier.tvToggleable(
-    enabled: Boolean,
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
-    interactionSource: MutableInteractionSource,
-) = handleDPadEnter(
-        enabled = enabled,
-        interactionSource = interactionSource,
-        checked = checked,
-        onCheckedChanged = onCheckedChange
-    )
-    .focusable(enabled = enabled, interactionSource = interactionSource)
-    .semantics(mergeDescendants = true) {
-        onClick {
-            onCheckedChange(!checked)
-            true
-        }
-        if (!enabled) {
-            disabled()
-        }
-    }
-
-/**
- * This modifier is used to perform some actions when the user clicks the D-PAD enter button
- *
- * @param enabled if this is false, the D-PAD enter event is ignored
- * @param interactionSource used to emit [PressInteraction] events
- * @param onClick this lambda will be triggered on D-PAD enter event
- * @param checked differentiates whether the current item is checked or unchecked
- * @param onCheckedChanged executes the provided lambda while returning the inverse state of
- * [checked]
- */
 private fun Modifier.handleDPadEnter(
     enabled: Boolean,
     interactionSource: MutableInteractionSource,
-    onClick: (() -> Unit)? = null,
-    checked: Boolean = false,
-    onCheckedChanged: ((Boolean) -> Unit)? = null
+    onClick: (() -> Unit)?,
+    value: Boolean,
+    onValueChanged: ((Boolean) -> Unit)?
 ) = composed(
     inspectorInfo = debugInspectorInfo {
         name = "handleDPadEnter"
         properties["enabled"] = enabled
         properties["interactionSource"] = interactionSource
         properties["onClick"] = onClick
-        properties["checked"] = checked
-        properties["onCheckedChanged"] = onCheckedChanged
+        properties["onValueChanged"] = onValueChanged
+        properties["value"] = value
     }
 ) {
     val coroutineScope = rememberCoroutineScope()
@@ -471,7 +334,7 @@ private fun Modifier.handleDPadEnter(
                                 interactionSource.emit(PressInteraction.Release(pressInteraction))
                             }
                             onClick?.invoke()
-                            onCheckedChanged?.invoke(!checked)
+                            onValueChanged?.invoke(!value)
                         }
                     }
                 }
