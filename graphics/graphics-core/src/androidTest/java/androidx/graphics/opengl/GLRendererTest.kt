cd169cb782657b72599db748a8786ff757e1256d
Nader Jawad <njawad@google.com>
2023-03-09 11:50:19 -0800

    Introduced SurfaceTextureRenderer
    Added SurfaceTextureRenderer class to handle
    drawing a RenderNode with HardwareRenderer into
    a SurfaceTexture so it can be consumed by GL
    
    Fixes: 272548920
    Test: Added SurfaceTextureRendererTest and GLRendererTest
    Change-Id: I41e9b00d26bcbd35e1aab4ab5089600e7c1c6cfa

--- graphics/graphics-core/src/androidTest/java/androidx/graphics/opengl/GLRendererTest.kt
+++ graphics/graphics-core/src/androidTest/java/androidx/graphics/opengl/GLRendererTest.kt
@@ -21,6 +21,7 @@ import android.graphics.Color
 import android.graphics.ColorSpace
 import android.graphics.Paint
 import android.graphics.PixelFormat
+import android.graphics.RenderNode
 import android.graphics.SurfaceTexture
 import android.hardware.HardwareBuffer
 import android.media.Image
@@ -38,6 +39,7 @@ import android.view.SurfaceHolder
 import android.view.TextureView
 import androidx.annotation.RequiresApi
 import androidx.annotation.WorkerThread
+import androidx.graphics.SurfaceTextureRenderer
 import androidx.graphics.lowlatency.LineRenderer
 import androidx.graphics.lowlatency.Rectangle
 import androidx.hardware.SyncFenceCompat
@@ -1029,6 +1031,146 @@ class GLRendererTest {
                 fail("Unable to obtain hardwarebuffer from FrameBuffer")
             }
         } finally {
+            hardwareBuffer?.close()
+            glRenderer.stop(true) {
+                teardownLatch.countDown()
+            }
+            frameHandlerThread.quit()
+            assertTrue(teardownLatch.await(3000, TimeUnit.MILLISECONDS))
+        }
+    }
+
+    @Test
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.Q)
+    fun testRenderSurfaceTextureFromSurfaceTextureRenderer() {
+        val width = 10
+        val height = 10
+        val renderLatch = CountDownLatch(1)
+        val teardownLatch = CountDownLatch(1)
+        val glRenderer = GLRenderer().apply { start() }
+        var frameBuffer: FrameBuffer? = null
+        var status = false
+        var supportsFence = false
+        val frameHandlerThread = HandlerThread("frameAvailable").apply { start() }
+        val frameHandler = Handler(frameHandlerThread.looper)
+        val renderNode = RenderNode("node").apply {
+            setPosition(0, 0, width, height)
+            scaleY = -1f
+            pivotX = width / 2f
+            pivotY = height / 2f
+            val canvas = beginRecording()
+            val paint = Paint()
+            // top left
+            canvas.drawRect(0f, 0f, width / 2f, height / 2f,
+                paint.apply { color = Color.RED })
+            // top right
+            canvas.drawRect(width / 2f, 0f, width.toFloat(), height / 2f,
+                paint.apply { color = Color.BLUE })
+            // bottom left
+            canvas.drawRect(0f, height / 2f, width / 2f, height.toFloat(),
+                paint.apply { color = Color.YELLOW })
+            // bottom right
+            canvas.drawRect(width / 2f, height / 2f, width.toFloat(), height.toFloat(),
+                paint.apply { color = Color.GREEN })
+            endRecording()
+        }
+        val frameAvailableLatch = CountDownLatch(1)
+        var surfaceTexture: SurfaceTexture? = null
+        val surfaceTextureRenderer = SurfaceTextureRenderer(
+            renderNode,
+            width,
+            height,
+            frameHandler
+        ) {
+            surfaceTexture = it
+            frameAvailableLatch.countDown()
+        }
+        surfaceTextureRenderer.renderFrame()
+
+        val callbacks = object : FrameBufferRenderer.RenderCallback {
+
+            private val mOrthoMatrix = FloatArray(16)
+
+            override fun obtainFrameBuffer(egl: EGLSpec): FrameBuffer =
+                FrameBuffer(
+                    egl,
+                    HardwareBuffer.create(
+                        width,
+                        height,
+                        HardwareBuffer.RGBA_8888,
+                        1,
+                        HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE
+                    )
+                ).also { frameBuffer = it }
+
+            override fun onDraw(eglManager: EGLManager) {
+                val texId = genTexture()
+                assertTrue(frameAvailableLatch.await(3000, TimeUnit.MILLISECONDS))
+                assertNotNull(surfaceTexture)
+                surfaceTexture!!.attachToGLContext(texId)
+
+                GLES20.glViewport(0, 0, width, height)
+                Matrix.orthoM(
+                    mOrthoMatrix,
+                    0,
+                    0f,
+                    width.toFloat(),
+                    0f,
+                    height.toFloat(),
+                    -1f,
+                    1f
+                )
+                val quadRenderer = QuadTextureRenderer().apply {
+                    setSurfaceTexture(surfaceTexture!!)
+                }
+                quadRenderer.draw(
+                    mOrthoMatrix,
+                    width.toFloat(),
+                    height.toFloat()
+                )
+                supportsFence = eglManager.supportsNativeAndroidFence()
+                quadRenderer.release()
+                deleteTexture(texId)
+            }
+
+            override fun onDrawComplete(
+                frameBuffer: FrameBuffer,
+                syncFenceCompat: SyncFenceCompat?
+            ) {
+                status = syncFenceCompat?.await(3000) ?: true
+                renderLatch.countDown()
+            }
+        }
+
+        glRenderer.createRenderTarget(
+            width,
+            height,
+            FrameBufferRenderer(callbacks)
+        ).requestRender()
+
+        var hardwareBuffer: HardwareBuffer? = null
+        try {
+            assertTrue(renderLatch.await(3000, TimeUnit.MILLISECONDS))
+            if (supportsFence) {
+                assertTrue(status)
+            }
+
+            hardwareBuffer = frameBuffer?.hardwareBuffer
+            if (hardwareBuffer != null) {
+                val colorSpace = ColorSpace.get(ColorSpace.Named.LINEAR_SRGB)
+                // Copy to non hardware bitmap to be able to sample pixels
+                val bitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, colorSpace)
+                    ?.copy(Bitmap.Config.ARGB_8888, false)
+                if (bitmap != null) {
+                    bitmap.verifyQuadrants(Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN)
+                } else {
+                    fail("Unable to obtain Bitmap from hardware buffer")
+                }
+            } else {
+                fail("Unable to obtain hardwarebuffer from FrameBuffer")
+            }
+        } finally {
+            surfaceTextureRenderer.release()
             hardwareBuffer?.close()
             glRenderer.stop(true) {
                 teardownLatch.countDown()
