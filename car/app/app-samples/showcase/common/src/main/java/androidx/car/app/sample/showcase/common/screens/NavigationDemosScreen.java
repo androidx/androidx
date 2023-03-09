5593cc6e2a76edf3967a4c079d7d08592d495c6c
Wesley Gyde <wgyde@google.com>
2023-03-08 16:26:59 -0800

    Genericize the paging mechanism for NavigationDemosScreen
    This is purely a code quality change. We were going to share the paging
    mechanism with another screen, but ended up going with a different
    solution. However, the generic paging mechanism may be useful in the
    future.
    
    Bug: 272831758
    Test: ./gradlew :car:app:app:test
    Relnote: "Genericize the paging mechanism for NavigationDemosScreen"
    Change-Id: If7183d3bec7f0e7d64392da7515be2e6cf7cdf0a

--- car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/NavigationDemosScreen.java
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/NavigationDemosScreen.java
@@ -19,14 +19,9 @@ package androidx.car.app.sample.showcase.common.screens;
 import androidx.annotation.NonNull;
 import androidx.car.app.CarContext;
 import androidx.car.app.Screen;
-import androidx.car.app.constraints.ConstraintManager;
-import androidx.car.app.model.Action;
-import androidx.car.app.model.ActionStrip;
+import androidx.car.app.ScreenManager;
 import androidx.car.app.model.CarIcon;
-import androidx.car.app.model.ItemList;
-import androidx.car.app.model.ListTemplate;
 import androidx.car.app.model.Row;
-import androidx.car.app.model.Template;
 import androidx.car.app.sample.showcase.common.R;
 import androidx.car.app.sample.showcase.common.screens.navigationdemos.MapTemplateWithListDemoScreen;
 import androidx.car.app.sample.showcase.common.screens.navigationdemos.MapTemplateWithPaneDemoScreen;
@@ -37,6 +32,7 @@ import androidx.car.app.sample.showcase.common.screens.navigationdemos.Navigatio
 import androidx.car.app.sample.showcase.common.screens.navigationdemos.PlaceListNavigationTemplateDemoScreen;
 import androidx.car.app.sample.showcase.common.screens.navigationdemos.PlaceListTemplateBrowseDemoScreen;
 import androidx.car.app.sample.showcase.common.screens.navigationdemos.RoutePreviewDemoScreen;
+import androidx.car.app.sample.showcase.common.screens.paging.PagedListTemplate;
 import androidx.car.app.versioning.CarAppApiLevels;
 import androidx.core.graphics.drawable.IconCompat;
 
@@ -44,121 +40,110 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** A screen showing a list of navigation demos */
-public final class NavigationDemosScreen extends Screen {
-    private static final int MAX_PAGES = 2;
+public final class NavigationDemosScreen extends PagedListTemplate.RowList {
+    private final @NonNull CarContext mCarContext;
 
-    private final int mPage;
-
-    public NavigationDemosScreen(@NonNull CarContext carContext) {
-        this(carContext, /* page= */ 0);
+    private NavigationDemosScreen(@NonNull CarContext carContext) {
+        this.mCarContext = carContext;
     }
 
-    public NavigationDemosScreen(@NonNull CarContext carContext, int page) {
-        super(carContext);
-        mPage = page;
+    /** Creates a screen with navigation demos */
+    @NonNull
+    public static Screen createScreen(@NonNull CarContext carContext) {
+        return new PagedListTemplate(new NavigationDemosScreen(carContext), carContext);
     }
 
     @NonNull
     @Override
-    public Template onGetTemplate() {
-        ItemList.Builder listBuilder = new ItemList.Builder();
-
-        int listLimit = getCarContext().getCarService(ConstraintManager.class).getContentLimit(
-                ConstraintManager.CONTENT_LIMIT_TYPE_LIST);
-
+    protected List<Row> getRows(@NonNull ScreenManager screenManager) {
         List<Row> screenList = new ArrayList<>();
 
-        screenList.add(createRow(buildCarIcon(R.drawable.ic_explore_white_24dp),
-                getCarContext().getString(R.string.nav_template_demos_title),
-                new NavigationTemplateDemoScreen(getCarContext())));
-
-        screenList.add(createRow(getCarContext().getString(
-                        R.string.place_list_nav_template_demo_title),
-                new PlaceListNavigationTemplateDemoScreen(getCarContext())));
-
-        screenList.add(createRow(getCarContext().getString(
-                        R.string.route_preview_template_demo_title),
-                new RoutePreviewDemoScreen(getCarContext())));
-
-        screenList.add(createRow(getCarContext().getString(
-                        R.string.notification_template_demo_title),
-                new NavigationNotificationsDemoScreen(getCarContext())));
-
-        screenList.add(createRow(getCarContext().getString(R.string.nav_map_template_demo_title),
-                new NavigationMapOnlyScreen(getCarContext())));
+        screenList.add(createRow(
+                screenManager,
+                buildCarIcon(R.drawable.ic_explore_white_24dp),
+                mCarContext.getString(R.string.nav_template_demos_title),
+                new NavigationTemplateDemoScreen(mCarContext)
+        ));
 
         screenList.add(createRow(
-                getCarContext().getString(R.string.place_list_template_demo_title),
-                new PlaceListTemplateBrowseDemoScreen(getCarContext())));
+                screenManager,
+                mCarContext.getString(R.string.place_list_nav_template_demo_title),
+                new PlaceListNavigationTemplateDemoScreen(mCarContext)
+        ));
 
-        screenList.add(createRow(getCarContext().getString(R.string.map_template_list_demo_title),
-                new MapTemplateWithListDemoScreen(getCarContext())));
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.route_preview_template_demo_title),
+                new RoutePreviewDemoScreen(mCarContext)
+        ));
 
-        screenList.add(createRow(getCarContext().getString(R.string.map_template_pane_demo_title),
-                new MapTemplateWithPaneDemoScreen(getCarContext())));
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.notification_template_demo_title),
+                new NavigationNotificationsDemoScreen(mCarContext)
+        ));
 
-        if (getCarContext().getCarAppApiLevel() >= CarAppApiLevels.LEVEL_6) {
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.nav_map_template_demo_title),
+                new NavigationMapOnlyScreen(mCarContext)
+        ));
+
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.place_list_template_demo_title),
+                new PlaceListTemplateBrowseDemoScreen(mCarContext)
+        ));
+
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.map_template_list_demo_title),
+                new MapTemplateWithListDemoScreen(mCarContext)
+        ));
+
+        screenList.add(createRow(
+                screenManager,
+                mCarContext.getString(R.string.map_template_pane_demo_title),
+                new MapTemplateWithPaneDemoScreen(mCarContext)
+        ));
+
+        if (mCarContext.getCarAppApiLevel() >= CarAppApiLevels.LEVEL_6) {
             screenList.add(
-                    createRow(getCarContext().getString(R.string.map_template_toggle_demo_title),
-                        new MapTemplateWithToggleDemoScreen(getCarContext())));
+                    createRow(
+                            screenManager,
+                            mCarContext.getString(R.string.map_template_toggle_demo_title),
+                            new MapTemplateWithToggleDemoScreen(mCarContext)));
         }
 
-        // If the screenArray size is under the limit, we will show all of them on the first page.
-        // Otherwise we will show them in multiple pages.
-        if (screenList.size() <= listLimit) {
-            for (int i = 0; i < screenList.size(); i++) {
-                listBuilder.addItem(screenList.get(i));
-            }
-        } else {
-            int currentItemStartIndex = mPage * listLimit;
-            int currentItemEndIndex = Math.min(currentItemStartIndex + listLimit,
-                    screenList.size());
-            for (int i = currentItemStartIndex; i < currentItemEndIndex; i++) {
-                listBuilder.addItem(screenList.get(i));
-            }
-        }
+        return screenList;
+    }
 
-        ListTemplate.Builder builder = new ListTemplate.Builder()
-                .setSingleList(listBuilder.build())
-                .setTitle(getCarContext().getString(R.string.nav_demos_title))
-                .setHeaderAction(Action.BACK);
-
-        // If the current page does not cover the last item, we will show a More button
-        if ((mPage + 1) * listLimit < screenList.size() && mPage + 1 < MAX_PAGES) {
-            builder.setActionStrip(new ActionStrip.Builder()
-                    .addAction(new Action.Builder()
-                            .setTitle(getCarContext().getString(R.string.more_action_title))
-                            .setOnClickListener(() -> {
-                                getScreenManager().push(
-                                        new NavigationDemosScreen(getCarContext(), mPage + 1));
-                            })
-                            .build())
-                    .build());
-        }
-
-        return builder.build();
+    @NonNull
+    @Override
+    protected String getTemplateTitle() {
+        return mCarContext.getString(R.string.nav_demos_title);
     }
 
     private CarIcon buildCarIcon(int imageId) {
         return new CarIcon.Builder(
                 IconCompat.createWithResource(
-                        getCarContext(),
+                        mCarContext,
                         imageId))
                 .build();
     }
 
-    private Row createRow(String title, Screen screen) {
+    private Row createRow(ScreenManager screenManager, String title, Screen screen) {
         return new Row.Builder()
                 .setTitle(title)
-                .setOnClickListener(() -> getScreenManager().push(screen))
+                .setOnClickListener(() -> screenManager.push(screen))
                 .build();
     }
 
-    private Row createRow(CarIcon image, String title, Screen screen) {
+    private Row createRow(ScreenManager screenManager, CarIcon image, String title, Screen screen) {
         return new Row.Builder()
                 .setImage(image)
                 .setTitle(title)
-                .setOnClickListener(() -> getScreenManager().push(screen))
+                .setOnClickListener(() -> screenManager.push(screen))
                 .setBrowsable(true)
                 .build();
     }
---
+++ car/app/app-samples/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/screens/paging/PagedListTemplate.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.app.sample.showcase.common.screens.paging;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.CarContext;
+import androidx.car.app.Screen;
+import androidx.car.app.ScreenManager;
+import androidx.car.app.constraints.ConstraintManager;
+import androidx.car.app.model.Action;
+import androidx.car.app.model.ActionStrip;
+import androidx.car.app.model.ItemList;
+import androidx.car.app.model.ListTemplate;
+import androidx.car.app.model.Row;
+import androidx.car.app.model.Template;
+import androidx.car.app.sample.showcase.common.R;
+
+import java.util.List;
+
+/**
+ * A generic screen featuring a paged list template.
+ *
+ * <p> Paging is useful to avoid truncation in situations where list length is aggressively limited.
+ */
+public class PagedListTemplate extends Screen {
+    private static final int MAX_PAGES = 2;
+
+    private final RowList mRowList;
+    private final int mPage;
+
+    public PagedListTemplate(@NonNull RowList rowList, @NonNull CarContext carContext) {
+        this(rowList, carContext, /* page= */ 0);
+    }
+
+    public PagedListTemplate(@NonNull RowList rowList, @NonNull CarContext carContext, int page) {
+        super(carContext);
+        mRowList = rowList;
+        mPage = page;
+    }
+
+    @NonNull
+    @Override
+    public Template onGetTemplate() {
+        ItemList.Builder listBuilder = new ItemList.Builder();
+
+        int listLimit = getCarContext().getCarService(ConstraintManager.class).getContentLimit(
+                ConstraintManager.CONTENT_LIMIT_TYPE_LIST);
+
+        List<Row> screenList = mRowList.getRows(getScreenManager());
+
+        // If the screenArray size is under the limit, we will show all of them on the first page.
+        // Otherwise we will show them in multiple pages.
+        if (screenList.size() <= listLimit) {
+            for (int i = 0; i < screenList.size(); i++) {
+                listBuilder.addItem(screenList.get(i));
+            }
+        } else {
+            int currentItemStartIndex = mPage * listLimit;
+            int currentItemEndIndex = Math.min(currentItemStartIndex + listLimit,
+                    screenList.size());
+            for (int i = currentItemStartIndex; i < currentItemEndIndex; i++) {
+                listBuilder.addItem(screenList.get(i));
+            }
+        }
+
+        ListTemplate.Builder builder = new ListTemplate.Builder()
+                .setSingleList(listBuilder.build())
+                .setTitle(mRowList.getTemplateTitle())
+                .setHeaderAction(Action.BACK);
+
+        // If the current page does not cover the last item, we will show a More button
+        if ((mPage + 1) * listLimit < screenList.size() && mPage + 1 < MAX_PAGES) {
+            builder.setActionStrip(new ActionStrip.Builder()
+                    .addAction(new Action.Builder()
+                            .setTitle(getCarContext().getString(R.string.more_action_title))
+                            .setOnClickListener(() -> {
+                                getScreenManager().push(
+                                        new PagedListTemplate(
+                                                mRowList,
+                                                getCarContext(),
+                                                mPage + 1
+                                        )
+                                );
+                            })
+                            .build())
+                    .build());
+        }
+
+        return builder.build();
+    }
+
+    /** A list of rows, used to populate a {@link PagedListTemplate} */
+    public abstract static class RowList {
+        @NonNull
+        protected abstract List<Row> getRows(@NonNull ScreenManager screenManager);
+
+        @NonNull
+        protected abstract String getTemplateTitle();
+    }
+}
