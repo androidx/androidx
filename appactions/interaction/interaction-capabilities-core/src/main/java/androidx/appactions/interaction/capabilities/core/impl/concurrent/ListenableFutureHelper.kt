55c3f490179c64d6eb0fedb698812eba011354c8
yunfanjin <yunfanjin@google.com>
2023-03-09 19:02:54 +0000

    Remove the wrapper class of ListenableFutureHelper, because Kotlin doesn't need it.
    Test: refactoring CL. Existing unit tests still pass.
    Change-Id: I61d4d07c09f7a9e04205efdfed93b1f0bc2920ae

--- appactions/interaction/interaction-capabilities-core/src/main/java/androidx/appactions/interaction/capabilities/core/impl/concurrent/ListenableFutureHelper.kt
+++ appactions/interaction/interaction-capabilities-core/src/main/java/androidx/appactions/interaction/capabilities/core/impl/concurrent/ListenableFutureHelper.kt
@@ -1,35 +1,33 @@
 package androidx.appactions.interaction.capabilities.core.impl.concurrent
 
+import androidx.annotation.RestrictTo
 import androidx.concurrent.futures.CallbackToFutureAdapter
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 // TODO(b/269525385): merge this into Futures utility class once it's migrated to Kotlin.
-internal class ListenableFutureHelper {
-    companion object {
-        fun <T> convertToListenableFuture(
-            tag: String,
-            block: suspend CoroutineScope.() -> T,
-        ): ListenableFuture<T> {
-            val scope = CoroutineScope(Dispatchers.Default)
-            return CallbackToFutureAdapter.getFuture {
-                    completer ->
-                val job = scope.launch {
-                    try {
-                        completer.set(scope.block())
-                    } catch (t: Throwable) {
-                        completer.setException(t)
-                    }
+/** @hide */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+fun <T> convertToListenableFuture(
+    tag: String,
+    block: suspend CoroutineScope.() -> T,
+): ListenableFuture<T> {
+    val scope = CoroutineScope(Dispatchers.Default)
+    return CallbackToFutureAdapter.getFuture { completer ->
+        val job =
+            scope.launch {
+                try {
+                    completer.set(scope.block())
+                } catch (t: Throwable) {
+                    completer.setException(t)
                 }
-                completer.addCancellationListener(
-                    { job.cancel() },
-                    Runnable::run,
-                )
-                "ListenableFutureHelper#convertToListenableFuture for '$tag'"
             }
-        }
+        completer.addCancellationListener(
+            { job.cancel() },
+            Runnable::run,
+        )
+        "ListenableFutureHelper#convertToListenableFuture for '$tag'"
     }
 }
