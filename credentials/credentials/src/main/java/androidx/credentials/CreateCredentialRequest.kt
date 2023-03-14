228f216db84fca1dd9574020cbf1507763d31ac6
Helen Qin <helenqin@google.com>
2023-03-14 00:28:04 +0000

    Allow default provider overriding for createCredential
    Following up this, will make the unhiding api changes to set this value
    from subclasses.
    
    Bug: 273307962
    Test: unit tested
    Change-Id: I587a66a3dd3c8d6d330c3b559de21b74ba9f1d9d

--- credentials/credentials/src/main/java/androidx/credentials/CreateCredentialRequest.kt
+++ credentials/credentials/src/main/java/androidx/credentials/CreateCredentialRequest.kt
@@ -67,13 +67,15 @@ abstract class CreateCredentialRequest internal constructor(
      * @property userId the user identifier of the created credential
      * @property userDisplayName an optional display name in addition to the [userId] that may be
      * displayed next to the `userId` during the user consent to help your user better understand
-     * the credential being created.
+     * the credential being created
      */
     class DisplayInfo internal /** @hide */ constructor(
         val userId: CharSequence,
         val userDisplayName: CharSequence?,
         /** @hide */
-        val credentialTypeIcon: Icon?
+        val credentialTypeIcon: Icon?,
+        /** @hide */
+        val defaultProvider: String?,
     ) {
 
         /**
@@ -82,7 +84,7 @@ abstract class CreateCredentialRequest internal constructor(
          * @param userId the user id of the created credential
          * @param userDisplayName an optional display name in addition to the [userId] that may be
          * displayed next to the `userId` during the user consent to help your user better
-         * understand the credential being created.
+         * understand the credential being created
          * @throws IllegalArgumentException If [userId] is empty
          */
         @JvmOverloads constructor(
@@ -91,7 +93,8 @@ abstract class CreateCredentialRequest internal constructor(
         ) : this(
             userId,
             userDisplayName,
-            null
+            null,
+            null,
         )
 
         init {
@@ -105,6 +108,9 @@ abstract class CreateCredentialRequest internal constructor(
             bundle.putCharSequence(BUNDLE_KEY_USER_ID, userId)
             if (!TextUtils.isEmpty(userDisplayName)) {
                 bundle.putCharSequence(BUNDLE_KEY_USER_DISPLAY_NAME, userDisplayName)
+            }
+            if (!TextUtils.isEmpty(defaultProvider)) {
+                bundle.putString(BUNDLE_KEY_DEFAULT_PROVIDER, defaultProvider)
             }
             // Today the type icon is determined solely within this library right before the
             // request is passed into the framework. Later if needed a new API can be added for
@@ -132,6 +138,10 @@ abstract class CreateCredentialRequest internal constructor(
             const val BUNDLE_KEY_CREDENTIAL_TYPE_ICON =
                 "androidx.credentials.BUNDLE_KEY_CREDENTIAL_TYPE_ICON"
 
+            /** @hide */
+            const val BUNDLE_KEY_DEFAULT_PROVIDER =
+                "androidx.credentials.BUNDLE_KEY_DEFAULT_PROVIDER"
+
             /**
              * Returns a RequestDisplayInfo from a `credentialData` Bundle, or otherwise `null` if
              * parsing fails.
@@ -149,7 +159,9 @@ abstract class CreateCredentialRequest internal constructor(
                         displayInfoBundle.getCharSequence(BUNDLE_KEY_USER_DISPLAY_NAME)
                     val icon: Icon? =
                         displayInfoBundle.getParcelable(BUNDLE_KEY_CREDENTIAL_TYPE_ICON)
-                    DisplayInfo(userId!!, displayName, icon)
+                    val defaultProvider: String? =
+                        displayInfoBundle.getString(BUNDLE_KEY_DEFAULT_PROVIDER)
+                    DisplayInfo(userId!!, displayName, icon, defaultProvider)
                 } catch (e: Exception) {
                     null
                 }
