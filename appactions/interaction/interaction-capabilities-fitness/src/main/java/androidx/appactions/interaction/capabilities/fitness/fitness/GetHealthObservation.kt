7795dcf076df73b8b1f9ac5797354b879d8b6f55
Ianna Li <iannali@google.com>
2023-03-14 18:18:07 -0700

    Check in P0 capabilities for health&fitness.
    Test: n/a
    Relnote: n/a
    Bug: 267813238
    Change-Id: I3f7c5320c82be2ad22f9c686779d4a5822f984fb

---
+++ appactions/interaction/interaction-capabilities-fitness/src/main/java/androidx/appactions/interaction/capabilities/fitness/fitness/GetHealthObservation.kt
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appactions.interaction.capabilities.fitness.fitness
+
+import androidx.appactions.interaction.capabilities.core.CapabilityBuilderBase
+import androidx.appactions.interaction.capabilities.core.ActionCapability
+import androidx.appactions.interaction.capabilities.core.BaseSession
+import androidx.appactions.interaction.capabilities.core.CapabilityFactory
+import androidx.appactions.interaction.capabilities.core.impl.BuilderOf
+import androidx.appactions.interaction.capabilities.core.impl.converters.TypeConverters
+import androidx.appactions.interaction.capabilities.core.impl.spec.ActionSpecBuilder
+import androidx.appactions.interaction.capabilities.core.properties.SimpleProperty
+import androidx.appactions.interaction.capabilities.core.task.impl.AbstractTaskUpdater
+import java.time.LocalTime
+import java.util.Optional
+
+/** GetHealthObservation.kt in interaction-capabilities-fitness */
+private const val CAPABILITY_NAME = "actions.intent.START_EXERCISE"
+
+// TODO(b/273602015): Update to use Name property from builtintype library.
+private val ACTION_SPEC =
+    ActionSpecBuilder.ofCapabilityNamed(CAPABILITY_NAME)
+        .setDescriptor(GetHealthObservation.Property::class.java)
+        .setArgument(
+            GetHealthObservation.Argument::class.java,
+            GetHealthObservation.Argument::Builder
+        )
+        .setOutput(GetHealthObservation.Output::class.java)
+        .bindStructParameter(
+            "exerciseObservation.startTime",
+            { property -> Optional.ofNullable(property.startTime) },
+            GetHealthObservation.Argument.Builder::setStartTime,
+            TypeConverters::toLocalTime
+        )
+        .bindStructParameter(
+            "exerciseObservation.endTime",
+            { property -> Optional.ofNullable(property.endTime) },
+            GetHealthObservation.Argument.Builder::setEndTime,
+            TypeConverters::toLocalTime
+        )
+        .build()
+
+@CapabilityFactory(name = CAPABILITY_NAME)
+class GetHealthObservation private constructor() {
+    class CapabilityBuilder :
+        CapabilityBuilderBase<
+            CapabilityBuilder, Property, Argument, Output, Confirmation, TaskUpdater, Session
+            >(ACTION_SPEC) {
+        private var propertyBuilder: Property.Builder = Property.Builder()
+        fun setStartTimeProperty(startTime: SimpleProperty): CapabilityBuilder = apply {
+            propertyBuilder.setEndTime(startTime)
+        }
+
+        fun setEndTimeProperty(endTime: SimpleProperty): CapabilityBuilder = apply {
+            propertyBuilder.setEndTime(endTime)
+        }
+
+        override fun build(): ActionCapability {
+            // TODO(b/268369632): Clean this up after Property is removed
+            super.setProperty(propertyBuilder.build())
+            return super.build()
+        }
+    }
+
+    // TODO(b/268369632): Remove Property from public capability APIs.
+    class Property internal constructor(
+        val startTime: SimpleProperty?,
+        val endTime: SimpleProperty?
+    ) {
+        override fun toString(): String {
+            return "Property(startTime=$startTime, endTime=$endTime)"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass !== other?.javaClass) return false
+
+            other as Property
+
+            if (startTime != other.startTime) return false
+            if (endTime != other.endTime) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = startTime.hashCode()
+            result += 31 * endTime.hashCode()
+            return result
+        }
+
+        class Builder {
+            private var startTime: SimpleProperty? = null
+            private var endTime: SimpleProperty? = null
+
+            fun setStartTime(startTime: SimpleProperty): Builder =
+                apply { this.startTime = startTime }
+
+            fun setEndTime(endTime: SimpleProperty): Builder =
+                apply { this.endTime = endTime }
+
+            fun build(): Property = Property(startTime, endTime)
+        }
+    }
+
+    class Argument internal constructor(
+        val startTime: LocalTime?,
+        val endTime: LocalTime?
+    ) {
+        override fun toString(): String {
+            return "Argument(startTime=$startTime, endTime=$endTime)"
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass !== other?.javaClass) return false
+
+            other as Argument
+
+            if (startTime != other.startTime) return false
+            if (endTime != other.endTime) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = startTime.hashCode()
+            result += 31 * endTime.hashCode()
+            return result
+        }
+
+        class Builder : BuilderOf<Argument> {
+            private var startTime: LocalTime? = null
+            private var endTime: LocalTime? = null
+
+            fun setStartTime(startTime: LocalTime): Builder =
+                apply { this.startTime = startTime }
+
+            fun setEndTime(endTime: LocalTime): Builder =
+                apply { this.endTime = endTime }
+
+            override fun build(): Argument = Argument(startTime, endTime)
+        }
+    }
+
+    class Output internal constructor()
+
+    class Confirmation internal constructor()
+
+    class TaskUpdater internal constructor() : AbstractTaskUpdater()
+
+    sealed interface Session : BaseSession<Argument, Output>
+}
