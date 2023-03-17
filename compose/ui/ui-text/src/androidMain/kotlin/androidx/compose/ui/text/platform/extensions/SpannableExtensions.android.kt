9cd31139e2e765a14377fe5d3fa2a77db178c8de
Siyamed Sinir <siyamed@google.com>
2023-03-17 16:42:39 +0000

    Partial Revert "Use LineHeightStyle as default for includeFontPadding=true"
    This reverts commit 5ddf42b0308c9c64303dd378baae8b13aa1f5614.
    
    Reason for revert: In cases with multi style text with different 
    size  annotations, top/bottom calculation ends up different. 
    Another minor side effect was that there was 1px height diff in 
    some screenshot tests.
    
    The changes in LineHeightStyleSpan as a result of the previous review 
    are not reverted.
    
    Test: TreeHugger
    Test: Following tests on Android 11 and 13
    Test: ./gradlew text:text:test
    Test: ./gradlew text:text:cAT
    Test: ./gradlew compose:ui:ui-text:test
    Test: ./gradlew compose:ui:ui-text:cAT
    
    Change-Id: I24f5e94622b220cce3db1df5f5e7eedbc385f94d

--- compose/ui/ui-text/src/androidMain/kotlin/androidx/compose/ui/text/platform/extensions/SpannableExtensions.android.kt
+++ compose/ui/ui-text/src/androidMain/kotlin/androidx/compose/ui/text/platform/extensions/SpannableExtensions.android.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.text.android.style.BaselineShiftSpan
 import androidx.compose.ui.text.android.style.FontFeatureSpan
 import androidx.compose.ui.text.android.style.LetterSpacingSpanEm
 import androidx.compose.ui.text.android.style.LetterSpacingSpanPx
+import androidx.compose.ui.text.android.style.LineHeightSpan
 import androidx.compose.ui.text.android.style.LineHeightStyleSpan
 import androidx.compose.ui.text.android.style.ShadowSpan
 import androidx.compose.ui.text.android.style.SkewXSpan
@@ -133,6 +134,22 @@ internal fun Spannable.setLineHeight(
                 trimLastLineBottom = lineHeightStyle.trim.isTrimLastLineBottom(),
                 topRatio = lineHeightStyle.alignment.topRatio
             ),
+            start = 0,
+            end = length
+        )
+    }
+}
+
+@OptIn(InternalPlatformTextApi::class)
+internal fun Spannable.setLineHeight(
+    lineHeight: TextUnit,
+    contextFontSize: Float,
+    density: Density
+) {
+    val resolvedLineHeight = resolveLineHeightInPx(lineHeight, contextFontSize, density)
+    if (!resolvedLineHeight.isNaN()) {
+        setSpan(
+            span = LineHeightSpan(lineHeight = resolvedLineHeight),
             start = 0,
             end = length
         )
