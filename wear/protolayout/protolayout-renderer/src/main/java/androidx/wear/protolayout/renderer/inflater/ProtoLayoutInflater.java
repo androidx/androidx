18d6cabe0edadf9b7a529c91b48fab7cf0ff1a0e
Achraf Feydi <achraffeydi@google.com>
2023-03-23 17:47:41 +0000

    Add StrokeCap Builders.
    RelNote: Add StrokeCap support for ArcLine.
    Bug: b/274752191
    Test: Unit tests added.
    
    Change-Id: I94951c20ab62491fa3d4eafdc119b80a66dc5431

--- wear/protolayout/protolayout-renderer/src/main/java/androidx/wear/protolayout/renderer/inflater/ProtoLayoutInflater.java
+++ wear/protolayout/protolayout-renderer/src/main/java/androidx/wear/protolayout/renderer/inflater/ProtoLayoutInflater.java
@@ -2604,6 +2604,9 @@ public final class ProtoLayoutInflater {
                 case STROKE_CAP_ROUND:
                     lineView.setStrokeCap(Cap.ROUND);
                     break;
+                case STROKE_CAP_SQUARE:
+                    lineView.setStrokeCap(Cap.SQUARE);
+                    break;
                 case UNRECOGNIZED:
                 case STROKE_CAP_UNDEFINED:
                     Log.w(TAG, "Undefined StrokeCap value.");
