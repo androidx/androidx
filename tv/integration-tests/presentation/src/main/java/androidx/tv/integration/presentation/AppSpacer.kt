4ac7728bee1e658681d546efda231d4c3b5242af
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-27 20:55:50 +0530

    feat: add presentation-app code
    Test: NA
    
    Change-Id: I049551d6496e013df2a1aecd3e7fe5b76e7b2b62

--- tv/integration-tests/presentation/src/main/java/androidx/tv/integration/presentation/AppSpacer.kt
+++ tv/integration-tests/presentation/src/main/java/androidx/tv/integration/presentation/AppSpacer.kt
@@ -16,22 +16,24 @@
 
 package androidx.tv.integration.presentation
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Create the "data.json" file in "presentation/src/main/assets" directory and add the
-        // content from this link: go/compose-tv-presentation-app-data
-        val jsonData = assets.readAssetsFile("data.json")
-        val deserializedData = getRootDataFromJson(jsonData)
-
-        initializeData(deserializedData)
-
-        super.onCreate(savedInstanceState)
-        setContent {
-            App()
-        }
+@Composable
+fun AppSpacer(
+    width: Dp? = null,
+    height: Dp? = null
+) {
+    var modifier: Modifier = Modifier
+    if (width != null) {
+        modifier = modifier.width(width)
     }
+    if (height != null) {
+        modifier = modifier.height(height)
+    }
+    Spacer(modifier)
 }
