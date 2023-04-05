387c6a56d89db59ca5366beefd01d23b712dba29
Xi Zhang <xizh@google.com>
2023-04-05 11:12:23 -0700

    Update Effects API to handle processor errors
    This CL handles API council feedback: https://b.corp.google.com/issues/271441321#comment4
    
    CameraEffect injects app code into CameraX pipeline that performs camera configuration and post-processing. In case the injected code run into unrecoverable errors, we use this listener to propagate it back to the app.
    
    - Updated the CameraEffect class to accept an error listener in the constructor.
    - In InternalImageProcessor and SurfaceProcessorWithExecutor, forward exceptions to the error listener.
    - Changed "androidx.core:core" dependency from implementation to api because the Consumer<> interface.
    
    Relnote: Add an error listener to CameraEffect to handle unrecoverable errors
    
    Bug: 271441321
    Test: manual test and ./gradlew bOS
    Change-Id: Ice47156c364e628483ab6df46bdeceedd3c27be9

--- camera/camera-core/src/main/java/androidx/camera/core/processing/SurfaceProcessorWithExecutor.java
+++ camera/camera-core/src/main/java/androidx/camera/core/processing/SurfaceProcessorWithExecutor.java
@@ -17,18 +17,21 @@
 package androidx.camera.core.processing;
 
 import static androidx.camera.core.impl.utils.futures.Futures.immediateFailedFuture;
-import static androidx.core.util.Preconditions.checkState;
+
+import static java.util.Objects.requireNonNull;
 
 import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
+import androidx.camera.core.CameraEffect;
 import androidx.camera.core.Logger;
 import androidx.camera.core.ProcessingException;
 import androidx.camera.core.SurfaceOutput;
 import androidx.camera.core.SurfaceProcessor;
 import androidx.camera.core.SurfaceRequest;
+import androidx.core.util.Consumer;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -50,14 +53,13 @@ public class SurfaceProcessorWithExecutor implements SurfaceProcessorInternal {
     private final SurfaceProcessor mSurfaceProcessor;
     @NonNull
     private final Executor mExecutor;
+    @NonNull
+    private final Consumer<Throwable> mErrorListener;
 
-    public SurfaceProcessorWithExecutor(
-            @NonNull SurfaceProcessor surfaceProcessor,
-            @NonNull Executor executor) {
-        checkState(!(surfaceProcessor instanceof SurfaceProcessorInternal),
-                "SurfaceProcessorInternal should always be thread safe. Do not wrap.");
-        mSurfaceProcessor = surfaceProcessor;
-        mExecutor = executor;
+    public SurfaceProcessorWithExecutor(@NonNull CameraEffect cameraEffect) {
+        mSurfaceProcessor = requireNonNull(cameraEffect.getSurfaceProcessor());
+        mExecutor = cameraEffect.getExecutor();
+        mErrorListener = cameraEffect.getErrorListener();
     }
 
     @NonNull
@@ -79,6 +81,7 @@ public class SurfaceProcessorWithExecutor implements SurfaceProcessorInternal {
                 mSurfaceProcessor.onInputSurface(request);
             } catch (ProcessingException e) {
                 Logger.e(TAG, "Failed to setup SurfaceProcessor input.", e);
+                mErrorListener.accept(e);
             }
         });
     }
@@ -90,6 +93,7 @@ public class SurfaceProcessorWithExecutor implements SurfaceProcessorInternal {
                 mSurfaceProcessor.onOutputSurface(surfaceOutput);
             } catch (ProcessingException e) {
                 Logger.e(TAG, "Failed to setup SurfaceProcessor output.", e);
+                mErrorListener.accept(e);
             }
         });
     }
