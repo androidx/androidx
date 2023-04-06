dfbea3ec8e88bf2a9a77c1bca0b02a8e2b8b0fa4
Andrei Shikov <ashikov@google.com>
2023-04-06 17:53:20 +0100

    Execute deactivation callbacks for LayoutNode before disposing effects
    Ensures that layout node deactivation is performed before composition disposal. In cases when Modifier.Node uses values that are owned by composition, node should be reset before the owned value is disposed by composition.
    
    Test: Existing tests
    Fixes: 277124390
    Change-Id: I5957617e9f15af8d581b5cf304f5312bba310169

--- tv/tv-foundation/src/androidTest/java/androidx/tv/foundation/lazy/list/LazyListTest.kt
+++ tv/tv-foundation/src/androidTest/java/androidx/tv/foundation/lazy/list/LazyListTest.kt
@@ -602,7 +602,6 @@ class LazyListTest(orientation: Orientation) : BaseLazyListTestWithOrientation(o
             .assertHeightIsEqualTo(150.dp)
     }
 
-    @Ignore("Disabled due to b/277124390")
     @Test
     fun whenNotAnymoreAvailableItemWasDisplayed() {
         var items by mutableStateOf((1..30).toList())
