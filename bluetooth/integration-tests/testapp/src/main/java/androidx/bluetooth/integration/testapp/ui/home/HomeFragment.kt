75da763c49bb854deabf609d0f558174e5311dc0
Kyunglyul Hyun <klhyun@google.com>
2023-04-12 15:22:53 +0900

    Implement GATT subscribe method
    Test: Test manually with a remote GATT server
    notifying a characteristic
    
    Bug: 269369934
    
    Change-Id: I0ae4653108b448dbbd794a65a2ea9aadf309da5b

--- bluetooth/integration-tests/testapp/src/main/java/androidx/bluetooth/integration/testapp/ui/home/HomeFragment.kt
+++ bluetooth/integration-tests/testapp/src/main/java/androidx/bluetooth/integration/testapp/ui/home/HomeFragment.kt
@@ -17,6 +17,7 @@
 package androidx.bluetooth.integration.testapp.ui.home
 
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_NOTIFY
 import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ
 import android.bluetooth.le.AdvertiseData
 import android.bluetooth.le.AdvertiseSettings
@@ -39,6 +40,7 @@ import androidx.lifecycle.ViewModelProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
@@ -150,14 +152,19 @@ class HomeFragment : Fragment() {
                     val jobs = ArrayList<Job>()
                     for (srv in getServices()) {
                         for (char in srv.characteristics) {
-                            Log.d(TAG, "trying to read characteristic ${char.uuid}")
                             if (char.properties.and(PROPERTY_READ) == 0) continue
                             jobs.add(launch {
-                                val value = read(char).getOrNull()
+                                val value = readCharacteristic(char).getOrNull()
                                 if (value != null) {
                                     Log.d(TAG, "Successfully read characteristic value=$value")
                                 }
                             })
+                            if (char.properties.and(PROPERTY_NOTIFY) != 0) {
+                                jobs.add(launch {
+                                    val value = subscribeToCharacteristic(char).first()
+                                    Log.d(TAG, "Successfully get characteristic value=$value")
+                                })
+                            }
                         }
                     }
                     jobs.joinAll()
