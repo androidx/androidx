5f833996e545b84587b159b07ab29e52a80a559e
Paulina Koch <pakoch@google.com>
2023-04-13 20:07:29 +0100

    Add a write exercise routes permission definition.
    Bug: 245082202
    Test: Will be added with full support for routes
    
    Change-Id: I7489b5648df0a690080fbd1a0c036f45b42dbbbb

--- health/connect/connect-client/src/main/java/androidx/health/connect/client/permission/HealthPermission.kt
+++ health/connect/connect-client/src/main/java/androidx/health/connect/client/permission/HealthPermission.kt
@@ -195,6 +195,7 @@ internal constructor(
         internal const val WRITE_DISTANCE = PERMISSION_PREFIX + "WRITE_DISTANCE"
         internal const val WRITE_ELEVATION_GAINED = PERMISSION_PREFIX + "WRITE_ELEVATION_GAINED"
         internal const val WRITE_EXERCISE = PERMISSION_PREFIX + "WRITE_EXERCISE"
+        internal const val WRITE_EXERCISE_ROUTE = PERMISSION_PREFIX + "WRITE_EXERCISE_ROUTE"
         internal const val WRITE_FLOORS_CLIMBED = PERMISSION_PREFIX + "WRITE_FLOORS_CLIMBED"
         internal const val WRITE_STEPS = PERMISSION_PREFIX + "WRITE_STEPS"
         internal const val WRITE_TOTAL_CALORIES_BURNED =
