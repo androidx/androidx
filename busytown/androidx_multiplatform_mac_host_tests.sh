8ef149a799a4a07e81912d09f9e759f21096cf47
Fred Sladkey <fsladkey@google.com>
2023-04-14 16:02:28 -0400

    Remove old symlinks from target renaming.
    Bug: 277914824
    Test: Presubmit + mac targets on ABTD
    Change-Id: I87356f3da9147607cdfbde3ad53b7335322315a9

--- busytown/androidx_multiplatform_mac_host_tests.sh
+++ busytown/androidx_multiplatform_mac_host_tests.sh
@@ -1,1 +1,27 @@
-androidx_multiplatform_host_tests_mac.sh
+#!/bin/bash
+set -e
+
+# Runs host tests for all projects that support KMP except for Compose-specific projects which are
+# already covered by androidx_compose_multiplatform.sh
+
+# Must be run on Mac
+
+export ANDROIDX_PROJECTS=KMP
+
+# disable GCP cache, these machines don't have credentials.
+export USE_ANDROIDX_REMOTE_BUILD_CACHE=false
+
+echo "Starting $0 at $(date)"
+
+cd "$(dirname $0)"
+
+# Setup simulators
+impl/androidx-native-mac-simulator-setup.sh
+
+impl/build.sh darwinBenchmarkResults allTests \
+    --no-configuration-cache \
+    -Pandroidx.ignoreTestFailures \
+    -Pandroidx.displayTestOutput=false \
+    "$@"
+
+echo "Completing $0 at $(date)"
