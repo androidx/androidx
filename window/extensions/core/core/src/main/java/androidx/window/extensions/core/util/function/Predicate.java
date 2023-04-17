0b2133f40b53bf24411fc38fa28661eb2f4a4480
Diego Vela <diegovela@google.com>
2023-04-17 20:22:29 +0000

    Reinstate extensions.core APIs.
    Reinstate extensions.core APIs.
    
    Relnote: Make core interfaces public so it can be used in extensions.
    
    Bug: 278593264
    Test: n/a
    Merged-In: I450527ca1ed9cb8bcd27289d681f441b0074f012
    Change-Id: I450527ca1ed9cb8bcd27289d681f441b0074f012

--- window/extensions/core/core/src/main/java/androidx/window/extensions/core/util/function/Predicate.java
+++ window/extensions/core/core/src/main/java/androidx/window/extensions/core/util/function/Predicate.java
@@ -16,10 +16,6 @@
 
 package androidx.window.extensions.core.util.function;
 
-import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
-
-import androidx.annotation.RestrictTo;
-
 /**
  * Represents a predicate boolean-valued function of one argument.
  * It is used internally to avoid using Java 8 functional interface that leads to desugaring and
@@ -29,7 +25,6 @@ import androidx.annotation.RestrictTo;
  *
  * @see java.util.function.Predicate
  */
-@RestrictTo(LIBRARY_GROUP_PREFIX)
 @FunctionalInterface
 public interface Predicate<T> {
     /**
