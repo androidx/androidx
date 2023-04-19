1a7463b8e64f9218b5c04973ec35c3072744e8df
Grant Toepfer <grantapher@google.com>
2023-04-19 13:39:55 -0700

    Fix detectDownAndDragGesturesWithObserver missing first pointer event
    Test: Manually tested in demo app with text selection handles
    Test: Not reproducible in connectedAndroidTests
    Change-Id: I4b1588673fb920a8cfe7fcc55de83be56552622c

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/LongPressTextDragObserver.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/LongPressTextDragObserver.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.util.fastAny
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
@@ -78,10 +79,10 @@ internal suspend fun PointerInputScope.detectDownAndDragGesturesWithObserver(
     observer: TextDragObserver
 ) {
     coroutineScope {
-        launch {
+        launch(start = CoroutineStart.UNDISPATCHED) {
             detectPreDragGesturesWithObserver(observer)
         }
-        launch {
+        launch(start = CoroutineStart.UNDISPATCHED) {
             detectDragGesturesWithObserver(observer)
         }
     }
