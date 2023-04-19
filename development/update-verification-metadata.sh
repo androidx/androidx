2f575883a682cec38817d1c28c8f058b6511216c
Jeff Gaston <jeffrygaston@google.com>
2023-04-19 13:47:50 -0400

    When updating signatures, copy buildSrc deps to root project
    a workaround for https://github.com/gradle/gradle/issues/24822
    
    Bug: 278890578
    
    Change-Id: I90d15bd8022880aa52afb11bf852875471734e12

--- development/update-verification-metadata.sh
+++ development/update-verification-metadata.sh
@@ -17,7 +17,7 @@ function regenerateVerificationMetadata() {
   echo "regenerating verification metadata and keyring"
   # regenerate metadata
   # Need to run a clean build, https://github.com/gradle/gradle/issues/19228
-  runGradle --stacktrace --write-verification-metadata pgp,sha256 --export-keys --dry-run --clean bOS :docs-kmp:zipCombinedKmpDocs
+  runGradle --stacktrace --write-verification-metadata pgp,sha256 --export-keys --dry-run --clean -Pandroidx.update.signatures=true bOS :docs-kmp:zipCombinedKmpDocs
 
   # update verification metadata file
   # also remove 'version=' lines, https://github.com/gradle/gradle/issues/20192
