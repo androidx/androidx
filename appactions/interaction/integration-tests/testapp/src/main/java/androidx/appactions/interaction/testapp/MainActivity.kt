9fdaea7b8091b97a095529ae63f6a3fec015bebc
Jaaz Meribole <jaazm@google.com>
2023-04-28 11:43:06 -0700

    Fix time formatting for timer.
    Test: n/a
    Change-Id: I331a49db49be1300a3c168cffe6fba5d0e3e44ad

--- appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/MainActivity.kt
+++ appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/MainActivity.kt
@@ -6,11 +6,9 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-  private var duration = 59
+  private var duration = 120
   private var hasRunningTimer = false
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,27 +26,16 @@ class MainActivity : AppCompatActivity() {
         object : CountDownTimer((duration * 1000).toLong(), 1000) {
           override fun onTick(millisUntilFinished: Long) {
             runOnUiThread {
-              val time = String.format(
-                Locale.getDefault(),
-                "%02d:%02d:%02d",
-                TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
-                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
-                  TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
-                ),
-                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
-                  TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
-                )
-              )
-              val hourMinSecArray = time.split(":")
-              hours.text = hourMinSecArray[0]
-              mins.text = hourMinSecArray[1]
-              seconds.text = hourMinSecArray[2]
+              val timeArray = millisecondsToTimeString(millisUntilFinished).split(":")
+              hours.text = timeArray[0]
+              mins.text = timeArray[1]
+              seconds.text = timeArray[2]
             }
           }
 
           override fun onFinish() {
             // Reset timer duration
-            duration = 59
+            duration = 120
             hasRunningTimer = false
           }
         }.start()
@@ -56,5 +43,12 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this@MainActivity, "Timer is already running", Toast.LENGTH_SHORT).show()
       }
     }
+  }
+
+  private fun millisecondsToTimeString(milliseconds: Long): String {
+    val hours = milliseconds / 3600000
+    val minutes = (milliseconds % 3600000) / 60000
+    val seconds = (milliseconds % 60000) / 1000
+    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
   }
 }
