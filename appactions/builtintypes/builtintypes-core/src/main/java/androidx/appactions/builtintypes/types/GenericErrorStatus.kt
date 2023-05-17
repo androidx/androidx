3f91502a6eb21f4b03630df59f50ba05e9b498ce
raghavmehta <raghavmehta@google.com>
2023-05-16 17:45:08 -0700

    Introduce ExecutionStatus and various subtypes
    Relnote: Added the ExecutionStatus, GenericErrorStatus, ObjectCreationLimitReachedStatus, SuccessStatus and UnsupportedOperationStatus Built-in Type
    Test: No tests. Builder pattern and polymorphism already tested.
    Change-Id: Ie002315e338e355eff3b1cb3940bc9fc926da58b

---
+++ appactions/builtintypes/builtintypes-core/src/main/java/androidx/appactions/builtintypes/types/GenericErrorStatus.kt
@@ -0,0 +1,349 @@
+// Copyright 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package androidx.appactions.builtintypes.types
+
+import androidx.appactions.builtintypes.properties.DisambiguatingDescription
+import androidx.appactions.builtintypes.properties.Name
+import java.util.Objects
+import kotlin.Any
+import kotlin.Boolean
+import kotlin.Int
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.Map
+import kotlin.collections.emptyMap
+import kotlin.collections.joinToString
+import kotlin.collections.map
+import kotlin.collections.mutableMapOf
+import kotlin.collections.plusAssign
+import kotlin.jvm.JvmStatic
+
+/**
+ * Status indicating that the task was not executed successfully.
+ *
+ * See http://schema.googleapis.com/GenericErrorStatus for context.
+ *
+ * Should not be directly implemented. More properties may be added over time. Instead consider
+ * using [Companion.Builder] or see [AbstractGenericErrorStatus] if you need to extend this type.
+ */
+public interface GenericErrorStatus : ExecutionStatus {
+  /** Converts this [GenericErrorStatus] to its builder with all the properties copied over. */
+  public override fun toBuilder(): Builder<*>
+
+  public companion object {
+    /** Returns a default implementation of [Builder] with no properties set. */
+    @JvmStatic public fun Builder(): Builder<*> = GenericErrorStatusImpl.Builder()
+  }
+
+  /**
+   * Builder for [GenericErrorStatus].
+   *
+   * Should not be directly implemented. More methods may be added over time. See
+   * [AbstractGenericErrorStatus.Builder] if you need to extend this builder.
+   */
+  public interface Builder<Self : Builder<Self>> : ExecutionStatus.Builder<Self> {
+    /** Returns a built [GenericErrorStatus]. */
+    public override fun build(): GenericErrorStatus
+  }
+}
+
+/**
+ * An abstract implementation of [GenericErrorStatus].
+ *
+ * Allows for extension like:
+ * ```kt
+ * class MyGenericErrorStatus internal constructor(
+ *   genericErrorStatus: GenericErrorStatus,
+ *   val foo: String,
+ *   val bars: List<Int>,
+ * ) : AbstractGenericErrorStatus<
+ *   MyGenericErrorStatus,
+ *   MyGenericErrorStatus.Builder
+ * >(genericErrorStatus) {
+ *
+ *   override val selfTypeName =
+ *     "MyGenericErrorStatus"
+ *
+ *   override val additionalProperties: Map<String, Any?>
+ *     get() = mapOf("foo" to foo, "bars" to bars)
+ *
+ *   override fun toBuilderWithAdditionalPropertiesOnly(): Builder {
+ *     return Builder()
+ *       .setFoo(foo)
+ *       .addBars(bars)
+ *   }
+ *
+ *   class Builder :
+ *     AbstractGenericErrorStatus.Builder<
+ *       Builder,
+ *       MyGenericErrorStatus> {...}
+ * }
+ * ```
+ *
+ * Also see [AbstractGenericErrorStatus.Builder].
+ */
+@Suppress("UNCHECKED_CAST")
+public abstract class AbstractGenericErrorStatus<
+  Self : AbstractGenericErrorStatus<Self, Builder>,
+  Builder : AbstractGenericErrorStatus.Builder<Builder, Self>>
+internal constructor(
+  public final override val disambiguatingDescription: DisambiguatingDescription?,
+  public final override val identifier: String?,
+  public final override val name: Name?,
+) : GenericErrorStatus {
+  /**
+   * Human readable name for the concrete [Self] class.
+   *
+   * Used in the [toString] output.
+   */
+  protected abstract val selfTypeName: String
+
+  /**
+   * The additional properties that exist on the concrete [Self] class.
+   *
+   * Used for equality comparison and computing the hash code.
+   */
+  protected abstract val additionalProperties: Map<String, Any?>
+
+  /** A copy-constructor that copies over properties from another [GenericErrorStatus] instance. */
+  public constructor(
+    genericErrorStatus: GenericErrorStatus
+  ) : this(
+    genericErrorStatus.disambiguatingDescription,
+    genericErrorStatus.identifier,
+    genericErrorStatus.name
+  )
+
+  /**
+   * Returns a concrete [Builder] with the additional, non-[GenericErrorStatus] properties copied
+   * over.
+   */
+  protected abstract fun toBuilderWithAdditionalPropertiesOnly(): Builder
+
+  public final override fun toBuilder(): Builder =
+    toBuilderWithAdditionalPropertiesOnly()
+      .setDisambiguatingDescription(disambiguatingDescription)
+      .setIdentifier(identifier)
+      .setName(name)
+
+  public final override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || this::class.java != other::class.java) return false
+    other as Self
+    if (disambiguatingDescription != other.disambiguatingDescription) return false
+    if (identifier != other.identifier) return false
+    if (name != other.name) return false
+    if (additionalProperties != other.additionalProperties) return false
+    return true
+  }
+
+  public final override fun hashCode(): Int =
+    Objects.hash(disambiguatingDescription, identifier, name, additionalProperties)
+
+  public final override fun toString(): String {
+    val attributes = mutableMapOf<String, String>()
+    if (disambiguatingDescription != null) {
+      attributes["disambiguatingDescription"] =
+        disambiguatingDescription.toString(includeWrapperName = false)
+    }
+    if (identifier != null) {
+      attributes["identifier"] = identifier
+    }
+    if (name != null) {
+      attributes["name"] = name.toString(includeWrapperName = false)
+    }
+    attributes += additionalProperties.map { (k, v) -> k to v.toString() }
+    val commaSeparated = attributes.entries.joinToString(separator = ", ") { (k, v) -> """$k=$v""" }
+    return """$selfTypeName($commaSeparated)"""
+  }
+
+  /**
+   * An abstract implementation of [GenericErrorStatus.Builder].
+   *
+   * Allows for extension like:
+   * ```kt
+   * class MyGenericErrorStatus :
+   *   : AbstractGenericErrorStatus<
+   *     MyGenericErrorStatus,
+   *     MyGenericErrorStatus.Builder>(...) {
+   *
+   *   class Builder
+   *   : Builder<
+   *       Builder,
+   *       MyGenericErrorStatus
+   *   >() {
+   *     private var foo: String? = null
+   *     private val bars = mutableListOf<Int>()
+   *
+   *     override val selfTypeName =
+   *       "MyGenericErrorStatus.Builder"
+   *
+   *     override val additionalProperties: Map<String, Any?>
+   *       get() = mapOf("foo" to foo, "bars" to bars)
+   *
+   *     override fun buildFromGenericErrorStatus(
+   *       genericErrorStatus: GenericErrorStatus
+   *     ): MyGenericErrorStatus {
+   *       return MyGenericErrorStatus(
+   *         genericErrorStatus,
+   *         foo,
+   *         bars.toList()
+   *       )
+   *     }
+   *
+   *     fun setFoo(string: String): Builder {
+   *       return apply { foo = string }
+   *     }
+   *
+   *     fun addBar(int: Int): Builder {
+   *       return apply { bars += int }
+   *     }
+   *
+   *     fun addBars(values: Iterable<Int>): Builder {
+   *       return apply { bars += values }
+   *     }
+   *   }
+   * }
+   * ```
+   *
+   * Also see [AbstractGenericErrorStatus].
+   */
+  @Suppress("StaticFinalBuilder")
+  public abstract class Builder<
+    Self : Builder<Self, Built>, Built : AbstractGenericErrorStatus<Built, Self>> :
+    GenericErrorStatus.Builder<Self> {
+    /**
+     * Human readable name for the concrete [Self] class.
+     *
+     * Used in the [toString] output.
+     */
+    @get:Suppress("GetterOnBuilder") protected abstract val selfTypeName: String
+
+    /**
+     * The additional properties that exist on the concrete [Self] class.
+     *
+     * Used for equality comparison and computing the hash code.
+     */
+    @get:Suppress("GetterOnBuilder") protected abstract val additionalProperties: Map<String, Any?>
+
+    private var disambiguatingDescription: DisambiguatingDescription? = null
+
+    private var identifier: String? = null
+
+    private var name: Name? = null
+
+    /**
+     * Builds a concrete [Built] instance, given a built [GenericErrorStatus].
+     *
+     * Subclasses should override this method to build a concrete [Built] instance that holds both
+     * the [GenericErrorStatus]-specific properties and the subclass specific
+     * [additionalProperties].
+     *
+     * See the sample code in the documentation of this class for more context.
+     */
+    @Suppress("BuilderSetStyle")
+    protected abstract fun buildFromGenericErrorStatus(
+      genericErrorStatus: GenericErrorStatus
+    ): Built
+
+    public final override fun build(): Built =
+      buildFromGenericErrorStatus(
+        GenericErrorStatusImpl(disambiguatingDescription, identifier, name)
+      )
+
+    public final override fun setDisambiguatingDescription(
+      disambiguatingDescription: DisambiguatingDescription?
+    ): Self {
+      this.disambiguatingDescription = disambiguatingDescription
+      return this as Self
+    }
+
+    public final override fun setIdentifier(text: String?): Self {
+      this.identifier = text
+      return this as Self
+    }
+
+    public final override fun setName(name: Name?): Self {
+      this.name = name
+      return this as Self
+    }
+
+    @Suppress("BuilderSetStyle")
+    public final override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (other == null || this::class.java != other::class.java) return false
+      other as Self
+      if (disambiguatingDescription != other.disambiguatingDescription) return false
+      if (identifier != other.identifier) return false
+      if (name != other.name) return false
+      if (additionalProperties != other.additionalProperties) return false
+      return true
+    }
+
+    @Suppress("BuilderSetStyle")
+    public final override fun hashCode(): Int =
+      Objects.hash(disambiguatingDescription, identifier, name, additionalProperties)
+
+    @Suppress("BuilderSetStyle")
+    public final override fun toString(): String {
+      val attributes = mutableMapOf<String, String>()
+      if (disambiguatingDescription != null) {
+        attributes["disambiguatingDescription"] =
+          disambiguatingDescription!!.toString(includeWrapperName = false)
+      }
+      if (identifier != null) {
+        attributes["identifier"] = identifier!!
+      }
+      if (name != null) {
+        attributes["name"] = name!!.toString(includeWrapperName = false)
+      }
+      attributes += additionalProperties.map { (k, v) -> k to v.toString() }
+      val commaSeparated =
+        attributes.entries.joinToString(separator = ", ") { (k, v) -> """$k=$v""" }
+      return """$selfTypeName($commaSeparated)"""
+    }
+  }
+}
+
+internal class GenericErrorStatusImpl :
+  AbstractGenericErrorStatus<GenericErrorStatusImpl, GenericErrorStatusImpl.Builder> {
+  protected override val selfTypeName: String
+    get() = "GenericErrorStatus"
+
+  protected override val additionalProperties: Map<String, Any?>
+    get() = emptyMap()
+
+  public constructor(
+    disambiguatingDescription: DisambiguatingDescription?,
+    identifier: String?,
+    name: Name?,
+  ) : super(disambiguatingDescription, identifier, name)
+
+  public constructor(genericErrorStatus: GenericErrorStatus) : super(genericErrorStatus)
+
+  protected override fun toBuilderWithAdditionalPropertiesOnly(): Builder = Builder()
+
+  internal class Builder : AbstractGenericErrorStatus.Builder<Builder, GenericErrorStatusImpl>() {
+    protected override val selfTypeName: String
+      get() = "GenericErrorStatus.Builder"
+
+    protected override val additionalProperties: Map<String, Any?>
+      get() = emptyMap()
+
+    protected override fun buildFromGenericErrorStatus(
+      genericErrorStatus: GenericErrorStatus
+    ): GenericErrorStatusImpl =
+      genericErrorStatus as? GenericErrorStatusImpl ?: GenericErrorStatusImpl(genericErrorStatus)
+  }
+}
