0c55e58b9dfe0c1db741f48d886670d5fde970ce
Shalom Gibly <sgibly@google.com>
2023-05-23 01:48:09 +0200

    Removes a direct use of NumberFormat in commonMain
    - The NumberFormat is from java.text, so this change relocate its direct
      usage to jvmMain for multi-platform usage.
    - Also, remove a usage of java.lang.Integer.max in favor of kotlin.math.max.
    
    Bug: 282996823
    Test: Manually tested that the Date and Time pickers are functional and
    format the numbers correctly in English and Arabic.
    Relnote: N/A
    
    Change-Id: Ib2dfd6c9c83e13080fc4e8c0aaed43d7d1f83cf9

--- compose/material3/material3/src/jvmMain/kotlin/androidx/compose/material3/ActualJvm.kt
+++ compose/material3/material3/src/jvmMain/kotlin/androidx/compose/material3/ActualJvm.kt
@@ -18,6 +18,8 @@
 
 package androidx.compose.material3
 
+import java.text.NumberFormat
+
 /* Copy of androidx.compose.material.ActualJvm, mirrored from Foundation. This is used for the
    M2/M3-internal copy of MutatorMutex.
  */
@@ -34,3 +36,18 @@ actual typealias CalendarLocale = java.util.Locale
  * Returns the default [CalendarLocale].
  */
 internal actual fun defaultLocale(): CalendarLocale = java.util.Locale.getDefault()
+
+/**
+ * Returns a string representation of an integer for the current Locale.
+ */
+internal actual fun Int.toLocalString(
+    minDigits: Int,
+    maxDigits: Int,
+    isGroupingUsed: Boolean
+): String {
+    val formatter = NumberFormat.getIntegerInstance()
+    formatter.isGroupingUsed = isGroupingUsed
+    formatter.minimumIntegerDigits = minDigits
+    formatter.maximumIntegerDigits = maxDigits
+    return formatter.format(this)
+}
