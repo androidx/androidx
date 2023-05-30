58f586c739a150b261a0f5cf8b9efa539880bb87
drchen <conradchen@google.com>
2023-05-30 13:02:13 -0700

    Create calculation function of ThreePaneScaffoldState
    Relnote: introduce calculation function of ThreePaneScaffoldState
    Test: unit-test
    Change-Id: I3d0beab44ef327c80bc9feb7217d60a513f2e828

--- compose/material3/material3-adaptive/src/commonMain/kotlin/androidx/compose/material3/adaptive/ThreePaneScaffoldValue.kt
+++ compose/material3/material3-adaptive/src/commonMain/kotlin/androidx/compose/material3/adaptive/ThreePaneScaffoldValue.kt
@@ -18,16 +18,63 @@ package androidx.compose.material3.adaptive
 
 import androidx.compose.runtime.Immutable
 
+@ExperimentalMaterial3AdaptiveApi
+private inline fun buildThreePaneScaffoldValue(
+    buildAction: (ThreePaneScaffoldRole) -> PaneAdaptedValue
+): ThreePaneScaffoldValue {
+    return ThreePaneScaffoldValue(
+        buildAction(ThreePaneScaffoldRole.Primary),
+        buildAction(ThreePaneScaffoldRole.Secondary),
+        buildAction(ThreePaneScaffoldRole.Tertiary)
+    )
+}
+
 /**
- * The adapted state of [ThreePaneScaffold]. It contains each pane's adapted state.
- * [ThreePaneScaffold] will use the adapted states to decide which panes should be displayed
+ * Calculates the current adapted value of [ThreePaneScaffold] according to the given
+ * [maxHorizontalPartitions], [adaptStrategies] and [currentFocus]. The returned value can be used
+ * as a unique representation of the current layout structure.
+ *
+ * The function will treat the current focus as the highest priority and then adapt the rest
+ * panes according to the order of [ThreePaneScaffoldRole.Primary],
+ * [ThreePaneScaffoldRole.Secondary] and [ThreePaneScaffoldRole.Tertiary]. If there are still
+ * remaining partitions to put the pane, the pane will be set as [PaneAdaptedValue.Expanded],
+ * otherwise it will be adapted according to its associated [AdaptStrategy].
+ *
+ * @param maxHorizontalPartitions The maximum allowed partitions along the horizontal axis, i.e.
+ *                                how many expanded panes can be shown at the same time.
+ * @param adaptStrategies The adapt strategies of each pane role that [ThreePaneScaffold] supports.
+ * @param currentFocus The current focused pane, which will be treated as the highest priority, can
+ *                     be `null`.
+ */
+@ExperimentalMaterial3AdaptiveApi
+fun calculateThreePaneScaffoldValue(
+    maxHorizontalPartitions: Int,
+    adaptStrategies: ThreePaneScaffoldAdaptStrategies,
+    currentFocus: ThreePaneScaffoldRole? = null,
+): ThreePaneScaffoldValue {
+    var expandedCount = if (currentFocus != null) 1 else 0
+    return buildThreePaneScaffoldValue { role ->
+        when {
+            role == currentFocus -> PaneAdaptedValue.Expanded
+            expandedCount < maxHorizontalPartitions -> {
+                expandedCount++
+                PaneAdaptedValue.Expanded
+            }
+            else -> adaptStrategies[role].adapt()
+        }
+    }
+}
+
+/**
+ * The adapted value of [ThreePaneScaffold]. It contains each pane's adapted value.
+ * [ThreePaneScaffold] will use the adapted values to decide which panes should be displayed
  * and how they should be displayed. With other input parameters of [ThreePaneScaffold] fixed,
  * each possible instance of this class should represent a unique state of [ThreePaneScaffold]
  * and developers can compare two [ThreePaneScaffoldValue] to decide if there is a layout structure
  * change.
  *
- * For a Material-opinionated layout, it's suggested to use [calculateThreePaneScaffoldState] to
- * calculate the current scaffold state.
+ * For a Material-opinionated layout, it's suggested to use [calculateThreePaneScaffoldValue] to
+ * calculate the current scaffold value.
  *
  * @param primary [PaneAdaptedValue] of the primary pane of [ThreePaneScaffold]
  * @param secondary [PaneAdaptedValue] of the secondary pane of [ThreePaneScaffold]
@@ -55,4 +102,11 @@ class ThreePaneScaffoldValue(
         result = 31 * result + tertiary.hashCode()
         return result
     }
+
+    operator fun get(role: ThreePaneScaffoldRole): PaneAdaptedValue =
+        when (role) {
+            ThreePaneScaffoldRole.Primary -> primary
+            ThreePaneScaffoldRole.Secondary -> secondary
+            ThreePaneScaffoldRole.Tertiary -> tertiary
+        }
 }
