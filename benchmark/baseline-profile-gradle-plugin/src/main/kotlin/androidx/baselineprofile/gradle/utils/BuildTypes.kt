f814c1eac9161460d2aa708e56d1818b52b7f47d
Marcello Albano <maralb@google.com>
2023-05-31 14:15:50 -0700

    Extended build type will now copy base build type sources
    This PR ensures that the extended build types created in the app target
    module for baseline profile gradle plugin, copy also the sources of the
    base build type. For example, `nonMinifiedRelease` should use all the
    sources of `release`. In order to test this, Hilt has been added to
    baselineprofile-consumer integration test.
    
    Bug: 282787832
    Test: ./gradlew :benchmark:benchmark-baseline-profile-gradle-plugin:test :benchmark:integration-tests:baselineprofile-library-consumer:testBaselineProfileGeneration :benchmark:integration-tests:baselineprofile-flavor-consumer:testBaselineProfileGeneration :benchmark:integration-tests:baselineprofile-consumer:testBaselineProfileGeneration
    Change-Id: Ib4d56a85daae71f2330acc839a04f90a3dd57a38

--- benchmark/baseline-profile-gradle-plugin/src/main/kotlin/androidx/baselineprofile/gradle/utils/BuildTypes.kt
+++ benchmark/baseline-profile-gradle-plugin/src/main/kotlin/androidx/baselineprofile/gradle/utils/BuildTypes.kt
@@ -15,7 +15,11 @@
  */
 package androidx.baselineprofile.gradle.utils
 
+import com.android.build.api.dsl.AndroidSourceDirectorySet
+import com.android.build.api.dsl.AndroidSourceSet
 import com.android.build.api.dsl.BuildType
+import com.android.build.gradle.internal.api.DefaultAndroidSourceDirectorySet
+import com.android.build.gradle.internal.api.DefaultAndroidSourceFile
 import org.gradle.api.GradleException
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
@@ -29,33 +33,34 @@ internal inline fun <reified T : BuildType> createExtendedBuildTypes(
     extendedBuildTypeToOriginalBuildTypeMapping: MutableMap<String, String> = mutableMapOf()
 ) {
     extensionBuildTypes.filter { buildType ->
-            if (buildType !is T) {
-                throw GradleException(
-                    "Build type `${buildType.name}` is not of type ${T::class}"
-                )
-            }
-            filterBlock(buildType)
-        }.forEach { buildType ->
-
-            val newBuildTypeName = camelCase(newBuildTypePrefix, buildType.name)
-
-            // Check in case the build type was created manually (to allow full customization)
-            if (extensionBuildTypes.findByName(newBuildTypeName) != null) {
-                project.logger.info(
-                    "Build type $newBuildTypeName won't be created because already exists."
-                )
-            } else {
-                // If the new build type doesn't exist, create it simply extending the configured
-                // one (by default release).
-                extensionBuildTypes.create(newBuildTypeName).apply {
-                    initWith(buildType)
-                    matchingFallbacks += listOf(buildType.name)
-                    configureBlock(this as T)
-                }
-            }
-            // Mapping the build type to the newly created
-            extendedBuildTypeToOriginalBuildTypeMapping[newBuildTypeName] = buildType.name
+        if (buildType !is T) {
+            throw GradleException(
+                "Build type `${buildType.name}` is not of type ${T::class}"
+            )
         }
+        filterBlock(buildType)
+    }.forEach { buildType ->
+
+        val newBuildTypeName = camelCase(newBuildTypePrefix, buildType.name)
+
+        // Check in case the build type was created manually (to allow full customization)
+        if (extensionBuildTypes.findByName(newBuildTypeName) != null) {
+            project.logger.info(
+                "Build type $newBuildTypeName won't be created because already exists."
+            )
+        } else {
+            // If the new build type doesn't exist, create it simply extending the configured
+            // one (by default release).
+            extensionBuildTypes.create(newBuildTypeName).apply {
+                initWith(buildType)
+                matchingFallbacks += listOf(buildType.name)
+                configureBlock(this as T)
+            }
+        }
+
+        // Mapping the build type to the newly created
+        extendedBuildTypeToOriginalBuildTypeMapping[newBuildTypeName] = buildType.name
+    }
 }
 
 internal inline fun <reified T : BuildType> createBuildTypeIfNotExists(
@@ -76,4 +81,39 @@ internal inline fun <reified T : BuildType> createBuildTypeIfNotExists(
     extensionBuildTypes.create(buildTypeName).apply {
         configureBlock(this)
     }
+}
+
+internal fun copyBuildTypeSources(
+    extensionSourceSets: NamedDomainObjectContainer<out AndroidSourceSet>,
+    fromToMapping: Map<String, String>
+) {
+    extensionSourceSets
+        .filter { it.name in fromToMapping.keys }
+        .forEach { toSourceSets ->
+
+            val fromBuildType = fromToMapping[toSourceSets.name]!!
+            val fromSourceSets = extensionSourceSets.getByName(fromBuildType)
+
+            // Copies each specified source set
+            for (dirSet in listOf<(AndroidSourceSet) -> (AndroidSourceDirectorySet)>(
+                { it.aidl },
+                { it.assets },
+                { it.java },
+                { it.jniLibs },
+                { it.kotlin },
+                { it.mlModels },
+                { it.renderscript },
+                { it.res },
+                { it.resources },
+                { it.shaders },
+            )) {
+                val fromSet = dirSet(fromSourceSets) as DefaultAndroidSourceDirectorySet
+                val toSet = dirSet(toSourceSets) as DefaultAndroidSourceDirectorySet
+                toSet.srcDirs(*fromSet.srcDirs.toTypedArray())
+            }
+
+            // Copies the manifest file
+            val manifestFile = (fromSourceSets.manifest as DefaultAndroidSourceFile).srcFile
+            toSourceSets.manifest.srcFile(manifestFile)
+        }
 }
