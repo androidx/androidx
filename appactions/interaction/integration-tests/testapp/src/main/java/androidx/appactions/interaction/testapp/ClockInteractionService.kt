bb9502254f77075b711cfc35e65af4d6bf62bb58
srilaxmikanna <srilaxmikanna@google.com>
2023-06-02 14:00:40 +0000

    Add Pause and Resume functionality to Timer
    Test: Manual
    
    Change-Id: I230c6f056ec5cd24802333b34808b665b0c2ffb0

--- appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/ClockInteractionService.kt
+++ appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/ClockInteractionService.kt
@@ -16,6 +16,8 @@
 
 package androidx.appactions.interaction.testapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
@@ -23,16 +25,29 @@ import androidx.appactions.interaction.capabilities.core.Capability
 import androidx.appactions.interaction.capabilities.core.ExecutionCallback
 import androidx.appactions.interaction.capabilities.core.ExecutionResult
 import androidx.appactions.interaction.capabilities.core.properties.Property
+import androidx.appactions.interaction.capabilities.productivity.PauseTimer
+import androidx.appactions.interaction.capabilities.productivity.ResumeTimer
 import androidx.appactions.interaction.capabilities.productivity.StartTimer
+import androidx.appactions.interaction.capabilities.productivity.StopTimer
 import androidx.appactions.interaction.service.AppInteractionService
 import androidx.appactions.interaction.service.AppVerificationInfo
 import java.time.Duration
 
+const val KEY_TIMER_SHARED_PREFERENCES = "timer_shared_preferences"
+const val KEY_TIMER_STARTED_AT = "timer_started_at"
+const val KEY_TIMER_NAME = "timer_name"
+const val KEY_TIMER_DURATION = "timer_duration"
+const val KEY_TIMER_PAUSED_AT = "timer_paused_at"
+
 class ClockInteractionService : AppInteractionService() {
 
-    lateinit var mHandler: Handler
+    private lateinit var mHandler: Handler
+    private var duration: Long = 0L
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate() {
         super.onCreate()
+        sharedPreferences = applicationContext
+            .getSharedPreferences(KEY_TIMER_SHARED_PREFERENCES, Context.MODE_PRIVATE)
         mHandler = Handler(Looper.myLooper()!!)
     }
 
@@ -46,22 +61,66 @@ class ClockInteractionService : AppInteractionService() {
         })
     }
 
-    private val capability =
+    private val startTimerCapability =
         StartTimer.CapabilityBuilder()
             .setId("start_timer_oneshot")
             .setDurationProperty(Property<Duration>(isRequiredForExecution = true))
             .setExecutionCallback(ExecutionCallback<StartTimer.Arguments, StartTimer.Output> {
+                val result = ExecutionResult.Builder<StartTimer.Output>().build()
                 val name = it.name ?: "Default title"
                 val duration = it.duration!!
+                if (sharedPreferences.getBoolean(KEY_IS_TIMER_RUNNING, false)) {
+                    showToast(msg = "Could not start a timer because one is already running")
+                    return@ExecutionCallback result
+                }
+                sharedPreferences.edit().putLong(KEY_TIMER_STARTED_AT, System.currentTimeMillis())
+                sharedPreferences.edit().putBoolean(KEY_IS_TIMER_RUNNING, true)
+                sharedPreferences.edit().putString(KEY_TIMER_NAME, name)
+                sharedPreferences.edit().putLong(KEY_TIMER_DURATION, duration.toMillis())
+                showToast(msg = "Timer Name:$name Duration:$duration has been started")
 
-                // Do execution. ie. create the Timer called $name for $duration
-                showToast(msg = "Name:$name Duration:$duration")
-
-                ExecutionResult.Builder<StartTimer.Output>().build()
+                result
             })
             .build()
 
-    override val registeredCapabilities: List<Capability> = listOf(capability)
+    private val pauseTimerCapability =
+        PauseTimer.CapabilityBuilder()
+            .setId("pause_timer_oneshot")
+            .setExecutionCallback(ExecutionCallback {
+                sharedPreferences.edit().putLong(KEY_TIMER_PAUSED_AT, System.currentTimeMillis())
+                ExecutionResult.Builder<PauseTimer.Output>().build()
+            })
+            .build()
+
+    private val resumeTimerCapability =
+        ResumeTimer.CapabilityBuilder()
+            .setId("resume_timer_oneshot")
+            .setExecutionCallback(ExecutionCallback {
+                val pausedTime = sharedPreferences.getLong(KEY_TIMER_PAUSED_AT, 0L)
+                val startedTime = sharedPreferences.getLong(KEY_TIMER_STARTED_AT, 0L)
+                val elapsedTime = pausedTime - startedTime
+                val duration = duration - elapsedTime
+                sharedPreferences.edit().putLong(KEY_TIMER_STARTED_AT, System.currentTimeMillis())
+                sharedPreferences.edit().putLong(KEY_TIMER_DURATION, duration)
+                ExecutionResult.Builder<ResumeTimer.Output>().build()
+            })
+            .build()
+
+    private val stopTimerCapability =
+        StopTimer.CapabilityBuilder()
+            .setId("resume_timer_oneshot")
+            .setExecutionCallback(ExecutionCallback {
+                sharedPreferences.edit().putBoolean(KEY_IS_TIMER_RUNNING, false)
+                ExecutionResult.Builder<StopTimer.Output>().build()
+            })
+            .build()
+
+    override val registeredCapabilities: List<Capability> = listOf(
+        startTimerCapability,
+        pauseTimerCapability,
+        resumeTimerCapability,
+        stopTimerCapability,
+    )
     override val allowedApps: List<AppVerificationInfo> = listOf(
         AppVerificationInfo.Builder()
             .addSignature(hex2Byte(ASSISTANT_SIGNATURE))
--- appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/MainActivity.kt
+++ appactions/interaction/integration-tests/testapp/src/main/java/androidx/appactions/interaction/testapp/MainActivity.kt
@@ -1,54 +1,112 @@
 package androidx.appactions.interaction.testapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
+const val KEY_IS_TIMER_PAUSED = "timer_paused"
+const val KEY_IS_TIMER_RUNNING = "timer_running"
+const val DEFAULT_DURATION = 120 * 1000L
+
 class MainActivity : AppCompatActivity() {
-  private var duration = 120
-  private var hasRunningTimer = false
+    private var remainingDuration = 0L
+    private var hasRunningTimer = false
+    private var startButton: AppCompatButton? = null
+    private lateinit var timer: CountDownTimer
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var remainingTimeLabel: TextView
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sharedPreferences = applicationContext
+            .getSharedPreferences(KEY_TIMER_SHARED_PREFERENCES, Context.MODE_PRIVATE)
+        startButton = findViewById(R.id.startButton)
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel)
+        hasRunningTimer = sharedPreferences.getBoolean(KEY_IS_TIMER_RUNNING, false)
 
-    val hours: TextView = findViewById(R.id.hour)
-    val mins: TextView = findViewById(R.id.minute)
-    val seconds: TextView = findViewById(R.id.second)
-    val startButton: AppCompatButton = findViewById(R.id.startButton)
-
-    startButton.setOnClickListener {
-      if (!hasRunningTimer) {
-        hasRunningTimer = true
-        object : CountDownTimer((duration * 1000).toLong(), 1000) {
-          override fun onTick(millisUntilFinished: Long) {
-            runOnUiThread {
-              val timeArray = millisecondsToTimeString(millisUntilFinished).split(":")
-              hours.text = timeArray[0]
-              mins.text = timeArray[1]
-              seconds.text = timeArray[2]
+        if (hasRunningTimer) {
+            remainingDuration = sharedPreferences.getLong(KEY_TIMER_DURATION, 0L)
+            if (!sharedPreferences.getBoolean(KEY_IS_TIMER_PAUSED, false)) {
+                val startedTime = sharedPreferences.getLong(KEY_TIMER_STARTED_AT, 0L)
+                val elapsedTime = System.currentTimeMillis() - startedTime
+                remainingDuration = remainingDuration - elapsedTime
             }
-          }
+            startTimer(remainingDuration)
+        }
 
-          override fun onFinish() {
-            // Reset timer duration
-            duration = 120
-            hasRunningTimer = false
-          }
-        }.start()
-      } else {
-        Toast.makeText(this@MainActivity, "Timer is already running", Toast.LENGTH_SHORT).show()
-      }
+        startButton?.apply {
+            if (!hasRunningTimer) {
+                text = "Start"
+            } else if (sharedPreferences.getBoolean(KEY_IS_TIMER_PAUSED, false)) {
+                text = "Resume"
+            } else {
+                text = "Pause"
+            }
+            setOnClickListener {
+                setTimer(DEFAULT_DURATION)
+            }
+        }
     }
-  }
+    private fun setTimer(duration: Long) {
+        hasRunningTimer = sharedPreferences.getBoolean(KEY_IS_TIMER_RUNNING, false)
+        if (!hasRunningTimer) {
+            startButton?.text = "Pause"
+            startTimer(duration)
+        } else if (sharedPreferences.getBoolean(KEY_IS_TIMER_PAUSED, false)) {
+            startButton?.text = "Pause"
+            resumeTimer()
+        } else {
+            startButton?.text = "Resume"
+            pauseTimer()
+        }
+    }
+    private fun startTimer(duration: Long) {
+        sharedPreferences.edit().putBoolean(KEY_IS_TIMER_RUNNING, true).apply()
+        timer = object : CountDownTimer(duration, 1000) {
 
-  private fun millisecondsToTimeString(milliseconds: Long): String {
-    val hours = milliseconds / 3600000
-    val minutes = (milliseconds % 3600000) / 60000
-    val seconds = (milliseconds % 60000) / 1000
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
-  }
+            override fun onTick(millisUntilFinished: Long) {
+                runOnUiThread {
+                    this@MainActivity.remainingDuration = millisUntilFinished
+                    val timeArray = millisecondsToTimeString(millisUntilFinished).split(":")
+                    remainingTimeLabel.text = "$timeArray[0]:$timeArray[1]:$timeArray[2]"
+                }
+            }
+            override fun onFinish() {
+                // Reset timer duration
+                this@MainActivity.remainingDuration = 0
+                sharedPreferences.edit().putBoolean(KEY_IS_TIMER_RUNNING, false).apply()
+            }
+        }
+        timer.start()
+    }
+
+    private fun pauseTimer() {
+        timer.cancel()
+        sharedPreferences.edit().putBoolean(KEY_IS_TIMER_PAUSED, true).apply()
+        sharedPreferences.edit().putLong(KEY_TIMER_DURATION, this.remainingDuration).apply()
+    }
+
+    private fun resumeTimer() {
+        val duration = sharedPreferences.getLong(KEY_TIMER_DURATION, DEFAULT_DURATION)
+        sharedPreferences.edit().putBoolean(KEY_IS_TIMER_PAUSED, false).apply()
+        sharedPreferences.edit().putLong(KEY_TIMER_STARTED_AT, System.currentTimeMillis()).apply()
+        startTimer(duration)
+    }
+
+    private fun stopTimer() {
+        sharedPreferences.edit().putBoolean(KEY_IS_TIMER_RUNNING, false).apply()
+        sharedPreferences.edit().putBoolean(KEY_IS_TIMER_PAUSED, false).apply()
+    }
+
+    private fun millisecondsToTimeString(milliseconds: Long): String {
+        val hours = milliseconds / 3600000
+        val minutes = (milliseconds % 3600000) / 60000
+        val seconds = (milliseconds % 60000) / 1000
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    }
 }
