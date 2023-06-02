5b2d285fe8a604f046ea355aa14d461130d75280
Alan Viverette <alanv@google.com>
2023-06-02 16:51:58 -0400

    Insert default platform dependencies for KMP anchor publications
    Specifies default for projects with multiple platforms.
    
    Bug: 285353844
    Test: MavenUploadHelperTest
    Change-Id: I1ea37fa55db8089bfc597b6408fcf13e41ba0c8d

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXMultiplatformExtension.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXMultiplatformExtension.kt
@@ -18,6 +18,7 @@ package androidx.build
 
 import groovy.lang.Closure
 import org.gradle.api.Action
+import org.gradle.api.GradleException
 import org.gradle.api.NamedDomainObjectCollection
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
@@ -48,6 +49,65 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     }
     private val kotlinExtension: KotlinMultiplatformExtension by kotlinExtensionDelegate
 
+    /**
+     * The list of platforms that have been requested in the build configuration.
+     *
+     * The list of enabled platforms in [targetPlatforms] will vary based on the build environment.
+     * For example, a project's build configuration may have requested `mac()` but this is not
+     * available when building on Linux.
+     */
+    val requestedPlatforms: MutableSet<PlatformIdentifier> = mutableSetOf()
+
+    /**
+     * The list of platforms that are enabled.
+     *
+     * This will vary across build environments. For example, a project's build configuration may
+     * have requested `mac()` but this is not available when building on Linux.
+     */
+    val targetPlatforms: List<String>
+        get() = if (kotlinExtensionDelegate.isInitialized()) {
+            kotlinExtension.targets.mapNotNull {
+                if (it.targetName != "metadata") {
+                    it.targetName
+                } else {
+                    null
+                }
+            }
+        } else {
+            throw GradleException("Kotlin multi-platform extension has not been initialized")
+        }
+
+    /**
+     * Default platform identifier used for specifying POM dependencies.
+     *
+     * This platform will be added as a dependency to the multi-platform anchor artifact's POM
+     * publication. For example, if the anchor artifact is `collection` and the default platform
+     * is `jvm`, then the POM for `collection` will express a dependency on `collection-jvm`. This
+     * ensures that developers who are silently upgrade to KMP artifacts but are not using Gradle
+     * still see working artifacts.
+     *
+     * If no default was specified and a single platform is requested (ex. using [jvm]), returns
+     * the identifier for that platform.
+     */
+    var defaultPlatform: String? = null
+        get() = field ?: requestedPlatforms.singleOrNull()?.id
+
+        set(value) {
+            if (value != null) {
+                if (requestedPlatforms.none { it.id == value }) {
+                    throw GradleException("Platform $value has not been requested as a target. " +
+                        "Available platforms are: " +
+                        requestedPlatforms.joinToString(", ") { it.id })
+                }
+                if (targetPlatforms.none { it == value }) {
+                    throw GradleException("Platform $value is not available in this build " +
+                        "environment. Available platforms are: " +
+                        targetPlatforms.joinToString(", "))
+                }
+            }
+            field = value
+        }
+
     val presets: NamedDomainObjectCollection<KotlinTargetPreset<*>>
         get() = kotlinExtension.presets
     val targets: NamedDomainObjectCollection<KotlinTarget>
@@ -65,10 +125,24 @@ open class AndroidXMultiplatformExtension(val project: Project) {
         }
     }
 
+    /**
+     * Sets the default target platform.
+     *
+     * The default target platform *must* be enabled in all build environments. For projects which
+     * request multiple target platforms, this method *must* be called to explicitly specify a
+     * default target platform.
+     *
+     * See [defaultPlatform] for details on how the value is used.
+     */
+    fun defaultPlatform(value: PlatformIdentifier) {
+        defaultPlatform = value.id
+    }
+
     @JvmOverloads
     fun jvm(
         block: Action<KotlinJvmTarget>? = null
     ): KotlinJvmTarget? {
+        requestedPlatforms.add(PlatformIdentifier.JVM)
         return if (project.enableJvm()) {
             kotlinExtension.jvm {
                 block?.execute(this)
@@ -80,6 +154,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun android(
         block: Action<KotlinAndroidTarget>? = null
     ): KotlinAndroidTarget? {
+        requestedPlatforms.add(PlatformIdentifier.ANDROID)
         return if (project.enableJvm()) {
             kotlinExtension.android {
                 block?.execute(this)
@@ -91,6 +166,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun desktop(
         block: Action<KotlinJvmTarget>? = null
     ): KotlinJvmTarget? {
+        requestedPlatforms.add(PlatformIdentifier.DESKTOP)
         return if (project.enableDesktop()) {
             kotlinExtension.jvm("desktop") {
                 block?.execute(this)
@@ -115,6 +191,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun macosX64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTargetWithHostTests? {
+        requestedPlatforms.add(PlatformIdentifier.MAC_OSX_64)
         return if (project.enableMac()) {
             kotlinExtension.macosX64().also {
                 block?.execute(it)
@@ -126,6 +203,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun macosArm64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTargetWithHostTests? {
+        requestedPlatforms.add(PlatformIdentifier.MAC_ARM_64)
         return if (project.enableMac()) {
             kotlinExtension.macosArm64().also {
                 block?.execute(it)
@@ -137,6 +215,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun iosArm64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTarget? {
+        requestedPlatforms.add(PlatformIdentifier.IOS_ARM_64)
         return if (project.enableMac()) {
             kotlinExtension.iosArm64().also {
                 block?.execute(it)
@@ -161,6 +240,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun iosX64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTarget? {
+        requestedPlatforms.add(PlatformIdentifier.IOS_X_64)
         return if (project.enableMac()) {
             kotlinExtension.iosX64().also {
                 block?.execute(it)
@@ -172,6 +252,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun iosSimulatorArm64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTarget? {
+        requestedPlatforms.add(PlatformIdentifier.IOS_SIMULATOR_ARM_64)
         return if (project.enableMac()) {
             kotlinExtension.iosSimulatorArm64().also {
                 block?.execute(it)
@@ -192,6 +273,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun linuxX64(
         block: Action<KotlinNativeTarget>? = null
     ): KotlinNativeTargetWithHostTests? {
+        requestedPlatforms.add(PlatformIdentifier.LINUX_64)
         return if (project.enableLinux()) {
             kotlinExtension.linuxX64().also {
                 block?.execute(it)
@@ -203,6 +285,7 @@ open class AndroidXMultiplatformExtension(val project: Project) {
     fun js(
         block: Action<KotlinJsTargetDsl>? = null
     ): KotlinJsTargetDsl? {
+        requestedPlatforms.add(PlatformIdentifier.JS)
         return if (project.enableJs()) {
             kotlinExtension.js().also {
                 block?.execute(it)
@@ -223,4 +306,15 @@ open class AndroidXMultiplatformExtension(val project: Project) {
  */
 internal fun Project.hasKotlinNativeTarget(): Provider<Boolean> = project.provider {
     project.extensions.getByType(AndroidXMultiplatformExtension::class.java).hasNativeTarget()
+}
+
+fun Project.validatePublishedMultiplatformHasDefault() {
+    val extension = project.extensions.getByType(AndroidXMultiplatformExtension::class.java)
+    if (extension.defaultPlatform == null && extension.requestedPlatforms.isNotEmpty()) {
+        throw GradleException("Project is published and multiple platforms are requested. You " +
+            "must explicitly specify androidXMultiplatform.defaultPlatform as one of: " +
+            extension.targetPlatforms.joinToString(", ") {
+                "PlatformIdentifier.${PlatformIdentifier.fromId(it)!!.name}"
+            })
+    }
 }
