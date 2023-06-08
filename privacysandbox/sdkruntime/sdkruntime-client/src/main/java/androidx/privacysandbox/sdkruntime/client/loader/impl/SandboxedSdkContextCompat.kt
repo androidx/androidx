de5a501bd86014282dd418157020fcbab88a899a
Anton Kulakov <akulakov@google.com>
2023-06-08 16:07:01 +0000

    Per SDK storage support for SDKs loaded in App process
    Override all storage related methods in SandboxedSdkContextCompat
    to provide dedicated folder to each SDK loaded locally.
    
    Bug: 260563854
    Test: SandboxedSdkContextCompatTest
    Change-Id: I3bfd4eb5113210dc786ca3e5f0d99e184f10db72

--- privacysandbox/sdkruntime/sdkruntime-client/src/main/java/androidx/privacysandbox/sdkruntime/client/loader/impl/SandboxedSdkContextCompat.kt
+++ privacysandbox/sdkruntime/sdkruntime-client/src/main/java/androidx/privacysandbox/sdkruntime/client/loader/impl/SandboxedSdkContextCompat.kt
@@ -17,19 +17,171 @@ package androidx.privacysandbox.sdkruntime.client.loader.impl
 
 import android.content.Context
 import android.content.ContextWrapper
-import androidx.annotation.RestrictTo
+import android.os.Build
+import androidx.annotation.DoNotInline
+import androidx.annotation.RequiresApi
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 /**
  * Refers to the context of the SDK loaded locally.
  *
- * @suppress
+ * Supports Per-SDK storage by pointing storage related APIs to folders unique for each SDK.
+ * Where possible maintains same folders hierarchy as for applications by creating folders
+ * inside [getDataDir].
+ * Folders with special permissions or additional logic (caches, etc) created as subfolders of same
+ * application folders.
+ *
+ * SDK Folders hierarchy (from application [getDataDir]):
+ * 1) /cache/RuntimeEnabledSdksData/<sdk_package_name> - cache
+ * 2) /code_cache/RuntimeEnabledSdksData/<sdk_package_name> - code_cache
+ * 3) /no_backup/RuntimeEnabledSdksData/<sdk_package_name> - no_backup
+ * 4) /app_RuntimeEnabledSdksData/<sdk_package_name>/ - SDK Root (data dir)
+ * 5) /app_RuntimeEnabledSdksData/<sdk_package_name>/files - [getFilesDir]
+ * 6) /app_RuntimeEnabledSdksData/<sdk_package_name>/app_<folder_name> - [getDir]
  */
-@RestrictTo(RestrictTo.Scope.LIBRARY)
 internal class SandboxedSdkContextCompat(
     base: Context,
+    private val sdkPackageName: String,
     private val classLoader: ClassLoader?
 ) : ContextWrapper(base) {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun createDeviceProtectedStorageContext(): Context {
+        return SandboxedSdkContextCompat(
+            Api24.createDeviceProtectedStorageContext(baseContext),
+            sdkPackageName,
+            classLoader
+        )
+    }
+
+    /**
+     *  Points to <app_data_dir>/app_RuntimeEnabledSdksData/<sdk_package_name>
+     */
+    override fun getDataDir(): File {
+        val sdksDataRoot = baseContext.getDir(
+            SDK_ROOT_FOLDER,
+            Context.MODE_PRIVATE
+        )
+        return ensureDirExists(sdksDataRoot, sdkPackageName)
+    }
+
+    /**
+     *  Points to <app_data_dir>/cache/RuntimeEnabledSdksData/<sdk_package_name>
+     */
+    override fun getCacheDir(): File {
+        val sdksCacheRoot = ensureDirExists(baseContext.cacheDir, SDK_ROOT_FOLDER)
+        return ensureDirExists(sdksCacheRoot, sdkPackageName)
+    }
+
+    /**
+     *  Points to <app_data_dir>/code_cache/RuntimeEnabledSdksData/<sdk_package_name>
+     */
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun getCodeCacheDir(): File {
+        val sdksCodeCacheRoot = ensureDirExists(
+            Api21.codeCacheDir(baseContext),
+            SDK_ROOT_FOLDER
+        )
+        return ensureDirExists(sdksCodeCacheRoot, sdkPackageName)
+    }
+
+    /**
+     *  Points to <app_data_dir>/no_backup/RuntimeEnabledSdksData/<sdk_package_name>
+     */
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun getNoBackupFilesDir(): File {
+        val sdksNoBackupRoot = ensureDirExists(
+            Api21.noBackupFilesDir(baseContext),
+            SDK_ROOT_FOLDER
+        )
+        return ensureDirExists(sdksNoBackupRoot, sdkPackageName)
+    }
+
+    /**
+     *  Points to <app_data_dir>/app_RuntimeEnabledSdksData/<sdk_package_name>/app_<folder_name>
+     *  Prefix required to maintain same hierarchy as for applications - when dir could be
+     *  accessed by both [getDir] and [getDir]/app_<folder_name>.
+     */
+    override fun getDir(name: String, mode: Int): File {
+        val dirName = "app_$name"
+        return ensureDirExists(dataDir, dirName)
+    }
+
+    /**
+     *  Points to <app_data_dir>/app_RuntimeEnabledSdksData/<sdk_package_name>/files
+     */
+    override fun getFilesDir(): File {
+        return ensureDirExists(dataDir, "files")
+    }
+
+    override fun openFileInput(name: String): FileInputStream {
+        val file = makeFilename(filesDir, name)
+        return FileInputStream(file)
+    }
+
+    override fun openFileOutput(name: String, mode: Int): FileOutputStream {
+        val file = makeFilename(filesDir, name)
+        val append = (mode and MODE_APPEND) != 0
+        return FileOutputStream(file, append)
+    }
+
+    override fun deleteFile(name: String): Boolean {
+        val file = makeFilename(filesDir, name)
+        return file.delete()
+    }
+
+    override fun getFileStreamPath(name: String): File {
+        return makeFilename(filesDir, name)
+    }
+
+    override fun fileList(): Array<String> {
+        return listOrEmpty(filesDir)
+    }
+
     override fun getClassLoader(): ClassLoader? {
         return classLoader
+    }
+
+    private fun listOrEmpty(dir: File?): Array<String> {
+        return dir?.list() ?: emptyArray()
+    }
+
+    private fun makeFilename(parent: File, name: String): File {
+        if (name.indexOf(File.separatorChar) >= 0) {
+            throw IllegalArgumentException(
+                "File $name contains a path separator"
+            )
+        }
+        return File(parent, name)
+    }
+
+    private fun ensureDirExists(parent: File, dirName: String): File {
+        val dir = File(parent, dirName)
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+        return dir
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private object Api21 {
+        @DoNotInline
+        fun codeCacheDir(context: Context): File = context.codeCacheDir
+
+        @DoNotInline
+        fun noBackupFilesDir(context: Context): File = context.noBackupFilesDir
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private object Api24 {
+        @DoNotInline
+        fun createDeviceProtectedStorageContext(context: Context): Context =
+            context.createDeviceProtectedStorageContext()
+    }
+
+    private companion object {
+        private const val SDK_ROOT_FOLDER = "RuntimeEnabledSdksData"
     }
 }
