9cc90fb383ca7a3daeea1d9b30be7cd1de5e312b
Gavin Corkery <gavincorkery@google.com>
2023-06-12 11:21:08 +0000

    Transfer touch focus to host when onScroll/onFling detected
    Adds a gesture detector to the remote view created by the UI
    provider. If an onScroll or onFling gesture is detected, the touch
    focus is transferred to the host. For all other gestures, nothing
    is intercepted.
    
    The touch interception happens on a UI library owned ViewGroup which
    is a parent of the UI provider's view. This ensures that the SDK's
    onTouchListener is not overridden.
    
    Relnote: Added logic to transfer touch focus to host
    Test: Manual
    Test: BinderAdapterDelegateTest
    Bug: 283748196
    Change-Id: I33b54b772e2802e2f987aa2414e06f83bafd9e7f

--- privacysandbox/ui/ui-provider/src/main/java/androidx/privacysandbox/ui/provider/BinderAdapterDelegate.kt
+++ privacysandbox/ui/ui-provider/src/main/java/androidx/privacysandbox/ui/provider/BinderAdapterDelegate.kt
@@ -112,7 +112,15 @@ private class BinderAdapterDelegate(
 
         override fun onSessionOpened(session: SandboxedUiAdapter.Session) {
             val view = session.view
-            surfaceControlViewHost.setView(view, initialWidth, initialHeight)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                val touchTransferringView = TouchFocusTransferringView(
+                    sandboxContext, surfaceControlViewHost)
+                touchTransferringView.addView(view)
+                surfaceControlViewHost.setView(touchTransferringView, initialWidth, initialHeight)
+            } else {
+                surfaceControlViewHost.setView(view, initialWidth, initialHeight)
+            }
+
             val surfacePackage = surfaceControlViewHost.surfacePackage
             val remoteSessionController =
                 RemoteSessionController(surfaceControlViewHost, session)
