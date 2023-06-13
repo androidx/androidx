96e38b3a3820ff0fa3dcba4aab7551183b781344
marian <ymarian@google.com>
2023-06-13 10:52:41 -0400

    [TimePicker] Modifier incorrectly passed to two composables
    Test: manually tested passing Modifier.padding to timeinput
    Fixes: 282761472
    Fixes: 278242122
    (cherry picked from https://android-review.googlesource.com/q/commit:2469e93ab4c232923d0a89b77f1c44e6436205a0)
    Merged-In: I59df5252ac4adc3559bbda51939dbdb485bc9899
    Change-Id: I59df5252ac4adc3559bbda51939dbdb485bc9899

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/TimePicker.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/TimePicker.kt
@@ -784,7 +784,7 @@ private fun TimeInputImpl(
         }
 
         if (!state.is24hour) {
-            Box(modifier.padding(start = PeriodToggleMargin)) {
+            Box(Modifier.padding(start = PeriodToggleMargin)) {
                 VerticalPeriodToggle(
                     modifier = Modifier.size(
                         PeriodSelectorContainerWidth,
