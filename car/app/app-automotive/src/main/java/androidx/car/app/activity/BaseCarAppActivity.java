97c6e8ff2b2f4fad3d463f7138cbe958ea97231c
Akshat Divekar <adivekar@google.com>
2023-06-15 09:58:06 -0700

    Add support for Android U System Overlays
    Android U adds a new type of inset called SYSTEM_OVERLAY. This commit adds support for that inset on Android U+.
    
    Test: Manual
    Change-Id: I5f7141768ad6407a08d8792b206b9f05d412ae75

--- car/app/app-automotive/src/main/java/androidx/car/app/activity/BaseCarAppActivity.java
+++ car/app/app-automotive/src/main/java/androidx/car/app/activity/BaseCarAppActivity.java
@@ -134,10 +134,18 @@ public abstract class BaseCarAppActivity extends FragmentActivity {
                     // SystemUiVisibility set in CarAppActivity#onCreate(). Failing to do so would
                     // cause a mismatch between the insets applied to the content on the hosts side
                     // vs. the actual visible window available on the client side.
-                    Insets insets = WindowInsetsCompat.toWindowInsetsCompat(windowInsets)
-                            .getInsets(WindowInsetsCompat.Type.systemBars()
-                                    | WindowInsetsCompat.Type.ime())
-                            .toPlatformInsets();
+                    Insets insets;
+                    // Android U+ (SDK 34+) introduced SYSTEM_OVERLAYS insets, which we pass to the
+                    // host to adjust padding.
+                    if (Build.VERSION.SDK_INT >= 34) {
+                        // TODO(b/287700349): Add tests once Robolectric supports SDK 34.
+                        insets = Api34Impl.getInsets(windowInsets);
+                    } else {
+                        insets = WindowInsetsCompat.toWindowInsetsCompat(windowInsets)
+                                .getInsets(WindowInsetsCompat.Type.systemBars()
+                                        | WindowInsetsCompat.Type.ime())
+                                .toPlatformInsets();
+                    }
                     DisplayCutoutCompat displayCutout =
                             WindowInsetsCompat.toWindowInsetsCompat(windowInsets)
                                     .getDisplayCutout();
@@ -243,6 +251,18 @@ public abstract class BaseCarAppActivity extends FragmentActivity {
                     BaseCarAppActivity.this.showAssist(args);
                 }
             };
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private static class Api34Impl {
+
+        private Api34Impl() {
+        }
+
+        static Insets getInsets(WindowInsets windowInsets) {
+            return windowInsets.getInsets(WindowInsets.Type.systemBars() | WindowInsets.Type.ime()
+                    | WindowInsets.Type.systemOverlays());
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.R)
     private static class Api30Impl {
