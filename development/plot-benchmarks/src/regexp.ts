9c70b8f022ceb7ab2239e32c0ea9fedc697b1a9a
Rahul Ravikumar <rahulrav@google.com>
2023-06-15 13:29:04 -0700

    Add a utility to plot benchmark results.
    * This tool is useful when iterating locally, and compare benchmark runs over time.
    
    Test: Tested this locally.
    Change-Id: I7a79df0fa6d3e011056e427b136144fed4f6a032

---
+++ development/plot-benchmarks/src/regexp.ts
@@ -0,0 +1,17 @@
+/**
+ * Supports regular expressions, while falling back gracefully to substring matching.
+ */
+export function expressionFilter(expression: string) {
+  let regExp: RegExp | null = null;
+  try {
+    regExp = new RegExp(expression, 'g')
+  } catch (error) {
+    // Invalid regular expression.
+    // Falling back to substring matching.
+    console.log(`Invalid regular expression ${expression}. Falling back to substring matching.`)
+  }
+  if (regExp) {
+    return (label: string) => regExp.test(label);
+  }
+  return (label: string) => label.indexOf(expression) >= 0
+}
