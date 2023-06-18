e4e01b8205a225df7c3ef0c1ad73f7e12ba5871d
Prachi Mantri <prachimantri@google.com>
2023-06-18 18:52:14 +0000

    Update integration demos for a11y bugs
    Bug: b/287026021, b/287015141, b/287012903
    Test: Run integration demos
    Relnote: "Updating integration test demos to fix a11y bugs"
    Change-Id: I61ce9c80437224e210d39be789d775163f140ca8

--- wear/compose/integration-tests/demos/src/main/java/androidx/wear/compose/integration/demos/DemoApp.kt
+++ wear/compose/integration-tests/demos/src/main/java/androidx/wear/compose/integration/demos/DemoApp.kt
@@ -39,6 +39,8 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.AutoCenteringParams
@@ -140,7 +142,8 @@ internal fun BoxScope.DisplayDemoList(
                     style = MaterialTheme.typography.caption1,
                     color = Color.White,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().semantics { heading() }
+
                 )
             }
         }
