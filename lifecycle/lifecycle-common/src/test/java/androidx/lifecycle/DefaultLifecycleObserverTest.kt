998f6a4c935525ecb077555efcf2646141aab795
sanura <sanura@google.com>
2023-06-20 20:12:12 +0000

    Convert DefaultLifecycleObserverTest to Kotlin
    Part of the kotlin conversion process for the tests in the Lifecycle library.
    
    Test: all tests pass
    Bug: 240298180
    Change-Id: Idc01d2e3caba7303c29ad2a8a90ca196bf47ea6c

--- lifecycle/lifecycle-common/src/test/java/androidx/lifecycle/DefaultLifecycleObserverTest.kt
+++ lifecycle/lifecycle-common/src/test/java/androidx/lifecycle/DefaultLifecycleObserverTest.kt
@@ -13,203 +13,127 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.lifecycle
 
-package androidx.lifecycle;
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito.inOrder
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.reset
+import org.mockito.Mockito.`when`
 
-import static androidx.lifecycle.Lifecycle.Event.ON_ANY;
-import static androidx.lifecycle.Lifecycle.Event.ON_CREATE;
-import static androidx.lifecycle.Lifecycle.Event.ON_DESTROY;
-import static androidx.lifecycle.Lifecycle.Event.ON_PAUSE;
-import static androidx.lifecycle.Lifecycle.Event.ON_RESUME;
-import static androidx.lifecycle.Lifecycle.Event.ON_START;
-import static androidx.lifecycle.Lifecycle.Event.ON_STOP;
-import static androidx.lifecycle.Lifecycle.State.CREATED;
-import static androidx.lifecycle.Lifecycle.State.INITIALIZED;
-import static androidx.lifecycle.Lifecycle.State.RESUMED;
-import static androidx.lifecycle.Lifecycle.State.STARTED;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-
-import androidx.annotation.NonNull;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.InOrder;
-import org.mockito.Mockito;
-
-@RunWith(JUnit4.class)
-public class DefaultLifecycleObserverTest {
-    private LifecycleOwner mOwner;
-    private Lifecycle mLifecycle;
+@RunWith(JUnit4::class)
+class DefaultLifecycleObserverTest {
+    private lateinit var owner: LifecycleOwner
+    private lateinit var lifecycle: Lifecycle
 
     @Before
-    public void initMocks() {
-        mOwner = mock(LifecycleOwner.class);
-        mLifecycle = mock(Lifecycle.class);
-        when(mOwner.getLifecycle()).thenReturn(mLifecycle);
+    fun initMocks() {
+        owner = mock(LifecycleOwner::class.java)
+        lifecycle = mock(Lifecycle::class.java)
+        `when`(owner.lifecycle).thenReturn(lifecycle)
     }
 
     @Test
-    public void eachEvent() {
-        DefaultLifecycleObserver obj = mock(DefaultLifecycleObserver.class);
-        DefaultLifecycleObserverAdapter observer = new DefaultLifecycleObserverAdapter(obj, null);
-        when(mLifecycle.getCurrentState()).thenReturn(CREATED);
-
-        observer.onStateChanged(mOwner, ON_CREATE);
-        InOrder inOrder = Mockito.inOrder(obj);
-        inOrder.verify(obj).onCreate(mOwner);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(STARTED);
-        observer.onStateChanged(mOwner, ON_START);
-        inOrder.verify(obj).onStart(mOwner);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(RESUMED);
-        observer.onStateChanged(mOwner, ON_RESUME);
-        inOrder.verify(obj).onResume(mOwner);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(STARTED);
-        observer.onStateChanged(mOwner, ON_PAUSE);
-        inOrder.verify(obj).onPause(mOwner);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(CREATED);
-        observer.onStateChanged(mOwner, ON_STOP);
-        inOrder.verify(obj).onStop(mOwner);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(INITIALIZED);
-        observer.onStateChanged(mOwner, ON_DESTROY);
-        inOrder.verify(obj).onDestroy(mOwner);
-        reset(obj);
+    fun eachEvent() {
+        val obj = mock(DefaultLifecycleObserver::class.java)
+        val observer = DefaultLifecycleObserverAdapter(obj, null)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.CREATED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_CREATE)
+        val inOrder = inOrder(obj)
+        inOrder.verify(obj).onCreate(owner)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.STARTED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_START)
+        inOrder.verify(obj).onStart(owner)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.RESUMED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_RESUME)
+        inOrder.verify(obj).onResume(owner)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.STARTED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_PAUSE)
+        inOrder.verify(obj).onPause(owner)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.CREATED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_STOP)
+        inOrder.verify(obj).onStop(owner)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.INITIALIZED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_DESTROY)
+        inOrder.verify(obj).onDestroy(owner)
+        reset(obj)
     }
 
     @Test
-    public void defaultLifecycleObserverAndLifecycleEventObserver() {
-        class AllObservers implements DefaultLifecycleObserver, LifecycleEventObserver {
-
-            @Override
-            public void onCreate(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onStart(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onResume(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onPause(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onStop(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onDestroy(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onStateChanged(@NonNull LifecycleOwner source,
-                    @NonNull Lifecycle.Event event) {
-
+    fun defaultLifecycleObserverAndLifecycleEventObserver() {
+        open class AllObservers : DefaultLifecycleObserver, LifecycleEventObserver {
+            override fun onCreate(owner: LifecycleOwner) {}
+            override fun onStart(owner: LifecycleOwner) {}
+            override fun onResume(owner: LifecycleOwner) {}
+            override fun onPause(owner: LifecycleOwner) {}
+            override fun onStop(owner: LifecycleOwner) {}
+            override fun onDestroy(owner: LifecycleOwner) {}
+            override fun onStateChanged(
+                source: LifecycleOwner,
+                event: Lifecycle.Event
+            ) {
             }
         }
 
-        AllObservers obj = mock(AllObservers.class);
-        DefaultLifecycleObserverAdapter observer = new DefaultLifecycleObserverAdapter(obj, obj);
-        when(mLifecycle.getCurrentState()).thenReturn(CREATED);
-
-        observer.onStateChanged(mOwner, ON_CREATE);
-        InOrder inOrder = Mockito.inOrder(obj);
-        inOrder.verify(obj).onCreate(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_CREATE);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(STARTED);
-        observer.onStateChanged(mOwner, ON_START);
-        inOrder.verify(obj).onStart(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_START);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(RESUMED);
-        observer.onStateChanged(mOwner, ON_RESUME);
-        inOrder.verify(obj).onResume(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_RESUME);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(STARTED);
-        observer.onStateChanged(mOwner, ON_PAUSE);
-        inOrder.verify(obj).onPause(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_PAUSE);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(CREATED);
-        observer.onStateChanged(mOwner, ON_STOP);
-        inOrder.verify(obj).onStop(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_STOP);
-        reset(obj);
-
-        when(mLifecycle.getCurrentState()).thenReturn(INITIALIZED);
-        observer.onStateChanged(mOwner, ON_DESTROY);
-        inOrder.verify(obj).onDestroy(mOwner);
-        inOrder.verify(obj).onStateChanged(mOwner, ON_DESTROY);
-        reset(obj);
+        val obj = mock(AllObservers::class.java)
+        val observer = DefaultLifecycleObserverAdapter(obj, obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.CREATED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_CREATE)
+        val inOrder = inOrder(obj)
+        inOrder.verify(obj).onCreate(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_CREATE)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.STARTED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_START)
+        inOrder.verify(obj).onStart(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_START)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.RESUMED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_RESUME)
+        inOrder.verify(obj).onResume(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_RESUME)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.STARTED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_PAUSE)
+        inOrder.verify(obj).onPause(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_PAUSE)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.CREATED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_STOP)
+        inOrder.verify(obj).onStop(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_STOP)
+        reset(obj)
+        `when`(lifecycle.currentState).thenReturn(Lifecycle.State.INITIALIZED)
+        observer.onStateChanged(owner, Lifecycle.Event.ON_DESTROY)
+        inOrder.verify(obj).onDestroy(owner)
+        inOrder.verify(obj).onStateChanged(owner, Lifecycle.Event.ON_DESTROY)
+        reset(obj)
     }
 
-    public void fullLifecycleObserverAndAnnotations() {
-        @SuppressWarnings("deprecation")
-        class AnnotatedFullLifecycleObserver implements DefaultLifecycleObserver {
-            @OnLifecycleEvent(ON_ANY)
-            public void onAny() {
-                throw new IllegalStateException("Annotations in FullLifecycleObserver "
-                        + "must not be called");
+    fun fullLifecycleObserverAndAnnotations() {
+        @Suppress("deprecation")
+        class AnnotatedFullLifecycleObserver : DefaultLifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAny() {
+                throw IllegalStateException(
+                    "Annotations in FullLifecycleObserver must not be called"
+                )
             }
 
-            @Override
-            public void onCreate(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onStart(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onResume(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onPause(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onStop(@NonNull LifecycleOwner owner) {
-
-            }
-
-            @Override
-            public void onDestroy(@NonNull LifecycleOwner owner) {
-
-            }
+            override fun onCreate(owner: LifecycleOwner) {}
+            override fun onStart(owner: LifecycleOwner) {}
+            override fun onResume(owner: LifecycleOwner) {}
+            override fun onPause(owner: LifecycleOwner) {}
+            override fun onStop(owner: LifecycleOwner) {}
+            override fun onDestroy(owner: LifecycleOwner) {}
         }
     }
 }
