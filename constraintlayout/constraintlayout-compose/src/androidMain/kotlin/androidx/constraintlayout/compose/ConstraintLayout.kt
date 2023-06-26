3bfe631945311b92e3909ce65375fabfe7f28a83
Oscar Adame Vázquez <oscarad@google.com>
2023-06-26 11:23:21 -0700

    Update ConstraintLayout documentation
    Elaborates on usage with examples.
    
    For both the version with the inline Modifier DSL and the ConstraintSet
    DSL.
    
    The ConstraintSet DSL is thororughly documented on the ConstraintSet
    interface, since that's where kotlin resolves to in the IDE for KDoc
    (Javadoc equivalent in Kotlin). And it doesn't allow for referencing
    specific overloads of the same reference.
    
    The KDoc in the ConstraintLayout Composable that takes ConstraintSet
    instead focuses on good practices.
    
    Bug: n/a
    Test: n/a
    Change-Id: I496c99cf1fceb9efd7c091a4592cb4a05bcbfb8e

--- constraintlayout/constraintlayout-compose/src/androidMain/kotlin/androidx/constraintlayout/compose/ConstraintLayout.kt
+++ constraintlayout/constraintlayout-compose/src/androidMain/kotlin/androidx/constraintlayout/compose/ConstraintLayout.kt
@@ -112,6 +112,247 @@ import org.intellij.lang.annotations.Language
 
 /**
  * Layout that positions its children according to the constraints between them.
+ *
+ * Constraints are defined within the content of this ConstraintLayout [Composable].
+ *
+ * Items in the layout that are to be constrained are initialized with
+ * [ConstraintLayoutScope.createRef]:
+ *
+ * ```
+ * val textRef = createRef()
+ * val imageRef = createRef()
+ * ```
+ *
+ * You may also use [ConstraintLayoutScope.createRefs] to declare up to 16 items using the
+ * destructuring declaration pattern:
+ *
+ * ```
+ * val (textRef, imageRef) = createRefs()
+ * ```
+ *
+ * Individual constraints are defined with [Modifier.constrainAs][ConstraintLayoutScope.constrainAs],
+ * this will also bind the Composable to the given [ConstrainedLayoutReference].
+ *
+ * So, a simple layout with a text in the middle and an image next to it may be declared like this
+ * (keep in mind, when using `center...`, `start` or `end` the layout direction will automatically
+ * change in RTL locales):
+ * ```
+ * ConstraintLayout(Modifier.fillMaxSize()) {
+ *     val (textRef, imageRef) = createRefs()
+ *     Text(
+ *         modifier = Modifier.constrainAs(textRef) {
+ *             centerTo(parent)
+ *         },
+ *         text = "Hello, World!"
+ *     )
+ *     Image(
+ *         modifier = Modifier.constrainAs(imageRef) {
+ *             centerVerticallyTo(textRef)
+ *             start.linkTo(textRef.end, margin = 8.dp)
+ *         },
+ *         imageVector = Icons.Default.Android,
+ *         contentDescription = null
+ *     )
+ * }
+ * ```
+ * See [ConstrainScope] to learn more about how to constrain elements together.
+ *
+ * &nbsp;
+ *
+ * ## Helpers
+ * You may also use helpers, a set of virtual (not shown on screen) components that provide special
+ * layout behaviors, you may find these in the [ConstraintLayoutScope] with the '`create...`' prefix,
+ * a few of these are **Guidelines**, **Chains** and **Barriers**.
+ *
+ * &nbsp;
+ *
+ * ### Guidelines
+ * Lines to which other [ConstrainedLayoutReference]s may be constrained to, these are defined at
+ * either a fixed or percent position from an anchor of the ConstraintLayout parent (top, bottom,
+ * start, end, absoluteLeft, absoluteRight).
+ *
+ * &nbsp;
+ *
+ * Example:
+ * ```
+ * val (textRef) = createRefs()
+ * val vG = createGuidelineFromStart(fraction = 0.3f)
+ * Text(
+ *     modifier = Modifier.constrainAs(textRef) {
+ *         centerVerticallyTo(parent)
+ *         centerAround(vG)
+ *     },
+ *     text = "Hello, World!"
+ * )
+ * ```
+ *
+ * See
+ * - [ConstraintLayoutScope.createGuidelineFromTop]
+ * - [ConstraintLayoutScope.createGuidelineFromBottom]
+ * - [ConstraintLayoutScope.createGuidelineFromStart]
+ * - [ConstraintLayoutScope.createGuidelineFromEnd]
+ * - [ConstraintLayoutScope.createGuidelineFromAbsoluteLeft]
+ * - [ConstraintLayoutScope.createGuidelineFromAbsoluteRight]
+ *
+ * ### Chains
+ * Chains may be either horizontal or vertical, these, take a set of [ConstrainedLayoutReference]s
+ * and create bi-directional constraints on each of them at the same orientation of the chain in the
+ * given order, meaning that an horizontal chain will create constraints between the start and end anchors.
+ *
+ * The result, a layout that evenly distributes the space within its elements.
+ *
+ * &nbsp;
+ *
+ * For example, to make a layout with three text elements distributed so that the spacing between
+ * them (and around them) is equal:
+ * ```
+ * val (textRef0, textRef1, textRef2) = createRefs()
+ * createHorizontalChain(textRef0, textRef1, textRef2, chainStyle = ChainStyle.Spread)
+ *
+ * Text(modifier = Modifier.constrainAs(textRef0) {}, text = "Hello")
+ * Text(modifier = Modifier.constrainAs(textRef1) {}, text = "Foo")
+ * Text(modifier = Modifier.constrainAs(textRef2) {}, text = "Bar")
+ * ```
+ *
+ * You may set margins within elements in a chain with [ConstraintLayoutScope.withChainParams]:
+ *
+ * ```
+ * val (textRef0, textRef1, textRef2) = createRefs()
+ * createHorizontalChain(
+ *     textRef0,
+ *     textRef1.withChainParams(startMargin = 100.dp, endMargin = 100.dp),
+ *     textRef2,
+ *     chainStyle = ChainStyle.Spread
+ * )
+ *
+ * Text(modifier = Modifier.constrainAs(textRef0) {}, text = "Hello")
+ * Text(modifier = Modifier.constrainAs(textRef1) {}, text = "Foo")
+ * Text(modifier = Modifier.constrainAs(textRef2) {}, text = "Bar")
+ * ```
+ *
+ * You can also change the way space is distributed, as chains have three different styles:
+ * - [ChainStyle.Spread]  Layouts are evenly distributed after margins are accounted for (the space
+ * around and between each item is even). This is the **default** style for chains.
+ * - [ChainStyle.SpreadInside]  The first and last layouts are affixed to each end of the chain,
+ * and the rest of the items are evenly distributed (after margins are accounted for).
+ * I.e.: Items are spread from the inside, distributing the space between them with no space around
+ * the first and last items.
+ * - [ChainStyle.Packed] The layouts are packed together after margins are accounted for, by
+ * default, they're packed together at the middle, you can change this behavior with the **bias**
+ * parameter of [ChainStyle.Packed].
+ * - Alternatively, you can make every Layout in the chain to be [Dimension.fillToConstraints] and
+ * then set a particular weight to each of them to create a **weighted chain**.
+ *
+ * #### Weighted Chain
+ * Weighted chains are useful when you want the size of the elements to depend on the remaining size
+ * of the chain. As opposed to just distributing the space around and/or in-between the items.
+ *
+ * &nbsp;
+ *
+ * For example, to create a layout with three text elements in a row where each element takes the
+ * exact same size regardless of content, you can use a simple weighted chain where each item has the
+ * same weight:
+ *
+ * ```
+ * val (textRef0, textRef1, textRef2) = createRefs()
+ * createHorizontalChain(
+ *     textRef0.withChainParams(weight = 1f),
+ *     textRef1.withChainParams(weight = 1f),
+ *     textRef2.withChainParams(weight = 1f),
+ *     chainStyle = ChainStyle.Spread
+ * )
+ *
+ * Text(modifier = Modifier.background(Color.Cyan).constrainAs(textRef0) {
+ *     width = Dimension.fillToConstraints
+ * }, text = "Hello, World!")
+ * Text(modifier = Modifier.background(Color.Red).constrainAs(textRef1) {
+ *     width = Dimension.fillToConstraints
+ * }, text = "Foo")
+ * Text(modifier = Modifier.background(Color.Cyan).constrainAs(textRef2) {
+ *     width = Dimension.fillToConstraints
+ * }, text = "This text is six words long")
+ * ```
+ *
+ * This way, the texts will horizontally occupy the same space even if one of them is significantly
+ * larger than the others.
+ *
+ * &nbsp;
+ *
+ * Keep in mind that chains have a relatively high performance cost. For example, if you plan on
+ * having multiple chains one below the other, consider instead, applying just one chain and using
+ * it as a reference to constrain all other elements to the ones that match their position in that
+ * one chain. It may provide increased performance with no significant changes in the layout output.
+ *
+ * Alternatively, consider if other helpers such as [ConstraintLayoutScope.createGrid] can
+ * accomplish the same layout.
+ *
+ * &nbsp;
+ *
+ * See
+ * - [ConstraintLayoutScope.createHorizontalChain]
+ * - [ConstraintLayoutScope.createVerticalChain]
+ * - [ConstraintLayoutScope.withChainParams]
+ *
+ * ### Barriers
+ * Barriers take a set of [ConstrainedLayoutReference]s and creates the most further point in a
+ * given direction where other [ConstrainedLayoutReference] can constrain to.
+ *
+ * &nbsp;
+ *
+ * This is useful in situations where elements in a layout may have different sizes but you want to
+ * always constrain to the largest item, for example, if you have a text element on top of another
+ * and want an image to always be constrained to the end of them:
+ *
+ * ```
+ * val (textRef0, textRef1, imageRef) = createRefs()
+ *
+ * // Creates a point at the furthest end anchor from the elements in the barrier
+ * val endTextsBarrier = createEndBarrier(textRef0, textRef1)
+ *
+ * Text(
+ *     modifier = Modifier.constrainAs(textRef0) {
+ *         centerTo(parent)
+ *     },
+ *     text = "Hello, World!"
+ * )
+ * Text(
+ *     modifier = Modifier.constrainAs(textRef1) {
+ *         top.linkTo(textRef0.bottom)
+ *         start.linkTo(textRef0.start)
+ *     },
+ *     text = "Foo Bar"
+ * )
+ * Image(
+ *     modifier = Modifier.constrainAs(imageRef) {
+ *         top.linkTo(textRef0.top)
+ *         bottom.linkTo(textRef1.bottom)
+ *
+ *         // Image will always be at the end of both texts, regardless of their size
+ *         start.linkTo(endTextsBarrier, margin = 8.dp)
+ *     },
+ *     imageVector = Icons.Default.Android,
+ *     contentDescription = null
+ * )
+ * ```
+ *
+ * Be careful not to constrain a [ConstrainedLayoutReference] to a barrier that references it or
+ * that depends on it indirectly. This creates a cyclic dependency that results in unsupported
+ * layout behavior.
+ *
+ * See
+ * - [ConstraintLayoutScope.createTopBarrier]
+ * - [ConstraintLayoutScope.createBottomBarrier]
+ * - [ConstraintLayoutScope.createStartBarrier]
+ * - [ConstraintLayoutScope.createEndBarrier]
+ * - [ConstraintLayoutScope.createAbsoluteLeftBarrier]
+ * - [ConstraintLayoutScope.createAbsoluteRightBarrier]
+ *
+ * &nbsp;
+ *
+ * **Tip**: If you notice that you are creating many different constraints based on [State][androidx.compose.runtime.State]
+ * variables or configuration changes, consider using the [ConstraintSet] pattern instead, makes it
+ * clearer to distinguish different layouts and allows you to automatically animate the layout when
+ * the provided [ConstraintSet] is different.
  */
 @Composable
 inline fun ConstraintLayout(
@@ -238,10 +479,190 @@ internal class ConstraintSetForInlineDsl(
 /**
  * Layout that positions its children according to the constraints between them.
  *
- * When recomposed with different [constraintSet], you can use the [animateChanges] parameter
- * to animate the layout changes ([animationSpec] and [finishedAnimationListener] attributes can
- * also be useful in this mode). This is only intended for basic transitions, if more control
- * is needed, we recommend using [MotionLayout] instead.
+ * This [Composable] of [ConstraintLayout] takes a [ConstraintSet] where the layout is defined using
+ * references and constraints.
+ *
+ * Layouts referenced in the given [constraintSet] can be bound to immediate child Composables
+ * using [Modifier.layoutId], where the given layoutIds match each named reference.
+ *
+ * &nbsp;
+ *
+ * So, a simple layout with a text in the middle and an image next to it may be declared like this:
+ *
+ * ```
+ * // IDs
+ * val textId = "text"
+ * val imageId = "image"
+ *
+ * // Layout definition with references and constraints
+ * val constraintSet = remember {
+ *     ConstraintSet {
+ *         val (textRef, imageRef) = createRefsFor(textId, imageId)
+ *         constrain(textRef) {
+ *             centerTo(parent)
+ *         }
+ *         constrain(imageRef) {
+ *             centerVerticallyTo(textRef)
+ *             start.linkTo(textRef.end, margin = 8.dp)
+ *         }
+ *     }
+ * }
+ *
+ * // ConstraintLayout uses our given ConstraintSet
+ * ConstraintLayout(
+ *     constraintSet = constraintSet,
+ *     modifier = Modifier.fillMaxSize()
+ * ) {
+ *     // References are bound to Composables using Modifier.layoutId(Any)
+ *     Text(
+ *         modifier = Modifier.layoutId(textId),
+ *         text = "Hello, World!"
+ *     )
+ *     Image(
+ *         modifier = Modifier.layoutId(imageId),
+ *         imageVector = Icons.Default.Android,
+ *         contentDescription = null
+ *     )
+ * }
+ * ```
+ * See [ConstraintSet] to learn more on how to declare layouts using constraints.
+ *
+ * &nbsp;
+ *
+ * ### Handling of ConstraintSet objects
+ *
+ * You typically want to *`remember`* declared [ConstraintSet]s, to avoid unnecessary allocations on
+ * recomposition, if the [ConstraintSetScope] block consumes any [State][androidx.compose.runtime.State]
+ * variables, then something like *`remember { derivedStateOf { ConstraintSet { ... } } }`* would be
+ * more appropriate.
+ *
+ * &nbsp;
+ *
+ * However, note in the example above that our ConstraintSet is constant, so we can declare it at a
+ * top level, improving overall Composition performance:
+ *
+ * ```
+ * private const val TEXT_ID = "text"
+ * private const val IMAGE_ID = "image"
+ * private val mConstraintSet by lazy(LazyThreadSafetyMode.NONE) {
+ *     ConstraintSet {
+ *         val (textRef, imageRef) = createRefsFor(TEXT_ID, IMAGE_ID)
+ *         constrain(textRef) {
+ *             centerTo(parent)
+ *         }
+ *         constrain(imageRef) {
+ *             centerVerticallyTo(textRef)
+ *             start.linkTo(textRef.end, margin = 8.dp)
+ *         }
+ *     }
+ * }
+ *
+ * @Preview
+ * @Composable
+ * fun ConstraintSetExample() {
+ *     ConstraintLayout(
+ *         constraintSet = mConstraintSet,
+ *         modifier = Modifier.fillMaxSize()
+ *     ) {
+ *         Text(
+ *             modifier = Modifier.layoutId(TEXT_ID),
+ *             text = "Hello, World!"
+ *         )
+ *         Image(
+ *             modifier = Modifier.layoutId(IMAGE_ID),
+ *             imageVector = Icons.Default.Android,
+ *             contentDescription = null
+ *         )
+ *     }
+ * }
+ * ```
+ *
+ * This pattern (as opposed to defining constraints with [ConstraintLayoutScope.constrainAs]) is
+ * preferred when you want different layouts to be produced on different [State][androidx.compose.runtime.State]
+ * variables or configuration changes. As it makes it easier to create distinguishable layouts, for
+ * example when building adaptive layouts based on Window size class:
+ *
+ * ```
+ * private const val NAV_BAR_ID = "navBar"
+ * private const val CONTENT_ID = "content"
+ *
+ * private val compactConstraintSet by lazy(LazyThreadSafetyMode.NONE) {
+ *     ConstraintSet {
+ *         val (navBarRef, contentRef) = createRefsFor(NAV_BAR_ID, CONTENT_ID)
+ *
+ *         // Navigation bar at the bottom for Compact devices
+ *         constrain(navBarRef) {
+ *             width = Dimension.percent(1f)
+ *             height = 40.dp.asDimension()
+ *             bottom.linkTo(parent.bottom)
+ *         }
+ *
+ *         constrain(contentRef) {
+ *             width = Dimension.percent(1f)
+ *             height = Dimension.fillToConstraints
+ *
+ *             top.linkTo(parent.top)
+ *             bottom.linkTo(navBarRef.top)
+ *         }
+ *     }
+ * }
+ *
+ * private val mediumConstraintSet by lazy(LazyThreadSafetyMode.NONE) {
+ *     ConstraintSet {
+ *         val (navBarRef, contentRef) = createRefsFor(NAV_BAR_ID, CONTENT_ID)
+ *
+ *         // Navigation bar at the start on Medium class devices
+ *         constrain(navBarRef) {
+ *             width = 40.dp.asDimension()
+ *             height = Dimension.percent(1f)
+ *
+ *             start.linkTo(parent.start)
+ *         }
+ *
+ *         constrain(contentRef) {
+ *             width = Dimension.fillToConstraints
+ *             height = Dimension.percent(1f)
+ *
+ *             start.linkTo(navBarRef.end)
+ *             end.linkTo(parent.end)
+ *         }
+ *     }
+ * }
+ *
+ * @Composable
+ * fun MyAdaptiveLayout(
+ *     windowWidthSizeClass: WindowWidthSizeClass
+ * ) {
+ *     val constraintSet = if (windowWidthSizeClass == WindowWidthSizeClass.Compact) {
+ *         compactConstraintSet
+ *     }
+ *     else {
+ *         mediumConstraintSet
+ *     }
+ *     ConstraintLayout(
+ *         constraintSet = constraintSet,
+ *         modifier = Modifier.fillMaxSize()
+ *     ) {
+ *         Box(Modifier.background(Color.Blue).layoutId(NAV_BAR_ID))
+ *         Box(Modifier.background(Color.Red).layoutId(CONTENT_ID))
+ *     }
+ * }
+ * ```
+ *
+ * ### Animate Changes
+ *
+ * At this point, you may also use the [animateChanges] flag to animate the layout changes. This is
+ * triggered whenever a different (by equality) [constraintSet] is provided on recomposition. And,
+ * is driven by [animationSpec], [finishedAnimationListener] is called whenever a layout animation
+ * ends.
+ *
+ * On the example above, using [animateChanges] would result on the layout being animated when the
+ * device changes to non-compact window class, typical behavior in some Foldable devices.
+ *
+ * &nbsp;
+ *
+ * If more control is needed, we recommend using [MotionLayout] instead, which has a very similar
+ * pattern through the [MotionScene] object.
  */
 @OptIn(ExperimentalMotionApi::class)
 @Suppress("NOTHING_TO_INLINE")
@@ -1018,13 +1439,17 @@ fun ConstraintSet(
     JSONConstraintSet(content = jsonContent, extendFrom = extendConstraintSet)
 
 /**
- * Creates a [ConstraintSet].
+ * Creates a [ConstraintSet] with the constraints defined in the [description] block.
+ *
+ * See [ConstraintSet] to learn how to define constraints.
  */
 fun ConstraintSet(description: ConstraintSetScope.() -> Unit): ConstraintSet =
     DslConstraintSet(description)
 
 /**
  * Creates a [ConstraintSet] that extends the changes applied by [extendConstraintSet].
+ *
+ * See [ConstraintSet] to learn how to define constraints.
  */
 fun ConstraintSet(
     extendConstraintSet: ConstraintSet,
