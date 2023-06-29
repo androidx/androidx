57e51bf96d8a5751cf62a3deb4125d29fc78405a
Louis Pullen-Freilich <lpf@google.com>
2023-06-29 16:30:49 +0100

    Removes androidx.compose.multiplatformEnabled flag
    This is no longer needed since we migrated to AndroidX KMP setup. Also enables compose:desktop* modules by default, these can still be removed with androidx.enabled.kmp.target.platforms as for any other KMP target.
    
    Test: n/a
    Change-Id: Ifc46a819c7add1431feed229965d6e0f56fb4f2e

--- buildSrc/public/src/main/kotlin/androidx/build/KmpPlatforms.kt
+++ buildSrc/public/src/main/kotlin/androidx/build/KmpPlatforms.kt
@@ -113,6 +113,17 @@ private val Project.enabledKmpPlatforms: Set<PlatformGroup>
     }
 
 /**
+ * Returns true if kotlin native targets should be enabled.
+ */
+private fun Project.isKotlinNativeEnabled(): Boolean {
+    return "KMP".equals(System.getenv()["ANDROIDX_PROJECTS"], ignoreCase = true) ||
+        "INFRAROGUE".equals(System.getenv()["ANDROIDX_PROJECTS"], ignoreCase = true) ||
+        ProjectLayoutType.isPlayground(project) ||
+        project.providers.gradleProperty("androidx.kmp.native.enabled")
+            .orNull?.toBoolean() == true
+}
+
+/**
  * Extension used to store parsed KMP configuration information.
  */
 private open class KmpPlatformsExtension(project: Project) {
@@ -123,9 +134,9 @@ private open class KmpPlatformsExtension(project: Project) {
 
 fun Project.enableJs(): Boolean = enabledKmpPlatforms.contains(PlatformGroup.JS)
 fun Project.enableMac(): Boolean =
-    enabledKmpPlatforms.contains(PlatformGroup.MAC) || Multiplatform.isKotlinNativeEnabled(this)
+    enabledKmpPlatforms.contains(PlatformGroup.MAC) || isKotlinNativeEnabled()
 fun Project.enableLinux(): Boolean =
-    enabledKmpPlatforms.contains(PlatformGroup.LINUX) || Multiplatform.isKotlinNativeEnabled(this)
+    enabledKmpPlatforms.contains(PlatformGroup.LINUX) || isKotlinNativeEnabled()
 fun Project.enableJvm(): Boolean = enabledKmpPlatforms.contains(PlatformGroup.JVM)
 fun Project.enableDesktop(): Boolean = enabledKmpPlatforms.contains(PlatformGroup.DESKTOP)
 fun Project.enableNative(): Boolean = enableMac() && enableLinux()
