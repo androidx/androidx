240287b05fd58a85ab14d3c5c4f1f19e86194852
Igor Demin <igordmn@users.noreply.github.com>
2023-07-13 16:36:51 +0200

    1.5.0-beta03 merge. Fix CoreText.kt (#664)
    - Apply changes from jb-main between the base commit 6022301db806601f282c53b8cbb5a981923a1589 and the pre-merge commit 4ac3bda449d4606e5190208d074654ba041d2537:
    
      - 37564c1f (this commit was a squash, and its message is a mess)
    
    The `pointerInput(Unit)` is restarted every recomposition, because we create `mouseSelectionObserver` every time.
    
    It can cause selection issues. We can accidentaly not have issues (they depend on how `mouseDragGestureDetector` is used inside TextField), but it can return in the future, so better to write these modifiers in a proper way.

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/modifiers/SelectionController.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/modifiers/SelectionController.kt
@@ -29,7 +29,10 @@ import androidx.compose.foundation.text.selection.mouseSelectionDetector
 import androidx.compose.foundation.text.textPointerHoverIcon
 import androidx.compose.foundation.text.textPointerIcon
 import androidx.compose.runtime.RememberObserver
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -341,8 +344,12 @@ private fun SelectionRegistrar.makeSelectionModifier(
                 return true
             }
         }
-        Modifier.pointerInput(mouseSelectionObserver) {
-            mouseSelectionDetector(mouseSelectionObserver)
+        Modifier.composed {
+            // TODO(https://youtrack.jetbrains.com/issue/COMPOSE-79) how we can rewrite this without `composed`?
+            val currentMouseSelectionObserver by rememberUpdatedState(mouseSelectionObserver)
+            pointerInput(Unit) {
+                mouseSelectionDetector(currentMouseSelectionObserver)
+            }
         }.pointerHoverIcon(textPointerIcon)
     }
 }
