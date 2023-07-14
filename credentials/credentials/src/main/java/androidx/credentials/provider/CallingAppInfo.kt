b5bbcbe0fd40cb165b7421ef294a2b2e3aa4b72f
Reema Bajwa <reemabajwa@google.com>
2023-07-14 21:30:22 +0000

    Match calling app signatures with allowlist to return origin
    Test: unit tests
    Bug: 289402940
    
    Change-Id: I247f77ea746e500583fcc54ee0a60086c72da09b

--- credentials/credentials/src/main/java/androidx/credentials/provider/CallingAppInfo.kt
+++ credentials/credentials/src/main/java/androidx/credentials/provider/CallingAppInfo.kt
@@ -16,9 +16,16 @@
 
 package androidx.credentials.provider
 
+import android.content.pm.Signature
 import android.content.pm.SigningInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.annotation.RestrictTo
+import androidx.credentials.provider.utils.PrivilegedApp
 import androidx.credentials.provider.utils.RequestValidationUtil
+import java.security.MessageDigest
+import org.json.JSONException
+import org.json.JSONObject
 
 /**
  * Information pertaining to the calling application.
@@ -42,6 +49,9 @@ class CallingAppInfo @JvmOverloads constructor(
     @get:RestrictTo(RestrictTo.Scope.LIBRARY)
     val origin: String? = null
 ) {
+    internal companion object {
+        private const val TAG = "CallingAppInfo"
+    }
 
     /**
      * Returns the origin of the calling app. This is only non-null if a
@@ -51,21 +61,117 @@ class CallingAppInfo @JvmOverloads constructor(
      * Additionally, in order to get the origin, the credential provider must
      * provide an allowlist of privileged browsers/apps that it trusts.
      * This allowlist must be in the form of a valid, non-empty JSON. The
-     * origin will only be returned if the [packageName] and [signingInfo]
-     * match with an app allowlisted in [privilegedAllowlist].
+     * origin will only be returned if the [packageName] and the fingerprints of certificates
+     * obtained from the [signingInfo] match with that of an app allowlisted
+     * in [privilegedAllowlist]. The format of this JSON must adhere to the following sample.
+     *
+     * {"apps": [
+     *    {
+     *       "type": "android",
+     *       "info": {
+     *          "package_name": "com.example.myapp",
+     *          "signatures" : [
+     *          {"build": "release",
+     *              "cert_fingerprint_sha256": "59:0D:2D:7B:33:6A:BD:FB:54:CD:3D:8B:36:8C:5C:3A:
+     *              7D:22:67:5A:9A:85:9A:6A:65:47:FD:4C:8A:7C:30:32"
+     *          },
+     *          {"build": "userdebug",
+     *          "cert_fingerprint_sha256": "59:0D:2D:7B:33:6A:BD:FB:54:CD:3D:8B:36:8C:5C:3A:7D:
+     *          22:67:5A:9A:85:9A:6A:65:47:FD:4C:8A:7C:30:32"
+     *          }]
+     *       }
+     *     }
+     * ]}
+     *
+     * All keys in the JSON must be exactly as stated in the sample above. Note that if the build
+     * for a given fingerprint is specified as 'userdebug', that fingerprint will
+     * only be considered if the device is on a 'userdebug' build, as determined by [Build.TYPE].
      *
      * @throws IllegalArgumentException If [privilegedAllowlist] is empty, or an
-     * invalid JSON
+     * invalid JSON, or does not follow the format detailed above
+     * @throws IllegalStateException If the origin is non-null, but the [packageName] and
+     * [signingInfo] do not have a match in the [privilegedAllowlist]
      */
     fun getOrigin(privilegedAllowlist: String): String? {
         if (!RequestValidationUtil.isValidJSON(privilegedAllowlist)) {
-            throw IllegalArgumentException("privilegedAllowlist must not be " +
-                "empty, and must be a valid JSON")
+            throw IllegalArgumentException(
+                "privilegedAllowlist must not be " +
+                    "empty, and must be a valid JSON"
+            )
         }
-        return null
+        if (origin == null) {
+            // If origin is null, then this is not a privileged call
+            return origin
+        }
+        try {
+            if (isAppPrivileged(
+                    PrivilegedApp.extractPrivilegedApps(
+                        JSONObject(privilegedAllowlist)
+                    )
+                )
+            ) {
+                return origin
+            }
+        } catch (_: JSONException) {
+            throw IllegalArgumentException("privilegedAllowlist must be formatted properly")
+        }
+        throw IllegalStateException("Origin is not being returned as the calling app did not" +
+            "match the privileged allowlist")
+    }
+
+    private fun isAppPrivileged(
+        candidateApps: List<PrivilegedApp>
+    ): Boolean {
+        for (app in candidateApps) {
+            if (app.packageName == packageName &&
+                !app.fingerprints.intersect(getSignatureFingerprints(signingInfo)).isEmpty()
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun getSignatureFingerprints(signingInfo: SigningInfo): Set<String> {
+        val fingerprints = mutableSetOf<String>()
+        if (Build.VERSION.SDK_INT >= 28) {
+            return SignatureParserApi28(signingInfo).getSignatureFingerprints()
+        } else {
+            // TODO("Extend to <= 28 if needed")
+        }
+        return fingerprints
     }
 
     init {
         require(packageName.isNotEmpty()) { "packageName must not be empty" }
+    }
+
+    @RequiresApi(28)
+    private class SignatureParserApi28(private val signingInfo: SigningInfo) {
+        fun getSignatureFingerprints(): Set<String> {
+            val fingerprints = mutableSetOf<String>()
+            if (signingInfo.hasMultipleSigners()) {
+                val signatures = signingInfo.apkContentsSigners
+                if (signatures != null) {
+                    fingerprints.addAll(convertToFingerprints(signatures))
+                }
+            } else {
+                val signatures = signingInfo.signingCertificateHistory
+                if (signatures != null) {
+                    fingerprints.addAll(convertToFingerprints(signatures))
+                }
+            }
+            return fingerprints
+        }
+
+        private fun convertToFingerprints(signatures: Array<Signature>): Set<String> {
+            val fingerprints = mutableSetOf<String>()
+            for (signature in signatures) {
+                val md = MessageDigest.getInstance("SHA-256")
+                val digest = md.digest(signature.toByteArray())
+                fingerprints.add(digest.joinToString(":") { "%02X".format(it) })
+            }
+            return fingerprints
+        }
     }
 }
