71ebc34cc315cb3c2b703c322c66b857d1c5cd39
Igor Demin <igor.demin@jetbrains.com>
2023-07-17 20:06:33 +0200

    material3. Fix Int.toLocalString

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/Expect.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/Expect.kt
@@ -39,15 +39,10 @@ internal expect fun defaultLocale(): CalendarLocale
  * @param minDigits sets the minimum number of digits allowed in the integer portion of a number.
  * If the minDigits value is greater than the [maxDigits] value, then [maxDigits] will also be set
  * to this value.
- * @param maxDigits sets the maximum number of digits allowed in the integer portion of a number.
- * If this maxDigits value is less than the [minDigits] value, then [minDigits] will also be set to
- * this value.
- * @param isGroupingUsed set whether or not grouping will be used when formatting into a local
- * string. By default, this value is false, which eliminates any use of delimiters when formatting
- * the integer.
+ *
+ *
  */
 internal expect fun Int.toLocalString(
     minDigits: Int = 1,
-    maxDigits: Int = 40,
-    isGroupingUsed: Boolean = false
+    // Removed other arguments comparing with upstream because they aren't used
 ): String
