70d33beb3a99134641419e46f3df39ce046b7416
sanura <sanura@google.com>
2023-07-20 20:12:15 +0000

    Convert DerivedWithNoNewMethods to Kotlin
    Part of the kotlin conversion process for the tests in the Lifecycle library.
    
    Test: all tests still pass
    Bug: 240298180
    Change-Id: Iba09914a17a48a06b6fa75a2875f52a90e3aa353

--- lifecycle/lifecycle-common/src/test/java/androidx/lifecycle/observers/DerivedWithNoNewMethods.kt
+++ lifecycle/lifecycle-common/src/test/java/androidx/lifecycle/observers/DerivedWithNoNewMethods.kt
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.lifecycle.observers
 
-package androidx.lifecycle.observers;
-
-public class DerivedWithNoNewMethods extends Base {
-}
+class DerivedWithNoNewMethods : Base()
