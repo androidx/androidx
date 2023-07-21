48eb6b8ea9a1b1da1258231f232396842d028ba0
Bishoy Gendy <bishoygendy@google.com>
2023-07-21 09:03:20 +0000

    Remove unused method and fields in global media router
    Bug: 289034805
    Test: Building and running sample app as it's a non functional change
    Change-Id: Ie4d9cf5348755b0eb4b1ac14675d32c595272f26

--- mediarouter/mediarouter/src/main/java/androidx/mediarouter/media/MediaRouter.java
+++ mediarouter/mediarouter/src/main/java/androidx/mediarouter/media/MediaRouter.java
@@ -27,7 +27,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Build;
@@ -2641,9 +2640,6 @@ public final class MediaRouter {
         private int mCallbackCount;
         OnPrepareTransferListener mOnPrepareTransferListener;
         PrepareTransferNotifier mTransferNotifier;
-        RouteInfo mTransferredRoute;
-        RouteController mTransferredRouteController;
-
         private MediaSessionRecord mMediaSession;
         MediaSessionCompat mRccMediaSession;
         private MediaSessionCompat mCompatSession;
@@ -2754,18 +2750,6 @@ public final class MediaRouter {
 
         public ContentResolver getContentResolver() {
             return mApplicationContext.getContentResolver();
-        }
-
-        public Context getProviderContext(String packageName) {
-            if (packageName.equals(SystemMediaRouteProvider.PACKAGE_NAME)) {
-                return mApplicationContext;
-            }
-            try {
-                return mApplicationContext.createPackageContext(
-                        packageName, Context.CONTEXT_RESTRICTED);
-            } catch (NameNotFoundException ex) {
-                return null;
-            }
         }
 
         public Display getDisplay(int displayId) {
