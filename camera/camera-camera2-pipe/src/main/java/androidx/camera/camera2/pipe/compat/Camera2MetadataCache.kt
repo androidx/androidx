f03df35a98250474e78b67fa90f7d17411d975f5
Catherine Sun <catsun@google.com>
2023-07-27 23:56:36 +0000

    Add a quirk inside CameraPipe to ensure that CameraExtensionCharacteristics have been loaded before the CameraExtensionSession call is executed
    An Android Framework issue has been discovered where if an application does not query for CameraExtensionCharacteristics before trying to create a CameraExtensionSession, the session will fail to create.
    
    This is because Camera2 has a global variable recording if advanced extensions are supported or not, and the variable is updated the first time CameraExtensionCharacteristics are queried. If the variable is not set, Camera2 will fall back to basic extensions, even if they are not supported.
    
    Bug: 293473614
    
    Test: ./gradlew\
     :camera:camera-camera2-pipe:testDebugUnitTest\
     :camera:camera-camera2-pipe-testing:testDebugUnitTest\
     :camera:camera-camera2-pipe-integration:testDebugUnitTest
    Change-Id: I3d6acd0baeaae81f405933c45bd88f0123c3e1d0

--- camera/camera-camera2-pipe/src/main/java/androidx/camera/camera2/pipe/compat/Camera2MetadataCache.kt
+++ camera/camera-camera2-pipe/src/main/java/androidx/camera/camera2/pipe/compat/Camera2MetadataCache.kt
@@ -18,12 +18,14 @@ package androidx.camera.camera2.pipe.compat
 
 import android.content.Context
 import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraExtensionCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.util.ArrayMap
 import androidx.annotation.GuardedBy
 import androidx.annotation.RequiresApi
 import androidx.camera.camera2.pipe.CameraError
+import androidx.camera.camera2.pipe.CameraExtensionMetadata
 import androidx.camera.camera2.pipe.CameraId
 import androidx.camera.camera2.pipe.CameraMetadata
 import androidx.camera.camera2.pipe.CameraPipe
@@ -41,10 +43,11 @@ import javax.inject.Singleton
 import kotlinx.coroutines.withContext
 
 /**
- * Provides caching and querying of [CameraMetadata] via Camera2.
+ * Provides caching and querying of [CameraMetadata] and [CameraExtensionMetadata]
+ * via Camera2.
  *
  * This class is thread safe and provides suspending functions for querying and accessing
- * [CameraMetadata].
+ * [CameraMetadata] and [CameraExtensionMetadata].
  */
 @RequiresApi(21) // TODO(b/200306659): Remove and replace with annotation on package-info.java
 @Singleton
@@ -57,8 +60,15 @@ constructor(
     private val cameraMetadataConfig: CameraPipe.CameraMetadataConfig,
     private val timeSource: TimeSource
 ) : Camera2MetadataProvider {
+
     @GuardedBy("cache")
     private val cache = ArrayMap<String, CameraMetadata>()
+
+    @GuardedBy("extensionCache")
+    private val extensionCache = ArrayMap<String, CameraExtensionMetadata>()
+
+    @GuardedBy("extensionCharacteristicsCache")
+    private val extensionCharacteristicsCache = ArrayMap<String, CameraExtensionCharacteristics>()
 
     override suspend fun getCameraMetadata(cameraId: CameraId): CameraMetadata {
         synchronized(cache) {
@@ -70,6 +80,23 @@ constructor(
 
         // Suspend and query CameraMetadata on a background thread.
         return withContext(threads.backgroundDispatcher) { awaitCameraMetadata(cameraId) }
+    }
+
+    override suspend fun getCameraExtensionMetadata(
+        cameraId: CameraId,
+        extension: Int
+    ): CameraExtensionMetadata {
+        synchronized(extensionCache) {
+            val existing = extensionCache[cameraId.value]
+            if (existing != null) {
+                return existing
+            }
+        }
+
+        // Suspend and query CameraExtensionMetadata on a background thread.
+        return withContext(threads.backgroundDispatcher) {
+            awaitCameraExtensionMetadata(cameraId, extension)
+        }
     }
 
     override fun awaitCameraMetadata(cameraId: CameraId): CameraMetadata {
@@ -88,7 +115,36 @@ constructor(
         }
     }
 
-    private fun createCameraMetadata(cameraId: CameraId, redacted: Boolean): Camera2CameraMetadata {
+    override fun awaitCameraExtensionMetadata(
+        cameraId: CameraId,
+        extension: Int
+    ): CameraExtensionMetadata {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return Debug.trace("Camera-${cameraId.value}#awaitExtensionMetadata") {
+                synchronized(extensionCache) {
+                    val existing = extensionCache[cameraId.value]
+                    if (existing != null) {
+                        return@trace existing
+                    } else if (!isMetadataRedacted()) {
+                        val result = createCameraExtensionMetadata(cameraId, false, extension)
+                        extensionCache[cameraId.value] = result
+                        return@trace result
+                    }
+                }
+                return@trace createCameraExtensionMetadata(cameraId, true, extension)
+            }
+        } else {
+            throw Exception(
+                "Extension sessions are only supported on Android S or higher. " +
+                    "Device SDK is ${Build.VERSION.SDK_INT}"
+            )
+        }
+    }
+
+    private fun createCameraMetadata(
+        cameraId: CameraId,
+        redacted: Boolean
+    ): Camera2CameraMetadata {
         val start = Timestamps.now(timeSource)
 
         return Debug.trace("Camera-${cameraId.value}#readCameraMetadata") {
@@ -151,6 +207,77 @@ constructor(
                 throw IllegalStateException("Failed to load metadata for $cameraId!", throwable)
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun createCameraExtensionMetadata(
+        cameraId: CameraId,
+        redacted: Boolean,
+        extension: Int
+    ): Camera2CameraExtensionMetadata {
+        val start = Timestamps.now(timeSource)
+
+        return Debug.trace("Camera-${cameraId.value}#readCameraExtensionMetadata") {
+            try {
+                Log.debug { "Loading extension metadata for $cameraId" }
+
+                val extensionCharacteristics = getCameraExtensionCharacteristics(cameraId)
+
+                val extensionMetadata =
+                    Camera2CameraExtensionMetadata(
+                        cameraId,
+                        redacted,
+                        extension,
+                        extensionCharacteristics,
+                        emptyMap()
+                    )
+
+                Log.info {
+                    val duration = Timestamps.now(timeSource) - start
+                    val redactedString =
+                        when (redacted) {
+                            false -> ""
+                            true -> " (redacted)"
+                        }
+                    "Loaded extension metadata for $cameraId in " +
+                        "${duration.formatMs()}$redactedString"
+                }
+
+                return@trace extensionMetadata
+            } catch (throwable: Throwable) {
+                throw IllegalStateException(
+                    "Failed to load extension metadata " +
+                        "for $cameraId!", throwable
+                )
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun getCameraExtensionCharacteristics(
+        cameraId: CameraId
+    ): CameraExtensionCharacteristics {
+        synchronized(extensionCharacteristicsCache) {
+            val existing = extensionCharacteristicsCache[cameraId.value]
+            if (existing != null) {
+                return existing
+            }
+        }
+        Log.debug { "Retrieving CameraExtensionCharacteristics for $cameraId" }
+        val cameraManager =
+            cameraPipeContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        val extensionCharacteristics = Api31Compat
+            .getCameraExtensionCharacteristics(cameraManager, cameraId.value)
+
+        // This technically shouldn't be null per documentation, but we suspect it could be
+        // under certain devices in certain situations.
+        @Suppress("RedundantRequireNotNullCall")
+        checkNotNull(extensionCharacteristics) {
+            "Failed to get CameraExtensionCharacteristics for $cameraId!"
+        }
+
+        return extensionCharacteristics
     }
 
     private fun isMetadataRedacted(): Boolean = !permissions.hasCameraPermission
