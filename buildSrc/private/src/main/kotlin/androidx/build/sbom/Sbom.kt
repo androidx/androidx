7f1f058d72ee71dd59570ea8d44d73d720a58004
Jeff Gaston <jeffrygaston@google.com>
2023-07-31 10:07:39 -0400

    copying sboms to dist
    where the build server hopefully will find them
    
    Bug: 283119819
    
    Test: ./gradlew exportSboms && find ../../out/dist/sboms -type f
    Test: Treehugger runs busytown/androidx_incremental.sh
    
    Change-Id: I501111cd88f40962a54158efc0d24bafefa74ba7

--- buildSrc/private/src/main/kotlin/androidx/build/sbom/Sbom.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/sbom/Sbom.kt
@@ -21,6 +21,7 @@ import androidx.build.BundleInsideHelper
 import androidx.build.GMavenZipTask
 import androidx.build.ProjectLayoutType
 import androidx.build.addToBuildOnServer
+import androidx.build.getDistributionDirectory
 import androidx.build.getPrebuiltsRoot
 import androidx.build.getSupportRootFolder
 import androidx.build.gitclient.MultiGitClient
@@ -34,6 +35,7 @@ import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ModuleVersionIdentifier
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.AbstractArchiveTask
 import org.gradle.api.tasks.bundling.Zip
 import org.gradle.jvm.tasks.Jar
@@ -201,6 +203,8 @@ fun Project.validateAllArchiveInputsRecognized() {
 /** Enables the publishing of an sbom that lists our embedded dependencies */
 fun Project.configureSbomPublishing() {
     val uuid = project.coordinatesToUUID().toString()
+    val projectName = project.name
+    val projectVersion = project.version.toString()
 
     project.configurations.create(sbomEmptyConfiguration)
     project.apply(plugin = "org.spdx.sbom")
@@ -209,9 +213,30 @@ fun Project.configureSbomPublishing() {
     val supportRootDir = getSupportRootFolder()
 
     val allowPublicRepos = System.getenv("ALLOW_PUBLIC_REPOS") != null
+    val sbomPublishDir = project.getSbomPublishDir()
+
+    val sbomBuiltFile = project.layout.buildDirectory.file(
+        "spdx/release.spdx.json"
+    ).get().getAsFile()
+
+    val publishTask = project.tasks.register("exportSboms", Copy::class.java) { publishTask ->
+        publishTask.destinationDir = sbomPublishDir
+        val sbomBuildDir = sbomBuiltFile.parentFile
+        publishTask.from(sbomBuildDir)
+        publishTask.rename(sbomBuiltFile.name, "$projectName-$projectVersion.spdx.json")
+
+        publishTask.doFirst {
+            if (!sbomBuiltFile.exists()) {
+                throw GradleException(
+                    "sbom file does not exist: $sbomBuiltFile"
+                )
+            }
+        }
+    }
 
     project.tasks.withType(SpdxSbomTask::class.java).configureEach { task ->
         val sbomProjectDir = project.projectDir
+
         task.taskExtension.set(
             object : DefaultSpdxSbomTaskExtension() {
                 override fun mapRepoUri(repoUri: URI, artifact: ModuleVersionIdentifier): URI {
@@ -277,6 +302,9 @@ fun Project.configureSbomPublishing() {
             target.getConfigurations().set(sbomConfigurations)
         }
         project.addToBuildOnServer(tasks.named("spdxSbomForRelease"))
+        publishTask.configure { task ->
+            task.dependsOn("spdxSbomForRelease")
+        }
     }
 }
 
@@ -308,6 +336,11 @@ fun getGitRemoteUrl(dir: File, supportRootDir: File): String {
         return "android.googlesource.com/platform/external/icing"
     }
     throw GradleException("Could not identify git remote url for project at $dir")
+}
+
+fun Project.getSbomPublishDir(): File {
+    val groupPath = project.group.toString().replace(".", "/")
+    return File(getDistributionDirectory(), "sboms/$groupPath/${project.name}/${project.version}")
 }
 
 private const val MAVEN_CENTRAL_REPO_URL = "https://repo.maven.apache.org/maven2"
