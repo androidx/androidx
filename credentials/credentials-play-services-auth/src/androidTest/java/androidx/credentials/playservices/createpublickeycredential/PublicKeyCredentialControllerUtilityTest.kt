e922fb93191c64ed1eabf0240c374d4af4c14871
Becca Hughes <beccahughes@google.com>
2023-08-01 10:21:01 -0700

    Add some tests to cover some of the logic
    Bug: 293743991
    Test: run tests
    Change-Id: I925d6d43bd85f9022ca81f3ae2dde6df40879409

--- credentials/credentials-play-services-auth/src/androidTest/java/androidx/credentials/playservices/createpublickeycredential/PublicKeyCredentialControllerUtilityTest.kt
+++ credentials/credentials-play-services-auth/src/androidTest/java/androidx/credentials/playservices/createpublickeycredential/PublicKeyCredentialControllerUtilityTest.kt
@@ -16,9 +16,12 @@
 
 package androidx.credentials.playservices.controllers.CreatePublicKeyCredential
 
+import androidx.credentials.GetPublicKeyCredentialOption
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.google.android.gms.fido.fido2.api.common.ErrorCode
 import com.google.common.truth.Truth.assertThat
+import org.json.JSONObject
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -72,8 +75,151 @@ class PublicKeyCredentialControllerUtilityTest {
         "pubKeyCredParams"
     )
 
+  private val TEST_REQUEST_JSON = "{\"hi\":{\"there\":{\"lol\":\"Value\"}}}"
+
+  private val TEST_DOM_EXCEPTION_PREFIX =
+    "androidx.credentials.TYPE_GET_PUBLIC_KEY_CREDENTIAL_DOM_EXCEPTION/"
+
+  private val ERROR_CODE_TO_TYPES =
+    mapOf(
+      ErrorCode.UNKNOWN_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_UNKNOWN_ERROR",
+      ErrorCode.ABORT_ERR to TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_ABORT_ERROR",
+      ErrorCode.ATTESTATION_NOT_PRIVATE_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_NOT_READABLE_ERROR",
+      ErrorCode.CONSTRAINT_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_CONSTRAINT_ERROR",
+      ErrorCode.DATA_ERR to TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_DATA_ERROR",
+      ErrorCode.INVALID_STATE_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_INVALID_STATE_ERROR",
+      ErrorCode.ENCODING_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_ENCODING_ERROR",
+      ErrorCode.NETWORK_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_NETWORK_ERROR",
+      ErrorCode.NOT_ALLOWED_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_NOT_ALLOWED_ERROR",
+      ErrorCode.NOT_SUPPORTED_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_NOT_SUPPORTED_ERROR",
+      ErrorCode.SECURITY_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_SECURITY_ERROR",
+      ErrorCode.TIMEOUT_ERR to
+        TEST_DOM_EXCEPTION_PREFIX + "androidx.credentials.TYPE_TIMEOUT_ERROR",
+    )
+
   @Test
   fun verifyJsonKeyNamingHasNotChanged() {
     JSON_KEYS_TO_EXPECTED_VAULES.forEach { entry -> assertThat(entry.key).isEqualTo(entry.value) }
+  }
+
+  @Test
+  fun verifyCheckAlgSupported() {
+    assertThat(PublicKeyCredentialControllerUtility.Companion.checkAlgSupported(-7))
+      .isTrue() // es-256
+    assertThat(PublicKeyCredentialControllerUtility.Companion.checkAlgSupported(999999)).isFalse()
+  }
+
+  @Test
+  fun convertToPlayAuthPasskeyJsonRequest_success() {
+    val option = GetPublicKeyCredentialOption(TEST_REQUEST_JSON)
+    val output =
+      PublicKeyCredentialControllerUtility.Companion.convertToPlayAuthPasskeyJsonRequest(option)
+
+    assertThat(output.isSupported()).isTrue()
+    assertThat(output.getRequestJson()).isEqualTo(TEST_REQUEST_JSON)
+  }
+
+  @Test
+  fun toAssertPasskeyResponse_authenticatorErrorResponse_success() {
+    ERROR_CODE_TO_TYPES.forEach { entry ->
+      var exception =
+        PublicKeyCredentialControllerUtility.beginSignInPublicKeyCredentialResponseContainsError(
+          entry.key,
+          "test message"
+        )
+      assertThat(exception.type).isEqualTo(entry.value)
+      assertThat(exception.errorMessage).isEqualTo("test message")
+    }
+  }
+
+  @Test
+  fun toAssertPasskeyResponse_authenticatorAssertionResponse_success() {
+    val byteArrayClientDataJson = byteArrayOf(0x48, 101, 108, 108, 111)
+    val byteArrayAuthenticatorData = byteArrayOf(0x48, 101, 108, 108, 112)
+    val byteArraySignature = byteArrayOf(0x48, 101, 108, 108, 113)
+    val byteArrayUserHandle = byteArrayOf(0x48, 101, 108, 108, 114)
+    val json = JSONObject()
+    val publicKeyCredId = "id"
+    val publicKeyCredRawId = byteArrayOf(0x48, 101, 108, 108, 115)
+    val publicKeyCredType = "type"
+
+    PublicKeyCredentialControllerUtility.beginSignInAssertionResponse(
+      byteArrayClientDataJson,
+      byteArrayAuthenticatorData,
+      byteArraySignature,
+      byteArrayUserHandle,
+      json,
+      publicKeyCredId,
+      publicKeyCredRawId,
+      publicKeyCredType
+    )
+
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_ID))
+      .isEqualTo(publicKeyCredId)
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_RAW_ID))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(publicKeyCredRawId))
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_TYPE))
+      .isEqualTo(publicKeyCredType)
+
+    // There is some embedded JSON so we should make sure we test that.
+    var embeddedResponse =
+      json.getJSONObject(PublicKeyCredentialControllerUtility.JSON_KEY_RESPONSE)
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_CLIENT_DATA))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArrayClientDataJson))
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_AUTH_DATA))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArrayAuthenticatorData))
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_SIGNATURE))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArraySignature))
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_USER_HANDLE))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArrayUserHandle))
+  }
+
+  fun toAssertPasskeyResponse_authenticatorAssertionResponse_noUserHandle_success() {
+    val byteArrayClientDataJson = byteArrayOf(0x48, 101, 108, 108, 111)
+    val byteArrayAuthenticatorData = byteArrayOf(0x48, 101, 108, 108, 112)
+    val byteArraySignature = byteArrayOf(0x48, 101, 108, 108, 113)
+    val json = JSONObject()
+    val publicKeyCredId = "id"
+    val publicKeyCredRawId = byteArrayOf(0x48, 101, 108, 108, 115)
+    val publicKeyCredType = "type"
+
+    PublicKeyCredentialControllerUtility.beginSignInAssertionResponse(
+      byteArrayClientDataJson,
+      byteArrayAuthenticatorData,
+      byteArraySignature,
+      null,
+      json,
+      publicKeyCredId,
+      publicKeyCredRawId,
+      publicKeyCredType
+    )
+
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_ID))
+      .isEqualTo(publicKeyCredId)
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_RAW_ID))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(publicKeyCredRawId))
+    assertThat(json.get(PublicKeyCredentialControllerUtility.JSON_KEY_TYPE))
+      .isEqualTo(publicKeyCredType)
+
+    // There is some embedded JSON so we should make sure we test that.
+    var embeddedResponse =
+      json.getJSONObject(PublicKeyCredentialControllerUtility.JSON_KEY_RESPONSE)
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_CLIENT_DATA))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArrayClientDataJson))
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_AUTH_DATA))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArrayAuthenticatorData))
+    assertThat(embeddedResponse.get(PublicKeyCredentialControllerUtility.JSON_KEY_SIGNATURE))
+      .isEqualTo(PublicKeyCredentialControllerUtility.b64Encode(byteArraySignature))
+    assertThat(embeddedResponse.has(PublicKeyCredentialControllerUtility.JSON_KEY_USER_HANDLE))
+      .isFalse()
   }
 }
