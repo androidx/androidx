873983b4199785f417b60ace05b466208369fa4e
Sergey Vasilinets <sergeyv@google.com>
2023-08-02 13:51:23 +0100

    Limit workers ran by GreedyScheduler
    Previously, GreedyScheduler let run worker indefinitely.
    Now we generously limit them by 90min.
    This limit isn't applicable to foreground workers.
    
    Test: GreedySchedulerTimeoutTest
    Change-Id: Id2dfe3cf0b7de5a815038fc6fb643e81c4be39e4

---
+++ work/work-runtime/src/main/java/androidx/work/impl/background/greedy/TimeLimiter.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.work.impl.background.greedy
+
+import android.app.job.JobParameters
+import androidx.work.RunnableScheduler
+import androidx.work.StopReason
+import androidx.work.impl.StartStopToken
+import androidx.work.impl.WorkLauncher
+import java.util.concurrent.TimeUnit
+
+internal class TimeLimiter @JvmOverloads constructor(
+    private val runnableScheduler: RunnableScheduler,
+    private val launcher: WorkLauncher,
+    private val timeoutMs: Long = TimeUnit.MINUTES.toMillis(90),
+) {
+    private val lock = Any()
+    private val tracked = mutableMapOf<StartStopToken, Runnable>()
+
+    fun track(token: StartStopToken) {
+        val stopRunnable = Runnable {
+            launcher.stopWork(token, StopReason(JobParameters.STOP_REASON_TIMEOUT))
+        }
+        synchronized(lock) { tracked.put(token, stopRunnable) }
+        runnableScheduler.scheduleWithDelay(timeoutMs, stopRunnable)
+    }
+
+    fun cancel(token: StartStopToken) {
+        synchronized(lock) { tracked.remove(token) }?.let { runnableScheduler.cancel(it) }
+    }
+}
