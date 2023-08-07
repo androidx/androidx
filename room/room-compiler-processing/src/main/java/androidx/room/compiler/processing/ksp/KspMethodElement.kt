b76e542972f17371d786c7418dc8c1f23fabcd67
Daniel Santiago Rivera <danysantiago@google.com>
2023-08-07 14:36:26 -0400

    Use owner if available when checking for overrides in KSP.
    Bug: 281665731
    Test: XTypeElementTest#overrideConsideringOwner
    Change-Id: I740e096e0168940ad5ad4779093c791ea8286548

--- room/room-compiler-processing/src/main/java/androidx/room/compiler/processing/ksp/KspMethodElement.kt
+++ room/room-compiler-processing/src/main/java/androidx/room/compiler/processing/ksp/KspMethodElement.kt
@@ -140,7 +140,7 @@ internal sealed class KspMethodElement(
     override fun isExtensionFunction() = declaration.extensionReceiver != null
 
     override fun overrides(other: XMethodElement, owner: XTypeElement): Boolean {
-        return env.resolver.overrides(this, other)
+        return env.resolver.overrides(this, other, owner as? KspTypeElement)
     }
 
     override fun isKotlinPropertyMethod() = false
