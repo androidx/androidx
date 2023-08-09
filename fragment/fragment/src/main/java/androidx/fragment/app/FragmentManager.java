45b68bbb181cd8aa3e04fc2025bb42f854a21a29
Jeremy Woods <jbwoods@google.com>
2023-08-09 22:54:43 +0000

    Fix an issue with popping the last back stack entry
    When using predictive back if you pop the last entry on the back stack,
    attempting to go back to the first fragment, the app will skip the first
    fragment and then destroy the activity.
    
    This happens because when predictive back starts a back gesture, the
    transition in progress is no longer includes as part of the back stack
    count, which cause the fragment manager to prematurely disabled its
    onBackCallback thinking that it has no entries and then it finishes the
    activity instead of going back.
    
    We should ensure we continue to consider the transitioning operations as
    part of the back stack while transitioning.
    
    RelNote: "Fixed an issue with predictive back that causes the Activity
    to be finished, before ever seeing the first fragment in the fragment
    manager."
    Test: added PredictiveBackTest
    Bug: 295231788
    Fixes: 295168369
    
    Change-Id: I0664b44dd7f6dc49bafb932e3ae47f1aba483fd6

--- fragment/fragment/src/main/java/androidx/fragment/app/FragmentManager.java
+++ fragment/fragment/src/main/java/androidx/fragment/app/FragmentManager.java
@@ -1024,7 +1024,7 @@ public abstract class FragmentManager implements FragmentResultOwner {
      * Return the number of entries currently in the back stack.
      */
     public int getBackStackEntryCount() {
-        return mBackStack != null ? mBackStack.size() : 0;
+        return (mBackStack != null ? mBackStack.size() : 0) + (mTransitioningOp != null ? 1 : 0);
     }
 
     /**
@@ -1033,6 +1033,12 @@ public abstract class FragmentManager implements FragmentResultOwner {
      */
     @NonNull
     public BackStackEntry getBackStackEntryAt(int index) {
+        if (index == mBackStack.size()) {
+            if (mTransitioningOp == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            return mTransitioningOp;
+        }
         return mBackStack.get(index);
     }
 
