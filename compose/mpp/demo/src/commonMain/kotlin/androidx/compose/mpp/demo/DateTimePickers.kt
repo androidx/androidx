06eaced806a06cbc2c05010140cda3a7ada9debf
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-08-11 14:26:58 +0200

    Expect `DatePickerDialog` in common (#745)
    * Copy DatePickerDialog from android
    
    * Expect DatePickerDialog in common
    
    * Add DatePickerDialog sample to mpp

--- compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/DateTimePickers.kt
+++ compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/DateTimePickers.kt
@@ -16,33 +16,71 @@
 
 package androidx.compose.mpp.demo
 
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateTimePickers() {
     Column(
         modifier = Modifier
+            .padding(5.dp)
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .verticalScroll(rememberScrollState())
     ) {
-        val dpState = rememberDatePickerState()
-        val tpState = rememberTimePickerState()
+        Text("DatePicker:")
+        Box(Modifier.border(1.dp, Color.Black)) {
+            val state = rememberDatePickerState()
+            DatePicker(state)
+        }
 
-        DatePicker(dpState)
-        TimePicker(tpState)
+        Text("TimePicker:")
+        Box(Modifier.border(1.dp, Color.Black)) {
+            val state = rememberTimePickerState()
+            TimePicker(state)
+        }
+
+        var openDialog by remember { mutableStateOf(false) }
+        Button(onClick = { openDialog = true }) {
+            Text("DatePickerDialog")
+        }
+        if (openDialog) {
+            val state = rememberDatePickerState()
+            DatePickerDialog(
+                onDismissRequest = { openDialog = false },
+                confirmButton = {
+                    Button(onClick = {
+                        openDialog = false
+                        println("Selected date timestamp: ${state.selectedDateMillis}")
+                    }) {
+                        Text("Confirm")
+                    }
+                },
+                content = {
+                    DatePicker(state)
+                }
+            )
+        }
     }
 }
