d41dcb94cf0e506fe52bf94e5816b59ff521f450
Andrew Chiang <andrewchiang@google.com>
2023-08-30 18:32:57 +0800

    Add Moto G20, Itel A48 and Realme Cll 2021 to FlashTooSlowQuirk.
    Bug: 296816175, 296814664
    Test: FlashTooSlowQuirkTest and manually testing on these devices.
    Change-Id: I7419bbb5a43a9cbf528565868056a824f6b841ee

--- camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/compat/quirk/FlashTooSlowQuirk.kt
+++ camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/compat/quirk/FlashTooSlowQuirk.kt
@@ -26,11 +26,12 @@ import androidx.camera.camera2.pipe.CameraMetadata
  * Quirks that denotes the device has a slow flash sequence that could result in blurred pictures.
  *
  * QuirkSummary
- * - Bug Id: 211474332, 286190938, 280221967
+ * - Bug Id: 211474332, 286190938, 280221967, 296814664, 296816175
  * - Description: When capturing still photos in auto flash mode, it needs more than 1 second to
  *   flash or capture actual photo after flash, and therefore it easily results in blurred or dark
  *   or overexposed pictures.
- * - Device(s): Pixel 3a / Pixel 3a XL, all models of Pixel 4 and 5, SM-A320
+ * - Device(s): Pixel 3a / Pixel 3a XL, all models of Pixel 4 and 5, SM-A320, Moto G20, Itel A48,
+ *   Realme C11 2021
  *
  * TODO(b/270421716): enable CameraXQuirksClassDetector lint check when kotlin is supported.
  */
@@ -44,7 +45,10 @@ class FlashTooSlowQuirk : UseTorchAsFlashQuirk {
             "PIXEL 3A XL",
             "PIXEL 4", // includes Pixel 4 XL, 4A, and 4A (5g) too
             "PIXEL 5", // includes Pixel 5A too
-            "SM-A320"
+            "SM-A320",
+            "MOTO G(20)",
+            "ITEL L6006", // Itel A48
+            "RMX3231" // Realme C11 2021
         )
 
         fun isEnabled(cameraMetadata: CameraMetadata): Boolean {
