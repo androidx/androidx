4ec63830db7f1db517f6e7119639e75b224248ef
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-09-12 16:49:04 +0200

    Expect `material3.SearchBar` and `material3.DockedSearchBar` in common (#801)
    * Move SearchBar to common
    
    * Handle platform specifics
    
    * Add search bar sample

--- compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/Example1.kt
+++ compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/Example1.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,22 +17,30 @@ import androidx.compose.material.RadioButton
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Applier
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.unit.dp
 import kotlinx.serialization.Serializable
 
 @Serializable // Not used at the moment, but added here to test compose with kotlinx.serialization compilation
@@ -53,6 +62,9 @@ fun Example1() {
     val clipboard = LocalClipboardManager.current
     val uriHandler = LocalUriHandler.current
     Column {
+        SearchBarSample()
+        DockedSearchBarSample()
+
         TextField(
             value = textFieldState,
             onValueChange = {
@@ -136,5 +148,71 @@ fun Example1() {
             }
         }
 
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchBarSample() {
+    var text by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+    Box(Modifier.fillMaxWidth().padding(5.dp)) {
+        SearchBar(
+            modifier = Modifier.align(Alignment.TopCenter),
+            query = text,
+            onQueryChange = { text = it },
+            onSearch = { active = false },
+            active = active,
+            onActiveChange = { active = it },
+            placeholder = { Text("SearchBar") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+        ) {
+            SearchBarSampleContent {
+                text = it
+                active = false
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DockedSearchBarSample() {
+    var text by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+    Box(Modifier.fillMaxWidth().padding(5.dp)) {
+        DockedSearchBar(
+            modifier = Modifier.align(Alignment.TopCenter),
+            query = text,
+            onQueryChange = { text = it },
+            onSearch = { active = false },
+            active = active,
+            onActiveChange = { active = it },
+            placeholder = { Text("DockedSearchBar") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+            trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+        ) {
+            SearchBarSampleContent {
+                text = it
+                active = false
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchBarSampleContent(onClick: (String) -> Unit) {
+    repeat(4) { index ->
+        val resultText = "Suggestion $index"
+        ListItem(
+            headlineContent = { Text(resultText) },
+            supportingContent = { Text("Additional info") },
+            leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 4.dp)
+                .clickable { onClick(resultText) }
+        )
     }
 }
