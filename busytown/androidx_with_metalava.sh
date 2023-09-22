18bfa128e0066d75177e6908a0cd5455ff1a60fe
Aurimas Liutikas <aurimas@google.com>
2023-09-22 21:41:34 +0000

    Temporarily disable androidx_with_metalava
    Disable running checkApi while metalava breaking change is landed
    
    Change-Id: Id95ef05651d451aa432b2998941b503955be7845
    Test: None

--- busytown/androidx_with_metalava.sh
+++ busytown/androidx_with_metalava.sh
@@ -2,6 +2,8 @@
 set -e
 SCRIPT_PATH="$(cd $(dirname $0) && pwd)"
 
+
 $SCRIPT_PATH/impl/build-metalava-and-androidx.sh \
-  listTaskOutputs \
-  checkApi
+  listTaskOutputs #\
+  # Temporarily disable running checkApi while metalava breaking change is landed
+  # checkApi
