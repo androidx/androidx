e7ce289a8a1e6dbe5d7024c36d6b16c56625ceaa
Sergey Vasilinetc <sergeyv@google.com>
2023-10-18 14:17:25 +0100

    Migrate Worker to kotlin
    API Council note: it was specifically chosen to use
    `fun getForegroundInfo()` over `val foregroundInfo` for
    following reasons:
    1. `getForegroundInfo` by meaning is actually closer to
    `buildForegroundInfo`, rather being just a property of
    Worker
    2. It is beneficial for developers, because clients override
    this method. `getForegroundInfo()` keeps source compatibility
    for kotlin developers. Moreover, this method is mainly called by
    WorkManager itself, rather than calling it themselves.
    
    Relnote: N/A
    Test: refactoring
    Change-Id: I9c8293c0cdacdcec1f5f50924be133c80b6819ce

--- work/work-runtime/src/main/java/androidx/work/Worker.kt
+++ work/work-runtime/src/main/java/androidx/work/Worker.kt
@@ -13,118 +13,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.work
 
-package androidx.work;
-
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.WorkerThread;
-import androidx.work.impl.utils.futures.SettableFuture;
-
-import com.google.common.util.concurrent.ListenableFuture;
+import android.content.Context
+import androidx.annotation.WorkerThread
+import androidx.concurrent.futures.CallbackToFutureAdapter
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executor
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
- * A class that performs work synchronously on a background thread provided by {@link WorkManager}.
- * <p>
- * Worker classes are instantiated at runtime by WorkManager and the {@link #doWork()} method is
- * called on a pre-specified background thread (see {@link Configuration#getExecutor()}).  This
- * method is for <b>synchronous</b> processing of your work, meaning that once you return from that
+ * A class that performs work synchronously on a background thread provided by [WorkManager].
+ *
+ * Worker classes are instantiated at runtime by WorkManager and the [.doWork] method is
+ * called on a pre-specified background thread (see [Configuration.executor]).  This
+ * method is for **synchronous** processing of your work, meaning that once you return from that
  * method, the Worker is considered to be finished and will be destroyed.  If you need to do your
- * work asynchronously or call asynchronous APIs, you should use {@link ListenableWorker}.
- * <p>
+ * work asynchronously or call asynchronous APIs, you should use [ListenableWorker].
+ *
+ *
  * In case the work is preempted for any reason, the same instance of Worker is not reused.  This
- * means that {@link #doWork()} is called exactly once per Worker instance.  A new Worker is created
+ * means that [.doWork] is called exactly once per Worker instance.  A new Worker is created
  * if a unit of work needs to be rerun.
- * <p>
+ *
+ *
  * A Worker is given a maximum of ten minutes to finish its execution and return a
- * {@link androidx.work.ListenableWorker.Result}.  After this time has expired, the Worker will be
+ * [androidx.work.ListenableWorker.Result].  After this time has expired, the Worker will be
  * signalled to stop.
  */
-
-public abstract class Worker extends ListenableWorker {
-
-    // Package-private to avoid synthetic accessor.
-    SettableFuture<Result> mFuture;
-
-    public Worker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-        super(context, workerParams);
-    }
+abstract class Worker(
+    context: Context,
+    workerParams: WorkerParameters
+) : ListenableWorker(context, workerParams) {
 
     /**
      * Override this method to do your actual background processing.  This method is called on a
-     * background thread - you are required to <b>synchronously</b> do your work and return the
-     * {@link androidx.work.ListenableWorker.Result} from this method.  Once you return from this
+     * background thread - you are required to **synchronously** do your work and return the
+     * [androidx.work.ListenableWorker.Result] from this method.  Once you return from this
      * method, the Worker is considered to have finished what its doing and will be destroyed.  If
      * you need to do your work asynchronously on a thread of your own choice, see
-     * {@link ListenableWorker}.
-     * <p>
+     * [ListenableWorker].
+     *
+     *
      * A Worker has a well defined
-     * <a href="https://d.android.com/reference/android/app/job/JobScheduler">execution window</a>
-     * to finish its execution and return a {@link androidx.work.ListenableWorker.Result}.  After
+     * [execution window](https://d.android.com/reference/android/app/job/JobScheduler)
+     * to finish its execution and return a [androidx.work.ListenableWorker.Result].  After
      * this time has expired, the Worker will be signalled to stop.
      *
-     * @return The {@link androidx.work.ListenableWorker.Result} of the computation; note that
-     *         dependent work will not execute if you use
-     *         {@link androidx.work.ListenableWorker.Result#failure()} or
-     *         {@link androidx.work.ListenableWorker.Result#failure(Data)}
+     * @return The [androidx.work.ListenableWorker.Result] of the computation; note that
+     * dependent work will not execute if you use
+     * [androidx.work.ListenableWorker.Result.failure] or
+     * [androidx.work.ListenableWorker.Result.failure]
      */
     @WorkerThread
-    public abstract @NonNull Result doWork();
+    abstract fun doWork(): Result
 
-    @Override
-    public final @NonNull ListenableFuture<Result> startWork() {
-        mFuture = SettableFuture.create();
-        getBackgroundExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Result result = doWork();
-                    mFuture.set(result);
-                } catch (Throwable throwable) {
-                    mFuture.setException(throwable);
-                }
-
-            }
-        });
-        return mFuture;
+    final override fun startWork(): ListenableFuture<Result> = backgroundExecutor.future {
+        doWork()
     }
 
-    @Override
-    public @NonNull ListenableFuture<ForegroundInfo> getForegroundInfoAsync() {
-        SettableFuture<ForegroundInfo> future = SettableFuture.create();
-        getBackgroundExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ForegroundInfo info = getForegroundInfo();
-                    future.set(info);
-                } catch (Throwable throwable) {
-                    future.setException(throwable);
-                }
-            }
-        });
-        return future;
-    }
+    override fun getForegroundInfoAsync(): ListenableFuture<ForegroundInfo> =
+        backgroundExecutor.future { getForegroundInfo() }
 
     /**
-     * Return an instance of {@link  ForegroundInfo} if the {@link WorkRequest} is important to
+     * An instance of [ForegroundInfo] if the [WorkRequest] is important to
      * the user.  In this case, WorkManager provides a signal to the OS that the process should
      * be kept alive while this work is executing.
-     * <p>
+     *
+     *
      * Prior to Android S, WorkManager manages and runs a foreground service on your behalf to
-     * execute the WorkRequest, showing the notification provided in the {@link ForegroundInfo}.
+     * execute the WorkRequest, showing the notification provided in the [ForegroundInfo].
      * To update this notification subsequently, the application can use
-     * {@link android.app.NotificationManager}.
-     * <p>
+     * [android.app.NotificationManager].
+     *
+     *
      * Starting in Android S and above, WorkManager manages this WorkRequest using an immediate job.
      *
-     * @return A {@link ForegroundInfo} instance if the WorkRequest is marked immediate.
-     * For more information look at {@link WorkRequest.Builder#setExpedited(OutOfQuotaPolicy)}.
+     * @return A [ForegroundInfo] instance if the WorkRequest is marked immediate.
+     * For more information look at [WorkRequest.Builder.setExpedited].
+     * @throws IllegalStateException if it is not overridden and worker tries to go to foreground
      */
     @WorkerThread
-    public @NonNull ForegroundInfo getForegroundInfo() {
-        throw new IllegalStateException("Expedited WorkRequests require a Worker to "
-                + "provide an implementation for \n `getForegroundInfo()`");
+    open fun getForegroundInfo(): ForegroundInfo {
+        throw IllegalStateException(
+            "Expedited WorkRequests require a Worker to provide an implementation for " +
+                "`getForegroundInfo()`"
+        )
+    }
+}
+
+private fun <T> Executor.future(
+    block: () -> T
+): ListenableFuture<T> = CallbackToFutureAdapter.getFuture {
+    val cancelled = AtomicBoolean(false)
+    it.addCancellationListener({ cancelled.set(true) }, DirectExecutor.INSTANCE)
+    execute {
+        if (cancelled.get()) return@execute
+        try {
+            it.set(block())
+        } catch (t: Throwable) {
+            it.setException(t)
+        }
     }
 }
