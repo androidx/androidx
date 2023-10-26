1ac2124d0e27761bc7f09d122d99254c69411631
Ian Lake <ilake@google.com>
2023-10-26 23:03:00 +0000

    Convert ActivityResultCaller to Kotlin
    Rewrite ActivityResultCaller to Kotlin.
    
    This also moves the `ActivityResultCaller` Kotlin
    extensions from `activity-ktx` back to `activity` since
    we can't have two Kotlin files with the same name+package.
    
    Relnote: "`ActivityResultCaller` has been rewritten in
    Kotlin."
    Test: ./gradlew activity:activity:checkApi
    BUG: 257291701
    
    Change-Id: Ib02e49c49dbed2ce0d25ac71c8caf0325ffb8142

--- activity/activity/src/main/java/androidx/activity/result/ActivityResultCaller.kt
+++ activity/activity/src/main/java/androidx/activity/result/ActivityResultCaller.kt
@@ -13,69 +13,127 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package androidx.activity.result
 
-
-package androidx.activity.result;
-
-import android.app.Activity;
-import android.content.Intent;
-
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.annotation.NonNull;
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.app.ActivityOptionsCompat
 
 /**
- * A class that can call {@link Activity#startActivityForResult}-style APIs without having to manage
- * request codes, and converting request/response to an {@link Intent}
+ * A class that can call [Activity.startActivityForResult]-style APIs without having to manage
+ * request codes, and converting request/response to an [Intent]
  */
-public interface ActivityResultCaller {
-
+interface ActivityResultCaller {
     /**
-     * Register a request to {@link Activity#startActivityForResult start an activity for result},
-     * designated by the given {@link ActivityResultContract contract}.
+     * Register a request to [start an activity for result][Activity.startActivityForResult],
+     * designated by the given [contract][ActivityResultContract].
      *
-     * This creates a record in the {@link ActivityResultRegistry registry} associated with this
-     * caller, managing request code, as well as conversions to/from {@link Intent} under the hood.
+     * This creates a record in the [registry][ActivityResultRegistry] associated with this
+     * caller, managing request code, as well as conversions to/from [Intent] under the hood.
      *
      * This *must* be called unconditionally, as part of initialization path, typically as a field
      * initializer of an Activity or Fragment.
      *
-     * @param <I> the type of the input(if any) required to call the activity
-     * @param <O> the type of output returned as an activity result
+     * @param I the type of the input(if any) required to call the activity
+     * @param O the type of output returned as an activity result
      *
-     * @param contract the contract, specifying conversions to/from {@link Intent}s
+     * @param contract the contract, specifying conversions to/from [Intent]s
      * @param callback the callback to be called on the main thread when activity result
-     *                 is available
+     * is available
      *
      * @return the launcher that can be used to start the activity or dispose of the prepared call.
      */
-    @NonNull
-    <I, O> ActivityResultLauncher<I> registerForActivityResult(
-            @NonNull ActivityResultContract<I, O> contract,
-            @NonNull ActivityResultCallback<O> callback);
+    fun <I, O> registerForActivityResult(
+        contract: ActivityResultContract<I, O>,
+        callback: ActivityResultCallback<O>
+    ): ActivityResultLauncher<I>
 
     /**
-     * Register a request to {@link Activity#startActivityForResult start an activity for result},
-     * designated by the given {@link ActivityResultContract contract}.
+     * Register a request to [start an activity for result][Activity.startActivityForResult],
+     * designated by the given [contract][ActivityResultContract].
      *
-     * This creates a record in the given {@link ActivityResultRegistry registry}, managing request
-     * code, as well as conversions to/from {@link Intent} under the hood.
+     * This creates a record in the given [registry][ActivityResultRegistry], managing request
+     * code, as well as conversions to/from [Intent] under the hood.
      *
      * This *must* be called unconditionally, as part of initialization path, typically as a field
      * initializer of an Activity or Fragment.
      *
-     * @param <I> the type of the input(if any) required to call the activity
-     * @param <O> the type of output returned as an activity result
+     * @param I the type of the input(if any) required to call the activity
+     * @param O the type of output returned as an activity result
      *
-     * @param contract the contract, specifying conversions to/from {@link Intent}s
+     * @param contract the contract, specifying conversions to/from [Intent]s
      * @param registry the registry where to hold the record.
      * @param callback the callback to be called on the main thread when activity result
-     *                 is available
+     * is available
      *
      * @return the launcher that can be used to start the activity or dispose of the prepared call.
      */
-    @NonNull
-    <I, O> ActivityResultLauncher<I> registerForActivityResult(
-            @NonNull ActivityResultContract<I, O> contract,
-            @NonNull ActivityResultRegistry registry,
-            @NonNull ActivityResultCallback<O> callback);
+    fun <I, O> registerForActivityResult(
+        contract: ActivityResultContract<I, O>,
+        registry: ActivityResultRegistry,
+        callback: ActivityResultCallback<O>
+    ): ActivityResultLauncher<I>
+}
+
+/**
+ * A version of [ActivityResultCaller.registerForActivityResult]
+ * that additionally takes an input right away, producing a launcher that doesn't take any
+ * additional input when called.
+ *
+ * @see ActivityResultCaller.registerForActivityResult
+ */
+fun <I, O> ActivityResultCaller.registerForActivityResult(
+    contract: ActivityResultContract<I, O>,
+    input: I,
+    registry: ActivityResultRegistry,
+    callback: (@JvmSuppressWildcards O) -> Unit
+): ActivityResultLauncher<Unit> {
+    val resultLauncher = registerForActivityResult(contract, registry) { callback(it) }
+    return ActivityResultCallerLauncher(resultLauncher, contract, input)
+}
+
+/**
+ * A version of [ActivityResultCaller.registerForActivityResult]
+ * that additionally takes an input right away, producing a launcher that doesn't take any
+ * additional input when called.
+ *
+ * @see ActivityResultCaller.registerForActivityResult
+ */
+fun <I, O> ActivityResultCaller.registerForActivityResult(
+    contract: ActivityResultContract<I, O>,
+    input: I,
+    callback: (@JvmSuppressWildcards O) -> Unit
+): ActivityResultLauncher<Unit> {
+    val resultLauncher = registerForActivityResult(contract) { callback(it) }
+    return ActivityResultCallerLauncher(resultLauncher, contract, input)
+}
+
+internal class ActivityResultCallerLauncher<I, O>(
+    private val launcher: ActivityResultLauncher<I>,
+    val callerContract: ActivityResultContract<I, O>,
+    val callerInput: I
+) : ActivityResultLauncher<Unit>() {
+    private val resultContract: ActivityResultContract<Unit, O> by lazy {
+        object : ActivityResultContract<Unit, O>() {
+            override fun createIntent(context: Context, input: Unit): Intent {
+                return callerContract.createIntent(context, callerInput)
+            }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): O {
+                return callerContract.parseResult(resultCode, intent)
+            }
+        }
+    }
+
+    override fun launch(input: Unit, options: ActivityOptionsCompat?) {
+        launcher.launch(callerInput, options)
+    }
+
+    override fun unregister() {
+        launcher.unregister()
+    }
+
+    override val contract: ActivityResultContract<Unit, O> = resultContract
 }
