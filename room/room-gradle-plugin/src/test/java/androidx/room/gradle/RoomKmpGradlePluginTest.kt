69f5107bee7239c7b760f6aeca9d8648179e49da
elifbilgin <elifbilgin@google.com>
2023-11-03 14:16:11 -0700

    Adding XProcessingEnv API to include all target platforms for codegen usage.
    This CL also introduces two checks for verifying the correct usage of
     code language and target platform:
    
    [1] Java codegen is not allowed for any platform other than Android.
    [2] Only suspend functions and/or blocking functions with deferred type
    return types (e.g. Flow) are allowed in non-Android platforms.
    
    Bug: 299168035
    Test: ./gradlew :room:room-compiler:test
    Change-Id: Ib8346617830a1aa520de922645eebad027d8dca5

--- room/room-gradle-plugin/src/test/java/androidx/room/gradle/RoomKmpGradlePluginTest.kt
+++ room/room-gradle-plugin/src/test/java/androidx/room/gradle/RoomKmpGradlePluginTest.kt
@@ -33,6 +33,7 @@ class RoomKmpGradlePluginTest {
 
     private fun setup(
         projectRoot: File = projectSetup.rootDir,
+        generateKotlin: String = "true"
     ) {
         // copy test project
         File("src/test/test-data/multiplatform-project").copyRecursively(projectRoot)
@@ -65,13 +66,9 @@ class RoomKmpGradlePluginTest {
             |kotlin {
             |  androidTarget()
             |  linuxX64("native")
+            |  jvm()
             |  sourceSets {
-            |    androidMain {
-            |      dependencies {
-            |        implementation "androidx.room:room-runtime:$roomVersion"
-            |      }
-            |    }
-            |    nativeMain {
+            |    commonMain {
             |      dependencies {
             |        implementation "androidx.room:room-runtime:$roomVersion"
             |      }
@@ -80,8 +77,10 @@ class RoomKmpGradlePluginTest {
             |}
             |
             |dependencies {
+            |    add("kspCommonMainMetadata", "androidx.room:room-compiler:$roomVersion")
             |    add("kspAndroid", "androidx.room:room-compiler:$roomVersion")
             |    add("kspNative", "androidx.room:room-compiler:$roomVersion")
+            |    add("kspJvm", "androidx.room:room-compiler:$roomVersion")
             |}
             |
             |android {
@@ -96,9 +95,11 @@ class RoomKmpGradlePluginTest {
             |}
             |
             |room {
+            |  schemaDirectory("metadata", "${'$'}projectDir/schemas/common")
             |  schemaDirectory("android", "${'$'}projectDir/schemas/android")
             |  schemaDirectory("native", "${'$'}projectDir/schemas/native")
-            |  generateKotlin = true
+            |  schemaDirectory("jvm", "${'$'}projectDir/schemas/jvm")
+            |  generateKotlin = $generateKotlin
             |}
             |
             """.trimMargin()
@@ -106,7 +107,7 @@ class RoomKmpGradlePluginTest {
     }
 
     @Test
-    fun testWorkflow() {
+    fun `Test Workflow`() {
         setup()
 
         // First build, all tasks run
@@ -131,10 +132,85 @@ class RoomKmpGradlePluginTest {
         assertThat(androidSchema.readText()).isNotEqualTo(nativeSchema.readText())
     }
 
+    @Test
+    fun `Generate Java with Non-Android targets error`() {
+        setup(generateKotlin = "false")
+
+        // Common should fail with Kotlin codegen off as there are JVM and Native targets from it
+        runGradle(
+            COMMON_KSP_TASK,
+            projectDir = projectSetup.rootDir,
+            expectFailure = true
+        ).let { result ->
+            assertThat(result.output)
+                .contains("Cannot generate Java targeting a non-Android platform")
+            result.assertTaskOutcome(COMMON_KSP_TASK, TaskOutcome.FAILED)
+        }
+
+        // Native should fail with Kotlin codegen off
+        runGradle(
+            NATIVE_COMPILE_TASK,
+            projectDir = projectSetup.rootDir,
+            expectFailure = true
+        ).let { result ->
+            assertThat(result.output)
+                .contains("Cannot generate Java targeting a non-Android platform")
+            result.assertTaskOutcome(NATIVE_KSP_TASK, TaskOutcome.FAILED)
+        }
+
+        // JVM should fail with Kotlin codegen off
+        runGradle(
+            JVM_COMPILE_TASK,
+            projectDir = projectSetup.rootDir,
+            expectFailure = true
+        ).let { result ->
+            assertThat(result.output)
+                .contains("Cannot generate Java targeting a non-Android platform")
+            result.assertTaskOutcome(JVM_KSP_TASK, TaskOutcome.FAILED)
+        }
+
+        // Android is OK when Kotlin codegen is off
+        runGradle(
+            ANDROID_COMPILE_TASK,
+            projectDir = projectSetup.rootDir,
+            expectFailure = false
+        ).assertTaskOutcome(ANDROID_KSP_TASK, TaskOutcome.SUCCESS)
+    }
+
+    @Test
+    fun `Blocking DAO function in non-Android source set`() {
+        setup(generateKotlin = "true")
+
+        // Make a change that changes the schema at version 1
+        searchAndReplace(
+            file = projectSetup.rootDir.resolve("src/nativeMain/kotlin/room/testapp/MyDatabase.kt"),
+            search = "// Insert-change",
+            replace = """
+                @Insert
+                fun blockingInsert(entity: NativeEntity)
+            """.trimIndent()
+        )
+
+        runGradle(
+            NATIVE_COMPILE_TASK,
+            projectDir = projectSetup.rootDir,
+            expectFailure = true
+        ).let { result ->
+            result.assertTaskOutcome(NATIVE_KSP_TASK, TaskOutcome.FAILED)
+            result.output.contains("Only suspend functions are allowed in DAOs" +
+                " declared in non-Android platforms.")
+        }
+    }
+
     companion object {
         private const val CLEAN_TASK = ":clean"
+        private const val COMMON_KSP_TASK = ":kspCommonMainKotlinMetadata"
         private const val ANDROID_COMPILE_TASK = ":compileDebugKotlinAndroid"
+        private const val ANDROID_KSP_TASK = ":kspDebugKotlinAndroid"
         private const val NATIVE_COMPILE_TASK = ":compileKotlinNative"
+        private const val NATIVE_KSP_TASK = ":kspKotlinNative"
+        private const val JVM_COMPILE_TASK = ":compileKotlinJvm"
+        private const val JVM_KSP_TASK = ":kspKotlinJvm"
         private const val ANDROID_COPY_TASK = ":copyRoomSchemasAndroid"
         private const val NATIVE_COPY_TASK = ":copyRoomSchemasNative"
     }
