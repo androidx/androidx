1d91c433c31f002ba5d4bc82fef3551899abcee0
Ian Lake <ilake@google.com>
2023-11-13 21:12:31 +0000

    Fix restoring Parcelable instances in SavedStateHandle
    By explicitly setting the ClassLoader on the
    restoredState Bundle, we can ensure that we properly
    support restoring custom Parcelable classes from
    SavedStateHandle.
    
    As part of the exploration of this problem, arrays
    of Parcelables were found impossible to actually
    properly support out of the box, so additional KDocs
    were added to specifically call out the workaround
    required to support those.
    
    Test: new SavedStateHandleParcelingTest
    BUG: 306146502
    Relnote: "Fixed an issue where `SavedStateHandle` would
    not properly restore custom `Parcelable` classes after
    process death and recreation. Due to type information
    that is lost by the Android framework, arrays of custom
    Parcelables require additional work (manually creating
    a typed array of the right type) and the documentation
    on `get`, `getLiveData`, and getStateFlow` now specifically
    calls this limitation out."
    (cherry picked from https://android-review.googlesource.com/q/commit:2ffce096e13e3aa4675a8b0fd8b0d74cb1ced653)
    Merged-In: I0b55ab44b636a5d0e5b33cdc21821310e2d18b9d
    Change-Id: I0b55ab44b636a5d0e5b33cdc21821310e2d18b9d

--- lifecycle/lifecycle-viewmodel-savedstate/src/androidTest/java/androidx/lifecycle/SavedStateHandleParcelingTest.kt
+++ lifecycle/lifecycle-viewmodel-savedstate/src/androidTest/java/androidx/lifecycle/SavedStateHandleParcelingTest.kt
@@ -17,7 +17,9 @@ package androidx.lifecycle
 
 import android.os.Bundle
 import android.os.Bundle.CREATOR
+import android.os.Parcel
 import android.os.Parcel.obtain
+import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle.Companion.createHandle
 import androidx.test.annotation.UiThreadTest
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -57,6 +59,36 @@ class SavedStateHandleParcelingTest {
 
     @UiThreadTest
     @Test
+    fun testParcelable() {
+        val handle = SavedStateHandle()
+        handle["custom"] = CustomTestParcelable("test")
+        handle["customArray"] = arrayOf(
+            CustomTestParcelable("test"),
+            CustomTestParcelable("test2")
+        )
+        val savedState = handle.savedStateProvider().saveState()
+        val parcel = obtain()
+        savedState.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+        val newBundle = CREATOR.createFromParcel(parcel)
+        val newHandle: SavedStateHandle = createHandle(newBundle, null)
+        assertThat<CustomTestParcelable>(
+            newHandle["custom"],
+            `is`(CustomTestParcelable("test"))
+        )
+        assertThat(
+            newHandle.get<Array<Parcelable>>("customArray").contentEquals(
+                arrayOf(
+                    CustomTestParcelable("test"),
+                    CustomTestParcelable("test2")
+                )
+            ),
+            `is`(true)
+        )
+    }
+
+    @UiThreadTest
+    @Test
     fun testRemoveFromDefault() {
         val defaultState = Bundle()
         defaultState.putString("string", "default")
@@ -67,5 +99,24 @@ class SavedStateHandleParcelingTest {
         val savedState = handle.savedStateProvider().saveState()
         val newHandle = createHandle(savedState, defaultState)
         assertThat(newHandle.contains("string"), `is`(false))
+    }
+}
+
+/**
+ * [CustomTestParcelable] that helps testing bundled custom parcels
+ */
+data class CustomTestParcelable(val name: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString())
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(name)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<CustomTestParcelable> {
+        override fun createFromParcel(parcel: Parcel) = CustomTestParcelable(parcel)
+
+        override fun newArray(size: Int): Array<CustomTestParcelable?> = arrayOfNulls(size)
     }
 }
