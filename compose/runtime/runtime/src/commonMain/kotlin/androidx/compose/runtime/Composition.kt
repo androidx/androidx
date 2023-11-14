d12ae1efc1b8cbffea3f8f7f3030a45483cab2ce
Andrei Shikov <ashikov@google.com>
2023-11-14 11:46:48 +0000

    Clear unapplied changes if recomposer loop exited early
    Adds a cleanup when recomposition crashed to make sure we leave composition in a correct state. Also marks compositions that were recomposed but not applied as "failed" to fully invalidate them on the next live edit update.
    
    Test: LiveEditTests
    Fixes: 299965807
    Change-Id: I27bed340134d773ed9f151d5dc438b0353609fbe

--- compose/runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composition.kt
+++ compose/runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composition.kt
@@ -266,6 +266,12 @@ sealed interface ControlledComposition : Composition {
     fun changesApplied()
 
     /**
+     * Abandon current changes and reset composition state. Called when recomposer cannot proceed
+     * with current recomposition loop and needs to reset composition.
+     */
+    fun abandonChanges()
+
+    /**
      * Invalidate all invalidation scopes. This is called, for example, by [Recomposer] when the
      * Recomposer becomes active after a previous period of inactivity, potentially missing more
      * granular invalidations.
@@ -1040,11 +1046,14 @@ internal class CompositionImpl(
             throw e
         }
 
-    private fun abandonChanges() {
+    override fun abandonChanges() {
         pendingModifications.set(null)
         changes.clear()
         lateChanges.clear()
-        abandonSet.clear()
+
+        if (abandonSet.isNotEmpty()) {
+            RememberEventDispatcher(abandonSet).dispatchAbandons()
+        }
     }
 
     override fun invalidateAll() {
