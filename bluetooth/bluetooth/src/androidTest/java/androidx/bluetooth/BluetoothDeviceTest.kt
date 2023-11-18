3d145dbe886e7a9ea3770a360c7fb1dfdd72b503
Ömer Faruk Yılmaz <ofy@google.com>
2023-11-18 08:03:45 +0000

    Standardize tests for BluetoothX
    Test: build
    Change-Id: I20fe06da3e13eb0a717954059f8a063aee5223d5

--- bluetooth/bluetooth/src/androidTest/java/androidx/bluetooth/BluetoothDeviceTest.kt
+++ bluetooth/bluetooth/src/androidTest/java/androidx/bluetooth/BluetoothDeviceTest.kt
@@ -22,43 +22,48 @@ import android.content.Context
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.GrantPermissionRule
-import junit.framework.TestCase.assertEquals
-import org.junit.Assume
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assume.assumeNotNull
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+/**
+ * Test cases for [BluetoothDevice]
+ */
 @RunWith(JUnit4::class)
 class BluetoothDeviceTest {
 
     @Rule
     @JvmField
-    val permissionRule: GrantPermissionRule =
-        if (Build.VERSION.SDK_INT >= 31) {
-            GrantPermissionRule.grant(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.BLUETOOTH_CONNECT,
-                android.Manifest.permission.BLUETOOTH_SCAN
-            )
-        } else GrantPermissionRule.grant(
+    val permissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= 31) {
+        GrantPermissionRule.grant(
             android.Manifest.permission.ACCESS_FINE_LOCATION,
-            android.Manifest.permission.BLUETOOTH
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.BLUETOOTH_SCAN
         )
+    } else GrantPermissionRule.grant(
+        android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.BLUETOOTH
+    )
 
     private val context: Context = ApplicationProvider.getApplicationContext()
-    private val bluetoothManager: BluetoothManager =
-        context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+    private val bluetoothManager: BluetoothManager? =
+        context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
+    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
+
+    @Before
+    fun setUp() {
+        assumeNotNull(bluetoothAdapter)
+    }
 
     @Test
     fun constructorWithFwkInstance() {
-        Assume.assumeNotNull(bluetoothAdapter) // Bluetooth is not available if adapter is null
         val fwkBluetoothDevice = bluetoothAdapter!!.getRemoteDevice("00:01:02:03:04:05")
-
         val bluetoothDevice = BluetoothDevice(fwkBluetoothDevice)
 
-        assertEquals(bluetoothDevice.bondState, fwkBluetoothDevice.bondState)
-        assertEquals(bluetoothDevice.name, fwkBluetoothDevice.name)
+        assertThat(fwkBluetoothDevice.bondState).isEqualTo(bluetoothDevice.bondState)
+        assertThat(fwkBluetoothDevice.name).isEqualTo(bluetoothDevice.name)
     }
 }
--- bluetooth/bluetooth/src/androidTest/java/androidx/bluetooth/ScanResultTest.kt
+++ bluetooth/bluetooth/src/androidTest/java/androidx/bluetooth/ScanResultTest.kt
@@ -25,35 +25,43 @@ import android.os.ParcelUuid
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SdkSuppress
 import androidx.test.rule.GrantPermissionRule
+import com.google.common.truth.Truth.assertThat
 import java.util.UUID
-import junit.framework.TestCase.assertEquals
-import org.junit.Assume
+import org.junit.Assume.assumeNotNull
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+/**
+ * Test cases for [ScanResult]
+ */
 @RunWith(JUnit4::class)
 class ScanResultTest {
-
     @Rule
     @JvmField
-    val permissionRule: GrantPermissionRule =
-        if (Build.VERSION.SDK_INT >= 31)
-            GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_CONNECT)
-        else
-            GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH)
+    val permissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= 31) {
+        GrantPermissionRule.grant(
+            android.Manifest.permission.BLUETOOTH_CONNECT
+        )
+    } else GrantPermissionRule.grant(
+        android.Manifest.permission.BLUETOOTH
+    )
 
     private val context: Context = ApplicationProvider.getApplicationContext()
-    private val bluetoothManager: BluetoothManager =
-        context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+    private val bluetoothManager: BluetoothManager? =
+        context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
+    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
+
+    @Before
+    fun setUp() {
+        assumeNotNull(bluetoothAdapter)
+    }
 
     @SdkSuppress(minSdkVersion = 26)
     @Test
     fun constructorWithFwkInstance() {
-        Assume.assumeNotNull(bluetoothAdapter) // Bluetooth is not available if adapter is null
-
         val address = "00:01:02:03:04:05"
         val fwkBluetoothDevice = bluetoothAdapter!!.getRemoteDevice(address)
         val timeStampNanos: Long = 1
@@ -78,27 +86,27 @@ class ScanResultTest {
         )
         val scanResult = ScanResult(fwkScanResult)
 
-        assertEquals(scanResult.device.name, BluetoothDevice(fwkBluetoothDevice).name)
-        assertEquals(scanResult.device.bondState, BluetoothDevice(fwkBluetoothDevice).bondState)
-        assertEquals(scanResult.deviceAddress.address, address)
-        assertEquals(scanResult.deviceAddress.addressType,
-            BluetoothAddress.ADDRESS_TYPE_UNKNOWN)
-        assertEquals(scanResult.isConnectable(), true)
-        assertEquals(scanResult.timestampNanos, timeStampNanos)
-        assertEquals(scanResult.getManufacturerSpecificData(1), null)
-        assertEquals(scanResult.serviceUuids, emptyList<UUID>())
-        assertEquals(scanResult.serviceSolicitationUuids, emptyList<UUID>())
-        assertEquals(scanResult.serviceData, emptyMap<ParcelUuid, ByteArray>())
-        assertEquals(scanResult.getServiceData(serviceUuid), null)
-        assertEquals(scanResult.rssi, rssi)
-        assertEquals(scanResult.periodicAdvertisingInterval, expectedPeriodicAdvertisingInterval)
+        assertThat(BluetoothDevice(fwkBluetoothDevice).name).isEqualTo(scanResult.device.name)
+        assertThat(BluetoothDevice(fwkBluetoothDevice).bondState)
+            .isEqualTo(scanResult.device.bondState)
+        assertThat(address).isEqualTo(scanResult.deviceAddress.address)
+        assertThat(BluetoothAddress.ADDRESS_TYPE_UNKNOWN)
+            .isEqualTo(scanResult.deviceAddress.addressType)
+        assertThat(true).isEqualTo(scanResult.isConnectable())
+        assertThat(timeStampNanos).isEqualTo(scanResult.timestampNanos)
+        assertThat(scanResult.getManufacturerSpecificData(1)).isNull()
+        assertThat(emptyList<UUID>()).isEqualTo(scanResult.serviceUuids)
+        assertThat(emptyList<UUID>()).isEqualTo(scanResult.serviceSolicitationUuids)
+        assertThat(scanResult.serviceData).isEqualTo(emptyMap<ParcelUuid, ByteArray>())
+        assertThat(scanResult.getServiceData(serviceUuid)).isNull()
+        assertThat(rssi).isEqualTo(scanResult.rssi)
+        assertThat(expectedPeriodicAdvertisingInterval)
+            .isEqualTo(scanResult.periodicAdvertisingInterval)
     }
 
     @SdkSuppress(minSdkVersion = 26)
     @Test
     fun sameDeviceReturned() {
-        Assume.assumeNotNull(bluetoothAdapter) // Bluetooth is not available if adapter is null
-
         val address = "00:01:02:03:04:05"
         val fwkBluetoothDevice = bluetoothAdapter!!.getRemoteDevice(address)
         val rssi = 34
@@ -118,7 +126,7 @@ class ScanResultTest {
             timeStampNanos
         )
         val scanResult = ScanResult(fwkScanResult)
-        assertEquals(scanResult.device, scanResult.device)
-        assertEquals(scanResult.deviceAddress, scanResult.deviceAddress)
+        assertThat(scanResult.device).isEqualTo(scanResult.device)
+        assertThat(scanResult.deviceAddress).isEqualTo(scanResult.deviceAddress)
     }
 }
