58234cdb088ff0213bcf3e79d93b79f8ca279e78
Romain Guy <romainguy@google.com>
2023-11-21 11:29:27 -0800

    Optimize FloatFloatPair
    Improves performance of FloatFloatPair by using the same technique
    used in Compose to dramatically reduce the number of instructions
    generated by converting int bits to floats. The default Kotlin
    implementation forces a static resolution that is completely un-
    necessary and expensive. With this change, the generated code
    boils down to a single fmov on arm64 after JIT or AOT compilation.
    
    This means we also greatly reduce the amount of generated code
    since FloatFloatPair inlines its getters at the call site.
    
    The Kotlin Native implementation unfortunately still suffers
    from the original issue as Kotlin hides kotlin.fromBits() from
    us. There isn't an easy way to fix this currently unfortunately.
    
    RelNote: Improved performance of FloatFloatPair
    Test: PairTest (jvm and native)
    Change-Id: If5537d58ae9248b558bfc1c63e4fdec4f343253c

---
+++ collection/collection/src/commonMain/kotlin/androidx/collection/internal/PackingHelpers.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.collection.internal
+
+// The function below is technically identical to Float.fromBits()
+// However, since it is declared as top- level functions, it does
+// not incur the cost of a static fetch through the Companion class.
+// Using this top-level function, the generated arm64 code after
+// dex2oat is exactly a single `fmov`
+
+/**
+ * Returns the [Float] value corresponding to a given bit representation.
+ */
+@PublishedApi
+internal expect fun floatFromBits(bits: Int): Float
