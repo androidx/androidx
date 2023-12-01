bf6b1ef0c0180c36ce7937d29de23bd438097f04
Daniel Santiago Rivera <danysantiago@google.com>
2023-12-01 09:27:42 -0500

    Expand SQLite multiplatform conformance tests
    * Added tests coverage for all binds and get functions.
    * Added tests that verify UTF-8 binding and getting of text.
    * Added tests that verify binding and getting NULL.
    
    Test: BaseConformanceTest
    Change-Id: I965ce7cbf3043387a7a189bce050de041ffd316d

--- sqliteMultiplatform/conformanceTest/src/commonMain/kotlin/androidx/sqliteMultiplatform/BaseConformanceTest.kt
+++ sqliteMultiplatform/conformanceTest/src/commonMain/kotlin/androidx/sqliteMultiplatform/BaseConformanceTest.kt
@@ -39,17 +39,72 @@ abstract class BaseConformanceTest {
     }
 
     @Test
-    fun simpleReadAndWrite() {
+    fun bindAndReadColumns() {
         val driver = getDriver()
         val connection = driver.open()
         try {
-            connection.execSQL("CREATE TABLE Pet (id INTEGER NOT NULL PRIMARY KEY)")
-            connection.execSQL("INSERT INTO Pet (id) VALUES (3)")
-            connection.prepare("SELECT * FROM Pet WHERE id = ?").use { statement ->
-                statement.bindLong(1, 3)
-                assertThat(statement.step()).isTrue()
-                assertThat(statement.getColumnCount()).isEqualTo(1)
-                assertThat(statement.getLong(0)).isEqualTo(3)
+            connection.execSQL(
+                "CREATE TABLE Test(integerCol INTEGER, realCol REAL, textCol TEXT, blobCol BLOB)"
+            )
+            connection.prepare(
+                "INSERT INTO Test (integerCol, realCol, textCol, blobCol) VALUES (?, ?, ?, ?)"
+            ).use {
+                it.bindLong(1, 3)
+                it.bindDouble(2, 7.87)
+                it.bindText(3, "PR")
+                it.bindBlob(4, byteArrayOf(0x0F, 0x12, 0x1B))
+                assertThat(it.step()).isFalse() // SQLITE_DONE
+            }
+            connection.prepare("SELECT * FROM Test").use {
+                assertThat(it.step()).isTrue() // SQLITE_ROW
+                assertThat(it.getColumnCount()).isEqualTo(4)
+                assertThat(it.getColumnName(0)).isEqualTo("integerCol")
+                assertThat(it.getColumnName(1)).isEqualTo("realCol")
+                assertThat(it.getColumnName(2)).isEqualTo("textCol")
+                assertThat(it.getColumnName(3)).isEqualTo("blobCol")
+                assertThat(it.getLong(0)).isEqualTo(3)
+                assertThat(it.getDouble(1)).isEqualTo(7.87)
+                assertThat(it.getText(2)).isEqualTo("PR")
+                assertThat(it.getBlob(3)).isEqualTo(byteArrayOf(0x0F, 0x12, 0x1B))
+            }
+        } finally {
+            connection.close()
+        }
+    }
+
+    @Test
+    fun bindAndReadTextUtf8() {
+        val driver = getDriver()
+        val connection = driver.open()
+        try {
+            val konnichiwa = "こんにちわ"
+            connection.execSQL("CREATE TABLE Test (textCol)")
+            connection.prepare("INSERT INTO Test (textCol) VALUES (?)").use {
+                it.bindText(1, konnichiwa)
+                assertThat(it.step()).isFalse() // SQLITE_DONE
+            }
+            connection.prepare("SELECT * FROM Test").use {
+                assertThat(it.step()).isTrue() // SQLITE_ROW
+                assertThat(it.getText(0)).isEqualTo(konnichiwa)
+            }
+        } finally {
+            connection.close()
+        }
+    }
+
+    @Test
+    fun bindAndReadNull() {
+        val driver = getDriver()
+        val connection = driver.open()
+        try {
+            connection.execSQL("CREATE TABLE Test (col)")
+            connection.prepare("INSERT INTO Test (col) VALUES (?)").use {
+                it.bindNull(1)
+                assertThat(it.step()).isFalse() // SQLITE_DONE
+            }
+            connection.prepare("SELECT * FROM Test").use {
+                assertThat(it.step()).isTrue() // SQLITE_ROW
+                assertThat(it.isNull(0)).isTrue()
             }
         } finally {
             connection.close()
