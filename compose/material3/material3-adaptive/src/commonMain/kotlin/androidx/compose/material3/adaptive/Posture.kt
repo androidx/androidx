ee165ac99eb89e1f1a4a818904e3d87d1be1f132
drchen <conradchen@google.com>
2023-12-07 13:57:18 -0800

    Move hinge bounds properties in Posture to a list of hinge info
    Relnote: move hinge bounds properties in Posture to a list of hinge info
    Test: unit test
    Bug: 299462250
    Change-Id: I24f90b83c594e547afb74e6b1b1d867f19e11054

--- compose/material3/material3-adaptive/src/commonMain/kotlin/androidx/compose/material3/adaptive/Posture.kt
+++ compose/material3/material3-adaptive/src/commonMain/kotlin/androidx/compose/material3/adaptive/Posture.kt
@@ -18,7 +18,7 @@ package androidx.compose.material3.adaptive
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.util.fastDistinctBy
+import androidx.compose.ui.util.fastMapNotNull
 
 /**
  * Posture info that can help make layout adaptation decisions. For example when
@@ -31,71 +31,109 @@ import androidx.compose.ui.util.fastDistinctBy
  * coordinate.
  *
  * @constructor create an instance of [Posture]
- * @param isTabletop `true` if the current window is considered as in the table top mode, i.e. there
- *        is one half-opened horizontal hinge in the middle of the current window. When this is
- *        `true` it usually means it's hard for users to interact with the window area around
- *        the hinge and developers may consider separating the layout along the hinge and show
- *        software keyboard or other controls in the bottom half of the window.
- * @param separatingVerticalHingeBounds the list of vertical hinge bounds that are separating.
- *        A separating hinge means it creates two logical display areas.
- * @param occludingVerticalHingeBounds the list of vertical hinge bounds that are occluding.
- *        An occluding hinge means it conceals part of the display.
- * @param allVerticalHingeBounds the list of all vertical hinge bounds.
- * @param separatingHorizontalHingeBounds the list of horizontal hinge bounds that are separating.
- *        A separating hinge means it creates two logical display areas.
- * @param occludingHorizontalHingeBounds the list of horizontal hinge bounds that are occluding.
- *        An occluding hinge means it conceals part of the display.
- * @param allHorizontalHingeBounds the list of all horizontal hinge bounds.
+ * @property isTabletop `true` if the current window is considered as in the table top mode, i.e.
+ *           there is one half-opened horizontal hinge in the middle of the current window. When
+ *           this is `true` it usually means it's hard for users to interact with the window area
+ *           around the hinge and developers may consider separating the layout along the hinge and
+ *           show software keyboard or other controls in the bottom half of the window.
+ * @property hingeList a list of all hinges that are relevant to the posture.
  */
 @ExperimentalMaterial3AdaptiveApi
 @Immutable
 class Posture(
     val isTabletop: Boolean = false,
-    val separatingVerticalHingeBounds: List<Rect> = emptyList(),
-    val occludingVerticalHingeBounds: List<Rect> = emptyList(),
-    val allVerticalHingeBounds: List<Rect> =
-        combineList(separatingVerticalHingeBounds, occludingVerticalHingeBounds)
-            .fastDistinctBy { it },
-    val separatingHorizontalHingeBounds: List<Rect> = emptyList(),
-    val occludingHorizontalHingeBounds: List<Rect> = emptyList(),
-    val allHorizontalHingeBounds: List<Rect> =
-        combineList(separatingHorizontalHingeBounds, occludingHorizontalHingeBounds)
-            .fastDistinctBy { it },
+    val hingeList: List<HingeInfo> = emptyList()
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Posture) return false
         if (isTabletop != other.isTabletop) return false
-        if (separatingVerticalHingeBounds != other.separatingVerticalHingeBounds) return false
-        if (occludingVerticalHingeBounds != other.occludingVerticalHingeBounds) return false
-        if (allVerticalHingeBounds != other.allVerticalHingeBounds) return false
-        if (separatingHorizontalHingeBounds != other.separatingHorizontalHingeBounds) return false
-        if (occludingHorizontalHingeBounds != other.occludingHorizontalHingeBounds) return false
-        if (allHorizontalHingeBounds != other.allHorizontalHingeBounds) return false
         return true
     }
 
     override fun hashCode(): Int {
         var result = isTabletop.hashCode()
-        result = 31 * result + separatingVerticalHingeBounds.hashCode()
-        result = 31 * result + occludingVerticalHingeBounds.hashCode()
-        result = 31 * result + allVerticalHingeBounds.hashCode()
-        result = 31 * result + separatingHorizontalHingeBounds.hashCode()
-        result = 31 * result + occludingHorizontalHingeBounds.hashCode()
-        result = 31 * result + allHorizontalHingeBounds.hashCode()
+        result = 31 * result + hingeList.hashCode()
         return result
     }
 }
 
-private fun <T> combineList(first: List<T>, second: List<T>): List<T> {
-    return when {
-        first.isEmpty() -> second
-        second.isEmpty() -> first
-        else -> {
-            val list = ArrayList<T>(first.size + second.size)
-            list.addAll(first)
-            list.addAll(second)
-            list
-        }
+/**
+ * Returns the list of vertical hinge bounds that are separating.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.separatingVerticalHingeBounds get() = hingeList.getBounds { isVertical && isSeparating }
+
+/**
+ *  Returns the list of vertical hinge bounds that are occluding.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.occludingVerticalHingeBounds get() = hingeList.getBounds { isVertical && isOccluding }
+
+/**
+ *  Returns the list of all vertical hinge bounds.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.allVerticalHingeBounds get() = hingeList.getBounds { isVertical }
+
+/**
+ * Returns the list of horizontal hinge bounds that are separating.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.separatingHorizontalHingeBounds
+    get() = hingeList.getBounds { !isVertical && isSeparating }
+
+/**
+ * Returns the list of horizontal hinge bounds that are occluding.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.occludingHorizontalHingeBounds
+    get() = hingeList.getBounds { !isVertical && isOccluding }
+
+/**
+ *  Returns the list of all horizontal hinge bounds.
+ */
+@ExperimentalMaterial3AdaptiveApi
+val Posture.allHorizontalHingeBounds
+    get() = hingeList.getBounds { !isVertical }
+
+/**
+ * A class that contains the info of a hinge relevant to a [Posture].
+ *
+ * @param bounds the bounds of the hinge in the relevant viewport.
+ * @param isVertical `true` if the hinge is a vertical one, i.e., it separates the viewport into
+ *        left and right; `false` if the hinge is horizontal, i.e., it separates the viewport
+ *        into top and bottom.
+ * @param isSeparating `true` if the hinge creates two logical display areas.
+ * @param isOccluding `true` if the hinge conceals part of the display.
+ */
+@ExperimentalMaterial3AdaptiveApi
+@Immutable
+class HingeInfo(
+    val bounds: Rect,
+    val isVertical: Boolean,
+    val isSeparating: Boolean,
+    val isOccluding: Boolean
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is HingeInfo) return false
+        if (bounds != other.bounds) return false
+        if (isVertical != other.isVertical) return false
+        if (isSeparating != other.isSeparating) return false
+        if (isOccluding != other.isOccluding) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = bounds.hashCode()
+        result = 31 * result + isVertical.hashCode()
+        result = 31 * result + isSeparating.hashCode()
+        result = 31 * result + isOccluding.hashCode()
+        return result
     }
 }
+
+@ExperimentalMaterial3AdaptiveApi
+private inline fun List<HingeInfo>.getBounds(predicate: HingeInfo.() -> Boolean): List<Rect> =
+    fastMapNotNull { if (it.predicate()) it.bounds else null }
