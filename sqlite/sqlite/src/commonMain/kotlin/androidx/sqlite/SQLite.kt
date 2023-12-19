a2acb3d890dac90ce573a6c915bd3dd60b3f5f9c
Daniel Santiago Rivera <danysantiago@google.com>
2023-12-18 19:28:38 -0500

    Expose driver utility methods to be used in androidx.room and add exclusiveTransaction()
    Bug: 300660920
    Test: n/a
    Relnote: "Make SQLite KMP utility APIs public."
    Change-Id: Iaf0cced1f5ad17d26f0b1b539547d30ac62055a2

--- sqlite/sqlite/src/commonMain/kotlin/androidx/sqlite/SQLite.kt
+++ sqlite/sqlite/src/commonMain/kotlin/androidx/sqlite/SQLite.kt
@@ -16,14 +16,51 @@
 
 package androidx.sqlite
 
-import androidx.annotation.RestrictTo
-
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+/**
+ * Executes a single SQL statement that returns no values.
+ */
 fun SQLiteConnection.execSQL(sql: String) {
     prepare(sql).use { it.step() }
 }
 
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+/**
+ * Performs an `EXCLUSIVE TRANSACTION`, committing to it if the [block] completes or rolling it back
+ * if an exception is thrown.
+ */
+// TODO(b/304302260): To be replaced with proper threading & transaction APIs.
+fun <R> SQLiteConnection.exclusiveTransaction(block: SQLiteConnection.() -> R): R {
+    val result: R
+    var success = false
+    this.beginExclusiveTransaction()
+    try {
+        result = this.block()
+        success = true
+    } finally {
+        if (success) {
+            this.endTransaction()
+        } else {
+            this.rollbackTransaction()
+        }
+    }
+    return result
+}
+
+private fun SQLiteConnection.beginExclusiveTransaction() {
+    execSQL("BEGIN EXCLUSIVE TRANSACTION")
+}
+
+private fun SQLiteConnection.rollbackTransaction() {
+    execSQL("ROLLBACK TRANSACTION")
+}
+
+private fun SQLiteConnection.endTransaction() {
+    execSQL("END TRANSACTION")
+}
+
+/**
+ * Use the receiver statement within the [block] and closes it once it is done.
+ */
+// TODO(b/315461431): Migrate to a Closeable interface in KMP
 fun <R> SQLiteStatement.use(block: (SQLiteStatement) -> R): R {
     try {
         return block.invoke(this)
@@ -32,7 +69,9 @@ fun <R> SQLiteStatement.use(block: (SQLiteStatement) -> R): R {
     }
 }
 
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+/**
+ * Throws a [SQLiteException] with its message formed by the given [errorCode] amd [errorMsg].
+ */
 fun throwSQLiteException(errorCode: Int, errorMsg: String?): Nothing {
     val message = buildString {
         append("Error code: $errorCode")
