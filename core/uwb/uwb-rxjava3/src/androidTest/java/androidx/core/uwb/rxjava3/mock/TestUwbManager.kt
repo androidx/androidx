5d5a6778bc9b6da5440da816020e36185fa011c8
ziyiw <ziyiw@google.com>
2023-12-26 18:38:56 +0000

    [Jetpack] Update nearby gms dependency to 19.1.0.
    Bug: 308605198
    Test: manual
    Change-Id: I78138bceb3bdf4fc013a8a2268a4de9af61785eb

--- core/uwb/uwb-rxjava3/src/androidTest/java/androidx/core/uwb/rxjava3/mock/TestUwbManager.kt
+++ core/uwb/uwb-rxjava3/src/androidTest/java/androidx/core/uwb/rxjava3/mock/TestUwbManager.kt
@@ -22,7 +22,7 @@ import androidx.core.uwb.UwbClientSessionScope
 import androidx.core.uwb.UwbControleeSessionScope
 import androidx.core.uwb.UwbControllerSessionScope
 import androidx.core.uwb.UwbManager
-import com.google.android.gms.internal.nearby.zzua
+import com.google.android.gms.internal.nearby.zzpe
 import com.google.android.gms.nearby.uwb.UwbComplexChannel
 
 /** A default implementation of [UwbManager] used in testing. */
@@ -53,7 +53,7 @@ class TestUwbManager : UwbManager {
 
         val rangingCapabilities =
         com.google.android.gms.nearby.uwb.RangingCapabilities(true, false, false, false,
-            200, zzua.zzl(9), zzua.zzl(1), zzua.zzn(1, 2, 3), zzua.zzl(2), zzua.zzl(1), false
+            200, zzpe.zzl(9), zzpe.zzl(1), zzpe.zzn(1, 2, 3), zzpe.zzl(2), zzpe.zzl(1), false
         )
         val uwbClient = TestUwbClient(complexChannel, localAddress, rangingCapabilities, true)
         return if (isController) {
