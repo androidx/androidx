7f5eab67eb13bfec62342b440a70eca7f316ea4d
Igor Demin <igordmn@gmail.com>
2024-01-04 01:57:49 +0100

    TextFieldSelectionHandle2, textFieldMagnifierNode. fix compilation

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text2/input/internal/selection/TextFieldSelectionHandle2.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text2/input/internal/selection/TextFieldSelectionHandle2.skiko.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.compose.foundation.text2.selection
+package androidx.compose.foundation.text2.input.internal.selection
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
