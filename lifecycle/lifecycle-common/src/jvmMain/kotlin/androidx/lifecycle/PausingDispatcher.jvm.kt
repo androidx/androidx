7845dbc09c2f96a3b59992b03ea57a945c8e1ad6
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-01-16 12:00:08 +0100

    Multiplatform support for lifecycle-common
    This change introduces multiplatform support for the lifecycle-common module by exposing the Lifecycle API to common, ensuring compatibility with published versions on Android. It will allow bring multiplatform support for dependent modules and use it as multiplatform abstraction for lifecycle events.
    
    Please note that `MainThread` annotation in common requires a new version of `androidx.annotation` package.
    
    Test: Run existing tests
    Change-Id: Iedc79ae83a3a9f2ed6b6de16f002a4c91abdf4f9

--- lifecycle/lifecycle-common/src/jvmMain/kotlin/androidx/lifecycle/PausingDispatcher.jvm.kt
+++ lifecycle/lifecycle-common/src/jvmMain/kotlin/androidx/lifecycle/PausingDispatcher.jvm.kt
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+@file:JvmName("PausingDispatcherKt")
+
 package androidx.lifecycle
 
 import kotlin.coroutines.CoroutineContext
---
+++ lifecycle/lifecycle-common/src/nativeMain/kotlin/androidx/lifecycle/Lifecycle.native.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.lifecycle
+
+import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.CoroutineScope
+
+public actual class AtomicReference<V> actual constructor(value: V) {
+    private val delegate = atomic(value)
+    public actual fun get() = delegate.value
+    public actual fun compareAndSet(expect: V, newValue: V) =
+        delegate.compareAndSet(expect, newValue)
+}
+
+public actual abstract class LifecycleCoroutineScope internal actual constructor() :
+    CoroutineScope {
+    internal actual abstract val lifecycle: Lifecycle
+}
