22d93d0227b60115561aacb0af20dbe363438f13
Halil Ozercan <halilibo@google.com>
2024-01-16 15:18:02 +0000

    Change readPlainText to be an extension on ClipEntry
    Test: TransferableContentTest
    Change-Id: If590fb6acfc5ea9f37fae7f5a02802eafcbadc2c

--- compose/foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/content/TransferableContent.android.kt
+++ compose/foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/content/TransferableContent.android.kt
@@ -21,6 +21,7 @@ import android.content.ClipDescription
 import android.net.Uri
 import android.os.Bundle
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.platform.toClipMetadata
 
@@ -110,18 +111,17 @@ actual fun TransferableContent.hasMediaType(mediaType: MediaType): Boolean {
     return clipMetadata.clipDescription.hasMimeType(mediaType.representation)
 }
 
-@OptIn(ExperimentalFoundationApi::class)
-internal actual fun TransferableContent.readPlainText(): String? {
+internal actual fun ClipEntry.readPlainText(): String? {
     var seenText = false
-    for (i in 0 until clipEntry.clipData.itemCount) {
-        seenText = seenText || (clipEntry.clipData.getItemAt(i).text != null)
+    for (i in 0 until clipData.itemCount) {
+        seenText = seenText || (clipData.getItemAt(i).text != null)
     }
     return if (seenText) {
         // note: text may be null, ensure this is null-safe
         buildString {
             var seenFirstItem = false
-            for (i in 0 until clipEntry.clipData.itemCount) {
-                clipEntry.clipData.getItemAt(i).text?.let { text ->
+            for (i in 0 until clipData.itemCount) {
+                clipData.getItemAt(i).text?.let { text ->
                     if (seenFirstItem) {
                         append("\n")
                     }
