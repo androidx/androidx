ab6663e9a6d02e99a4c00dcea02bbb47385e3c11
Clara Fok <clarafok@google.com>
2024-01-17 11:04:39 -0800

    Add new LoadStates and CombinedLoadStates API
    Added new isIdle and hasError APIS which returns whether the LoadStates or CombinedLoadStates is in NotLoading(idle) states or is in Error(hasError) states.
    
    Test: ANDROIDX_PROJECTS=INFRAROGUE ./gradlew paging:paging-common:allTest --no-configuration-cache
    Test: ./gradlew paging:paging-testing:test
    Bug: n/a
    Relnote: "Added new LoadStates and CombinedLoadStates helper methods to check if LoadStates is in Error or NotLoading state. Also added a new API that awaits on a LoadStateFlow until a load has settled into either NotLoading or Error state"
    Change-Id: Id6c67a6ba3805130d1a08840bad38e179604f786

--- paging/paging-testing/src/commonMain/kotlin/androidx/paging/testing/PagerFlowSnapshot.kt
+++ paging/paging-testing/src/commonMain/kotlin/androidx/paging/testing/PagerFlowSnapshot.kt
@@ -25,6 +25,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingData
 import androidx.paging.PagingDataEvent
 import androidx.paging.PagingDataPresenter
+import androidx.paging.awaitNotLoading
 import androidx.paging.testing.ErrorRecovery.RETRY
 import androidx.paging.testing.ErrorRecovery.RETURN_CURRENT_SNAPSHOT
 import androidx.paging.testing.ErrorRecovery.THROW
@@ -36,10 +37,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 
 /**
@@ -188,11 +186,9 @@ internal abstract class CompletablePagingDataPresenter<Value : Any>(
 internal suspend fun <Value : Any> CompletablePagingDataPresenter<Value>.awaitNotLoading(
     errorHandler: LoadErrorHandler
 ) {
-    val state = completableLoadStateFlow.filterNotNull().debounce(1).filter {
-        it.isIdle() || it.hasError()
-    }.firstOrNull()
+    val state = completableLoadStateFlow.filterNotNull().awaitNotLoading()
 
-    if (state != null && state.hasError()) {
+    if (state != null && state.hasError) {
         handleLoadError(state, errorHandler)
     }
 }
@@ -209,26 +205,6 @@ internal fun <Value : Any> PagingDataPresenter<Value>.handleLoadError(
     }
 }
 private class ReturnSnapshotStub : Exception()
-
-private fun CombinedLoadStates?.isIdle(): Boolean {
-    if (this == null) return false
-    return source.isIdle() && mediator?.isIdle() ?: true
-}
-
-private fun LoadStates.isIdle(): Boolean {
-    return refresh is LoadState.NotLoading && append is LoadState.NotLoading &&
-        prepend is LoadState.NotLoading
-}
-
-private fun CombinedLoadStates?.hasError(): Boolean {
-    if (this == null) return false
-    return source.hasError() || mediator?.hasError() ?: false
-}
-
-private fun LoadStates.hasError(): Boolean {
-    return refresh is LoadState.Error || append is LoadState.Error ||
-        prepend is LoadState.Error
-}
 
 private fun CombinedLoadStates.getErrorState(): LoadState.Error {
     return if (refresh is LoadState.Error) {
