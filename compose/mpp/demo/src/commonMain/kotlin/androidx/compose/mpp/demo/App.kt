3ca5625828f34c6c9962dd4d829dd8aa87996fb3
dima.avdeev <dima.avdeev@jetbrains.com>
2024-02-01 15:57:04 +0400

    platformLayers Demo app use cases (#1042)
    Collect some use cases for testing platfromLayers

--- compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/App.kt
+++ compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/App.kt
@@ -30,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.mpp.demo.bug.BugReproducers
 import androidx.compose.mpp.demo.components.Components
+import androidx.compose.mpp.demo.layers.PlatformLayers
 import androidx.compose.mpp.demo.textfield.android.AndroidTextFieldSamples
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -43,6 +44,7 @@ import androidx.compose.ui.unit.dp
 val MainScreen = Screen.Selection(
     "Demo",
     Components,
+    PlatformLayers,
     BugReproducers,
     Screen.Example("Example1") { Example1() },
     Screen.Example("ImageViewer") { ImageViewer() },
