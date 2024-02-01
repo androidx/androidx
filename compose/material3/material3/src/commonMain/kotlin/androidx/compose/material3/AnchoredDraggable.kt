ca76bb51d501749b4a07a41961140aff1981dcd6
Igor Demin <igor.demin@jetbrains.com>
2024-02-01 01:25:33 +0100

    material3. implement `PlatformOptimizedCancellationException`
    It is copied from `material` module

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/AnchoredDraggable.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/AnchoredDraggable.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.internal.PlatformOptimizedCancellationException
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -720,12 +721,7 @@ internal object AnchoredDraggableDefaults {
     val AnimationSpec = SpringSpec<Float>()
 }
 
-private class AnchoredDragFinishedSignal : CancellationException() {
-    override fun fillInStackTrace(): Throwable {
-        stackTrace = emptyArray()
-        return this
-    }
-}
+private class AnchoredDragFinishedSignal : PlatformOptimizedCancellationException()
 
 private suspend fun <I> restartable(inputs: () -> I, block: suspend (I) -> Unit) {
     try {
