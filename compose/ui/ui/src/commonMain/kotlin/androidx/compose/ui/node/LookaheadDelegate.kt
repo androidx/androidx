harryzz/compose-multiplatform-core

Add check for layout with large dimensions.
Bug: 316365522

When a layout uses extremely large values in its calculated
size, such as Constraints.Infinity, it can cause problems
in other layouts. It is very difficult to track down the
source of the problem layout, as the problem is usually
detected in a parent when it tries to use the large
dimension in Constraints.

This CL adds a check whenever a child uses layout()
to ensure that the dimension isn't absurdly large.
This should eliminate any common problem with using
Constraints.Infinity, such as directly using
Constraints.maxWidth or a calculation from a maximum
dimension.

Test: ran existing layout tests. Created new tests.
Change-Id: I339a906701f382f7e043be65de57af6b9838e695

compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LookaheadDelegate.kt
@@ -21,6 +21,7 @@ import androidx.collection.MutableScatterMap
 import androidx.collection.MutableScatterSet
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.GraphicsLayerScope
+import androidx.compose.ui.internal.checkPrecondition
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.LookaheadLayoutCoordinates
@@ -193,14 +194,21 @@ internal abstract class LookaheadCapablePlaceable : Placeable(), MeasureScopeWit
         alignmentLines: Map<AlignmentLine, Int>,
         rulers: (RulerScope.() -> Unit)?,
         placementBlock: PlacementScope.() -> Unit
-    ): MeasureResult = object : MeasureResult {
-        override val width: Int = width
-        override val height: Int = height
-        override val alignmentLines: Map<AlignmentLine, Int> = alignmentLines
-        override val rulers: (RulerScope.() -> Unit)? = rulers
+    ): MeasureResult {
+        checkMeasuredSize(width, height)
+        return object : MeasureResult {
+            override val width: Int
+                get() = width
+            override val height: Int
+                get() = height
+            override val alignmentLines: Map<AlignmentLine, Int>
+                get() = alignmentLines
+            override val rulers: (RulerScope.() -> Unit)?
+                get() = rulers
 
-        override fun placeChildren() {
-            placementScope.placementBlock()
+            override fun placeChildren() {
+                placementScope.placementBlock()
+            }
         }
     }
 
@@ -308,6 +316,19 @@ private data class PlaceableResult(
 ) : OwnerScope {
     override val isValidOwnerScope: Boolean
         get() = placeable.coordinates.isAttached
+}
+
+// This is about 16 million pixels. That should be big enough. We'll treat anything bigger as an
+// error.
+private const val MaxLayoutDimension = (1 shl 24) - 1
+private const val MaxLayoutMask: Int = 0xFF00_0000.toInt()
+
+@Suppress("NOTHING_TO_INLINE")
+internal inline fun checkMeasuredSize(width: Int, height: Int) {
+    checkPrecondition(width and MaxLayoutMask == 0 && height and MaxLayoutMask == 0) {
+        "Size($width x $height) is out of range. Each dimension must be between 0 and " +
+            "$MaxLayoutDimension."
+    }
 }
 
 internal abstract class LookaheadDelegate(

androidx/androidx

(cherry picked from https://android-review.googlesource.com/q/commit:f042e370cabf95810a698c072aeced0ee7f0d2a0)
(cherry picked from https://android-review.googlesource.com/q/commit:41f95616788460852b4f06407a10feda2271b59b)
Merged-In: I339a906701f382f7e043be65de57af6b9838e695

compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LookaheadDelegate.kt
@@ -93,15 +93,33 @@ internal abstract class LookaheadCapablePlaceable : Placeable(), MeasureScopeWit
         height: Int,
         alignmentLines: Map<AlignmentLine, Int>,
         placementBlock: PlacementScope.() -> Unit
-    ): MeasureResult = object : MeasureResult {
-        override val width: Int
-            get() = width
-        override val height: Int
-            get() = height
-        override val alignmentLines: Map<AlignmentLine, Int>
-            get() = alignmentLines
+    ): MeasureResult {
+        checkMeasuredSize(width, height)
+        return object : MeasureResult {
+            override val width: Int
+                get() = width
+            override val height: Int
+                get() = height
+            override val alignmentLines: Map<AlignmentLine, Int>
+                get() = alignmentLines
 
-        override fun placeChildren() = placementScope.placementBlock()
+            override fun placeChildren() {
+                placementScope.placementBlock()
+            }
+        }
+    }
+}
+
+// This is about 16 million pixels. That should be big enough. We'll treat anything bigger as an
+// error.
+private const val MaxLayoutDimension = (1 shl 24) - 1
+private const val MaxLayoutMask: Int = 0xFF00_0000.toInt()
+
+@Suppress("NOTHING_TO_INLINE")
+internal inline fun checkMeasuredSize(width: Int, height: Int) {
+    check(width and MaxLayoutMask == 0 && height and MaxLayoutMask == 0) {
+        "Size($width x $height) is out of range. Each dimension must be between 0 and " +
+            "$MaxLayoutDimension."
     }
 }
 
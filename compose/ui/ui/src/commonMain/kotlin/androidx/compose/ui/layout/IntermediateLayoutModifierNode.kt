e74af5db9d9f23e962b70a70b702c7b8ab60dcc1
George Mount <mount@google.com>
2024-01-09 10:10:32 -0800

    Add check for layout with large dimensions.
    Bug: 316365522
    
    When a layout uses extremely large values in its calculated
    size, such as Constraints.Infinity, it can cause problems
    in other layouts. It is very difficult to track down the
    source of the problem layout, as the problem is usually
    detected in a parent when it tries to use the large
    dimension in Constraints.
    
    This CL adds a check whenever a child uses layout()
    to ensure that the dimension isn't absurdly large.
    This should eliminate any common problem with using
    Constraints.Infinity, such as directly using
    Constraints.maxWidth or a calculation from a maximum
    dimension.
    
    Test: ran existing layout tests. Created new tests.
    (cherry picked from https://android-review.googlesource.com/q/commit:f042e370cabf95810a698c072aeced0ee7f0d2a0)
    (cherry picked from https://android-review.googlesource.com/q/commit:41f95616788460852b4f06407a10feda2271b59b)
    Merged-In: I339a906701f382f7e043be65de57af6b9838e695
    Change-Id: I339a906701f382f7e043be65de57af6b9838e695

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/layout/IntermediateLayoutModifierNode.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/layout/IntermediateLayoutModifierNode.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.GraphicsLayerScope
 import androidx.compose.ui.node.LayoutModifierNode
 import androidx.compose.ui.node.NodeMeasuringIntrinsics
 import androidx.compose.ui.node.Nodes
+import androidx.compose.ui.node.checkMeasuredSize
 import androidx.compose.ui.node.requireLayoutNode
 import androidx.compose.ui.node.visitAncestors
 import androidx.compose.ui.unit.Constraints
@@ -287,12 +288,15 @@ internal class IntermediateLayoutModifierNode(
             height: Int,
             alignmentLines: Map<AlignmentLine, Int>,
             placementBlock: Placeable.PlacementScope.() -> Unit
-        ) = object : MeasureResult {
-            override val width = width
-            override val height = height
-            override val alignmentLines = alignmentLines
-            override fun placeChildren() {
-                coordinator!!.placementScope.placementBlock()
+        ): MeasureResult {
+            checkMeasuredSize(width, height)
+            return object : MeasureResult {
+                override val width = width
+                override val height = height
+                override val alignmentLines = alignmentLines
+                override fun placeChildren() {
+                    coordinator!!.placementScope.placementBlock()
+                }
             }
         }
 
