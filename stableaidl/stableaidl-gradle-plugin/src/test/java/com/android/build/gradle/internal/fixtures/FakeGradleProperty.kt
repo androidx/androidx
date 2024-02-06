9fbcda9925d6386e6453c97ff3f0c7e848c44d47
Aurimas Liutikas <aurimas@google.com>
2024-02-06 13:05:15 -0800

    Upgradle to 8.6 stable
    Test: ./gradlew bOS --dry-run
    Change-Id: I97c4a4bba66fa3aa44872773b3de71fed7a6f981

--- stableaidl/stableaidl-gradle-plugin/src/test/java/com/android/build/gradle/internal/fixtures/FakeGradleProperty.kt
+++ stableaidl/stableaidl-gradle-plugin/src/test/java/com/android/build/gradle/internal/fixtures/FakeGradleProperty.kt
@@ -108,10 +108,6 @@ class FakeGradleProperty<T>(private var value: T? = null) : Property<T> {
         throw NotImplementedError()
     }
 
-    override fun update(transform: Transformer<out Provider<out T>?, in Provider<T>>) {
-        throw NotImplementedError()
-    }
-
     override fun <U : Any?, R : Any?> zip(
         p0: Provider<U>,
         p1: BiFunction<in T, in U, out R>
