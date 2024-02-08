f6a7a2445f68952bbc719741b0bfb8ebbfc8d6fa
Jose Figueroa <serniebanders@google.com>
2024-02-08 12:50:56 -0500

    [Material3][Card] Update ElevatedCard and FilledCard to leverage SurfaceContainerLow and SurfaceContainerHighest color role
    RelNote: ElevatedCard container color is now SurfaceContainerLow. FilledCards container color of SurfaceContainerHighest.
    Test: Updated screenshots
    Bug: 324079909
    Change-Id: I35141af0d7a069353309f267143e5808a9c0afca

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/FilledCardTokens.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/FilledCardTokens.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// VERSION: v0_103
+// VERSION: v0_210
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 package androidx.compose.material3.tokens
@@ -21,14 +21,15 @@ package androidx.compose.material3.tokens
 import androidx.compose.ui.unit.dp
 
 internal object FilledCardTokens {
-    val ContainerColor = ColorSchemeKeyTokens.SurfaceVariant
+    val ContainerColor = ColorSchemeKeyTokens.SurfaceContainerHighest
     val ContainerElevation = ElevationTokens.Level0
     val ContainerShape = ShapeKeyTokens.CornerMedium
     val DisabledContainerColor = ColorSchemeKeyTokens.SurfaceVariant
     val DisabledContainerElevation = ElevationTokens.Level0
-    const val DisabledContainerOpacity = 0.38f
+    val DisabledContainerOpacity = 0.38f
     val DraggedContainerElevation = ElevationTokens.Level3
     val FocusContainerElevation = ElevationTokens.Level0
+    val FocusIndicatorColor = ColorSchemeKeyTokens.Secondary
     val HoverContainerElevation = ElevationTokens.Level1
     val IconColor = ColorSchemeKeyTokens.Primary
     val IconSize = 24.0.dp
