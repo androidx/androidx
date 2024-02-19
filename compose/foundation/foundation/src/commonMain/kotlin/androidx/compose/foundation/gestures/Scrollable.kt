c31d655bbbacc20adae6208e89aeec626d76720f
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2024-02-19 12:17:16 +0100

    Revert usage of CancellationException in Pager (#1115)
    Revert change by using CancellationException instead of
    ScrollCancellationException in Pager component

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
@@ -949,9 +949,6 @@ internal class DefaultFlingBehavior(
                         lastAnimationCycleCount++
                     }
                 } catch (exception: CancellationException) {
-                    // This is where the ScrollCancellationException should be caught,
-                    // but the parent class CancellationException is caught here for
-                    // backwards compatibility.
                     velocityLeft = animationState.velocity
                 }
                 velocityLeft
@@ -1002,5 +999,3 @@ internal val UnityDensity = object : Density {
     override val fontScale: Float
         get() = 1f
 }
-
-internal class ScrollCancellationException(message: String?): CancellationException(message)
