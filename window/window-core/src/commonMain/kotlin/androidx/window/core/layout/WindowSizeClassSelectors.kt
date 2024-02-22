436f58ade392a308e75a16c2c324fe255d45852c
Diego Vela <diegovela@google.com>
2024-02-22 22:28:31 +0000

    Split WindowSizeClassUtil to focused methods.
    Split WindowSizeClassUtil into two classes.
    Extract selector functions to WindowSizeClassSelectors.
    Extract scoring functions to WindowSizeClassScoreCalculator.
    Refactor tests to match the class structure.
    
    Relnote: Split WindowSizeClassUtil.
    Bug: 324092710
    Test: ./gradlew window:window-core:test
    Change-Id: Ie929241cd5acc56ecf352590f03374f2165b4e31

--- window/window-core/src/commonMain/kotlin/androidx/window/core/layout/WindowSizeClassSelectors.kt
+++ window/window-core/src/commonMain/kotlin/androidx/window/core/layout/WindowSizeClassSelectors.kt
@@ -13,63 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:JvmName("WindowSizeClassUtil")
+@file:JvmName("WindowSizeClassSelectors")
 package androidx.window.core.layout
-
-import androidx.window.core.ExperimentalWindowCoreApi
-
-/**
- * A scoring function to calculate how close the width of a [WindowSizeClass] is to [widthDp]
- * without exceeding it.
- *
- * @param widthDp the width bound to try to match.
- * @return an integer from -1 to [Integer.MAX_VALUE] where a larger value indicates a better match.
- */
-@ExperimentalWindowCoreApi
-fun WindowSizeClass.scoreWithinWidthDp(widthDp: Int): Int {
-    return if (this.widthDp <= widthDp) {
-        Integer.MAX_VALUE / (1 + widthDp - this.widthDp)
-    } else {
-        -1
-    }
-}
-
-/**
- * A scoring function to calculate how close the height of a [WindowSizeClass] is to [heightDp]
- * without exceeding it.
- *
- * @param heightDp the height bound to try to match.
- * @return an integer from -1 to [Integer.MAX_VALUE] where a larger value indicates a better match.
- */
-@ExperimentalWindowCoreApi
-fun WindowSizeClass.scoreWithinHeightDp(heightDp: Int): Int {
-    return if (this.heightDp <= heightDp) {
-        Integer.MAX_VALUE / (1 + heightDp - this.heightDp)
-    } else {
-        -1
-    }
-}
-
-/**
- * A scoring function to calculate how close the area of a [WindowSizeClass] is to the area of a
- * window without exceeding it.
- *
- * @param windowWidthDp the width of a window constraint.
- * @param windowHeightDp the height of a window constraint.
- *
- * @return an integer from -1 to [Integer.MAX_VALUE] where a larger value indicates a better match.
- */
-@ExperimentalWindowCoreApi
-fun WindowSizeClass.scoreWithinAreaBounds(
-    windowWidthDp: Int,
-    windowHeightDp: Int
-): Int {
-    if (windowWidthDp < this.widthDp || windowHeightDp < this.heightDp) {
-        return -1
-    }
-    val areaDifference = windowWidthDp * windowHeightDp - this.widthDp * this.heightDp
-    return Integer.MAX_VALUE / (1 + areaDifference)
-}
 
 /**
  * Calculates which [WindowSizeClass] has the closest matching [windowWidthDp] within the given
