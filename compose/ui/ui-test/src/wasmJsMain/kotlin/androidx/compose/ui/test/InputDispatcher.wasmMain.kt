c148554b2f6fbc68fbcebf27b90a33ba1c7ddd77
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-02-29 11:15:39 +0100

    Support sendKey test utility function on wasm target (#1150)

--- compose/ui/ui-test/src/wasmJsMain/kotlin/androidx/compose/ui/test/InputDispatcher.wasmMain.kt
+++ compose/ui/ui-test/src/wasmJsMain/kotlin/androidx/compose/ui/test/InputDispatcher.wasmMain.kt
@@ -13,13 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:OptIn(ExperimentalTestApi::class)
 
 package androidx.compose.ui.test
 
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
+import org.jetbrains.skiko.SkikoInputModifiers
+import org.jetbrains.skiko.SkikoKey
+import org.jetbrains.skiko.SkikoKeyboardEvent
+import org.jetbrains.skiko.SkikoKeyboardEventKind
+import org.w3c.dom.events.KeyboardEvent
+import org.w3c.dom.events.KeyboardEventInit
 
 /**
  * The [KeyEvent] is usually created by the system. This function creates an instance of
@@ -27,6 +32,65 @@ import androidx.compose.ui.input.key.KeyEventType
  */
 internal actual fun keyEvent(
     key: Key, keyEventType: KeyEventType, modifiers: Int
-): KeyEvent = TODO()
+): KeyEvent {
+    val nativeCode = key.keyCode.toInt()
 
-internal actual fun Int.updatedKeyboardModifiers(key: Key, down: Boolean): Int = this // TODO: implement updatedKeyboardModifiers
+    val kind = when (keyEventType) {
+        KeyEventType.KeyUp -> SkikoKeyboardEventKind.UP
+        KeyEventType.KeyDown -> SkikoKeyboardEventKind.DOWN
+        else -> SkikoKeyboardEventKind.UNKNOWN
+    }
+
+    return KeyEvent(
+        SkikoKeyboardEvent(
+            kind = kind,
+            key = SkikoKey.valueOf(nativeCode),
+            modifiers = SkikoInputModifiers(modifiers),
+            platform = KeyboardEvent(
+                when (kind) {
+                    SkikoKeyboardEventKind.DOWN -> "keydown"
+                    SkikoKeyboardEventKind.UP -> "keyup"
+                    else -> "keypress"
+                }, KeyboardEventInit(
+                    key = if (key.isPrintable()) nativeCode.toChar().toString() else "Unknown"
+                )
+            )
+        )
+    )
+}
+
+// Basically this is what we've tried to avoid in isTypedEvent implementation, but in tests it's totally fine
+// Just add any non-printable key you need for tests here
+private val NonPrintableKeys = setOf(
+    Key.AltLeft,
+    Key.MetaLeft,
+    Key.ShiftLeft,
+    Key.CtrlLeft,
+    Key.Delete,
+    Key.DirectionLeft,
+    Key.DirectionRight,
+    Key.DirectionDown,
+    Key.DirectionUp,
+    Key.Home,
+    Key.MoveEnd
+)
+
+private fun Key.isPrintable(): Boolean {
+    return !NonPrintableKeys.contains(this)
+}
+
+internal actual fun Int.updatedKeyboardModifiers(key: Key, down: Boolean): Int {
+    val mask = when (key) {
+        Key.ShiftLeft, Key.ShiftRight -> SkikoInputModifiers.SHIFT.value
+        Key.CtrlLeft, Key.CtrlRight -> SkikoInputModifiers.CONTROL.value
+        Key.MetaLeft, Key.MetaRight -> SkikoInputModifiers.META.value
+        Key.AltLeft, Key.AltRight -> SkikoInputModifiers.ALT.value
+        else -> null
+    }
+
+    return if (mask != null) {
+        if (down) this or mask else this xor mask
+    } else {
+        this
+    }
+}
