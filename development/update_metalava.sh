70a1e2315eb6a8fe6fe4ec6bbe41f094b72c9e6d
Jeff Gaston <jeffrygaston@google.com>
2024-03-13 11:51:00 -0400

    update to update_metalava.sh
    import_maven_artifacts.py recommends using importMaven.sh instead
    
    Bug: 321970847
    Test: ./development/update_metalava.sh 1.0.0-alpha10 11569610
    Change-Id: Ia3a1801ca9ad78993cfeb6ecf3b9f560dda95d78

--- development/update_metalava.sh
+++ development/update_metalava.sh
@@ -6,12 +6,12 @@ METALAVA_VERSION="$1"
 METALAVA_BUILD_ID="$2"
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [ -z "$METALAVA_VERSION" ]; then
+if [ -z "$METALAVA_BUILD_ID" ]; then
     echo "You must specify a build id for Metalava: ./updateMetalava.sh <metalava_version> <build_id>"
     exit 1
 fi
 
-python3 "$SCRIPT_PATH"/importMaven/import_maven_artifacts.py -n com.android.tools.metalava:metalava:"$METALAVA_VERSION" -mb "$METALAVA_BUILD_ID"
+"$SCRIPT_PATH"/importMaven/importMaven.sh com.android.tools.metalava:metalava:"$METALAVA_VERSION" --metalava-build-id "$METALAVA_BUILD_ID" --redownload
 sed -i "s/\(androidx\.playground\.metalavaBuildId=\)[0-9]*/\1$METALAVA_BUILD_ID/g" "$SCRIPT_PATH"/../playground-common/playground.properties
 sed -i "s/^\\(metalava\\s*=.*version\\s*=\\s*\"\\)[^\"]*/\\1$METALAVA_VERSION/" "$SCRIPT_PATH/../gradle/libs.versions.toml"
 
