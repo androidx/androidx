androidx/androidx

commit 339dbd38be0da2bed1189a9b3a78a2be36bacb02

Fix render order of interop views (#1145)
## Proposed Changes

Currently on both Desktop and iOS interop views are added to the view
hierarchy in order to add nodes to Compose. It works only if all
intersecting interop views were added at the same time (frame). So it's
basically last-added - above-displayed. This PR changes this behavior in
a way that it will respect the order inside Compose like regular compose
elements.

**It does NOT make any changes in the ability to display Compose content
above interop view on Desktop**, this fix was made in #915

Main changes:
- Unify a way to work with interop on Desktop (`SwingPanel`) and iOS
(`UIKitView`)
- `LocalInteropContainer` -> `LocalUIKitInteropContainer` on iOS
- `LocalLayerContainer` -> `LocalSwingInteropContainer` on Desktop
- Reduce copy-pasting by moving `OverlayLayout` and `EmptyLayout`
- Remove overriding `add` method on `ComposePanel` and
`ComposeWindowPanel` - it was required to redirect interop, but now it's
not required and it's better to avoid changing default AWT hierarchy
behaviour
- Do not use `JLayeredPane`'s layers anymore - it brings a lot of
transparency issues (faced with it on Windows too after unrelated
change). Sorting via indexes is used instead
- Add `InteropOrder` page to mpp demo

### How it works

It utilizes `TraversableNode` to traverse the tree in the right order
and calculate the index based on interop views count that placed before
the current node in the hierarchy. All interop nodes are marked via
`Modifier.trackSwingInterop`/`Modifier.trackUIKitInterop` modifier to
filter them from the `LayoutNode`s tree.

## Testing

Test: run reproducers from the issues or look at "InteropOrder" page in
mpp demo

Desktop | iOS
--- | ---
<img width="400" alt="Screenshot 2024-02-27 at 12 51 06"
src="https://github.com/JetBrains/compose-multiplatform-core/assets/1836384/534cbdc8-9671-4ab7-bd6d-b577d2004d1b">

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/EmptyLayout.skiko.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,21 @@
  * limitations under the License.
  */
 
-package androidx.compose.ui.awt
+package androidx.compose.ui.layout
 
-import androidx.compose.runtime.staticCompositionLocalOf
-import java.awt.Container
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
-internal val LocalLayerContainer = staticCompositionLocalOf<Container> {
-    error("CompositionLocal LayerContainer not provided")
-}
+/**
+ * Creates a layout with no content. It's usually used to add modifier to the hierarchy.
+ *
+ * @param modifier The modifier to be applied to the layout.
+ */
+@Composable
+internal fun EmptyLayout(modifier: Modifier) = Layout(
+    content = {},
+    modifier = modifier,
+    measurePolicy = { _, _ ->
+        layout(0, 0) {}
+    }
+)

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/OverlayLayout.skiko.kt
@@ -1,0 +1,43 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.layout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Creates an overlay layout that places the content on top of each other.
+ *
+ * @param modifier The modifier to be applied to the layout.
+ * @param content The composable content to be placed in the overlay layout.
+ */
+@Composable
+internal fun OverlayLayout(modifier: Modifier, content: @Composable () -> Unit) = Layout(
+    content = content,
+    modifier = modifier,
+    measurePolicy = { measurables, constraints ->
+        val placeables = measurables.map { it.measure(constraints) }
+        layout(
+            placeables.maxOfOrNull { it.width } ?: 0,
+            placeables.maxOfOrNull { it.height } ?: 0
+        ) {
+            placeables.forEach {
+                it.place(0, 0)
+            }
+        }
+    }
+)

compose/mpp/demo/build.gradle.kts
@@ -161,6 +161,7 @@ kotlin {
                 implementation(project(":compose:ui:ui"))
                 implementation(project(":compose:ui:ui-graphics"))
                 implementation(project(":compose:ui:ui-text"))
+                implementation(libs.kotlinStdlib)
                 implementation(libs.kotlinCoroutinesCore)
                 api(libs.kotlinSerializationCore)
             }
@@ -181,19 +182,18 @@ kotlin {
             }
         }
 
-        val jsMain by getting {
+        val webMain by creating {
             dependsOn(skikoMain)
             resources.setSrcDirs(resources.srcDirs)
             resources.srcDirs(unzipTask.map { it.destinationDir })
         }
 
+        val jsMain by getting {
+            dependsOn(webMain)
+        }
+
         val wasmJsMain by getting {
-            dependsOn(skikoMain)
-            resources.setSrcDirs(resources.srcDirs)
-            resources.srcDirs(unzipTask.map { it.destinationDir })
-            dependencies {
-                implementation(libs.kotlinStdlib)
-            }
+            dependsOn(webMain)
         }
 
         val nativeMain by creating { dependsOn(skikoMain) }

compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/App.kt
@@ -54,6 +54,7 @@ val MainScreen = Screen.Selection(
     Screen.Example("GraphicsLayerSettings") { GraphicsLayerSettings() },
     Screen.Example("Blending") { Blending() },
     Screen.Example("FontRasterization") { FontRasterization() },
+    Screen.Example("InteropOrder") { InteropOrder() },
     AndroidTextFieldSamples,
 )
 

compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/InteropOrder.kt
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.mpp.demo
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InteropOrder() {
+    var red by remember { mutableStateOf(true) }
+    var green by remember { mutableStateOf(true) }
+    var blue by remember { mutableStateOf(true) }
+    Column(
+        modifier = Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Button(onClick = { red = !red }) {
+            Text("Red")
+        }
+        Button(onClick = { green = !green }) {
+            Text("Green")
+        }
+        Button(onClick = { blue = !blue }) {
+            Text("Blue")
+        }
+
+        Box {
+            if (red) {
+                TestInteropView(Modifier.size(150.dp).offset(0.dp, 0.dp), Color.Red)
+            }
+            if (green) {
+                TestInteropView(Modifier.size(150.dp).offset(75.dp, 75.dp), Color.Green)
+            }
+            if (blue) {
+                TestInteropView(Modifier.size(150.dp).offset(150.dp, 150.dp), Color.Blue)
+            }
+        }
+    }
+}
+
+@Composable
+internal expect fun TestInteropView(modifier: Modifier, color: Color)

compose/mpp/demo/src/desktopMain/kotlin/androidx/compose/mpp/demo/InteropOrder.desktop.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.mpp.demo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
+import androidx.compose.ui.graphics.Color
+import javax.swing.JPanel
+
+@Composable
+internal actual fun TestInteropView(modifier: Modifier, color: Color) {
+    SwingPanel(
+        background = color,
+        factory = { JPanel().apply { background = color.toAwtColor() } },
+        modifier = modifier
+    )
+}
+
+private fun Color.toAwtColor() = java.awt.Color(red, green, blue, alpha)

compose/mpp/demo/src/macosMain/kotlin/androidx/compose/mpp/demo/InteropOrder.macos.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,15 @@
  * limitations under the License.
  */
 
-package androidx.compose.ui.interop
+package androidx.compose.mpp.demo
 
-import androidx.compose.runtime.staticCompositionLocalOf
-import platform.UIKit.UIView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-internal val LocalInteropContainer = staticCompositionLocalOf<UIView> {
-    error("CompositionLocal LayerContainer not provided")
+@Composable
+internal actual fun TestInteropView(modifier: Modifier, color: Color) {
+    Box(modifier.background(color)) // TODO
 }

compose/mpp/demo/src/uikitMain/kotlin/UIKitViewOrder.kt
@@ -1,52 +1,0 @@
-/*
- * Copyright 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.mpp.demo.Screen
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.interop.UIKitView
-import androidx.compose.ui.unit.dp
-import platform.UIKit.UIColor
-import platform.UIKit.UIView
-
-/**
- * Issue https://github.com/JetBrains/compose-multiplatform/issues/4004
- */
-val UIKitViewOrder = Screen.Example("UIKitViewOrder") {
-    Box(modifier = Modifier.fillMaxSize().background(Color.Yellow)) {
-        UIKitView(
-            factory = {
-                UIView().apply {
-                    backgroundColor = UIColor.blueColor
-                }
-            },
-            modifier = Modifier.size(100.dp),
-        )
-        UIKitView(
-            factory = {
-                UIView().apply {
-                    backgroundColor = UIColor.redColor
-                }
-            },
-            modifier = Modifier.size(100.dp).offset(50.dp, 50.dp),
-        )
-    }
-}

commit 0b8d76de34e68f50ed876a9e007e5e89679296cc

Fix zero size layout in iOS interop (#1194)
## Proposed Changes

Regression after #1145 (more specific is -
https://github.com/JetBrains/compose-multiplatform-core/pull/1145/commits/156b2ee13ec8bd513d62b9e9b7d3ac788641aa22)
- it was replaced by a zero-size layout that prevented drawing anything
and might lead to crashes in native views due to a zero size.
Please note that before we had incorrect/inconsistent behavior - iOS
interop occupied max available space. This PR aligns behaviour with
[`AndroidView`](https://github.com/JetBrains/compose-multiplatform-core/blob/v1.6.0/compose/ui/ui/src/androidMain/kotlin/androidx/compose/ui/viewinterop/AndroidViewHolder.android.kt#L387)
and just
[`Box`](https://github.com/JetBrains/compose-multiplatform-core/blob/v1.6.0/compose/foundation/foundation-layout/src/commonMain/kotlin/androidx/compose/foundation/layout/Box.kt#L212-L214)

## Testing

Test: mpp demo, pages with interop

## Issues Fixed

Fixes https://github.com/JetBrains/compose-multiplatform/issues/4447

compose/mpp/demo/src/uikitMain/kotlin/bugs/ComposeAndNativeScroll.kt
@@ -81,7 +81,7 @@ val ComposeAndNativeScroll = Screen.Example("ScrollDraggingTest") {
         Spacer(Modifier.height(10.dp))
         Text("UIKit:")
         Box(modifier = Modifier.height(ItemsHeight.dp)) {
-            UIKitView(::makeScrollView, Modifier.height(ItemsHeight.dp))
+            UIKitView(::makeScrollView, Modifier.fillMaxSize())
         }
     }
 }

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/EmptyLayout.skiko.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.Modifier
 internal fun EmptyLayout(modifier: Modifier) = Layout(
     content = {},
     modifier = modifier,
-    measurePolicy = { _, _ ->
-        layout(0, 0) {}
+    measurePolicy = { _, constraints ->
+        layout(constraints.minWidth, constraints.minHeight) {}
     }
 )

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/OverlayLayout.skiko.kt
@@ -32,8 +32,8 @@ internal fun OverlayLayout(modifier: Modifier, content: @Composable () -> Unit) 
     measurePolicy = { measurables, constraints ->
         val placeables = measurables.map { it.measure(constraints) }
         layout(
-            placeables.maxOfOrNull { it.width } ?: 0,
-            placeables.maxOfOrNull { it.height } ?: 0
+            placeables.maxOfOrNull { it.width } ?: constraints.minWidth,
+            placeables.maxOfOrNull { it.height } ?: constraints.minHeight
         ) {
             placeables.forEach {
                 it.place(0, 0)