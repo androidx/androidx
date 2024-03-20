f1ed7cac31f3f730bc146a0419faf11bd7e54a59
Aurimas Liutikas <aurimas@google.com>
2024-03-20 18:41:09 +0000

    Revert "Rename source from `viewmodel-compose` to follow KMP con..."
    Revert submission 3007635-vm-compose-kmp
    
    Reason for revert: broke the build
    https://android-build.corp.google.com/artifact/submitted/11602986/androidx/latest/view/logs%2Fbuild_error.log
    
    Reverted changes: /q/submissionid:3007635-vm-compose-kmp
    
    Change-Id: I8d9624596d74a36bd8f1d7b4945ad7d08e18709c

--- lifecycle/lifecycle-viewmodel-compose/src/androidMain/kotlin/androidx/lifecycle/viewmodel/compose/ViewModel.kt
+++ lifecycle/lifecycle-viewmodel-compose/src/androidMain/kotlin/androidx/lifecycle/viewmodel/compose/ViewModel.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:JvmName("ViewModelKt")
 
 package androidx.lifecycle.viewmodel.compose
 
