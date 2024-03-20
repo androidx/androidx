382353f82f49b3f8744daa02fd144890dad6ea24
Aurimas Liutikas <aurimas@google.com>
2024-03-20 18:41:09 +0000

    Revert "Move `LocalViewModelStoreOwner` to common on `viewmodel-..."
    Revert submission 3007635-vm-compose-kmp
    
    Reason for revert: broke the build
    https://android-build.corp.google.com/artifact/submitted/11602986/androidx/latest/view/logs%2Fbuild_error.log
    
    Reverted changes: /q/submissionid:3007635-vm-compose-kmp
    
    Change-Id: Ib136a0f210d2871641b454b373ab56169c42ed1e

--- lifecycle/lifecycle-viewmodel-compose/src/androidMain/kotlin/androidx/lifecycle/viewmodel/compose/LocalViewModelStoreOwner.android.kt
+++ lifecycle/lifecycle-viewmodel-compose/src/androidMain/kotlin/androidx/lifecycle/viewmodel/compose/LocalViewModelStoreOwner.android.kt
@@ -17,10 +17,36 @@
 package androidx.lifecycle.viewmodel.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.platform.LocalView
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
-@Composable
-internal actual fun findViewTreeViewModelStoreOwner(): ViewModelStoreOwner? =
-    LocalView.current.findViewTreeViewModelStoreOwner()
+/**
+ * The CompositionLocal containing the current [ViewModelStoreOwner].
+ */
+public object LocalViewModelStoreOwner {
+    private val LocalViewModelStoreOwner =
+        compositionLocalOf<ViewModelStoreOwner?> { null }
+
+    /**
+     * Returns current composition local value for the owner or `null` if one has not
+     * been provided nor is one available via [findViewTreeViewModelStoreOwner] on the
+     * current [LocalView].
+     */
+    public val current: ViewModelStoreOwner?
+        @Composable
+        get() = LocalViewModelStoreOwner.current
+            ?: LocalView.current.findViewTreeViewModelStoreOwner()
+
+    /**
+     * Associates a [LocalViewModelStoreOwner] key to a value in a call to
+     * [CompositionLocalProvider].
+     */
+    public infix fun provides(viewModelStoreOwner: ViewModelStoreOwner):
+        ProvidedValue<ViewModelStoreOwner?> {
+            return LocalViewModelStoreOwner.provides(viewModelStoreOwner)
+        }
+}
