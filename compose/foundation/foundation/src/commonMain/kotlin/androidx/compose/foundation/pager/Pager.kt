228152c5a5e7fd38a56c0bf8dc63b98c9da56177
Igor Demin <igordmn@gmail.com>
2024-03-27 14:55:10 +0100

    Pager. Fix compilation of CancelationException

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/pager/Pager.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/pager/Pager.kt
@@ -896,7 +896,7 @@ private class DefaultPagerNestedScrollConnection(
         source: NestedScrollSource
     ): Offset {
         if (source == NestedScrollSource.Fling && available.mainAxis() != 0f) {
-            throw CancellationException()
+            throw CancellationException("End of scrollable area reached")
         }
         return Offset.Zero
     }
