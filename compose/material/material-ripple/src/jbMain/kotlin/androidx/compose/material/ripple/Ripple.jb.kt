c9ea696b5ed77ca5c0f25e2aed7c53a6f13682c9
Igor Demin <igordmn@gmail.com>
2024-04-08 12:52:59 +0200

    material. Implement createPlatformRippleNode

--- compose/material/material-ripple/src/jbMain/kotlin/androidx/compose/material/ripple/Ripple.jb.kt
+++ compose/material/material-ripple/src/jbMain/kotlin/androidx/compose/material/ripple/Ripple.jb.kt
@@ -17,6 +17,24 @@
 
 package androidx.compose.material.ripple
 
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.ui.graphics.ColorProducer
+import androidx.compose.ui.node.DelegatableNode
+import androidx.compose.ui.unit.Dp
+
+/**
+ * Desktop ripple implementation using the Compose-rendered [CommonRippleNode] implementation.
+ */
+internal actual fun createPlatformRippleNode(
+    interactionSource: InteractionSource,
+    bounded: Boolean,
+    radius: Dp,
+    color: ColorProducer,
+    rippleAlpha: () -> RippleAlpha
+): DelegatableNode {
+    return CommonRippleNode(interactionSource, bounded, radius, color, rippleAlpha)
+}
+
 /**
  * Desktop ripple implementation using the Compose-rendered [CommonRipple] implementation.
  */
