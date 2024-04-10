2fe5dd709460c726af91f40a17c7d943faf18c1a
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-04-10 18:46:14 +0200

    Revert lifecycle-runtime-compose to 2.8.0-alpha02 state

--- lifecycle/lifecycle-runtime-compose/src/commonMain/kotlin/androidx/lifecycle/compose/DropUnlessLifecycle.kt
+++ lifecycle/lifecycle-runtime-compose/src/commonMain/kotlin/androidx/lifecycle/compose/DropUnlessLifecycle.kt
@@ -18,6 +18,7 @@ package androidx.lifecycle.compose
 
 import androidx.annotation.CheckResult
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle.State
 import androidx.lifecycle.LifecycleOwner
