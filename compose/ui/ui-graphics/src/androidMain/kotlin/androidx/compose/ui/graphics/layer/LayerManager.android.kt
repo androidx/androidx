ef01fd2c0d01fa4f7a957ce0800fb7f4b3ad52c6
Andrey Kulikov <andreykulikov@google.com>
2024-04-17 13:57:28 +0100

    Enable GraphicsLayerOwnerLayer on API levels M..O
    On those Android versions we use GraphicsLayerV23. We will enable it on versions using GraphicsViewLayer separately.
    One of tests caught an issue where we were discarding the layer content before the persistence logic had a change to be executed. In order to fix it I updated the handler scheduling to use sendMessageAtFrontOfQueue.
    
    Test: existing test from all the compose modules
    Change-Id: Ic61a639009b1759688b304ae5d25f040954042b6

--- compose/ui/ui-graphics/src/androidMain/kotlin/androidx/compose/ui/graphics/layer/LayerManager.android.kt
+++ compose/ui/ui-graphics/src/androidMain/kotlin/androidx/compose/ui/graphics/layer/LayerManager.android.kt
@@ -20,6 +20,7 @@ import android.graphics.PixelFormat
 import android.media.ImageReader
 import android.os.Build
 import android.os.Looper
+import android.os.Message
 import android.view.Surface
 import androidx.annotation.RequiresApi
 import androidx.collection.ObjectList
@@ -55,7 +56,13 @@ internal class LayerManager(val canvasHolder: CanvasHolder) {
         if (!layerList.contains(layer)) {
             layerList.add(layer)
             if (!handler.hasMessages(0)) {
-                handler.sendEmptyMessage(0)
+                // we don't run persistLayers() synchronously in order to do less work as there
+                // might be a lot of new layers created during one frame. however we also want
+                // to execute it as soon as possible to be able to persist the layers before
+                // they discard their content. it is possible that there is some other work
+                // scheduled on the main thread which is going to change what layers are drawn.
+                // we use sendMessageAtFrontOfQueue() in order to be executed before that.
+                handler.sendMessageAtFrontOfQueue(Message.obtain())
             }
         }
     }
