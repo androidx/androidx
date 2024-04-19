63ad7d7cc1613a6773531a828458c4719f4a1802
Clara Fok <clarafok@google.com>
2024-04-19 16:26:31 -0700

    Add new demo NavWithArgsInNestedGraphDemo
    Test: manual
    Bug: 188693139
    Change-Id: I23f0178e28a7fff2a2149776a2d518e4e8040c35

--- navigation/navigation-compose/integration-tests/navigation-demos/src/main/java/androidx/navigation/compose/demos/NavigationDemos.kt
+++ navigation/navigation-compose/integration-tests/navigation-demos/src/main/java/androidx/navigation/compose/demos/NavigationDemos.kt
@@ -27,6 +27,7 @@ val NavigationDemos = DemoCategory(
         ComposableDemo("Nested Nav In Graph Demo") { NestNavInGraphDemo() },
         ComposableDemo("Bottom Bar Nav Demo") { BottomBarNavDemo() },
         ComposableDemo("Navigation with Args") { NavWithArgsDemo() },
+        ComposableDemo("Navigation with Args in Nested Graph") { NavWithArgsInNestedGraphDemo() },
         ComposableDemo("Navigation by DeepLink") { NavByDeepLinkDemo() },
         ComposableDemo("Navigation PopUpTo") { NavPopUpToDemo() },
         ComposableDemo("Navigation SingleTop") { NavSingleTopDemo() },
