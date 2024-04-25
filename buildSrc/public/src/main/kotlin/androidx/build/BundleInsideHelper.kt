876f55215e6d8a055660761973672b5cc7d85c07
Omar Ismail <omarismail@google.com>
2024-04-25 13:24:51 +0100

    Remove unused functions in BundleInsideHelper as we moved to Repackaging plugin
    BUG: 296864329
    
    TESTED: Did a find all in project for removed functions and none existed
    Change-Id: Ic0f77ffa87bb520b768555719131c4e2b0ad8cdf

--- buildSrc/public/src/main/kotlin/androidx/build/BundleInsideHelper.kt
+++ buildSrc/public/src/main/kotlin/androidx/build/BundleInsideHelper.kt
@@ -21,7 +21,6 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.Transformer
 import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext
 import org.apache.tools.zip.ZipOutputStream
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.attributes.Usage
 import org.gradle.api.file.FileTreeElement
@@ -29,12 +28,8 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.named
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
-import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
 /** Allow java and Android libraries to bundle other projects inside the project jar/aar. */
 object BundleInsideHelper {
@@ -87,101 +82,6 @@ object BundleInsideHelper {
     @JvmStatic
     fun Project.forInsideAar(from: String, to: String, dropResourcesWithSuffix: String?) {
         forInsideAar(listOf(Relocation(from, to)), dropResourcesWithSuffix)
-    }
-
-    /**
-     * Creates a configuration for the users to use that will be used bundle these dependency jars
-     * inside of this project's jar.
-     *
-     * ```
-     * dependencies {
-     *   bundleInside(project(":foo"))
-     *   debugBundleInside(project(path: ":bar", configuration: "someDebugConfiguration"))
-     *   releaseBundleInside(project(path: ":bar", configuration: "someReleaseConfiguration"))
-     * }
-     * ```
-     *
-     * @param from specifies from which package the rename should happen
-     * @param to specifies to which package to put the renamed classes
-     * @param dropResourcesWithSuffix used to drop Java resources if they match this suffix, null
-     *   means no filtering
-     * @receiver the project that should bundle jars specified by these configurations
-     */
-    @JvmStatic
-    fun Project.forInsideJar(from: String, to: String, dropResourcesWithSuffix: String?) {
-        val bundle = createBundleConfiguration()
-        val repackage =
-            configureRepackageTaskForType(
-                relocations = listOf(Relocation(from, to)),
-                configuration = bundle,
-                dropResourcesWithSuffix = dropResourcesWithSuffix
-            )
-        dependencies.add("compileOnly", files(repackage.flatMap { it.archiveFile }))
-        dependencies.add("testImplementation", files(repackage.flatMap { it.archiveFile }))
-
-        val jarTask = tasks.named("jar")
-        jarTask.configure {
-            it as Jar
-            it.from(repackage.map { files(zipTree(it.archiveFile.get().asFile)) })
-        }
-        addArchivesToConfiguration("apiElements", jarTask)
-        addArchivesToConfiguration("runtimeElements", jarTask)
-    }
-
-    private fun Project.addArchivesToConfiguration(
-        configName: String,
-        jarTask: TaskProvider<Task>
-    ) {
-        configurations.getByName(configName) {
-            it.outgoing.artifacts.clear()
-            it.outgoing.artifact(
-                jarTask.flatMap { jarTask ->
-                    jarTask as Jar
-                    jarTask.archiveFile
-                }
-            )
-        }
-    }
-
-    /**
-     * KMP Version of [Project.forInsideJar]. See those docs for details.
-     *
-     * @param dropResourcesWithSuffix used to drop Java resources if they match this suffix,
-     *     * null means no filtering
-     *
-     * TODO(b/237104605): bundleInside is a global configuration. Should figure out how to make it
-     *   work properly with kmp and source sets so it can reside inside a sourceSet dependency.
-     */
-    @JvmStatic
-    fun Project.forInsideJarKmp(from: String, to: String, dropResourcesWithSuffix: String?) {
-        val kmpExtension =
-            extensions.findByType<KotlinMultiplatformExtension>() ?: error("kmp only")
-        val bundle = createBundleConfiguration()
-        val repackage =
-            configureRepackageTaskForType(
-                relocations = listOf(Relocation(from, to)),
-                configuration = bundle,
-                dropResourcesWithSuffix = dropResourcesWithSuffix
-            )
-
-        // To account for KMP structure we need to find the jvm specific target
-        // and add the repackaged archive files to only their compilations.
-        val jvmTarget =
-            kmpExtension.targets.firstOrNull { it.platformType == KotlinPlatformType.jvm }
-                as? KotlinJvmTarget ?: error("cannot find jvm target")
-        jvmTarget.compilations["main"].defaultSourceSet {
-            dependencies { compileOnly(files(repackage.flatMap { it.archiveFile })) }
-        }
-        jvmTarget.compilations["test"].defaultSourceSet {
-            dependencies { implementation(files(repackage.flatMap { it.archiveFile })) }
-        }
-        val jarTask = tasks.named(jvmTarget.artifactsTaskName)
-        jarTask.configure {
-            it as Jar
-            it.from(repackage.map { files(zipTree(it.archiveFile.get().asFile)) })
-        }
-        addArchivesToConfiguration("jvmApiElements", jarTask)
-        addArchivesToConfiguration("jvmRuntimeElements", jarTask)
     }
 
     /**
