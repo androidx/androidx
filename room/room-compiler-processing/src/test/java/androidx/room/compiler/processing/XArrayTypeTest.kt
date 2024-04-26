afe02e06ad917cdedf47396291bfdf64ccb0492f
Kuan-Ying Chou <kuanyingchou@google.com>
2024-04-26 18:51:03 +0100

    [KSP2] Fix array test in XArrayTypeTest
    Java arrays have covariant upper bound in KSP2 which makes more sense so we're adjusting our test to reflect that.
    
    Test: XArrayTypeTest.kt
    Change-Id: I063078b20940e8ba45d56d52977045dad8b354e0

--- room/room-compiler-processing/src/test/java/androidx/room/compiler/processing/XArrayTypeTest.kt
+++ room/room-compiler-processing/src/test/java/androidx/room/compiler/processing/XArrayTypeTest.kt
@@ -19,6 +19,7 @@ package androidx.room.compiler.processing
 import androidx.kruth.assertThat
 import androidx.kruth.assertWithMessage
 import androidx.room.compiler.codegen.JArrayTypeName
+import androidx.room.compiler.processing.compat.XConverters.toKS
 import androidx.room.compiler.processing.ksp.KspProcessingEnv
 import androidx.room.compiler.processing.ksp.createTypeReference
 import androidx.room.compiler.processing.util.Source
@@ -31,6 +32,7 @@ import androidx.room.compiler.processing.util.runProcessorTest
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.javapoet.JTypeName
 import com.squareup.kotlinpoet.javapoet.KTypeName
+import com.squareup.kotlinpoet.javapoet.KWildcardTypeName
 import org.junit.Test
 
 class XArrayTypeTest {
@@ -58,7 +60,12 @@ class XArrayTypeTest {
             )
             if (invocation.isKsp) {
                 assertThat(type.asTypeName().kotlin).isEqualTo(
-                    com.squareup.kotlinpoet.ARRAY.parameterizedBy(String::class.asKTypeName())
+                    if (invocation.processingEnv.toKS().kspVersion >= KotlinVersion(2, 0)) {
+                        com.squareup.kotlinpoet.ARRAY.parameterizedBy(
+                            KWildcardTypeName.producerOf(String::class.asKTypeName()))
+                    } else {
+                        com.squareup.kotlinpoet.ARRAY.parameterizedBy(String::class.asKTypeName())
+                    }
                 )
             }
             check(type.isArray())
