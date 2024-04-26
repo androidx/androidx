2a6d08875060dd8337b134ad20cb624617aba8dc
Arkadii Ivanov <arkivanov@google.com>
2024-04-26 14:43:50 +0100

    Added TableSubject to Kruth.
    NOTE TO API COUNCIL: Please ignore this CL, API tracking is only enabled for Kruth to compare compatibility with Google Truth, and this is an unpublished library with no intention to publish.
    
    Test: ./gradlew :kruth:kruth:allTests
    Relnote: N/A
    Change-Id: I8e5f00cdd87cbda23dff5a2cac7db27ea3202570

--- kruth/kruth/src/jvmMain/kotlin/androidx/kruth/PlatformStandardSubjectBuilder.jvm.kt
+++ kruth/kruth/src/jvmMain/kotlin/androidx/kruth/PlatformStandardSubjectBuilder.jvm.kt
@@ -19,6 +19,7 @@ package androidx.kruth
 import com.google.common.base.Optional
 import com.google.common.collect.Multimap
 import com.google.common.collect.Multiset
+import com.google.common.collect.Table
 import java.math.BigDecimal
 
 internal actual interface PlatformStandardSubjectBuilder {
@@ -28,6 +29,7 @@ internal actual interface PlatformStandardSubjectBuilder {
     fun that(actual: BigDecimal): BigDecimalSubject
     fun <T> that(actual: Multiset<T>): MultisetSubject<T>
     fun <K, V> that(actual: Multimap<K, V>): MultimapSubject<K, V>
+    fun <R, C, V> that(actual: Table<R, C, V>): TableSubject<R, C, V>
 }
 
 internal actual class PlatformStandardSubjectBuilderImpl actual constructor(
@@ -48,4 +50,7 @@ internal actual class PlatformStandardSubjectBuilderImpl actual constructor(
 
     override fun <K, V> that(actual: Multimap<K, V>): MultimapSubject<K, V> =
         MultimapSubject(actual = actual, metadata = metadata)
+
+    override fun <R, C, V> that(actual: Table<R, C, V>): TableSubject<R, C, V> =
+        TableSubject(actual = actual, metadata = metadata)
 }
