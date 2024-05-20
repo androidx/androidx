99bb2aaac447079358a8101896712d6a372f5c0a
Paul Thomas <pfthomas@google.com>
2024-05-20 15:07:31 -0400

    [CircularProgressIndicator] Fix gapSize not scaling with diameter
    Test: screenshot tests
    Change-Id: I7ac735db30be422b60747b7c28b11c470c512f0d

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/ProgressIndicator.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/ProgressIndicator.kt
@@ -581,7 +581,7 @@ fun CircularProgressIndicator(
             gapSize + strokeWidth
         }
         val gapSizeSweep =
-            (adjustedGapSize.value / (Math.PI * CircularIndicatorDiameter.value).toFloat()) * 360f
+            (adjustedGapSize.value / (Math.PI * size.width.toDp().value).toFloat()) * 360f
 
         drawCircularIndicator(
             startAngle + sweep + min(sweep, gapSizeSweep),
