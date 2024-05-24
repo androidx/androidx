64d8e847283bb06b3b086fafd766949db094b4e1
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-05-24 17:29:54 +0200

    Move the current uikit implementation of SelectionHandle to skikoMain (#1372)
    The goal of following PR is pretty straight-forward - we take current
    implementation of selection handlers that is used in uiKit and commonise
    it - make it available for using in all non-android targets as-is.
    
    This does not affect any current functionality for non-touch devices but
    automatically will enable selection handlers on mobile web target. This
    selection handlers work suboptimally due to interference with the
    virtual keyboard but we do have a dedicated task about fixing this

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.skiko.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,6 +60,15 @@ private val RADIUS = 6.dp
  */
 private val THICKNESS = 2.dp
 
+
+/**
+ * [SelectionHandle] is used for drawing selection handlers on all non-android platforms if a certain set of touch-based events happened.
+ * The logic of when that happened is out of scope of [SelectionHandle], this is something that basically implementations of Modifier.textFieldPointer
+ * are in charge of. De-facto on any platform, these handles won't be shown on mouse events. That said, one can see them while selecting text on
+ * iOS and mobile web targets.
+ *
+ * [SelectionHandle] was initially designed as iOS entity but later was commonized as is.
+ */
 @Composable
 internal actual fun SelectionHandle(
     offsetProvider: OffsetProvider,
