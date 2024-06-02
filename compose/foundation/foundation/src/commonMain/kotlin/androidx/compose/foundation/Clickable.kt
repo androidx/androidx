d2d43b13b4512018a9625754e739a77497ea48f0
Igor Demin <igordmn@gmail.com>
2024-06-02 20:25:52 +0200

    Apply jb-main Clickable.kt changes
    ```
    git diff androidx/compose-ui/1.6.7 origin/jb-main -- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/Clickable.kt > 1.patch
    ```

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/Clickable.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/Clickable.kt
@@ -28,7 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusEventModifierNode
+import androidx.compose.ui.focus.FocusRequesterModifierNode
 import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.requestFocus
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
@@ -633,6 +635,7 @@ internal open class ClickableNode(
         detectTapAndPress(
             onPress = { offset ->
                 if (enabled) {
+                    focusableNode.requestFocusWhenInMouseInputMode()
                     handlePressInteraction(offset)
                 }
             },
@@ -777,13 +780,14 @@ private class CombinedClickableNodeImpl(
     override suspend fun PointerInputScope.clickPointerInput() {
         detectTapGestures(
             onDoubleTap = if (enabled && onDoubleClick != null) {
-                { onDoubleClick?.invoke() }
+                { focusableNode.requestFocusWhenInMouseInputMode(); onDoubleClick?.invoke() }
             } else null,
             onLongPress = if (enabled && onLongClick != null) {
-                { onLongClick?.invoke() }
+                { focusableNode.requestFocusWhenInMouseInputMode(); onLongClick?.invoke() }
             } else null,
             onPress = { offset ->
                 if (enabled) {
+                    focusableNode.requestFocusWhenInMouseInputMode()
                     handlePressInteraction(offset)
                 }
             },
@@ -880,8 +884,7 @@ internal abstract class AbstractClickableNode(
 
     final override val shouldAutoInvalidate: Boolean = false
 
-    private val focusableInNonTouchMode: FocusableInNonTouchMode = FocusableInNonTouchMode()
-    private val focusableNode: FocusableNode = FocusableNode(interactionSource)
+    protected val focusableNode: FocusableNode = FocusableNode(interactionSource)
     private var pointerInputNode: SuspendingPointerInputModifierNode? = null
     private var indicationNode: DelegatableNode? = null
 
@@ -929,11 +932,9 @@ internal abstract class AbstractClickableNode(
         }
         if (this.enabled != enabled) {
             if (enabled) {
-                delegate(focusableInNonTouchMode)
                 delegate(focusableNode)
             } else {
                 // TODO: Should we remove indicationNode? Previously we always emitted indication
-                undelegate(focusableInNonTouchMode)
                 undelegate(focusableNode)
                 disposeInteractions()
             }
@@ -972,7 +973,6 @@ internal abstract class AbstractClickableNode(
             initializeIndicationAndInteractionSourceIfNeeded()
         }
         if (enabled) {
-            delegate(focusableInNonTouchMode)
             delegate(focusableNode)
         }
     }
@@ -1295,4 +1295,10 @@ internal fun TraversableNode.hasScrollableContainer(): Boolean {
         !hasScrollable
     }
     return hasScrollable
+}
+
+private fun FocusRequesterModifierNode.requestFocusWhenInMouseInputMode() {
+    if (isMouseInputWorkaround()) {
+        requestFocus()
+    }
 }
