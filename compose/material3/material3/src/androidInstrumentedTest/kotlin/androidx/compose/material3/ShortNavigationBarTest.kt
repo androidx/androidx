971c8a0e791d51dc34238df9618916ec40ae5bb3
Leticia Santos <leticiars@google.com>
2024-06-14 14:29:55 -0400

    [ShortNavigationBar] Updated to use expressive experimental tag and added samples.
    Test: N/A
    Relnote: use ExperimentalMaterial3ExpressiveApi in ShortNavigationBar APIs.
    Change-Id: I4f2e7f54679bd3df62b78f39cb8caadda33c4c8c

--- compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/ShortNavigationBarTest.kt
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/ShortNavigationBarTest.kt
@@ -66,7 +66,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class ShortNavigationBarTest {
