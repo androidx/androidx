addf1658bebe31250cb00f1295bf11bd67334d40
Kevin Truong <kevinctruong@google.com>
2024-06-16 18:39:54 -0700

    [Expressive][ToggleButtons] Adding expressive buttons : ToggleButtons
    Adding ToggleButtons for expressive buttons. These new buttons have corner morph and color change on checked. Adding samples for the new ToggleButtons and variants.
    
    Bug: 315133044
    Test: Adding unit tests and screenshot tests for ToggleButton and its variants.
    Relnote: Adding ToggleButtons which is a checkable button that animates the shape depending on what state it's in (default, pressed, hovered, focused, or checked).
    Change-Id: I8116f5d094d0061d456f484089ef1ec42202fea3

---
+++ compose/material3/material3/samples/src/main/java/androidx/compose/material3/samples/ToggleButtonSamples.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.material3.samples
+
+import androidx.annotation.Sampled
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ButtonShapes
+import androidx.compose.material3.ElevatedToggleButton
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedToggleButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleButton
+import androidx.compose.material3.ToggleButtonDefaults
+import androidx.compose.material3.TonalToggleButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun ToggleButtonSample() {
+    var checked by remember { mutableStateOf(false) }
+    ToggleButton(checked = checked, onCheckedChange = { checked = it }) { Text("Button") }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun RoundToggleButtonSample() {
+    var checked by remember { mutableStateOf(false) }
+    val shapes =
+        ButtonShapes(
+            shape = ToggleButtonDefaults.roundShape,
+            pressedShape = ToggleButtonDefaults.pressedShape,
+            hoveredShape = ToggleButtonDefaults.hoveredShape,
+            focusedShape = ToggleButtonDefaults.focusedShape,
+            checkedShape = ToggleButtonDefaults.squareShape
+        )
+    ToggleButton(checked = checked, onCheckedChange = { checked = it }, shapes = shapes) {
+        Text("Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun ElevatedToggleButtonSample() {
+    var checked by remember { mutableStateOf(false) }
+    ElevatedToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+        Text("Elevated Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun TonalToggleButtonSample() {
+    var checked by remember { mutableStateOf(false) }
+    TonalToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+        Text("Tonal Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun OutlinedToggleButtonSample() {
+    var checked by remember { mutableStateOf(false) }
+    OutlinedToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+        Text("Outlined Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Preview
+@Sampled
+@Composable
+fun ToggleButtonWithIconSample() {
+    var checked by remember { mutableStateOf(false) }
+    ToggleButton(checked = checked, onCheckedChange = { checked = it }) {
+        Icon(
+            Icons.Filled.Favorite,
+            contentDescription = "Localized description",
+            modifier = Modifier.size(ToggleButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ToggleButtonDefaults.IconSpacing))
+        Text("Like")
+    }
+}
---
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/ToggleButtonTest.kt
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.material3
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.tokens.ElevatedButtonTokens
+import androidx.compose.material3.tokens.OutlinedButtonTokens
+import androidx.compose.material3.tokens.PrimaryButtonTokens
+import androidx.compose.material3.tokens.TonalButtonTokens
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.testutils.assertIsEqualTo
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.getUnclippedBoundsInRoot
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ToggleButtonTest {
+    @get:Rule val rule = createComposeRule()
+
+    private val ToggleButtonTag = "ToggleButtonTag"
+    private val TextTag = "TextTag"
+    private val IconTag = "IconTag"
+
+    @Test
+    fun default_semantics() {
+        rule.setMaterialContent(lightColorScheme()) {
+            Box {
+                var checked by remember { mutableStateOf(false) }
+                ToggleButton(
+                    checked = checked,
+                    modifier = Modifier.testTag(ToggleButtonTag),
+                    onCheckedChange = { checked = it }
+                ) {
+                    Text("test")
+                }
+            }
+        }
+
+        rule
+            .onNodeWithTag(ToggleButtonTag)
+            .assert(SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Checkbox))
+            .assertIsEnabled()
+            .assertIsOff()
+            .performClick()
+            .assertIsOn()
+    }
+
+    @Test
+    fun disabledSemantics() {
+        rule.setMaterialContent(lightColorScheme()) {
+            var checked by remember { mutableStateOf(false) }
+            Box {
+                ToggleButton(
+                    checked = checked,
+                    modifier = Modifier.testTag(ToggleButtonTag),
+                    onCheckedChange = { checked = it },
+                    enabled = false
+                ) {
+                    Text("test")
+                }
+            }
+        }
+
+        rule
+            .onNodeWithTag(ToggleButtonTag)
+            .assert(SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Checkbox))
+            .assertIsNotEnabled()
+            .assertIsOff()
+            .performClick()
+            .assertIsOff()
+    }
+
+    @Test
+    fun toggleButton_contentPositioning() {
+        rule.setMaterialContent(lightColorScheme()) {
+            var checked by remember { mutableStateOf(false) }
+            Box {
+                ToggleButton(
+                    checked = checked,
+                    modifier = Modifier.testTag(ToggleButtonTag),
+                    onCheckedChange = { checked = it },
+                    enabled = false
+                ) {
+                    Text(
+                        text = "test",
+                        modifier = Modifier.testTag(TextTag).semantics(mergeDescendants = true) {}
+                    )
+                }
+            }
+        }
+
+        val textBounds = rule.onNodeWithTag(TextTag).getUnclippedBoundsInRoot()
+        val toggleButtonBounds = rule.onNodeWithTag(ToggleButtonTag).getUnclippedBoundsInRoot()
+
+        (textBounds.left - toggleButtonBounds.left).assertIsEqualTo(16.dp)
+        (toggleButtonBounds.right - textBounds.right).assertIsEqualTo(16.dp)
+    }
+
+    @Test
+    fun toggleButtonWithIcon_contentPositioning() {
+        rule.setMaterialContent(lightColorScheme()) {
+            Box {
+                ToggleButton(
+                    checked = false,
+                    modifier = Modifier.testTag(ToggleButtonTag),
+                    onCheckedChange = {},
+                    enabled = false
+                ) {
+                    Box(
+                        modifier =
+                            Modifier.size(ToggleButtonDefaults.IconSize).testTag(IconTag).semantics(
+                                mergeDescendants = true
+                            ) {}
+                    )
+                    Spacer(modifier = Modifier.width(ToggleButtonDefaults.IconSpacing))
+                    Text(
+                        text = "test",
+                        modifier = Modifier.testTag(TextTag).semantics(mergeDescendants = true) {}
+                    )
+                }
+            }
+        }
+
+        val textBounds = rule.onNodeWithTag(TextTag).getUnclippedBoundsInRoot()
+        val toggleButtonBounds = rule.onNodeWithTag(ToggleButtonTag).getUnclippedBoundsInRoot()
+        val iconBounds = rule.onNodeWithTag(IconTag).getUnclippedBoundsInRoot()
+
+        (iconBounds.left - toggleButtonBounds.left).assertIsEqualTo(16.dp)
+        (textBounds.left - iconBounds.right).assertIsEqualTo(8.dp)
+        (toggleButtonBounds.right - textBounds.right).assertIsEqualTo(16.dp)
+    }
+
+    @Test
+    fun toggleButton_defaultColors() {
+        rule.setMaterialContent(lightColorScheme()) {
+            assertThat(
+                    ToggleButtonDefaults.toggleButtonColors(
+                        containerColor = Color.Unspecified,
+                        contentColor = Color.Unspecified,
+                        disabledContainerColor = Color.Unspecified,
+                        disabledContentColor = Color.Unspecified,
+                        checkedContainerColor = Color.Unspecified,
+                        checkedContentColor = Color.Unspecified,
+                    )
+                )
+                .isEqualTo(
+                    ToggleButtonColors(
+                        containerColor = PrimaryButtonTokens.UnselectedContainerColor.value,
+                        contentColor = PrimaryButtonTokens.UnselectedPressedLabelTextColor.value,
+                        disabledContainerColor =
+                            PrimaryButtonTokens.DisabledContainerColor.value.copy(
+                                alpha = PrimaryButtonTokens.DisabledContainerOpacity
+                            ),
+                        disabledContentColor =
+                            PrimaryButtonTokens.DisabledLabelTextColor.value.copy(
+                                alpha = PrimaryButtonTokens.DisabledLabelTextOpacity
+                            ),
+                        checkedContainerColor = PrimaryButtonTokens.SelectedContainerColor.value,
+                        checkedContentColor =
+                            PrimaryButtonTokens.SelectedPressedLabelTextColor.value
+                    )
+                )
+        }
+    }
+
+    @Test
+    fun elevatedToggleButton_defaultColors() {
+        rule.setMaterialContent(lightColorScheme()) {
+            assertThat(
+                    ToggleButtonDefaults.elevatedToggleButtonColors(
+                        containerColor = Color.Unspecified,
+                        contentColor = Color.Unspecified,
+                        disabledContainerColor = Color.Unspecified,
+                        disabledContentColor = Color.Unspecified,
+                        checkedContainerColor = Color.Unspecified,
+                        checkedContentColor = Color.Unspecified,
+                    )
+                )
+                .isEqualTo(
+                    ToggleButtonColors(
+                        containerColor = ElevatedButtonTokens.UnselectedContainerColor.value,
+                        contentColor = ElevatedButtonTokens.UnselectedPressedLabelTextColor.value,
+                        disabledContainerColor =
+                            ElevatedButtonTokens.DisabledContainerColor.value.copy(
+                                alpha = ElevatedButtonTokens.DisabledContainerOpacity
+                            ),
+                        disabledContentColor =
+                            ElevatedButtonTokens.DisabledLabelTextColor.value.copy(
+                                alpha = ElevatedButtonTokens.DisabledLabelTextOpacity
+                            ),
+                        checkedContainerColor = ElevatedButtonTokens.SelectedContainerColor.value,
+                        checkedContentColor =
+                            ElevatedButtonTokens.SelectedPressedLabelTextColor.value
+                    )
+                )
+        }
+    }
+
+    @Test
+    fun tonalToggleButton_defaultColors() {
+        rule.setMaterialContent(lightColorScheme()) {
+            assertThat(
+                    ToggleButtonDefaults.tonalToggleButtonColors(
+                        containerColor = Color.Unspecified,
+                        contentColor = Color.Unspecified,
+                        disabledContainerColor = Color.Unspecified,
+                        disabledContentColor = Color.Unspecified,
+                        checkedContainerColor = Color.Unspecified,
+                        checkedContentColor = Color.Unspecified,
+                    )
+                )
+                .isEqualTo(
+                    ToggleButtonColors(
+                        containerColor = TonalButtonTokens.UnselectedContainerColor.value,
+                        contentColor = TonalButtonTokens.UnselectedLabelTextColor.value,
+                        disabledContainerColor =
+                            TonalButtonTokens.DisabledContainerColor.value.copy(
+                                alpha = TonalButtonTokens.DisabledContainerOpacity
+                            ),
+                        disabledContentColor =
+                            TonalButtonTokens.DisabledLabelTextColor.value.copy(
+                                alpha = TonalButtonTokens.DisabledLabelTextOpacity
+                            ),
+                        checkedContainerColor = TonalButtonTokens.SelectedContainerColor.value,
+                        checkedContentColor = TonalButtonTokens.SelectedLabelTextColor.value
+                    )
+                )
+        }
+    }
+
+    @Test
+    fun outlinedToggleButton_defaultColors() {
+        rule.setMaterialContent(lightColorScheme()) {
+            assertThat(
+                    ToggleButtonDefaults.outlinedToggleButtonColors(
+                        containerColor = Color.Unspecified,
+                        contentColor = Color.Unspecified,
+                        disabledContainerColor = Color.Unspecified,
+                        disabledContentColor = Color.Unspecified,
+                        checkedContainerColor = Color.Unspecified,
+                        checkedContentColor = Color.Unspecified,
+                    )
+                )
+                .isEqualTo(
+                    ToggleButtonColors(
+                        containerColor = OutlinedButtonTokens.UnselectedPressedOutlineColor.value,
+                        contentColor = OutlinedButtonTokens.UnselectedLabelTextColor.value,
+                        disabledContainerColor =
+                            OutlinedButtonTokens.DisabledOutlineColor.value.copy(
+                                alpha = OutlinedButtonTokens.DisabledContainerOpacity
+                            ),
+                        disabledContentColor =
+                            OutlinedButtonTokens.DisabledLabelTextColor.value.copy(
+                                alpha = OutlinedButtonTokens.DisabledLabelTextOpacity
+                            ),
+                        checkedContainerColor = OutlinedButtonTokens.SelectedContainerColor.value,
+                        checkedContentColor = OutlinedButtonTokens.SelectedLabelTextColor.value
+                    )
+                )
+        }
+    }
+}
