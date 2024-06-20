9eea0f856e373303e9c54c5003c748770319b648
Levi Albuquerque <levima@google.com>
2024-06-20 08:02:02 +0100

    Remove kdocs references to experimental APIS
    Since these APIs have been marked stable we need to update the kdocs to remove kdocs references to experimental APIs.
    
    Relnote: N/A
    Test: N/A
    Change-Id: I1a40630017c4e4f6556884c2af9fb15e19c402e6

--- compose/foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/OverscrollConfiguration.android.kt
+++ compose/foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/OverscrollConfiguration.android.kt
@@ -24,9 +24,6 @@ import androidx.compose.ui.graphics.Color
 /**
  * Metadata for overscroll effects for android platform.
  *
- * Note: this API is experimental and liable to be changed / removed. [glowColor] and [drawPadding]
- * only apply before Android S and so might be misleading to have in a generic configuration object.
- *
  * @param glowColor color for the glow effect, if the platform effect is a glow effect, otherwise
  *   ignored.
  * @param drawPadding the amount of padding to apply from scrollable container bounds to the effect
@@ -63,8 +60,6 @@ class OverscrollConfiguration(
 /**
  * Composition local to provide configuration for scrolling containers down the hierarchy. `null`
  * means there will be no overscroll at all.
- *
- * Note: this is experimental since the underlying [OverscrollConfiguration] is also experimental.
  */
 @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
 val LocalOverscrollConfiguration =
