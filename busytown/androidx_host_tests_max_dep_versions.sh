d2365a21f00d07888c7d5783f7be4283c3e6b8a0
Aurimas Liutikas <aurimas@google.com>
2024-07-19 12:46:56 -0700

    Re-enable AGP compileSdkChecks
    Now with fixes for -Pandroidx.constraints
    
    Test: ./gradlew cRAM
    Change-Id: Icbf93936487e9502f5595f2dde6e76dd2ee2c6cb

--- busytown/androidx_host_tests_max_dep_versions.sh
+++ busytown/androidx_host_tests_max_dep_versions.sh
@@ -6,6 +6,7 @@ echo "Starting $0 at $(date)"
 cd "$(dirname $0)"
 
 impl/build.sh test allHostTests zipOwnersFiles createModuleInfo \
+    -Pandroid.experimental.disableCompileSdkChecks=true \
     -Pandroidx.useMaxDepVersions \
     -Pandroidx.displayTestOutput=false \
     -Pandroidx.ignoreTestFailures "$@"
