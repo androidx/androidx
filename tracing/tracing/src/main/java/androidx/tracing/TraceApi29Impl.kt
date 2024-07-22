565f196e9365c045ddf15cf46a6d4bb995241f15
Arkadii Ivanov <arkivanov@google.com>
2024-07-22 11:54:11 +0100

    Converted TraceApi29Impl to Kotlin.
    Test: ./gradlew :tracing:tracing:connectedAndroidTest
    Change-Id: I1028e5d2e09c99ab5956928c622f3290c144fa54

---
+++ tracing/tracing/src/main/java/androidx/tracing/TraceApi29Impl.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.tracing
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+/**
+ * This is a helper class that handles [android.os.Trace] functionality in API >= 29.
+ *
+ * This class is being defined separately to avoid class verification failures. For more information
+ * read https://chromium.googlesource
+ * .com/chromium/src/build/+/refs/heads/master/android/docs/class_verification_failures
+ * .md#understanding-the-reason-for-the-failure
+ */
+@RequiresApi(Build.VERSION_CODES.Q)
+internal object TraceApi29Impl {
+
+    /** Checks whether or not tracing is currently enabled. */
+    @JvmStatic
+    val isEnabled: Boolean
+        get() = android.os.Trace.isEnabled()
+
+    /**
+     * Writes a trace message to indicate that a given section of code has begun. Must be followed
+     * by a call to [endAsyncSection] with the same methodName and cookie.
+     *
+     * @param methodName The method name to appear in the trace.
+     * @param cookie Unique identifier for distinguishing simultaneous events.
+     */
+    @JvmStatic
+    fun beginAsyncSection(methodName: String, cookie: Int) {
+        android.os.Trace.beginAsyncSection(methodName, cookie)
+    }
+
+    /**
+     * Writes a trace message to indicate that the current method has ended. Must be called exactly
+     * once for each call to [beginAsyncSection] using the same name and cookie.
+     *
+     * @param methodName The method name to appear in the trace.
+     * @param cookie Unique identifier for distinguishing simultaneous events.
+     */
+    @JvmStatic
+    fun endAsyncSection(methodName: String, cookie: Int) {
+        android.os.Trace.endAsyncSection(methodName, cookie)
+    }
+
+    /**
+     * Writes trace message to indicate the value of a given counter.
+     *
+     * @param counterName The counter name to appear in the trace.
+     * @param counterValue The counter value.
+     */
+    @JvmStatic
+    fun setCounter(counterName: String, counterValue: Int) {
+        android.os.Trace.setCounter(counterName, counterValue.toLong())
+    }
+}
