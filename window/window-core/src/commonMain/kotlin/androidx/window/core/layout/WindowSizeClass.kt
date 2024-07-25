41f67627c98f52328685113382cc67d0676d40af
Aurimas Liutikas <aurimas@google.com>
2024-07-25 14:10:45 -0700

    Add additional native platforms to window-core library
    Test: ./gradlew window:window-core:build
    Bug: 349894318
    Change-Id: I488534b84bb0b646c422bb28349ed391290d3c1d

--- window/window-core/src/commonMain/kotlin/androidx/window/core/layout/WindowSizeClass.kt
+++ window/window-core/src/commonMain/kotlin/androidx/window/core/layout/WindowSizeClass.kt
@@ -60,7 +60,7 @@ private constructor(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (other == null || this::class != other::class) return false
 
         other as WindowSizeClass
 
