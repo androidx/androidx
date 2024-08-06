f6193605ffb38554dc07bda02b7aa4c7aa7364de
Dillon Simpson <mdsimpson@google.com>
2024-08-06 16:54:59 -0600

    Save and restore PaginationModel across a configuration change to avoid re-loading dimensions
    Bug: 355057860
    Test: ./gradlew :pdf:pdf-viewer:test, ./gradlew :pdf:pdf-viewer:connectedAndroidTest, manual
    Change-Id: I35e6fb54631b12dec450d1449b8c32ab9af6dcb7

--- pdf/pdf-viewer/src/main/java/androidx/pdf/viewer/PaginationModel.java
+++ pdf/pdf-viewer/src/main/java/androidx/pdf/viewer/PaginationModel.java
@@ -16,11 +16,15 @@
 
 package androidx.pdf.viewer;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Rect;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RestrictTo;
+import androidx.core.os.ParcelCompat;
 import androidx.pdf.data.Range;
 import androidx.pdf.models.Dimensions;
 import androidx.pdf.util.PaginationUtils;
@@ -57,7 +61,8 @@ import java.util.Set;
  * pages are added
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY)
-public class PaginationModel {
+@SuppressLint("BanParcelableUsage")
+public class PaginationModel implements Parcelable {
     /**
      * The spacing added before and after each page (the actual space between 2 consecutive pages is
      * twice this distance), in pixels.
@@ -86,6 +91,30 @@ public class PaginationModel {
     public PaginationModel(@NonNull Context context) {
         mPageSpacingPx = PaginationUtils.getPageSpacingInPixels(context);
     }
+
+    protected PaginationModel(@NonNull Parcel in) {
+        PaginationModelData data = ParcelCompat.readParcelable(in, getClass().getClassLoader(),
+                PaginationModelData.class);
+        mPageSpacingPx = data.getPageSpacingPx();
+        mMaxPages = data.getMaxPages();
+        mPages = data.getPages();
+        mPageStops = data.getPageStops();
+        mSize = data.getSize();
+        mEstimatedPageHeight = data.getEstimatedPageHeight();
+        mAccumulatedPageSize = data.getAccumulatedPageSize();
+    }
+
+    public static final Creator<PaginationModel> CREATOR = new Creator<PaginationModel>() {
+        @Override
+        public PaginationModel createFromParcel(Parcel in) {
+            return new PaginationModel(in);
+        }
+
+        @Override
+        public PaginationModel[] newArray(int size) {
+            return new PaginationModel[size];
+        }
+    };
 
     /**
      * Initializes the model.
@@ -264,7 +293,6 @@ public class PaginationModel {
     }
 
 
-
     /**
      * Returns the location of the page in the model.
      *
@@ -277,7 +305,7 @@ public class PaginationModel {
      *       maximizes the portion of that view that is visible on the screen
      * </ul>
      *
-     * @param pageNum - index of requested page
+     * @param pageNum  - index of requested page
      * @param viewArea - the current viewport in content coordinates
      * @return - coordinates of the page within this model
      */
@@ -390,5 +418,17 @@ public class PaginationModel {
     protected void finalize() throws Throwable {
         mObservers.clear();
         super.finalize();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        PaginationModelData data = new PaginationModelData(mPageSpacingPx, mMaxPages, mPages,
+                mPageStops, mSize, mEstimatedPageHeight, mAccumulatedPageSize);
+        dest.writeParcelable(data, flags);
     }
 }
