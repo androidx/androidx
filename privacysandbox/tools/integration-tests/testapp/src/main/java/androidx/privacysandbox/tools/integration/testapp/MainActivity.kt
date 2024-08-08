451d630bffc952f54ceb5e11e20b91bf4465b0d9
Tanuj <tanujdhir@google.com>
2024-08-08 14:35:32 +0100

    Add UI integration tests using shim tools
    Test: ./gradlew :privacysandbox:tools:integration-tests:testapp:connectedDebugAndroidTest
    Bug: 346965982
    Change-Id: I54b5cdf065e2805a621b01d812790db407e1386d

--- privacysandbox/tools/integration-tests/testapp/src/main/java/androidx/privacysandbox/tools/integration/testapp/MainActivity.kt
+++ privacysandbox/tools/integration-tests/testapp/src/main/java/androidx/privacysandbox/tools/integration/testapp/MainActivity.kt
@@ -18,17 +18,32 @@ package androidx.privacysandbox.tools.integration.testapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.privacysandbox.sdkruntime.client.SdkSandboxManagerCompat
 import androidx.privacysandbox.sdkruntime.core.LoadSdkCompatException
 import androidx.privacysandbox.tools.integration.testsdk.MySdk
 import androidx.privacysandbox.tools.integration.testsdk.MySdkFactory.wrapToMySdk
+import androidx.privacysandbox.ui.client.view.SandboxedSdkUiSessionState
+import androidx.privacysandbox.ui.client.view.SandboxedSdkView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.idling.CountingIdlingResource
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     internal var sdk: MySdk? = null
+    private val idlingResource = CountingIdlingResource("MainActivity idlingResource")
+
+    override fun onDestroy() {
+        super.onDestroy()
+        IdlingRegistry.getInstance().unregister(idlingResource)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        IdlingRegistry.getInstance().register(idlingResource)
+
+        lifecycleScope.launch { loadSdk() }
     }
 
     internal suspend fun loadSdk() {
@@ -45,5 +60,22 @@ class MainActivity : AppCompatActivity() {
                 sandboxManagerCompat.getSandboxedSdks().first()
             }
         sdk = sandboxedSdk.getInterface()?.let { wrapToMySdk(it) }
+    }
+
+    internal suspend fun renderAd() {
+        idlingResource.increment()
+
+        val textViewAd = sdk!!.getTextViewAd()
+        val sandboxedSdkView = findViewById<SandboxedSdkView>(R.id.sandboxedSdkView)
+
+        runOnUiThread {
+            sandboxedSdkView.setAdapter(textViewAd)
+
+            sandboxedSdkView.addStateChangedListener { state ->
+                if (state is SandboxedSdkUiSessionState.Active) {
+                    idlingResource.decrement()
+                }
+            }
+        }
     }
 }
