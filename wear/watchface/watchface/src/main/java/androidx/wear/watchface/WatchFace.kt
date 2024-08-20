12c2b86e03e98e3b8a3131987a0e43dbe7de583d
Anubha Kushwaha <anubhakushwaha@google.com>
2024-08-20 09:06:27 +0000

    Revert "Don't double delete headless edtior client"
    This reverts commit 8481219d2e064eedaba6caf79893399131df2f5f.
    
    Reason for revert: b/360844136 (Breaking SysUI TAP)
    
    Change-Id: I6b77023a37e2ae015f1c165b54e87272c06d38ad

--- wear/watchface/watchface/src/main/java/androidx/wear/watchface/WatchFace.kt
+++ wear/watchface/watchface/src/main/java/androidx/wear/watchface/WatchFace.kt
@@ -999,7 +999,6 @@ constructor(
                 }
                 if (watchState.isHeadless) {
                     headlessWatchFaceImpl!!.release()
-                } else {
                     this@WatchFaceImpl.onDestroy()
                 }
             }
