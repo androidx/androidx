0c76cc154a409f37200e7c7f46268ee4ae56d392
Daniel Santiago Rivera <danysantiago@google.com>
2024-08-29 11:30:14 -0400

    Fix thread usage in RoomTrackingLiveData
    In aosp/3205550 Room's LiveData implementation was converted to Coroutines and in aosp/3238929 the main thread usage was fixed. But a difference from the original executor implementation was that refresh() is scheduled either on the query executor or transaction executor it should not thread-hop in between like it was changed in aosp/3238929 using withContext. This fixes the threading but launching in refresh() coroutine in the right thread pool.
    
    Bug: 361216129
    Test: Manually validated test in b/361216129 with snapshot version.
    Change-Id: I3b94bfdd3c324c4728542e65b572b526681dd5f8

--- room/room-runtime/src/androidMain/kotlin/androidx/room/RoomTrackingLiveData.android.kt
+++ room/room-runtime/src/androidMain/kotlin/androidx/room/RoomTrackingLiveData.android.kt
@@ -22,8 +22,8 @@ import androidx.room.util.performSuspending
 import androidx.sqlite.SQLiteConnection
 import java.util.concurrent.Callable
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * A LiveData implementation that closely works with [InvalidationTracker] to implement database
@@ -52,6 +52,17 @@ internal sealed class RoomTrackingLiveData<T>(
     private val invalid = AtomicBoolean(true)
     private val computing = AtomicBoolean(false)
     private val registeredObserver = AtomicBoolean(false)
+
+    private val launchContext =
+        if (database.inCompatibilityMode()) {
+            if (inTransaction) {
+                database.getTransactionContext()
+            } else {
+                database.getQueryContext()
+            }
+        } else {
+            EmptyCoroutineContext
+        }
 
     private suspend fun refresh() {
         if (registeredObserver.compareAndSet(false, true)) {
@@ -105,7 +116,7 @@ internal sealed class RoomTrackingLiveData<T>(
         val isActive = hasActiveObservers()
         if (invalid.compareAndSet(false, true)) {
             if (isActive) {
-                database.getCoroutineScope().launch { refresh() }
+                database.getCoroutineScope().launch(launchContext) { refresh() }
             }
         }
     }
@@ -115,7 +126,7 @@ internal sealed class RoomTrackingLiveData<T>(
     override fun onActive() {
         super.onActive()
         container.onActive(this)
-        database.getCoroutineScope().launch { refresh() }
+        database.getCoroutineScope().launch(launchContext) { refresh() }
     }
 
     override fun onInactive() {
@@ -132,13 +143,7 @@ internal class RoomCallableTrackingLiveData<T>(
     private val callableFunction: Callable<T?>
 ) : RoomTrackingLiveData<T>(database, container, inTransaction, tableNames) {
     override suspend fun compute(): T? {
-        val queryContext =
-            if (inTransaction) {
-                database.getTransactionContext()
-            } else {
-                database.getQueryContext()
-            }
-        return withContext(queryContext) { callableFunction.call() }
+        return callableFunction.call()
     }
 }
 
