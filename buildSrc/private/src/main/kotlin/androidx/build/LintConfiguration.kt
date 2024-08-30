f9ad1fcc60e08cdcfe8adcef7316b083599be51b
Scott Pollom <spollom@google.com>
2024-08-30 22:52:43 +0000

    Revert "Disable lint checks that fail for multiplatform libraries"
    This reverts commit 4e2c436f2f25a26979220c5839b7f3cc58f8a45e.
    
    Reason for revert: Need a fix for b/360840930 instead
    
    Change-Id: I640d9c0f611a29d52c484dc923e1ad0399334b00

--- buildSrc/private/src/main/kotlin/androidx/build/LintConfiguration.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/LintConfiguration.kt
@@ -267,11 +267,7 @@ private fun Project.configureLint(lint: Lint, isLibrary: Boolean) {
             // Disable classfile-based checks because lint cannot find the class files for
             // multiplatform projects and `SourceSet.java.classesDirectory` is not configurable.
             // This is not ideal, but it's better than having no lint checks at all.
-            // These should be re-enabled (b/363305780)
-            disable.add("ObsoleteSampledAnnotation")
-            disable.add("UnresolvedSampleLink")
-            disable.add("MultipleSampledFunctions")
-            disable.add("InvalidSamplesLocation")
+            disable.add("LintError")
         }
 
         // Disable a check that's only relevant for apps that ship to Play Store. (b/299278101)
