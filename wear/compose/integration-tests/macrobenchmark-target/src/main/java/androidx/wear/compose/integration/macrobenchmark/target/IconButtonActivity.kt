3a3146eea7ba26ee1971f41247750dd8943ba4d5
Filip Stanis <fstanis@google.com>
2024-09-17 11:59:16 +0100

    Moves ButtonBenchmark code into ButtonBenchmarkBase and adds IconButtonBenchmark
    Example comparative results of the three button tests:
    
    Button: P50 3.39, P90 4.57, P95 5.62, P99 6.73
    IconButton: P50 3.58, P90 4.89, P95 5.80, P99 7.11
    animated IconButton: P50 4.22, P90 5.98, P95 6.68, P99 8.04
    
    Change-Id: I813bb8c679b31702ab247799926c1cbd652ec8a8
    Test: N/A, updating tests
    Bug: 367610504

---
+++ wear/compose/integration-tests/macrobenchmark-target/src/main/java/androidx/wear/compose/integration/macrobenchmark/target/IconButtonActivity.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.compose.integration.macrobenchmark.target
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.wear.compose.material3.Icon
+import androidx.wear.compose.material3.IconButton
+import androidx.wear.compose.material3.IconButtonDefaults
+import androidx.wear.compose.material3.MaterialTheme
+
+class IconButtonActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Column(
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    repeat(4) {
+                        IconButton(
+                            modifier =
+                                Modifier.semantics {
+                                    contentDescription = numberedContentDescription(it)
+                                },
+                            colors = IconButtonDefaults.filledIconButtonColors(),
+                            onClick = {}
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.ic_favorite_rounded),
+                                contentDescription = null,
+                                modifier = Modifier.size(IconButtonDefaults.DefaultIconSize)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
