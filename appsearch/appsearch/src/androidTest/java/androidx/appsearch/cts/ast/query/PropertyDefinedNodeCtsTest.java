ff1c9c30b3aaf81c2fc9f2d706103cfded5fc28c
Angus Fung <angusfung@google.com>
2024-09-23 18:33:45 +0000

    Implement AST representation of PropertyDefined query function.
    Implement node representing the PropertyDefined query function.
    
    Bug: 356661489
    Bug: 356662352
    
    Relnote: Added node representing the PropertyDefined query function.
    Test: Wrote PropertyDefinedNodeCtsTest to verify correctness.
    Change-Id: I1aeafb0dbb97fa06ecd90df9e60f5cdccc098789

---
+++ appsearch/appsearch/src/androidTest/java/androidx/appsearch/cts/ast/query/PropertyDefinedNodeCtsTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appsearch.cts.ast.query;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.assertThrows;
+
+import androidx.appsearch.app.PropertyPath;
+import androidx.appsearch.ast.query.PropertyDefinedNode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class PropertyDefinedNodeCtsTest {
+    @Test
+    public void testConstructor_throwsOnNullPointer() {
+        assertThrows(NullPointerException.class, () -> new PropertyDefinedNode(null));
+    }
+
+    @Test
+    public void testGetFunctionName_functionNameCorrect() {
+        List<PropertyPath.PathSegment> pathSegmentList = List.of(
+                PropertyPath.PathSegment.create("property"),
+                PropertyPath.PathSegment.create("path"));
+        PropertyPath propertyPath = new PropertyPath(pathSegmentList);
+
+        PropertyDefinedNode propertyDefinedNode =
+                new PropertyDefinedNode(propertyPath);
+        assertThat(propertyDefinedNode.getFunctionName()).isEqualTo("propertyDefined");
+    }
+
+    @Test
+    public void testGetChildren_returnsEmptyList() {
+        List<PropertyPath.PathSegment> pathSegmentList = List.of(
+                PropertyPath.PathSegment.create("property"),
+                PropertyPath.PathSegment.create("path"));
+        PropertyPath propertyPath = new PropertyPath(pathSegmentList);
+
+        PropertyDefinedNode propertyDefinedNode =
+                new PropertyDefinedNode(propertyPath);
+
+        assertThat(propertyDefinedNode.getChildren().isEmpty()).isTrue();
+    }
+
+    @Test
+    public void testGetProperty_returnsCorrectProperty() {
+        List<PropertyPath.PathSegment> pathSegmentList = List.of(
+                PropertyPath.PathSegment.create("property"),
+                PropertyPath.PathSegment.create("path"));
+        PropertyPath propertyPath = new PropertyPath(pathSegmentList);
+
+        PropertyDefinedNode propertyDefinedNode =
+                new PropertyDefinedNode(propertyPath);
+
+        assertThat(propertyDefinedNode.getProperty()).isEqualTo(propertyPath);
+    }
+
+    @Test
+    public void testSetProperty_throwsOnNullPointer() {
+        List<PropertyPath.PathSegment> pathSegmentList = List.of(
+                PropertyPath.PathSegment.create("property"),
+                PropertyPath.PathSegment.create("path"));
+        PropertyPath propertyPath = new PropertyPath(pathSegmentList);
+
+        PropertyDefinedNode propertyDefinedNode =
+                new PropertyDefinedNode(propertyPath);
+
+        assertThrows(NullPointerException.class, () -> propertyDefinedNode.setProperty(null));
+    }
+
+    @Test
+    public void testSetProperty_setsCorrectProperty() {
+        List<PropertyPath.PathSegment> pathSegmentList = List.of(
+                PropertyPath.PathSegment.create("property"),
+                PropertyPath.PathSegment.create("path"));
+        PropertyPath propertyPath = new PropertyPath(pathSegmentList);
+        PropertyDefinedNode propertyDefinedNode =
+                new PropertyDefinedNode(propertyPath);
+
+        List<PropertyPath.PathSegment> anotherPathSegmentList = List.of(
+                PropertyPath.PathSegment.create("another"),
+                PropertyPath.PathSegment.create("property"));
+        PropertyPath anotherProperty = new PropertyPath(anotherPathSegmentList);
+        propertyDefinedNode.setProperty(anotherProperty);
+
+        assertThat(propertyDefinedNode.getProperty()).isEqualTo(anotherProperty);
+    }
+}
