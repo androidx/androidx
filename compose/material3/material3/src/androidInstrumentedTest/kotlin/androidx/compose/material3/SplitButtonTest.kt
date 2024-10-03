4e01a103155ce38d8571a4db934fe656d69fb371
kentfan <kentfan@google.com>
2024-10-03 15:52:14 -0700

    [split-button] Add test case for checked trailing button
    Bug: b/370605854
    Test: test pass
    Change-Id: Ifc2d2c9eb0c3d2d246f0ed075015cf199129dde0

--- compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/SplitButtonTest.kt
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/SplitButtonTest.kt
@@ -20,6 +20,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.Role
@@ -30,10 +34,14 @@ import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.isToggleable
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -82,6 +90,46 @@ class SplitButtonTest {
         rule.onNodeWithTag("trailingButton").assertHasClickAction()
         rule.onNodeWithContentDescription("Leading Icon").assertIsDisplayed()
         rule.onNodeWithContentDescription("Trailing Icon").assertIsDisplayed()
+    }
+
+    @Test
+    fun filledSplitButton_trailingButtonChecked() {
+        rule.setMaterialContent(lightColorScheme()) {
+            var trailingButtonChecked by remember { mutableStateOf(false) }
+
+            SplitButtonLayout(
+                leadingButton = {
+                    SplitButtonDefaults.LeadingButton(
+                        onClick = { /* Do Nothing */ },
+                        modifier = Modifier.testTag("leadingButton")
+                    ) {
+                        Icon(
+                            Icons.Outlined.Edit,
+                            contentDescription = "Leading Icon",
+                        )
+                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                        Text("My Button")
+                    }
+                },
+                trailingButton = {
+                    SplitButtonDefaults.TrailingButton(
+                        modifier = Modifier.size(34.dp).testTag("trailingButton"),
+                        checked = trailingButtonChecked,
+                        onCheckedChange = { trailingButtonChecked = !trailingButtonChecked },
+                    ) {
+                        Icon(Icons.Outlined.KeyboardArrowDown, contentDescription = "Trailing Icon")
+                    }
+                }
+            )
+        }
+
+        rule
+            .onNode(isToggleable())
+            .assert(SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Checkbox))
+            .assertIsEnabled()
+            .assertIsOff()
+            .performClick()
+            .assertIsOn()
     }
 
     @Test
