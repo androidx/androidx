e460823876253a30a5e4f36552446ca42175019b
Don Turner <donturner@google.com>
2024-10-22 10:24:27 +0000

    Improve description of ViewModelStore
    Change-Id: I920e99af6de9a6e591b142f43d7678262bf64c87

--- lifecycle/lifecycle-viewmodel/src/commonMain/kotlin/androidx/lifecycle/ViewModelStore.kt
+++ lifecycle/lifecycle-viewmodel/src/commonMain/kotlin/androidx/lifecycle/ViewModelStore.kt
@@ -20,15 +20,15 @@ import androidx.annotation.RestrictTo
 /**
  * Class to store `ViewModel`s.
  *
- * An instance of `ViewModelStore` must be retained through configuration changes: if an owner of
- * this `ViewModelStore` is destroyed and recreated due to configuration changes, new instance of an
- * owner should still have the same old instance of `ViewModelStore`.
+ * Instances of `ViewModelStore` must be retained through configuration changes. If the owner of a
+ * `ViewModelStore`, typically a [`ViewModelStoreOwner`], is destroyed and recreated due to a
+ * configuration change, the new owner must have the old instance of the `ViewModelStore`.
  *
- * If an owner of this `ViewModelStore` is destroyed and is not going to be recreated, then it
- * should call [clear] on this `ViewModelStore`, so `ViewModel`s would be notified that they are no
- * longer used.
+ * If the owner of a `ViewModelStore` is destroyed and is _not_ going to be recreated, it should
+ * call [`clear`] on this `ViewModelStore` so that The `ViewModel`s stored by it are notified that
+ * they are no longer needed.
  *
- * Use [ViewModelStoreOwner.getViewModelStore] to retrieve a `ViewModelStore` for activities and
+ * Use [`ViewModelStoreOwner.getViewModelStore`] to retrieve a `ViewModelStore` for activities and
  * fragments.
  */
 public open class ViewModelStore {
