androidx/androidx

commit 40560bb649f4d033e5e84861f8ecee1162e0031e

Implement platform layers using UIWindow (#2270)
Use a separate window with `ComposeLayersViewController` to display
platform layers.
Provide `ComposeLayersViewController` via LocalUIVIewController.

Fixes
https://youtrack.jetbrains.com/issue/CMP-8154/Support-UIKitViewController-in-Popups-and-Dialogs

## Release Notes
### Fixes - iOS
- Fix ability to use `UIKitViewController` inside `Popup`s and
`Dialog`s.

compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/scene/ComposeLayersViewController.kt
@@ -1,0 +1,319 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.scene
+
+import androidx.compose.ui.graphics.asComposeCanvas
+import androidx.compose.ui.platform.PlatformWindowContext
+import androidx.compose.ui.uikit.addLayoutConstraintsToMatch
+import androidx.compose.ui.uikit.embedSubview
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.viewinterop.UIKitInteropTransaction
+import androidx.compose.ui.window.ComposeView
+import androidx.compose.ui.window.DisplayLinkListener
+import androidx.compose.ui.window.MetalView
+import kotlin.coroutines.CoroutineContext
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+import kotlinx.cinterop.CValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import org.jetbrains.skia.Canvas
+import platform.CoreGraphics.CGPoint
+import platform.CoreGraphics.CGSize
+import platform.UIKit.UIEvent
+import platform.UIKit.UIScreen
+import platform.UIKit.UIView
+import platform.UIKit.UIViewController
+import platform.UIKit.UIViewControllerTransitionCoordinatorProtocol
+import platform.UIKit.UIWindow
+import platform.UIKit.UIWindowLevelAlert
+import platform.UIKit.UIWindowLevelNormal
+
+/**
+ * A class responsible for managing and rendering [UIKitComposeSceneLayer]s.
+ */
+internal class ComposeLayersViewController(
+    useSeparateRenderThreadWhenPossible: Boolean,
+    private val context: CoroutineContext
+): UIViewController(nibName = null, bundle = null) {
+    val windowContext = PlatformWindowContext()
+
+    private val window = LayersWindow()
+    private var hasViewAppeared = false
+
+    val metalView: MetalView = MetalView(
+        retrieveInteropTransaction = ::retrieveAndMergeInteropTransactions,
+        useSeparateRenderThreadWhenPossible = useSeparateRenderThreadWhenPossible,
+        render = ::render
+    ).apply {
+        canBeOpaque = false
+    }
+
+    private val rootView = ComposeView(
+        useOpaqueConfiguration = false,
+        transparentForTouches = true
+    ).also {
+        it.updateMetalView(
+            metalView = metalView,
+            onLayoutSubviews = { windowContext.updateWindowContainerSize() }
+        )
+    }
+
+    fun withLayers(block: (List<UIKitComposeSceneLayer>) -> Unit) = layersCache.withCopy(block)
+
+    override fun loadView() {
+        this.view = rootView
+    }
+
+    val hasInvalidations: Boolean get() = this.layers.any { it.hasInvalidations }
+
+    private val layers = mutableListOf<UIKitComposeSceneLayer>()
+
+    private val layersCache = CopiedList {
+        it.addAll(this.layers)
+    }
+
+    /**
+     * Transactions of the layers that were imperatively removed before their changes were applied.
+     */
+    private var removedLayersTransactions = mutableListOf<UIKitInteropTransaction>()
+
+    var referenceWindow: UIWindow? = null
+        set(value) {
+            field = value
+            window.windowScene = value?.windowScene
+            window.windowLevel = value?.windowLevel ?: UIWindowLevelNormal
+        }
+
+    private fun show() {
+        windowContext.setWindowContainer(window)
+        window.rootViewController = this
+        window.windowLevel = UIWindowLevelAlert + 1
+        window.makeKeyAndVisible()
+    }
+
+    private fun hide() {
+        window.rootViewController = null
+        window.resignKeyWindow()
+    }
+
+    fun dispose() {
+        // `dispose` is called instead of `close`, because `close` is also used imperatively
+        // to remove the layer from the array based on user interaction.
+        while (this.layers.isNotEmpty()) {
+            val layer = this.layers.removeLast()
+
+            if (hasViewAppeared) {
+                layer.sceneWillDisappear()
+            }
+
+            layer.dispose()
+        }
+
+        hide()
+        rootView.updateMetalView(metalView = null)
+        referenceWindow = null
+    }
+
+    fun attach(layer: UIKitComposeSceneLayer) {
+        val isFirstLayer = layers.isEmpty()
+        layers.add(layer)
+        view.insertSubview(layer.interactionView, belowSubview = metalView)
+        layer.interactionView.addLayoutConstraintsToMatch(view)
+        view.embedSubview(layer.overlayView)
+
+        if (isFirstLayer) {
+            show()
+        }
+        if (hasViewAppeared) {
+            layer.sceneDidAppear()
+        }
+    }
+
+    fun detach(layer: UIKitComposeSceneLayer) {
+        if (hasViewAppeared) {
+            layer.sceneWillDisappear()
+        }
+
+        this.layers.remove(layer)
+
+        // Intercept the actions UIKitInteropTransaction from the layer
+        val transaction = layer.retrieveInteropTransaction()
+
+        if (this.layers.isEmpty()) {
+            // It was the last layer, remove the view and executed the actions immediately
+            hide()
+
+            transaction.actions.fastForEach { it.invoke() }
+
+            // If the layers list is empty, the draw call will clear the canvas.
+            metalView.redrawer.draw(waitUntilCompletion = false)
+        } else {
+            // It wasn't the last layer, pending transactions should be added to the list
+            removedLayersTransactions.add(transaction)
+
+            // Redraw content with layer removed
+            metalView.redrawer.setNeedsRedraw()
+        }
+    }
+
+    override fun viewDidAppear(animated: Boolean) {
+        super.viewDidAppear(animated)
+
+        hasViewAppeared = true
+        this.layers.fastForEach {
+            it.sceneDidAppear()
+        }
+    }
+
+    override fun viewWillDisappear(animated: Boolean) {
+        super.viewWillDisappear(animated)
+
+        hasViewAppeared = false
+        this.layers.fastForEach {
+            it.sceneWillDisappear()
+        }
+    }
+
+    private val hasInteropViews: Boolean
+        get() {
+            layersCache.withCopy { layers ->
+                layers.fastForEach {
+                    if (it.hasInteropViews) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+
+    /**
+     * Iterate through existing layers and merge their interop transactions to be consumed by the
+     * [MetalView], also include transactions of the layers that were removed and are not
+     * present in [layers] anymore.
+     */
+    private fun retrieveAndMergeInteropTransactions(): UIKitInteropTransaction {
+        val removedLayersTransactionsCopy = removedLayersTransactions.toList()
+        removedLayersTransactions.clear()
+
+        val transactions = this.layers.map {
+            it.retrieveInteropTransaction()
+        } + removedLayersTransactionsCopy
+        return UIKitInteropTransaction.merge(
+            transactions = transactions
+        )
+    }
+
+    private fun render(canvas: Canvas, nanoTime: Long) {
+        val composeCanvas = canvas.asComposeCanvas()
+
+        // Some layers may be removed during rendering, because recomposition will happen in the
+        // process, so we need to make a temporary copy of the list
+        layersCache.withCopy { layers ->
+            layers.fastForEach {
+                it.render(composeCanvas, nanoTime)
+            }
+        }
+    }
+
+    override fun viewWillTransitionToSize(
+        size: CValue<CGSize>,
+        withTransitionCoordinator: UIViewControllerTransitionCoordinatorProtocol
+    ) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator)
+
+        if (layers.isEmpty()) {
+            return
+        }
+
+        val duration = withTransitionCoordinator.transitionDuration.toDuration(DurationUnit.SECONDS)
+        if (duration == Duration.ZERO) return
+
+        if (hasInteropViews) {
+            // Heavy interop views may slow down the animation of per-frame screen rotation.
+            // For these cases, a cross-fade transition will be used instead.
+            crossFadeSizeTransition(withTransitionCoordinator)
+        } else {
+            animateSizeTransition(withTransitionCoordinator, duration)
+        }
+    }
+
+    /**
+     * Animates the layout transition of layers.
+     * See [androidx.compose.ui.scene.ComposeHostingViewController.animateSizeTransition]
+     */
+    private fun animateSizeTransition(
+        transitionCoordinator: UIViewControllerTransitionCoordinatorProtocol,
+        duration: Duration,
+    ) {
+        val displayLinkListener = DisplayLinkListener()
+        val sizeTransitionScope = CoroutineScope(context + displayLinkListener.frameClock)
+        displayLinkListener.start()
+
+        animateSizeTransition(sizeTransitionScope, duration)
+
+        transitionCoordinator.animateAlongsideTransition(
+            animation = {},
+            completion = {
+                sizeTransitionScope.cancel()
+                displayLinkListener.invalidate()
+            }
+        )
+    }
+
+    private fun crossFadeSizeTransition(
+        transitionCoordinator: UIViewControllerTransitionCoordinatorProtocol
+    ) {
+        val transitionScope = CoroutineScope(context)
+        val layersAnimationClosure = rootView.animateCrossFadeTransition(transitionScope)
+
+        transitionCoordinator.animateAlongsideTransition(
+            animation = {
+                layersAnimationClosure.invoke()
+            },
+            completion = {
+                transitionScope.cancel()
+            }
+        )
+    }
+
+    fun animateSizeTransition(scope: CoroutineScope, duration: Duration) {
+        if (this.layers.isEmpty()) {
+            return
+        }
+        val animations = listOf(
+            windowContext.prepareAndGetSizeTransitionAnimation()
+        ) + this.layers.map {
+            it.prepareAndGetSizeTransitionAnimation()
+        }
+
+        rootView.animateSizeTransition(scope) {
+            animations.map {
+                scope.launch { it.invoke(duration) }
+            }.joinAll()
+        }
+    }
+}
+
+private class LayersWindow: UIWindow(frame = UIScreen.mainScreen.bounds) {
+    override fun hitTest(point: CValue<CGPoint>, withEvent: UIEvent?): UIView? {
+        return super.hitTest(point, withEvent).takeUnless { it == this }
+    }
+}

compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
@@ -4,7 +4,6 @@ package androidx.compose.mpp.demo
 import androidx.compose.mpp.demo.bugs.IosBugs
 import androidx.compose.mpp.demo.bugs.StartRecompositionCheck
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeUIViewController

compose/ui/ui/api/ui.klib.api
@@ -4991,6 +4991,9 @@ final val androidx.compose.ui.platform/androidx_compose_ui_platform_UiKitPlatfor
 final val androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeHostingViewController$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeHostingViewController$stableprop|#static{}androidx_compose_ui_scene_ComposeHostingViewController$stableprop[0]
 
 // Targets: [ios]
+final val androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeLayersViewController$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeLayersViewController$stableprop|#static{}androidx_compose_ui_scene_ComposeLayersViewController$stableprop[0]
+
+// Targets: [ios]
 final val androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeSceneMediator$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeSceneMediator$stableprop|#static{}androidx_compose_ui_scene_ComposeSceneMediator$stableprop[0]
 
 // Targets: [ios]
@@ -4998,9 +5001,6 @@ final val androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneL
 
 // Targets: [ios]
 final val androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop|#static{}androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop[0]
-
-// Targets: [ios]
-final val androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop|#static{}androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop[0]
 
 // Targets: [ios]
 final val androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop|#static{}androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop[0]
@@ -5198,6 +5198,9 @@ final fun androidx.compose.ui.platform/androidx_compose_ui_platform_UiKitPlatfor
 final fun androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeHostingViewController$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeHostingViewController$stableprop_getter|androidx_compose_ui_scene_ComposeHostingViewController$stableprop_getter(){}[0]
 
 // Targets: [ios]
+final fun androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeLayersViewController$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeLayersViewController$stableprop_getter|androidx_compose_ui_scene_ComposeLayersViewController$stableprop_getter(){}[0]
+
+// Targets: [ios]
 final fun androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeSceneMediator$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_ComposeSceneMediator$stableprop_getter|androidx_compose_ui_scene_ComposeSceneMediator$stableprop_getter(){}[0]
 
 // Targets: [ios]
@@ -5205,9 +5208,6 @@ final fun androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneL
 
 // Targets: [ios]
 final fun androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop_getter|androidx_compose_ui_scene_UIKitComposeSceneLayerView$stableprop_getter(){}[0]
-
-// Targets: [ios]
-final fun androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop_getter|androidx_compose_ui_scene_UIKitComposeSceneLayersHolder$stableprop_getter(){}[0]
 
 // Targets: [ios]
 final fun androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop_getter(): kotlin/Int // androidx.compose.ui.scene/androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop_getter|androidx_compose_ui_scene_UIKitTransparentContainerView$stableprop_getter(){}[0]

compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/interaction/TextFieldFocusOrderTest.kt
@@ -33,6 +33,24 @@ import platform.UIKit.UIView
 
 class TextFieldFocusOrderTest {
     @Test
+    fun testModalTextFieldsFocusOnDialogAppear() = runUIKitInstrumentedTest {
+        val dialogFocusRequester = FocusRequester()
+
+        setContent {
+            TextField("Text 0", {})
+
+            Dialog({}) {
+                TextField("Text 1", {}, modifier = Modifier.focusRequester(dialogFocusRequester))
+                LaunchedEffect(Unit) {
+                    dialogFocusRequester.requestFocus()
+                }
+            }
+        }
+
+        assertEquals("Text 1", findFocusedUITextInput()?.text)
+    }
+
+    @Test
     fun testModalTextFieldsRefocus() = runUIKitInstrumentedTest {
         val showDialog1 = mutableStateOf(false)
         val showDialog2 = mutableStateOf(false)
@@ -49,18 +67,18 @@ class TextFieldFocusOrderTest {
             if (showDialog1.value) {
                 Dialog({}) {
                     TextField("Text 1", {}, modifier = Modifier.focusRequester(focusRequester1))
-                }
-                LaunchedEffect(Unit) {
-                    focusRequester1.requestFocus()
+                    LaunchedEffect(Unit) {
+                        focusRequester1.requestFocus()
+                    }
                 }
             }
 
             if (showDialog2.value) {
                 Dialog({}) {
                     TextField("Text 2", {}, modifier = Modifier.focusRequester(focusRequester2))
-                }
-                LaunchedEffect(Unit) {
-                    focusRequester2.requestFocus()
+                    LaunchedEffect(Unit) {
+                        focusRequester2.requestFocus()
+                    }
                 }
             }
         }
@@ -87,8 +105,8 @@ class TextFieldFocusOrderTest {
     @Test
     fun testModalTextFieldsRefocusWhenParentDismissed() = runUIKitInstrumentedTest {
         val showTextField = mutableStateOf(true)
-        val showDialog1 = mutableStateOf(true)
-        val showDialog2 = mutableStateOf(true)
+        val showDialog1 = mutableStateOf(false)
+        val showDialog2 = mutableStateOf(false)
         val focusRequester0 = FocusRequester()
         val focusRequester1 = FocusRequester()
         val focusRequester2 = FocusRequester()
@@ -104,21 +122,26 @@ class TextFieldFocusOrderTest {
             if (showDialog1.value) {
                 Dialog({}) {
                     TextField("Text 1", {}, modifier = Modifier.focusRequester(focusRequester1))
-                }
-                LaunchedEffect(Unit) {
-                    focusRequester1.requestFocus()
+                    LaunchedEffect(Unit) {
+                        focusRequester1.requestFocus()
+                    }
                 }
             }
 
             if (showDialog2.value) {
                 Dialog({}) {
                     TextField("Text 2", {}, modifier = Modifier.focusRequester(focusRequester2))
-                }
-                LaunchedEffect(Unit) {
-                    focusRequester2.requestFocus()
+                    LaunchedEffect(Unit) {
+                        focusRequester2.requestFocus()
+                    }
                 }
             }
         }
+
+        showDialog1.value = true
+        waitForIdle()
+        showDialog2.value = true
+        waitForIdle()
 
         assertEquals("Text 2", findFocusedUITextInput()?.text)
 
@@ -141,7 +164,7 @@ class TextFieldFocusOrderTest {
     }
 
     private fun UIKitInstrumentedTest.findFocusedUITextInput(): UITextInputProtocol? {
-        val window = hostingViewController.view.window ?: return null
+        val windowScene = hostingViewController.view.window?.windowScene ?: return null
 
         fun findFirstResponder(view: UIView): UIView? {
             if (view.isFirstResponder) {
@@ -153,6 +176,8 @@ class TextFieldFocusOrderTest {
             return null
         }
 
-        return findFirstResponder(view = window) as? UITextInputProtocol
+        return windowScene.windows.reversed().firstNotNullOfOrNull {
+            findFirstResponder(view = it as UIView)
+        } as? UITextInputProtocol
     }
 }

compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/interop/InteropInsidePopupsTest.kt
@@ -1,0 +1,111 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.interop
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.runUIKitInstrumentedTest
+import androidx.compose.ui.test.utils.dpRectInWindow
+import androidx.compose.ui.viewinterop.UIKitView
+import androidx.compose.ui.viewinterop.UIKitViewController
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.Popup
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import platform.UIKit.UIColor
+import platform.UIKit.UIView
+import platform.UIKit.UIViewController
+
+class InteropInsidePopupsTest {
+    @Test
+    fun uiKitViewControllerInsideDialog() = runUIKitInstrumentedTest {
+        val controller = UIViewController()
+        controller.view.backgroundColor = UIColor.redColor
+        setContent {
+            Dialog(
+                onDismissRequest = {},
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                UIKitViewController(
+                    factory = { controller },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+        assertEquals(safeDrawingRect, controller.view.dpRectInWindow())
+        assertNotNull(controller.view.window)
+    }
+
+    @Test
+    fun uiKitViewControllerInsidePopup() = runUIKitInstrumentedTest {
+        val controller = UIViewController()
+        controller.view.backgroundColor = UIColor.redColor
+
+        setContent {
+            Popup(onDismissRequest = {}) {
+                UIKitViewController(
+                    factory = { controller },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+        assertEquals(safeDrawingRect, controller.view.dpRectInWindow())
+        assertNotNull(controller.view.window)
+    }
+
+    @Test
+    fun uiKitViewInsideDialog() = runUIKitInstrumentedTest {
+        val view = UIView()
+        view.backgroundColor = UIColor.redColor
+        setContent {
+            Dialog(
+                onDismissRequest = {},
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                UIKitView(
+                    factory = { view },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+        assertEquals(safeDrawingRect, view.dpRectInWindow())
+        assertNotNull(view.window)
+    }
+
+    @Test
+    fun uiKitViewInsidePopup() = runUIKitInstrumentedTest {
+        val view = UIView()
+        view.backgroundColor = UIColor.redColor
+
+        setContent {
+            Popup(onDismissRequest = {}) {
+                UIKitView(
+                    factory = { view },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+        assertEquals(safeDrawingRect, view.dpRectInWindow())
+        assertNotNull(view.window)
+    }
+}

compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/test/AccessibilityTestNode.kt
@@ -52,6 +52,8 @@ import platform.UIKit.UIAccessibilityTraitToggleButton
 import platform.UIKit.UIAccessibilityTraitUpdatesFrequently
 import platform.UIKit.UIAccessibilityTraits
 import platform.UIKit.UIView
+import platform.UIKit.UIWindow
+import platform.UIKit.UIWindowScene
 import platform.UIKit.accessibilityCustomActions
 import platform.UIKit.accessibilityElementAtIndex
 import platform.UIKit.accessibilityElementCount
@@ -76,32 +78,41 @@ import platform.darwin.NSObject
  */
 @OptIn(ExperimentalForeignApi::class)
 internal fun UIKitInstrumentedTest.getAccessibilityTree(): AccessibilityTestNode {
-    fun buildNode(element: NSObject, level: Int): AccessibilityTestNode {
+    fun buildNode(element: NSObject): AccessibilityTestNode {
         val children = mutableListOf<AccessibilityTestNode>()
         val elements = element.accessibilityElements()
 
         if (elements != null) {
             elements.forEach {
-                children.add(buildNode(it as NSObject, level = level + 1))
+                children.add(buildNode(it as NSObject))
             }
         } else {
             val count = element.accessibilityElementCount()
             if (count == NSIntegerMax) {
                 when {
                     element is UIView -> {
-                        element.subviews.mapNotNull {
-                            children.add(buildNode(it as UIView, level = level + 1))
+                        element.subviews.forEach {
+                            children.add(buildNode(it as UIView))
+                        }
+                    }
+                    element is UIWindowScene -> {
+                        element.windows.forEach {
+                            children.add(buildNode(it as UIWindow))
                         }
                     }
                 }
             } else if (count > 0) {
-                (0 until count).mapNotNull {
+                (0 until count).forEach {
                     val child = element.accessibilityElementAtIndex(it) as NSObject
-                    children.add(buildNode(child, level = level + 1))
+                    children.add(buildNode(child))
                 }
             } else if (element is UIView) {
-                element.subviews.mapNotNull {
-                    children.add(buildNode(it as UIView, level = level + 1))
+                element.subviews.forEach {
+                    children.add(buildNode(it as UIView))
+                }
+            } else if (element is UIWindowScene) {
+                element.windows.forEach {
+                    children.add(buildNode(it as UIWindow))
                 }
             }
         }
@@ -122,7 +133,7 @@ internal fun UIKitInstrumentedTest.getAccessibilityTree(): AccessibilityTestNode
         }
     }
 
-    return buildNode(appDelegate.window!!, 0)
+    return buildNode(appDelegate.window!!.windowScene!!)
 }
 
 private val allAccessibilityTraits = mapOf(

commit 07049c8c2fdaffcba341e9ecfe3707d67654f2b7

Fix keyboard blinks on selection (#2323)
Fix case when popup with selection handles capture focus from text input
view.

Fixes the issue introduced in
https://github.com/JetBrains/compose-multiplatform-core/pull/2270

## Release Notes
N/A

compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/scene/ComposeLayersViewController.kt
@@ -106,12 +106,12 @@ internal class ComposeLayersViewController(
         windowContext.setWindowContainer(window)
         window.rootViewController = this
         window.windowLevel = UIWindowLevelAlert + 1
-        window.makeKeyAndVisible()
+        window.setHidden(false)
     }
 
     private fun hide() {
         window.rootViewController = null
-        window.resignKeyWindow()
+        window.setHidden(true)
     }
 
     fun dispose() {