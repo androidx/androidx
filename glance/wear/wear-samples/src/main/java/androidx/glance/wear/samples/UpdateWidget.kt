08aad6b29de95bbba61e5b79c6be449ae97b814a
Lucas Muller Oliveira <lucasmo@google.com>
2026-01-21 16:36:11 +0000

    Add Widget sample for performing updates.
    Change-Id: I296fe4163bdcfe9dc7a856a2c557d4e57a25d264
    Bug: 446828899
    Test: sample code

---
+++ glance/wear/wear-samples/src/main/java/androidx/glance/wear/samples/UpdateWidget.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.glance.wear.samples
+
+import android.content.Context
+import androidx.compose.remote.creation.compose.layout.RemoteAlignment
+import androidx.compose.remote.creation.compose.layout.RemoteArrangement
+import androidx.compose.remote.creation.compose.layout.RemoteBox
+import androidx.compose.remote.creation.compose.layout.RemoteColumn
+import androidx.compose.remote.creation.compose.layout.RemoteComposable
+import androidx.compose.remote.creation.compose.layout.RemoteText
+import androidx.compose.remote.creation.compose.modifier.RemoteModifier
+import androidx.compose.remote.creation.compose.modifier.fillMaxSize
+import androidx.compose.remote.creation.compose.modifier.wrapContentSize
+import androidx.compose.remote.creation.compose.state.rc
+import androidx.compose.remote.creation.compose.state.rs
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.glance.wear.AssociateWithGlanceWearWidget
+import androidx.glance.wear.GlanceWearWidget
+import androidx.glance.wear.GlanceWearWidgetService
+import androidx.glance.wear.WearWidgetBrush
+import androidx.glance.wear.WearWidgetData
+import androidx.glance.wear.WearWidgetDocument
+import androidx.glance.wear.color
+import androidx.glance.wear.core.ContainerInfo
+import androidx.glance.wear.core.WearWidgetParams
+import androidx.glance.wear.tooling.preview.RectangularLargeWidgetPreviewParams
+import androidx.glance.wear.tooling.preview.WearWidgetPreview
+import java.time.Instant
+
+@AssociateWithGlanceWearWidget(UpdateWidget::class)
+class UpdateWidgetService : GlanceWearWidgetService() {
+    override val widget: GlanceWearWidget = UpdateWidget()
+}
+
+class UpdateWidget : GlanceWearWidget() {
+    override suspend fun provideWidgetData(
+        context: Context,
+        params: WearWidgetParams,
+    ): WearWidgetData {
+        val backgroundColor =
+            when (params.containerType) {
+                ContainerInfo.CONTAINER_TYPE_LARGE -> Color.Blue
+                else -> Color.Yellow
+            }
+        return WearWidgetDocument(WearWidgetBrush.color(backgroundColor.rc)) {
+            UpdateWidgetContent()
+        }
+    }
+}
+
+@RemoteComposable
+@Composable
+fun UpdateWidgetContent() {
+    RemoteBox(modifier = RemoteModifier.fillMaxSize(), contentAlignment = RemoteAlignment.Center) {
+        RemoteColumn(
+            modifier = RemoteModifier.wrapContentSize(),
+            verticalArrangement = RemoteArrangement.Center,
+            horizontalAlignment = RemoteAlignment.CenterHorizontally,
+        ) {
+            val (dateStr, timeStr) = Instant.now().toString().split("T")
+            RemoteText(text = "Last Updated:".rs)
+            RemoteText(text = dateStr.rs)
+            RemoteText(text = timeStr.rs)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UpdateWidgetPreview(
+    @PreviewParameter(RectangularLargeWidgetPreviewParams::class) params: WearWidgetParams
+) = WearWidgetPreview(UpdateWidget(), params = params)
