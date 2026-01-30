06f7c75fb3ca49cd2a0bc69de032886d8b35a6a3
Igor Demin <igor.demin@jetbrains.com>
2026-01-30 15:47:23 +0100

    Fix :navigation:navigation-testing:compileKotlinDesktop in TestNavigatorState.jb.kt
    ```
    > Task  FAILED
    e: file:///home/runner/work/compose-multiplatform-core/compose-multiplatform-core/navigation/navigation-testing/src/jbMain/kotlin/androidx/navigation/testing/TestNavigatorState.jb.kt:46:32 Return type must be specified in explicit API mode.
    ```

--- navigation/navigation-testing/src/jbMain/kotlin/androidx/navigation/testing/TestNavigatorState.jb.kt
+++ navigation/navigation-testing/src/jbMain/kotlin/androidx/navigation/testing/TestNavigatorState.jb.kt
@@ -46,7 +46,7 @@ public actual class TestNavigatorState actual constructor() : NavigatorState() {
     public actual override fun createBackStackEntry(
         destination: NavDestination,
         arguments: SavedState?
-    ) = NavBackStackEntry.create(
+    ): NavBackStackEntry = NavBackStackEntry.create(
         context = navContext,
         destination = destination,
         arguments = arguments,
