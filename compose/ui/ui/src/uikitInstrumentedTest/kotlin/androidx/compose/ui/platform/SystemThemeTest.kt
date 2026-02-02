a96cc35437c8027338b1a457dee5eaa0e29a9630
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2026-02-02 16:26:11 +0100

    Fix system theme change handling (#2735)
    - Read actual value of the `traitCollection.userInterfaceStyle` in
    `ComposeHostingView` and `ComposeHostingViewController`.
    - Ensure that the initial system theme is propagated correctly.
    
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-9707/iOS-window-update-overrideUserInterfaceStyle-value-isSystemInDarkTheme-not-updating-correctly
    
    ## Release Notes
    ### Fixes - iOS
    - Fix an issue where manually overriding the user interface style does
    not propagate properly to the `LocalSystemTheme`.

---
+++ compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/platform/SystemThemeTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.platform
+
+import androidx.compose.ui.LocalSystemTheme
+import androidx.compose.ui.SystemTheme
+import androidx.compose.ui.test.runUIKitInstrumentedTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import platform.UIKit.UIUserInterfaceStyle
+
+class SystemThemeTest {
+
+    @Test
+    fun testInitialOverrideUserInterfaceStyleLight() = runUIKitInstrumentedTest {
+        appDelegate.window?.overrideUserInterfaceStyle = UIUserInterfaceStyle.UIUserInterfaceStyleLight
+        var systemTheme: SystemTheme? = null
+        setContent {
+            systemTheme = LocalSystemTheme.current
+        }
+
+        assertEquals(SystemTheme.Light, systemTheme)
+    }
+
+    @Test
+    fun testInitialOverrideUserInterfaceStyleDark() = runUIKitInstrumentedTest {
+        appDelegate.window?.overrideUserInterfaceStyle = UIUserInterfaceStyle.UIUserInterfaceStyleDark
+        var systemTheme: SystemTheme? = null
+        setContent {
+            systemTheme = LocalSystemTheme.current
+        }
+
+        assertEquals(SystemTheme.Dark, systemTheme)
+    }
+
+    @Test
+    fun testOverrideUserInterfaceStyle() = runUIKitInstrumentedTest {
+        var systemTheme: SystemTheme? = null
+        setContent {
+            systemTheme = LocalSystemTheme.current
+        }
+
+        assertNotNull(systemTheme)
+
+        appDelegate.window?.overrideUserInterfaceStyle =
+            UIUserInterfaceStyle.UIUserInterfaceStyleLight
+        waitUntil("System theme should eventually be Light") { systemTheme == SystemTheme.Light }
+
+        appDelegate.window?.overrideUserInterfaceStyle =
+            UIUserInterfaceStyle.UIUserInterfaceStyleDark
+        waitUntil("System theme should eventually be Dark") { systemTheme == SystemTheme.Dark }
+
+        appDelegate.window?.overrideUserInterfaceStyle =
+            UIUserInterfaceStyle.UIUserInterfaceStyleLight
+        waitUntil("System theme should eventually be Light") { systemTheme == SystemTheme.Light }
+    }
+}
