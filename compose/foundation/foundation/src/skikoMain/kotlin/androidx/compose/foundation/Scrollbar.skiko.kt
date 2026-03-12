561214faed2b48835cca6b83b1cde6509b5c88a0
Vendula Švastalová <vendula.svastalova@jetbrains.com>
2026-03-12 20:14:12 +0100

    Migrate Modifier.scrollbarDrag from Modifier.composed to Modifier.Node (#2853)
    Migrate `Modifier.scrollbarDrag` from `Modifier.composed` to
    `Modifier.Node`
    
    Fixes https://youtrack.jetbrains.com/issue/CMP-9908
    
    ## Release Notes
    N/A

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/Scrollbar.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/Scrollbar.skiko.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
@@ -54,10 +53,14 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.PointerInputScope
+import androidx.compose.ui.input.pointer.SuspendingPointerInputModifierNode
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.node.DelegatingNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Constraints
@@ -835,30 +838,93 @@ private fun Modifier.scrollbarDrag(
     interactionSource: MutableInteractionSource,
     draggedInteraction: MutableState<DragInteraction.Start?>,
     sliderAdapter: SliderAdapter,
-): Modifier = composed {
-    val currentInteractionSource by rememberUpdatedState(interactionSource)
-    val currentDraggedInteraction by rememberUpdatedState(draggedInteraction)
-    val currentSliderAdapter by rememberUpdatedState(sliderAdapter)
+): Modifier = this.then(
+    ScrollbarDragModifierElement(
+        interactionSource = interactionSource,
+        draggedInteraction = draggedInteraction,
+        sliderAdapter = sliderAdapter,
+    )
+)
 
-    pointerInput(Unit) {
-        awaitEachGesture {
-            val down = awaitFirstDown(requireUnconsumed = false)
-            val interaction = DragInteraction.Start()
-            currentInteractionSource.tryEmit(interaction)
-            currentDraggedInteraction.value = interaction
-            currentSliderAdapter.onDragStarted()
-            val isSuccess = drag(down.id) { change ->
-                currentSliderAdapter.onDragDelta(change.positionChange())
-                change.consume()
+private class ScrollbarDragModifierElement(
+    private val interactionSource: MutableInteractionSource,
+    private val draggedInteraction: MutableState<DragInteraction.Start?>,
+    private val sliderAdapter: SliderAdapter,
+) : ModifierNodeElement<ScrollbarDragModifierNode>() {
+    override fun create(): ScrollbarDragModifierNode = ScrollbarDragModifierNode(
+        interactionSource = interactionSource,
+        draggedInteraction = draggedInteraction,
+        sliderAdapter = sliderAdapter,
+    )
+
+    override fun update(node: ScrollbarDragModifierNode) = node.update(
+        interactionSource = interactionSource,
+        draggedInteraction = draggedInteraction,
+        sliderAdapter = sliderAdapter,
+    )
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "scrollbarDrag"
+        properties["interactionSource"] = interactionSource
+        properties["draggedInteraction"] = draggedInteraction
+        properties["sliderAdapter"] = sliderAdapter
+    }
+
+    override fun hashCode(): Int {
+        var result = interactionSource.hashCode()
+        result = 31 * result + draggedInteraction.hashCode()
+        result = 31 * result + sliderAdapter.hashCode()
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ScrollbarDragModifierElement) return false
+        return interactionSource == other.interactionSource &&
+            draggedInteraction == other.draggedInteraction &&
+            sliderAdapter == other.sliderAdapter
+    }
+}
+
+private class ScrollbarDragModifierNode(
+    private var interactionSource: MutableInteractionSource,
+    private var draggedInteraction: MutableState<DragInteraction.Start?>,
+    private var sliderAdapter: SliderAdapter,
+) : DelegatingNode() {
+
+    private val pointerInputNode = delegate(
+        SuspendingPointerInputModifierNode(
+            pointerInputEventHandler = {
+                awaitEachGesture {
+                    val down = awaitFirstDown(requireUnconsumed = false)
+                    val interaction = DragInteraction.Start()
+                    interactionSource.tryEmit(interaction)
+                    draggedInteraction.value = interaction
+                    sliderAdapter.onDragStarted()
+                    val isSuccess = drag(down.id) { change ->
+                        sliderAdapter.onDragDelta(change.positionChange())
+                        change.consume()
+                    }
+                    val finishInteraction = if (isSuccess) {
+                        DragInteraction.Stop(interaction)
+                    } else {
+                        DragInteraction.Cancel(interaction)
+                    }
+                    interactionSource.tryEmit(finishInteraction)
+                    draggedInteraction.value = null
+                }
             }
-            val finishInteraction = if (isSuccess) {
-                DragInteraction.Stop(interaction)
-            } else {
-                DragInteraction.Cancel(interaction)
-            }
-            currentInteractionSource.tryEmit(finishInteraction)
-            currentDraggedInteraction.value = null
-        }
+        )
+    )
+
+    fun update(
+        interactionSource: MutableInteractionSource,
+        draggedInteraction: MutableState<DragInteraction.Start?>,
+        sliderAdapter: SliderAdapter,
+    ) {
+        this.interactionSource = interactionSource
+        this.draggedInteraction = draggedInteraction
+        this.sliderAdapter = sliderAdapter
     }
 }
 
