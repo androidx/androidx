4dcc5c86387a1d9799c892255df3cdab6e1b26cf
Naomi <rightnao@google.com>
2026-06-26 17:54:52 +0000

    Promote HorizontalCenteredHeroCarousel to stable API
    Promotes HorizontalCenteredHeroCarousel in compose/material3/material3 to stable by removing the @ExperimentalMaterial3Api annotation.
    
    Updated public API and native ABI signature files.
    
    Test: ./gradlew :compose:material3:material3:compileAndroidMain
    Relnote: Promoted HorizontalCenteredHeroCarousel to stable API.
    Change-Id: I4e0fabf3d98fe8417b0d4f33aa5e1dc018813321

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/carousel/Carousel.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/carousel/Carousel.kt
@@ -35,7 +35,6 @@ import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -236,7 +235,6 @@ fun HorizontalUncontainedCarousel(
  *   last one. Use [itemSpacing] to add spacing between the items.
  * @param content The carousel's content Composable
  */
-@ExperimentalMaterial3Api
 @Composable
 fun HorizontalCenteredHeroCarousel(
     state: CarouselState,
