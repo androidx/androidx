fedcab46c1d1a04ed1a145354e806b13c5ed8815
Marcello Galhardo <mgalhardo@google.com>
2026-07-01 15:36:23 +0100

    Add ExperimentalNavigationEventApi annotation
    Add the @ExperimentalNavigationEventApi annotation to the library.
    This provides a standard way to mark new or unstable declarations
    within the navigationevent API, warning consumers that the behavior
    or signature may change in the future and requiring explicit opt-in.
    
    RelNote: "Add ExperimentalNavigationEventApi annotation to allow
    explicit opt-in for new and unstable APIs."
    
    Bug: N/A
    Test: N/A
    Change-Id: Ia5d25fb1adf652a55d3c29740a5f4689b0f0a2de

---
+++ navigationevent/navigationevent/src/commonMain/kotlin/androidx/navigationevent/Annotations.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.navigationevent
+
+import kotlin.RequiresOptIn
+
+/**
+ * Marks declarations that are still **experimental** in the NavigationEvent API, which means that
+ * the design of the corresponding declarations has open issues which may (or may not) lead to
+ * changes in the future. There is a chance that these declarations will be deprecated in the near
+ * future, or their behavior and semantics may change in a way that breaks existing code.
+ */
+@MustBeDocumented
+@Retention(value = AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.LOCAL_VARIABLE,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.TYPEALIAS,
+)
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+public annotation class ExperimentalNavigationEventApi
