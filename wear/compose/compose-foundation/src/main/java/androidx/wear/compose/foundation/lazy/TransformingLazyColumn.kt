1b22faed15844fcc016e82051e49eab33485d764
Tianming Xu <tianmingxu@google.com>
2026-07-06 06:21:02 -0700

    Revert "Use ScrollableArea in TLC"
    This reverts commit bd28fac528ff600fdebbac5251c930b46878be0e.
    
    Reason for revert: ScrollableArea will clip the content when AlertDialog has a padding modifier.
    
    Bug: b/444409304
    Change-Id: If5afe48e53daa83fd8a48bae21b5a2defdc7465c

--- wear/compose/compose-foundation/src/main/java/androidx/wear/compose/foundation/lazy/TransformingLazyColumn.kt
+++ wear/compose/compose-foundation/src/main/java/androidx/wear/compose/foundation/lazy/TransformingLazyColumn.kt
@@ -25,14 +25,15 @@ import androidx.compose.foundation.OverscrollEffect
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.layout.LazyLayout
 import androidx.compose.foundation.lazy.layout.LazyLayoutIntervalContent
 import androidx.compose.foundation.lazy.layout.LazyLayoutItemProvider
 import androidx.compose.foundation.lazy.layout.getDefaultLazyLayoutKey
+import androidx.compose.foundation.overscroll
 import androidx.compose.foundation.rememberOverscrollEffect
-import androidx.compose.foundation.scrollableArea
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -198,6 +199,12 @@ public fun TransformingLazyColumn(
             coroutineScope = coroutineScope,
             reverseLayout = reverseLayout,
         )
+    val reverseDirection =
+        ScrollableDefaults.reverseDirection(
+            LocalLayoutDirection.current,
+            Orientation.Vertical,
+            reverseScrolling = reverseLayout,
+        )
 
     val semanticState = remember(state) { TransformingLazyColumnSemanticState(state) }
     val focusRequester = remember { FocusRequester() }
@@ -227,13 +234,14 @@ public fun TransformingLazyColumn(
                     userScrollEnabled = userScrollEnabled,
                     reverseScrolling = reverseLayout,
                 )
-                .scrollableArea(
+                .overscroll(overscrollEffect)
+                .scrollable(
                     state = state,
+                    reverseDirection = reverseDirection,
                     enabled = userScrollEnabled,
                     orientation = Orientation.Vertical,
                     flingBehavior = flingBehavior,
                     overscrollEffect = overscrollEffect,
-                    reverseScrolling = reverseLayout,
                 )
                 .then(
                     if (
