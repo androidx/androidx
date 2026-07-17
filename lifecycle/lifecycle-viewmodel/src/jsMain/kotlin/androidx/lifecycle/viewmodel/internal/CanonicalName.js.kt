ef3854315baf03dadb8d49cea8dd21ee27b87a03
Marcello Galhardo <mgalhardo@google.com>
2026-07-17 14:48:00 +0200

    Use qualifiedName in ViewModel
    With Kotlin 2.3.0, qualifiedName is supported by default in K/Wasm.
    
    Bug: 470109441
    Test: existing passes
    Change-Id: Ic2a9b057c86c246c1f70d6d17aa1cda1e4b67f62

--- lifecycle/lifecycle-viewmodel/src/jsMain/kotlin/androidx/lifecycle/viewmodel/internal/CanonicalName.js.kt
+++ lifecycle/lifecycle-viewmodel/src/jsMain/kotlin/androidx/lifecycle/viewmodel/internal/CanonicalName.js.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Android Open Source Project
+ * Copyright 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,5 +18,6 @@ package androidx.lifecycle.viewmodel.internal
 
 import kotlin.reflect.KClass
 
+// TODO(KT-34534): `qualifiedName` reflection API is not supported in K/JS.
 internal actual val KClass<*>?.canonicalName: String?
-    get() = this?.simpleName // `qualifiedName` reflection API is not supported yet in Web.
+    get() = this?.simpleName
