9a2ea838945880d16400d9791a1e75a4edfc9d45
Max Alfonso-Ying <maxying@google.com>
2026-07-22 21:12:16 +0000

    Make exposed dropdown menu extension and stabilize
    - EDM is now an extension function, following the
      recommended pattern for composables, not a member
      of the scope.
    - EDM APIs have been promoted.
    - Deprecated experimental APIs that have been in
      at least one stable release have been removed.
    
    Bug: b/356452026
    Test: existing tests pass
    Relnote: "`ExposedDropdownMenu` is now an extension function on `ExposedDropdownMenuBoxScope` instead of a member. You may need to update your code with a new import."
    Change-Id: Ie8a65e982bcc6ee222ce0a84ed63ad9445760f6d

--- xr/compose/integration-tests/testapp/src/main/kotlin/androidx/xr/compose/testapp/spatialarrangementusage/SpatialArrangementUsageActivity.kt
+++ xr/compose/integration-tests/testapp/src/main/kotlin/androidx/xr/compose/testapp/spatialarrangementusage/SpatialArrangementUsageActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
