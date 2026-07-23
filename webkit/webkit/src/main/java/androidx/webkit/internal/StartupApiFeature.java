b623ffb45b2986825225d5e719e79381c5aa26d9
Peter Pakkenberg <pbirk@google.com>
2026-07-23 14:25:12 +0000

    Create test for forgotten DEV_SUFFIX
    This test will prevent any feature constants for WebView API features
    from being made public until the WebView version used by the test
    runners supports the feature in production builds, i.e. without the
    DEV_SUFFIX.
    
    Test: Adding new test
    Change-Id: I3ec212c40643b35c9bae493c2fa9f6e2be6b6f96

--- webkit/webkit/src/main/java/androidx/webkit/internal/StartupApiFeature.java
+++ webkit/webkit/src/main/java/androidx/webkit/internal/StartupApiFeature.java
@@ -76,6 +76,11 @@ public abstract class StartupApiFeature {
         return mPublicFeatureValue;
     }
 
+    @VisibleForTesting
+    public @NonNull String getInternalFeatureValueForTesting() {
+        return mInternalFeatureValue;
+    }
+
     /**
      * Return whether this feature is supported.
      */
