dd172009d502b233aab3a1492945ec1f210fff99
Siyuan Ren <rensiyuan@google.com>
2026-07-30 16:46:12 +0000

    Add restricted parser helper for OpenID4VCI selection metadata
    Added OpenId4VciSelectionMetadata class in registry-digitalcredentials-openid
    module to parse the selection metadata (eidx and ridx) returned by the
    matcher during VCI flow.
    
    Also added selectedEntryMetadata extension property to
    ProviderCreateCredentialRequest in registry-provider to expose this
    metadata to the provider.
    
    Both APIs are annotated with @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
    as they are currently only intended for internal/group use (e.g. by CMWallet
    demo).
    
    Test: ./gradlew :credentials:registry:registry-digitalcredentials-openid:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=androidx.credentials.registry.digitalcredentials.openid4vci.OpenId4VciSelectionMetadataTest
    Relnote: N/A
    Bug: 469975180
    
    TAG=agy
    CONV=115f7a46-8209-4634-aef7-916eb071c9d6
    
    Change-Id: Ibb3b6802791d741d51efd643302707d2e82a27bf

---
+++ credentials/registry/registry-digitalcredentials-openid/src/androidTest/java/androidx/credentials/registry/digitalcredentials/openid4vci/OpenId4VciSelectionMetadataTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.credentials.registry.digitalcredentials.openid4vci
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import org.json.JSONException
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+public class OpenId4VciSelectionMetadataTest {
+
+    @Test
+    fun parse_validJson_success() {
+        val json = """{"eidx":1,"ridx":2}"""
+        val metadata = OpenId4VciSelectionMetadata.parse(json)
+        assertThat(metadata.entryIndex).isEqualTo(1)
+        assertThat(metadata.requestIndex).isEqualTo(2)
+    }
+
+    @Test
+    fun parse_malformedJson_throwsJSONException() {
+        val json = """{"eidx":1,"ridx"}"""
+        assertThrows(JSONException::class.java) { OpenId4VciSelectionMetadata.parse(json) }
+    }
+
+    @Test
+    fun parse_missingEntryIndex_throwsJSONException() {
+        val json = """{"ridx":2}"""
+        assertThrows(JSONException::class.java) { OpenId4VciSelectionMetadata.parse(json) }
+    }
+
+    @Test
+    fun parse_missingRequestIndex_throwsJSONException() {
+        val json = """{"eidx":1}"""
+        assertThrows(JSONException::class.java) { OpenId4VciSelectionMetadata.parse(json) }
+    }
+
+    @Test
+    fun parse_invalidValueType_throwsJSONException() {
+        val json = """{"eidx":"one","ridx":2}"""
+        assertThrows(JSONException::class.java) { OpenId4VciSelectionMetadata.parse(json) }
+    }
+}
