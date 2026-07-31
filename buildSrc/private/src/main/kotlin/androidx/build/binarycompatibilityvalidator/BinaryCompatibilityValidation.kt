23d96c6587c334b2830fe0ff886ed5184e83d04e
Samuel Freilich <sfreilich@google.com>
2026-07-31 11:48:07 -0700

    Add new ExperimentalInkCrossPlatformRenderingApi to opt-in annotation list
    Change-Id: I18adb83a5ee556f929770ef5019e33409c057efe

--- buildSrc/private/src/main/kotlin/androidx/build/binarycompatibilityvalidator/BinaryCompatibilityValidation.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/binarycompatibilityvalidator/BinaryCompatibilityValidation.kt
@@ -396,6 +396,7 @@ private val nonPublicMarkers =
         "androidx.ink.brush.ExperimentalInkBrushCompatibilityApi",
         "androidx.ink.brush.ExperimentalInkCustomBrushApi",
         "androidx.ink.strokes.ExperimentalInkEraserApi",
+        "androidx.ink.rendering.ExperimentalInkCrossPlatformRenderingApi",
         "androidx.ink.nativeloader.InkInternalOnlyApi",
         "androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi",
         "androidx.paging.ExperimentalPagingApi",
