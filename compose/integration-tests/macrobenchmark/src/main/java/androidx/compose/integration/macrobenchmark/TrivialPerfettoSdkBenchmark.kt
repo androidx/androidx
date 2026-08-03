aae9502751c37a959b9660e4702efe52d2da0e5f
Rahul Ravikumar <rahulrav@google.com>
2026-08-03 11:31:22 -0700

    Improve recomposition tracing tests.
    * This should make the tests much faster.
    
    Test: Updated tests
    Change-Id: I169b45ea128e9942114f66140e7bd915663d4326

--- compose/integration-tests/macrobenchmark/src/main/java/androidx/compose/integration/macrobenchmark/TrivialPerfettoSdkBenchmark.kt
+++ compose/integration-tests/macrobenchmark/src/main/java/androidx/compose/integration/macrobenchmark/TrivialPerfettoSdkBenchmark.kt
@@ -17,69 +17,88 @@
 package androidx.compose.integration.macrobenchmark
 
 import android.content.Intent
-import android.os.Build
+import androidx.benchmark.DeviceInfo
 import androidx.benchmark.ExperimentalBenchmarkConfigApi
+import androidx.benchmark.InProcessTracingMode
+import androidx.benchmark.Outputs
+import androidx.benchmark.ShellFile
 import androidx.benchmark.macro.ExperimentalMetricApi
-import androidx.benchmark.macro.TraceSectionMetric
-import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.benchmark.perfetto.ExperimentalPerfettoCaptureApi
 import androidx.benchmark.perfetto.PerfettoCapture
+import androidx.benchmark.perfetto.PerfettoCaptureWrapper
+import androidx.benchmark.runSingleSessionServer
+import androidx.benchmark.traceprocessor.TraceProcessor
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.filters.SdkSuppress
+import androidx.test.uiautomator.uiAutomator
 import junit.framework.TestCase.assertTrue
-import org.junit.Rule
+import org.junit.Assert.assertEquals
+import org.junit.Assume.assumeTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
 
 @LargeTest
-@RunWith(Parameterized::class)
+@RunWith(AndroidJUnit4::class)
 /**
  * End-to-end test for compose-runtime-tracing verifying that names of Composables show up in a
  * Perfetto trace.
  */
-@OptIn(ExperimentalMetricApi::class, ExperimentalBenchmarkConfigApi::class)
-@SdkSuppress(minSdkVersion = Build.VERSION_CODES.R) // TODO(234351579): Support API < 30
-class TrivialPerfettoSdkBenchmark(private val composableName: String) {
-    @get:Rule val benchmarkRule = MacrobenchmarkRule()
+@OptIn(
+    ExperimentalMetricApi::class,
+    ExperimentalBenchmarkConfigApi::class,
+    ExperimentalPerfettoCaptureApi::class,
+)
+class TrivialPerfettoSdkBenchmark {
 
-    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.R) // TODO(234351579): Support API < 30
+    @Before
+    fun checkDeviceSupport() {
+        assumeTrue(DeviceInfo.expectedToSupportTracingInTests)
+    }
+
     @Test
     fun test_composable_names_present_in_trace() {
-        val metrics =
-            listOf(
-                TraceSectionMetric(
-                    "%$PACKAGE_NAME.$composableName %$FILE_NAME:%",
-                    TraceSectionMetric.Mode.First,
-                )
+        val traceFiles =
+            trace(packageName = PACKAGE_NAME) {
+                uiAutomator {
+                    val intent = Intent(ACTION).apply { setPackage(PACKAGE_NAME) }
+                    startActivityIntent(intent)
+                }
+            }
+        assertTrue(traceFiles.isNotEmpty())
+        assertEquals(1, traceFiles.size)
+        val traceFile = traceFiles.first()
+        // Copy the file to a directory usable by the test.
+        val copiedPath =
+            Outputs.writeFile("temp.pb") { file ->
+                val bytes = ShellFile(traceFile).readBytes()
+                file.writeBytes(bytes)
+            }
+        val sliceNames = COMPOSABLE_NAMES.map { name -> "%$PACKAGE_NAME.$name %$FILE_NAME:%" }
+        val slices =
+            TraceProcessor.runSingleSessionServer(copiedPath) {
+                querySlices(*sliceNames.toTypedArray(), packageName = null).map { it.name }
+            }
+        assertTrue(slices.isNotEmpty())
+        assertEquals(3, slices.size)
+    }
+
+    internal inline fun trace(packageName: String, block: () -> Unit): List<String> {
+        val wrapper = PerfettoCaptureWrapper()
+        val config =
+            PerfettoCapture.TracingLibraryConfig(
+                targetPackage = packageName,
+                inProcessTracingMode = InProcessTracingMode.Require,
             )
-        benchmarkRule.measureRepeated(
-            packageName = PACKAGE_NAME,
-            metrics = metrics,
-            iterations = 1, // we are only verifying the presence of entries (not the timing data)
-            setupBlock = {
-                PerfettoCapture()
-                    .enableAndroidxTracingPerfetto(
-                        PerfettoCapture.TracingLibraryConfig(
-                            PACKAGE_NAME,
-                            PerfettoCapture.TracingLibraryConfig.InitialProcessState.Alive,
-                        )
-                    )
-                    .let { (resultCode, _) ->
-                        assertTrue(
-                            "Ensuring Perfetto SDK is enabled",
-                            resultCode in arrayOf(1, 2), // 1 = success, 2 = already enabled
-                        )
-                    }
-            },
-        ) {
-            startActivityAndWait(Intent(ACTION))
-        }
+        val start = wrapper.startInProcessTracing(config = config)
+        assertTrue("Unable to start in-process tracing for $packageName", start.isSuccess())
+        block()
+        val traceFiles = wrapper.stopInProcessTracing(config)
+        return traceFiles
     }
 
     companion object {
         private const val PACKAGE_NAME = "androidx.compose.integration.macrobenchmark.target"
-
         private const val ACTION =
             "androidx.compose.integration.macrobenchmark.target.TRIVIAL_TRACING_ACTIVITY"
 
@@ -91,7 +110,5 @@ class TrivialPerfettoSdkBenchmark(private val composableName: String) {
                 "Bar_4888EA32_ABC5_4550_BA78_1247FEC1AAC9",
                 "Baz_609801AB_F5A9_47C3_94蛸5_2E82542F21B8",
             )
-
-        @JvmStatic @Parameters(name = "{0}") fun parameters() = COMPOSABLE_NAMES
     }
 }
