ef1f3a2b01d09e15eb0b0a9a9c5a589da6a355f0
Vlad Nicolaescu <vladnicolaescu@google.com>
2026-08-12 16:14:39 +0000

    Add skeleton for GlanceAdaptiveWidgetManager
    This CL introduces the initial concrete skeleton for GlanceAdaptiveWidgetManager
    and its associated delegation architecture (BaseWidgetDelegate implementing
    GlanceAdaptiveWidgetDelegate).
    
    It includes a placeholder for pushUpdate.
    
    Bug: 545974608
    Test: ./gradlew :glance:adaptive:adaptive-core:test
    Relnote: N/A
    Change-Id: I49edd3cefe0668405bc82589412dc5510d8d2005

---
+++ glance/adaptive/adaptive-core/src/test/kotlin/androidx/glance/adaptive/core/BaseWidgetDelegateTest.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.glance.adaptive.core
+
+import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@Config(sdk = [Config.TARGET_SDK])
+@RunWith(RobolectricTestRunner::class)
+class BaseWidgetDelegateTest {
+
+    @Test
+    fun baseWidgetDelegate_placeholdersDoNotThrow() = runTest {
+        val delegate = BaseWidgetDelegate(ApplicationProvider.getApplicationContext())
+        delegate.pushUpdate()
+    }
+}
