c329a2ea64bf9d86a068502c0672cbc7576c1603
Gilad Amar <giladamar@google.com>
2026-08-18 12:51:23 +0300

    Add MaterialImageComponent to material3-a2ui Implements the Image component for the A2UI catalog, translating the URL, variant, and fit parameters into standard Compose layout boundaries, relying on an injected LocalA2uiImageProvider for asynchronous image loading decoupled from AndroidX core. Relnote: Added `MaterialImageComponent` to support rendering images in the A2UI catalog. Test: MaterialImageComponentTest TAG=agy
    Change-Id: I868feffde3d322619fd423fdf886ee7f4403a865

---
+++ compose/material3/material3-a2ui/src/androidTest/kotlin/androidx/compose/material3/a2ui/MaterialImageComponentFitTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.material3.a2ui
+
+import androidx.a2ui.compose.ui.A2uiCatalog
+import androidx.a2ui.compose.ui.testing.A2uiTestController
+import androidx.a2ui.compose.ui.testing.A2uiTestSurface
+import androidx.a2ui.model.catalog.functions.A2uiFormatStringFunction
+import androidx.a2ui.model.protocol.A2uiComponentPayload
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.v2.runComposeUiTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@OptIn(ExperimentalTestApi::class)
+@RunWith(Parameterized::class)
+class MaterialImageComponentFitTest(private val fitTestParam: FitTestParam) {
+
+    var capturedUrl: String? = null
+    var capturedContentScale: ContentScale? = null
+
+    private val fakeImageRenderer =
+        A2uiImageRenderer { url, contentDescription, contentScale, modifier, onError ->
+            capturedUrl = url
+            capturedContentScale = contentScale
+        }
+
+    private val testCatalog =
+        A2uiCatalog(
+            catalogId = "test_catalog",
+            components = listOf(MaterialImageComponent(fakeImageRenderer)),
+            functions = listOf(A2uiFormatStringFunction.INSTANCE),
+        )
+
+    @Test
+    fun fitProperty_passedToRendererAsContentScale() = runComposeUiTest {
+        val payload =
+            A2uiComponentPayload(
+                id = "root",
+                type = "Image",
+                properties =
+                    mapOf("url" to "https://test.com/img.png", "fit" to fitTestParam.fitToken),
+            )
+        val controller =
+            A2uiTestController(catalog = testCatalog, initialComponents = listOf(payload))
+        val surface = controller.start()
+
+        setContent { MaterialTheme { A2uiTestSurface(surface) } }
+
+        waitForIdle()
+
+        assertThat(capturedUrl).isEqualTo("https://test.com/img.png")
+        assertThat(capturedContentScale).isEqualTo(fitTestParam.expectedContentScale)
+    }
+
+    enum class FitTestParam(val fitToken: String, val expectedContentScale: ContentScale) {
+        Cover("cover", ContentScale.Crop),
+        Contain("contain", ContentScale.Fit),
+        Fill("fill", ContentScale.FillBounds),
+        None("none", ContentScale.None),
+        ScaleDown("scaleDown", ContentScale.Inside),
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun data(): Collection<FitTestParam> {
+            return listOf(
+                FitTestParam.Cover,
+                FitTestParam.Contain,
+                FitTestParam.Fill,
+                FitTestParam.None,
+                FitTestParam.ScaleDown,
+            )
+        }
+    }
+}
