87a434278d99c180b2cc547134b999797c515399
Yuri Schimke <yschimke@google.com>
2026-08-19 11:20:26 +0000

    Add shape helpers and RemoteOffset overloads to RemotePathScope
    Add higher-level shape helper functions (addRect, addOval, addCircle,
    addRoundRect, addArc, addPath) and RemoteOffset / RemoteSize overloads to
    RemotePathScope to improve consistency with Compose UI path building.
    
    Test: ./gradlew :compose:remote:remote-creation-compose:test
    Relnote: "Added shape helpers (`addRect`, `addOval`, `addCircle`, `addRoundRect`, `addArc`, `addPath`) and `RemoteOffset`/`RemoteSize` overloads to `RemotePathScope`."
    TAG=agy
    CONV=9b6b10cf-ac1f-4deb-9757-5666651cc209
    
    Change-Id: I14972b389d644bebe3d33b38e43b46bd9bea9dfb

--- compose/remote/remote-creation-compose/src/main/java/androidx/compose/remote/creation/compose/vector/RemotePathScope.kt
+++ compose/remote/remote-creation-compose/src/main/java/androidx/compose/remote/creation/compose/vector/RemotePathScope.kt
@@ -16,8 +16,12 @@
 
 package androidx.compose.remote.creation.compose.vector
 
+import androidx.annotation.RestrictTo
+import androidx.compose.remote.creation.compose.layout.RemoteOffset
+import androidx.compose.remote.creation.compose.layout.RemoteSize
 import androidx.compose.remote.creation.compose.state.RemoteBoolean
 import androidx.compose.remote.creation.compose.state.RemoteFloat
+import androidx.compose.remote.creation.compose.state.rf
 import kotlin.collections.ArrayList
 
 /** Provides a DSL to describe a vector path. */
@@ -48,6 +52,15 @@ public class RemotePathScope constructor() {
     }
 
     /**
+     * Starts a new contour at [point].
+     *
+     * @param point The start position of the new contour
+     */
+    public fun moveTo(point: RemoteOffset) {
+        moveTo(point.x, point.y)
+    }
+
+    /**
      * Starts a new contour at the offset ([dx], [dy]) relative to the last path position.
      *
      * @param dx The x offset of the start of the new contour, relative to the last path position
@@ -55,6 +68,15 @@ public class RemotePathScope constructor() {
      */
     public fun moveToRelative(dx: RemoteFloat, dy: RemoteFloat) {
         _nodes.add(RemotePathNode.RelativeMoveTo(dx, dy))
+    }
+
+    /**
+     * Starts a new contour at [offset] relative to the last path position.
+     *
+     * @param offset The offset of the start of the new contour, relative to the last path position
+     */
+    public fun moveToRelative(offset: RemoteOffset) {
+        moveToRelative(offset.x, offset.y)
     }
 
     /**
@@ -69,6 +91,16 @@ public class RemotePathScope constructor() {
     }
 
     /**
+     * Adds a line from the last point to [point]. If no contour has been created by calling
+     * [moveTo] first, the origin of the line is set to (0, 0).
+     *
+     * @param point The end position of the line
+     */
+    public fun lineTo(point: RemoteOffset) {
+        lineTo(point.x, point.y)
+    }
+
+    /**
      * Adds a line from the last point to the offset ([dx], [dy]) relative to the last point. If no
      * contour has been created by calling [moveTo] first, the origin of the line is set to (0, 0).
      *
@@ -77,6 +109,16 @@ public class RemotePathScope constructor() {
      */
     public fun lineToRelative(dx: RemoteFloat, dy: RemoteFloat) {
         _nodes.add(RemotePathNode.RelativeLineTo(dx, dy))
+    }
+
+    /**
+     * Adds a line from the last point to [offset] relative to the last point. If no contour has
+     * been created by calling [moveTo] first, the origin of the line is set to (0, 0).
+     *
+     * @param offset The offset of the end of the line, relative to the last path position
+     */
+    public fun lineToRelative(offset: RemoteOffset) {
+        lineToRelative(offset.x, offset.y)
     }
 
     /**
@@ -135,6 +177,18 @@ public class RemotePathScope constructor() {
      * @param x3 The x coordinate of the end point of the cubic curve
      * @param y3 The y coordinate of the end point of the cubic curve
      */
+    /**
+     * Adds a cubic Bézier from the last point to the position ([x3], [y3]), approaching the control
+     * points ([x1], [y1]) and ([x2], [y2]). If no contour has been created by calling [moveTo]
+     * first, the origin of the curve is set to (0, 0).
+     *
+     * @param x1 The x coordinate of the first control point of the cubic curve
+     * @param y1 The y coordinate of the first control point of the cubic curve
+     * @param x2 The x coordinate of the second control point of the cubic curve
+     * @param y2 The y coordinate of the second control point of the cubic curve
+     * @param x3 The x coordinate of the end point of the cubic curve
+     * @param y3 The y coordinate of the end point of the cubic curve
+     */
     public fun curveTo(
         x1: RemoteFloat,
         y1: RemoteFloat,
@@ -144,6 +198,19 @@ public class RemotePathScope constructor() {
         y3: RemoteFloat,
     ) {
         _nodes.add(RemotePathNode.CurveTo(x1, y1, x2, y2, x3, y3))
+    }
+
+    /**
+     * Adds a cubic Bézier from the last point to [end], approaching the control points [control1]
+     * and [control2]. If no contour has been created by calling [moveTo] first, the origin of the
+     * curve is set to (0, 0).
+     *
+     * @param control1 The first control point of the cubic curve
+     * @param control2 The second control point of the cubic curve
+     * @param end The end point of the cubic curve
+     */
+    public fun curveTo(control1: RemoteOffset, control2: RemoteOffset, end: RemoteOffset) {
+        curveTo(control1.x, control1.y, control2.x, control2.y, end.x, end.y)
     }
 
     /**
@@ -173,6 +240,31 @@ public class RemotePathScope constructor() {
         dy3: RemoteFloat,
     ) {
         _nodes.add(RemotePathNode.RelativeCurveTo(dx1, dy1, dx2, dy2, dx3, dy3))
+    }
+
+    /**
+     * Adds a cubic Bézier from the last point to [endOffset] relative to the last point,
+     * approaching control points [control1Offset] and [control2Offset] relative to the last point.
+     *
+     * @param control1Offset The offset of the first control point, relative to the last path
+     *   position
+     * @param control2Offset The offset of the second control point, relative to the last path
+     *   position
+     * @param endOffset The offset of the end point, relative to the last path position
+     */
+    public fun curveToRelative(
+        control1Offset: RemoteOffset,
+        control2Offset: RemoteOffset,
+        endOffset: RemoteOffset,
+    ) {
+        curveToRelative(
+            control1Offset.x,
+            control1Offset.y,
+            control2Offset.x,
+            control2Offset.y,
+            endOffset.x,
+            endOffset.y,
+        )
     }
 
     /**
@@ -235,6 +327,18 @@ public class RemotePathScope constructor() {
     }
 
     /**
+     * Adds a quadratic Bézier from the last point to [end], approaching control point [control]. If
+     * no contour has been created by calling [moveTo] first, the origin of the curve is set to (0,
+     * 0).
+     *
+     * @param control The control point of the quadratic curve
+     * @param end The end point of the quadratic curve
+     */
+    public fun quadTo(control: RemoteOffset, end: RemoteOffset) {
+        quadTo(control.x, control.y, end.x, end.y)
+    }
+
+    /**
      * Adds a quadratic Bézier where the control point and end point of the curve are defined by
      * offsets relative to the last point. If no contour has been created by calling [moveTo] first,
      * the origin of the curve is set to (0, 0).
@@ -255,6 +359,17 @@ public class RemotePathScope constructor() {
         dy2: RemoteFloat,
     ) {
         _nodes.add(RemotePathNode.RelativeQuadTo(dx1, dy1, dx2, dy2))
+    }
+
+    /**
+     * Adds a quadratic Bézier from the last point to [endOffset] relative to the last point,
+     * approaching control point [controlOffset] relative to the last point.
+     *
+     * @param controlOffset The offset of the control point, relative to the last path position
+     * @param endOffset The offset of the end point, relative to the last path position
+     */
+    public fun quadToRelative(controlOffset: RemoteOffset, endOffset: RemoteOffset) {
+        quadToRelative(controlOffset.x, controlOffset.y, endOffset.x, endOffset.y)
     }
 
     /**
@@ -307,6 +422,19 @@ public class RemotePathScope constructor() {
     }
 
     /**
+     * Adds a conic Bézier from the last point to [end], approaching control point [control] with
+     * the given [weight]. If no contour has been created by calling [moveTo] first, the origin of
+     * the curve is set to (0, 0).
+     *
+     * @param control The control point of the conic curve
+     * @param end The end point of the conic curve
+     * @param weight The weight of the control point
+     */
+    public fun conicTo(control: RemoteOffset, end: RemoteOffset, weight: RemoteFloat) {
+        conicTo(control.x, control.y, end.x, end.y, weight)
+    }
+
+    /**
      * Adds a conic Bézier with the given [weight] where the control point and end point of the
      * curve are defined by offsets relative to the last point. If no contour has been created by
      * calling [moveTo] first, the origin of the curve is set to (0, 0).
@@ -329,6 +457,22 @@ public class RemotePathScope constructor() {
         weight: RemoteFloat,
     ) {
         _nodes.add(RemotePathNode.RelativeConicTo(dx1, dy1, dx2, dy2, weight))
+    }
+
+    /**
+     * Adds a conic Bézier from the last point to [endOffset] relative to the last point,
+     * approaching control point [controlOffset] relative to the last point with the given [weight].
+     *
+     * @param controlOffset The offset of the control point, relative to the last path position
+     * @param endOffset The offset of the end point, relative to the last path position
+     * @param weight The weight of the control point
+     */
+    public fun conicToRelative(
+        controlOffset: RemoteOffset,
+        endOffset: RemoteOffset,
+        weight: RemoteFloat,
+    ) {
+        conicToRelative(controlOffset.x, controlOffset.y, endOffset.x, endOffset.y, weight)
     }
 
     /**
@@ -472,5 +616,285 @@ public class RemotePathScope constructor() {
         sweepAngle: RemoteFloat,
     ) {
         arcTo(left, top, right, bottom, startAngle, sweepAngle, forceMoveTo = true)
+    }
+
+    /**
+     * Adds an elliptical arc to the path.
+     *
+     * @param topLeft The top-left corner of the bounding oval
+     * @param size The size of the bounding oval
+     * @param startAngle Starting angle (in degrees) where the arc begins
+     * @param sweepAngle Sweep angle (in degrees) measured clockwise
+     * @param forceMoveTo If true, always begin a new contour with the arc
+     */
+    public fun arcTo(
+        topLeft: RemoteOffset,
+        size: RemoteSize,
+        startAngle: RemoteFloat,
+        sweepAngle: RemoteFloat,
+        forceMoveTo: Boolean = false,
+    ) {
+        arcTo(
+            topLeft.x,
+            topLeft.y,
+            topLeft.x + size.width,
+            topLeft.y + size.height,
+            startAngle,
+            sweepAngle,
+            forceMoveTo = forceMoveTo,
+        )
+    }
+
+    /**
+     * Adds a new subpath with an arc that occupies the rectangle defined by [topLeft] and [size].
+     *
+     * @param topLeft The top-left corner of the bounding rectangle
+     * @param size The size of the bounding rectangle
+     * @param startAngle Starting angle (in degrees) where the arc begins
+     * @param sweepAngle Sweep angle (in degrees) measured clockwise
+     */
+    public fun addArc(
+        topLeft: RemoteOffset,
+        size: RemoteSize,
+        startAngle: RemoteFloat,
+        sweepAngle: RemoteFloat,
+    ) {
+        arcTo(topLeft, size, startAngle, sweepAngle, forceMoveTo = true)
+    }
+
+    /**
+     * Adds a new subpath with an arc that occupies the rectangle defined by [size] starting at
+     * [RemoteOffset.Zero].
+     *
+     * @param size The size of the bounding rectangle
+     * @param startAngle Starting angle (in degrees) where the arc begins
+     * @param sweepAngle Sweep angle (in degrees) measured clockwise
+     */
+    public fun addArc(size: RemoteSize, startAngle: RemoteFloat, sweepAngle: RemoteFloat) {
+        addArc(RemoteOffset.Zero, size, startAngle, sweepAngle)
+    }
+
+    /**
+     * Adds a new subpath that consists of four lines that outline the given rectangle.
+     *
+     * @param left The left bound of the rectangle
+     * @param top The top bound of the rectangle
+     * @param right The right bound of the rectangle
+     * @param bottom The bottom bound of the rectangle
+     */
+    public fun addRect(
+        left: RemoteFloat,
+        top: RemoteFloat,
+        right: RemoteFloat,
+        bottom: RemoteFloat,
+    ) {
+        moveTo(left, top)
+        lineTo(right, top)
+        lineTo(right, bottom)
+        lineTo(left, bottom)
+        close()
+    }
+
+    /**
+     * Adds a new subpath that consists of four lines that outline the rectangle defined by
+     * [topLeft] and [size].
+     *
+     * @param topLeft The top-left corner of the rectangle
+     * @param size The size of the rectangle
+     */
+    public fun addRect(topLeft: RemoteOffset, size: RemoteSize) {
+        addRect(topLeft.x, topLeft.y, topLeft.x + size.width, topLeft.y + size.height)
+    }
+
+    /**
+     * Adds a new subpath that consists of four lines that outline the rectangle defined by [size]
+     * starting at [RemoteOffset.Zero].
+     *
+     * @param size The size of the rectangle
+     */
+    public fun addRect(size: RemoteSize) {
+        addRect(RemoteOffset.Zero, size)
+    }
+
+    /**
+     * Adds a new subpath that consists of the ellipse that fills the given rectangle bounds.
+     *
+     * @param left The left bound of the oval
+     * @param top The top bound of the oval
+     * @param right The right bound of the oval
+     * @param bottom The bottom bound of the oval
+     */
+    public fun addOval(
+        left: RemoteFloat,
+        top: RemoteFloat,
+        right: RemoteFloat,
+        bottom: RemoteFloat,
+    ) {
+        arcTo(left, top, right, bottom, 0f.rf, 360f.rf, forceMoveTo = true)
+        close()
+    }
+
+    /**
+     * Adds a new subpath that consists of the ellipse that fills the rectangle defined by [topLeft]
+     * and [size].
+     *
+     * @param topLeft The top-left corner of the bounding rectangle
+     * @param size The size of the bounding rectangle
+     */
+    public fun addOval(topLeft: RemoteOffset, size: RemoteSize) {
+        addOval(topLeft.x, topLeft.y, topLeft.x + size.width, topLeft.y + size.height)
+    }
+
+    /**
+     * Adds a new subpath that consists of the ellipse that fills the rectangle defined by [size]
+     * starting at [RemoteOffset.Zero].
+     *
+     * @param size The size of the bounding rectangle
+     */
+    public fun addOval(size: RemoteSize) {
+        addOval(RemoteOffset.Zero, size)
+    }
+
+    /**
+     * Adds a new circular subpath centered at [center] with the given [radius].
+     *
+     * @param center The center of the circle
+     * @param radius The radius of the circle
+     */
+    public fun addCircle(center: RemoteOffset, radius: RemoteFloat) {
+        addOval(center.x - radius, center.y - radius, center.x + radius, center.y + radius)
+    }
+
+    /**
+     * Adds a new circular subpath centered at ([centerX], [centerY]) with the given [radius].
+     *
+     * @param centerX The x coordinate of the center of the circle
+     * @param centerY The y coordinate of the center of the circle
+     * @param radius The radius of the circle
+     */
+    public fun addCircle(centerX: RemoteFloat, centerY: RemoteFloat, radius: RemoteFloat) {
+        addOval(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+    }
+
+    /**
+     * Adds a new rounded rectangle subpath to the path.
+     *
+     * @param left The left bound of the rounded rectangle
+     * @param top The top bound of the rounded rectangle
+     * @param right The right bound of the rounded rectangle
+     * @param bottom The bottom bound of the rounded rectangle
+     * @param radiusX The horizontal radius of the rounded corners
+     * @param radiusY The vertical radius of the rounded corners
+     */
+    public fun addRoundRect(
+        left: RemoteFloat,
+        top: RemoteFloat,
+        right: RemoteFloat,
+        bottom: RemoteFloat,
+        radiusX: RemoteFloat,
+        radiusY: RemoteFloat = radiusX,
+    ) {
+        val rx2 = radiusX * 2f
+        val ry2 = radiusY * 2f
+        moveTo(left + radiusX, top)
+        lineTo(right - radiusX, top)
+        arcTo(right - rx2, top, right, top + ry2, 270f.rf, 90f.rf, forceMoveTo = false)
+        lineTo(right, bottom - radiusY)
+        arcTo(right - rx2, bottom - ry2, right, bottom, 0f.rf, 90f.rf, forceMoveTo = false)
+        lineTo(left + radiusX, bottom)
+        arcTo(left, bottom - ry2, left + rx2, bottom, 90f.rf, 90f.rf, forceMoveTo = false)
+        lineTo(left, top + radiusY)
+        arcTo(left, top, left + rx2, top + ry2, 180f.rf, 90f.rf, forceMoveTo = false)
+        close()
+    }
+
+    /**
+     * Adds a new rounded rectangle subpath to the path defined by [topLeft] and [size].
+     *
+     * @param topLeft The top-left corner of the rounded rectangle
+     * @param size The size of the rounded rectangle
+     * @param cornerRadius The corner radii as a [RemoteOffset] (x = horizontal radius, y = vertical
+     *   radius)
+     */
+    public fun addRoundRect(topLeft: RemoteOffset, size: RemoteSize, cornerRadius: RemoteOffset) {
+        addRoundRect(
+            topLeft.x,
+            topLeft.y,
+            topLeft.x + size.width,
+            topLeft.y + size.height,
+            cornerRadius.x,
+            cornerRadius.y,
+        )
+    }
+
+    /**
+     * Adds a new rounded rectangle subpath to the path defined by [size] starting at
+     * [RemoteOffset.Zero].
+     *
+     * @param size The size of the rounded rectangle
+     * @param cornerRadius The corner radii as a [RemoteOffset] (x = horizontal radius, y = vertical
+     *   radius)
+     */
+    public fun addRoundRect(size: RemoteSize, cornerRadius: RemoteOffset) {
+        addRoundRect(RemoteOffset.Zero, size, cornerRadius)
+    }
+
+    /**
+     * Adds a new rounded rectangle subpath to the path defined by [topLeft] and [size].
+     *
+     * @param topLeft The top-left corner of the rounded rectangle
+     * @param size The size of the rounded rectangle
+     * @param radiusX The horizontal radius of the rounded corners
+     * @param radiusY The vertical radius of the rounded corners
+     */
+    public fun addRoundRect(
+        topLeft: RemoteOffset,
+        size: RemoteSize,
+        radiusX: RemoteFloat,
+        radiusY: RemoteFloat = radiusX,
+    ) {
+        addRoundRect(
+            topLeft.x,
+            topLeft.y,
+            topLeft.x + size.width,
+            topLeft.y + size.height,
+            radiusX,
+            radiusY,
+        )
+    }
+
+    /**
+     * Adds a new rounded rectangle subpath to the path defined by [size] starting at
+     * [RemoteOffset.Zero].
+     *
+     * @param size The size of the rounded rectangle
+     * @param radiusX The horizontal radius of the rounded corners
+     * @param radiusY The vertical radius of the rounded corners
+     */
+    public fun addRoundRect(
+        size: RemoteSize,
+        radiusX: RemoteFloat,
+        radiusY: RemoteFloat = radiusX,
+    ) {
+        addRoundRect(RemoteOffset.Zero, size, radiusX, radiusY)
+    }
+
+    /**
+     * Adds the nodes from another [RemotePathScope] to this path.
+     *
+     * @param path The [RemotePathScope] whose nodes should be added
+     */
+    public fun addPath(path: RemotePathScope) {
+        _nodes.addAll(path.nodes)
+    }
+
+    /**
+     * Adds the [nodes] to this path.
+     *
+     * @param nodes The list of [RemotePathNode]s to add
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public fun addPath(nodes: List<RemotePathNode>) {
+        _nodes.addAll(nodes)
     }
 }
--- compose/remote/remote-creation-compose/src/test/java/androidx/compose/remote/creation/compose/vector/RemotePathScopeTest.kt
+++ compose/remote/remote-creation-compose/src/test/java/androidx/compose/remote/creation/compose/vector/RemotePathScopeTest.kt
@@ -17,6 +17,8 @@
 package androidx.compose.remote.creation.compose.vector
 
 import androidx.compose.remote.creation.compose.capture.NoRemoteCompose
+import androidx.compose.remote.creation.compose.layout.RemoteOffset
+import androidx.compose.remote.creation.compose.layout.RemoteSize
 import androidx.compose.remote.creation.compose.state.remotePath
 import androidx.compose.remote.creation.compose.state.rf
 import org.junit.Assert.assertEquals
@@ -81,5 +83,132 @@ class RemotePathScopeTest {
             }
         val floatArray = path.createFloatArray()
         assertTrue(floatArray.isNotEmpty())
+    }
+
+    @Test
+    fun remoteOffsetOverloads_addExpectedNodes() {
+        val scope = RemotePathScope()
+        scope.moveTo(RemoteOffset(10f.rf, 20f.rf))
+        scope.lineTo(RemoteOffset(30f.rf, 40f.rf))
+        scope.quadTo(RemoteOffset(50f.rf, 60f.rf), RemoteOffset(70f.rf, 80f.rf))
+        scope.curveTo(
+            RemoteOffset(90f.rf, 100f.rf),
+            RemoteOffset(110f.rf, 120f.rf),
+            RemoteOffset(130f.rf, 140f.rf),
+        )
+        scope.conicTo(RemoteOffset(150f.rf, 160f.rf), RemoteOffset(170f.rf, 180f.rf), 0.5f.rf)
+        scope.arcTo(RemoteOffset(0f.rf, 0f.rf), RemoteSize(100f.rf, 100f.rf), 0f.rf, 90f.rf)
+        scope.close()
+
+        assertEquals(7, scope.nodes.size)
+        assertTrue(scope.nodes[0] is RemotePathNode.MoveTo)
+        assertTrue(scope.nodes[1] is RemotePathNode.LineTo)
+        assertTrue(scope.nodes[2] is RemotePathNode.QuadTo)
+        assertTrue(scope.nodes[3] is RemotePathNode.CurveTo)
+        assertTrue(scope.nodes[4] is RemotePathNode.ConicTo)
+        assertTrue(scope.nodes[5] is RemotePathNode.AddArc)
+        assertEquals(RemotePathNode.Close, scope.nodes[6])
+    }
+
+    @Test
+    fun addRect_addsFiveNodes() {
+        val scope = RemotePathScope()
+        scope.addRect(RemoteOffset(10f.rf, 20f.rf), RemoteSize(100f.rf, 50f.rf))
+
+        assertEquals(5, scope.nodes.size)
+        val move = scope.nodes[0] as RemotePathNode.MoveTo
+        assertEquals(10f, move.x.constantValueOrNull)
+        assertEquals(20f, move.y.constantValueOrNull)
+
+        val line1 = scope.nodes[1] as RemotePathNode.LineTo
+        assertEquals(110f, line1.x.constantValueOrNull)
+        assertEquals(20f, line1.y.constantValueOrNull)
+
+        val line2 = scope.nodes[2] as RemotePathNode.LineTo
+        assertEquals(110f, line2.x.constantValueOrNull)
+        assertEquals(70f, line2.y.constantValueOrNull)
+
+        val line3 = scope.nodes[3] as RemotePathNode.LineTo
+        assertEquals(10f, line3.x.constantValueOrNull)
+        assertEquals(70f, line3.y.constantValueOrNull)
+
+        assertEquals(RemotePathNode.Close, scope.nodes[4])
+    }
+
+    @Test
+    fun addArc_addsArcWithForceMoveTo() {
+        val scope = RemotePathScope()
+        scope.addArc(RemoteOffset(10f.rf, 20f.rf), RemoteSize(100f.rf, 50f.rf), 0f.rf, 90f.rf)
+
+        assertEquals(1, scope.nodes.size)
+        val arc = scope.nodes[0] as RemotePathNode.AddArc
+        assertEquals(10f, arc.left.constantValueOrNull)
+        assertEquals(20f, arc.top.constantValueOrNull)
+        assertEquals(110f, arc.right.constantValueOrNull)
+        assertEquals(70f, arc.bottom.constantValueOrNull)
+        assertEquals(0f, arc.startAngle.constantValueOrNull)
+        assertEquals(90f, arc.sweepAngle.constantValueOrNull)
+        assertTrue(arc.forceMoveTo)
+    }
+
+    @Test
+    fun addOval_addsArcAndClose() {
+        val scope = RemotePathScope()
+        scope.addOval(RemoteOffset(0f.rf, 0f.rf), RemoteSize(100f.rf, 100f.rf))
+
+        assertEquals(2, scope.nodes.size)
+        val arc = scope.nodes[0] as RemotePathNode.AddArc
+        assertEquals(0f, arc.left.constantValueOrNull)
+        assertEquals(0f, arc.top.constantValueOrNull)
+        assertEquals(100f, arc.right.constantValueOrNull)
+        assertEquals(100f, arc.bottom.constantValueOrNull)
+        assertEquals(0f, arc.startAngle.constantValueOrNull)
+        assertEquals(360f, arc.sweepAngle.constantValueOrNull)
+        assertTrue(arc.forceMoveTo)
+
+        assertEquals(RemotePathNode.Close, scope.nodes[1])
+    }
+
+    @Test
+    fun addCircle_addsOval() {
+        val scope = RemotePathScope()
+        scope.addCircle(RemoteOffset(50f.rf, 50f.rf), 25f.rf)
+
+        assertEquals(2, scope.nodes.size)
+        val arc = scope.nodes[0] as RemotePathNode.AddArc
+        assertEquals(25f, arc.left.constantValueOrNull)
+        assertEquals(25f, arc.top.constantValueOrNull)
+        assertEquals(75f, arc.right.constantValueOrNull)
+        assertEquals(75f, arc.bottom.constantValueOrNull)
+        assertEquals(RemotePathNode.Close, scope.nodes[1])
+    }
+
+    @Test
+    fun addRoundRect_addsArcsAndLines() {
+        val scope = RemotePathScope()
+        scope.addRoundRect(
+            RemoteOffset(0f.rf, 0f.rf),
+            RemoteSize(100f.rf, 100f.rf),
+            RemoteOffset(10f.rf, 10f.rf),
+        )
+
+        // 1 MoveTo + (1 LineTo + 1 ArcTo) * 4 + 1 Close = 10 nodes
+        assertEquals(10, scope.nodes.size)
+        assertTrue(scope.nodes[0] is RemotePathNode.MoveTo)
+        assertEquals(RemotePathNode.Close, scope.nodes[9])
+    }
+
+    @Test
+    fun addPath_combinesNodes() {
+        val path1 = RemotePathScope()
+        path1.moveTo(0f.rf, 0f.rf)
+        path1.lineTo(10f.rf, 10f.rf)
+
+        val path2 = RemotePathScope()
+        path2.moveTo(20f.rf, 20f.rf)
+        path2.lineTo(30f.rf, 30f.rf)
+
+        path1.addPath(path2)
+        assertEquals(4, path1.nodes.size)
     }
 }
