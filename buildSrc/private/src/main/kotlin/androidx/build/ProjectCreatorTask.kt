a26633a5ef82cedaf5181ba8e4ce70cc9a664e79
Alex Vanyo <vanyo@google.com>
2026-08-24 21:48:59 +0000

    Partially revert compileSdk bumps from aosp/4176117
    In aosp/4176117, compileSdk was bumped to 37.1 across 257 Compose-related
    modules. This was more broad than necessary, as only compose:ui:ui and
    the modules that depend on it in their main source sets or runtime
    application classpath actually require compileSdk 37.1.
    
    This change reverts the compileSdk bump on 158 modules that do not
    require compileSdk 37.1, returning them to their prior compileSdk
    values (35, 36, 37, or default).
    
    Test: ./gradlew checkReleaseAarMetadata checkAndroidMainAarMetadata checkDebugAarMetadata
    Test: ./gradlew :buildSrc-tests:test
    Relnote: N/A
    Bug: 524717365
    TAG=agy
    CONV=f6fe7d14-7d10-4c08-bbc1-556c66dccf4f
    
    Change-Id: I36336ac4e5c6d13568c7e46fc2ec96206bdf9ec6

--- buildSrc/private/src/main/kotlin/androidx/build/ProjectCreatorTask.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/ProjectCreatorTask.kt
@@ -635,7 +635,7 @@ internal class ProjectGenerator {
                 """
                 androidLibrary {
                     namespace = "androidx.compose.${artifactId.removePrefix("compose-").replace("-", ".")}"
-                    compileSdk { version = release(37) { minorApiLevel = 1 } }
+                    compileSdk { version = release(35) }
                 }
                 jvmStubs()
                 linuxX64Stubs()
