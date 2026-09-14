7ae07c9f4299ae0d14f5a789213332777b9eaf73
kentfan <kentfan@google.com>
2026-09-14 10:50:39 -0700

    [analytics] Add kdoc tag for top level m3 expressive ShortNavigationBar APIs
    Fix: b/558816299
    Test: N/A
    Change-Id: I9a28f14be4c6536b5a62cad10f7743a9b6c2026a

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/ShortNavigationBar.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/ShortNavigationBar.kt
@@ -88,6 +88,8 @@ import kotlin.math.roundToInt
  * @param windowInsets a window insets of the navigation bar
  * @param arrangement the [ShortNavigationBarArrangement] of this navigation bar
  * @param content the content of this navigation bar, typically [ShortNavigationBarItem]s
+ *
+ * @material3expressive
  */
 @Composable
 public fun ShortNavigationBar(
@@ -183,6 +185,8 @@ public value class ShortNavigationBarArrangement private constructor(private val
  *   emitting [Interaction]s for this item. You can use this to change the item's appearance or
  *   preview the item in different states. Note that if `null` is provided, interactions will still
  *   happen internally.
+ *
+ * @material3expressive
  */
 @Composable
 public fun ShortNavigationBarItem(
