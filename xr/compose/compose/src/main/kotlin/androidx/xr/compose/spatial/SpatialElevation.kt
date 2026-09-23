d991aeaa956539378a214c0cb342d788a3f6ab01
George Mount <mount@google.com>
2026-09-23 14:09:52 -0700

    Enable isDelayedWindowInsetsRulersEnabled by default
    Enables the isDelayedWindowInsetsRulersEnabled feature flag in AndroidComposeUiFlags by default.
    
    Test: ./gradlew :compose:ui:ui:connectedAndroidTest
    Test: ./gradlew :xr:compose:compose:testReleaseUnitTest
    Bug: 516858588
    Relnote: "If tests fail with `AndroidComposeUiFlags`.`isDelayedWindowInsetsRulersEnabled` enabled, they likely rely on a superfluous recomposition and should be corrected."
    Change-Id: I0fa5d3ab703ab4152bdb50560e2ce61d20e71cbd

--- xr/compose/compose/src/main/kotlin/androidx/xr/compose/spatial/SpatialElevation.kt
+++ xr/compose/compose/src/main/kotlin/androidx/xr/compose/spatial/SpatialElevation.kt
@@ -47,6 +47,7 @@ import androidx.xr.compose.R
 import androidx.xr.compose.platform.LocalSession
 import androidx.xr.compose.platform.LocalSpatialCapabilities
 import androidx.xr.compose.platform.findNearestParentEntity
+import androidx.xr.compose.subspace.SpatialComposeView
 import androidx.xr.compose.subspace.layout.CoreEntity
 import androidx.xr.compose.subspace.layout.CorePanelEntity
 import androidx.xr.compose.subspace.spatialComposeView
@@ -207,7 +208,7 @@ private class SpatialElevationRenderer(
             }
         }
 
-    private var view: ComposeView? = null
+    private var view: SpatialComposeView? = null
     private var panelEntity: CorePanelEntity? = null
 
     override fun onRemembered() {
